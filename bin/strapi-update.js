b7d55291cdd1b48fc3b67419411ec9731d23d9bc
loicsaintroch <hack@loic.io>
2015-10-08 09:12:55 +0200

    Fix update for custom generators

--- bin/strapi-update.js
+++ bin/strapi-update.js
@@ -38,43 +38,43 @@ module.exports = function () {
   fs.access(path.resolve(HOME, '.strapirc'), fs.F_OK | fs.R_OK | fs.W_OK, function (err) {
     if (err) {
       if (err.code === 'ENOENT') {
-        logger.error('No `.strapirc` file detected at `' + HOME + '`');
-        logger.error('Execute `$ strapi config` to create one');
+        logger.error('No `.strapirc` file detected at `' + HOME + '`.');
+        logger.error('Execute `$ strapi config` to create one.');
       } else if (err.code === 'EACCES') {
-        logger.error('Impossible to access the `.strapirc` file at `' + HOME + '`');
-        logger.error('Please check read/write permissions before execute `$ strapi update`');
+        logger.error('Impossible to access the `.strapirc` file at `' + HOME + '`.');
+        logger.error('Please check read/write permissions before execute `$ strapi update`.');
       }
       process.exit(1);
     } else {
       const config = JSON.parse(fs.readFileSync(path.resolve(HOME, '.strapirc')));
-      _.forEach(config.repositories, function (info, name) {
+      _.forEach(config.generators, function (info, name) {
         try {
-          process.chdir(path.resolve(__dirname, '..', 'node_modules', name));
-          logger.debug('Pulling the latest updates of `' + name + '`');
+          process.chdir(path.resolve(__dirname, '..', 'node_modules', 'strapi-generate-' + name));
+          logger.debug('Pulling the latest updates of `strapi-generate-' + name + '`.');
           exec('git pull ' + info.remote + ' ' + info.branch, function (err) {
             if (err) {
-              logger.error('Impossible to update `' + name + '`');
+              logger.error('Impossible to update `strapi-generate-' + name + '`.');
             } else {
-              logger.info('Successfully updated `' + name + '`');
+              logger.info('Successfully updated `strapi-generate-' + name + '`.');
             }
           });
         } catch (err) {
           process.chdir(path.resolve(__dirname, '..', 'node_modules'));
-          logger.debug('Cloning the `' + name + '` repository for the first time...');
-          exec('git clone ' + info.repository + ' ' + name, function (err) {
+          logger.debug('Cloning the `strapi-generate-' + name + '` repository for the first time...');
+          exec('git clone ' + info.repository + ' strapi-generate-' + name, function (err) {
             if (err) {
-              logger.error('Impossible to clone the `' + name + '` repository');
+              logger.error('Impossible to clone the `strapi-generate-' + name + '` repository.');
               console.log(err);
             } else {
-              logger.info('Successfully cloned the `' + name + '` repository');
-              process.chdir(path.resolve(__dirname, '..', 'node_modules', name));
-              logger.debug('Installing dependencies for `' + name + '`...');
+              logger.info('Successfully cloned the `strapi-generate-' + name + '` repository.');
+              process.chdir(path.resolve(__dirname, '..', 'node_modules', 'strapi-generate-' + name));
+              logger.debug('Installing dependencies for `strapi-generate-' + name + '`...');
               exec('npm install', function (err) {
                 if (err) {
-                  logger.error('Impossible to install dependencies for `' + name + '`');
+                  logger.error('Impossible to install dependencies for `strapi-generate-' + name + '`.');
                   console.log(err);
                 } else {
-                  logger.info('Successfully installed dependencies for `' + name + '`');
+                  logger.info('Successfully installed dependencies for `strapi-generate-' + name + '`.');
                 }
               });
             }
