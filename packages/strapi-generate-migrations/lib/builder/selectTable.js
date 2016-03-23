aad090c62d96eeb1e97915006634bef0f3db2801
Aurélien Georget <aurelsicoko@gmail.com>
2016-03-23 16:07:39 +0100

    Generated migration files manages attribute's type update

--- packages/strapi-generate-migrations/lib/builder/selectTable.js
+++ packages/strapi-generate-migrations/lib/builder/selectTable.js
@@ -17,38 +17,48 @@ const _ = require('lodash');
 
 module.exports = function (models, modelName) {
 
-  models[modelName].up = {};
+  if (!models[modelName].hasOwnProperty('up')) {
+    models[modelName].up = {
+      drop: '',
+      others: ''
+    };
+  }
 
   // Template: select the table for the `up` export.
   // Every attribute with `create` key will be added in this template.
   const tplSelectTableUp = fs.readFileSync(path.resolve(__dirname, '..', '..', 'templates', 'builder', 'tables', 'select', 'up.template'), 'utf8');
-  models[modelName].up.drop = _.unescape(_.template(tplSelectTableUp)({
+  models[modelName].up.drop += _.unescape(_.template(tplSelectTableUp)({
     models: models,
     tableName: modelName,
     attributes: models[modelName].newAttributes,
     toDrop: true
   }));
 
-  models[modelName].up.others = _.unescape(_.template(tplSelectTableUp)({
+  models[modelName].up.others += _.unescape(_.template(tplSelectTableUp)({
     models: models,
     tableName: modelName,
     attributes: models[modelName].newAttributes,
     toDrop: false
   }));
 
-  models[modelName].down = {};
+  if (!models[modelName].hasOwnProperty('down')) {
+    models[modelName].down = {
+      drop: '',
+      others: ''
+    };
+  }
 
   // Template: select the table for the `down` export.
   // Every attribute with `delete` key will be added in this template.
   const tplSelectTableDown = fs.readFileSync(path.resolve(__dirname, '..', '..', 'templates', 'builder', 'tables', 'select', 'down.template'), 'utf8');
-  models[modelName].down.drop = _.unescape(_.template(tplSelectTableDown)({
+  models[modelName].down.drop += _.unescape(_.template(tplSelectTableDown)({
     models: models,
     tableName: modelName,
     attributes: models[modelName].newAttributes,
     toDrop: true
   }));
 
-  models[modelName].down.others = _.unescape(_.template(tplSelectTableDown)({
+  models[modelName].down.others += _.unescape(_.template(tplSelectTableDown)({
     models: models,
     tableName: modelName,
     attributes: models[modelName].newAttributes,
