6df354d8096f7a9fda20177c46c0eb6b3242aab1
loicsaintroch <hack@loic.io>
2016-04-04 21:53:52 +0200

    Add basic tests about Koa core and middlewares

---
+++ packages/strapi/test/application.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('assert');
+
+const strapi = require('../lib/');
+
+/**
+ * No need to test everything about Koa.
+ * We just need need to make sure that everything
+ * is correctly required and loaded inside `strapi`.
+ */
+
+describe('application', function () {
+  it('`strapi.app` should be an object', function () {
+    assert(typeof strapi.app === 'object');
+  });
+
+  it('`strapi.app.use` should be a function', function () {
+    assert(typeof strapi.app.use === 'function');
+  });
+
+  it('`strapi.app.context` should be an object', function () {
+    assert(typeof strapi.app.context === 'object');
+  });
+
+  it('`strapi.app.request` should be an object', function () {
+    assert(typeof strapi.app.request === 'object');
+  });
+
+  it('`strapi.app.response` should be an object', function () {
+    assert(typeof strapi.app.response === 'object');
+  });
+});
