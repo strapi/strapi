0007f53a9118e8c3bb6d8469160e36357bd44253
Aurélien Georget <aurelsicoko@gmail.com>
2016-08-19 12:33:19 +0200

    Use forever-monitor to reload server on changes (#130)

--- packages/strapi/bin/strapi-start.js
+++ packages/strapi/bin/strapi-start.js
@@ -9,9 +9,9 @@
 // Node.js core.
 const path = require('path');
 
-// Local Strapi dependencies.
-const isLocalStrapiValid = require('../lib/private/isLocalStrapiValid');
-const packageJSON = require('../package.json');
+// Public dependencies
+const _ = require('lodash');
+const forever = require('forever-monitor');
 
 // Logger.
 const logger = require('strapi-utils').logger;
@@ -24,30 +24,42 @@ const logger = require('strapi-utils').logger;
  */
 
 module.exports = function () {
+  try {
+    // Require server configurations
+    const server = require(path.resolve(process.cwd(), 'config', 'environments', 'development', 'server.json'));
+    const options = {};
 
-  // Build initial scope.
-  const scope = {
-    rootPath: process.cwd(),
-    strapiPackageJSON: packageJSON
-  };
-
-  // Use the app's local `strapi` in `node_modules` if it's existant and valid.
-  const localStrapiPath = path.resolve(scope.rootPath, 'node_modules', 'strapi');
-
-  if (isLocalStrapiValid(localStrapiPath, scope.rootPath)) {
-    return require(localStrapiPath).start(scope, afterwards);
-  }
-
-  // Otherwise, if no workable local `strapi` module exists,
-  // run the application using the currently running version
-  // of `strapi`. This is probably always the global install.
-  return require('../lib/')().start(scope, afterwards);
-
-  function afterwards(err, strapi) {
-    if (err) {
-      const message = err.stack ? err.stack : err;
-      logger.error(message);
-      strapi ? strapi.stop() : process.exit(1);
+    // Set NODE_ENV
+    if (_.isEmpty(process.env.NODE_ENV)) {
+      process.env.NODE_ENV = 'development';
     }
+
+    if (server.reload === true && process.env.NODE_ENV === 'development') {
+      _.assign(options, {
+        silent: false,
+        watch: true,
+        watchDirectory: process.cwd(),
+        killTree: true // Kills the entire child process tree on `exit`
+      });
+    } else {
+      _.assign(options, {
+        silent: process.env.NODE_ENV === 'production',
+        watch: false
+      });
+    }
+
+    const child = new (forever.Monitor)('server.js', options);
+
+    // Run listeners
+    child.on('restart', function() {
+      logger.info('Restarting due to changes...');
+      console.log();
+    });
+
+    // Start child process
+    child.start();
+  } catch (e) {
+    console.error(e);
+    process.exit(0);
   }
 };
