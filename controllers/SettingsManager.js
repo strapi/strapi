d8e5730ddff87907b2393fe9d02c3c8a140dc73f
Pierre BURGY <pierre.burgy@gmail.com>
2016-08-26 14:07:57 +0200

    Integrate in Strapi project

---
+++ controllers/SettingsManager.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `SettingsManager`
+ */
+
+module.exports = {
+
+  find: function *() {
+    this.body = [];
+  }
+
+};
---
+++ services/SettingsManager.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+// Public dependencies.
+const _ = require('lodash');
+
+/**
+ * A set of functions called "actions" for `SettingsManager`
+ */
+
+module.exports = {
+
+};
