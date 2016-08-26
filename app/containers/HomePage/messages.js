28e993710b103201f63677df23d4bc725d78e410
Pierre BURGY <pierre.burgy@gmail.com>
2016-08-26 11:47:18 +0200

    Register plugin logic

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.HomePage.header',
-    defaultMessage: 'This is HomePage components !',
+    defaultMessage: 'This is Settings Manager plugin!',
   },
 });
