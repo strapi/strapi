0c76f27e78a0265822826b9f49506aebb7b3db20
Pierre BURGY <pierre.burgy@gmail.com>
2016-08-30 15:07:53 +0200

    Plugins injector logic for production

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const sendfile = require('koa-sendfile');
+const $ = require('cheerio');
+const fs = require('fs');
 const path = require('path');
-
+const sendfile = require('koa-sendfile');
+const _ = require('lodash');
 /**
  * A set of functions called "actions" for `Admin`
  */
@@ -10,24 +12,38 @@ const path = require('path');
 module.exports = {
 
   index: function *() {
-    console.log('index');
 
-    // Send the admin build
-    yield sendfile(this, path.resolve(__dirname, '..', 'public', 'build', 'index.html'));
-    if (!this.status) this.throw(404);
+    // Read the default `index.html` file from the admin panel build
+    const html = fs.readFileSync(path.resolve(__dirname, '..', 'public', 'build', 'index.html'));
+
+    // Convert the stream to a string
+    const htmlString = html.toString();
+
+    // Use `cheerio` to parse the HTML string
+    const parsedHTML = $.load(htmlString);
+
+    // Some plugins are ignored
+    const ignoredPlugins = ['admin', 'user'];
+
+    // Inject `js` files from plugins builds in the main admin panel
+    _.forEach(strapi.api, (value, pluginName) => {
+      if (!_.includes(ignoredPlugins, pluginName)) {
+        // Main plugin `js` file
+        const pluginMainScript = $('<script>').attr('src', '/plugins/' + pluginName + '/main.js');
+        parsedHTML('body').append(pluginMainScript);
+
+        // Vendors plugin `js` file
+        const pluginVendorScript = $('<script>').attr('src', '/plugins/' + pluginName + '/vendor.js');
+        parsedHTML('body').append(pluginVendorScript);
+      }
+    });
+    
+    // Finally, send the HTML file with injected scripts
+    this.body = parsedHTML.html();
   },
 
   file: function *() {
-  console.log('file');
-    // TODO: manage assets
-    console.log(this.params) ;
-
-
-    // Send the admin build
-    // TODO: manage assets
     yield sendfile(this, path.resolve(__dirname, '..', 'public', 'build', this.params.file));
     if (!this.status) this.throw(404);
   }
-
-
 };
