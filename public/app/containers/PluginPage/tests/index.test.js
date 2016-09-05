d45fb3ae09994a66826d5e6943d534da7ff1e31e
Pierre BURGY <pierre.burgy@gmail.com>
2016-09-05 15:31:04 +0200

    Use npm run test for pre-commit

--- public/app/containers/PluginPage/tests/index.test.js
+++ public/app/containers/PluginPage/tests/index.test.js
@@ -6,6 +6,6 @@ import expect from 'expect';
 
 describe('<PluginPage />', () => {
   it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+    expect(true).toEqual(true);
   });
 });
