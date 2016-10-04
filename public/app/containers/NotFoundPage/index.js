890b0295c2ec17a9b694648b186935a6eb9a20b1
Pierre Burgy <pierre.burgy@gmail.com>
2016-10-04 16:55:23 +0200

    Improve not found page

--- public/app/containers/NotFoundPage/index.js
+++ public/app/containers/NotFoundPage/index.js
@@ -12,14 +12,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import Container from 'components/Container';
 
 export default class NotFound extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <div className="container">
+          <h1>
+            <FormattedMessage {...messages.header} />
+          </h1>
+        </div>
+      </div>
     );
   }
 }
--- public/app/containers/NotFoundPage/messages.js
+++ public/app/containers/NotFoundPage/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.NotFoundPage.header',
-    defaultMessage: 'This is NotFoundPage component !',
+    defaultMessage: 'Page not found.',
   },
 });
