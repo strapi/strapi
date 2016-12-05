c5f8070df6b6fceeb0033793b0826d44f96c9355
Aurélien Georget <aurelsicoko@gmail.com>
2016-12-05 17:52:51 +0100

    Run npm install before running tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,5 @@ fi
 
 node --harmony-async-await node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
 cd packages/strapi-generate-admin/files/admin/public
+npm install
 npm run test
