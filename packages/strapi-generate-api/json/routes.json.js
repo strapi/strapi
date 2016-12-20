b5c6ebc01655b05684c92775d2235deda124d8b9
Aurélien Georget <aurelsicoko@gmail.com>
2016-12-20 20:41:42 +0100

    Allow to generate an API inside an existing API

--- packages/strapi-generate-api/json/routes.json.js
+++ packages/strapi-generate-api/json/routes.json.js
@@ -16,42 +16,63 @@ const _ = require('lodash');
  */
 
 module.exports = scope => {
-  return {
-    routes: [{
-      method: 'GET',
-      path: '/ ' + scope.humanizeId,
-      handler: scope.globalID + '.find',
-      config: {
-        policies: []
-      }
-    }, {
-      method: 'GET',
-      path: '/ ' + scope.humanizeId + '/:id',
-      handler: scope.globalID + '.findOne',
-      config: {
-        policies: []
-      }
-    }, {
-      method: 'POST',
-      path: '/ ' + scope.humanizeId,
-      handler: scope.globalID + '.create',
-      config: {
-        policies: []
-      }
-    }, {
-      method: 'PUT',
-      path: '/ ' + scope.humanizeId + '/:id',
-      handler: scope.globalID + '.update',
-      config: {
-        policies: []
-      }
-    }, {
-      method: 'DELETE',
-      path: '/ ' + scope.humanizeId + '/:id',
-      handler: scope.globalID + '.destroy',
-      config: {
-        policies: []
-      }
-    }]
-  };
+  function generateRoutes() {
+    return {
+      routes: [{
+        method: 'GET',
+        path: '/' + (scope.humanizeSubId || scope.humanizeId),
+        handler: scope.globalID + '.find',
+        config: {
+          policies: []
+        }
+      }, {
+        method: 'GET',
+        path: '/' + (scope.humanizeSubId || scope.humanizeId) + '/:id',
+        handler: scope.globalID + '.findOne',
+        config: {
+          policies: []
+        }
+      }, {
+        method: 'POST',
+        path: '/' + (scope.humanizeSubId || scope.humanizeId),
+        handler: scope.globalID + '.create',
+        config: {
+          policies: []
+        }
+      }, {
+        method: 'PUT',
+        path: '/' + (scope.humanizeSubId || scope.humanizeId) + '/:id',
+        handler: scope.globalID + '.update',
+        config: {
+          policies: []
+        }
+      }, {
+        method: 'DELETE',
+        path: '/' + (scope.humanizeSubId || scope.humanizeId) + '/:id',
+        handler: scope.globalID + '.destroy',
+        config: {
+          policies: []
+        }
+      }]
+    };
+  }
+
+  // We have to delete current file
+  if (!_.isEmpty(scope.subId)) {
+    const current = require(scope.rootPath);
+
+    try {
+      // Remove current routes.json
+      fs.unlinkSync(scope.rootPath);
+      // Merge both array of routes
+      current.routes = _.concat(generateRoutes().routes, current.routes);
+
+      return current;
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+  }
+
+  return generateRoutes();
 };
