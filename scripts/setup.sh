80aa83d8460c95366547e143c74bf79ea6ae69f8
Aurélien Georget <aurelsicoko@gmail.com>
2017-01-15 17:57:43 +0100

    Remove existing binary to fix dev env setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+rm -f /usr/local/bin/strapi.js # Remove existing binary
 echo "${GREEN}Installing dependencies..."
 npm install
 echo "${GREEN}Bootstraping packages and building dashboard...${NC}"
