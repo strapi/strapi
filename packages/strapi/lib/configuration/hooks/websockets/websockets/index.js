6e462cff5dcdb76a051f824309864b806982b6e1
Aurélien Georget <aurelsicoko@gmail.com>
2017-01-15 17:53:02 +0100

    Update websocket hook

--- packages/strapi/lib/configuration/hooks/websockets/websockets/index.js
+++ packages/strapi/lib/configuration/hooks/websockets/websockets/index.js
@@ -26,13 +26,6 @@ module.exports = strapi => {
     initialize: cb => {
       if (strapi.config.websockets === true) {
         global.io = require('socket.io')(strapi.server);
-
-        if (strapi.config.reload.workers > 0) {
-          strapi.log.warn('Strapi doesn\'t handle WebSockets using multiple nodes yet.');
-          strapi.log.warn('WebSockets only work on the master process.');
-          strapi.log.warn('You can disable the `websockets` and implement Socket.IO on your own.');
-          strapi.log.warn('Feel free to join us on Slack to talk about it: http://slack.strapi.io/');
-        }
       }
 
       cb();
