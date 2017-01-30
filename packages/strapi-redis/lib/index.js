4f15be4871e3332285108e0ff44eb016f13ca91b
Aurélien Georget <aurelsicoko@gmail.com>
2017-01-30 15:44:47 +0100

    Initialize strapi-redis hook

---
+++ packages/strapi-redis/lib/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+/**
+ * Redis hook
+ */
+
+module.exports = function () {
+  const hook = {
+
+    /**
+     * Default options
+     */
+
+    defaults: {},
+
+    /**
+     * Initialize the hook
+     */
+
+    initialize: cb => {
+      cb();
+    }
+  };
+
+  return hook;
+};
