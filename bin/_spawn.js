99e9502f6c7e143707307174fde9ca999ab9e2f8
Pierre Burgy <pierre.burgy@gmail.com>
2017-04-24 14:12:06 +0200

    Remove harmony flag

--- bin/_spawn.js
+++ bin/_spawn.js
@@ -3,6 +3,6 @@
 var spawn = require('child_process').spawn;
 var args = [ __dirname + '/strapi.js' ].concat(process.argv.slice(2));
 
-spawn(process.argv[0], ['--harmony'].concat(args), {
+spawn(process.argv[0], args, {
   stdio: [0, 1, 2]
 });
