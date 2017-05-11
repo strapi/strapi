666553ea30b6e81fd9fb037c1c092d18853be205
Pierre Burgy <pierre.burgy@gmail.com>
2017-05-11 17:30:00 +0200

    Separate strapi-generate-admin and strapi-admin packages

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,6 @@ if [ -z "$TEST_GREP" ]; then
 fi
 
 node --harmony-async-await node_modules/mocha/bin/_mocha `scripts/_get-test-directories.sh` --opts test/mocha.opts --grep "$TEST_GREP"
-cd packages/strapi-generate-admin/files/admin/public
+cd packages/strapi-admin
 npm install
 npm run test
