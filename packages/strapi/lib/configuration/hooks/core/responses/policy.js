3529b9fd5e4c4a94e01f1286e91a9c1e42ed3188
Pierre Burgy <pierre.burgy@gmail.com>
2017-06-25 21:41:51 +0200

    Set status on error

--- packages/strapi/lib/configuration/hooks/core/responses/policy.js
+++ packages/strapi/lib/configuration/hooks/core/responses/policy.js
@@ -44,9 +44,10 @@ module.exports = async function(ctx, next) {
     strapi.log.error(error);
 
     // Wrap error into a Boom's response.
+    ctx.status = error.status || 500;
     ctx.body = _.get(ctx.body, 'isBoom')
       ? ctx.body || error && error.message
-      : Boom.wrap(error, error.status, ctx.body || error.message);
+      : Boom.wrap(error, ctx.status, ctx.body || error.message);
   }
 
   // Empty body is considered as `notFound` response.
