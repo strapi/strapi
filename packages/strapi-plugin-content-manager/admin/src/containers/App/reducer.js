1fb39080c399191f71853fade4e50f3acb7c53b7
Pierre Burgy <pierre.burgy@gmail.com>
2017-07-06 17:51:13 +0200

    Auto generate admin fields

--- packages/strapi-plugin-content-manager/admin/src/containers/App/reducer.js
+++ packages/strapi-plugin-content-manager/admin/src/containers/App/reducer.js
@@ -9,7 +9,7 @@ import { fromJS } from 'immutable';
 import { LOAD_MODELS, LOADED_MODELS, UPDATE_SCHEMA } from './constants';
 
 const initialState = fromJS({
-  loading: false,
+  loading: true,
   models: false,
   schema: false,
 });
@@ -17,11 +17,14 @@ const initialState = fromJS({
 function appReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_MODELS:
-      return state.set('loading', true);
+      return state;
     case LOADED_MODELS:
-      return state.set('loading', false).set('models', action.models);
+      return state
+        .set('models', action.models);
     case UPDATE_SCHEMA:
-      return state.set('schema', action.schema);
+      return state
+        .set('loading', false)
+        .set('schema', action.schema);
     default:
       return state;
   }
--- packages/strapi-plugin-content-manager/admin/src/containers/App/sagas.js
+++ packages/strapi-plugin-content-manager/admin/src/containers/App/sagas.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
-import {takeLatest} from 'redux-saga';
-import {fork, put} from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { fork, put, select } from 'redux-saga/effects';
 
-import {loadedModels} from './actions';
-import {LOAD_MODELS, UPDATE_SCHEMA} from './constants';
+import { generateSchema } from 'utils/schema';
+
+import { loadedModels, updateSchema } from './actions';
+import { LOAD_MODELS, LOADED_MODELS, UPDATE_SCHEMA } from './constants';
+import { makeSelectModels } from './selectors';
+
 
 export function* getModels() {
   try {
@@ -26,6 +30,22 @@ export function* getModels() {
   }
 }
 
+export function* modelsLoaded() {
+  const models = yield select(makeSelectModels());
+  let schema;
+
+  try {
+    schema = generateSchema(models);
+  } catch (err) {
+    window.Strapi.notification.error(
+      'An error occurred during schema generation.'
+    );
+    throw new Error(err);
+  }
+
+  yield put(updateSchema(schema));
+}
+
 export function* schemaUpdated(action) {
   // Display the links only if the `displayed` attribute is not set to false
   const displayedModels = _.pickBy(action.schema, model => (model.displayed !== false));
@@ -44,6 +64,7 @@ export function* schemaUpdated(action) {
 export function* defaultSaga() {
   yield fork(takeLatest, LOAD_MODELS, getModels);
   yield fork(takeLatest, UPDATE_SCHEMA, schemaUpdated);
+  yield fork(takeLatest, LOADED_MODELS, modelsLoaded);
 }
 
 // All sagas to be loaded
