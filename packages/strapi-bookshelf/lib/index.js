f316b51e334d64b8620ad6786cac7541ffcc31b3
Pierre Burgy <pierre.burgy@gmail.com>
2017-07-17 10:54:27 +0200

    Remove useless default config in strapi-bookshelf

--- packages/strapi-bookshelf/lib/index.js
+++ packages/strapi-bookshelf/lib/index.js
@@ -117,11 +117,6 @@ module.exports = function(strapi) {
           _.forEach(models, (definition, model) => {
             globalName = _.upperFirst(_.camelCase(definition.globalId));
 
-            // Set the default values to model settings.
-            _.defaults(definition, {
-              primaryKey: 'id'
-            });
-
             // Make sure the model has a table name.
             // If not, use the model name.
             if (_.isEmpty(definition.tableName)) {
