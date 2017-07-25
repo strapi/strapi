cbdca752745b2d6a2055c6e746922db8f7b880ba
cyril lopez <cyril.lpz@gmail.com>
2017-07-25 17:03:47 +0200

    Handle language delete

--- packages/strapi-plugin-settings-manager/admin/src/containers/Home/constants.js
+++ packages/strapi-plugin-settings-manager/admin/src/containers/Home/constants.js
@@ -14,3 +14,7 @@ export const DEFAULT_ACTION = 'SettingsManager/Home/DEFAULT_ACTION';
 export const EDIT_SETTINGS = 'SettingsManager/Home/EDIT_SETTINGS';
 export const EDIT_SETTINGS_SUCCEEDED = 'SettingsManager/Home/EDIT_SETTINGS_SUCCEEDED';
 export const CHANGE_DEFAULT_LANGUAGE = 'SettingsManager/Home/CHANGE_DEFAULT_LANGUAGE';
+export const NEW_LANGUAGE_POST = 'SettingsManager/Home/NEW_LANGUAGE_POST';
+export const NEW_LANGUAGE_POST_SUCCEEDED = 'SettingsManager/Home/NEW_LANGUAGE_POST_SUCCEEDED';
+export const LANGUAGE_DELETE = 'SettingsManager/Home/LANGUAGE_DELETE';
+export const LANGUAGE_ACTION_SUCCEEDED = 'SettingsManager/Home/LANGUAGE_ACTION_SUCCEEDED';
