14933b38b9907e89a5fbfe6486f9b8a3fc6c5754
cyril lopez <cyril.lpz@gmail.com>
2017-08-01 17:00:37 +0200

    Remove styles const from EditFormSection

--- packages/strapi-plugin-settings-manager/admin/src/components/EditFormSection/index.js
+++ packages/strapi-plugin-settings-manager/admin/src/components/EditFormSection/index.js
@@ -15,15 +15,13 @@ import EditFormSectionNested from 'components/EditFormSectionNested';
 class EditFormSection extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     const sectionName = isEmpty(this.props.section.name) ? '' : <FormattedMessage {...{id: this.props.section.name}} />;
-    // get the styles from the WithFormSection HOC
-    const styles = this.props.styles;
 
     return (
-      <div className={styles.editFormSection}>
+      <div className={this.props.styles.editFormSection}>
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
-              <span className={styles.sectionHeader}>
+              <span className={this.props.styles.sectionHeader}>
                 {sectionName}
               </span>
             </div>
