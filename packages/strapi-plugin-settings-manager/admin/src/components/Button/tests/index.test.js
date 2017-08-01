971e33e4b6f965ac8f03f5190f9190b2211b048c
cyril lopez <cyril.lpz@gmail.com>
2017-08-01 17:48:43 +0200

    Remove _deprecated folder and add test to components/Button

--- packages/strapi-plugin-settings-manager/admin/src/components/Button/tests/index.test.js
+++ packages/strapi-plugin-settings-manager/admin/src/components/Button/tests/index.test.js
@@ -1,10 +1,10 @@
-// import OldList from '../index';
+// import Button from '../index';
 
 import expect from 'expect';
 // import { shallow } from 'enzyme';
 // import React from 'react';
 
-describe('<OldList />', () => {
+describe('<Button />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
   });
