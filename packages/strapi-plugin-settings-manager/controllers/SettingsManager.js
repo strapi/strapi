bc603bea777e707c926b3e42ec06da5f9c1cf81e
Pierre Burgy <pierre.burgy@gmail.com>
2017-08-23 13:21:54 +0200

    Fix settings manager flag build issue

--- packages/strapi-plugin-settings-manager/controllers/SettingsManager.js
+++ packages/strapi-plugin-settings-manager/controllers/SettingsManager.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
+const send = require('koa-send');
 
 module.exports = {
   menu: async ctx => {
@@ -327,5 +328,13 @@ module.exports = {
     !_.isEmpty(updateErrors) ? ctx.badRequest(null, Service.formatErrors(updateErrors)) : ctx.send({ ok: true });
 
     strapi.reload();
+  },
+
+  assets: async ctx => {
+    try {
+      await send(ctx, `plugins/settings-manager/admin/build/${ctx.params.file}`);
+    } catch (err) {
+      ctx.body = ctx.notFound();
+    }
   }
 };
