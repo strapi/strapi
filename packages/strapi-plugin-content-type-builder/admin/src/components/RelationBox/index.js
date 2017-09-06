528e461aa29f15a58eda4dd0a01606c73ac5bb24
cyril lopez <cyril.lpz@gmail.com>
2017-09-06 09:30:34 +0200

    Created RelationBox component for relation PopUp

---
+++ packages/strapi-plugin-content-type-builder/admin/src/components/RelationBox/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* RelationBox
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.scss';
+
+class RelationBox extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.relationBox}>
+      </div>
+    );
+  }
+}
+
+export default RelationBox;
---
+++ packages/strapi-plugin-content-type-builder/admin/src/components/RelationBox/tests/index.test.js
@@ -0,0 +1,11 @@
+// import RelationBox from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<RelationBox />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
