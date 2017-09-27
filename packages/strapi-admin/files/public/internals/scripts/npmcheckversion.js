056503d382520f1d68896da5e9e2a5331fa2d65d
Pierre Burgy <pierre.burgy@gmail.com>
2017-09-27 18:40:37 +0200

    Delete files folder

--- packages/strapi-admin/files/public/internals/scripts/npmcheckversion.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-var exec = require('child_process').exec;
-exec('npm -v', function (err, stdout, stderr) {
-  if (err) throw err;
-  if (parseFloat(stdout) < 3) {
-    throw new Error('[ERROR: React Boilerplate] You need npm version @>=3');
-    process.exit(1);
-  }
-});
