055cc4cea68c070726b3a447984bb8270607ae62
Pierre Burgy <pierre.burgy@gmail.com>
2017-10-11 10:44:05 +0200

    Delete _book folders

--- docs/3.x.x/_book/gitbook/gitbook-plugin-anchorjs/anchor-style.js
+++
@@ -1,7 +0,0 @@
-gitbook.events.bind('start', function(e, config) {
-    anchors.options = config.anchorjs || {};
-});
-
-gitbook.events.bind('page.change', function() {
-    anchors.add('h1,h2,h3,h4,h5');
-});
