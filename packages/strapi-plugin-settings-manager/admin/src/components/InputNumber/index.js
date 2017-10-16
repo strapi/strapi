9b1d658fa53cc8cb8723622e6ce0fd5590bce716
cyril lopez <cyril.lpz@gmail.com>
2017-10-16 16:24:48 +0200

    Fix settings-manager input styles

--- packages/strapi-plugin-settings-manager/admin/src/components/InputNumber/index.js
+++ packages/strapi-plugin-settings-manager/admin/src/components/InputNumber/index.js
@@ -94,7 +94,7 @@ class InputNumber extends React.Component { // eslint-disable-line react/prefer-
           const displayError = isObject(error) && error.id ?
             <FormattedMessage {...error} /> : error;
           return (
-            <div key={key} className="form-control-feedback" style={{marginBottom: '1.8rem'}}>{displayError}</div>
+            <div key={key} className="form-control-feedback invalid-feedback" style={{marginBottom: '1.8rem', fontSize: '1.3rem' }}>{displayError}</div>
           );
         })
       );
@@ -112,7 +112,7 @@ class InputNumber extends React.Component { // eslint-disable-line react/prefer-
           onChange={this.props.handleChange}
           value={inputValue}
           type="number"
-          className={`form-control ${this.state.errors? 'form-control-danger' : ''}`}
+          className={`form-control ${!isEmpty(this.state.errors) ? 'form-control-danger is-invalid' : ''}`}
           placeholder={message}
           style={{marginBottom}}
         />
@@ -141,7 +141,7 @@ class InputNumber extends React.Component { // eslint-disable-line react/prefer-
           onBlur={handleBlur}
           onChange={this.props.handleChange}
           onFocus={this.props.handleFocus}
-          className={`form-control ${this.state.errors? 'form-control-danger' : ''}`}
+          className={`form-control ${!isEmpty(this.state.errors) ? 'form-control-danger is-invalid' : ''}`}
           placeholder={placeholder}
           style={{marginBottom: marginBottomInput }}
         />
