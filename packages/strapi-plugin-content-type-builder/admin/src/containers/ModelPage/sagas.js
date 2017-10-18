2274637b00e45e3540f2ca7dd84e03205e5b7c28
cyril lopez <cyril.lpz@gmail.com>
2017-10-18 17:16:28 +0200

    Remove params dominant when relation change from manyToMany to something else

--- packages/strapi-plugin-content-type-builder/admin/src/containers/ModelPage/sagas.js
+++ packages/strapi-plugin-content-type-builder/admin/src/containers/ModelPage/sagas.js
@@ -36,7 +36,7 @@ export function* submitChanges(action) {
     const modelName = get(storeData.getContentType(), 'name');
 
     const body = yield select(makeSelectModel());
-  
+
     map(body.attributes, (attribute, index) => {
       // Remove the connection key from attributes
       if (attribute.connection) {
@@ -44,6 +44,10 @@ export function* submitChanges(action) {
       }
 
       forEach(attribute.params, (value, key) => {
+        if (key === 'dominant' && get(attribute.params, 'nature') !== 'manyToMany') {
+          delete body.attributes[index].params.dominant;
+        }
+
         if (includes(key, 'Value')) {
           // Remove and set needed keys for params
           set(body.attributes[index].params, replace(key, 'Value', ''), value);
