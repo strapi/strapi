0cc377cc80e39b49317217458d45b11a36945002
cyril lopez <cyril.lpz@gmail.com>
2017-10-30 13:46:40 +0100

    Rename strapi with Strapi

--- packages/strapi-plugin-content-type-builder/admin/src/containers/ModelPage/sagas.js
+++ packages/strapi-plugin-content-type-builder/admin/src/containers/ModelPage/sagas.js
@@ -43,7 +43,7 @@ export function* getTableExistance() {
     yield put(checkIfTableExistsSucceeded(tableExists));
 
   } catch(error) {
-    window.strapi.notification.error('An error occured');
+    window.Strapi.notification.error('An error occured');
   }
 }
 
