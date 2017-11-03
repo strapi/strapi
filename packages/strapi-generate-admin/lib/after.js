c2925d98e86f42b8f97016ec6b5b9e5017ba6d87
Jim Laurie <j.laurie6993@gmail.com>
2017-11-03 17:15:53 +0100

    Change gitignore for generated admin

--- packages/strapi-generate-admin/lib/after.js
+++ packages/strapi-generate-admin/lib/after.js
@@ -6,6 +6,7 @@
 
 const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs-extra');
 
 /**
  * Runs after this generator has finished
@@ -15,10 +16,13 @@ const path = require('path');
  */
 
 module.exports =  (scope, cb) => {
+  // Copy the admin files.
+  fs.copySync(path.resolve(__dirname, '..', 'files'), path.resolve(scope.rootPath, 'admin'));
+
   if (scope.developerMode) {
     return cb();
   }
-  
+
   // Install back-end admin `node_modules`.
   exec('npm install --production --ignore-scripts', {
     cwd: path.resolve(scope.rootPath, 'admin')
