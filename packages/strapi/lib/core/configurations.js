ab62578fac9e07d4e053d50bc3640645f3919d09
Jim Laurie <j.laurie6993@gmail.com>
2017-11-21 10:07:49 +0100

    Fix API assign

--- packages/strapi/lib/core/configurations.js
+++ packages/strapi/lib/core/configurations.js
@@ -4,7 +4,7 @@
 const path = require('path');
 const glob = require('glob');
 const utils = require('../utils');
-const {merge, setWith, get, upperFirst, isString, isEmpty, isObject, pullAll, defaults, isPlainObject, forEach, assign} = require('lodash');
+const {merge, setWith, get, upperFirst, isString, isEmpty, isObject, pullAll, defaults, isPlainObject, forEach, assign, clone} = require('lodash');
 
 module.exports.nested = function() {
   return Promise.all([
@@ -296,7 +296,7 @@ const enableHookNestedDependencies = function (name, flattenHooksConfig, force =
   // Couldn't find configurations for this hook.
   if (isEmpty(get(flattenHooksConfig, name, true))) {
     // Check if database connector is used
-    const modelsUsed = Object.keys(assign(this.api || {}, this.plugins || {}))
+    const modelsUsed = Object.keys(assign(clone(this.api) || {}, this.plugins || {}))
       .filter(x => isObject(get(this.api, [x, 'models']) || get(this.plugins, [x, 'models']))) // Filter API with models
       .map(x => get(this.api, [x, 'models']) || get(this.plugins, [x, 'models'])) // Keep models
       .filter(models => {
