ee1ece28d8fa850d683e0a39834082e8641ee372
Jim Laurie <j.laurie6993@gmail.com>
2017-11-27 16:47:16 +0100

    Use request route to detect current action

--- packages/strapi-plugin-users-permissions/middlewares/users-permissions/index.js
+++ packages/strapi-plugin-users-permissions/middlewares/users-permissions/index.js
@@ -4,13 +4,13 @@ module.exports = strapi => {
   return {
     initialize: function(cb) {
       _.forEach(strapi.config.routes, value => {
-        value.config.policies.unshift('plugins.users-permissions.isAuthenticated');
+        value.config.policies.unshift('plugins.users-permissions.permissions');
       });
 
       if (strapi.plugins) {
         _.forEach(strapi.plugins, (plugin, name) => {
           _.forEach(plugin.config.routes, value => {
-            value.config.policies.unshift('plugins.users-permissions.isAuthenticated');
+            value.config.policies.unshift('plugins.users-permissions.permissions');
           });
         });
       }
--- packages/strapi-plugin-users-permissions/services/Jwt.js
+++ packages/strapi-plugin-users-permissions/services/Jwt.js
@@ -15,5 +15,47 @@ module.exports = {
       _.clone(payload.toJSON()),
       process.env.JWT_SECRET || _.get(strapi, 'api.user.config.jwtSecret') || 'oursecret'
     );
+  },
+
+  verify: (token) => {
+    return new Promise(function (resolve, reject) {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET || _.get(strapi, 'api.user.config.jwtSecret') || 'oursecret',
+        {},
+        function (err, user) {
+          if (err || !user || !user.id) {
+            return reject('Invalid token.');
+          }
+          resolve(user);
+        }
+      );
+    });
+  },
+
+  getToken: function (ctx) {
+    const params = _.assign({}, ctx.request.body, ctx.request.query);
+
+    let token = '';
+
+    if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+      const parts = ctx.request.header.authorization.split(' ');
+
+      if (parts.length === 2) {
+        const scheme = parts[0];
+        const credentials = parts[1];
+        if (/^Bearer$/i.test(scheme)) {
+          token = credentials;
+        }
+      } else {
+        throw new Error('Invalid authorization header format. Format is Authorization: Bearer [token]');
+      }
+    } else if (params.token) {
+      token = params.token;
+    } else {
+      throw new Error('No authorization header was found');
+    }
+
+    return this.verify(token);
   }
 };
