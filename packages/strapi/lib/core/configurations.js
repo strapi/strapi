d050e4df28c6283fe042c8b566528bcf457bd8f7
Aurelsicoko <aurelsicoko@gmail.com>
2017-12-07 12:08:13 +0100

    Fix redirect and load custom middleware

--- packages/strapi/lib/core/configurations.js
+++ packages/strapi/lib/core/configurations.js
@@ -269,7 +269,7 @@ module.exports.app = async function() {
     // Preset config in alphabetical order.
     this.config.middleware.settings = Object.keys(this.middleware).reduce((acc, current) => {
       // Try to find the settings in the current environment, then in the main configurations.
-      const currentSettings = merge(get(this.middleware[current], ['defaults', current], {}), flattenMiddlewaresConfig[current] || this.config[current]);
+      const currentSettings = merge(get(this.middleware[current], ['defaults', current], {}), flattenMiddlewaresConfig[current] || this.config.currentEnvironment[current] || this.config[current]);
       acc[current] = !isObject(currentSettings) ? {} : currentSettings;
 
       if (!acc[current].hasOwnProperty('enabled')) {
