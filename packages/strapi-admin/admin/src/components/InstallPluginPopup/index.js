2a739aa6bc23f337a3df4d528ecdf5ec81bd0b69
cyril lopez <cyril.lpz@gmail.com>
2017-12-12 15:44:04 +0100

    Design popup navlink

--- packages/strapi-admin/admin/src/components/InstallPluginPopup/index.js
+++ packages/strapi-admin/admin/src/components/InstallPluginPopup/index.js
@@ -39,41 +39,44 @@ class InstallPluginPopup extends React.Component {
       <Modal isOpen={this.props.isOpen} toggle={this.toggle} className={styles.modalPosition}>
         <ModalHeader toggle={this.toggle} className={styles.modalHeader} />
         <ModalBody className={styles.modalBody}>
-          <div className={styles.headerWrapper}>
-            <div className={styles.logo} style={{ backgroundImage: `url(${this.props.plugin.logo})`}} />
-            <div className={styles.headerInfo}>
-              <div className={styles.name}>{this.props.plugin.name}</div>
-              <div className={styles.ratings}>
-                <div className={styles.starsContainer}>
-                  <div>
-                    {map(this.props.coloredStars, star => <i key={star} className="fa fa-star" />)}
-                  </div>
-                  <div>
-                    {map(this.props.emptyStars, star => <i key={star} className="fa fa-star" />)}
-                  </div>
-                </div>
-                <div>
-                  <span style={{ fontWeight: '600', color: '#333740', fontSize: '12px'}}>{this.props.plugin.ratings}</span>
-                  <span style={{ fontWeight: '500', color: '#666666', fontSize: '11px' }}>/5</span>
-                </div>
-              </div>
-              <div className={styles.headerDescription}>
-                <FormattedMessage id={this.props.plugin.description} />
-              </div>
-              <div className={styles.headerButtonContainer}>
-                <div>
-                  <i className={`fa fa-${this.props.plugin.isCompatible ? 'check' : 'times'}`} />
-                  <FormattedMessage id={`app.components.PluginCard.compatible${this.props.plugin.id === 'support-us' ? 'Community' : ''}`} />
-                </div>
-                <div>
-                  <span style={{ fontWeight: '600' }}>+{this.props.plugin.downloads_nb}k&nbsp;</span><FormattedMessage id="app.components.InstallPluginPopup.downloads" />
-                </div>
-                <div>
-                  <div className={styles.buttonWrapper}>
+          <div className={styles.wrapper}>
+
+            <div className={styles.headerWrapper}>
+              <div className={styles.logo} style={{ backgroundImage: `url(${this.props.plugin.logo})`}} />
+              <div className={styles.headerInfo}>
+                <div className={styles.name}>{this.props.plugin.name}</div>
+                <div className={styles.ratings}>
+                  <div className={styles.starsContainer}>
                     <div>
-                      <FormattedMessage id="app.components.InstallPluginPopup.downloads" />
+                      {map(this.props.coloredStars, star => <i key={star} className="fa fa-star" />)}
                     </div>
-                    <div>{this.props.plugin.price}&nbsp;€</div>
+                    <div>
+                      {map(this.props.emptyStars, star => <i key={star} className="fa fa-star" />)}
+                    </div>
+                  </div>
+                  <div>
+                    <span style={{ fontWeight: '600', color: '#333740', fontSize: '12px'}}>{this.props.plugin.ratings}</span>
+                    <span style={{ fontWeight: '500', color: '#666666', fontSize: '11px' }}>/5</span>
+                  </div>
+                </div>
+                <div className={styles.headerDescription}>
+                  <FormattedMessage id={this.props.plugin.description} />
+                </div>
+                <div className={styles.headerButtonContainer}>
+                  <div>
+                    <i className={`fa fa-${this.props.plugin.isCompatible ? 'check' : 'times'}`} />
+                    <FormattedMessage id={`app.components.PluginCard.compatible${this.props.plugin.id === 'support-us' ? 'Community' : ''}`} />
+                  </div>
+                  <div>
+                    <span style={{ fontWeight: '600' }}>+{this.props.plugin.downloads_nb}k&nbsp;</span><FormattedMessage id="app.components.InstallPluginPopup.downloads" />
+                  </div>
+                  <div>
+                    <div className={styles.buttonWrapper}>
+                      <div>
+                        <FormattedMessage id="app.components.InstallPluginPopup.downloads" />
+                      </div>
+                      <div>{this.props.plugin.price}&nbsp;€</div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -81,8 +84,15 @@ class InstallPluginPopup extends React.Component {
           </div>
           <div className={styles.navContainer}>
             {map(this.navLinks, link => {
+              const isActive = this.props.history.location.hash.split('::')[1] === link.name;
+
               return (
-                <div>
+                <div
+                  key={link.name}
+                  className={isActive ? styles.navLink : ''}
+                  onClick={() => this.props.history.push({ pathname: this.props.history.location.pathname, hash: `${this.props.plugin.id}::${link.name}` })}
+                  style={isActive ? { paddingTop: '5px'} : { paddingTop: '7px' }}
+                >
                   <FormattedMessage id={link.content} />
                 </div>
               );
