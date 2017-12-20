963a09b3a36d78d89c835397e342f40c0ad8fdea
Aurelsicoko <aurelsicoko@gmail.com>
2017-12-20 12:20:22 +0100

    Ignore dotfile during post-install and optimize plugins' middlewares load

--- packages/strapi-admin/scripts/setup.js
+++ packages/strapi-admin/scripts/setup.js
@@ -1,18 +1,36 @@
 const shell = require('shelljs');
 const path = require('path');
+const _ = require('lodash');
 
-shell.echo("It can takes few minutes...");
-
-shell.exec('cd ./node_modules/strapi-helper-plugin');
-shell.exec('npm install');
-
-shell.exec('cd ../../');
+shell.echo('');
+shell.echo('🕓  The setup process can take few minutes.');
+shell.echo('📦  Installing admin packages...');
+shell.echo('');
 
 const pwd = shell.pwd();
+
+shell.exec(`cd ${path.resolve(pwd.stdout, 'node_modules', 'strapi-helper-plugin')} && npm install`, {
+  silent: true
+});
+
 const plugins = path.resolve(pwd.stdout, '..', 'plugins');
 
 shell.ls('* -d', plugins).forEach(function (plugin) {
-  shell.exec(`cd ${path.resolve(plugins, plugin)} && npm install`);
-  shell.exec(`cd ${path.resolve(plugins, plugin, 'node_modules', 'strapi-helper-plugin')} && npm install`);
-  shell.exec(`cd ${path.resolve(plugins, plugin)} && npm run build`);
+  shell.echo(`🔸  ${_.upperFirst(plugin)} (plugin)`);
+  shell.echo('📦  Installing packages...');
+  shell.exec(`cd ${path.resolve(plugins, plugin)} && npm install`, {
+    silent: true
+  });
+  shell.exec(`cd ${path.resolve(plugins, plugin, 'node_modules', 'strapi-helper-plugin')} && npm install`, {
+    silent: true
+  });
+  shell.echo('🏗  Building...');
+
+  const build = shell.exec(`cd ${path.resolve(plugins, plugin)} && npm run build`, {
+    silent: true
+  });
+
+   sa
+
+  shell.echo('');
 });
