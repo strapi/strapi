6baf2e5090b76dfb71e60254414fbf3c1a6d1860
Aurelsicoko <aurelsicoko@gmail.com>
2017-12-20 18:34:33 +0100

    Build admin during setup

--- packages/strapi-admin/scripts/setup.js
+++ packages/strapi-admin/scripts/setup.js
@@ -13,6 +13,16 @@ shell.exec(`cd ${path.resolve(pwd.stdout, 'node_modules', 'strapi-helper-plugin'
   silent: true
 });
 
+shell.echo('🏗  Building...');
+
+const build = shell.exec(`cd ${path.resolve(pwd.stdout)} && npm run build`, {
+  silent: true
+});
+
+if (build.stderr) {
+  console.error(build.stderr);
+}
+
 const plugins = path.resolve(pwd.stdout, '..', 'plugins');
 
 shell.ls('* -d', plugins).forEach(function (plugin) {
