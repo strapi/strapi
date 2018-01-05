565cfda087a4cde782981a232f58e632a5ce50d1
Aurelsicoko <aurelsicoko@gmail.com>
2018-01-05 17:17:58 +0100

    Make exception for Yarn to find the package in the right folder

--- packages/strapi-generate/lib/target.js
+++ packages/strapi-generate/lib/target.js
@@ -233,7 +233,11 @@ function parseTarget(target, scope, cb) {
     // try requiring `strapi-generate-<module>` to get the core generator.
     if (!subGenerator && !module.match(/^strapi-generate-/)) {
       try {
-        subGenerator = require(path.resolve(process.mainModule.paths[1], 'strapi-generate-' + module));
+        if (process.mainModule.filename.indexOf('yarn') !== -1) {
+          subGenerator = require(path.resolve(process.mainModule.paths[2], 'strapi-generate-' + module));
+        } else {
+          subGenerator = require(path.resolve(process.mainModule.paths[1], 'strapi-generate-' + module));
+        }
       } catch (e1) {
         requireError = e1;
       }
