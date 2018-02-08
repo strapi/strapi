26a5d95850e259c1f5126aa6908e71ecec7355cb
cyril lopez <cyril.lpz@gmail.com>
2018-02-08 15:50:12 +0100

    Created ImgPreview and InputFile components
    Init only the structure

---
+++ packages/strapi-helper-plugin/lib/src/components/ImgPreview/index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ *
+ * ImgPreview
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import styles from './styles.scss';
+
+function ImgPreview(props) {
+  return (
+    <div></div>
+  );
+}
+
+ImgPreview.defaultProps = {};
+ImgPreview.propTypes = {};
+
+export default ImgPreview;
---
+++ packages/strapi-helper-plugin/lib/src/components/InputFile/index.js
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ * InputFile
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { isEmpty  } from 'lodash';
+import cn from 'classnames';
+
+import ImgPreview from 'components/ImgPreview';
+
+import styles from './styles.scss';
+
+function InputFile(props) {
+  return (
+    <div></div>
+  );
+}
+
+InputFile.defaultProps = {};
+InputFile.propTypes = {};
+
+export default InputFile;
