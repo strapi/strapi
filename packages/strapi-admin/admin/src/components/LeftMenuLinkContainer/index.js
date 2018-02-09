7c2317cba2404b64d79749458d90616f5560916e
Aurelsicoko <aurelsicoko@gmail.com>
2018-02-09 17:17:48 +0100

    Update plugins list icon

--- packages/strapi-admin/admin/src/components/LeftMenuLinkContainer/index.js
+++ packages/strapi-admin/admin/src/components/LeftMenuLinkContainer/index.js
@@ -79,7 +79,7 @@ function LeftMenuLinkContainer({ plugins }) {
         <p className={styles.title}><FormattedMessage {...messages.general} /></p>
         <ul className={styles.list}>
           <LeftMenuLink
-            icon="puzzle-piece"
+            icon="list"
             label={messages.listPlugins.id}
             destination="/list-plugins"
           />
