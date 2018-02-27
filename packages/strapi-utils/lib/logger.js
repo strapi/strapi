4e404b149e81299478ffb54b63a31f2c24739332
Jørgensen <kristian@froelund.net>
2018-02-27 09:08:31 +0100

    Update logger.js
    Added environment variables to configure strapi logger.

--- packages/strapi-utils/lib/logger.js
+++ packages/strapi-utils/lib/logger.js
@@ -5,10 +5,31 @@
  */
 
 const pino = require('pino');
+const _ = require('lodash');
 
-module.exports = pino({
-  level: 'debug',
-  timestamp: false,
-  prettyPrint: true,
-  forceColor: true
-});
+const logLevels = ['fatal', 'error', 'warn', 'info', 'debug', 'trace'];
+
+function getLogLevel() {
+  if (_.isString(process.env.STRAPI_LOG_LEVEL) && _.includes(logLevels, process.env.STRAPI_LOG_LEVEL.toLowerCase())) {
+    return process.env.STRAPI_LOG_LEVEL;
+  }
+  return 'debug'
+}
+
+function getBool(envVar, defaultValue) {
+  if (_.isBoolean(envVar)) return envVar;
+  if (_.isString(envVar)) {
+    if (envVar === 'true') return true;
+    if (envVar === 'false') return false;
+  }
+  return defaultValue;
+}
+
+const loggerConfig = {
+  level: getLogLevel(),
+  timestamp: getBool(process.env.STRAPI_LOG_TIMESTAMP, false),
+  prettyPrint: getBool(process.env.STRAPI_LOG_PRETTY_PRINT, true),
+  forceColor: getBool(process.env.STRAPI_LOG_FORCE_COLOR, true),
+};
+
+module.exports = pino(loggerConfig);
