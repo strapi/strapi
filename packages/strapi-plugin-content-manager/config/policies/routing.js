3c1ec201550bdd24680363f46fc1ba2e50ac74f1
Jim Laurie <j.laurie6993@gmail.com>
2018-03-13 15:57:41 +0100

    Fix form data update user

--- packages/strapi-plugin-content-manager/config/policies/routing.js
+++ packages/strapi-plugin-content-manager/config/policies/routing.js
@@ -8,7 +8,40 @@ module.exports = async (ctx, next) => {
 
     if (controller && action) {
       // Redirect to specific controller.
-      return await strapi.plugins[source].controllers[controller.toLowerCase()][action](ctx);
+      if (ctx.request.body.hasOwnProperty('fields') && ctx.request.body.hasOwnProperty('files')) {
+        const {files, fields} = _.cloneDeep(ctx.request.body);
+
+        const fileAttributes = _.get(strapi.plugins, [source, 'models', ctx.params.model, 'associations'], []).filter(association => {
+          return association[association.type] === 'file' && association.via === 'related';
+        }).reduce((acc, association) => {
+          acc.push(association.alias);
+          return acc;
+        }, []);
+
+        ctx.request.body = _.omit(fields, fileAttributes);
+
+        await strapi.plugins[source].controllers[controller.toLowerCase()][action](ctx);
+        const resBody = _.cloneDeep(ctx.body);
+
+        Object.keys(files).map(async field => {
+          ctx.request.body = {
+            files: {
+              files: files[field]
+            },
+            fields: {
+              refId: resBody.id || resBody._id,
+              ref: ctx.params.model,
+              source,
+              field
+            }
+          };
+
+          await strapi.plugins.upload.controllers.upload.upload(ctx);
+        });
+        return ctx.send(resBody);
+      } else {
+        return await strapi.plugins[source].controllers[controller.toLowerCase()][action](ctx);
+      }
     }
   }
 
