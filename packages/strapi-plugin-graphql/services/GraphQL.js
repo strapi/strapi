5acbc5de384dc08da1a4f7f48ad7f114edcc4551
Aurelsicoko <aurelsicoko@gmail.com>
2018-03-28 18:40:59 +0200

    Refactor code and write generated schema

--- packages/strapi-plugin-graphql/services/GraphQL.js
+++ packages/strapi-plugin-graphql/services/GraphQL.js
@@ -6,9 +6,15 @@
  * @description: A set of functions similar to controller's actions to avoid code duplication.
  */
 
+const fs = require('fs');
+const path = require('path');
+
+const _ = require('lodash');
 const { makeExecutableSchema } = require('graphql-tools');
 
 module.exports = {
+
+  formatGQL: (str) => JSON.stringify(str, null, 2).replace(/['"]+/g, ''),
 
   convertType: (type) => {
     switch (type) {
@@ -22,73 +28,64 @@ module.exports = {
     }
   },
 
-  shadowCRUD: (type, source) => {
+  shadowCRUD: function (models) {
     // Retrieve generic service from the Content Manager plugin.
     const resolvers = strapi.plugins['content-manager'].services['contentmanager'];
 
-    const params = {
-      model: type
-    };
+    return models.reduce((acc, model) => {
+      const params = {
+        model
+      };
 
-    const query = {
-      source
-    };
+      const query = {};
 
-    // TODO
-    // - Apply and execute policies first.
-    // - Use pluralize to generate shadow query.
-    // - Handle mutations.
+      // Convert our layer Model to the GraphQL DL.
+      const attributes = Object.keys(strapi.models[model].attributes)
+        .reduce((acc, attribute) => {
+          // Convert our type to the GraphQL type.
+          acc[attribute] = this.convertType(strapi.models[model].attributes[attribute].type);
 
-    return {
-      [`${type}s`]: (_, options) => resolvers.fetchAll(params, {...query, ...options}),
-      [`${type}`]: (_, { id }) => resolvers.fetch({ ...params, id }, query)
-    };
+          return acc;
+        }, { id: 'String' });
+
+      acc.definition += `type ${strapi.models[model].globalId} ${this.formatGQL(attributes)}\n\n`;
+
+      Object.assign(acc.query, {
+        [`${model}s`]: `[${strapi.models[model].globalId}]`,
+        [`${model}(id: String!)`]: strapi.models[model].globalId
+      });
+
+      // TODO
+      // - Apply and execute policies first.
+      // - Use pluralize to generate shadow query.
+      // - Handle mutations.
+      Object.assign(acc.resolver, {
+        [`${model}s`]: (_, options) => resolvers.fetchAll(params, {...query, ...options}),
+        [`${model}`]: (_, { id }) => resolvers.fetch({ ...params, id }, query)
+      });
+
+      return acc;
+    }, { definition: ``, query: {}, resolver: {} });
   },
 
   generateSchema: function () {
     // Exclude core models.
     const models = Object.keys(strapi.models).filter(model => model !== 'core_store');
 
+    // Generate type definition and query/mutation for models.
+    const shadowCRUD = true ? this.shadowCRUD(models) : {};
+
     // Build resolvers.
     const resolvers = {
-      Query: models.reduce((acc, current) => {
-        return Object.assign(acc, this.shadowCRUD(current));
-      }, {})
+      Query: shadowCRUD.resolver || {}
     };
 
-    // TODO
-    // - Expose plugins models.
-
-    // Build types definitions.
-    const modelsDefinition = models.reduce((acc, current) => {
-      // Convert our layer Model to the GraphQL DL.
-      const attributes = Object.keys(strapi.models[current].attributes)
-        .reduce((acc, attribute) => {
-          // Convert our type to the GraphQL type.
-          acc[attribute] = this.convertType(strapi.models[current].attributes[attribute].type);
-
-          return acc;
-        }, {});
-
-      const definition = JSON.stringify(attributes, null, 2).replace(/['"]+/g, '');
-
-      return acc += `type ${strapi.models[current].globalId} ${definition}\n\n`;
-    }, ``);
-
-    const modelsQueriesDefinition = models.reduce((acc, current) => {
-      return Object.assign(acc, {
-        [`${current}s`]: [strapi.models[current].globalId],
-        [`${current}(id: String!)`]: strapi.models[current].globalId
-      });
-    }, {});
-
-    const modelsQueriesDefinitionCleaned = JSON.stringify(modelsQueriesDefinition, null, 2).replace(/['"]+/g, '');
-
-    // Build queries.
-    const queriesDefinition = `type Query ${modelsQueriesDefinitionCleaned}`;
-
     // Concatenate.
-    const typeDefs = modelsDefinition + queriesDefinition;
+    const typeDefs = shadowCRUD.definition + `type Query ${this.formatGQL(shadowCRUD.query)}`;
+
+    // Write schema.
+    this.writeGenerateSchema(typeDefs);
+
     // Build schema.
     const schema = makeExecutableSchema({
       typeDefs,
@@ -96,6 +93,23 @@ module.exports = {
     });
 
     return schema;
+  },
+
+  writeGenerateSchema(schema) {
+    const generatedFolder = path.resolve(strapi.config.appPath, 'plugins', 'graphql', 'config', 'generated');
+
+    // Create folder if necessary.
+    try {
+      fs.accessSync(generatedFolder, fs.constants.R_OK | fs.constants.W_OK);
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        fs.mkdirSync(generatedFolder);
+      } else {
+        console.error(err);
+      }
+    }
+
+    fs.writeFileSync(path.join(generatedFolder, 'schema.graphql'), schema);
   }
 
 };
