32aefbe5bef2fbe45c220ed1bffea6734d1da5c0
cece95 <cesaregri@gmail.com>
2018-04-16 08:49:41 +0200

    fixed issue #923

--- packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
+++ packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
@@ -90,7 +90,7 @@ module.exports = async cb => {
             email: 'no-reply@strapi.io'
           },
           response_email: '',
-          object: '­Reset password 🔑 ',
+          object: '­Reset password ',
           message: `<p>We heard that you lost your password. Sorry about that!</p>
 
 <p>But don’t worry! You can use the following link to reset your password:</p>
