fb35bc75e0d3a8b6e0ea38cf159db0dde14962be
soupette <cyril.lpz@gmail.com>
2018-04-25 14:59:28 +0200

    Fix monorepo setup

--- packages/strapi-helper-plugin/lib/internals/scripts/loadAdminConfigurations.js
+++ packages/strapi-helper-plugin/lib/internals/scripts/loadAdminConfigurations.js
@@ -4,13 +4,16 @@ const shell = require('shelljs');
 const pwd = shell.pwd();
 const isDevelopmentMode = path.resolve(pwd.stdout).indexOf('strapi-admin') !== -1;
 const appPath = isDevelopmentMode ? path.resolve(process.env.PWD, '..') : path.resolve(pwd.stdout, '..');
+const isSetup = path.resolve(process.env.PWD, '..', '..') === path.resolve(process.env.INIT_CWD);
 
-const strapi = require(path.join(appPath, 'node_modules', 'strapi'));
-strapi.config.appPath = appPath;
-strapi.log.level = 'silent';
+if (!isSetup) {
+  const strapi = require(path.join(appPath, 'node_modules', 'strapi'));
+  strapi.config.appPath = appPath;
+  strapi.log.level = 'silent';
 
-(async () => {
-  await strapi.load({
-    environment: process.env.NODE_ENV,
-  });
-})();
+  (async () => {
+    await strapi.load({
+      environment: process.env.NODE_ENV,
+    });
+  })();
+}
