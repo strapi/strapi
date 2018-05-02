e3fc6ad30bde2f5612957dcef85bd0ca6a60ecc0
Aurelsicoko <aurelsicoko@gmail.com>
2018-05-02 17:07:03 +0200

    Remove the return to avoid the loop break

--- packages/strapi-plugin-content-manager/config/queries/bookshelf.js
+++ packages/strapi-plugin-content-manager/config/queries/bookshelf.js
@@ -216,7 +216,7 @@ module.exports = {
               // Remove existing relationship because only one file
               // can be related to this field.
               if (association.nature === 'manyMorphToOne') {
-                return virtualFields.push(
+                virtualFields.push(
                   module.exports.removeRelationMorph.call(this, {
                     alias: association.alias,
                     ref: model.collectionName,
@@ -233,15 +233,15 @@ module.exports = {
                     })
                   )
                 );
+              } else {
+                virtualFields.push(module.exports.addRelationMorph.call(this, {
+                  id: response[this.primaryKey],
+                  alias: association.alias,
+                  ref: model.collectionName,
+                  refId: obj.refId,
+                  field: obj.field
+                }));
               }
-
-              virtualFields.push(module.exports.addRelationMorph.call(this, {
-                id: response[this.primaryKey],
-                alias: association.alias,
-                ref: model.collectionName,
-                refId: obj.refId,
-                field: obj.field
-              }));
             });
             break;
           case 'oneToManyMorph':
