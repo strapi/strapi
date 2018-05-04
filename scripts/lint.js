e593d03ae56bc51045c779023a89bcd515587c09
soupette <cyril.lpz@gmail.com>
2018-05-04 18:03:41 +0200

    Lint strapi-knex

--- scripts/lint.js
+++ scripts/lint.js
@@ -29,7 +29,7 @@ const watcher = (label, pckgName, type = 'front') => {
 const packagesPath = path.resolve(process.env.PWD, 'packages');
 shell.ls('* -d', packagesPath)
   // TODO temporary just for eslint
-  .filter(package => package === 'strapi-helper-plugin')
+  .filter(package => package === 'strapi-knex')
   // .filter(package => package !== 'README.md')
   .forEach(package => {
     watcher(`Testing ${package}`, package);
