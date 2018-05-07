deb7749213a685036183ec9894d57cd451675e77
soupette <cyril.lpz@gmail.com>
2018-05-07 12:07:16 +0200

    Add prettier

---
+++ packages/strapi-lint/lib/internals/prettier/back/.prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * prettier configuration for front-end files
+ */
+
+module.exports = {
+  printWidth: 110,
+  parser: 'flow',
+  useTabs: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'es5',
+};
