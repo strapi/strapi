d816e5b313c02b832c47696c5f86f2e6250a594c
Aurelsicoko <aurelsicoko@gmail.com>
2018-05-17 15:05:53 +0200

    Hide divider on hover

--- packages/strapi-admin/admin/src/containers/AdminPage/index.js
+++ packages/strapi-admin/admin/src/containers/AdminPage/index.js
@@ -172,7 +172,6 @@ export class AdminPage extends React.Component { // eslint-disable-line react/pr
         )}
         <CTAWrapper>
           {this.shouldDisplayLogout() && <Logout />}
-          {this.shouldDisplayLogout() && <div className={styles.adminPageSeparator} />}
           <LocaleToggle isLogged={this.shouldDisplayLogout() === true} />
         </CTAWrapper>
         <div className={styles.adminPageRightWrapper} style={style}>
