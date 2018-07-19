e10f2e61b4462b1fc733e87e03984a14b4f0e2fd
cyril lopez <cyril.lpz@gmail.com>
2018-07-19 03:07:15 +0200

    Add utils & fix lint

--- packages/strapi-plugin-content-manager/admin/src/utils/Manager.js
+++ packages/strapi-plugin-content-manager/admin/src/utils/Manager.js
@@ -1,129 +1,151 @@
 export default class Manager {
-	constructor(state, list, keys, index) {
-		this.state = state;
-		this.keys = keys.split('.');
-		this.list = list;
-		this.index = index;
-		this.arrayOfEndLineElements = this.getLinesBound();
-	}
+  constructor(state, list, keys, index) {
+    this.state = state;
+    this.keys = keys.split('.');
+    this.list = list;
+    this.index = index;
+    this.arrayOfEndLineElements = this.getLinesBound();
+  }
 
-	/**
-	 * Retrieve the bootstrap col index, name and type of a field
-	 * @param {Number} index 
-	 * @returns {Object}
-	 */
-	getAttrInfos(index) {
-		const name = this.getAttrName(index);
-		const type = this.getType(name);
-		const boostrapCol = this.getBootStrapCol(type);
+  /**
+   * Retrieve the bootstrap col index, name and type of a field
+   * @param {Number} index 
+   * @returns {Object}
+   */
+  getAttrInfos(index) {
+    const name = this.getAttrName(index);
+    const type = this.getType(name);
+    const boostrapCol = this.getBootStrapCol(type);
 
-		const infos = {
-			boostrapCol,
-			index,
-			name,
-			type,
-		}
+    const infos = {
+      boostrapCol,
+      index,
+      name,
+      type,
+    };
 
-		return infos;
-	}
+    return infos;
+  }
 
-	/**
-	 * Retrieve a field default bootstrap col
-	 * NOTE: will change if we add the customisation of an input's width
-	 * @param {String} type 
-	 * @returns {Number}
-	 */
-	getBootStrapCol(type) {
-		switch(type) {
-			case 'checkbox':
-			case 'boolean':
-			return 3;
-			case 'date':
-			return 4;
-			case 'json':
-			case 'wysiwyg':
-			return 12;
-			default:
-			return 6;
-		}
-	};
+  /**
+   * Retrieve a field default bootstrap col
+   * NOTE: will change if we add the customisation of an input's width
+   * @param {String} type 
+   * @returns {Number}
+   */
+  getBootStrapCol(type) {
+    switch(type) {
+      case 'checkbox':
+      case 'boolean':
+        return 3;
+      case 'date':
+        return 4;
+      case 'json':
+      case 'wysiwyg':
+        return 12;
+      default:
+        return 6;
+    }
+  }
 
-	/**
-	 * Retrieve the field to remove infos
-	 * @returns {Object}
-	 */
-	attrToRemoveInfos() {
-		return this.getAttrInfos(this.index);
-	}
+  /**
+   * Retrieve the field to remove infos
+   * @returns {Object}
+   */
+  attrToRemoveInfos() {
+    return this.getAttrInfos(this.index);
+  }
 
-	/**
-	 * 
-	 * Retrieve the last element of each bootstrap line
-	 * @returns {Array}
-	 */
-	getLinesBound() { // NOTE: doesn't work for the last element if the line is not full!
-		const array = [];
-		let sum = 0;
+  /**
+   * 
+   * Retrieve the last element of each bootstrap line
+   * @returns {Array}
+   */
+  getLinesBound() { // NOTE: doesn't work for the last element if the line is not full!
+    const array = [];
+    let sum = 0;
 
-		this.list.forEach((item, i) => {
-			// Retrieve the item's bootstrap col
-			// Similutes the layout that is not in the core store yet
-			const { /*bootstrapCol, */ index, name, /* type */ } = this.getAttrInfos(i); // TODO: use these variables when layout in core store
-			const type = item.includes('long') ? 'wysiwyg' : this.getType(item);
-			const boostrapCol = this.getBootStrapCol(type);
+    this.list.forEach((item, i) => {
+      // Retrieve the item's bootstrap col
+      // Similutes the layout that is not in the core store yet
+      const { /*bootstrapCol, */ index, name /* type */ } = this.getAttrInfos(i); // TODO: use these variables when layout in core store
+      const type = item.includes('long') ? 'wysiwyg' : this.getType(item);
+      const boostrapCol = this.getBootStrapCol(type);
 
-			sum += boostrapCol;
+      sum += boostrapCol;
 
-			if (sum === 12 || boostrapCol === 12) {
-				const isFullSize = boostrapCol === 12;
-				array.push({ name, index, isFullSize });
-				sum + 0;
-			}
+      if (sum === 12 || boostrapCol === 12) {
+        const isFullSize = boostrapCol === 12;
+        array.push({ name, index, isFullSize });
+        sum + 0;
+      }
 
-			if (sum > 12) {
-				sum = 0;
-			}
-		});
+      if (sum > 12) {
+        sum = 0;
+      }
+    });
 
-		return array;
-	}
+    return array;
+  }
 
-	/**
-	 * 
-	 * Retrieve the field's type depending on its name
-	 * @param {String} itemName 
-	 * @returns {String}
-	 */
-	getType(itemName) {
-		return this.state
-			.getIn(['modifiedSchema', 'models', ...this.keys, 'availableFields', itemName, 'type']);
-	}
+  /**
+   * 
+   * Retrieve the field's type depending on its name
+   * @param {String} itemName 
+   * @returns {String}
+   */
+  getType(itemName) {
+    return this.state
+      .getIn(['modifiedSchema', 'models', ...this.keys, 'availableFields', itemName, 'type']);
+  }
 
-	/**
-	 * Retrieve a field name depending on its index
-	 * @param {Number} itemIndex
-	 * @returns {String}
-	 */
-	getAttrName(itemIndex){
-		return this.state
-			.getIn(['modifiedSchema', 'models', ...this.keys, 'fields', itemIndex]);
-	};
+  /**
+   * Retrieve a field name depending on its index
+   * @param {Number} itemIndex
+   * @returns {String}
+   */
+  getAttrName(itemIndex){
+    return this.state
+      .getIn(['modifiedSchema', 'models', ...this.keys, 'fields', itemIndex]);
+  }
 
-	/**
-	 * Retrieving : (end of prev line < studiedField < end of prev Line)
-	 * @param {Number} leftBound 
-	 * @param {Number} rightBound 
-	 * @returns {Number}
-	 */
-	getLineColSize(leftBound, rightBound) {
-		return this.list
-			.slice(leftBound + 1, rightBound)
-			.reduce((acc, current) => {
-				const type = this.getType(current);
-				// Simulates the layout => NEEDS TO BE REMOVED
-				const col = current.includes('long') ? 12 : this.getBootStrapCol(type);
+  /**
+   * Retrieves the studiedField's line boostrap col length
+   * @param {Number} leftBound 
+   * @param {Number} rightBound 
+   * @returns {Number}
+   */
+  getLineColSize(leftBound, rightBound) {
+    return this.list
+      .slice(leftBound + 1, rightBound)
+      .reduce((acc, current) => {
+        const type = this.getType(current);
+        // Simulates the layout => NEEDS TO BE REMOVED
+        const col = current.includes('long') ? 12 : this.getBootStrapCol(type);
 
-				return acc += col;
-			}, 0);
-	}
+        return acc += col;
+      }, 0);
+  }
+
+  /**
+   * 
+   * @param {Bool} dir sup or min
+   * @param {Number} pivot the center of 
+   * @returns {Object} the first sup or last sup
+   */
+  getBound(dir, pivot = attrToRemoveIndex) {
+    let result = {};
+    let hasResult = false;
+
+    arrayOfLastLineElements.forEach(item => {
+      const cond = dir === true ? item.index >= pivot && !hasResult : item.index <= pivot;
+
+      if (cond) {
+        hasResult = true;
+        result = item;
+      }
+    });
+
+    return result;
+  }
 }
