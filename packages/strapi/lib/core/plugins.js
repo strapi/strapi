fcd5db11fac0e2f12cdd592ac6a7f7c4d67fa185
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-08-27 21:34:50 +0900

    beautify plugins.js in strapi core

--- packages/strapi/lib/core/plugins.js
+++ packages/strapi/lib/core/plugins.js
@@ -1,64 +1,52 @@
-"use strict";
+'use strict';
 
 // Dependencies.
-const path = require("path");
-const fs = require("fs-extra");
-const _ = require("lodash");
+const path = require('path');
+const fs = require('fs-extra');
+const _ = require('lodash');
 
 module.exports = async function() {
-  const folder = ((
-    url = _.get(strapi.config.currentEnvironment.server, "admin.path", "admin")
-  ) => (url[0] === "/" ? url.substring(1) : url))().replace(/\/$/, "");
+  const folder = ((url = _.get(strapi.config.currentEnvironment.server, 'admin.path', 'admin')) =>
+    url[0] === '/' ? url.substring(1) : url)().replace(/\/$/, '');
 
   const configuratePlugin = (acc, current, source, name) => {
     switch (source) {
-      case "host": {
+      case 'host': {
         const host =
-          _.get(
-            this.config.environments[current].server,
-            "admin.build.host"
-          ).replace(/\/$/, "") || "/";
+          _.get(this.config.environments[current].server, 'admin.build.host').replace(/\/$/, '') || '/';
 
         if (!host) {
-          throw new Error(
-            `You can't use \`remote\` as a source without set the \`host\` configuration.`
-          );
+          throw new Error(`You can't use \`remote\` as a source without set the \`host\` configuration.`);
         }
 
-        const folder = _.get(
-          this.config.environments[current].server,
-          "admin.build.plugins.folder",
-          null
-        );
+        const folder = _.get(this.config.environments[current].server, 'admin.build.plugins.folder', null);
 
         if (_.isString(folder)) {
-          const cleanFolder = folder[0] === "/" ? folder.substring(1) : folder;
+          const cleanFolder = folder[0] === '/' ? folder.substring(1) : folder;
 
-          return `/${host}/${cleanFolder}/${name}/main.js`.replace("//", "/");
+          return `/${host}/${cleanFolder}/${name}/main.js`.replace('//', '/');
         }
 
-        return `/${host}/${name}/main.js`.replace("//", "/");
+        return `/${host}/${name}/main.js`.replace('//', '/');
       }
-      case "custom":
-        if (
-          !_.isEmpty(_.get(this.plugins[name].config, `sources.${current}`, {}))
-        ) {
+      case 'custom':
+        if (!_.isEmpty(_.get(this.plugins[name].config, `sources.${current}`, {}))) {
           return (acc[current] = this.plugins[name].config.sources[current]);
         }
 
         throw new Error(
-          `You have to define the source URL for each environment in \`./plugins/**/config/sources.json\``
+          `You have to define the source URL for each environment in \`./plugins/**/config/sources.json\``,
         );
-      case "backend": {
+      case 'backend': {
         const backend = _.get(
           this.config.environments[current],
-          "server.admin.build.backend",
+          'server.admin.build.backend',
           `http://${this.config.environments[current].server.host}:${
             this.config.environments[current].server.port
-          }`
-        ).replace(/\/$/, "");
+          }`,
+        ).replace(/\/$/, '');
 
-        return `${backend}/${folder.replace(/\/$/, "")}/${name}/main.js`;
+        return `${backend}/${folder.replace(/\/$/, '')}/${name}/main.js`;
       }
       default:
         return `/${name}/main.js`;
@@ -66,47 +54,31 @@ module.exports = async function() {
   };
 
   const sourcePath =
-    this.config.environment !== "test"
-      ? path.resolve(
-          this.config.appPath,
-          "admin",
-          "admin",
-          "src",
-          "config",
-          "plugins.json"
-        )
+    this.config.environment !== 'test'
+      ? path.resolve(this.config.appPath, 'admin', 'admin', 'src', 'config', 'plugins.json')
       : path.resolve(
           this.config.appPath,
-          "packages",
-          "strapi-admin",
-          "admin",
-          "src",
-          "config",
-          "plugins.json"
+          'packages',
+          'strapi-admin',
+          'admin',
+          'src',
+          'config',
+          'plugins.json',
         );
   const buildPath =
-    this.config.environment !== "test"
-      ? path.resolve(
-          this.config.appPath,
-          "admin",
-          "admin",
-          "build",
-          "config",
-          "plugins.json"
-        )
+    this.config.environment !== 'test'
+      ? path.resolve(this.config.appPath, 'admin', 'admin', 'build', 'config', 'plugins.json')
       : path.resolve(
           this.config.appPath,
-          "packages",
-          "strapi-admin",
-          "admin",
-          "build",
-          "config",
-          "plugins.json"
+          'packages',
+          'strapi-admin',
+          'admin',
+          'build',
+          'config',
+          'plugins.json',
         );
 
-  const isAdmin = await fs.pathExists(
-    path.resolve(this.config.appPath, "admin", "admin")
-  );
+  const isAdmin = await fs.pathExists(path.resolve(this.config.appPath, 'admin', 'admin'));
   if (!isAdmin) return;
 
   // arrenge system directories
@@ -118,38 +90,24 @@ module.exports = async function() {
       if (existBuildPath) {
         await fs.remove(buildPath);
       } else {
-        await fs.ensureDir(path.resolve(buildPath, "..", ".."));
+        await fs.ensureDir(path.resolve(buildPath, '..', '..'));
       }
     })(),
 
     // Create `./config` folder
-    fs.ensureDir(path.resolve(buildPath, ".."))
+    fs.ensureDir(path.resolve(buildPath, '..')),
   ]);
 
   // Create `plugins.json` file.
   // Don't inject the plugins without an Admin
   const existingPlugins = await Object.keys(this.plugins).filter(plugin =>
-    fs.pathExists(
-      path.resolve(
-        this.config.appPath,
-        "plugins",
-        plugin,
-        "admin",
-        "src",
-        "containers",
-        "App"
-      )
-    )
+    fs.pathExists(path.resolve(this.config.appPath, 'plugins', plugin, 'admin', 'src', 'containers', 'App')),
   );
 
   const existingPluginsInfo = existingPlugins.map(id => ({
     id,
     source: Object.keys(this.config.environments).reduce((acc, current) => {
-      const source = _.get(
-        this.config.environments[current].server,
-        "admin.build.plugins.source",
-        "default"
-      );
+      const source = _.get(this.config.environments[current].server, 'admin.build.plugins.source', 'default');
 
       if (_.isString(source)) {
         acc[current] = configuratePlugin(acc, current, source, id);
@@ -158,17 +116,17 @@ module.exports = async function() {
       }
 
       return acc;
-    }, {})
+    }, {}),
   }));
 
   await Promise.all([
     fs.writeJSON(sourcePath, existingPluginsInfo, {
       spaces: 2,
-      encoding: "utf8"
+      encoding: 'utf8',
     }),
     fs.writeJSON(buildPath, existingPluginsInfo, {
       spaces: 2,
-      encoding: "utf8"
-    })
+      encoding: 'utf8',
+    }),
   ]);
 };
