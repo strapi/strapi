2e049da8caa8ca63396166febdb224b1a76c224c
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-09-02 03:28:22 +0900

    tweak new command message

--- packages/strapi-generate-new/lib/before.js
+++ packages/strapi-generate-new/lib/before.js
@@ -186,7 +186,7 @@ module.exports = (scope, cb) => {
                   when: !hasDatabaseConfig && scope.client.database === 'mongo',
                   type: 'input',
                   name: 'authenticationDatabase',
-                  message: 'Authentication database(Maybe "admin" or blank):',
+                  message: 'Authentication database (Maybe "admin" or blank):',
                   default: _.get(scope.database, 'authenticationDatabase', undefined)
                 },
                 {
