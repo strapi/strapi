589761a6444efad6a64d14b025e3c100a34601c9
Alberto Maturano <alberto@maturano.mx>
2018-09-12 17:54:42 -0500

    Simplify select files for linter

--- scripts/lint.js
+++ scripts/lint.js
@@ -2,14 +2,12 @@ const path = require('path');
 const shell = require('shelljs');
 const chalk = require('chalk');
 const eslintErrorsFormatter = require('./eslintErrorsFormatter');
-const glob = require('glob');
-const fs = require('fs');
 const listChangedFiles = require('../packages/strapi-lint/lib/internals/shared/listChangedFiles.js');
 const changedFiles = listChangedFiles();
-const { includes, take, template } = require('lodash');
+const { take, template } = require('lodash');
 
 const cmdEslint = template(
-    "node ../../node_modules/strapi-lint/node_modules/.bin/eslint --ignore-path .gitignore --ignore-pattern '${ignore}'"
+  'node ../../node_modules/strapi-lint/node_modules/.bin/eslint --ignore-path .gitignore --ignore-pattern "${ignore}"'
   + ' --config ../../node_modules/strapi-lint/lib/internals/eslint/${conf}/.eslintrc.json ${params}'
 );
 
@@ -17,10 +15,10 @@ const cmdFront = cmdEslint({ ignore: '/admin/build/', conf: 'front', params: 'ad
 const cmdHelper = cmdEslint({ ignore: '/admin/build/', conf: 'front', params: 'lib/src' });
 const cmdBack = cmdEslint({ ignore: '/admin', conf: 'back', params: 'controllers config services bin lib' });
 
-const watcher = (label, pckgName, type = 'front') => {
+const watcher = (label, pckgName) => {
   shell.echo(label);
   shell.cd(pckgName);
-  const cmd = includes(pckgName, 'strapi-helper-plugin') ? cmdHelper : `${cmdFront} && ${cmdBack}`;
+  const cmd = pckgName.includes('strapi-helper-plugin') ? cmdHelper : `${cmdFront} && ${cmdBack}`;
 
   const data = shell.exec(cmd, { silent: true });
   shell.echo(chalk(eslintErrorsFormatter(data.stdout)));
@@ -32,19 +30,17 @@ const watcher = (label, pckgName, type = 'front') => {
   shell.echo('');
 };
 
-const files = glob
-  .sync('**/*.js', { ignore: '**/node_modules/**' })
-  .filter(f => changedFiles.has(f))
-  .filter(
-    package =>
-      !package.includes('README.md') &&
-      !package.includes('strapi-middleware-views') &&
-      !package.includes('strapi-lint') &&
-      !package.includes('strapi-plugin-settings-manager') &&
-      !package.includes('scripts') &&
-      !package.includes('test') &&
-      !package.includes('jest.config.js')
-  )
+const except = [
+  'jest.config.js',
+  'scripts',
+  'strapi-lint',
+  'strapi-middleware-views',
+  'strapi-plugin-settings-manager',
+  'test',
+];
+
+const changedDirs = [...changedFiles]
+  .filter(file => path.extname(file) === '.js' && !except.some(path => file.includes(path)))
   .map(file => {
     const directoryArray = file.split('/');
     const toTake = directoryArray.length === 2 ? 1 : 2;
@@ -52,9 +48,7 @@ const files = glob
     return take(directoryArray, toTake).join('/');
   });
 
-
-files
-  .filter((directory, index) => files.indexOf(directory) === index)
-  .forEach(package => {
-    watcher(`Testing ${package}`, package);
+[...new Set(changedDirs)]
+  .forEach(directory => {
+    watcher(`Testing ${directory}`, directory);
   });
