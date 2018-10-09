8e727b18c547544d1953e2cdfe192b5928fd8868
Martin Muzatko <martin@happy-css.com>
2018-10-09 09:55:41 +0200

    cleanup of dist and added docs dist to gitignore

--- docs/.vuepress/dist/assets/js/1.77d89b12.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{174:function(n,w,o){}}]);
--- docs/.vuepress/dist/assets/js/71.ab3e74fc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{219:function(t,n,e){"use strict";e.r(n);var s=e(0),i=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);i.options.__file="README.md";n.default=i.exports}}]);
