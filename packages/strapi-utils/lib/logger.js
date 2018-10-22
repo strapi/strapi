556739b9b622784efad692209c73fb627f6fbac8
entwicklerfr <ent.wickler@laposte.net>
2018-10-22 11:59:19 +0200

    add file save feature

--- packages/strapi-utils/lib/logger.js
+++ packages/strapi-utils/lib/logger.js
@@ -1,26 +1,60 @@
-'use strict';
+"use strict";
 
 /**
  * Logger.
  */
 
-const pino = require('pino');
-const _ = require('lodash');
 
-const logLevels = ['fatal', 'error', 'warn', 'info', 'debug', 'trace'];
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const pino = require("pino");
+
+const logLevels = ["fatal", "error", "warn", "info", "debug", "trace"];
 
 function getLogLevel() {
-  if (_.isString(process.env.STRAPI_LOG_LEVEL) && _.includes(logLevels, process.env.STRAPI_LOG_LEVEL.toLowerCase())) {
+  if (
+    _.isString(process.env.STRAPI_LOG_LEVEL) &&
+    _.includes(logLevels, process.env.STRAPI_LOG_LEVEL.toLowerCase())
+  ) {
     return process.env.STRAPI_LOG_LEVEL;
   }
-  return 'debug';
+  return "debug";
 }
+
+const logTypes = ["stdout", "file"];
+function getLogType() {
+  if (
+    _.isString(process.env.STRAPI_LOG_TYPE) &&
+    _.includes(logTypes, process.env.STRAPI_LOG_TYPE.toLowerCase())
+  ) {
+    return process.env.STRAPI_LOG_TYPE;
+  }
+  return "stdout";
+}
+
+function parseLogPath(defaultLogFilename = "strapi.log") {
+  if (_.isString(process.env.STRAPI_LOG_PATH)) {
+    return path.parse(process.env.STRAPI_LOG_PATH);
+  }
+  return path.parse(path.join(__dirname, defaultLogFilename));
+}
+
+function getLogDir() {
+  return parseLogPath().dir;
+}
+
+function getLogBase() {
+  return parseLogPath().base;
+}
+
+const logFile = path.join(getLogDir(), getLogBase());
 
 function getBool(envVar, defaultValue) {
   if (_.isBoolean(envVar)) return envVar;
   if (_.isString(envVar)) {
-    if (envVar === 'true') return true;
-    if (envVar === 'false') return false;
+    if (envVar === "true") return true;
+    if (envVar === "false") return false;
   }
   return defaultValue;
 }
@@ -34,14 +68,32 @@ const loggerConfig = {
 
 const pretty = pino.pretty({
   formatter: (logs, options) => {
-    return `${options.asColoredText({ level: 10 }, `[${new Date().toISOString()}]`)} ${options.prefix.toLowerCase()} ${logs.msg}`;
+    return `${options.asColoredText(
+      { level: 10 },
+      `[${new Date().toISOString()}]`
+    )} ${options.prefix.toLowerCase()} ${logs.msg}`;
   }
 });
 
-pretty.pipe(process.stdout);
-
-const logger = getBool(process.env.STRAPI_LOG_PRETTY_PRINT, true) ?
-  pino(loggerConfig, pretty):
-  pino(loggerConfig);
+let logger;
+switch (getLogType()) {
+  case "stdout":
+    pretty.pipe(process.stdout);
+    logger = getBool(process.env.STRAPI_LOG_PRETTY_PRINT, true)
+      ? pino(loggerConfig, pretty)
+      : pino(loggerConfig);
+    break;
+  case "file":
+    pretty.pipe(fs.createWriteStream(logFile, { flags: "a" }));
+    logger = getBool(process.env.STRAPI_LOG_PRETTY_PRINT, true)
+      ? pino(loggerConfig, pretty)
+      : pino(loggerConfig, fs.createWriteStream(logFile, { flags: "a" }));
+    break;
+  default:
+    pretty.pipe(process.stdout);
+    logger = getBool(process.env.STRAPI_LOG_PRETTY_PRINT, true)
+      ? pino(loggerConfig, pretty)
+      : pino(loggerConfig);
+}
 
 module.exports = logger;
