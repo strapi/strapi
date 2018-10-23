0383c4a6ba8ce8bf10e35b125c3a0a9366a3090c
Benjamin Devaublanc <benjamin.devaublanc@gmail.com>
2018-10-23 18:44:49 +0200

    Check if user exists before testing user.confirmed

--- packages/strapi-plugin-users-permissions/controllers/Auth.js
+++ packages/strapi-plugin-users-permissions/controllers/Auth.js
@@ -52,12 +52,12 @@ module.exports = {
       // Check if the user exists.
       const user = await strapi.query('user', 'users-permissions').findOne(query, ['role']);
 
-      if (_.get(await store.get({key: 'advanced'}), 'email_confirmation') && user && user.confirmed !== true) {
-        return ctx.badRequest(null, ctx.request.admin ? [{ messages: [{ id: 'Auth.form.error.confirmed' }] }] : 'Your account email is not confirmed.');
-      }
-
       if (!user) {
         return ctx.badRequest(null, ctx.request.admin ? [{ messages: [{ id: 'Auth.form.error.invalid' }] }] : 'Identifier or password invalid.');
+      }
+      
+      if (_.get(await store.get({key: 'advanced'}), 'email_confirmation') && user.confirmed !== true) {
+        return ctx.badRequest(null, ctx.request.admin ? [{ messages: [{ id: 'Auth.form.error.confirmed' }] }] : 'Your account email is not confirmed.');
       }
 
       if (user.blocked === true) {
