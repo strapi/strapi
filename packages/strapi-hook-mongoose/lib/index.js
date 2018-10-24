285cff327f8ef2429437e29e1c4389fc45cb224d
Omid Golzarian <omid.golzarian@gmail.com>
2018-10-24 17:33:22 +0330

    Add ObjectId toString function to index.js
    It lets GraphQL plugin to use id attributes and retrive it as String.
    Currently GraphQL plugin throws and error when trying to retrieve id for models.

--- packages/strapi-hook-mongoose/lib/index.js
+++ packages/strapi-hook-mongoose/lib/index.js
@@ -19,6 +19,12 @@ const { models: utilsModels }  = require('strapi-utils');
 const utils = require('./utils/');
 const relations = require('./relations');
 
+// ObjectId toString
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function () {
+  return this.toString()
+}
+
 /**
  * Mongoose hook
  */
