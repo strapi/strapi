889a9ccc2affd43dc47d6d956b16c66d2852a1d4
Alberto Maturano <alberto@maturano.mx>
2018-10-30 11:49:34 -0600

    Introduce a linter error for testing CI
    This commit is provokin this error:
    
    ```
    /strapi/strapi/packages/strapi/lib/index.js  6:7  error
    'foo' is assigned a value but never used  no-unused-vars
    ```
    
    The CI should detect this and avoid the merge to `master`

--- packages/strapi/lib/index.js
+++ packages/strapi/lib/index.js
@@ -3,11 +3,12 @@
 // Starting date
 global.startedAt = Date.now();
 
+const foo = "bar";
+
 /**
  * Instantiate and expose a Strapi singleton
  * (maintains legacy support).
  */
-
 module.exports = function(global) {
   try {
     return global.strapi = require('./Strapi'); // Strapi instance instanciated
