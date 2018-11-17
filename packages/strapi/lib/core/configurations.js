6bc30095371c7b3566b0237f65d71696a8bcc29b
ByoungYong Kim <kbyoungyong@conclusion.nl>
2018-11-17 22:55:41 +0100

    Fixed a bug

--- packages/strapi/lib/core/configurations.js
+++ packages/strapi/lib/core/configurations.js
@@ -391,7 +391,7 @@ const isAdminInDevMode = function () {
 const getURLFromSegments = function ({ hostname, port, ssl = false }) {
   const protocol = ssl ? 'https' : 'http';
   const defaultPort = ssl ? 443 : 80;
-  const portString = (port === undefined || port === defaultPort) ? '' : `:${port}`;
+  const portString = (port === undefined || parseInt(port) === defaultPort) ? '' : `:${port}`;
 
   return `${protocol}://${hostname}${portString}`;
 };
