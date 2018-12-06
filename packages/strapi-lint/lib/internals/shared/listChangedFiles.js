e7585b7372843ab5c1212a0c4ac265088b34f50a
soupette <cyril.lpz@gmail.com>
2018-12-06 11:09:23 +0100

    Test travis

--- packages/strapi-lint/lib/internals/shared/listChangedFiles.js
+++ packages/strapi-lint/lib/internals/shared/listChangedFiles.js
@@ -8,28 +8,34 @@
 const execFileSync = require('child_process').execFileSync;
 
 const exec = (command, args) => {
- console.log('> ' + [command].concat(args).join(' '));
- const options = {
-   cwd: process.cwd(),
-   env: process.env,
-   stdio: 'pipe',
-   encoding: 'utf-8',
- };
- return execFileSync(command, args, options);
+  console.log('> ' + [command].concat(args).join(' '));
+  const options = {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: 'pipe',
+    encoding: 'utf-8',
+  };
+
+  return execFileSync(command, args, options);
 };
 
 const execGitCmd = args =>
- exec('git', args)
-   .trim()
-   .toString()
-   .split('\n');
+  exec('git', args)
+    .trim()
+    .toString()
+    .split('\n');
 
 const listChangedFiles = () => {
- const mergeBase = execGitCmd(['merge-base', 'HEAD', 'master']);
- return new Set([
-   ...execGitCmd(['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase]),
-   ...execGitCmd(['ls-files', '--others', '--exclude-standard']),
- ]);
+  const mergeBase = execGitCmd(['merge-base', 'HEAD', 'master']);
+
+  try {
+    return new Set([
+      ...execGitCmd(['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase]),
+      ...execGitCmd(['ls-files', '--others', '--exclude-standard']),
+    ]);
+  } catch (err) {
+    return [];
+  }
 };
 
 module.exports = listChangedFiles;
