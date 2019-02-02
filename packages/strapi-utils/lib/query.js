bd1930a75c099c494953c2267828fec236111bc4
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2019-02-02 13:25:09 +0100

    Create a generic Builder and Query APIs

---
+++ packages/strapi-utils/lib/query.js
@@ -0,0 +1,29 @@
+
+class Query {
+  constructor(model) {
+    this.model = model;
+    const hook = strapi.hook[this.model.orm];
+    const ORMQuery = hook.load().Query;
+    this.instance = new ORMQuery(this.model);
+  }
+
+  find(...args) {
+    return this.instance.find(...args);
+  }
+
+  count(filter) {
+    return this.instance.count(filter);
+  }
+
+  populate(populate) {
+    return this.instance.populate(populate);
+  }
+
+  execute() {
+    return this.instance.execute();
+  }
+}
+
+module.exports = {
+  Query
+};
