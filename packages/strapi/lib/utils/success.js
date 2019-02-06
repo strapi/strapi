3e62c372f88c588f7a48c1cf166d4f7b0edee4ca
Aurélien Georget <aurelsicoko@gmail.com>
2019-02-06 14:49:46 +0100

    Detect Yarn installation

--- packages/strapi/lib/utils/success.js
+++ packages/strapi/lib/utils/success.js
@@ -13,11 +13,15 @@ const { machineIdSync } = require('node-machine-id');
  * It allows us to know on how many times the package has been installed globally. 
  */
 
-if (process.env.npm_config_global === 'true') {
-  fetch('https://analytics.strapi.io/track', {
-    method: 'POST',
-    body: JSON.stringify({ event: 'didInstallStrapi', deviceId: machineIdSync() }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .catch(() => {});
+try {
+  if (process.env.npm_config_global === 'true' || JSON.parse(process.env.npm_config_argv).original.includes('global')) {
+    fetch('https://analytics.strapi.io/track', {
+      method: 'POST',
+      body: JSON.stringify({ event: 'didInstallStrapi', deviceId: machineIdSync() }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .catch(() => {});
+  }
+} catch (e) { 
+  //... 
 }
