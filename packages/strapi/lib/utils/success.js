1c740a40ce41106aec61476dcecef8907f1cbce0
Aurélien Georget <aurelsicoko@gmail.com>
2019-02-06 11:46:40 +0100

    Fix success install

--- packages/strapi/lib/utils/success.js
+++ packages/strapi/lib/utils/success.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-
 'use strict';
 
 /**
  * Module dependencies
  */
 
-// Node.js core.
 const fetch = require('node-fetch');
+const { machineIdSync } = require('node-machine-id');
+
+/*
+ * No need to worry about this file, we only retrieve anonymous data here.
+ * It allows us to know on how many times the package has been installed globally. 
+ */
 
 if (process.env.npm_config_global === 'true') {
   fetch('https://analytics.strapi.io/track', {
     method: 'POST',
-    body: JSON.stringify({ event: 'didInstallStrapi' }),
+    body: JSON.stringify({ event: 'didInstallStrapi', deviceId: machineIdSync() }),
     headers: { 'Content-Type': 'application/json' }
   })
     .catch(() => {});
 }
-
-
