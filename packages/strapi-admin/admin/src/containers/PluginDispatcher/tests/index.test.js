bb07e5eb39b746c13917a5d5912a03e0d0a31f05
soupette <cyril.lpz@gmail.com>
2019-02-06 15:12:01 +0100

    Created PluginDispatcher that replaces PluginPage

---
+++ packages/strapi-admin/admin/src/containers/PluginDispatcher/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import { PluginDispatcher } from '../index';
+
+describe('<PluginDispatcher />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
