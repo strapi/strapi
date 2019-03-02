5539a3e27c09b94cd7cd59a25e28a61837b816aa
REDDY PRASAD <dev.drprasad@aim.com>
2019-03-02 19:53:33 +0530

    :nail_care: Show proper error messages for 'File Upload' plugin

--- packages/strapi-plugin-upload/admin/src/containers/HomePage/saga.js
+++ packages/strapi-plugin-upload/admin/src/containers/HomePage/saga.js
@@ -1,6 +1,6 @@
 // import { LOCATION_CHANGE } from 'react-router-redux';
 import { Map } from 'immutable';
-import { isEmpty } from 'lodash';
+import { isEmpty, get, isObject } from 'lodash';
 import {
   all,
   call,
@@ -81,8 +81,11 @@ function* uploadFiles(action) {
       strapi.notification.success({ id: 'upload.notification.dropFiles.success', values: { number: newFiles.length } });
     }
 
-  } catch(err) {
-    strapi.notification.error('notification.error');
+  } catch(error) {
+    let message = get(error, ['response', 'payload', 'message', '0', 'messages', '0']);
+    if (isObject(message)) message = {...message, id: `upload.${message.id}`};
+
+    strapi.notification.error(message || 'notification.error');
   } finally {
     yield put(unsetLoading());
   }
