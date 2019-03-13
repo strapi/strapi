758825844529c41cd6d0d57c10b30b9bc655418e
kale-io <cos+a@iuvo.ai>
2019-03-13 20:31:51 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/strapi/lib/middlewares/csrf/index.js
+++ packages/strapi/lib/middlewares/csrf/index.js
@@ -14,7 +14,7 @@ module.exports = strapi => {
      * Initialize the hook
      */
 
-    initialize: function(cb) {
+    initialize: cb => {
       strapi.app.use(
         async (ctx, next) => {
           if (ctx.request.admin) return await next();
