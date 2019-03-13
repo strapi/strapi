536128d6c31176ab26196f355a5a12a1975846f3
kale-io <cos+a@iuvo.ai>
2019-03-13 18:50:19 +0000

    Convert checks for missing indexOf to use includes ECMAScript 7 introduced the includes function for arrays so that bitwise and comparisons to -1 are no longer needed

--- packages/strapi-plugin-upload/config/queries/mongoose.js
+++ packages/strapi-plugin-upload/config/queries/mongoose.js
@@ -43,7 +43,7 @@ module.exports = {
 
     return this.create(values)
       .catch((err) => {
-        if (err.message.indexOf('index:') !== -1) {
+        if (err.message.includes('index:')) {
           const message = err.message.split('index:');
           const field = _.words(_.last(message).split('_')[0]);
           const error = { message: `This ${field} is already taken`, field };
