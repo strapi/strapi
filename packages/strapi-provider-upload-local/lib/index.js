75d98871e83c8c124eea3ff9ab08e7cd81c8509f
EpicUsaMan <epicusaman@gmail.com>
2019-03-16 18:42:45 +0200

    Revert changes

--- packages/strapi-provider-upload-local/lib/index.js
+++ packages/strapi-provider-upload-local/lib/index.js
@@ -12,43 +12,19 @@ module.exports = {
   provider: 'local',
   name: 'Local server',
   init: (config) => {
-    // write file in public/assets folder
-    const write = (file, dirPath) => {
-      return new Promise((resolve, reject) => {
-        fs.writeFile(path.join(dirPath, `${file.hash}${file.ext}`), file.buffer, (err) => {
-          if (err) {
-            return reject(err);
-          }
-
-          file.url = `/uploads/${file.hash}${file.ext}`;
-
-          resolve();
-        });
-      });
-    };
-
     return {
       upload: (file) => {
         return new Promise((resolve, reject) => {
-          const dirPath = path.join(strapi.config.appPath, 'public', 'uploads');
-          
-          // Check if directory exists
-          fs.exists(dirPath, (exists) => {
-            if (exists) {
-              resolve(write(file, dirPath));
-            } else {
-              // Create directory if not exists
-              fs.mkdir(dirPath, {recursive: true}, (err) => {
-                if (err) {
-                  return reject(err);
-                }
-
-                resolve(write(file, dirPath));
-              });
+          // write file in public/assets folder
+          fs.writeFile(path.join(strapi.config.appPath, 'public', `uploads/${file.hash}${file.ext}`), file.buffer, (err) => {
+            if (err) {
+              return reject(err);
             }
+
+            file.url = `/uploads/${file.hash}${file.ext}`;
+
+            resolve();
           });
-          
-          
         });
       },
       delete: (file) => {
