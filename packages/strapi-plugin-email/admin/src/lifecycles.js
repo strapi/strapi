be95294bdcd6d34d5ce1f66e26c27f138543eb53
soupette <cyril.lpz@gmail.com>
2019-04-02 14:17:01 +0200

    Add lifecycles to all plugins

---
+++ packages/strapi-plugin-email/admin/src/lifecycles.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * SET THE HOOKS TO ENABLE THE MAGIC OF STRAPI.
+ * -------------------------------------------
+ *
+ * Secure, customise and enhance your project by setting
+ * the hooks via this file.
+ *
+ */
+
+module.exports = function lifecycles() {};
