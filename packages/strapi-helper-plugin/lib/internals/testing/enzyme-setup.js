5008038f53dd912f85a027d8498743064dc9ece8
soupette <cyril.lpz@gmail.com>
2019-04-25 14:32:50 +0200

    Clean helper plugin and admin scripts

--- packages/strapi-helper-plugin/lib/internals/testing/enzyme-setup.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const configure = require('enzyme').configure;
-const Adapter = require('enzyme-adapter-react-16');
-
-configure({
-  adapter: new Adapter(),
-  disableLifecycleMethods: false,
-});
