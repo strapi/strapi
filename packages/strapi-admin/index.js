7beda02158a10304a1b4836327af775a54eb73c4
Alexandre Bodin <bodin.alex@gmail.com>
2019-04-29 15:48:16 +0200

    Optimize build and clean scripts

---
+++ packages/strapi-admin/index.js
@@ -0,0 +1,158 @@
+const path = require('path');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const getWebpackConfig = require('./webpack.config.js');
+
+const getPkgPath = name =>
+  path.dirname(require.resolve(`${name}/package.json`));
+
+function createPluginsJs(plugins, dest) {
+  const content = `
+    const injectReducer = require('./utils/injectReducer').default;
+    const injectSaga = require('./utils/injectSaga').default;
+    const { languages } = require('./i18n');
+
+    window.strapi = Object.assign(window.strapi || {}, {
+      node: MODE || 'host',
+      backendURL: BACKEND_URL,
+      languages,
+      currentLanguage:
+      window.localStorage.getItem('strapi-admin-language') ||
+      window.navigator.language ||
+      window.navigator.userLanguage ||
+      'en',
+      injectReducer,
+      injectSaga,
+    });
+
+    module.exports = {
+      ${plugins
+        .map(name => {
+          const shortName = name.replace(/^strapi-plugin-/i, '');
+          const req = `require('../../plugins/${name}/admin/src').default`;
+          return `'${shortName}': ${req}`;
+        })
+        .join(',\n')}
+    }
+  `;
+
+  fs.writeFileSync(path.resolve(dest, 'admin', 'src', 'plugins.js'), content);
+}
+
+async function copyPlugin(name, dest) {
+  const pkgFilePath = getPkgPath(name);
+
+  const resolveDepPath = (...args) => path.resolve(pkgFilePath, ...args);
+  const resolveDest = (...args) => path.resolve(dest, 'plugins', name, ...args);
+
+  const copy = (...args) => {
+    fs.copySync(resolveDepPath(...args), resolveDest(...args));
+  };
+
+  // Copy the entire admin folder
+  copy('admin');
+
+  // Copy the layout.js if it exists
+  if (fs.existsSync(path.resolve(pkgFilePath, 'config', 'layout.js'))) {
+    fs.ensureDirSync(resolveDest('config'));
+    copy('config', 'layout.js');
+  }
+
+  copy('package.json');
+}
+
+async function copyAdmin(dest) {
+  const adminPath = getPkgPath('strapi-admin');
+
+  await fs.ensureDir(path.resolve(dest, 'config'));
+  await fs.copy(path.resolve(adminPath, 'admin'), path.resolve(dest, 'admin'));
+  await fs.copy(
+    path.resolve(adminPath, 'config', 'layout.js'),
+    path.resolve(dest, 'config', 'layout.js')
+  );
+}
+
+async function build({ dir, env }) {
+  console.log('Building your app');
+  const cacheDir = path.resolve(dir, '.cache');
+
+  const pkgJSON = require(path.join(dir, 'package.json'));
+
+  // create .cache dir
+  await fs.ensureDir(cacheDir);
+
+  await copyAdmin(cacheDir);
+
+  const pluginsToCopy = Object.keys(pkgJSON.dependencies).filter(
+    dep =>
+      dep.startsWith('strapi-plugin') &&
+      fs.existsSync(path.resolve(getPkgPath(dep), 'admin', 'src', 'index.js'))
+  );
+
+  pluginsToCopy.forEach(name => copyPlugin(name, cacheDir));
+
+  createPluginsJs(pluginsToCopy, cacheDir);
+
+  const entry = path.resolve(cacheDir, 'admin', 'src', 'app.js');
+  const dest = path.resolve(dir, 'build');
+
+  const config = getWebpackConfig({ entry, dest, env });
+
+  const compiler = webpack(config);
+
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      let messages;
+      if (err) {
+        if (!err.message) {
+          return reject(err);
+        }
+        messages = {
+          errors: [err.message],
+          warnings: [],
+        };
+      } else {
+        messages = stats.toJson({ all: false, warnings: true, errors: true });
+      }
+
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1;
+        }
+        return reject(new Error(messages.errors.join('\n\n')));
+      }
+
+      return resolve({
+        stats,
+        warnings: messages.warnings,
+      });
+    });
+  })
+    .then(
+      ({ stats, warnings }) => {
+        console.log(chalk.green('Compiled successfully.\n'));
+      },
+      err => {
+        console.log(chalk.red('Failed to compile.\n'));
+
+        const message = err != null && err.message;
+        console.log((message || err) + '\n');
+        console.log();
+
+        process.exit(1);
+      }
+    )
+    .catch(err => {
+      if (err && err.message) {
+        console.log(err.message);
+      }
+      process.exit(1);
+    });
+}
+
+module.exports = {
+  build,
+};
--- packages/strapi-admin/webpack.config.js
+++ packages/strapi-admin/webpack.config.js
@@ -6,221 +6,248 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackDashboard = require('webpack-dashboard/plugin');
 const DuplicatePckgChecker = require('duplicate-package-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const isWsl = require('is-wsl');
 const alias = require('./webpack.alias.js');
 
+// TODO: parametrize
 const URLs = {
   host: '/admin/',
   backend: 'http://localhost:1337',
-  publicPath: '/admin/',
   mode: 'host',
 };
 
-const webpackPlugins = ({ dev }) =>
-  dev
+module.exports = ({ publicPath = '/admin/', entry, dest, env }) => {
+  const isProduction = env === 'production';
+
+  const webpackPlugins = isProduction
     ? [
-        new WebpackDashboard(),
-        new DuplicatePckgChecker({
-          verbose: true,
-        }),
-        new FriendlyErrorsWebpackPlugin(),
-        // new BundleAnalyzerPlugin(),
-        // new OpenBrowserWebpackPlugin({
-        //   url: `http://localhost:${PORT}/${URLs.publicPath}`,
-        // }),
-      ]
-    : [
         new webpack.IgnorePlugin({
           resourceRegExp: /^\.\/locale$/,
           contextRegExp: /moment$/,
         }),
         new MiniCssExtractPlugin({
-          filename: dev ? '[name].css' : '[name].[chunkhash].css',
-          chunkFilename: dev
-            ? '[name].chunk.css'
-            : '[name].[chunkhash].chunkhash.css',
+          filename: '[name].[chunkhash].css',
+          chunkFilename: '[name].[chunkhash].chunkhash.css',
         }),
+      ]
+    : [
+        new DuplicatePckgChecker({
+          verbose: true,
+        }),
+        new FriendlyErrorsWebpackPlugin(),
       ];
 
-// Use style loader in dev mode to optimize compilation
-const scssLoader = ({ dev }) =>
-  dev
-    ? [{ loader: 'style-loader', options: {} }]
-    : [
+  const scssLoader = isProduction
+    ? [
         {
           loader: MiniCssExtractPlugin.loader,
           options: {
             fallback: require.resolve('style-loader'),
-            publicPath: URLs.publicPath,
+            publicPath: publicPath,
           },
         },
-      ];
+      ]
+    : [{ loader: 'style-loader', options: {} }];
 
-module.exports = ({ entry, dest, dev = false }) => ({
-  mode: dev ? 'development' : 'production',
-  bail: dev ? false : true,
-  devServer: {
-    historyApiFallback: {
-      index: URLs.publicPath,
+  return {
+    mode: isProduction ? 'production' : 'development',
+    bail: isProduction ? true : false,
+    devtool: isProduction ? false : 'cheap-module-source-map',
+    entry,
+    output: {
+      path: dest,
+      publicPath: publicPath,
+      // Utilize long-term caching by adding content hashes (not compilation hashes)
+      // to compiled assets for production
+      filename: isProduction ? '[name].js' : '[name].[chunkhash].js',
+      chunkFilename: isProduction
+        ? '[name].chunk.js'
+        : '[name].[chunkhash].chunk.js',
     },
-    port: 4000,
-    stats: 'minimal',
-    // hot: true,
-  },
-  stats: dev ? 'minimal' : 'errors-only',
-  devtool: 'cheap-module-source-map',
-  entry,
-  output: {
-    path: dest,
-    publicPath: URLs.publicPath,
-    // Utilize long-term caching by adding content hashes (not compilation hashes)
-    // to compiled assets for production
-    filename: dev ? '[name].js' : '[name].[chunkhash].js',
-    chunkFilename: dev ? '[name].chunk.js' : '[name].[chunkhash].chunk.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: require.resolve('babel-loader'),
-          options: {
-            cacheDirectory: true,
-            cacheCompression: !dev,
-            compact: !dev,
-            presets: [
-              require.resolve('@babel/preset-env'),
-              require.resolve('@babel/preset-react'),
-            ],
-            plugins: [
-              require.resolve('@babel/plugin-proposal-class-properties'),
-              require.resolve('@babel/plugin-syntax-dynamic-import'),
-              require.resolve(
-                '@babel/plugin-proposal-async-generator-functions'
-              ),
-              [
-                require.resolve('@babel/plugin-transform-runtime'),
-                {
-                  helpers: true,
-                  regenerator: true,
-                },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        // Copied from react-scripts
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+          parallel: !isWsl,
+          // Enable file caching
+          cache: true,
+          sourceMap: false,
+        }),
+      ],
+      // splitChunks: {
+      //   chunks: 'all',
+      //   name: false,
+      // },
+      runtimeChunk: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: require.resolve('babel-loader'),
+            options: {
+              cacheDirectory: true,
+              cacheCompression: isProduction,
+              compact: isProduction,
+              presets: [
+                require.resolve('@babel/preset-env'),
+                require.resolve('@babel/preset-react'),
               ],
-            ],
+              plugins: [
+                require.resolve('@babel/plugin-proposal-class-properties'),
+                require.resolve('@babel/plugin-syntax-dynamic-import'),
+                require.resolve(
+                  '@babel/plugin-proposal-async-generator-functions'
+                ),
+                [
+                  require.resolve('@babel/plugin-transform-runtime'),
+                  {
+                    helpers: true,
+                    regenerator: true,
+                  },
+                ],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        use: [
-          {
-            loader: require.resolve('style-loader'),
-          },
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              sourceMap: false,
+        {
+          test: /\.css$/,
+          include: /node_modules/,
+          use: [
+            {
+              loader: require.resolve('style-loader'),
             },
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              config: {
-                path: path.resolve(__dirname, 'postcss.config.js'),
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                sourceMap: false,
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: scssLoader({ dev }).concat([
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              localIdentName: '[local]__[path][name]__[hash:base64:5]',
-              modules: true,
-              importLoaders: 1,
-              sourceMap: false,
-            },
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              config: {
-                path: path.resolve(__dirname, 'postcss.config.js'),
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                config: {
+                  path: path.resolve(__dirname, 'postcss.config.js'),
+                },
               },
             },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ]),
-      },
-      {
-        test: /\.(svg|eot|otf|ttf|woff|woff2)$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loaders: [
-          require.resolve('file-loader'),
-          {
-            loader: require.resolve('image-webpack-loader'),
-            query: {
-              mozjpeg: {
-                progressive: true,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              optipng: {
-                optimizationLevel: 4,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        include: [path.join(__dirname, 'src')],
-        use: require.resolve('html-loader'),
-      },
-      {
-        test: /\.(mp4|webm)$/,
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
+          ],
         },
-      },
-    ],
-  },
-  resolve: {
-    alias,
-    symlinks: false,
-    extensions: ['.js', '.jsx', '.react.js'],
-    mainFields: ['browser', 'jsnext:main', 'main'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve(__dirname, 'index.html'),
-      favicon: path.resolve(__dirname, 'admin/src/favicon.ico'),
-    }),
-    new SimpleProgressWebpackPlugin(),
+        {
+          test: /\.scss$/,
+          use: scssLoader.concat([
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                localIdentName: '[local]__[path][name]__[hash:base64:5]',
+                modules: true,
+                importLoaders: 1,
+                sourceMap: false,
+              },
+            },
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                config: {
+                  path: path.resolve(__dirname, 'postcss.config.js'),
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ]),
+        },
+        {
+          test: /\.(svg|eot|otf|ttf|woff|woff2)$/,
+          use: 'file-loader',
+        },
+        {
+          test: /\.(jpg|png|gif)$/,
+          loaders: [
+            require.resolve('file-loader'),
+            {
+              loader: require.resolve('image-webpack-loader'),
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                optipng: {
+                  optimizationLevel: 4,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          include: [path.join(__dirname, 'src')],
+          use: require.resolve('html-loader'),
+        },
+        {
+          test: /\.(mp4|webm)$/,
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
+    },
+    resolve: {
+      alias,
+      symlinks: false,
+      extensions: ['.js', '.jsx', '.react.js'],
+      mainFields: ['browser', 'jsnext:main', 'main'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, 'index.html'),
+        favicon: path.resolve(__dirname, 'admin/src/favicon.ico'),
+      }),
+      new SimpleProgressWebpackPlugin(),
 
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      REMOTE_URL: JSON.stringify(URLs.host),
-      BACKEND_URL: JSON.stringify(URLs.backend),
-      MODE: JSON.stringify(URLs.mode), // Allow us to define the public path for the plugins assets.
-      PUBLIC_PATH: JSON.stringify(URLs.publicPath),
-    }),
-  ].concat(webpackPlugins({ dev })),
-});
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        REMOTE_URL: JSON.stringify(URLs.host),
+        BACKEND_URL: JSON.stringify(URLs.backend),
+        MODE: JSON.stringify(URLs.mode), // Allow us to define the public path for the plugins assets.
+        PUBLIC_PATH: JSON.stringify(publicPath),
+      }),
+
+      ...webpackPlugins,
+    ],
+  };
+};
