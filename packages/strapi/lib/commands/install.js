fae94ac8638570e74e1a44a2527eb5203cbfca08
Alexandre Bodin <bodin.alex@gmail.com>
2019-05-03 16:35:33 +0200

    Clean dependencies and fix install/uninstall plugin

--- packages/strapi/lib/commands/install.js
+++ packages/strapi/lib/commands/install.js
@@ -1,8 +1,9 @@
-const path = require('path');
-const fs = require('fs');
+const { join } = require('path');
+const { existsSync } = require('fs-extra');
 const ora = require('ora');
 const execa = require('execa');
 const { cli } = require('strapi-utils');
+const findPackagePath = require('../load/package-path');
 
 module.exports = async plugins => {
   if (!cli.isStrapiApp()) {
@@ -14,12 +15,14 @@ module.exports = async plugins => {
   const loader = ora();
   const dir = process.cwd();
 
-  const pluginArgs = plugins.map(name => `strapi-plugin-${name}`);
+  const version = require(join(dir, 'package.json')).dependencies.strapi;
+
+  const pluginArgs = plugins.map(name => `strapi-plugin-${name}@${version}`);
 
   try {
     loader.start(`Installing dependencies`);
 
-    const useYarn = fs.existsSync(path.join(dir, 'yarn.lock'));
+    const useYarn = existsSync(join(dir, 'yarn.lock'));
     if (useYarn) {
       await execa('yarn', ['add', ...pluginArgs]);
     } else {
@@ -28,12 +31,23 @@ module.exports = async plugins => {
 
     loader.succeed();
 
-    loader.start(`Rebuilding admin UI`);
-    await execa('npm', ['run', 'build']);
-    loader.succeed();
+    // check if rebuild is necessary
+    let shouldRebuild = false;
+    for (let name of plugins) {
+      let pkgPath = findPackagePath(`strapi-plugin-${name}`);
+      if (existsSync(join(pkgPath, 'admin', 'src', 'index.js'))) {
+        shouldRebuild = true;
+      }
+    }
+
+    if (shouldRebuild) {
+      loader.start(`Rebuilding admin UI`);
+      await execa('npm', ['run', 'build']);
+      loader.succeed();
+    }
   } catch (err) {
     loader.clear();
     console.error(err.message);
-    process.exit(1)
+    process.exit(1);
   }
 };
--- packages/strapi/lib/commands/uninstall.js
+++ packages/strapi/lib/commands/uninstall.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const path = require('path');
-const fs = require('fs');
+const { join } = require('path');
+const { existsSync } = require('fs-extra');
 const ora = require('ora');
 const execa = require('execa');
 const { cli } = require('strapi-utils');
+const findPackagePath = require('../load/package-path');
 
 module.exports = async plugins => {
   if (!cli.isStrapiApp()) {
@@ -16,12 +17,23 @@ module.exports = async plugins => {
   const loader = ora();
   const dir = process.cwd();
 
-  const pluginArgs = plugins.map(name => `strapi-plugin-${name}`);
+  const version = require(join(dir, 'package.json')).dependencies.strapi;
+
+  const pluginArgs = plugins.map(name => `strapi-plugin-${name}@${version}`);
 
   try {
+    // verify should rebuild before removing the pacakge
+    let shouldRebuild = false;
+    for (let name of plugins) {
+      let pkgPath = findPackagePath(`strapi-plugin-${name}`);
+      if (existsSync(join(pkgPath, 'admin', 'src', 'index.js'))) {
+        shouldRebuild = true;
+      }
+    }
+
     loader.start(`Uninstalling dependencies`);
 
-    const useYarn = fs.existsSync(path.join(dir, 'yarn.lock'));
+    const useYarn = existsSync(join(dir, 'yarn.lock'));
     if (useYarn) {
       await execa('yarn', ['remove', ...pluginArgs]);
     } else {
@@ -30,9 +42,11 @@ module.exports = async plugins => {
 
     loader.succeed();
 
-    loader.start(`Rebuilding admin UI`);
-    await execa('npm', ['run', 'build']);
-    loader.succeed();
+    if (shouldRebuild) {
+      loader.start(`Rebuilding admin UI`);
+      await execa('npm', ['run', 'build']);
+      loader.succeed();
+    }
   } catch (err) {
     loader.clear();
     console.error(err.message);
