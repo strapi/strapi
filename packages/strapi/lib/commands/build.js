773068bb2389d2412bb0b7578ccf029ac0824dcc
Alexandre Bodin <bodin.alex@gmail.com>
2019-05-16 16:33:47 +0200

    Fix issues with paths in windows

--- packages/strapi/lib/commands/build.js
+++ packages/strapi/lib/commands/build.js
@@ -48,7 +48,7 @@ module.exports = async () => {
       env: 'production',
       options: {
         backend: adminBackend,
-        publicPath: path.join('/', adminPath, '/'),
+        publicPath: addSlash(adminPath),
       },
     })
     .then(() => {
@@ -59,3 +59,12 @@ module.exports = async () => {
       process.exit(1);
     });
 };
+
+function addSlash(path) {
+  if (typeof path !== 'string') throw new Error('admin.path must be a string');
+  if (path === '' || path === '/') return '/';
+
+  if (path[0] != '/') path = '/' + path;
+  if (path[path.length - 1] != '/') path = path + '/';
+  return path;
+}
