0c970973c5f2cc16a2d1d3ee37e3b3d126d5e2f4
Virginie Ky <virginie.ky@gmail.com>
2019-07-01 19:15:41 +0200

    PR review UI

---
+++ packages/strapi-helper-plugin/lib/src/assets/styles/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+  beige: '#eff3f6',
+  black: '#3b3b3b',
+  brightGrey: '#f0f3f8',
+  green: '#27b70f',
+  greyOpacity: 'rgba(14, 22, 34, 0.02)',
+  lightGrey: '#fafafa',
+
+  // Specific colors for left menu
+  leftMenu: {
+    black: '#2D3138',
+    grey: '#666B74',
+    mediumGrey: '#f2f3f4',
+    lightGrey: '#E9EAEB',
+  },
+};
+
+export default colors;
---
+++ packages/strapi-helper-plugin/lib/src/assets/styles/sizes.js
@@ -0,0 +1,13 @@
+const sizes = {
+  header: {
+    height: '6rem',
+  },
+  margin: 10,
+  modal: {
+    footer: {
+      height: '6rem',
+    },
+  },
+};
+
+export default sizes;
