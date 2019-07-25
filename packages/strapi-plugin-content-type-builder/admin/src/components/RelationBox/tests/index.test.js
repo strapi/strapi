5c8b368338c1b1b2797079848ea8b50f7e972ca9
Virginie Ky <virginie.ky@gmail.com>
2019-07-25 18:13:19 +0200

    Relation box UT

--- packages/strapi-plugin-content-type-builder/admin/src/components/RelationBox/tests/index.test.js
+++ packages/strapi-plugin-content-type-builder/admin/src/components/RelationBox/tests/index.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { AST_True } from 'terser';
+import { shallow } from 'enzyme';
+
+import RelationBox from '../index';
+import { InputTextWithErrors as InputText } from 'strapi-helper-plugin';
 
 describe('<RelationBox />', () => {
+  const props = {
+    onChange: jest.fn(),
+    value: '',
+  };
+
   it('should not crash', () => {
-    expect(true).toBe(true);
+    shallow(<RelationBox {...props} />);
+  });
+
+  it('should use the defaultProps', () => {
+    const {
+      defaultProps: { onClick },
+    } = RelationBox;
+
+    expect(onClick).toBeDefined();
+    expect(onClick()).toBe(undefined);
+  });
+
+  it('should display source if it exists', () => {
+    props.main = true;
+    props.source = 'users-permissions';
+
+    const wrapper = shallow(<RelationBox {...props} />);
+
+    const span = wrapper
+      .find('p')
+      .at(0)
+      .find('span');
+
+    expect(span.text()).toContain('users-permissions');
+  });
+
+  it('target value should not be value if nature is oneWay or manyWays', () => {
+    props.nature = 'oneWay';
+    const wrapper = shallow(<RelationBox {...props} />);
+    const { value } = wrapper.find(InputText).props();
+
+    expect(value).toBe('-');
+
+    props.nature = 'manyWays';
+    expect(value).toBe('-');
   });
 });
-AST_True;
