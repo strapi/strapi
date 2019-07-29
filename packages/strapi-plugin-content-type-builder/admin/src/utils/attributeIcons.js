cdf40a2bc120222c0a8e14665b62f424024ee7fd
Virginie Ky <virginie.ky@gmail.com>
2019-07-29 18:09:37 +0200

    Groups type in attribute picker modal

--- packages/strapi-plugin-content-type-builder/admin/src/utils/attributeIcons.js
+++ packages/strapi-plugin-content-type-builder/admin/src/utils/attributeIcons.js
@@ -2,6 +2,7 @@ import boolean from '../assets/icons/attributes/icon_boolean.png';
 import date from '../assets/icons/attributes/icon_date.png';
 import email from '../assets/icons/attributes/icon_email.png';
 import enumeration from '../assets/icons/attributes/icon_enumeration.png';
+import group from '../assets/icons/attributes/icon_group.png';
 import media from '../assets/icons/attributes/icon_media.png';
 import json from '../assets/icons/attributes/icon_json.png';
 import number from '../assets/icons/attributes/icon_number.png';
@@ -15,6 +16,7 @@ const attributeIcons = {
   date,
   email,
   enumeration,
+  group,
   media,
   json,
   number,
