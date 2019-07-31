c530afa8a3d0869d1c17d146b54c44f955f9ef72
Alexandre Bodin <bodin.alex@gmail.com>
2019-07-31 09:12:49 +0200

    Validate content-type-buildeer group schema

---
+++ packages/strapi-plugin-content-type-builder/controllers/validation/types.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const yup = require('yup');
+const { validators, VALID_TYPES, isValidName } = require('./common');
+
+module.exports = obj => {
+  return {
+    type: yup
+      .string()
+      .oneOf(VALID_TYPES)
+      .required(),
+    ...getTypeShape(obj),
+  };
+};
+
+const getTypeShape = obj => {
+  switch (obj.type) {
+    /**
+     * complexe types
+     */
+
+    case 'media': {
+      return {
+        multiple: yup.boolean(),
+        required: validators.required,
+        unique: validators.unique,
+      };
+    }
+
+    /**
+     * scalar types
+     */
+    case 'string':
+    case 'text':
+    case 'richtext': {
+      return {
+        default: yup.string(),
+        required: validators.required,
+        unique: validators.unique,
+        min: validators.minLength,
+        max: validators.maxLength,
+      };
+    }
+    case 'json': {
+      return {
+        required: validators.required,
+        unique: validators.unique,
+      };
+    }
+    case 'enumeration': {
+      return {
+        enum: yup
+          .array()
+          .of(yup.string().test(isValidName))
+          .min(1)
+          .required(),
+        default: yup
+          .string()
+          .when('enum', enumVal => yup.string().oneOf(enumVal)),
+        enumName: yup.string().test(isValidName),
+        required: validators.required,
+        unique: validators.unique,
+      };
+    }
+    case 'password': {
+      return {
+        required: validators.required,
+        min: validators.minLength,
+        max: validators.maxLength,
+      };
+    }
+    case 'email': {
+      return {
+        default: yup.string().email(),
+        required: validators.required,
+        unique: validators.unique,
+        min: validators.minLength,
+        max: validators.maxLength,
+      };
+    }
+    case 'integer': {
+      return {
+        default: yup.number().integer(),
+        required: validators.required,
+        unique: validators.unique,
+        min: yup
+          .number()
+          .integer()
+          .positive(),
+        max: yup
+          .number()
+          .integer()
+          .positive(),
+      };
+    }
+    case 'float': {
+      return {
+        default: yup.number(),
+        required: validators.required,
+        unique: validators.unique,
+        min: yup.number().positive(),
+        max: yup.number().positive(),
+      };
+    }
+    case 'decimal': {
+      return {
+        default: yup.number(),
+        required: validators.required,
+        unique: validators.unique,
+        min: yup.number().positive(),
+        max: yup.number().positive(),
+      };
+    }
+    case 'date': {
+      return {
+        default: yup.date(),
+        required: validators.required,
+        unique: validators.unique,
+      };
+    }
+    case 'boolean': {
+      return {
+        default: yup.boolean(),
+        required: validators.required,
+        unique: validators.unique,
+      };
+    }
+    default: {
+      return {};
+    }
+  }
+};
