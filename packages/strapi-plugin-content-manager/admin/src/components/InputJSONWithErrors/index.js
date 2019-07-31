07fbfecc87231fd5ff71a2d640497e3d20a957c2
soupette <cyril.lpz@gmail.com>
2019-07-31 15:01:23 +0200

    Fixes #2882

--- packages/strapi-plugin-content-manager/admin/src/components/InputJSONWithErrors/index.js
+++ packages/strapi-plugin-content-manager/admin/src/components/InputJSONWithErrors/index.js
@@ -222,7 +222,11 @@ InputJSONWithErrors.propTypes = {
   style: PropTypes.object,
   tabIndex: PropTypes.string,
   validations: PropTypes.object,
-  value: PropTypes.object,
+  value: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
 };
 
 export default InputJSONWithErrors;
