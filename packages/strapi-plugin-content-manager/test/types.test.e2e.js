24a5f45e0d339a6fe5b462eab339f11df0215fc6
Alexandre Bodin <bodin.alex@gmail.com>
2019-08-07 18:06:01 +0200

    Add float tests

--- packages/strapi-plugin-content-manager/test/types.test.e2e.js
+++ packages/strapi-plugin-content-manager/test/types.test.e2e.js
@@ -742,11 +742,88 @@ describe('Types', () => {
   });
 
   describe('Test type float', () => {
-    test.todo('Create entry with valid value');
+    beforeAll(async () => {
+      await createModelWithType('withfloat', 'float');
+    }, 60000);
 
-    test.todo('Reading entry, returns correct value');
+    afterAll(async () => {
+      await deleteModel('withfloat');
+    }, 60000);
 
-    test.todo('Updating entry sets the right value and format');
+    test('Create entry with value input JSON', async () => {
+      const inputValue = 12.31;
+      const res = await rq.post('/content-manager/explorer/withfloat', {
+        body: {
+          field: inputValue,
+        },
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toMatchObject({
+        field: inputValue,
+      });
+    });
+
+    test('Create entry with value input Formdata', async () => {
+      const inputValue = 23.1;
+      const res = await rq.post('/content-manager/explorer/withfloat', {
+        formData: {
+          data: JSON.stringify({ field: inputValue }),
+        },
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toMatchObject({
+        field: inputValue,
+      });
+    });
+
+    test('Create entry with integer should convert to float', async () => {
+      const inputValue = 1821;
+      const res = await rq.post('/content-manager/explorer/withfloat', {
+        body: {
+          field: inputValue,
+        },
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toMatchObject({
+        field: 1821.0,
+      });
+    });
+
+    test('Reading entry, returns correct value', async () => {
+      const res = await rq.get('/content-manager/explorer/withfloat');
+
+      expect(res.statusCode).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      res.body.forEach(entry => {
+        expect(entry.field).toEqual(expect.any(Number));
+      });
+    });
+
+    test('Updating entry sets the right value and format', async () => {
+      const res = await rq.post('/content-manager/explorer/withfloat', {
+        body: {
+          field: 11.2,
+        },
+      });
+
+      const updateRes = await rq.put(
+        `/content-manager/explorer/withfloat/${res.body.id}`,
+        {
+          body: {
+            field: 14,
+          },
+        }
+      );
+
+      expect(updateRes.statusCode).toBe(200);
+      expect(updateRes.body).toMatchObject({
+        id: res.body.id,
+        field: 14.0,
+      });
+    });
   });
 
   describe('Test type json', () => {
