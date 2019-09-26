19aac3a576d27e8d9e311ad083879ba8bc582243
jasongitmail <50032291+jasongitmail@users.noreply.github.com>
2019-09-26 17:58:51 +0000

    Update LeftMenuHeader/index.js

--- packages/strapi-admin/admin/src/components/LeftMenuHeader/index.js
+++ packages/strapi-admin/admin/src/components/LeftMenuHeader/index.js
@@ -6,14 +6,19 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../../assets/images/logo-admin.png';
+import logo2x from '../../assets/images/logo-admin@2x.png';
 import styles from './styles.scss';
 
 function LeftMenuHeader() {
   return (
     <div className={styles.leftMenuHeader}>
       <Link to="/" className={styles.leftMenuHeaderLink}>
-        <span className={styles.projectName} />
+        <img
+          className={styles.leftMenuHeaderLogo}
+          src={logo}
+          srcSet={`${logo} 1x, ${logo2x} 2x`}
+          alt="logo" />
       </Link>
     </div>
   );
