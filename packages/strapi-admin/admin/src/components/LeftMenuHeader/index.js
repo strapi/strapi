4a1831a3c995f4ae383d773c85cff6382672baf8
jasongitmail <50032291+jasongitmail@users.noreply.github.com>
2019-10-06 00:18:09 +0000

    Update LeftMenuHeader/index.js to use SVG or PNGs

--- packages/strapi-admin/admin/src/components/LeftMenuHeader/index.js
+++ packages/strapi-admin/admin/src/components/LeftMenuHeader/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoSVG from '../../assets/images/logo-admin.svg';
 import logo from '../../assets/images/logo-admin.png';
 import logo2x from '../../assets/images/logo-admin@2x.png';
 import styles from './styles.scss';
@@ -14,11 +15,18 @@ function LeftMenuHeader() {
   return (
     <div className={styles.leftMenuHeader}>
       <Link to="/" className={styles.leftMenuHeaderLink}>
-        <img
-          className={styles.leftMenuHeaderLogo}
-          src={logo}
-          srcSet={`${logo} 1x, ${logo2x} 2x`}
-          alt="logo" />
+        { logoSVG ?
+          <img
+            className = {styles.leftMenuHeaderLogo + ' ' + styles.SVG}
+            src={logoSVG}
+            alt="logo" />
+        :
+          <img
+            className={styles.leftMenuHeaderLogo}
+            src={logo}
+            srcSet={`${logo} 1x, ${logo2x} 2x`}
+            alt="logo" />
+        }
       </Link>
     </div>
   );
