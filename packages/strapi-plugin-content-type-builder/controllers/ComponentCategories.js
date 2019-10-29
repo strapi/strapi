f0dc5cf5456a23526171e268b3d63db922463b3f
Alexandre Bodin <bodin.alex@gmail.com>
2019-10-29 14:43:47 +0100

    Add component category edit to allow renaming

---
+++ packages/strapi-plugin-content-type-builder/controllers/ComponentCategories.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const validateComponentCategory = require('./validation/componentCategory');
+
+module.exports = {
+  async editCategory(ctx) {
+    const { body } = ctx.request;
+    const service =
+      strapi.plugins['content-type-builder'].services.componentcategories;
+
+    try {
+      await validateComponentCategory(body);
+    } catch (error) {
+      return ctx.send({ error }, 400);
+    }
+
+    const { name } = ctx.params;
+
+    strapi.reload.isWatching = false;
+
+    await service.editCategory(name, body);
+
+    strapi.reload();
+
+    ctx.send({ name: body.name });
+  },
+};
