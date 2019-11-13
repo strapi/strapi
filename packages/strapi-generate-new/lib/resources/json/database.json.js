5f289162100139a22ed19fcd0fcd2d06b7f2eb3d
DMehaffy <derrickmehaffy@gmail.com>
2019-11-13 07:21:08 -0700

    Clean up template and remove keys that would break bookshelf/knex (#4444)
    * Clean up template and remove keys that would break bookshelf/knex
    
    * PR feedback remove debug

--- packages/strapi-generate-new/lib/resources/json/database.json.js
+++ packages/strapi-generate-new/lib/resources/json/database.json.js
@@ -3,24 +3,16 @@
 module.exports = ({ connection, env }) => {
   // Production/Staging Template
   if (['production', 'staging'].includes(env)) {
-    // All available settings (bookshelf and mongoose)
     const settingsBase = {
       client: connection.settings.client,
       host: "${process.env.DATABASE_HOST || '127.0.0.1'}",
       port: '${process.env.DATABASE_PORT || 27017}',
-      srv: '${process.env.DATABASE_SRV || false}',
       database: "${process.env.DATABASE_NAME || 'strapi'}",
       username: "${process.env.DATABASE_USERNAME || ''}",
       password: "${process.env.DATABASE_PASSWORD || ''}",
-      ssl: '${process.env.DATABASE_SSL || false}',
     };
 
-    // All available options (bookshelf and mongoose)
-    const optionsBase = {
-      ssl: '${process.env.DATABASE_SSL || false}',
-      authenticationDatabase:
-        "${process.env.DATABASE_AUTHENTICATION_DATABASE || ''}",
-    };
+    const optionsBase = {};
 
     return {
       defaultConnection: 'default',
