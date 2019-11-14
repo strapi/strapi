e5e0b5cdd85fdaedcc4bdf173cf26d90068fc9e1
Virginie Ky <virginie.ky@gmail.com>
2019-11-14 15:19:19 +0100

    Wysiwyg and input file errors

--- packages/strapi-plugin-content-manager/admin/src/components/WysiwygWithErrors/Wrapper.js
+++ packages/strapi-plugin-content-manager/admin/src/components/WysiwygWithErrors/Wrapper.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  margin-bottom: 1.6rem;
+  padding-bottom: 2.9rem;
   font-size: 1.3rem;
-  font-family: 'Lato';
+  font-family: 'Lato'; 
+  &.bordered {
+    .editorWrapper {
+      border-color: red;
+    }
+  }
+  > div + p {
+    width 100%;
+    padding-top: 13px;
+    font-size: 1.2rem;
+    line-height: normal;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-bottom: -10px;
+  }
 `;
 
 export default Wrapper;
