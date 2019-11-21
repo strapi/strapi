91401cc0b96deacd593733533eb611121249fec0
soupette <cyril.lpz@gmail.com>
2019-11-21 12:43:37 +0100

    Created relation icons

--- packages/strapi-plugin-content-type-builder/admin/src/components/RelationFormNaturePicker/index.js
+++ packages/strapi-plugin-content-type-builder/admin/src/components/RelationFormNaturePicker/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import Wrapper from './Wrapper';
+import ManyToMany from '../../icons/ManyToMany';
+import ManyToOne from '../../icons/ManyToOne';
+import ManyWay from '../../icons/ManyWay';
+import OneToMany from '../../icons/OneToMany';
+import OneToOne from '../../icons/OneToOne';
+import OneWay from '../../icons/OneWay';
 import getTrad from '../../utils/getTrad';
+import Wrapper from './Wrapper';
 
 const RelationFormNaturePicker = ({ nature }) => {
   return (
     <Wrapper>
       <div className="nature-container">
-        <div className="nature-buttons"></div>
+        <div className="nature-buttons">
+          <ManyToOne />
+          <ManyToOne isSelected />
+          <ManyWay />
+          <ManyWay isSelected />
+          <ManyToMany />
+          <ManyToMany isSelected />
+          <OneToMany />
+          <OneToMany isSelected />
+          <OneToOne />
+          <OneToOne isSelected />
+          <OneWay />
+          <OneWay isSelected />
+        </div>
         <div className="nature-txt">
           <span>left</span>
           &nbsp; <FormattedMessage id={getTrad(`relation.${nature}`)} />
