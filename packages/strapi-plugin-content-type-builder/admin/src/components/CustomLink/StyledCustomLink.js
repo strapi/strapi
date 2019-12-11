9d72d7373e64604547ef307a7d8486f27122278c
soupette <cyril.lpz@gmail.com>
2019-12-11 11:13:53 +0100

    Fix left menu link on safari

--- packages/strapi-plugin-content-type-builder/admin/src/components/CustomLink/StyledCustomLink.js
+++ packages/strapi-plugin-content-type-builder/admin/src/components/CustomLink/StyledCustomLink.js
@@ -6,25 +6,12 @@
 
 import styled from 'styled-components';
 
-import { colors } from 'strapi-helper-plugin';
-
 const StyledCustomLink = styled.div`
   padding-left: 15px;
   padding-top: 9px;
   line-height: 0;
-  p {
-    color: ${colors.blue};
-    font-size: 13px;
-    font-weight: 500;
-    line-height: 18px;
-    text-align: left;
-    svg {
-      margin-left: -3px;
-      margin-right: 7px;
-      vertical-align: initial;
-      -webkit-font-smoothing: subpixel-antialiased;
-    }
-  }
+  margin-left: -3px;
+
   button {
     cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
     padding: 0;
--- packages/strapi-plugin-content-type-builder/admin/src/components/CustomLink/index.js
+++ packages/strapi-plugin-content-type-builder/admin/src/components/CustomLink/index.js
@@ -2,16 +2,16 @@ import React, { memo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Plus } from '@buffetjs/icons';
-
+import P from './P';
 import StyledCustomLink from './StyledCustomLink';
 
 const CustomLink = ({ disabled, id, onClick }) => (
   <StyledCustomLink disabled={disabled}>
     <button onClick={onClick} role="button" disabled={disabled}>
-      <p>
+      <P>
         <Plus fill="#007EFF" width="11px" height="11px" />
         {id && <FormattedMessage id={id} />}
-      </p>
+      </P>
     </button>
   </StyledCustomLink>
 );
