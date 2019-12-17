7381acad58a743d6a92e50b495fec7337aceb7a7
Alexandre Bodin <bodin.alex@gmail.com>
2019-12-17 13:17:38 +0100

    Allow private field and collectionName with -

--- packages/strapi-plugin-content-type-builder/controllers/validation/common.js
+++ packages/strapi-plugin-content-type-builder/controllers/validation/common.js
@@ -16,6 +16,7 @@ const validators = {
 };
 
 const NAME_REGEX = new RegExp('^[A-Za-z][_0-9A-Za-z]*$');
+const COLLECTION_NAME_REGEX = new RegExp('^[A-Za-z][-_0-9A-Za-z]*$');
 const ENUM_REGEX = new RegExp('^[_A-Za-z][_0-9A-Za-z]*$');
 const ICON_REGEX = new RegExp('^[A-Za-z0-9][-A-Za-z0-9]*$');
 
@@ -23,6 +24,12 @@ const isValidName = {
   name: 'isValidName',
   message: '${path} must match the following regex: /^[A-Za-z][_0-9A-Za-z]*$/',
   test: val => val === '' || NAME_REGEX.test(val),
+};
+
+const isValidCollectionName = {
+  name: 'isValidCollectionName',
+  message: '${path} must match the following regex: /^[A-Za-z][-_0-9A-Za-z]*$/',
+  test: val => val === '' || COLLECTION_NAME_REGEX.test(val),
 };
 
 const isValidIcon = {
@@ -47,6 +54,7 @@ const isValidEnum = {
 module.exports = {
   validators,
 
+  isValidCollectionName,
   isValidName,
   isValidIcon,
   isValidKey,
