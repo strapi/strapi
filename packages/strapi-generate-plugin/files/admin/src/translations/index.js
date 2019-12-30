760a3d5dd8a0659c77c75972c0254df3261e9c1d
jozefcipa <jozef.cipa@strv.com>
2019-12-30 16:22:25 +0100

    Add slovak translations

--- packages/strapi-generate-plugin/files/admin/src/translations/index.js
+++ packages/strapi-generate-plugin/files/admin/src/translations/index.js
@@ -14,6 +14,7 @@ import tr from './tr.json';
 import vi from './vi.json';
 import zhHans from './zh-Hans.json';
 import zh from './zh.json';
+import sk from './sk.json';
 
 const trads = {
   ar,
@@ -32,6 +33,7 @@ const trads = {
   vi,
   'zh-Hans': zhHans,
   zh,
+  sk,
 };
 
 export default trads;
