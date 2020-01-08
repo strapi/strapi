2607b38bccc72f0b7f6c96dce48773b6702afe1a
Alexandre Bodin <bodin.alex@gmail.com>
2020-01-08 11:33:59 +0100

    Add url validation

--- packages/strapi-admin/controllers/Webhooks.js
+++ packages/strapi-admin/controllers/Webhooks.js
@@ -11,10 +11,15 @@ const ALLOWED_EVENTS = [
   'media.delete',
 ];
 
+const urlRegex = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
+
 const webhookValidator = yup
   .object({
     name: yup.string().required(),
-    url: yup.string().required(),
+    url: yup
+      .string()
+      .matches(urlRegex, 'url must be a valid URL')
+      .required(),
     headers: yup.lazy(data => {
       if (typeof data !== 'object') {
         return yup.object().required();
