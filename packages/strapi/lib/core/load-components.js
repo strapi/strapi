70fe8c93d4904131510b185f86171379ce15361a
Alexandre Bodin <bodin.alex@gmail.com>
2020-01-16 12:10:58 +0100

    Init CTB tests for content types
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi/lib/core/load-components.js
+++ packages/strapi/lib/core/load-components.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const _ = require('lodash');
 const { join } = require('path');
 const { exists } = require('fs-extra');
+const { LoadingError } = require('./errors');
 const loadFiles = require('../load/load-files');
 
 module.exports = async ({ dir }) => {
@@ -15,9 +17,30 @@ module.exports = async ({ dir }) => {
 
   return Object.keys(map).reduce((acc, category) => {
     Object.keys(map[category]).forEach(key => {
-      acc[`${category}.${key}`] = Object.assign(map[category][key], {
+      const schema = map[category][key];
+
+      const filePath = join(componentsDir, category, schema.__filename__);
+
+      if (!schema.connection) {
+        throw new LoadingError(
+          `Component ${key} is missing a "connection" property. (Check file ${filePath})`
+        );
+      }
+
+      if (!schema.collectionName) {
+        throw new LoadingError(
+          `Component ${key} is missing a "collectionName" property. (Check file ${filePath})`
+        );
+      }
+
+      acc[`${category}.${key}`] = Object.assign(schema, {
+        __schema__: _.cloneDeep(schema),
+        uid: key,
         category,
+        modelType: 'component',
         modelName: key,
+        globalId:
+          schema.globalId || _.upperFirst(_.camelCase(`component_${key}`)),
       });
     });
     return acc;
