8147a188edb9820a0ea3ab7f4cc07a34f1a11212
soupette <cyril.lpz@gmail.com>
2020-01-20 15:15:35 +0100

    Fix icons when error state, improve drag experience
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-content-manager/admin/src/components/FieldComponent/index.js
+++ packages/strapi-plugin-content-manager/admin/src/components/FieldComponent/index.js
@@ -20,6 +20,7 @@ const FieldComponent = ({
   icon,
   isFromDynamicZone,
   isRepeatable,
+  isSub,
   label,
   max,
   min,
@@ -87,6 +88,7 @@ const FieldComponent = ({
           componentUid={componentUid}
           fields={displayedFields}
           isFromDynamicZone={isFromDynamicZone}
+          isSub={isSub}
           max={max}
           min={min}
           name={name}
@@ -102,6 +104,7 @@ FieldComponent.defaultProps = {
   icon: 'smile',
   isFromDynamicZone: false,
   isRepeatable: false,
+  isSub: false,
   max: Infinity,
   min: -Infinity,
 };
@@ -112,6 +115,7 @@ FieldComponent.propTypes = {
   icon: PropTypes.string,
   isFromDynamicZone: PropTypes.bool,
   isRepeatable: PropTypes.bool,
+  isSub: PropTypes.bool,
   label: PropTypes.string.isRequired,
   max: PropTypes.number,
   min: PropTypes.number,
