3747f3e516da6707208d18b370e6fe8a4650df89
Virginie Ky <virginie.ky@gmail.com>
2020-01-20 16:43:37 +0100

    media update disabled comment
    Signed-off-by: Virginie Ky <virginie.ky@gmail.com>

--- packages/strapi-admin/admin/src/components/EventInput/index.js
+++ packages/strapi-admin/admin/src/components/EventInput/index.js
@@ -19,6 +19,7 @@ const EventInput = ({ onChange, name: inputName, value: inputValue }) => {
     media: ['media.create', 'media.update', 'media.delete'],
   };
 
+  // Media update disabled for now - until the media libray is ready
   const disabledEvents = ['media.update'];
 
   const formatValue = inputValue.reduce((acc, curr) => {
