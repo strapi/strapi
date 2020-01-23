96e092b300c8b34ceee4f52acbcbbb6dc879521c
soupette <cyril.lpz@gmail.com>
2020-01-23 16:46:50 +0100

    Change the way we load plugins
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-upload/admin/src/index.js
+++ packages/strapi-plugin-upload/admin/src/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
 import pluginPkg from '../../package.json';
 import App from './containers/App';
 import Initializer from './containers/Initializer';
-
 import lifecycles from './lifecycles';
 import trads from './translations';
 import pluginId from './pluginId';
 
-const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+export default strapi => {
+  const pluginDescription =
+    pluginPkg.strapi.description || pluginPkg.description;
+  const plugin = {
+    blockerComponent: null,
+    blockerComponentProps: {},
+    description: pluginDescription,
+    icon: pluginPkg.strapi.icon,
+    id: pluginId,
+    initializer: Initializer,
+    injectedComponents: [],
+    layout: null,
+    lifecycles,
+    leftMenuLinks: [],
+    leftMenuSections: [],
+    mainComponent: App,
+    name: pluginPkg.strapi.name,
+    preventComponentRendering: false,
+    trads,
+  };
 
-function Comp(props) {
-  return <App {...props} />;
-}
-
-const plugin = {
-  blockerComponent: null,
-  blockerComponentProps: {},
-  description: pluginDescription,
-  icon: pluginPkg.strapi.icon,
-  id: pluginId,
-  initializer: Initializer,
-  injectedComponents: [],
-  layout: null,
-  lifecycles,
-  leftMenuLinks: [],
-  leftMenuSections: [],
-  mainComponent: Comp,
-  name: pluginPkg.strapi.name,
-  preventComponentRendering: false,
-  trads,
+  return strapi.registerPlugin(plugin);
 };
-
-export default plugin;
