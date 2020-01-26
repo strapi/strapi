ab9393bb4bbdfea9feb338b3c1afcce68f9a6471
Mariusz Wachowski <wachowski.mariusz@gmail.com>
2020-01-26 09:44:19 +0100

    fix: content-type-builder forms gets proper default values
    Signed-off-by: Mariusz Wachowski <wachowski.mariusz@gmail.com>

--- packages/strapi-plugin-content-type-builder/admin/src/containers/FormModal/index.js
+++ packages/strapi-plugin-content-type-builder/admin/src/containers/FormModal/index.js
@@ -1244,7 +1244,7 @@ const FormModal = () => {
                           } else if (input.name === 'uid') {
                             value = input.value;
                           } else {
-                            value = retrievedValue;
+                            value = retrievedValue || input.value || '';
                           }
 
                           // The addon input is not present in @buffetjs so we are using the old lib
