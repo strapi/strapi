f6d8f51df3a438b96850fe94ce01ae5a2b43e907
soupette <cyril.lpz@gmail.com>
2020-01-30 17:38:25 +0100

    Fix link default message
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-helper-plugin/lib/src/hooks/useFormattedMessage.js
+++ packages/strapi-helper-plugin/lib/src/hooks/useFormattedMessage.js
@@ -5,7 +5,10 @@ const useFormattedMessage = message => {
   const { formatMessage } = useGlobalContext();
 
   if (isObject(message) && message.id) {
-    return formatMessage({ ...message, defaultMessage: message.id });
+    return formatMessage({
+      ...message,
+      defaultMessage: message.defaultMessage || message.id,
+    });
   }
 
   return message;
