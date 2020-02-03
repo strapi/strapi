c86e1dfb1842c8b5188385516f74f842747fb474
soupette <cyril.lpz@gmail.com>
2020-02-03 16:03:36 +0100

    Update front-end jest config
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
-    'babel-plugin-styled-components',
+    '@babel/plugin-transform-modules-commonjs',
   ].map(require.resolve),
 };
