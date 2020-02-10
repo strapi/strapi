30fba6d3fe58c0c533a42fca083705f4cfbf4d62
soupette <cyril.lpz@gmail.com>
2020-02-10 15:27:49 +0100

    Remove saga from Main container
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/config.js
+++ packages/strapi-admin/admin/src/config.js
@@ -1,4 +1,5 @@
 // DO NOT MODIFY THESE OPTIONS
 export const LOGIN_LOGO = null;
-export const SHOW_TUTORIALS = true;
+// TODO: uncomment
+export const SHOW_TUTORIALS = false;
 export const SETTINGS_BASE_URL = '/settings';
