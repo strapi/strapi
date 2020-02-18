320446412d6ce7890e73e283d35dc0360230c7c4
soupette <cyril.lpz@gmail.com>
2020-02-18 16:49:18 +0100

    Add navigation to upload form
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-upload/admin/src/components/UploadForm/index.js
+++ packages/strapi-plugin-upload/admin/src/components/UploadForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 // import PropTypes from 'prop-types';
 import {
   HeaderModalTitle,
@@ -9,16 +9,40 @@ import {
 import ModalNav from '../ModalNav';
 import NavLink from '../NavLink';
 import InputFile from '../InputFile';
+import init from './init';
+import reducer, { initialState } from './reducer';
 
 const UploadForm = () => {
+  const [reducerState, dispatch] = useReducer(reducer, initialState, init);
+  const { to } = reducerState.toJS();
+  const links = ['computer', 'url'];
+
+  const handleGoTo = to => {
+    dispatch({
+      type: 'SET_TAB',
+      to,
+    });
+  };
+
   return (
     <>
       <HeaderNavWrapper>
         <HeaderModalTitle>
           <div className="settings-tabs" style={{ left: 30 }}>
             <ModalNav>
-              <NavLink isActive to="computer" />
-              <NavLink to="url" isDisabled />
+              {links.map(link => {
+                const isActive = link === to;
+
+                return (
+                  <NavLink
+                    key={link}
+                    to={link}
+                    isActive={isActive}
+                    isDisabled={link === 'url'}
+                    onClick={handleGoTo}
+                  />
+                );
+              })}
             </ModalNav>
           </div>
           <hr />
@@ -29,7 +53,8 @@ const UploadForm = () => {
           <div className="container-fluid">
             <div className="row">
               <div className="col-12">
-                <InputFile />
+                {to === 'computer' && <InputFile />}
+                {to === 'url' && <div>COMING SOON</div>}
               </div>
             </div>
           </div>
