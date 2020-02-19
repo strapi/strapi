825ffa9f237a5aeb592f02f26d6ad2688d94186f
soupette <cyril.lpz@gmail.com>
2020-02-19 10:27:43 +0100

    Add trads
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-upload/admin/src/components/InfiniteLoadingIndicator/index.js
+++ packages/strapi-plugin-upload/admin/src/components/InfiniteLoadingIndicator/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ClearIcon } from 'strapi-helper-plugin';
+import { ClearIcon, useGlobalContext } from 'strapi-helper-plugin';
 import Button from './Button';
 import Indicator from './Indicator';
 import Wrapper from './Wrapper';
 
 const InfiniteLoadingIndicator = ({ onClick }) => {
+  const { formatMessage } = useGlobalContext();
+
   return (
     <Wrapper>
       <Indicator />
       <Button type="button" onClick={onClick}>
-        Cancel
+        {formatMessage({ id: 'app.components.Button.cancel' })}
         <ClearIcon />
       </Button>
     </Wrapper>
