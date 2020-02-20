f128edc73a6acb7d6d0ab8861e885f4e009d087b
soupette <cyril.lpz@gmail.com>
2020-02-20 07:06:58 +0100

    Add font-sizes and font-weight to the theme
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/themes/sizes.js
+++ packages/strapi-admin/admin/src/themes/sizes.js
@@ -6,6 +6,22 @@ const sizes = {
     height: '6rem',
     width: '24rem',
   },
+  margins: {
+    // TODO:
+    normal: '10px',
+  },
+  fontSizes: {
+    xs: '11px',
+    sm: '12px',
+    md: '13px',
+    lg: '18px',
+  },
+  fontWeight: {
+    regular: 400,
+    semiBold: 500,
+    bold: 600,
+    black: 900,
+  },
 };
 
 export default sizes;
