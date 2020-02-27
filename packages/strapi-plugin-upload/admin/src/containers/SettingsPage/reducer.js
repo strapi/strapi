6e3ca74aee1b1c6c1eaf6f2d1a76a1c267b1d112
soupette <cyril.lpz@gmail.com>
2020-02-27 09:53:50 +0100

    Created settings page
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/strapi-plugin-upload/admin/src/containers/SettingsPage/reducer.js
@@ -0,0 +1,40 @@
+import { fromJS } from 'immutable';
+
+const initialState = fromJS({
+  // TODO: switch to true when API is ready
+  isLoading: false,
+  initialData: {
+    responsiveDimensions: true,
+    sizeOptimization: true,
+    videoPreview: false,
+  },
+  modifiedData: {
+    responsiveDimensions: true,
+    sizeOptimization: true,
+    videoPreview: false,
+  },
+});
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CANCEL_CHANGES':
+      return state.update('modifiedData', () => state.get('initialData'));
+    case 'GET_DATA_SUCCEEDED':
+      return state
+        .update('isLoading', () => false)
+        .update('initialData', () => fromJS(action.data))
+        .update('modifiedData', () => fromJS(action.data));
+    case 'ON_CHANGE':
+      return state.updateIn(
+        ['modifiedData', ...action.keys.split('.')],
+        () => action.value
+      );
+    case 'SUBMIT_SUCCEEDED':
+      return state.update('initialData', () => state.get('modifiedData'));
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+export { initialState };
--- packages/strapi-plugin-upload/admin/src/index.js
+++ packages/strapi-plugin-upload/admin/src/index.js
@@ -1,6 +1,7 @@
 import pluginPkg from '../../package.json';
 import pluginLogo from './assets/images/logo.svg';
 import App from './containers/App';
+import SettingsPage from './containers/SettingsPage';
 
 import trads from './translations';
 import pluginId from './pluginId';
@@ -30,13 +31,13 @@ export default strapi => {
       global: [
         {
           title: {
-            id: getTrad('settings.link.label'),
+            id: getTrad('plugin.name'),
             defaultMessage: 'Media Library',
           },
           name: 'media-library',
           to: `${strapi.settingsBaseURL}/media-library`,
           // TODO
-          Component: () => 'COMING SOON',
+          Component: SettingsPage,
         },
       ],
     },
