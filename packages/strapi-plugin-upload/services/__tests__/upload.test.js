70c336cfe9305ed7f34c9b77c568ae4ae2b5fed1
Alexandre Bodin <bodin.alex@gmail.com>
2020-03-03 16:41:37 +0100

    Add tests and update old tests
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-plugin-upload/services/__tests__/upload.test.js
+++ packages/strapi-plugin-upload/services/__tests__/upload.test.js
@@ -17,5 +17,48 @@ describe('Upload service', () => {
         size: 1000,
       });
     });
+
+    test('Overrides name with fileInfo', () => {
+      const fileData = {
+        filename: 'File Name.png',
+        type: 'image/png',
+        size: 1000 * 1000,
+      };
+
+      const fileInfo = {
+        name: 'Custom File Name',
+      };
+
+      expect(uploadService.formatFileInfo(fileData, fileInfo)).toMatchObject({
+        name: fileInfo.name,
+        hash: expect.stringContaining('Custom_File_Name'),
+        ext: '.png',
+        mime: 'image/png',
+        size: 1000,
+      });
+    });
+
+    test('Sets alternativeText and caption', () => {
+      const fileData = {
+        filename: 'File Name.png',
+        type: 'image/png',
+        size: 1000 * 1000,
+      };
+
+      const fileInfo = {
+        alternativeText: 'some text',
+        caption: 'caption this',
+      };
+
+      expect(uploadService.formatFileInfo(fileData, fileInfo)).toMatchObject({
+        name: 'File Name',
+        caption: fileInfo.caption,
+        alternativeText: fileInfo.alternativeText,
+        hash: expect.stringContaining('File_Name'),
+        ext: '.png',
+        mime: 'image/png',
+        size: 1000,
+      });
+    });
   });
 });
--- packages/strapi-plugin-upload/test/graphqlUpload.test.e2e.js
+++ packages/strapi-plugin-upload/test/graphqlUpload.test.e2e.js
@@ -75,7 +75,7 @@ describe('Upload plugin end to end tests', () => {
       data: {
         upload: {
           id: expect.anything(),
-          name: 'rec.jpg',
+          name: 'rec',
         },
       },
     });
@@ -122,7 +122,7 @@ describe('Upload plugin end to end tests', () => {
         multipleUpload: expect.arrayContaining([
           expect.objectContaining({
             id: expect.anything(),
-            name: 'rec.jpg',
+            name: 'rec',
           }),
         ]),
       },
