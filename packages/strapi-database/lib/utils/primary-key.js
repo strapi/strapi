15f4ae67d756b43f2502987e16af10c6da448522
Pierre Noël <pierre.noel@strapi.io>
2020-03-10 16:01:10 +0100

    add test for replaceIdByPrimaryKey
    Signed-off-by: Pierre Noël <pierre.noel@strapi.io>

--- packages/strapi-database/lib/utils/primary-key.js
+++ packages/strapi-database/lib/utils/primary-key.js
@@ -2,13 +2,18 @@
 
 const _ = require('lodash');
 
+/**
+ * If exists, rename the key "id" by the primary key name of the model ("_id" by default for mongoose).
+ */
+const replaceIdByPrimaryKey = (params, model) => {
+  const newParams = { ...params };
+  if (_.has(params, 'id')) {
+    delete newParams.id;
+    newParams[model.primaryKey] = params[model.primaryKey] || params.id;
+  }
+  return newParams;
+};
+
 module.exports = {
-  replaceIdByPrimaryKey: (params, model) => {
-    const newParams = { ...params };
-    if (_.has(params, 'id')) {
-      delete newParams.id;
-      newParams[model.primaryKey] = params[model.primaryKey] || params.id;
-    }
-    return newParams;
-  },
+  replaceIdByPrimaryKey,
 };
