541cd72e972208d845afd00b8ae10eeeb29690c4
Virginie Ky <virginie.ky@gmail.com>
2020-03-20 14:40:56 +0100

    Admin leftmenu bullet icon
    Signed-off-by: Virginie Ky <virginie.ky@gmail.com>

--- packages/strapi-helper-plugin/lib/src/components/LeftMenuLink/Icon.js
+++ packages/strapi-helper-plugin/lib/src/components/LeftMenuLink/Icon.js
@@ -6,9 +6,9 @@ import colors from '../../assets/styles/colors';
 
 const LeftMenuIcon = styled(({ ...props }) => <FontAwesomeIcon {...props} icon="circle" />)`
   position: absolute;
-  top: calc(50% - 0.3rem);
-  left: 1.4rem;
-  font-size: 0.7rem;
+  top: calc(50% - 0.25rem);
+  left: 1.5rem;
+  font-size: 0.5rem;
   color: ${colors.leftMenu.darkGrey};
 `;
 
