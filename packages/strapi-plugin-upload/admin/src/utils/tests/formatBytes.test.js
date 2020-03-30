ec5e96075850748b5bf92a0ac8df552ac52fdbbc
soupette <cyril.lpz@gmail.com>
2020-03-30 09:49:12 +0200

    Fix tests labels
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-upload/admin/src/utils/tests/formatBytes.test.js
+++ packages/strapi-plugin-upload/admin/src/utils/tests/formatBytes.test.js
@@ -6,27 +6,23 @@ describe('UPLOAD | components | EditForm | utils', () => {
       expect(formatBytes(0)).toEqual('0B');
     });
 
-    it('should return 0B if less than 1 bytes is passed', () => {
+    it('should return 900B if 0.9 bytes is passed', () => {
       expect(formatBytes(0.9)).toEqual('900B');
-    });
-
-    it('should return 1KB if 1024 Bytes is passed', () => {
-      expect(formatBytes(1024)).toEqual('1000KB');
     });
 
     it("should return 1KB if '1024' Bytes is passed", () => {
       expect(formatBytes('1024')).toEqual('1000KB');
     });
 
-    it('should return 1.21KB if 1034 Bytes is passed', () => {
+    it('should return 1.18MB if 1034 Bytes is passed', () => {
       expect(formatBytes(1234)).toEqual('1.18MB');
     });
 
-    it('should return 1.21KB if 1034 Bytes is passed with 3 decimals', () => {
+    it('should return 1.177MB if 1234 Bytes is passed with 3 decimals', () => {
       expect(formatBytes(1234, 3)).toEqual('1.177MB');
     });
 
-    it('should return 1 MB if 1.1e+6 Bytes is passed', () => {
+    it('should return 1 GB if 1.1e+6 Bytes is passed', () => {
       expect(formatBytes(1100000, 0)).toEqual('1GB');
     });
   });
