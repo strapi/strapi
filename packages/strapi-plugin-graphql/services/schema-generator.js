9e3402987869afb3a4a6b607bc3e190b32706e83
Alexandre Bodin <bodin.alex@gmail.com>
2020-04-06 18:51:58 +0200

    Overwritte plugin config with user config
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-plugin-graphql/services/schema-generator.js
+++ packages/strapi-plugin-graphql/services/schema-generator.js
@@ -80,7 +80,7 @@ const generateSchema = () => {
     `;
 
   // // Build schema.
-  if (!strapi.config.environment === 'production') {
+  if (strapi.config.environment !== 'production') {
     // Write schema.
     const schema = makeExecutableSchema({
       typeDefs,
