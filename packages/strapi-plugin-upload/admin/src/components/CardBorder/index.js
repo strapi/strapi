1de70d9292bdcc0ce30d51fc05fa36fca7077397
Ky <virginie.ky@gmail.com>
2020-04-07 19:35:19 +0200

    theme borderRadius
    Signed-off-by: Ky <virginie.ky@gmail.com>

--- packages/strapi-plugin-upload/admin/src/components/CardBorder/index.js
+++ packages/strapi-plugin-upload/admin/src/components/CardBorder/index.js
@@ -11,7 +11,7 @@ const CardBorder = styled.div`
   display: none;
   ${({ shown }) => shown && 'display: block;'}
   border: 2px solid ${({ theme, color }) => theme.main.colors[color] || color};
-  border-radius: 2px;
+  border-radius: ${({ theme }) => theme.main.sizes.borderRadius};
 `;
 
 CardBorder.defaultProps = {
