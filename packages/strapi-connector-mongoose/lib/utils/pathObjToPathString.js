0692628bad62bf2f321a543da8576868bfae165d
Crhistian Raphael <crhistian.raphael@pixter.com.br>
2020-04-09 20:10:15 -0300

    fix: module.export
    Signed-off-by: Crhistian Raphael <crhistian.raphael@pixter.com.br>

--- packages/strapi-connector-mongoose/lib/utils/pathObjToPathString.js
+++ packages/strapi-connector-mongoose/lib/utils/pathObjToPathString.js
@@ -1,13 +1,17 @@
-export const pathObjToPathString = (pathObj, pathStr = '') => {
+const pathObjToPathString = (pathObj, pathStr = '') => {
 	if(!pathObj.path) return pathObj;
 	
 	pathStr += (pathStr === '')
-		? pathObj.path
-		: '.' + pathObj.path
+	? pathObj.path
+	: '.' + pathObj.path
 	
 	if(pathObj.populate) {
 		pathStr = pathObjToPathString(pathObj.populate, pathStr);
 	}
-
+	
 	return pathStr;
 };
+
+module.exports = {
+	pathObjToPathString
+}
