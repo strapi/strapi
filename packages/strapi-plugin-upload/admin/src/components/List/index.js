eae5dfbdf64ca7727945391151c696d29afe5e1e
Ky <virginie.ky@gmail.com>
2020-04-10 11:09:58 +0200

    autocomplete search
    Signed-off-by: Ky <virginie.ky@gmail.com>

--- packages/strapi-plugin-upload/admin/src/components/List/index.js
+++ packages/strapi-plugin-upload/admin/src/components/List/index.js
@@ -64,7 +64,7 @@ const List = ({
                       />
                     </CardControlsWrapper>
                     {renderCardControl && (
-                      <CardControlsWrapper className="card-control-wrapper">
+                      <CardControlsWrapper className="card-control-wrapper card-control-wrapper-hidden">
                         {renderCardControl(id)}
                       </CardControlsWrapper>
                     )}
