beb6e74f801bc5c58b8b7c34cf13efbe60f65a61
Leon <31146334+ierehon1905@users.noreply.github.com>
2020-04-17 18:10:31 +0300

    typo fixes (#5795)
    Signed-off-by: ierehon1905 <ierehon1905@gmail.com>
    
    Co-authored-by: cyril lopez <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/HomePage/components.js
+++ packages/strapi-admin/admin/src/containers/HomePage/components.js
@@ -9,7 +9,7 @@ const Block = styled.div`
   padding: 19px 30px 30px 30px;
   box-shadow: 0 2px 4px 0 #e3e9f3;
   border-radius: 3px;
-  line-heigth: 18px;
+  line-height: 18px;
 
   a {
     position: relative;
@@ -213,12 +213,12 @@ const LinkWrapper = styled.a`
     &:first-child {
       font-size: 16px;
     }
-    color: #919BAE;
+    color: #919bae;
     text-overflow: ellipsis;
     overflow: hidden;
   }
   .bold {
-    color: #333740
+    color: #333740;
     font-weight: 600;
   }
 `;
@@ -257,13 +257,4 @@ const SocialLinkWrapper = styled.div`
   }
 `;
 
-export {
-  ALink,
-  Block,
-  Container,
-  LinkWrapper,
-  P,
-  Separator,
-  SocialLinkWrapper,
-  Wave,
-};
+export { ALink, Block, Container, LinkWrapper, P, Separator, SocialLinkWrapper, Wave };
