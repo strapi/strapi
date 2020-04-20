555dbda454bf4697dc63bd0bcf71da05912da5c2
Pierre Noël <pierre.noel@strapi.io>
2020-04-20 16:43:00 +0200

    fix count() result not a number
    Signed-off-by: Pierre Noël <pierre.noel@strapi.io>

--- packages/strapi-connector-bookshelf/lib/queries.js
+++ packages/strapi-connector-bookshelf/lib/queries.js
@@ -68,10 +68,13 @@ module.exports = function createQueryBuilder({ model, modelKey, strapi }) {
   /**
    * Count entries based on filters
    */
-  function count(params = {}) {
+  async function count(params = {}) {
     const { where } = convertRestQueryParams(params);
 
-    return model.query(buildQuery({ model, filters: { where } })).count();
+    return model
+      .query(buildQuery({ model, filters: { where } }))
+      .count()
+      .then(Number);
   }
 
   async function create(values, { transacting } = {}) {
@@ -187,7 +190,8 @@ module.exports = function createQueryBuilder({ model, modelKey, strapi }) {
       .query(qb => {
         buildSearchQuery(qb, model, params);
       })
-      .count();
+      .count()
+      .then(Number);
   }
 
   async function createComponents(entry, values, { transacting }) {
