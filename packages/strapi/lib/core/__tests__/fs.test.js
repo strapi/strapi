11f5ea3fc9cf0faf4f9701e3fc3138fc1c138794
domenico ventura <domenico@digitware.it>
2020-04-24 15:26:00 +0200

    Tests were failing on Windows platform as the test was not platform agnostic. Now using path.combine to make the tests pass on all platforms

--- packages/strapi/lib/core/__tests__/fs.test.js
+++ packages/strapi/lib/core/__tests__/fs.test.js
@@ -1,6 +1,8 @@
 const fs = require('../fs');
 const fse = require('fs-extra');
 
+const path = require('path');
+
 jest.mock('fs-extra');
 
 describe('Strapi fs utils', () => {
@@ -23,8 +25,8 @@ describe('Strapi fs utils', () => {
 
       await strapiFS.writeAppFile('test', content);
 
-      expect(fse.ensureFile).toHaveBeenCalledWith('/tmp/test');
-      expect(fse.writeFile).toHaveBeenCalledWith('/tmp/test', content);
+      expect(fse.ensureFile).toHaveBeenCalledWith(path.join('\\', 'tmp', 'test'));
+      expect(fse.writeFile).toHaveBeenCalledWith(path.join('\\', 'tmp', 'test'), content);
     });
 
     test('Normalize the path to avoid relative access to folders in parent directories', async () => {
@@ -34,8 +36,8 @@ describe('Strapi fs utils', () => {
 
       await strapiFS.writeAppFile('../../test', content);
 
-      expect(fse.ensureFile).toHaveBeenCalledWith('/tmp/test');
-      expect(fse.writeFile).toHaveBeenCalledWith('/tmp/test', content);
+      expect(fse.ensureFile).toHaveBeenCalledWith(path.join('\\', 'tmp', 'test'));
+      expect(fse.writeFile).toHaveBeenCalledWith(path.join('\\', 'tmp', 'test'), content);
     });
 
     test('Works with array path', async () => {
@@ -45,8 +47,11 @@ describe('Strapi fs utils', () => {
 
       await strapiFS.writeAppFile(['test', 'sub', 'path'], content);
 
-      expect(fse.ensureFile).toHaveBeenCalledWith('/tmp/test/sub/path');
-      expect(fse.writeFile).toHaveBeenCalledWith('/tmp/test/sub/path', content);
+      expect(fse.ensureFile).toHaveBeenCalledWith(path.join('\\', 'tmp', 'test', 'sub', 'path'));
+      expect(fse.writeFile).toHaveBeenCalledWith(
+        path.join('\\', 'tmp', 'test', 'sub', 'path'),
+        content
+      );
     });
   });
 
@@ -58,11 +63,7 @@ describe('Strapi fs utils', () => {
 
       strapiFS.writeAppFile = jest.fn(() => Promise.resolve());
 
-      await strapiFS.writePluginFile(
-        'users-permissions',
-        ['test', 'sub', 'path'],
-        content
-      );
+      await strapiFS.writePluginFile('users-permissions', ['test', 'sub', 'path'], content);
 
       expect(strapiFS.writeAppFile).toHaveBeenCalledWith(
         'extensions/users-permissions/test/sub/path',
