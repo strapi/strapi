e0e561e6d0e2c146ebb54e1ec350cfa6bb27d981
Pierre Noël <petersg83@gmail.com>
2020-04-30 12:11:12 +0200

    add test for escapeQuery()
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi-utils/lib/stringFormatting.js
+++ packages/strapi-utils/lib/stringFormatting.js
@@ -6,13 +6,13 @@ const nameToSlug = name => slugify(name, { separator: '-' });
 
 const nameToCollectionName = name => slugify(name, { separator: '_' });
 
-const escapeQuery = (query, charsToEscape, espaceChar = '\\') => {
+const escapeQuery = (query, charsToEscape, escapeChar = '\\') => {
   return query
     .split('')
     .reduce(
       (escapedQuery, char) =>
         charsToEscape.includes(char)
-          ? `${escapedQuery}${espaceChar}${char}`
+          ? `${escapedQuery}${escapeChar}${char}`
           : `${escapedQuery}${char}`,
       ''
     );
