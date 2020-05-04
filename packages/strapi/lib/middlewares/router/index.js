4d5af2c95d6042cd04a67641d75d5cdb67ef2398
Alexandre Bodin <bodin.alex@gmail.com>
2020-05-04 19:21:36 +0200

    Make password input optional while editing
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi/lib/middlewares/router/index.js
+++ packages/strapi/lib/middlewares/router/index.js
@@ -25,9 +25,7 @@ module.exports = strapi => {
         composeEndpoint(value, { router: strapi.router });
       });
 
-      strapi.router.prefix(
-        _.get(strapi.config, 'currentEnvironment.request.router.prefix', '')
-      );
+      strapi.router.prefix(_.get(strapi.config, 'currentEnvironment.request.router.prefix', ''));
 
       if (!_.isEmpty(_.get(strapi.admin, 'config.routes', false))) {
         // Create router for admin.
@@ -52,27 +50,18 @@ module.exports = strapi => {
           });
 
           // Exclude routes with prefix.
-          const excludedRoutes = _.omitBy(
-            plugin.config.routes,
-            o => !_.has(o.config, 'prefix')
-          );
-
-          _.forEach(
-            _.omit(plugin.config.routes, _.keys(excludedRoutes)),
-            value => {
+          (plugin.config.routes || [])
+            .filter(route => !_.has(route.config, 'prefix'))
+            .forEach(value => {
               composeEndpoint(value, { plugin: pluginName, router });
-            }
-          );
-
-          // /!\ Could override main router's routes.
-          if (!_.isEmpty(excludedRoutes)) {
-            _.forEach(excludedRoutes, value => {
-              composeEndpoint(value, {
-                plugin: pluginName,
-                router: strapi.router,
-              });
             });
-          }
+
+          // if you set a prefix key in the route it will ignore the plugin prefix
+          (plugin.config.routes || [])
+            .filter(route => _.has(route.config, 'prefix'))
+            .forEach(value => {
+              composeEndpoint(value, { plugin: pluginName, router: strapi.router });
+            });
 
           // Mount plugin router
           strapi.app.use(router.routes()).use(router.allowedMethods());
