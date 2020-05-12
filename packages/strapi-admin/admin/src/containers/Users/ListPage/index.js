8015e17b0b84ed50e61cf05d04f6a323f015a30c
soupette <cyril.lpz@gmail.com>
2020-05-12 17:37:25 +0200

    Created form layout
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/Users/ListPage/index.js
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/index.js
@@ -1,40 +1,17 @@
-import React from 'react';
-import { useGlobalContext } from 'strapi-helper-plugin';
-import { Header } from '@buffetjs/custom';
+import React, { useState } from 'react';
+import Header from './Header';
+import ModalForm from './ModalForm';
 
 const ListPage = () => {
+  const [isModalOpened, setIsModalOpened] = useState(false);
   // TODO when API ready
   const usersCount = 1;
-  const { formatMessage } = useGlobalContext();
-  const tradBaseId = 'Settings.permissions.users.listview.';
-  const headerDescriptionSuffix =
-    usersCount > 1 ? 'header.description.plural' : 'header.description.singular';
-  const headerProps = {
-    actions: [
-      {
-        label: formatMessage({ id: 'app.utils.delete' }),
-        color: 'delete',
-        type: 'button',
-        disabled: true,
-      },
-
-      {
-        label: formatMessage({ id: `${tradBaseId}button-create` }),
-        color: 'primary',
-        type: 'button',
-        icon: true,
-      },
-    ],
-    content: formatMessage(
-      { id: `${tradBaseId}${headerDescriptionSuffix}` },
-      { number: usersCount }
-    ),
-    title: { label: formatMessage({ id: `${tradBaseId}header.title` }) },
-  };
+  const handleToggle = () => setIsModalOpened(prev => !prev);
 
   return (
     <div>
-      <Header {...headerProps} />
+      <Header count={usersCount} onClickAddUser={handleToggle} />
+      <ModalForm isOpen={isModalOpened} onToggle={handleToggle} />
     </div>
   );
 };
