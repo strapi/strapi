4057c6622baa6b5eb1d97e0faeadbd42a7121126
soupette <cyril.lpz@gmail.com>
2020-05-13 12:08:33 +0200

    Add validations and fix focus
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/Users/ListPage/ModalForm.js
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/ModalForm.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal, ModalHeader } from 'strapi-helper-plugin';
-import ModalBody from '../../../components/Users/ModalCreateBody';
+import { Button } from '@buffetjs/core';
+import { Modal, ModalFooter, ModalHeader, useGlobalContext } from 'strapi-helper-plugin';
+import stepper from './stepper';
 
 const ModalForm = ({ isOpen, onClosed, onToggle }) => {
+  const [currentStep, setStep] = useState('create');
+  // Little trick to focus the first input
+  // Without this the focus is lost
+  const [showBody, setShowBody] = useState(false);
+  const { formatMessage } = useGlobalContext();
+  const ref = useRef(null);
+  const { buttonSubmitLabel, Component, isDisabled, next } = stepper[currentStep];
+
+  const goNext = () => {
+    if (next) {
+      setStep(next);
+    } else {
+      onToggle();
+    }
+  };
+
+  const handleClick = () => {
+    if (ref.current) {
+      ref.current.submit();
+    } else {
+      goNext();
+    }
+  };
+
+  const handleClosed = () => {
+    setStep('create');
+    onClosed();
+    setShowBody(false);
+  };
+
+  const handleSubmit = () => {
+    goNext();
+  };
+
+  const handleOpened = () => {
+    setShowBody(true);
+  };
+
   return (
-    <Modal isOpen={isOpen} onToggle={onToggle} onClosed={onClosed}>
+    <Modal isOpen={isOpen} onOpened={handleOpened} onToggle={onToggle} onClosed={handleClosed}>
       <ModalHeader headerBreadcrumbs={['Settings.permissions.users.add-new']} />
-      <ModalBody />
+      {showBody && (
+        <Component
+          isDisabled={isDisabled}
+          onSubmit={handleSubmit}
+          ref={currentStep === 'create' ? ref : null}
+        />
+      )}
+      <ModalFooter>
+        <section>
+          <Button type="button" color="cancel" onClick={onToggle}>
+            {formatMessage({ id: 'app.components.Button.cancel' })}
+          </Button>
+          <Button color="success" type="button" onClick={handleClick}>
+            {formatMessage({ id: buttonSubmitLabel })}
+          </Button>
+        </section>
+      </ModalFooter>
     </Modal>
   );
 };
