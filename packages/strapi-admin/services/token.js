5eb1cc313270f36636baa76ea8e5741814e89dd2
Convly <jean-sebastien.herbaux@epitech.eu>
2020-05-14 10:37:32 +0200

    Cleanup strapi-admin services & domain
    Signed-off-by: Convly <jean-sebastien.herbaux@epitech.eu>

--- packages/strapi-admin/services/token.js
+++ packages/strapi-admin/services/token.js
@@ -2,8 +2,14 @@
 
 const crypto = require('crypto');
 
+/**
+ * Generate a random token
+ * @returns {string}
+ */
+function generate() {
+  return crypto.randomBytes(64).toString('hex');
+}
+
 module.exports = {
-  generate() {
-    return crypto.randomBytes(64).toString('hex');
-  },
+  generate,
 };
