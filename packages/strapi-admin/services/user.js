5b7f9d5c6ed535b32c70ffa601eece56aea5d879
Convly <jean-sebastien.herbaux@epitech.eu>
2020-05-14 10:37:32 +0200

    Cleanup strapi-admin services & domain
    Signed-off-by: Convly <jean-sebastien.herbaux@epitech.eu>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/services/user.js
+++ packages/strapi-admin/services/user.js
@@ -1,20 +1,27 @@
 'use strict';
 
+const { createUser } = require('../domain/user');
+
+/**
+ * Create and save a user in database
+ * @param attributes A partial user object
+ * @returns {Promise<user>}
+ */
+async function create(attributes) {
+  const user = createUser(attributes);
+  return strapi.query('user', 'admin').create(user);
+}
+
+/**
+ * Check if a user with specific attributes exists in the database
+ * @param attributes A partial user object
+ * @returns {Promise<boolean>}
+ */
+async function exists(attributes) {
+  return (await strapi.query('user', 'admin').count(attributes)) > 0;
+}
+
 module.exports = {
-  withDefaults(attributes) {
-    return {
-      roles: [],
-      isActive: false,
-      ...attributes,
-    };
-  },
-
-  async create(attributes) {
-    const user = this.withDefaults(attributes);
-    return strapi.query('user', 'admin').create(user);
-  },
-
-  async exists(attributes) {
-    return !!(await strapi.query('user', 'admin').findOne(attributes));
-  },
+  create,
+  exists,
 };
