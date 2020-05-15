f2dd146bf488c27c839b40f91d7ae651a06730f3
Alexandre Bodin <bodin.alex@gmail.com>
2020-05-15 19:41:56 +0200

    Clear getstarted config
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- examples/getstarted/config/plugins.js
+++ examples/getstarted/config/plugins.js
@@ -3,14 +3,4 @@ module.exports = ({ env }) => ({
     amountLimit: 5,
     depthLimit: 10,
   },
-  email: {
-    provider: 'mailgun',
-    providerOptions: {
-      apiKey: env('MAILGUN_API_KEY'),
-      domain: env('MAILGUN_DOMAIN'),
-    },
-    settings: {
-      defaultFrom: 'strapi@strapi.io',
-    },
-  },
 });
