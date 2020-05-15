bf360ff8a9b2d97f46ee1296bac7f0cc84ff320f
soupette <cyril.lpz@gmail.com>
2020-05-15 11:25:28 +0200

    Improve HeaderSearch component
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/strapi-admin/admin/src/components/HeaderSearch/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { upperFirst } from 'lodash';
+import { useHistory } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import { useQuery } from 'strapi-helper-plugin';
+import StyledHeaderSearch from './HeaderSearch';
+
+const HeaderSearch = ({ label, queryParameter }) => {
+  const { formatMessage } = useIntl();
+  const query = useQuery();
+  const [value, setValue] = useState(query.get(queryParameter) || '');
+  const { push } = useHistory();
+  const displayedLabel = typeof label === 'object' ? formatMessage(label) : label;
+  const capitalizedLabel = upperFirst(displayedLabel);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (value) {
+        push({ search: `${queryParameter}=${value}` });
+      } else {
+        push({ search: '' });
+      }
+    }, 300);
+
+    return () => clearTimeout(handler);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
+  const handleChange = ({ target: { value } }) => {
+    setValue(value);
+  };
+
+  const handleClear = () => {
+    setValue('');
+  };
+
+  return (
+    <StyledHeaderSearch
+      label={capitalizedLabel}
+      name={queryParameter}
+      value={value}
+      onChange={handleChange}
+      onClear={handleClear}
+    />
+  );
+};
+
+HeaderSearch.defaultProps = {
+  queryParameter: '_q',
+};
+
+HeaderSearch.propTypes = {
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ]).isRequired,
+  queryParameter: PropTypes.string,
+};
+
+export default HeaderSearch;
--- packages/strapi-admin/admin/src/containers/Users/ListPage/index.js
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/index.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import useSettingsHeaderSearchContext from '../../../hooks/useSettingsHeaderSearchContext';
 import Header from './Header';
 import ModalForm from './ModalForm';
 
 const ListPage = () => {
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const { toggleHeaderSearch } = useSettingsHeaderSearchContext();
+
+  useEffect(() => {
+    toggleHeaderSearch({ id: 'Settings.permissions.menu.link.users.label' });
+
+    return () => {
+      toggleHeaderSearch();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   // TODO when API ready
   const usersCount = 1;
   const handleToggle = () => setIsModalOpened(prev => !prev);
