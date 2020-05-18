465d28b489db3cba81fd29e4f6e5b4102762d4c2
soupette <cyril.lpz@gmail.com>
2020-05-18 15:37:22 +0200

    Add filter picker
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/components/Users/FilterPicker/index.js
+++ packages/strapi-admin/admin/src/components/Users/FilterPicker/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
 import { Picker } from '@buffetjs/core';
 import Button from './Button';
+import Card from './Card';
 
-const FilterPicker = () => {
-  return <Picker renderButtonContent={Button} />;
+const FilterPicker = ({ onChange }) => {
+  return (
+    <Picker
+      renderButtonContent={Button}
+      renderSectionContent={onToggle => (
+        <Card
+          onChange={({ value, ...rest }) => {
+            if (value !== '') {
+              onChange({ ...rest, value });
+            }
+
+            onToggle();
+          }}
+        />
+      )}
+    />
+  );
+};
+
+FilterPicker.defaultProps = {
+  onChange: () => {},
+};
+
+FilterPicker.propTypes = {
+  onChange: PropTypes.func,
 };
 
 export default FilterPicker;
