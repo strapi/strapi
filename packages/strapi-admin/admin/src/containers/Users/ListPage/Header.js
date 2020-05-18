8a80852a90014d081101c6a7de060e36ff8327a9
soupette <cyril.lpz@gmail.com>
2020-05-18 17:21:54 +0200

    Fix header
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/Users/ListPage/Header.js
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/Header.js
@@ -8,26 +8,31 @@ const Header = ({ count, dataToDelete, isLoading, onClickAddUser }) => {
   const tradBaseId = 'Settings.permissions.users.listview.';
   const headerDescriptionSuffix =
     count > 1 ? 'header.description.plural' : 'header.description.singular';
-  const headerProps = {
-    actions: [
-      {
-        color: 'delete',
-        disabled: !dataToDelete.length,
-        label: formatMessage({ id: 'app.utils.delete' }),
-        type: 'button',
-      },
 
-      {
-        color: 'primary',
-        icon: true,
-        label: formatMessage({ id: 'Settings.permissions.users.create' }),
-        onClick: onClickAddUser,
-        type: 'button',
-      },
-    ],
+  /* eslint-disable indent */
+  const headerProps = {
+    actions: isLoading
+      ? []
+      : [
+          {
+            color: 'delete',
+            disabled: !dataToDelete.length,
+            label: formatMessage({ id: 'app.utils.delete' }),
+            type: 'button',
+          },
+
+          {
+            color: 'primary',
+            icon: true,
+            label: formatMessage({ id: 'Settings.permissions.users.create' }),
+            onClick: onClickAddUser,
+            type: 'button',
+          },
+        ],
     content: formatMessage({ id: `${tradBaseId}${headerDescriptionSuffix}` }, { number: count }),
     title: { label: formatMessage({ id: `${tradBaseId}header.title` }) },
   };
+  /* eslint-enable indent */
 
   return <HeaderCompo {...headerProps} isLoading={isLoading} />;
 };
