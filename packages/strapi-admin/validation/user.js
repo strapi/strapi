a841400f851684e294dc660e7f35a21dba22dda5
Alexandre Bodin <bodin.alex@gmail.com>
2020-05-18 17:16:49 +0200

    Add register route
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-admin/validation/user.js
+++ packages/strapi-admin/validation/user.js
@@ -4,24 +4,27 @@ const { yup, formatYupErrors } = require('strapi-utils');
 
 const handleReject = error => Promise.reject(formatYupErrors(error));
 
-const userCreationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email()
-    .required(),
-  firstname: yup
-    .string()
-    .min(1)
-    .required(),
-  lastname: yup
-    .string()
-    .min(1)
-    .required(),
-  roles: yup
-    .array()
-    .min(1)
-    .required(),
-});
+const userCreationSchema = yup
+  .object()
+  .shape({
+    email: yup
+      .string()
+      .email()
+      .required(),
+    firstname: yup
+      .string()
+      .min(1)
+      .required(),
+    lastname: yup
+      .string()
+      .min(1)
+      .required(),
+    roles: yup
+      .array()
+      .min(1)
+      .required(),
+  })
+  .noUnknown();
 
 const validateUserCreationInput = data => {
   return userCreationSchema.validate(data, { strict: true, abortEarly: false }).catch(handleReject);
