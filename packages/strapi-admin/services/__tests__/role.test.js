67f33c8e7b098a05ddfdd6d75ff9740a61f0fd35
Pierre Noël <petersg83@gmail.com>
2020-05-18 17:54:20 +0200

    add test + services.role.create
    Signed-off-by: Pierre Noël <petersg83@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

---
+++ packages/strapi-admin/services/__tests__/role.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const _ = require('lodash');
+const roleService = require('../role');
+
+describe('Role', () => {
+  describe('create', () => {
+    test('Creates a role', async () => {
+      const dbCreate = jest.fn(role => Promise.resolve(role));
+
+      global.strapi = {
+        query: () => ({ create: dbCreate }),
+      };
+
+      const input = { name: 'super_admin', description: "Have all permissions. Can't be delete" };
+
+      const createdRole = await roleService.create(input);
+
+      expect(dbCreate).toHaveBeenCalled();
+      expect(createdRole).toStrictEqual(input);
+    });
+  });
+  describe('fetch', () => {
+    test('Fetches a role', async () => {
+      const role = {
+        id: 1,
+        name: 'super_admin',
+        description: "Have all permissions. Can't be delete",
+      };
+      const dbFindOne = jest.fn(({ id }) => _.find([role], { id }));
+
+      global.strapi = {
+        query: () => ({ findOne: dbFindOne }),
+      };
+
+      const fetchedRole = await roleService.fetch({ id: role.id });
+
+      expect(dbFindOne).toHaveBeenCalled();
+      expect(fetchedRole).toStrictEqual(role);
+    });
+  });
+});
