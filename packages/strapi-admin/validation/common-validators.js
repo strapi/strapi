67415d59b3541ae78254fb37dcb54dbe8f32c693
Alexandre Bodin <bodin.alex@gmail.com>
2020-05-18 20:39:39 +0200

    Remove email update of admin during test that break next tests
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-admin/validation/common-validators.js
+++ packages/strapi-admin/validation/common-validators.js
@@ -3,21 +3,18 @@
 const { yup } = require('strapi-utils');
 
 const validators = {
-  firstname: yup
+  email: yup
     .string()
-    .min(1)
-    .required(),
-  lastname: yup
-    .string()
-    .min(1)
-    .required(),
+    .email()
+    .min(1),
+  firstname: yup.string().min(1),
+  lastname: yup.string().min(1),
   password: yup
     .string()
     .min(8)
     .matches(/[a-z]/, '${path} must contain at least one lowercase character')
     .matches(/[A-Z]/, '${path} must contain at least one uppercase character')
-    .matches(/\d/, '${path} must contain at least one number')
-    .required(),
+    .matches(/\d/, '${path} must contain at least one number'),
 };
 
 module.exports = validators;
