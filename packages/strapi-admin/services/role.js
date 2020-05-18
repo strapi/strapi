37924553ea121973049426eb15f7f3554dc6f6b3
Pierre Noël <petersg83@gmail.com>
2020-05-18 17:54:20 +0200

    add test + services.role.create
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi-admin/services/role.js
+++ packages/strapi-admin/services/role.js
@@ -1,4 +1,7 @@
 module.exports = {
+  create(params) {
+    return strapi.query('role', 'admin').create(params);
+  },
   fetch(params) {
     return strapi.query('role', 'admin').findOne(params);
   },
