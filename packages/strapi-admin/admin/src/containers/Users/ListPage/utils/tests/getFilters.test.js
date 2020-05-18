7ed0dfac8e3991e9225c5d6b7d2c49f7ac01d5a8
soupette <cyril.lpz@gmail.com>
2020-05-18 16:55:26 +0200

    Add filter logic
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/utils/tests/getFilters.test.js
@@ -0,0 +1,27 @@
+import getFilters from '../getFilters';
+
+describe('ADMIN | CONTAINERS | USERS | ListPage | utils | getFilters', () => {
+  it('should return an empty array if there is not filter', () => {
+    const search = '_q=test&_sort=firstname&_page=1&_limit=1';
+
+    expect(getFilters(search)).toHaveLength(0);
+  });
+
+  it('should handle the = filter correctly ', () => {
+    const search = '_sort=firstname&_page=1&_limit=1&firstname=test&firstname_ne=something';
+    const expected = [
+      {
+        name: 'firstname',
+        filter: '=',
+        value: 'test',
+      },
+      {
+        name: 'firstname',
+        filter: '_ne',
+        value: 'something',
+      },
+    ];
+
+    expect(getFilters(search)).toEqual(expected);
+  });
+});
