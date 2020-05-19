dec7f4ff4ff796613745b2e8babef89bf0cb7250
Pierre Noël <petersg83@gmail.com>
2020-05-19 11:53:34 +0200

    add doc comments
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi-admin/services/role.js
+++ packages/strapi-admin/services/role.js
@@ -1,7 +1,18 @@
 module.exports = {
-  create(params) {
-    return strapi.query('role', 'admin').create(params);
+  /**
+   * Create and save a role in database
+   * @param attributes A partial role object
+   * @returns {Promise<role>}
+   */
+  create(attributes) {
+    return strapi.query('role', 'admin').create(attributes);
   },
+
+  /**
+   * fetch a role in database
+   * @param params query params to find the role
+   * @returns {Promise<user>}
+   */
   fetch(params) {
     return strapi.query('role', 'admin').findOne(params);
   },
