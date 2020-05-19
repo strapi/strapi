ccadbc9c2104c3bbbc5c6f90a1cb49eea8424dbe
soupette <cyril.lpz@gmail.com>
2020-05-19 16:07:03 +0200

    Design edit user page
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/Users/EditPage/index.js
+++ packages/strapi-admin/admin/src/containers/Users/EditPage/index.js
@@ -1,22 +1,33 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import { useIntl } from 'react-intl';
+import { get } from 'lodash';
 import {
-  // BackHeader,
   // LoadingIndicator,
-  // Row,
   // auth,
   // request,
   useGlobalContext,
 } from 'strapi-helper-plugin';
+import { Col } from 'reactstrap';
+import { Padded } from '@buffetjs/core';
+import BaselineAlignement from '../../../components/BaselineAlignement';
 import ContainerFluid from '../../../components/ContainerFluid';
+import FormBloc from '../../../components/FormBloc';
+import SizedInput from '../../../components/SizedInput';
 import Header from '../../../components/Users/Header';
+import SelectRoles from '../../../components/Users/SelectRoles';
+import form from './utils/form';
+import fakeData from './utils/tempData';
 import { initialState, reducer } from './reducer';
 import init from './init';
 
 const EditPage = () => {
   const { settingsBaseURL } = useGlobalContext();
-  const [{ isLoading }, dispatch] = useReducer(reducer, initialState, init);
+  const [{ formErrors, isLoading, modifiedData }, dispatch] = useReducer(
+    reducer,
+    initialState,
+    init
+  );
   const { formatMessage } = useIntl();
   const {
     params: { id },
@@ -25,11 +36,39 @@ const EditPage = () => {
     ? 'app.containers.Users.EditPage.header.label-loading'
     : 'app.containers.Users.EditPage.header.label';
   const headerLabel = formatMessage({ id: headerLabelId }, { name: 'soup' });
-  console.log({ dispatch, id });
+
+  useEffect(() => {
+    const getData = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          dispatch({
+            type: 'GET_DATA_SUCCEEDED',
+            data: fakeData,
+          });
+
+          resolve();
+        }, 1000);
+      });
+    };
+
+    getData();
+  }, []);
+  console.log({ id });
+
+  const handleChange = ({ target: { name, value, type: inputType } }) => {
+    dispatch({
+      type: 'ON_CHANGE',
+      inputType,
+      keys: name,
+      value,
+    });
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
   };
+
+  console.log({ modifiedData });
 
   return (
     <>
@@ -42,6 +81,45 @@ const EditPage = () => {
             modifiedData={{}}
             onCancel={() => {}}
           />
+          <BaselineAlignement top size="3px" />
+          <FormBloc
+            isLoading={isLoading}
+            title={formatMessage({
+              id: 'app.components.Users.ModalCreateBody.block-title.details',
+            })}
+          >
+            {Object.keys(form).map(key => {
+              return (
+                <SizedInput
+                  {...form[key]}
+                  key={key}
+                  error={formErrors[key]}
+                  name={key}
+                  onChange={handleChange}
+                  value={get(modifiedData, key, '')}
+                />
+              );
+            })}
+          </FormBloc>
+          <BaselineAlignement top size="32px" />
+          {!isLoading && (
+            <FormBloc
+              title={formatMessage({ id: 'app.containers.Users.EditPage.roles-bloc-title' })}
+            >
+              <Col xs="6">
+                <Padded top size="sm">
+                  <SelectRoles
+                    name="roles"
+                    onChange={handleChange}
+                    error={formErrors.roles}
+                    value={get(modifiedData, 'roles', [])}
+                  />
+                  {/* TODO fix padding for error */}
+                  <BaselineAlignement top size="17px" />
+                </Padded>
+              </Col>
+            </FormBloc>
+          )}
         </ContainerFluid>
       </form>
     </>
