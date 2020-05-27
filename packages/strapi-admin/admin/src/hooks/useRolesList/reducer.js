c7b54bbdfd9526498f838bf7842e37501139e2b9
HichamELBSI <elabbassih@gmail.com>
2020-05-27 17:18:48 +0200

    Fake role list
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/admin/src/hooks/useRolesList/reducer.js
+++ packages/strapi-admin/admin/src/hooks/useRolesList/reducer.js
@@ -2,7 +2,27 @@
 import produce from 'immer';
 
 export const initialState = {
-  roles: [],
+  // TODO : TEMP => Remove after role creation is done.
+  roles: [
+    {
+      id: 1,
+      name: 'Super admin',
+      description: 'This is the fake description of the super admin role.',
+      users: [1],
+    },
+    {
+      id: 2,
+      name: 'Editor',
+      description:
+        'This is the fake description of the editor role. This is the fake description of the editor role.',
+      users: [7, 2, 3, 4],
+    },
+    {
+      id: 3,
+      name: 'Author',
+      users: [5, 34],
+    },
+  ],
   isLoading: true,
 };
 
