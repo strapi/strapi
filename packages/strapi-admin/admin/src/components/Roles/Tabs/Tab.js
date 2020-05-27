b4e40e64fa73ebd9458e71ec1e9015ccb4579d55
soupette <cyril.lpz@gmail.com>
2020-05-27 15:29:32 +0200

    Fix tabs baseline
    Signed-off-by: soupette <cyril.lpz@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/admin/src/components/Roles/Tabs/Tab.js
+++ packages/strapi-admin/admin/src/components/Roles/Tabs/Tab.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+/* eslint-disable indent */
 const Tab = styled.div`
   flex: 1;
   padding: 1rem;
@@ -7,13 +8,14 @@ const Tab = styled.div`
   ${({ isActive, theme }) =>
     isActive
       ? {
-        borderTop: `2px solid ${theme.main.colors.blue}`,
-        backgroundColor: theme.main.colors.white,
-      }
+          borderTop: `2px solid ${theme.main.colors.blue}`,
+          backgroundColor: theme.main.colors.white,
+        }
       : {
-        backgroundColor: '#f2f3f4',
-        cursor: 'pointer',
-      }}
+          borderTop: '2px solid transparent',
+          backgroundColor: '#f2f3f4',
+          cursor: 'pointer',
+        }}
 `;
 
 export default Tab;
