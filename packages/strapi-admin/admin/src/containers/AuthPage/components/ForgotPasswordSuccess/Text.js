8c6e9e79cfbf75ea40fa3f09c3378b4e21cb2ec3
soupette <cyril.lpz@gmail.com>
2020-05-28 18:16:11 +0200

    Fix style
    Signed-off-by: soupette <cyril.lpz@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/admin/src/containers/AuthPage/components/ForgotPasswordSuccess/Text.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Text as Base } from '@buffetjs/core';
-
-/* FIXME IN BUFFET.JS */
-const Text = styled(Base)`
-  font-size: 24px;
-`;
-
-export default Text;
--- packages/strapi-admin/admin/src/containers/AuthPage/components/Oops/Text.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Text as Base } from '@buffetjs/core';
-
-const Text = styled(Base)`
-  font-size: 24px;
-`;
-
-export default Text;
