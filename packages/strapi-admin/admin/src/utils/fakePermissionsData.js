9a68e2b3e134fbb80028cf20b9104443b7fb0bdf
soupette <cyril.lpz@gmail.com>
2020-06-04 14:35:04 +0200

    init fake users permissions data
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/strapi-admin/admin/src/utils/fakePermissionsData.js
@@ -0,0 +1,223 @@
+const data = [
+  // Admin marketplace
+  {
+    action: 'admin::marketplace.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::marketplace.plugins.install',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::marketplace.plugins.uninstall',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Admin webhooks
+  {
+    action: 'admin::webhooks.create',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::webhooks.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::webhooks.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::webhooks.delete',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Admin users
+  {
+    action: 'admin::users.create',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::users.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::users.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::users.delete',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Admin roles
+  {
+    action: 'admin::roles.create',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::roles.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::roles.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'admin::roles.delete',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Content type builder
+  {
+    action: 'plugins::content-type-builder.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Documentation plugin
+  {
+    action: 'plugins::documentation.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::documentation.settings.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::documentation.settings.regenerate',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Upload plugin
+  {
+    action: 'plugins::upload.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::upload.assets.create',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::upload.assets.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::upload.assets.dowload',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::upload.assets.copy-link',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+
+  // Users-permissions
+  {
+    action: 'plugins::users-permissions.roles.create',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.roles.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.roles.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.roles.delete',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.email-templates.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.email-templates.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.providers.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.providers.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.advanced-settings.read',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+  {
+    action: 'plugins::users-permissions.advanced-settings.update',
+    subject: null,
+    fields: null,
+    conditions: null,
+  },
+];
+
+export default data;
