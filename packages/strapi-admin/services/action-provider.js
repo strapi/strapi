ccbd1481e39e6ee861b2bb8bb6b9fe655777d573
Pierre Noël <petersg83@gmail.com>
2020-06-09 12:23:35 +0200

    add js doc
    Signed-off-by: Pierre Noël <petersg83@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/services/action-provider.js
+++ packages/strapi-admin/services/action-provider.js
@@ -9,17 +9,38 @@ const actionProviderFactory = () => {
   const actions = new Map();
 
   return {
+    /**
+     * Get an action
+     * @param {String} uid uid given when registering the action
+     * @param {String} pluginName name of the plugin which is related to the action
+     * @returns {Promise<Action>}
+     */
     get(uid, pluginName) {
       const actionId = getActionId({ pluginName, uid });
       const action = actions.get(actionId);
       return _.cloneDeep(action);
     },
+
+    /**
+     * Get all actions stored in an array
+     * @returns {Promise<Array<Action>>}
+     */
     getAll() {
       return _.cloneDeep(Array.from(actions.values()));
     },
+
+    /**
+     * Get all actions stored in a Map
+     * @returns {Promise<Map<uid, Action>>}
+     */
     getAllByMap() {
       return _.cloneDeep(actions);
     },
+
+    /**
+     * Register actions
+     * @param {Array} newActions actions to register
+     */
     register(newActions) {
       if (strapi.isLoaded) {
         throw new Error(`You can't register new actions outside of the bootstrap function.`);
