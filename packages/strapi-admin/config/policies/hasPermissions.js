11ba971302bc8c7374cabef63241864b934e59ed
Convly <jean-sebastien.herbaux@epitech.eu>
2020-06-19 15:02:10 +0200

    Rework policy handling, add util to create policy factories, allow array declaration for hasPermissions
    Signed-off-by: Convly <jean-sebastien.herbaux@epitech.eu>

--- packages/strapi-admin/config/policies/hasPermissions.js
+++ packages/strapi-admin/config/policies/hasPermissions.js
@@ -1,23 +1,46 @@
 'use strict';
 
+const _ = require('lodash');
+const {
+  policy: { createPolicyFactory },
+} = require('strapi-utils');
 const { validateHasPermissionsInput } = require('../../validation/policies/hasPermissions');
 
-module.exports = permissions => {
-  try {
-    validateHasPermissionsInput(permissions);
-  } catch {
-    throw new Error('Invalid objects submitted to admin::hasPermissions policy.');
+const inputModifiers = [
+  {
+    check: _.isString,
+    transform: action => ({ action }),
+  },
+  {
+    check: _.isArray,
+    transform: arr => ({ action: arr[0], subject: arr[1] }),
+  },
+  {
+    check: _.isObject,
+    transform: perm => perm,
+  },
+];
+
+module.exports = createPolicyFactory(
+  input => {
+    const permissions = input.map(val =>
+      inputModifiers.find(modifier => modifier.check(val)).transform(val)
+    );
+
+    return (ctx, next) => {
+      const { userAbility: ability } = ctx.state;
+
+      const isAuthorized = permissions.every(({ action, subject }) => ability.can(action, subject));
+
+      if (!isAuthorized) {
+        throw strapi.errors.forbidden();
+      }
+
+      return next();
+    };
+  },
+  {
+    validator: validateHasPermissionsInput,
+    name: 'admin::hasPermissions',
   }
-
-  return (ctx, next) => {
-    const { userAbility: ability } = ctx.state;
-
-    const isAuthorized = permissions.every(({ action, subject }) => ability.can(action, subject));
-
-    if (!isAuthorized) {
-      throw strapi.errors.forbidden();
-    }
-
-    return next();
-  };
-};
+);
