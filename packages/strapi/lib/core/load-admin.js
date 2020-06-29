0a9665b3e0aa30a4b7e03a14e3907388d1584a40
Pierre Noël <petersg83@gmail.com>
2020-06-29 10:30:30 +0200

    move logic to load-admin
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi/lib/core/load-admin.js
+++ packages/strapi/lib/core/load-admin.js
@@ -1,16 +1,22 @@
 'use strict';
 
+const _ = require('lodash');
+
 const findPackagePath = require('../load/package-path');
 const loadFiles = require('../load/load-files');
 const loadConfig = require('../load/load-config-files');
 
-module.exports = async () => {
+module.exports = async strapi => {
   const adminPath = findPackagePath('strapi-admin');
 
   const [files, config] = await Promise.all([
     loadFiles(adminPath, '!(config|node_modules|scripts)/*.*(js|json)'),
     loadConfig(adminPath),
   ]);
+
+  // set admin config in strapi.config.server.admin
+  const userAdminConfig = strapi.config.get('server.admin');
+  strapi.config.set('server.admin', _.merge(config.config, userAdminConfig));
 
   return Object.assign({}, config, files);
 };
