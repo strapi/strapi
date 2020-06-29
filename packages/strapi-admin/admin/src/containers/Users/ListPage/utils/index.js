70300ffc3e9aa6f6c94cd7bd560dae39d002c4ad
soupette <cyril.lpz@gmail.com>
2020-06-29 17:42:58 +0200

    Delete the current user last
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/containers/Users/ListPage/utils/index.js
+++ packages/strapi-admin/admin/src/containers/Users/ListPage/utils/index.js
@@ -1,3 +1,4 @@
 export { default as getFilters } from './getFilters';
 export { default as retrieveAdminUsers } from './retrieveAdminUsers';
 export { default as retrieveNonAdminUsers } from './retrieveNonAdminUsers';
+export { default as sortIds } from './sortIds';
