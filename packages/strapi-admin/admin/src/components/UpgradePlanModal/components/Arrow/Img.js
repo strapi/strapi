30bf4c25034b8726f4edca7d992681fc6fe0efcc
soupette <cyril.lpz@gmail.com>
2020-07-01 18:24:54 +0200

    Add trads
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/admin/src/components/UpgradePlanModal/components/Arrow/Img.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Img = styled.img`
-  position: absolute;
-  top: 20px;
-  right: 195px;
-`;
-
-export default Img;
--- packages/strapi-admin/admin/src/components/UpgradePlanModal/components/Option/index.js
+++ packages/strapi-admin/admin/src/components/UpgradePlanModal/components/Option/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text } from '@buffetjs/core';
+import { useIntl } from 'react-intl';
 import Wrapper from './Wrapper';
 
 const Option = () => {
+  const { formatMessage } = useIntl();
+
   return (
     <Wrapper left right size="xs">
       <Text color="mediumBlue" lineHeight="23px">
-        Community Edition
+        {formatMessage({ id: 'app.components.UpgradePlanModal.text-ce' })}
       </Text>
     </Wrapper>
   );
