4d4c7678c0daf9abb262d13e5322d2a3377de320
Pierre Noël <petersg83@gmail.com>
2020-07-03 18:33:38 +0200

    refacto
    Signed-off-by: Pierre Noël <petersg83@gmail.com>

--- packages/strapi-admin/ee/validation/permission.js
+++ packages/strapi-admin/ee/validation/permission.js
@@ -5,10 +5,16 @@ const validators = require('../../validation/common-validators');
 
 const handleReject = error => Promise.reject(formatYupErrors(error));
 
-const validatedUpdatePermissionsInput = data => {
-  return validators.updatePermissions
-    .validate(data, { strict: true, abortEarly: false })
-    .catch(handleReject);
+const updatePermissionsSchemas = [...validators.updatePermissionsValidators];
+
+const validatedUpdatePermissionsInput = async data => {
+  try {
+    for (const schema of updatePermissionsSchemas) {
+      await schema.validate(data, { strict: true, abortEarly: false });
+    }
+  } catch (e) {
+    return handleReject(e);
+  }
 };
 
 module.exports = {
