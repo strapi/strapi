5d9a1ecc76203fe297251ba52a6627e6c14bfafb
ryanhex53 <ouyang.em@gmail.com>
2020-07-03 15:56:12 +0800

    Update index.js

--- packages/strapi-plugin-upload/admin/src/translations/index.js
+++ packages/strapi-plugin-upload/admin/src/translations/index.js
@@ -2,12 +2,15 @@ import en from './en.json';
 import ms from './ms.json';
 import ru from './ru.json';
 import uk from './uk.json';
+import zhHans from './zh-Hans.json';
 
 const trads = {
   en,
   ms,
   ru,
   uk,
+  zh: zhHans,
+  'zh-Hans': zhHans
 };
 
 export default trads;
