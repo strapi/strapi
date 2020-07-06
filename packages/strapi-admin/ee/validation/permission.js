9dc7bb719ef9911de319c197137a3de04ba8e6b9
Pierre Noël <petersg83@gmail.com>
2020-07-06 09:56:37 +0200

    simplify validator
    Signed-off-by: Pierre Noël <petersg83@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/ee/validation/permission.js
+++ packages/strapi-admin/ee/validation/permission.js
@@ -5,16 +5,10 @@ const validators = require('../../validation/common-validators');
 
 const handleReject = error => Promise.reject(formatYupErrors(error));
 
-const updatePermissionsSchemas = [...validators.updatePermissionsValidators];
-
-const validatedUpdatePermissionsInput = async data => {
-  try {
-    for (const schema of updatePermissionsSchemas) {
-      await schema.validate(data, { strict: true, abortEarly: false });
-    }
-  } catch (e) {
-    return handleReject(e);
-  }
+const validatedUpdatePermissionsInput = data => {
+  return validators.updatePermissions
+    .validate(data, { strict: true, abortEarly: false })
+    .catch(handleReject);
 };
 
 module.exports = {
