f99560efa31fff69ce921d6ac48a98869a7d4b10
Alexandre Bodin <bodin.alex@gmail.com>
2020-07-08 16:35:39 +0200

    Disallow disabling last super admin
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/domain/user.js
+++ packages/strapi-admin/domain/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { SUPER_ADMIN_CODE } = require('../services/constants');
+
 /**
  * Create a new user model by merging default and specified attributes
  * @param attributes A partial user object
@@ -13,6 +15,11 @@ function createUser(attributes) {
   };
 }
 
+const hasSuperAdminRole = user => {
+  return user.roles.filter(role => role.code === SUPER_ADMIN_CODE).length > 0;
+};
+
 module.exports = {
   createUser,
+  hasSuperAdminRole,
 };
