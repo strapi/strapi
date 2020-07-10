9f69a48aa9240bcab349b4efca9fb4edc8b45168
soupette <cyril.lpz@gmail.com>
2020-07-10 10:38:52 +0200

    Clean models
    Signed-off-by: soupette <cyril.lpz@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- examples/getstarted/api/dummy/services/dummy.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
-
-module.exports = {};
