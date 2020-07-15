b3ec0c37e27ab3b30ab60605018352966cc315bf
soupette <cyril.lpz@gmail.com>
2020-07-15 18:10:38 +0200

    Clean files
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-content-manager/admin/src/components/PreviewWysiwyg/index.js
+++ packages/strapi-plugin-content-manager/admin/src/components/PreviewWysiwyg/index.js
@@ -8,16 +8,15 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown/with-html';
 import useWysiwyg from '../../hooks/useWysiwyg';
-
-import PreviewWysiwygWrapper from './PreviewWysiwygWrapper';
+import Wrapper from './Wrapper';
 
 const PreviewWysiwyg = ({ data }) => {
   const { isFullscreen } = useWysiwyg;
 
   return (
-    <PreviewWysiwygWrapper isFullscreen={isFullscreen}>
+    <Wrapper isFullscreen={isFullscreen}>
       <ReactMarkdown source={data} skipHtml={false} escapeHtml={false} />
-    </PreviewWysiwygWrapper>
+    </Wrapper>
   );
 };
 
