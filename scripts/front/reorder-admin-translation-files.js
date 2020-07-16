934708362590ae16567d7632dab031b5647fd0ce
soupette <cyril.lpz@gmail.com>
2020-07-16 11:31:26 +0200

    Fix PR feedback
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- scripts/front/reorder-admin-translation-files.js
+++ scripts/front/reorder-admin-translation-files.js
@@ -5,8 +5,8 @@ const glob = promisify(require('glob').glob);
 
 async function orderTrads({ mainTranslationFile, translationFiles }) {
   const data = await fs.readJSON(mainTranslationFile);
-  const orderedData = Object.keys(data)
 
+  const orderedData = Object.keys(data)
     .sort()
     .reduce((acc, current) => {
       acc[current] = data[current];
@@ -16,25 +16,31 @@ async function orderTrads({ mainTranslationFile, translationFiles }) {
 
   await fs.writeJSON(mainTranslationFile, orderedData, { spaces: 2 });
 
-  await Promise.all(
-    translationFiles.map(async trad => {
-      const cleanedFile = Object.keys(orderedData).reduce((acc, current) => {
-        const currentTrad = fs.readJsonSync(trad);
+  const cleanFile = async trad => {
+    const cleanedFile = {};
+    const orderedDataKeys = Object.keys(orderedData);
 
-        if (currentTrad[current]) {
-          acc[current] = currentTrad[current];
-        }
-
-        return acc;
-      }, {});
-
+    for (let i in orderedDataKeys) {
       try {
-        await fs.writeJSON(trad, cleanedFile, { spaces: 2 });
+        const currentTrad = await fs.readJson(trad);
+        const currentKey = orderedDataKeys[i];
+
+        if (currentTrad[currentKey]) {
+          cleanedFile[currentKey] = currentTrad[currentKey];
+        }
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
-    })
-  );
+    }
+
+    try {
+      await fs.writeJSON(trad, cleanedFile, { spaces: 2 });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  await Promise.all(translationFiles.map(trad => cleanFile(trad)));
 }
 
 async function run() {
