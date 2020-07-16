708173a1dcd9ac400e816587cb0c22ead5865d79
Pierre Noël <petersg83@gmail.com>
2020-07-16 12:22:36 +0200

    use batch-delete users instead of deleteOne in front
    Signed-off-by: Pierre Noël <petersg83@gmail.com>
    Signed-off-by: Gil Fernandes <gil.fernandes@onepointltd.com>

--- packages/strapi-admin/admin/src/containers/Users/ListPage/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as getFilters } from './getFilters';
-export { default as retrieveAdminUsers } from './retrieveAdminUsers';
-export { default as retrieveNonAdminUsers } from './retrieveNonAdminUsers';
-export { default as sortIds } from './sortIds';
--- packages/strapi-admin/admin/src/containers/Users/ListPage/utils/retrieveNonAdminUsers.js
+++
@@ -1,8 +0,0 @@
-const retrieveNonAdminUsers = (usersToDelete, adminUsersToDelete) =>
-  usersToDelete.filter(id => {
-    const index = adminUsersToDelete.findIndex(adminId => adminId.toString() === id.toString());
-
-    return index === -1;
-  });
-
-export default retrieveNonAdminUsers;
