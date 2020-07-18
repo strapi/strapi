9f9808dfd0de78825dabf137f3d9ffab9033bf5b
Alexandre Bodin <bodin.alex@gmail.com>
2020-07-18 09:47:37 +0200

    Make sure dot env is loaded by core app config and as first require when used
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi/lib/Strapi.js
+++ packages/strapi/lib/Strapi.js
@@ -1,9 +1,7 @@
 'use strict';
 
-// Dependencies.
-const dotenv = require('dotenv');
-
-dotenv.config({ path: process.env.ENV_PATH });
+// required first because it loads env files.
+const loadConfiguration = require('./core/app-configuration');
 
 const http = require('http');
 const path = require('path');
@@ -18,7 +16,6 @@ const { createDatabaseManager } = require('strapi-database');
 
 const utils = require('./utils');
 const loadModules = require('./core/load-modules');
-const loadConfiguration = require('./core/app-configuration');
 const bootstrap = require('./core/bootstrap');
 const initializeMiddlewares = require('./middlewares');
 const initializeHooks = require('./hooks');
@@ -36,7 +33,6 @@ const createTelemetry = require('./services/metrics');
  *
  * @constructor
  */
-
 class Strapi {
   constructor(opts = {}) {
     this.reload = this.reload();
--- packages/strapi/lib/commands/build.js
+++ packages/strapi/lib/commands/build.js
@@ -1,11 +1,14 @@
 'use strict';
 
+// required first because it loads env files.
+const loadConfiguration = require('../core/app-configuration');
+
 const { green } = require('chalk');
+
 // eslint-disable-next-line node/no-extraneous-require
 const strapiAdmin = require('strapi-admin');
 const { getConfigUrls } = require('strapi-utils');
 
-const loadConfiguration = require('../core/app-configuration');
 const addSlash = require('../utils/addSlash');
 /**
  * `$ strapi build`
