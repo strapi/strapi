a6de3c2cc3aef100413201a17f0b8347c1175e77
Bartosz <67923777+akcyp@users.noreply.github.com>
2020-07-27 17:18:55 +0200

    Fix invalid local uploads path (#6925)
    * Fix invalid local uploads path
    
    File upload provider does not consider the strapi configuration. This patch should fix the problem.
    
    * getStaticPath -> uploadDir
    
    * Use config.get method
    
    * plugin-upload tests fix
    
    Signed-off-by: Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>

--- packages/strapi-provider-upload-local/lib/index.js
+++ packages/strapi-provider-upload-local/lib/index.js
@@ -23,6 +23,10 @@ module.exports = {
         });
       }
     };
+    const uploadDir = path.join(
+      strapi.dir,
+      strapi.config.get('middleware.settings.public.path', strapi.config.paths.static)
+    );
 
     return {
       upload(file) {
@@ -31,7 +35,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
           // write file in public/assets folder
           fs.writeFile(
-            path.join(strapi.config.paths.static, `/uploads/${file.hash}${file.ext}`),
+            path.join(uploadDir, `/uploads/${file.hash}${file.ext}`),
             file.buffer,
             err => {
               if (err) {
@@ -47,10 +51,7 @@ module.exports = {
       },
       delete(file) {
         return new Promise((resolve, reject) => {
-          const filePath = path.join(
-            strapi.config.paths.static,
-            `/uploads/${file.hash}${file.ext}`
-          );
+          const filePath = path.join(uploadDir, `/uploads/${file.hash}${file.ext}`);
 
           if (!fs.existsSync(filePath)) {
             return resolve("File doesn't exist");
