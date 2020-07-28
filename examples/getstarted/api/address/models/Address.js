fda711f76aa0298f9fd9b48e2ab1f363647ed926
Tomaszal <mrtomaszal@gmail.com>
2020-07-28 01:20:24 +0100

    Added virtual field example
    Signed-off-by: Tomaszal <mrtomaszal@gmail.com>

--- examples/getstarted/api/address/models/Address.js
+++ examples/getstarted/api/address/models/Address.js
@@ -4,4 +4,13 @@
  * Lifecycle callbacks for the `Address` model.
  */
 
-module.exports = {};
+module.exports = {
+  afterFind(result) {
+    // update array of results
+    result = result.map(strapi.services.address.setTitle);
+  },
+  afterFindOne(result) {
+    // update one item
+    strapi.services.address.setTitle(result);
+  },
+};
--- examples/getstarted/api/address/services/Address.js
+++ examples/getstarted/api/address/services/Address.js
@@ -5,4 +5,12 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  setTitle(data) {
+    const { city, postal_coder } = data;
+    data.title = [city, postal_coder].filter(Boolean).join(' ');
+    console.log(data);
+    // we need to return data, because we will update array of entities with map
+    return data;
+  },
+};
