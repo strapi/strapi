e46c92f2c14fd7141326e89c2b05a4d5d3a6b50e
eggpogg <eggpogg.t@gmail.com>
2020-07-28 16:44:19 +0900

    add Japanese translation of media library

--- packages/strapi-plugin-upload/admin/src/translations/index.js
+++ packages/strapi-plugin-upload/admin/src/translations/index.js
@@ -1,10 +1,12 @@
 import en from './en.json';
+import ja from './ja.json';
 import ms from './ms.json';
 import ru from './ru.json';
 import uk from './uk.json';
 
 const trads = {
   en,
+  ja,
   ms,
   ru,
   uk,
