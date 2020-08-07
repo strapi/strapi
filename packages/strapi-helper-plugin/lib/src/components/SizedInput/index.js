c3181576331776431150ffb84095dd372c75a2d1
soupette <cyril.lpz@gmail.com>
2020-08-07 12:55:24 +0200

    Fix import name
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-helper-plugin/lib/src/components/SizedInput/index.js
+++ packages/strapi-helper-plugin/lib/src/components/SizedInput/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col } from 'reactstrap';
 import PropTypes from 'prop-types';
-import IntlInput from '../InTlInput';
+import IntlInput from '../IntlInput';
 
 const SizedInput = ({ size, ...rest }) => {
   return (
