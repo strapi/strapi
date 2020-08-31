f1f1d1d3e9924aa9c7b972a26e73074e2f49f1f1
soupette <cyril.lpz@gmail.com>
2020-08-31 16:51:14 +0200

    Remove models
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- examples/getstarted/api/baba/controllers/baba.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
