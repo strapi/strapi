acf5524a78a7a1d2fb8fb3b8fdd3aa0091b8cd97
Abdón Rodríguez Davila <a@abdonrd.com>
2020-09-01 17:20:22 +0200

    Extract deprecated options
    Signed-off-by: Abdón Rodríguez Davila <a@abdonrd.com>

--- packages/strapi-plugin-graphql/hooks/graphql/index.js
+++ packages/strapi-plugin-graphql/hooks/graphql/index.js
@@ -97,6 +97,13 @@ module.exports = strapi => {
 
       const apolloServerConfig = _.get(strapi.plugins.graphql, 'config.apolloServerConfig', {});
 
+      // TODO: Remove these deprecated options in favor of `apolloServerConfig` in the next major version
+      const deprecatedOptions = {
+        tracing: _.get(strapi.plugins.graphql, 'config.tracing', false),
+        introspection: _.get(strapi.plugins.graphql, 'config.introspection', true),
+        engine: _.get(strapi.plugins.graphql, 'config.engine', false),
+      };
+
       const serverParams = {
         ...schemaDef,
         context: ({ ctx }) => {
@@ -118,10 +125,7 @@ module.exports = strapi => {
         playground: false,
         cors: false,
         bodyParserConfig: true,
-        // Deprecate `tracing`, `introspection` and `engine` in favor of `apolloServerConfig`.
-        tracing: _.get(strapi.plugins.graphql, 'config.tracing', false),
-        introspection: _.get(strapi.plugins.graphql, 'config.introspection', true),
-        engine: _.get(strapi.plugins.graphql, 'config.engine', false),
+        ...deprecatedOptions,
         ...apolloServerConfig,
       };
 
