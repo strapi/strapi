80235bcda5d5643c591625c1f9ca94546cc49ec5
Abdón Rodríguez Davila <a@abdonrd.com>
2020-09-02 14:22:02 +0200

    Add warning note
    Signed-off-by: Abdón Rodríguez Davila <a@abdonrd.com>

--- packages/strapi-plugin-graphql/hooks/graphql/index.js
+++ packages/strapi-plugin-graphql/hooks/graphql/index.js
@@ -102,6 +102,16 @@ module.exports = strapi => {
         engine: _.get(strapi.plugins.graphql, 'config.engine', false),
       };
 
+      if (
+        strapi.plugins.graphql.config.tracing ||
+        strapi.plugins.graphql.config.introspection ||
+        strapi.plugins.graphql.config.engine
+      ) {
+        strapi.log.warn(
+          'The `tracing`, `introspection` and `engine` options are deprecated in favor of the `apolloServerConfig` object and they will be removed in the next major version.'
+        );
+      }
+
       const apolloServerConfig = _.get(strapi.plugins.graphql, 'config.apolloServerConfig', {});
 
       const serverParams = {
