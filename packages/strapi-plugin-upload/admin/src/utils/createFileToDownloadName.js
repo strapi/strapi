5077b33c97795e01dd00d8fd63f503805583851e
soupette <cyril.lpz@gmail.com>
2020-09-08 17:36:21 +0200

    Fix PR feedback
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-upload/admin/src/utils/createFileToDownloadName.js
+++ packages/strapi-plugin-upload/admin/src/utils/createFileToDownloadName.js
@@ -3,7 +3,7 @@ import { get } from 'lodash';
 const createFileToDownloadName = ({ file, fileInfo: { name } }) => {
   const ext = get(file, 'ext', '');
 
-  return `${name.replace(ext, '')}${ext}`;
+  return name.endsWith(ext) ? name : `${name}${ext}`;
 };
 
 export default createFileToDownloadName;
