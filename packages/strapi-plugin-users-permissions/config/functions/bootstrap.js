a4a531f052dc424771afa33021a13556420ecd37
Rama <ralph.maroun@somedaynow.com>
2020-09-28 13:50:19 +0200

    Adding AWS Cognito provider configuration and key in all language files to translate the subdomain property that has been added to the aws cognito
    Signed-off-by: Rama <ralph.maroun@somedaynow.com>

--- packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
+++ packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
@@ -102,6 +102,15 @@ module.exports = async () => {
       callback: `${strapi.config.server.url}/auth/linkedin/callback`,
       scope: ['r_liteprofile', 'r_emailaddress'],
     },
+    cognito: {
+      enabled: false,
+      icon: 'aws',
+      key: '',
+      secret: '',
+      subdomain: '',
+      callback: `${strapi.config.server.url}/auth/cognito/callback`,
+      scope: ['email'],
+    },
   };
   const prevGrantConfig = (await pluginStore.get({ key: 'grant' })) || {};
   // store grant auth config to db
