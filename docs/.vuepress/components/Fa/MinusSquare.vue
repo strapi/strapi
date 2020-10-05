bc16fda42ab1916d5b44dca94c9d94315db4e1fa
Jim LAURIE <j.laurie6993@gmail.com>
2020-10-05 14:40:51 +0200

    docs: Add FA icons

---
+++ docs/.vuepress/components/Fa/MinusSquare.vue
@@ -0,0 +1,22 @@
+<template><span><font-awesome-icon :icon="theIcon" :pulse="pulse" :border="border" :spin="spin" :pull="pull" :flip="flip" :rotation="rotation" :size="size" :style="attachStyles()"></font-awesome-icon></span></template>
+<script>import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMinusSquare } from "@fortawesome/free-solid-svg-icons";
+library.add(faMinusSquare);
+export default {
+   name: "FaMinusSquare",
+   components: {FontAwesomeIcon},
+   props:{
+       size: {type: String,required: false,default: null},
+       color: { type: String, required: false, default: null},
+       rotation: { type: [String, Number], required: false, default: null},
+       flip: { type: String, required: false, default: null},
+       pull: { type: String, required: false, default: null},
+       spin: { type: Boolean, required: false, default: false},
+       pulse: { type: Boolean, required: false, default: false},
+       border: { type: Boolean, required: false, default: false},
+   },
+   computed:{ theIcon(){return faMinusSquare}},
+   methods: {attachStyles: function(){return this.color ? {color: this.color} : {}}}
+}
+</script>
