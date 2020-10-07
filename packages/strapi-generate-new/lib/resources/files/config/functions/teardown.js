e645650c66a13bb436ea9d01463c7126f4d26e5a
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-10-07 13:00:47 +0530

    feat(config): add default teardown file to generator
    Signed-off-by: tanmoyopenroot <tanmoy.openroot@gmail.com>

---
+++ packages/strapi-generate-new/lib/resources/files/config/functions/teardown.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * An asynchronous teardown function that runs before
+ * your application gets destroyed.
+ *
+ * This gives you an opportunity to perform some special logic
+ * before destroying the server.
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#teardown
+ */
+
+module.exports = () => {};
