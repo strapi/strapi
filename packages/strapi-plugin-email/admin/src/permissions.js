c87370298cc66c1b52b1a33964bd2462b5f17ff8
MattieBelt <mattiasvandenbelt@gmail.com>
2020-10-08 15:00:13 +0200

    Update email settings permissions
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>

--- packages/strapi-plugin-email/admin/src/permissions.js
+++ packages/strapi-plugin-email/admin/src/permissions.js
@@ -1,8 +1,11 @@
 const pluginPermissions = {
-  // Email
-  email: [
-    { action: 'plugins::email.config.read', subject: null },
-    { action: 'plugins::email.test', subject: null },
+  // This permission regards the main component (App) and is used to tell
+  // If the plugin link should be displayed in the menu
+  // And also if the plugin is accessible. This use case is found when a user types the url of the
+  // plugin directly in the browser
+  settings: [
+    { action: 'plugins::email.settings.read', subject: null },
+    { action: 'plugins::email.settings.test', subject: null },
   ],
 };
 
--- packages/strapi-plugin-email/config/functions/bootstrap.js
+++ packages/strapi-plugin-email/config/functions/bootstrap.js
@@ -18,4 +18,25 @@ const createProvider = emailConfig => {
 module.exports = async () => {
   const emailConfig = _.get(strapi.plugins, 'email.config', {});
   strapi.plugins.email.provider = createProvider(emailConfig);
+
+  // Add permissions
+  const actions = [
+    {
+      section: 'plugins',
+      displayName: 'Read',
+      uid: 'settings.read',
+      subCategory: 'settings',
+      pluginName: 'email',
+    },
+    {
+      section: 'plugins',
+      displayName: 'Send test emails',
+      uid: 'settings.test',
+      subCategory: 'settings',
+      pluginName: 'email',
+    },
+  ];
+
+  const { actionProvider } = strapi.admin.services.permission;
+  actionProvider.register(actions);
 };
