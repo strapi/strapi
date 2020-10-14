5020233d859fb23f82eaa512c2b0804a1747a216
Thomas Chezieres <tchezieres@MacBook-Pro.lan>
2020-10-14 10:36:05 +0100

    Ammending commit
    Signed-off-by: Thomas Chezieres <tchezieres@MacBook-Pro.lan>

--- packages/strapi-utils/lib/models.js
+++ packages/strapi-utils/lib/models.js
@@ -75,6 +75,13 @@ module.exports = {
         });
       });
     } else if (_.has(attribute, 'via') && _.has(attribute, 'collection')) {
+      if (!_.has(models, attribute.collection)) {
+        throw new Error(
+          `The collection \`${_.upperFirst(
+            attribute.collection
+          )}\` is missing from the models ${attribute.plugin ? '(plugin - ' + attribute.plugin + ')' : ''}`
+        );
+      }
       const relatedAttribute = models[attribute.collection].attributes[attribute.via];
 
       if (!relatedAttribute) {
