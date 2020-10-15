d450937f58040dcd2527b697535606af02074564
Alexandre Bodin <bodin.alex@gmail.com>
2020-10-15 10:19:12 +0200

    Fix code factor
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- .github/actions/install-modules/script.sh
+++ .github/actions/install-modules/script.sh
@@ -1,7 +1,7 @@
 # install global packages if set
 if [[ -n "$GLOBAL_PACKAGES" ]]; then
   yarn global add "$GLOBAL_PACKAGES"
-  echo "$(yarn global bin)" >>$GITHUB_PATH
+  yarn global bin >>$GITHUB_PATH
 fi
 
 # run yarn
