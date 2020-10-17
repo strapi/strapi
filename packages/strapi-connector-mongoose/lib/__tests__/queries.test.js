229f24bf8356c4ddcec26847cff329dd60ca09ff
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-10-17 13:11:02 +0530

    test(queries): add test for invalid ObjectId
    Signed-off-by: tanmoyopenroot <tanmoy.openroot@gmail.com>

---
+++ packages/strapi-connector-mongoose/lib/__tests__/queries.test.js
@@ -0,0 +1,43 @@
+const queries = require('../queries');
+
+const strapi = {};
+const model = {
+  primaryKey: '_id',
+  associations: [],
+  attributes: {},
+};
+
+describe('Mongo invalid primaryKey Queries', () => {
+  test('should return null for findOne', async () => {
+    const { findOne } = queries({ model, strapi });
+    let data;
+
+    try {
+      data = await findOne({ _id: '123' });
+    } finally {
+      expect(data).toBeNull();
+    }
+  });
+
+  test('should return null for update', async () => {
+    const { update } = queries({ model, strapi });
+    let data;
+
+    try {
+      data = await update({ _id: '123' });
+    } finally {
+      expect(data).toBeNull();
+    }
+  });
+
+  test('should return null for delete', async () => {
+    const { delete: deleteMany } = queries({ model, strapi });
+    let data;
+
+    try {
+      data = await deleteMany({ _id: '123' });
+    } finally {
+      expect(data).toBeNull();
+    }
+  });
+});
