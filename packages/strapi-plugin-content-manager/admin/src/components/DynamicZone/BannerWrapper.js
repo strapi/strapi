7bf00542fd4ee081147ead98d8e543e4016de98d
jbo@lachose.fr <julien@supergazol.com>
2020-10-26 16:02:51 +0100

    BannerWrapper include updates from @HichamELBSI
    Signed-off-by: Julien Boudringhin <julien.boudringhin@gmail.com>

--- packages/strapi-plugin-content-manager/admin/src/components/DynamicZone/BannerWrapper.js
+++ packages/strapi-plugin-content-manager/admin/src/components/DynamicZone/BannerWrapper.js
@@ -7,53 +7,12 @@ const BannerWrapper = styled.button`
   height: 36px;
   width: 100%;
   padding: 0 15px;
-  border: 1px solid
-    ${({ isOpen }) => {
-      if (isOpen) {
-        return '#AED4FB';
-      } else {
-        return 'rgba(227, 233, 243, 0.75)';
-      }
-    }};
-
-  ${({ isFirst }) => {
-    if (isFirst) {
-      return `
-        border-top-right-radius: 2px;
-        border-top-left-radius: 2px;
-      `;
-    }
-  }}
-
-  ${({ isOpen }) => {
-    if (!isOpen) {
-      return `
-        border-bottom: 0;
-      `;
-    }
-  }}
-  
-
-  font-size: 13px;
-  font-weight: 500;
+  border-bottom: 0;
+  border: 1px solid rgba(227, 233, 243, 0.75);
+  background-color: ${({ theme }) => theme.main.colors.white};
+  font-size: ${({ theme }) => theme.main.sizes.fonts.md};
+  font-weight: ${({ theme }) => theme.main.fontWeights.semiBold};
   cursor: pointer;
-
-  background-color: ${({ isOpen }) => {
-    if (isOpen) {
-      return '#E6F0FB';
-    } else {
-      return '#ffffff';
-    }
-  }};
-
-  ${({ isOpen }) => {
-    if (isOpen) {
-      return `
-        color: #007eff;
-        font-weight: 600;
-      `;
-    }
-  }}
 
   &:focus {
     outline: 0;
@@ -64,33 +23,18 @@ const BannerWrapper = styled.button`
     height: 19px;
     margin-right: 19px;
     border-radius: 50%;
-    background-color: ${({ isOpen }) => {
-      if (isOpen) {
-        return '#AED4FB';
-      } else {
-        return '#F3F4F4';
-      }
-    }};
+    background-color: ${({ theme }) => theme.main.colors.mediumGrey};
     text-align: center;
-    ${({ isOpen }) => !isOpen && 'transform: rotate(180deg)'};
   }
   .label {
     text-transform: capitalize;
   }
 
-  ${({ isOpen }) => {
-    let fill = '#ABB3C2';
-    if (isOpen) {
-      fill = '#007EFF';
+  svg {
+    path {
+      fill: ${({ theme }) => theme.main.colors.leftMenu['link-color']} !important;
     }
-    return `
-      svg {
-        path {
-          fill: ${fill} !important;
-        }
-      }
-    `;
-  }}
+  }
 
   -webkit-font-smoothing: antialiased;
 
@@ -98,6 +42,37 @@ const BannerWrapper = styled.button`
     align-self: center;
     margin-top: -2px;
   }
+
+  ${({ isFirst, theme }) => {
+    if (isFirst) {
+      return `
+        border-top-right-radius: ${theme.main.sizes.borderRadius};
+        border-top-left-radius: ${theme.main.sizes.borderRadius};
+      `;
+    }
+  }}
+
+  ${({ isOpen, theme }) => {
+    if (isOpen) {
+      return `
+        border: 1px solid ${theme.main.colors.darkBlue};
+        background-color: ${theme.main.colors.lightBlue};
+        color: ${theme.main.colors.mediumBlue};
+        font-weight: ${theme.main.fontWeights.bold};
+
+        .img-wrapper {
+          background-color: ${theme.main.colors.darkBlue};
+          transform: rotate(180deg);
+        }
+
+        svg {
+        path {
+            fill: ${theme.main.colors.mediumBlue} !important;
+          }
+        }
+      `;
+    }
+  }}
 `;
 
 export default BannerWrapper;
