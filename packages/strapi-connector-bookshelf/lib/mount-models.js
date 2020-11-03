4a32691c0739bb0622b134b4843e09563d438e65
Alexandre Bodin <bodin.alex@gmail.com>
2020-11-03 16:14:31 +0100

    fix lint
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-connector-bookshelf/lib/mount-models.js
+++ packages/strapi-connector-bookshelf/lib/mount-models.js
@@ -347,8 +347,7 @@ module.exports = async ({ models, target }, ctx, { selfFinalize = false } = {}) 
               details.via,
               `${definition.collectionName}`
             ).query(qb => {
-              qb.where(filter, name)
-              .orderBy('order');
+              qb.where(filter, name).orderBy('order');
             });
           };
           break;
