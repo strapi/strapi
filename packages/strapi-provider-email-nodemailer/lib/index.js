29faa4939a18a529da778047d5516b1fd7b60421
Alexandre Bodin <bodin.alex@gmail.com>
2020-11-09 15:51:03 +0100

    Make package ready to release
    Signed-off-by: Alexandre Bodin <bodin.alex@gmail.com>

--- packages/strapi-provider-email-nodemailer/lib/index.js
+++ packages/strapi-provider-email-nodemailer/lib/index.js
@@ -4,11 +4,20 @@
  * Module dependencies
  */
 
-/* eslint-disable import/no-unresolved */
-/* eslint-disable prefer-template */
-// Public node modules.
 const _ = require('lodash');
 const nodemailer = require('nodemailer');
+
+const emailFields = [
+  'from',
+  'replyTo',
+  'to',
+  'cc',
+  'bcc',
+  'subject',
+  'text',
+  'html',
+  'attachments',
+];
 
 module.exports = {
   provider: 'nodemailer',
@@ -19,31 +28,14 @@ module.exports = {
 
     return {
       send: options => {
-        return new Promise((resolve, reject) => {
-          // Default values.
-          options = _.isObject(options) ? options : {};
-          options.from = options.from || settings.defaultFrom;
-          options.replyTo = options.replyTo || settings.defaultReplyTo;
-          options.text = options.text || options.html;
-          options.html = options.html || options.text;
+        // Default values.
+        options = _.isObject(options) ? options : {};
+        options.from = options.from || settings.defaultFrom;
+        options.replyTo = options.replyTo || settings.defaultReplyTo;
+        options.text = options.text || options.html;
+        options.html = options.html || options.text;
 
-          const msg = [
-            'from',
-            'replyTo',
-            'to',
-            'cc',
-            'bcc',
-            'subject',
-            'text',
-            'html',
-            'attachments',
-          ];
-
-          transporter
-            .sendMail(_.pick(options, msg))
-            .then(resolve)
-            .catch(error => reject(error));
-        });
+        return transporter.sendMail(_.pick(options, emailFields));
       },
     };
   },
