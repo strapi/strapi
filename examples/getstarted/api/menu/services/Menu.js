aad1c2fa032a76932a27729345501827bd05a41b
DMehaffy <derrickmehaffy@gmail.com>
2020-12-18 07:50:37 -0700

    Cleaning up codebase for doc repo move (#8892)
    * [WIP] Cleaning up codebase for doc repo move
    
    WIP
    
    * Fix deleted files, add links to doc repo issues
    
    * Add repo note to readme

--- examples/getstarted/api/menu/services/Menu.js
+++ examples/getstarted/api/menu/services/Menu.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
 
