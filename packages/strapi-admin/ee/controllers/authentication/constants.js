4b6aa0db9198c95defaf0d625633b01c2678da0e
Jean-Sébastien Herbaux <jean-sebastien.herbaux@epitech.eu>
2020-12-23 15:32:42 +0100

    SSO Adjustement (server-side) (#8983)
    * Use dynamic admin url for redirections, set sso method as GET instead of POST
    
    * Fix unit tests

--- packages/strapi-admin/ee/controllers/authentication/constants.js
+++ packages/strapi-admin/ee/controllers/authentication/constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PROVIDER_REDIRECT_BASE = '/admin/auth/providers';
+const PROVIDER_REDIRECT_BASE = '/auth/providers';
 const PROVIDER_REDIRECT_SUCCESS = `${PROVIDER_REDIRECT_BASE}/success`;
 const PROVIDER_REDIRECT_ERROR = `${PROVIDER_REDIRECT_BASE}/error`;
 
--- packages/strapi-admin/ee/controllers/authentication/middlewares.js
+++ packages/strapi-admin/ee/controllers/authentication/middlewares.js
@@ -2,7 +2,6 @@
 
 const passport = require('koa-passport');
 
-const constants = require('./constants');
 const utils = require('./utils');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -11,6 +10,7 @@ const authenticate = async (ctx, next) => {
   const {
     params: { provider },
   } = ctx;
+  const redirectUrls = utils.getPrefixedRedirectUrls();
 
   return passport.authenticate(provider, null, async (error, profile) => {
     if (error || !profile) {
@@ -18,7 +18,7 @@ const authenticate = async (ctx, next) => {
         strapi.log.error(error);
       }
 
-      return ctx.redirect(constants.PROVIDER_REDIRECT_ERROR);
+      return ctx.redirect(redirectUrls.error);
     }
 
     const { email, firstname, lastname, username } = profile;
@@ -37,14 +37,14 @@ const authenticate = async (ctx, next) => {
     const isMissingRegisterFields = !username && (!firstname || !lastname);
 
     if (!providers.autoRegister || !providers.defaultRole || isMissingRegisterFields) {
-      return ctx.redirect(constants.PROVIDER_REDIRECT_ERROR);
+      return ctx.redirect(redirectUrls.error);
     }
 
     const defaultRole = await strapi.admin.services.role.findOne({ id: providers.defaultRole });
 
     // If the default role has been misconfigured, redirect with an error
     if (!defaultRole) {
-      return ctx.redirect(constants.PROVIDER_REDIRECT_ERROR);
+      return ctx.redirect(redirectUrls.error);
     }
 
     // Register a new user with the information given by the provider and login with it
@@ -66,9 +66,10 @@ const redirectWithAuth = ctx => {
   const { user } = ctx.state;
   const jwt = strapi.admin.services.token.createJwtToken(user);
   const cookiesOptions = { httpOnly: false, secure: isProduction, overwrite: true };
+  const redirectUrls = utils.getPrefixedRedirectUrls();
 
   ctx.cookies.set('jwtToken', jwt, cookiesOptions);
-  ctx.redirect(constants.PROVIDER_REDIRECT_SUCCESS);
+  ctx.redirect(redirectUrls.success);
 };
 
 module.exports = {
