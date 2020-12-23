a61f81d3c15b3181ba508acfd45092bbe79210eb
Convly <jean-sebastien.herbaux@epitech.eu>
2020-12-23 14:00:26 +0100

    Use dynamic admin url for redirections, set sso method as GET instead of POST

--- packages/strapi-admin/ee/controllers/authentication/utils.js
+++ packages/strapi-admin/ee/controllers/authentication/utils.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const { mapValues } = require('lodash/fp');
+const { PROVIDER_REDIRECT_ERROR, PROVIDER_REDIRECT_SUCCESS } = require('./constants');
+
 const getAdminStore = async () => strapi.store({ type: 'core', environment: '', name: 'admin' });
+
+const getPrefixedRedirectUrls = () => {
+  return mapValues(url => `${strapi.config.admin.path}${url}`, {
+    success: PROVIDER_REDIRECT_SUCCESS,
+    error: PROVIDER_REDIRECT_ERROR,
+  });
+};
 
 module.exports = {
   getAdminStore,
+  getPrefixedRedirectUrls,
 };
