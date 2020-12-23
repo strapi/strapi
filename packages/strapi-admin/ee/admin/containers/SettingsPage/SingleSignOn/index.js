4000243107184bda2e2ad400f58cee7cbb9e5dd4
soupette <cyril.lpz@gmail.com>
2020-12-23 19:59:50 +0100

    Created sso settings page
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/ee/admin/containers/SettingsPage/SingleSignOn/index.js
+++ packages/strapi-admin/ee/admin/containers/SettingsPage/SingleSignOn/index.js
@@ -1,121 +1,26 @@
-import React, {
-  memo,
-  // useCallback,
-  // useEffect,
-  useMemo,
-  // useReducer, useRef
-} from 'react';
-import {
-  BaselineAlignment,
-  SizedInput,
-  useGlobalContext,
-  // request
-} from 'strapi-helper-plugin';
-import {
-  // checkFormValidity,
-  getRequestUrl,
-} from '../../../../../admin/src/utils';
+import React, { memo, useMemo } from 'react';
+import { BaselineAlignment, SizedInput } from 'strapi-helper-plugin';
+import { useIntl } from 'react-intl';
+import { getRequestUrl } from '../../../../../admin/src/utils';
 import PageTitle from '../../../../../admin/src/components/SettingsPageTitle';
 import ContainerFluid from '../../../../../admin/src/components/ContainerFluid';
 import FormBloc from '../../../../../admin/src/components/FormBloc';
 import { Header } from '../../../../../admin/src/components/Settings';
-import { useRolesList, useUsersForm } from '../../../../../admin/src/hooks';
+import { useRolesList, useUsersForm as useForm } from '../../../../../admin/src/hooks';
 import { form, schema } from './utils';
-// import reducer, { initialState } from './reducer';
 
 const SingleSignOn = () => {
-  const { formatMessage } = useGlobalContext();
-  // const [
-  //   { formErrors, isLoading, initialData, modifiedData, showHeaderButtonLoader },
-  //   dispatch,
-  // ] = useReducer(reducer, initialState);
+  const { formatMessage } = useIntl();
   const [
     { formErrors, initialData, isLoading, modifiedData, showHeaderButtonLoader },
     // eslint-disable-next-line no-unused-vars
     dispatch,
     { handleCancel, handleChange, handleSubmit },
-  ] = useUsersForm(getRequestUrl('providers/options'), schema, () => {}, [
+  ] = useForm(getRequestUrl('providers/options'), schema, () => {}, [
     'autoRegister',
     'defaultRole',
   ]);
   const { roles, isLoading: isLoadingForRoles } = useRolesList();
-
-  // useEffect(() => {
-  //   const abortController = new AbortController();
-  //   const { signal } = abortController;
-
-  //   const getData = async signal => {
-  //     try {
-  //       dispatch({ type: 'GET_DATA' });
-  //       const data = await request(getRequestUrl('providers/options'), {
-  //         method: 'GET',
-  //         signal,
-  //       });
-
-  //       dispatch({
-  //         type: 'GET_DATA_SUCCEEDED',
-  //         data,
-  //       });
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getData(signal);
-
-  //   return () => {
-  //     abortController.abort();
-  //   };
-  // }, []);
-
-  // const handleCancel = useCallback(() => {
-  //   dispatch({ type: 'ON_CANCEL' });
-  // }, []);
-
-  // const handleChange = useCallback(({ target: { name, value } }) => {
-  //   dispatch({
-  //     type: 'ON_CHANGE',
-  //     keys: name,
-  //     value,
-  //   });
-  // }, []);
-
-  // const handleSubmit = useCallback(
-  //   async e => {
-  //     e.preventDefault();
-  //     const errors = await checkFormValidity(modifiedData, schema);
-
-  //     dispatch({
-  //       type: 'SET_ERRORS',
-  //       errors: errors || {},
-  //     });
-
-  //     if (!errors) {
-  //       try {
-  //         strapi.lockAppWithOverlay();
-  //         dispatch({ type: 'ON_SUBMIT' });
-
-  //         const data = await request(getRequestUrl('providers/options'), {
-  //           method: 'PUT',
-  //           body: modifiedData,
-  //         });
-
-  //         dispatch({
-  //           type: 'ON_SUBMIT_SUCCEEDED',
-  //           data,
-  //         });
-
-  //         strapi.notification.toggle({
-  //           type: 'success',
-  //           message: { id: 'notification.success.saved' },
-  //         });
-  //       } catch (err) {
-  //         console.error(err);
-  //       }
-  //     }
-  //   },
-  //   [modifiedData]
-  // );
 
   const showLoader = useMemo(() => isLoadingForRoles || isLoading, [isLoading, isLoadingForRoles]);
 
