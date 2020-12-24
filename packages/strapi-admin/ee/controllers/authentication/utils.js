2f92676c3325ea552844fdba37849e9bb21e4e2a
HichamELBSI <elabbassih@gmail.com>
2020-12-24 15:21:48 +0100

    Fix react-tooltip install
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- packages/strapi-admin/ee/controllers/authentication/utils.js
+++ packages/strapi-admin/ee/controllers/authentication/utils.js
@@ -6,8 +6,7 @@ const { PROVIDER_REDIRECT_ERROR, PROVIDER_REDIRECT_SUCCESS } = require('./consta
 const getAdminStore = async () => strapi.store({ type: 'core', environment: '', name: 'admin' });
 
 const getPrefixedRedirectUrls = () => {
-  // TODO : Remove before merge
-  return mapValues(url => `http://localhost:4000${strapi.config.admin.path}${url}`, {
+  return mapValues(url => `${strapi.config.admin.path}${url}`, {
     success: PROVIDER_REDIRECT_SUCCESS,
     error: PROVIDER_REDIRECT_ERROR,
   });
