4bd04e12a0fe69df7c929a048eb2190a1f84bc41
soupette <cyril.lpz@gmail.com>
2020-12-24 09:14:23 +0100

    split permissions between ee and ce
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/strapi-admin/ee/admin/permissions/customPermissions.js
@@ -0,0 +1,9 @@
+const customPermissions = {
+  sso: {
+    main: [{ action: 'admin::provider-login.read', subject: null }],
+    read: [{ action: 'admin::provider-login.read', subject: null }],
+    update: [{ action: 'admin::provider-login.update', subject: null }],
+  },
+};
+
+export default customPermissions;
