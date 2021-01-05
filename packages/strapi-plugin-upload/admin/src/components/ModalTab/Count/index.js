250828b4a851fa01fce16207c3b249a0d3964013
HichamELBSI <elabbassih@gmail.com>
2021-01-05 17:40:26 +0100

    Remove duplicate components from Upload plugin
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- packages/strapi-plugin-upload/admin/src/components/ModalTab/Count/index.js
+++ packages/strapi-plugin-upload/admin/src/components/ModalTab/Count/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Text } from '@buffetjs/core';
 
-import Text from '../../Text';
 import Wrapper from './Wrapper';
 
 const Count = ({ count, isActive }) => (
