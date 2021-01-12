81b32c5c8fd8be1b0285bcb95847f645a1d9c61d
TonyDeplanque <deplanque.tony@gmail.com>
2021-01-12 10:50:43 +0100

    import model components from plugins

--- packages/strapi/lib/core/load-components.js
+++ packages/strapi/lib/core/load-components.js
@@ -4,10 +4,48 @@ const { join } = require('path');
 const _ = require('lodash');
 const { exists } = require('fs-extra');
 const loadFiles = require('../load/load-files');
+const findPackagePath = require('../load/package-path');
 
-module.exports = async strapi => {
-  const componentsDir = join(strapi.dir, 'components');
+module.exports = async ({ dir, config }) => {
+  const localComponents = await loadLocalComponents({ dir, config });
+  const components = await loadExternalComponents({
+    installedPlugins: config.installedPlugins,
+    config,
+  });
 
+  const componentsIntersection = _.intersection(Object.keys(loadExternalComponents), Object.keys(components));
+
+  if (components.length > 0) {
+    throw new Error(
+      `You have some local components with the same name as npm installed plugins:\n${componentsIntersection
+        .map(p => `- ${p}`)
+        .join('\n')}`
+    );
+  }
+
+  // check for conflicts
+  return _.merge(components, localComponents);
+};
+
+const loadLocalComponents = async ({ dir }) => {
+  const componentsDir = join(dir, 'components');
+  return loadComponents(componentsDir)
+};
+
+const loadExternalComponents = async ({ installedPlugins }) => {
+  let components = {};
+
+  for (let plugin of installedPlugins) {
+    const pluginPath = findPackagePath(`strapi-plugin-${plugin}`);
+    const componentsDir = join(pluginPath, 'components');
+    const component = await loadComponents(componentsDir);
+    _.merge(components,  component);
+  }
+
+  return components;
+};
+
+async function loadComponents(componentsDir) {
   if (!(await exists(componentsDir))) {
     return {};
   }
@@ -40,4 +78,4 @@ module.exports = async strapi => {
 
     return acc;
   }, {});
-};
+}
