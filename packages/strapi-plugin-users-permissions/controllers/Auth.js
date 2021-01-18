b1d171dd3fb7e75fadadf1cdde4592e3ac63720f
Bobby Glidwell <sintex+github@gmail.com>
2021-01-18 12:35:17 -0600

    match Auth return to /users/me
    only resolving 'role' relationship

--- packages/strapi-plugin-users-permissions/controllers/Auth.js
+++ packages/strapi-plugin-users-permissions/controllers/Auth.js
@@ -68,7 +68,7 @@ module.exports = {
       }
 
       // Check if the user exists.
-      const user = await strapi.query('user', 'users-permissions').findOne(query);
+      const user = await strapi.query('user', 'users-permissions').findOne(query, ['role']);
 
       if (!user) {
         return ctx.badRequest(
@@ -461,7 +461,7 @@ module.exports = {
 
     const user = await strapi.query('user', 'users-permissions').findOne({
       email: params.email,
-    });
+    }, ['role']);
 
     if (user && user.provider === params.provider) {
       return ctx.badRequest(
@@ -577,7 +577,7 @@ module.exports = {
 
     const user = await strapi.query('user', 'users-permissions').findOne({
       email: params.email,
-    });
+    }, []);
 
     if (user.confirmed) {
       return ctx.badRequest('already.confirmed');
--- packages/strapi-plugin-users-permissions/controllers/User.js
+++ packages/strapi-plugin-users-permissions/controllers/User.js
@@ -120,11 +120,6 @@ module.exports = {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
 
-    const { id } = user;
-    let data = await strapi.plugins['users-permissions'].services.user.fetch({
-      id,
-    });
-
-    ctx.body = sanitizeUser(data);
+    ctx.body = sanitizeUser(user);
   },
 };
