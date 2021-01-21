2921e3a27a03ee271d8c761632f2d77a638b7ba8
Pierre Noël <petersg83@gmail.com>
2021-01-21 18:14:35 +0100

    move plugins and providers in folder plugins

---
+++ packages/plugins/strapi-plugin-content-manager/admin/src/containers/WysiwygProvider/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import WysiwygContext from '../../contexts/Wysiwyg';
+
+function WysiwygProvider({ children, ...rest }) {
+  return <WysiwygContext.Provider value={rest}>{children}</WysiwygContext.Provider>;
+}
+
+WysiwygProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default WysiwygProvider;
