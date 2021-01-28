4fe9117229b7066c9e4dd34c4e8c1bab6fcc532d
Rémi de Juvigny <remi@hey.com>
2021-01-28 16:47:35 +0100

    Restore transaction name

--- packages/strapi-plugin-sentry/config/functions/bootstrap.js
+++ packages/strapi-plugin-sentry/config/functions/bootstrap.js
@@ -14,9 +14,12 @@ module.exports = async () => {
         scope.addEventProcessor(event => {
           // Parse Koa context to add error metadata
           return sentryInstance.Handlers.parseRequest(event, ctx.request, {
+            // Don't parse the transaction name, we'll do it manually
             transaction: false,
           });
         });
+        // Manually add transaction name
+        scope.setTag('transaction', `${ctx.method} ${ctx.request.url}`);
         // Manually add Strapi version
         scope.setTag('strapi_version', strapi.config.info.strapi);
         scope.setTag('method', ctx.method);
