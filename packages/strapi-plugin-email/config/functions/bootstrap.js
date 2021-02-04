a3b1699e4905008e801b4f3a0530f18237479b6d
MattieBelt <mattiasvandenbelt@gmail.com>
2021-02-04 23:44:43 +0100

    Update email admin permissions
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>

--- packages/strapi-plugin-email/config/functions/bootstrap.js
+++ packages/strapi-plugin-email/config/functions/bootstrap.js
@@ -27,6 +27,7 @@ module.exports = async () => {
       displayName: 'Read',
       uid: 'settings.read',
       subCategory: 'settings',
+      pluginName: 'email',
     },
     {
       section: 'settings',
@@ -34,6 +35,7 @@ module.exports = async () => {
       displayName: 'Send test email',
       uid: 'settings.test',
       subCategory: 'settings',
+      pluginName: 'email',
     },
   ];
 
