78fa5374a5a48da7b527e888957961db880449d4
soupette <cyril.lpz@gmail.com>
2021-02-15 13:31:46 +0100

    Fix PR feedback
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-admin/ee/admin/containers/Roles/CreatePage/ContentTypeCollapses/index.js
+++ packages/strapi-admin/ee/admin/containers/Roles/CreatePage/ContentTypeCollapses/index.js
@@ -1,18 +1,15 @@
-import React, { memo, useCallback, useState } from 'react';
+import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import ContentTypeCollapse from '../ContentTypeCollapse';
 
 const ContentTypeCollapses = ({ actions, pathToData, subjects }) => {
   const [collapseToOpen, setCollapseToOpen] = useState(null);
 
-  const handleClickToggleCollapse = useCallback(
-    collapseName => {
-      const nextCollapseToOpen = collapseToOpen === collapseName ? null : collapseName;
+  const handleClickToggleCollapse = collapseName => {
+    const nextCollapseToOpen = collapseToOpen === collapseName ? null : collapseName;
 
-      setCollapseToOpen(nextCollapseToOpen);
-    },
-    [collapseToOpen]
-  );
+    setCollapseToOpen(nextCollapseToOpen);
+  };
 
   return subjects.map(({ uid, label, properties }, index) => {
     return (
