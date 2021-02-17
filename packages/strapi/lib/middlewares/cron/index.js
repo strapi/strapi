5eef9b1ed026fe6be9a03e985edfa8c309d9c869
boazpoolman <boaz_skater@hotmail.com>
2021-02-17 23:32:54 +0100

    Re-use code for task scheduling & use lodash.get to prevent undefined errors

--- packages/strapi/lib/middlewares/cron/index.js
+++ packages/strapi/lib/middlewares/cron/index.js
@@ -19,43 +19,33 @@ module.exports = strapi => {
      */
 
     initialize() {
+      const scheduleTask = (taskExpression, taskValue) => {
+        if (_.isFunction(taskValue)) {
+          return cron.scheduleJob(taskExpression, taskValue);
+        }
+
+        const options = _.get(taskValue, 'options', {});
+
+        cron.scheduleJob(
+          {
+            rule: taskExpression,
+            ...options,
+          },
+          taskValue.task,
+        );
+      };
+
       if (strapi.config.get('server.cron.enabled', false) === true) {
-        _.forEach(_.keys(strapi.config.get('functions.cron', {})), taskExpression => {
-          const taskValue = strapi.config.functions.cron[taskExpression];
-
-          if (_.isFunction(taskValue)) {
-            return cron.scheduleJob(taskExpression, taskValue);
-          }
-
-          const options = _.get(taskValue, 'options', {});
-
-          cron.scheduleJob(
-            {
-              rule: taskExpression,
-              ...options,
-            },
-            taskValue.task
-          );
+        _.forEach(_.entries(strapi.config.get('functions.cron', {})), ([taskExpression, taskValue]) => {
+          scheduleTask(taskExpression, taskValue);
         });
 
         _.forEach(_.keys(strapi.plugins), pluginName => {
-          const pluginCron = strapi.plugins[pluginName].config.functions.cron;
+          const pluginCron = _.get(strapi.plugins[pluginName], ['config', 'functions', 'cron']);
 
           if (pluginCron) {
             _.forEach(_.entries(pluginCron), ([taskExpression, taskValue]) => {
-              if (_.isFunction(taskValue)) {
-                return cron.scheduleJob(taskExpression, taskValue);
-              }
-
-              const options = _.get(taskValue, 'options', {});
-
-              cron.scheduleJob(
-                {
-                  rule: taskExpression,
-                  ...options,
-                },
-                taskValue.task
-              );
+              scheduleTask(taskExpression, taskValue);
             });
           }
         });
@@ -63,3 +53,4 @@ module.exports = strapi => {
     },
   };
 };
+
