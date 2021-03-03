4cb3fcce372e45c52b235b500726e1e85b545363
mfrachet <marvin.frachet@strapi.io>
2021-03-03 09:16:21 +0100

    Testing the reducer

--- packages/strapi-plugin-i18n/admin/src/index.js
+++ packages/strapi-plugin-i18n/admin/src/index.js
@@ -12,8 +12,8 @@ import CheckboxConfirmation from './components/CheckboxConfirmation';
 import SettingsPage from './containers/SettingsPage';
 import mutateCTBContentTypeSchema from './utils/mutateCTBContentTypeSchema';
 import LOCALIZED_FIELDS from './utils/localizedFields';
-import Initializer from './containers/Initializer'
-import i18nReducers from './reducers'
+import Initializer from './containers/Initializer';
+import i18nReducers from './hooks/reducers';
 
 export default strapi => {
   const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
