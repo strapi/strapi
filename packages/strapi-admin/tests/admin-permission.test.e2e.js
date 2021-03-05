8aaf797a1c79dee1ad40e8196129bf5e4bb6403a
Mattias van den Belt <mattias@taskhero.nl>
2021-03-05 10:37:33 +0100

    Add email test in plugin settings (#8156)
    * Add email test route and controller
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Add email settings page
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Add email Settings Container
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Remove unused getProviderConfig email service
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Add email (disabled) config fields
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings config form
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings container
    
    Move Test button
    Add testEmail input
    Update config fields
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings form
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email plugin settings
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email plugin docs
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings page
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings permissions
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update settings container form
    
    * Update mail text
    * Fix alignment
    * Add yup validation
    * Update form submission
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Fix e2e test
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Fix e2e test
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Fix Baseline
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email plugin docs
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update (temp) BaselineAlignment component
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update text and button styles and placement
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email routes and permissions
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email controller and service
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email admin permissions
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update test permissions snapshot
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email admin permissions
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update all test snapshot
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update email settings permissions
    
    Signed-off-by: MattieBelt <mattiasvandenbelt@gmail.com>
    
    * Update test snapshots
    
    * Fix text width
    
    * Update styling, baseline, and docs link
    
    Co-authored-by: Alexandre BODIN <alexandrebodin@users.noreply.github.com>

--- packages/strapi-admin/tests/admin-permission.test.e2e.js
+++ packages/strapi-admin/tests/admin-permission.test.e2e.js
@@ -312,6 +312,12 @@ describe('Role CRUD End to End', () => {
                 "subCategory": "general",
               },
               Object {
+                "action": "plugins::email.settings.read",
+                "category": "email",
+                "displayName": "Access the Email Settings page",
+                "subCategory": "general",
+              },
+              Object {
                 "action": "plugins::upload.settings.read",
                 "category": "media library",
                 "displayName": "Access the Media Library settings page",
