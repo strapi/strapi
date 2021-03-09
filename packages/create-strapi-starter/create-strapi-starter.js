d8135934bb78e11b87195dc41d5f101025f9aeb2
Mark Kaylor <mark.kaylor@strapi.io>
2021-03-09 10:17:40 +0100

    Simplify cli argument errors

--- packages/create-strapi-starter/create-strapi-starter.js
+++ packages/create-strapi-starter/create-strapi-starter.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const chalk = require('chalk');
 const commander = require('commander');
-const PrettyError = require('pretty-error');
-
-const pe = new PrettyError();
 
 const packageJson = require('./package.json');
 const buildStarter = require('./utils/build-starter');
@@ -34,46 +30,6 @@ program
 
     buildStarter(projectArgs, program);
   });
-
-// Get all possible options
-const options = [];
-program.options.forEach(option => {
-  options.push(option.long);
-  if (option.short) {
-    options.push(option.short);
-  }
-});
-
-// Filter options out of argument check
-const args = process.argv.slice(2);
-const filteredArgs = args.filter(arg => !options.includes(arg) && !arg.startsWith('-'));
-
-// Check correct number of arguments
-if (filteredArgs.length !== 2) {
-  console.log();
-  console.log(
-    pe.render(`You provided ${chalk.red(filteredArgs.length)} ${chalk.white('argument(s)')}`)
-  );
-  console.log(
-    `\t ${chalk.white('A starter requires')} ${chalk.green('2')} ${chalk.white('arguments')}`
-  );
-  console.log(
-    `\t ${chalk.white('1. The')} ${chalk.yellow('directory')} ${chalk.white(
-      'name for your project (i.e. my-project)'
-    )}`
-  );
-  console.log(
-    `\t ${chalk.white('2. The')} ${chalk.yellow(
-      'GitHub url or shortcut (i.e. gatsby-corporate)'
-    )} ${chalk.white('for the starter')}`
-  );
-  console.log();
-}
-
-program.configureOutput({
-  // Highlight errors in color.
-  outputError: (str, write) => write(pe.render(str)),
-});
 
 program.exitOverride();
 
