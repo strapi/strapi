c86ebd7b1478af9839a2923ead5048810613ce53
Marvin Frachet <marvin.frachet@strapi.io>
2021-03-30 16:59:37 +0200

    Fix bullet position in delete content for a locale modal (#9889)

--- packages/strapi-helper-plugin/lib/src/components/InjectionZone/InjectionZoneList.js
+++ packages/strapi-helper-plugin/lib/src/components/InjectionZone/InjectionZoneList.js
@@ -3,11 +3,22 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import useInjectionZone from './useInjectionZone';
 
+const List = styled.ul`
+  list-style-type: none;
+`;
+
 const ListItem = styled.li`
   font-size: ${p => p.theme.main.sizes.fonts.md};
 
-  &::marker {
-    color: ${p => p.theme.main.colors.grey};
+  &:before {
+    background: ${p => p.theme.main.colors.grey};
+    width: 4px;
+    height: 4px;
+    content: '';
+    position: absolute;
+    border-radius: 50%;
+    margin-left: -${p => p.theme.main.sizes.margins.sm};
+    margin-top: 8px;
   }
 `;
 
@@ -19,13 +30,13 @@ const InjectionZoneList = ({ area, ...props }) => {
   }
 
   return (
-    <ul>
+    <List>
       {compos.map(compo => (
         <ListItem key={compo.name}>
           <compo.Component {...props} />
         </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
