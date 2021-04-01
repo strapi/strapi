dbd93a9729e22d7b07dc3c5ce5e9fc7ea4c136d1
soupette <cyril.lpz@gmail.com>
2021-04-01 16:18:36 +0200

    Fix locale picker
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-plugin-i18n/admin/src/hooks/useContentTypePermissions/index.js
+++ packages/strapi-plugin-i18n/admin/src/hooks/useContentTypePermissions/index.js
@@ -5,8 +5,10 @@ const useContentTypePermissions = slug => {
   const collectionTypesRelatedPermissions = useSelector(selectCollectionTypesRelatedPermissions);
 
   const currentCTRelatedPermissions = collectionTypesRelatedPermissions[slug];
-  const readPermissions = currentCTRelatedPermissions['plugins::content-manager.explorer.read'];
-  const createPermissions = currentCTRelatedPermissions['plugins::content-manager.explorer.create'];
+  const readPermissions =
+    currentCTRelatedPermissions['plugins::content-manager.explorer.read'] || [];
+  const createPermissions =
+    currentCTRelatedPermissions['plugins::content-manager.explorer.create'] || [];
 
   return { createPermissions, readPermissions };
 };
