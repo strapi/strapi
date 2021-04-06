e45ac1e39637c89fed6fcdede82f479e3a2a37a3
soupette <cyril.lpz@gmail.com>
2021-04-06 09:50:05 +0200

    Fix PR feedback
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-helper-plugin/lib/src/components/ErrorFallback/index.js
+++ packages/strapi-helper-plugin/lib/src/components/ErrorFallback/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // https://github.com/bvaughn/react-error-boundary#usage
 
-function ErrorFallback({ error /* resetErrorBoundary */ }) {
+function ErrorFallback({ error }) {
   return (
     <div style={{ background: '#ffff' }}>
       <h2>Something went wrong.</h2>
@@ -18,12 +18,10 @@ function ErrorFallback({ error /* resetErrorBoundary */ }) {
 
 ErrorFallback.defaultProps = {
   error: { message: null },
-  // resetErrorBoundary: PropTypes.func,
 };
 
 ErrorFallback.propTypes = {
   error: PropTypes.shape({ message: PropTypes.string }),
-  // resetErrorBoundary: PropTypes.func,
 };
 
 export default ErrorFallback;
