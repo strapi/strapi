5f08fa37e7c0ea45f30a808594c0446dda528143
Tom Dyson <tom@torchbox.com>
2021-04-26 16:08:09 +0100

    Fix typo in query error message (#10116)
    The possessive `its` doesn't have an apostrophe.
    
    https://www.dictionary.com/e/its-vs-its/

--- packages/strapi-utils/lib/build-query.js
+++ packages/strapi-utils/lib/build-query.js
@@ -35,7 +35,7 @@ const getAssociationFromFieldKey = ({ model, field }) => {
 
     if (!assoc && (!isAttribute(tmpModel, part) || i !== fieldParts.length - 1)) {
       const err = new Error(
-        `Your filters contain a field '${field}' that doesn't appear on your model definition nor it's relations`
+        `Your filters contain a field '${field}' that doesn't appear on your model definition nor its relations`
       );
 
       err.status = 400;
