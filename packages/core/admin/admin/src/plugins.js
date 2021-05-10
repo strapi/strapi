0f78b38eb346650f5e4aa13c5b6c6dda86d4b6ed
soupette <cyril.lpz@gmail.com>
2021-05-10 14:12:40 +0200

    Load graphql plugin
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/core/admin/admin/src/plugins.js
+++ packages/core/admin/admin/src/plugins.js
@@ -1,9 +1,11 @@
 // TODO temp file
 import documentationPlugin from '../../../../plugins/documentation/admin/src';
+import graphqlPlugin from '../../../../plugins/graphql/admin/src';
 import usersPermissionsPlugin from '../../../../plugins/users-permissions/admin/src';
 
 const plugins = {
   '@strapi/plugin-documentation': documentationPlugin,
+  '@strapi/plugin-graphql': graphqlPlugin,
   '@strapi/plugin-users-permissions': usersPermissionsPlugin,
 };
 
