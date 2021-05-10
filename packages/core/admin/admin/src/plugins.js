ec7baf3bb36a491037cd935c453cd7ff992b8b90
soupette <cyril.lpz@gmail.com>
2021-05-10 14:15:09 +0200

    Load sentry plugin
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/core/admin/admin/src/plugins.js
+++ packages/core/admin/admin/src/plugins.js
@@ -1,11 +1,13 @@
 // TODO temp file
 import documentationPlugin from '../../../../plugins/documentation/admin/src';
 import graphqlPlugin from '../../../../plugins/graphql/admin/src';
+import sentryPlugin from '../../../../plugins/sentry/admin/src';
 import usersPermissionsPlugin from '../../../../plugins/users-permissions/admin/src';
 
 const plugins = {
   '@strapi/plugin-documentation': documentationPlugin,
   '@strapi/plugin-graphql': graphqlPlugin,
+  '@strapi/plugin-sentry': sentryPlugin,
   '@strapi/plugin-users-permissions': usersPermissionsPlugin,
 };
 
