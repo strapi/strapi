c805387527ba191bc3d9c1c06319a55e621d797e
soupette <cyril.lpz@gmail.com>
2021-05-10 14:18:30 +0200

    Load plugin email
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/core/admin/admin/src/plugins.js
+++ packages/core/admin/admin/src/plugins.js
@@ -3,9 +3,11 @@ import documentationPlugin from '../../../../plugins/documentation/admin/src';
 import graphqlPlugin from '../../../../plugins/graphql/admin/src';
 import sentryPlugin from '../../../../plugins/sentry/admin/src';
 import usersPermissionsPlugin from '../../../../plugins/users-permissions/admin/src';
+import emailPlugin from '../../../email/admin/src';
 
 const plugins = {
   '@strapi/plugin-documentation': documentationPlugin,
+  '@strapi/plugin-email': emailPlugin,
   '@strapi/plugin-graphql': graphqlPlugin,
   '@strapi/plugin-sentry': sentryPlugin,
   '@strapi/plugin-users-permissions': usersPermissionsPlugin,
