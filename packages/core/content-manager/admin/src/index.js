bc66c8ef872946ce3b3ec61238b6d0948ead0c8b
soupette <cyril.lpz@gmail.com>
2021-06-23 16:04:48 +0200

    Move cm translation files
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/core/content-manager/admin/src/index.js
+++ packages/core/content-manager/admin/src/index.js
@@ -4,7 +4,7 @@
 // Here's the file: strapi/docs/3.0.0-beta.x/guides/registering-a-field-in-admin.md
 // Also the strapi-generate-plugins/files/admin/src/index.js needs to be updated
 // IF THE DOC IS NOT UPDATED THE PULL REQUEST WILL NOT BE MERGED
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
+
 import pluginPkg from '../../package.json';
 import {
   INJECT_COLUMN_IN_TABLE,
@@ -54,27 +54,4 @@ export default {
     });
   },
   boot() {},
-  async registerTrads({ locales }) {
-    const importedTrads = await Promise.all(
-      locales.map(locale => {
-        return import(
-          /* webpackChunkName: "content-manager-translation-[request]" */ `./translations/${locale}.json`
-        )
-          .then(({ default: data }) => {
-            return {
-              data: prefixPluginTranslations(data, pluginId),
-              locale,
-            };
-          })
-          .catch(() => {
-            return {
-              data: {},
-              locale,
-            };
-          });
-      })
-    );
-
-    return Promise.resolve(importedTrads);
-  },
 };
