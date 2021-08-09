d54610dc06f774165b1a6e8e40a0ea59863d3a8d
Alexandre BODIN <alexandrebodin@users.noreply.github.com>
2021-08-09 17:33:02 +0200

    Remove hooks (#10711)

--- packages/core/strapi/lib/load/check-reserved-filename.js
+++ packages/core/strapi/lib/load/check-reserved-filename.js
@@ -3,15 +3,7 @@
 const _ = require('lodash');
 
 // files to load with filename key
-const prefixedPaths = [
-  'functions',
-  'policies',
-  'locales',
-  'hook',
-  'middleware',
-  'language',
-  'layout',
-];
+const prefixedPaths = ['functions', 'policies', 'locales', 'middleware', 'language', 'layout'];
 
 module.exports = function checkReservedFilenames(file) {
   return _.some(prefixedPaths, e => file.indexOf(`config/${e}`) >= 0) ? true : false;
