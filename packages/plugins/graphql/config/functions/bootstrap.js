d8d99262e36bdf214565b2cfa89acfcca551b928
Convly <jean-sebastien.herbaux@epitech.eu>
2021-08-10 17:28:53 +0200

    Update the plugin load

--- packages/plugins/graphql/config/functions/bootstrap.js
+++ packages/plugins/graphql/config/functions/bootstrap.js
@@ -1,74 +1,96 @@
 'use strict';
 
-const _ = require('lodash');
+const { isEmpty, getOr } = require('lodash/fp');
 const { ApolloServer } = require('apollo-server-koa');
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} = require('apollo-server-core');
 const depthLimit = require('graphql-depth-limit');
 const { graphqlUploadKoa } = require('graphql-upload');
 
-module.exports = ({ strapi }) => {
+module.exports = async ({ strapi }) => {
+  // Generate the GraphQL schema for the content API
   const schema = strapi.plugins.graphql.services.schema(strapi).generateContentAPISchema();
 
-  if (_.isEmpty(schema)) {
+  if (isEmpty(schema)) {
     strapi.log.warn('The GraphQL schema has not been generated because it is empty');
 
     return;
   }
 
-  const config = _.get(strapi.plugins.graphql, 'config', {});
-
-  // TODO: Remove these deprecated options in favor of `apolloServer` in the next major version
-  const deprecatedApolloServerConfig = {
-    tracing: _.get(config, 'tracing', false),
-    introspection: _.get(config, 'introspection', true),
-    engine: _.get(config, 'engine', false),
-  };
-
-  if (['tracing', 'introspection', 'engine'].some(key => _.has(config, key))) {
-    strapi.log.warn(
-      'The `tracing`, `introspection` and `engine` options are deprecated in favor of the `apolloServer` object and they will be removed in the next major version.'
-    );
-  }
-
-  const apolloServerConfig = _.get(config, 'apolloServer', {});
+  const config = getOr({}, 'config', strapi.plugins.graphql);
+  const apolloServerConfig = getOr({}, 'apolloServer', config);
 
   const serverParams = {
+    // Schema
     schema,
-    uploads: false,
-    context: ({ ctx }) => {
-      // Initialize loaders for this request.
-      // TODO: set loaders in the context not globally
 
+    // Initialize loaders for this request.
+    context: ({ ctx }) => {
+      // TODO: set loaders in the context not globally
       strapi.plugins.graphql.services.old['data-loaders'].initializeLoader();
 
       return {
         context: ctx,
       };
     },
+
+    // Format & validation
     formatError: err => {
-      const formatError = _.get(config, 'formatError', null);
+      const formatError = getOr(null, 'formatError', config);
 
       return typeof formatError === 'function' ? formatError(err) : err;
     },
     validationRules: [depthLimit(config.depthLimit)],
-    playground: false,
+
+    // Misc
     cors: false,
+    uploads: false,
     bodyParserConfig: true,
-    // TODO: Remove these deprecated options in favor of `apolloServerConfig` in the next major version
-    ...deprecatedApolloServerConfig,
+
+    plugins: [
+      // Specify which GraphQL landing page we want for the different env.
+      process.env.NODE_ENV !== 'production'
+        ? ApolloServerPluginLandingPageLocalDefault({ footer: false })
+        : ApolloServerPluginLandingPageProductionDefault({ footer: false }),
+    ],
     ...apolloServerConfig,
   };
 
-  // Disable GraphQL Playground in production environment.
-  if (strapi.config.environment !== 'production' || config.playgroundAlways) {
-    serverParams.playground = {
-      endpoint: `${strapi.config.server.url}${config.endpoint}`,
-      shareEnabled: config.shareEnabled,
-    };
-  }
-
+  // Create a new Apollo server
   const server = new ApolloServer(serverParams);
 
+  // Register the upload middleware
+  useUploadMiddleware(strapi, config);
+
+  try {
+    // Since Apollo-Server v3, server.start() must be called before using server.applyMiddleware()
+    await server.start();
+  } catch (e) {
+    strapi.log.error('Failed to start the Apollo server', e.message);
+  }
+
+  // Link the Apollo server & the Strapi app
+  server.applyMiddleware({
+    app: strapi.app,
+    path: config.endpoint,
+  });
+
+  // Register destroy behavior
+  strapi.plugins.graphql.destroy = async () => {
+    await server.stop();
+  };
+};
+
+/**
+ * Register the upload middleware powered by graphql-upload in Strapi
+ * @param {object} strapi
+ * @param {object} config
+ */
+const useUploadMiddleware = (strapi, config) => {
   const uploadMiddleware = graphqlUploadKoa();
+
   strapi.app.use((ctx, next) => {
     if (ctx.path === config.endpoint) {
       return uploadMiddleware(ctx, next);
@@ -76,15 +98,4 @@ module.exports = ({ strapi }) => {
 
     return next();
   });
-
-  server.start().then(() => {
-    server.applyMiddleware({
-      app: strapi.app,
-      path: config.endpoint,
-    });
-  });
-
-  strapi.plugins.graphql.destroy = async () => {
-    await server.stop();
-  };
 };
