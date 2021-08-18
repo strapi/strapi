ec85a4adf86031e5e6d4ae4ffcca35022191013f
HichamELBSI <elabbassih@gmail.com>
2021-08-18 17:43:57 +0200

    DS Migration : Auth pages (Forgot/reset password, Oops page)
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- packages/core/admin/admin/src/pages/AuthPage/components/ForgotPassword/index.js
+++ packages/core/admin/admin/src/pages/AuthPage/components/ForgotPassword/index.js
@@ -1,53 +1,75 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { Padded } from '@buffetjs/core';
 import PropTypes from 'prop-types';
-import { BaselineAlignment } from '@strapi/helper-plugin';
-import Button from '../../../../components/FullWidthButton';
-import AuthLink from '../AuthLink';
-import Input from '../Input';
+import { Form } from '@strapi/helper-plugin';
+import { Box, Stack, H1, Text, TextInput, Main, Row, Link } from '@strapi/parts';
+import { Formik } from 'formik';
+import UnauthenticatedLayout, {
+  Column,
+  LayoutContent,
+} from '../../../../layouts/UnauthenticatedLayout';
+import AuthButton from '../AuthButton';
 import Logo from '../Logo';
-import Section from '../Section';
-import Box from '../Box';
 
-const ForgotPassword = ({ formErrors, modifiedData, onChange, onSubmit }) => {
+const ForgotPassword = ({ onSubmit, schema }) => {
   const { formatMessage } = useIntl();
 
   return (
-    <>
-      <Section textAlign="center">
-        <Logo />
-      </Section>
-      <Section withBackground>
-        {/* FIXME IN BUFFET.JS */}
-        <BaselineAlignment top size="20px">
-          <Padded top size="xs">
-            <Box>
-              <form onSubmit={onSubmit}>
-                <Input
-                  autoFocus
-                  error={formErrors.email}
-                  label="Auth.form.email.label"
-                  name="email"
-                  onChange={onChange}
-                  placeholder="Auth.form.email.placeholder"
-                  type="email"
-                  validations={{ required: true }}
-                  value={modifiedData.email}
-                />
-                <BaselineAlignment top size="3px" />
-                <Padded top>
-                  <Button type="submit" color="primary" textTransform="uppercase">
+    <UnauthenticatedLayout>
+      <Main labelledBy="password-forgotten">
+        <LayoutContent>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              email: '',
+            }}
+            onSubmit={onSubmit}
+            validationSchema={schema}
+            validateOnChange={false}
+          >
+            {({ values, errors, handleChange }) => (
+              <Form noValidate>
+                <Column>
+                  <Logo />
+                  <Box paddingTop="6" paddingBottom="7">
+                    <H1 id="password-forgotten">
+                      {formatMessage({ id: 'Auth.form.button.password-forgotten' })}
+                    </H1>
+                  </Box>
+                  {errors.errorMessage && (
+                    <Text id="global-form-error" role="alert" tabIndex={-1} textColor="danger600">
+                      {formatMessage({ id: errors.errorMessage })}
+                    </Text>
+                  )}
+                </Column>
+
+                <Stack size={6}>
+                  <TextInput
+                    error={errors.email ? formatMessage({ id: errors.email }) : ''}
+                    value={values.email}
+                    onChange={handleChange}
+                    label={formatMessage({ id: 'Auth.form.email.label' })}
+                    placeholder={formatMessage({ id: 'Auth.form.email.placeholder' })}
+                    name="email"
+                    required
+                  />
+                  <AuthButton type="submit">
                     {formatMessage({ id: 'Auth.form.button.forgot-password' })}
-                  </Button>
-                </Padded>
-              </form>
-            </Box>
-          </Padded>
-        </BaselineAlignment>
-        <AuthLink label="Auth.link.ready" to="/auth/login" />
-      </Section>
-    </>
+                  </AuthButton>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </LayoutContent>
+        <Row justifyContent="center">
+          <Box paddingTop={4}>
+            <Link to="/auth/login">
+              <Text small>{formatMessage({ id: 'Auth.link.ready' })}</Text>
+            </Link>
+          </Box>
+        </Row>
+      </Main>
+    </UnauthenticatedLayout>
   );
 };
 
@@ -56,10 +78,10 @@ ForgotPassword.defaultProps = {
 };
 
 ForgotPassword.propTypes = {
-  formErrors: PropTypes.object.isRequired,
-  modifiedData: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func,
+  schema: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default ForgotPassword;
--- packages/core/admin/admin/src/pages/AuthPage/components/Register/index.js
+++ packages/core/admin/admin/src/pages/AuthPage/components/Register/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components';
+import get from 'lodash/get';
+import omit from 'lodash/omit';
 import { Show, Hide } from '@strapi/icons';
 import {
   Main,
@@ -15,9 +17,11 @@ import {
   Link,
   Row,
 } from '@strapi/parts';
-import { Form } from '@strapi/helper-plugin';
+import { Form, useQuery, useNotification } from '@strapi/helper-plugin';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
+import axios from 'axios';
 import UnauthenticatedLayout, {
   Column,
   LayoutContent,
@@ -33,10 +37,47 @@ const A = styled.a`
   color: ${({ theme }) => theme.colors.primary600};
 `;
 
-const Register = ({ fieldsToDisable, noSignin, onSubmit, modifiedData, schema }) => {
+const Register = ({ fieldsToDisable, noSignin, onSubmit, schema }) => {
+  const toggleNotification = useNotification();
+  const { push } = useHistory();
   const [passwordShown, setPasswordShown] = useState(false);
   const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
   const { formatMessage } = useIntl();
+  const query = useQuery();
+  const registrationToken = query.get('registrationToken');
+
+  useEffect(() => {
+    if (registrationToken) {
+      const getData = async () => {
+        try {
+          const {
+            data: { data },
+          } = await axios.get(
+            `${strapi.backendURL}/admin/registration-info?registrationToken=${registrationToken}`
+          );
+
+          if (data) {
+            setUserInfo(data);
+          }
+        } catch (err) {
+          const errorMessage = get(err, ['response', 'data', 'message'], 'An error occurred');
+
+          toggleNotification({
+            type: 'warning',
+            message: errorMessage,
+          });
+
+          // Redirect to the oops page in case of an invalid token
+          // @alexandrebodin @JAB I am not sure it is the wanted behavior
+          push(`/auth/oops?info=${encodeURIComponent(errorMessage)}`);
+        }
+      };
+
+      getData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [registrationToken]);
 
   return (
     <UnauthenticatedLayout>
@@ -44,15 +85,22 @@ const Register = ({ fieldsToDisable, noSignin, onSubmit, modifiedData, schema })
         <Formik
           enableReinitialize
           initialValues={{
-            // TODO: Fix the initial data during the clean of the AuthLogin codebase
-            firstname: modifiedData?.userInfo?.firstname || '',
-            lastname: modifiedData?.userInfo?.lastname || '',
-            email: modifiedData?.userInfo?.email || '',
-            password: modifiedData?.userInfo?.password || '',
-            confirmPassword: modifiedData?.userInfo?.confirmPassword || '',
+            firstname: userInfo.firstname || '',
+            lastname: userInfo.lastname || '',
+            email: userInfo.email || '',
+            password: '',
+            confirmPassword: '',
+            registrationToken: registrationToken || undefined,
             news: false,
           }}
-          onSubmit={onSubmit}
+          onSubmit={(data, formik) => {
+            if (registrationToken) {
+              // We need to pass the registration token in the url param to the api in order to submit another admin user
+              onSubmit({ userInfo: omit(data, ['registrationToken']), registrationToken }, formik);
+            } else {
+              onSubmit(data, formik);
+            }
+          }}
           validationSchema={schema}
           validateOnChange={false}
         >
@@ -111,6 +159,7 @@ const Register = ({ fieldsToDisable, noSignin, onSubmit, modifiedData, schema })
                     value={values.password}
                     error={errors.password ? formatMessage({ id: errors.password }) : undefined}
                     endAction={
+                      // eslint-disable-next-line react/jsx-wrap-multilines
                       <FieldActionWrapper
                         onClick={e => {
                           e.preventDefault();
@@ -125,6 +174,7 @@ const Register = ({ fieldsToDisable, noSignin, onSubmit, modifiedData, schema })
                         {passwordShown ? <Show /> : <Hide />}
                       </FieldActionWrapper>
                     }
+                    hint={formatMessage({ id: 'Auth.form.password.hint' })}
                     required
                     label={formatMessage({ id: 'Auth.form.password.label' })}
                     type={passwordShown ? 'text' : 'password'}
@@ -139,6 +189,7 @@ const Register = ({ fieldsToDisable, noSignin, onSubmit, modifiedData, schema })
                         : undefined
                     }
                     endAction={
+                      // eslint-disable-next-line react/jsx-wrap-multilines
                       <FieldActionWrapper
                         onClick={e => {
                           e.preventDefault();
@@ -210,7 +261,6 @@ Register.defaultProps = {
 
 Register.propTypes = {
   fieldsToDisable: PropTypes.array,
-  modifiedData: PropTypes.object.isRequired,
   noSignin: PropTypes.bool,
   onSubmit: PropTypes.func,
   schema: PropTypes.shape({ type: PropTypes.string.isRequired }).isRequired,
