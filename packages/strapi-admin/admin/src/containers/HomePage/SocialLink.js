a46cb75f8139b003161780a7675010928ded2d80
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-08-19 14:26:58 -0700

    Swap out Slack/Discord and cleanup old social links
    Signed-off-by: Derrick Mehaffy <derrickmehaffy@gmail.com>

--- packages/strapi-admin/admin/src/containers/HomePage/SocialLink.js
+++ packages/strapi-admin/admin/src/containers/HomePage/SocialLink.js
@@ -7,11 +7,10 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
 import Gh from '../../assets/images/social_gh.png';
-import Slack from '../../assets/images/social_slack.png';
-import Medium from '../../assets/images/social_medium.png';
+import Discord from '../../assets/images/social_discord.png';
 import Twitter from '../../assets/images/social_twitter.png';
 import Reddit from '../../assets/images/social_reddit.png';
-import Forum from '../../assets/images/social_forum.png';
+import Strapi from '../../assets/images/social_strapi.png';
 
 import { SocialLinkWrapper } from './components';
 
@@ -21,17 +20,16 @@ function getSrc(name) {
       return Gh;
     case 'Reddit':
       return Reddit;
-    case 'Medium':
-      return Medium;
-    case 'Slack':
-      return Slack;
+    case 'Discord':
+      return Discord;
     case 'Twitter':
       return Twitter;
+    case 'Blog':
     case 'Forum':
-    case 'Academy':
-      return Forum;
+    case 'Careers':
+      return Strapi;
     default:
-      return Gh;
+      return Strapi;
   }
 }
 
