3061190203a3c495b67b4d7db9f4b74f2bf4e9f5
soupette <cyril@strapi.io>
2021-08-20 09:20:35 +0200

    Add storybook
    Signed-off-by: soupette <cyril@strapi.io>

---
+++ packages/core/helper-plugin/stories/Header.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Header } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
