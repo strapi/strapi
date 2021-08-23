b1ff616fd59cce291ec2d607519f6d4e3d1afd9e
markkaylor <mark.kaylor@strapi.io>
2021-08-23 16:08:20 +0200

    add generators to cli (#10779)
    * add generators to cli
    
    * add strapi generate comment
    
    * remove old generators
    
    * rename generator-cli -> generate
    
    * list generators alphabetically
    
    * update package description
    
    * add check for strapi app in cwd

--- packages/generators/generate/plopfile.js
+++ packages/generators/generate/plopfile.js
@@ -10,92 +10,6 @@ module.exports = function(plop) {
 
   plop.addHelper('pluralize', text => pluralize(text));
 
-  // Service generator
-  plop.setGenerator('service', {
-    description: 'Generate a service for an API',
-    prompts: [
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Service name',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: join(rootDir, 'api/{{id}}/services/{{id}}.js'),
-        templateFile: 'templates/service.js.hbs',
-      },
-    ],
-  });
-
-  // Model generator
-  plop.setGenerator('model', {
-    description: 'Generate a model for an API',
-    prompts: [
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Model name',
-      },
-      {
-        type: 'confirm',
-        name: 'useDraftAndPublish',
-        message: 'Use draft and publish?',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: join(rootDir, 'api/{{id}}/models/{{id}}.js'),
-        templateFile: 'templates/model.js.hbs',
-      },
-      {
-        type: 'add',
-        path: join(rootDir, 'api/{{id}}/models/{{id}}.settings.json'),
-        templateFile: 'templates/model.settings.json.hbs',
-      },
-    ],
-  });
-
-  // Controller generator
-  plop.setGenerator('controller', {
-    description: 'Generate a controller for an API',
-    prompts: [
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Controller name',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: join(rootDir, 'api/{{id}}/controllers/{{id}}.js'),
-        templateFile: 'templates/controller.js.hbs',
-      },
-    ],
-  });
-
-  // Policy generator
-  plop.setGenerator('policy', {
-    description: 'Generate a policy for an API',
-    prompts: [
-      {
-        type: 'input',
-        name: 'id',
-        message: 'Policy name',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: join(rootDir, 'config/policies/{{id}}.js'),
-        templateFile: 'templates/policy.js.hbs',
-      },
-    ],
-  });
-
   // API generator
   plop.setGenerator('api', {
     description: 'Generate a basic API',
@@ -140,6 +54,54 @@ module.exports = function(plop) {
     ],
   });
 
+  // Controller generator
+  plop.setGenerator('controller', {
+    description: 'Generate a controller for an API',
+    prompts: [
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Controller name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: join(rootDir, 'api/{{id}}/controllers/{{id}}.js'),
+        templateFile: 'templates/controller.js.hbs',
+      },
+    ],
+  });
+
+  // Model generator
+  plop.setGenerator('model', {
+    description: 'Generate a model for an API',
+    prompts: [
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Model name',
+      },
+      {
+        type: 'confirm',
+        name: 'useDraftAndPublish',
+        message: 'Use draft and publish?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: join(rootDir, 'api/{{id}}/models/{{id}}.js'),
+        templateFile: 'templates/model.js.hbs',
+      },
+      {
+        type: 'add',
+        path: join(rootDir, 'api/{{id}}/models/{{id}}.settings.json'),
+        templateFile: 'templates/model.settings.json.hbs',
+      },
+    ],
+  });
+
   // Plugin generator
   plop.setGenerator('plugin', {
     description: 'Generate a basic plugin',
@@ -180,5 +142,43 @@ module.exports = function(plop) {
         },
       ];
     },
+  });
+
+  // Policy generator
+  plop.setGenerator('policy', {
+    description: 'Generate a policy for an API',
+    prompts: [
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Policy name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: join(rootDir, 'config/policies/{{id}}.js'),
+        templateFile: 'templates/policy.js.hbs',
+      },
+    ],
+  });
+
+  // Service generator
+  plop.setGenerator('service', {
+    description: 'Generate a service for an API',
+    prompts: [
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Service name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: join(rootDir, 'api/{{id}}/services/{{id}}.js'),
+        templateFile: 'templates/service.js.hbs',
+      },
+    ],
   });
 };
