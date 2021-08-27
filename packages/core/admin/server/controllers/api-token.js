4b7a1eb48f342112154e876aa81d33edba0b0478
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-08-27 16:32:36 +0200

    trim name and description before validation

--- packages/core/admin/server/controllers/api-token.js
+++ packages/core/admin/server/controllers/api-token.js
@@ -8,6 +8,14 @@ module.exports = {
     const { body: attributes } = ctx.request;
     const apiTokenService = getService('api-token');
 
+    /**
+     * We trim both field to avoid having issues with either:
+     * - having a space at the end or start of the value.
+     * - having only spaces as value;
+     */
+    attributes.name = attributes.name.trim();
+    attributes.description = attributes.description.trim();
+
     try {
       await validateApiTokenCreationInput(attributes);
     } catch (err) {
