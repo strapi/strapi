d4a19e276fd803b10ab8048be96bd8662f975c5e
MatsubaraDaisuke <matsunenekao@gmail.com>
2021-08-31 20:04:33 +0900

    Update index.js

--- packages/strapi-plugin-i18n/admin/src/translations/index.js
+++ packages/strapi-plugin-i18n/admin/src/translations/index.js
@@ -1,10 +1,12 @@
 import en from './en.json';
 import fr from './fr.json';
+import ja from './ja.json';
 import zhHans from './zh-Hans.json';
 
 const trads = {
   en,
   fr,
+  ja,
   'zh-Hans': zhHans,
 };
 
