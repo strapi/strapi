06d7d979e5454ce0b2f421e326e40fa60124eb2a
bulby97 <s.montlouis.calixte@gmail.com>
2021-09-07 13:53:45 +0200

    Bring back LeftMenu

---
+++ packages/core/helper-plugin/lib/src/old/components/LeftMenuHeader/Search.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import colors from '../../assets/styles/colors';
+
+const Search = styled.input`
+  width: 100%;
+  padding: 0 21px;
+  outline: 0;
+  color: ${colors.leftMenu.black};
+`;
+
+export default Search;
