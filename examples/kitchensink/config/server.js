528a56d4e9ee809edfc1e1fc0bdd8c9bbbe46c8d
soupette <cyril@strapi.io>
2021-09-08 10:25:15 +0200

    Add kitchensink app
    Signed-off-by: soupette <cyril@strapi.io>

---
+++ examples/kitchensink/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb07d090b62789dce667b54ae419850e'),
+    },
+  },
+});
