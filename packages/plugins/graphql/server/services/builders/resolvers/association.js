89c1e7e1ce214ef22c855fd47479ced2b12b7508
Convly <jean-sebastien.herbaux@epitech.eu>
2021-09-10 10:23:21 +0200

    Declare a new load method in the entity service

--- packages/plugins/graphql/server/services/builders/resolvers/association.js
+++ packages/plugins/graphql/server/services/builders/resolvers/association.js
@@ -1,15 +1,11 @@
 'use strict';
 
-const { omit } = require('lodash/fp');
-
 module.exports = ({ strapi }) => {
   const { isMorphRelation, isMedia } = strapi.plugin('graphql').service('utils').attributes;
   const { transformArgs } = strapi.plugin('graphql').service('builders').utils;
 
   return {
     buildAssociationResolver: ({ contentTypeUID, attributeName }) => {
-      const { entityManager } = strapi.db;
-
       const contentType = strapi.getModel(contentTypeUID);
       const attribute = contentType.attributes[attributeName];
 
@@ -33,20 +29,11 @@ module.exports = ({ strapi }) => {
           usePagination: true,
         });
 
-        // Since we're using the entity-manager & not the entity-service to load the
-        // association, we need to apply some transformation to the transformed args object
-        const entityManagerArgs = {
-          ...omit(['start', 'filters'], transformedArgs),
-          where: transformedArgs.filters,
-          offset: transformedArgs.start,
-        };
-
-        // todo[v4]: should we move the .load to the entity service so we can use the same args everywhere?
-        const data = await entityManager.load(
+        const data = await strapi.entityService.load(
           contentTypeUID,
           parent,
           attributeName,
-          entityManagerArgs
+          transformedArgs
         );
 
         // If this a polymorphic association, it returns the raw data
--- packages/plugins/graphql/server/services/builders/resolvers/component.js
+++ packages/plugins/graphql/server/services/builders/resolvers/component.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { omit } = require('lodash/fp');
-
 module.exports = ({ strapi }) => ({
   buildComponentResolver: ({ contentTypeUID, attributeName }) => {
     const { transformArgs } = strapi.plugin('graphql').service('builders').utils;
@@ -10,16 +8,7 @@ module.exports = ({ strapi }) => ({
       const contentType = strapi.contentTypes[contentTypeUID];
       const transformedArgs = transformArgs(args, { contentType, usePagination: true });
 
-      // Since we're using the entity-manager & not the entity-service to load the
-      // association, we need to apply some transformation to the transformed args object
-      const entityManagerArgs = {
-        ...omit(['start', 'filters'], transformedArgs),
-        where: transformedArgs.filters,
-        offset: transformedArgs.start,
-      };
-
-      // todo[v4]: should we move the .load to the entity service so we can use the same args everywhere?
-      return strapi.db.entityManager.load(contentTypeUID, parent, attributeName, entityManagerArgs);
+      return strapi.entityService.load(contentTypeUID, parent, attributeName, transformedArgs);
     };
   },
 });
