16a0d64784b9ea9b63ccced35a96d67a1c9665ab
soupette <cyril@strapi.io>
2021-09-15 09:32:27 +0200

    Add toggle list headers
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/admin/src/content-manager/contexts/index.js
+++ packages/core/admin/admin/src/content-manager/contexts/index.js
@@ -1,4 +1,3 @@
 export { default as ContentTypeLayoutContext } from './ContentTypeLayout';
 export { default as LayoutDndContext } from './LayoutDnd';
-export { default as ListViewContext } from './ListView';
 export { default as WysiwygContext } from './Wysiwyg';
