ca7b8b144237ffaec2ed90249027dd18083435c6
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-09-17 09:38:41 +0200

    remove console.log statement

--- packages/plugins/users-permissions/server/strategies/users-permissions.js
+++ packages/plugins/users-permissions/server/strategies/users-permissions.js
@@ -17,7 +17,6 @@ const getAdvancedSettings = () => {
 const authenticate = async ctx => {
   if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
     try {
-      console.log({ ctx });
       const { id } = await getService('jwt').getToken(ctx);
 
       if (id === undefined) {
