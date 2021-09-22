b06ec444aa44525936fd33c29d5ec7ff3d7f92f4
soupette <cyril@strapi.io>
2021-09-22 12:38:12 +0200

    Add kitchensink app
    Signed-off-by: soupette <cyril@strapi.io>

--- examples/kitchensink/src/admin/webpack.config.js
+++ examples/kitchensink/src/admin/webpack.config.js
@@ -5,5 +5,6 @@ module.exports = (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
+  console.log('custom');
   return config;
 };
