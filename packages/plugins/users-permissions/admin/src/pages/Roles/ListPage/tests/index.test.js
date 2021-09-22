9afcf943d45edb86e40ecc8dbc5b62c04733234d
mfrachet <marvin.frachet@strapi.io>
2021-09-22 14:53:36 +0200

    Fix tests

--- packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/index.test.js
+++ packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/index.test.js
@@ -333,6 +333,23 @@ describe('Plugin | Users and Permissions | RoleListPage', () => {
         pointer-events: none;
       }
 
+      .c22 {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        height: 2rem;
+        width: 2rem;
+      }
+
       .c22 svg > g,
       .c22 svg path {
         fill: #8e8ea9;
