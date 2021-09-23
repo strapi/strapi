522dce5b8a3cc7d2a25bb8684c8bdca668cd7f65
soupette <cyril@strapi.io>
2021-09-23 07:37:46 +0200

    Add count remaining files
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/MultipleMedias.js
+++ packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/MultipleMedias.js
@@ -2,12 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { AvatarGroup } from '@strapi/parts/Avatar';
 import Media from './Media';
+import FileWrapper from './Media/FileWrapper';
 
 const MultipleMedia = ({ value }) => {
   return (
     <AvatarGroup>
       {value.map((file, index) => {
         const key = `${file.id}${index}`;
+
+        if (index === 3) {
+          const remainingFiles = `+${value.length - 3}`;
+
+          return (
+            <FileWrapper key={key} preview={false}>
+              {remainingFiles}
+            </FileWrapper>
+          );
+        }
+
+        if (index > 3) {
+          return null;
+        }
 
         return <Media key={key} {...file} />;
       })}
