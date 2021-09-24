14a0ba9ee3ee01fa7699f8f2bf99efb56e24cd97
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-09-24 12:07:31 +0200

    comment the api-token auth strategy

--- packages/core/admin/server/register.js
+++ packages/core/admin/server/register.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const adminAuthStrategy = require('./strategies/admin');
-const apiTokenAuthStrategy = require('./strategies/api-token');
+// const apiTokenAuthStrategy = require('./strategies/api-token');
 
 module.exports = () => {
   const passportMiddleware = strapi.admin.services.passport.init();
 
   strapi.server.api('admin').use(passportMiddleware);
   strapi.container.get('auth').register('admin', adminAuthStrategy);
-  strapi.container.get('auth').register('content-api', apiTokenAuthStrategy);
+  // strapi.container.get('auth').register('content-api', apiTokenAuthStrategy);
 };
