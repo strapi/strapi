bc9bc1c498f8adac4af2ab473751295ac6bb4842
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-09-24 09:44:53 +0200

    remove commented code

--- packages/core/admin/server/policies/isAuthenticatedAdmin.js
+++ packages/core/admin/server/policies/isAuthenticatedAdmin.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// module.exports = (ctx, next) => {
-//   if (!ctx.state.isAuthenticated) {
-//     return ctx.unauthorized();
-//   }
-
-//   return next();
-// };
-
 module.exports = ({ ctx }) => {
   return ctx.state.isAuthenticated;
 };
