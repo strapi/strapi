7cf05bfb10b045e52fe4db93d7526c8ef4170b19
Alexandre Bodin <bodin.alex@gmail.com>
2021-09-24 09:04:44 +0200

    Move CM files into the new structure

--- packages/core/content-manager/config/functions/bootstrap.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { getService } = require('../../utils');
-
-module.exports = async () => {
-  await getService('components').syncConfigurations();
-  await getService('content-types').syncConfigurations();
-  await getService('permission').registerPermissions();
-};
