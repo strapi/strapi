b5e2ddfc3b614a8a0eca82e34cc6e994ee3ec4e3
Pierre Noël <petersg83@gmail.com>
2021-09-24 17:39:09 +0200

    add count feature inside populate field of content-api

--- packages/core/utils/lib/convert-query-params.js
+++ packages/core/utils/lib/convert-query-params.js
@@ -6,6 +6,7 @@
  */
 
 const _ = require('lodash');
+const parseType = require('./parse-type');
 
 const QUERY_OPERATORS = ['_where', '_or', '_and'];
 
@@ -27,6 +28,10 @@ const validateOrder = order => {
   if (!['asc', 'desc'].includes(order.toLocaleLowerCase())) {
     throw new InvalidOrderError();
   }
+};
+
+const convertCountQueryParams = countQuery => {
+  return parseType({ type: 'boolean', value: countQuery });
 };
 
 /**
@@ -161,7 +166,7 @@ const convertNestedPopulate = subPopulate => {
   }
 
   // TODO: We will need to consider a way to add limitation / pagination
-  const { sort, filters, fields, populate } = subPopulate;
+  const { sort, filters, fields, populate, count } = subPopulate;
 
   const query = {};
 
@@ -179,6 +184,10 @@ const convertNestedPopulate = subPopulate => {
 
   if (populate) {
     query.populate = convertPopulateQueryParams(populate);
+  }
+
+  if (count) {
+    query.count = convertCountQueryParams(count);
   }
 
   return query;
