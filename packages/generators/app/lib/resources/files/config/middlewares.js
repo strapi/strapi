4e051c5c04cf268a9e0fc18e5e8fbc8e53ae4f77
Alexandre Bodin <bodin.alex@gmail.com>
2021-09-28 12:06:53 +0200

    Add default config in generator

---
+++ packages/generators/app/lib/resources/files/config/middlewares.js
@@ -0,0 +1,10 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::request',
+  'strapi::favicon',
+  'strapi::public',
+];
