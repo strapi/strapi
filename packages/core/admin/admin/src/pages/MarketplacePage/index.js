db6fea361245db255ece52e663b613d524864990
ronronscelestes <julie.plantey@gmail.com>
2021-09-28 11:10:15 +0200

    documentation coming soon + translation fixes

--- packages/core/admin/admin/src/pages/MarketplacePage/index.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/index.js
@@ -136,7 +136,7 @@ const MarketPlacePage = () => {
           <ContentLayout>
             <NoContent
               content={{
-                id: 'coming soon',
+                id: 'coming.soon',
                 defaultMessage:
                   'This content is currently under construction and will be back in a few weeks!',
               }}
--- packages/plugins/documentation/admin/src/pages/App/index.js
+++ packages/plugins/documentation/admin/src/pages/App/index.js
@@ -6,11 +6,40 @@
  */
 
 import React from 'react';
-import { CheckPagePermissions } from '@strapi/helper-plugin';
+import { useIntl } from 'react-intl';
+import { CheckPagePermissions, NoContent } from '@strapi/helper-plugin';
+import { Layout, HeaderLayout, ContentLayout } from '@strapi/parts/Layout';
+import { Main } from '@strapi/parts/Main';
 import pluginPermissions from '../../permissions';
+import { getTrad } from '../../utils';
 // import HomePage from '../HomePage';
 
-const ComingSoon = () => <div>Coming soon</div>;
+const ComingSoon = () => {
+  const { formatMessage } = useIntl();
+  console.log(pluginPermissions);
+
+  return (
+    <Layout>
+      <Main>
+        <HeaderLayout
+          title={formatMessage({
+            id: getTrad('plugin.name'),
+            defaultMessage: 'Documentation',
+          })}
+        />
+        <ContentLayout>
+          <NoContent
+            content={{
+              id: getTrad('coming.soon'),
+              defaultMessage:
+                'This content is currently under construction and will be back in a few weeks!',
+            }}
+          />
+        </ContentLayout>
+      </Main>
+    </Layout>
+  );
+};
 
 function App() {
   return (
