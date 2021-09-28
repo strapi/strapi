e3399d7ae21e9d195b0399428df77905711eb2eb
Alexandre Bodin <bodin.alex@gmail.com>
2021-09-28 14:49:33 +0200

    Handle empty values

--- packages/core/strapi/lib/core-api/controller/transform.js
+++ packages/core/strapi/lib/core-api/controller/transform.js
@@ -24,13 +24,24 @@ const transformResponse = (resource, meta = {}, { contentType } = {}) => {
   };
 };
 
-const transformEntry = (entry, contentType) => {
+const transformComponent = (data, component) => {
+  const res = transformEntry(data, component);
+
+  if (isNil(res)) {
+    return res;
+  }
+
+  const { id, attributes } = res;
+  return { id, ...attributes };
+};
+
+const transformEntry = (entry, type) => {
   if (isNil(entry)) {
     return entry;
   }
 
   if (Array.isArray(entry)) {
-    return entry.map(singleEntry => transformEntry(singleEntry, contentType));
+    return entry.map(singleEntry => transformEntry(singleEntry, type));
   }
 
   if (!isPlainObject(entry)) {
@@ -43,28 +54,22 @@ const transformEntry = (entry, contentType) => {
 
   for (const key in properties) {
     const property = properties[key];
-    const attribute = contentType && contentType.attributes[key];
+    const attribute = type && type.attributes[key];
 
     if (attribute && attribute.type === 'relation') {
       const data = transformEntry(property, strapi.contentType(attribute.target));
 
       attributeValues[key] = { data };
     } else if (attribute && attribute.type === 'component') {
-      const { id, attributes } = transformEntry(property, strapi.components[attribute.component]);
-      attributeValues[key] = { id, ...attributes };
+      attributeValues[key] = transformComponent(property, strapi.components[attribute.component]);
     } else if (attribute && attribute.type === 'dynamiczone') {
-      if (Array.isArray(property)) {
-        attributeValues[key] = property.map(subProperty => {
-          const { id, attributes } = transformEntry(
-            subProperty,
-            strapi.components[subProperty.__component]
-          );
-
-          return { id, ...attributes };
-        });
-      } else {
+      if (isNil(property)) {
         attributeValues[key] = property;
       }
+
+      attributeValues[key] = property.map(subProperty => {
+        return transformComponent(subProperty, strapi.components[subProperty.__component]);
+      });
     } else if (attribute && attribute.type === 'media') {
       const data = transformEntry(property, strapi.contentType('plugin::upload.file'));
 
