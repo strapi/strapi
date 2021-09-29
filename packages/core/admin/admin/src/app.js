fffc389ec618868f282726dffd6c6302a46b0dc2
soupette <cyril@strapi.io>
2021-09-29 05:38:31 +0200

    Remove test injected component
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/admin/src/app.js
+++ packages/core/admin/admin/src/app.js
@@ -1,9 +1,4 @@
 export default {
   config: {},
-  bootstrap(app) {
-    app.injectContentManagerComponent('editView', 'informations', {
-      name: 'i18n-locale-filter-edit-view',
-      Component: () => 'test',
-    });
-  },
+  bootstrap() {},
 };
