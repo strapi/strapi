eb08556befa56416ce3fdd2f55088892479f312f
mfrachet <marvin.frachet@strapi.io>
2021-10-04 13:52:20 +0200

    Iterating on layout

--- packages/core/upload/admin/src/components/EditAssetDialog/index.js
+++ packages/core/upload/admin/src/components/EditAssetDialog/index.js
@@ -9,12 +9,18 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import { ModalLayout, ModalHeader, ModalBody, ModalFooter } from '@strapi/parts/ModalLayout';
 import { ButtonText } from '@strapi/parts/Text';
-import { Box } from '@strapi/parts/Box';
+import { Stack } from '@strapi/parts/Stack';
+import { Grid, GridItem } from '@strapi/parts/Grid';
 import { Button } from '@strapi/parts/Button';
+import { TextInput } from '@strapi/parts/TextInput';
+import { getFileExtension } from '@strapi/helper-plugin';
+import { AssetMeta } from './AssetMeta';
 import { getTrad } from '../../utils';
+import formatBytes from '../../utils/formatBytes';
 
-export const EditAssetDialog = ({ onClose }) => {
+export const EditAssetDialog = ({ onClose, asset }) => {
   const { formatMessage } = useIntl();
+  const formatter = new Intl.DateTimeFormat();
 
   // TODO implement when the code is ready
   const handleSubmit = () => {};
@@ -27,18 +33,68 @@ export const EditAssetDialog = ({ onClose }) => {
         </ButtonText>
       </ModalHeader>
       <ModalBody>
-        <p>Lol</p>
+        <Grid gap={4}>
+          <GridItem xs={12} col={6}>
+            Left
+          </GridItem>
+          <GridItem xs={12} col={6}>
+            <Stack size={3}>
+              <AssetMeta
+                size={formatBytes(asset.size)}
+                dimension={asset.height && asset.width ? `${asset.height}✕${asset.width}` : ''}
+                date={formatter.format(new Date(asset.createdAt))}
+                extension={getFileExtension(asset.ext)}
+              />
+
+              <TextInput
+                size="S"
+                label={formatMessage({
+                  id: getTrad('form.input.label.file-name'),
+                  defaultMessage: 'File name',
+                })}
+                name="filename"
+              />
+              <TextInput
+                size="S"
+                label={formatMessage({
+                  id: getTrad('form.input.label.file-alt'),
+                  defaultMessage: 'Alternative text',
+                })}
+                name="altText"
+                hint={formatMessage({
+                  id: getTrad({ id: getTrad('form.input.decription.file-alt') }),
+                  defaultMessage: 'This text will be displayed if the asset can’t be shown.',
+                })}
+              />
+              <TextInput
+                size="S"
+                label={formatMessage({
+                  id: getTrad('form.input.label.file-caption'),
+                  defaultMessage: 'Caption',
+                })}
+                name="caption"
+              />
+            </Stack>
+          </GridItem>
+        </Grid>
       </ModalBody>
       <ModalFooter
         startActions={
           <Button onClick={onClose} variant="tertiary">
-            Cancel
+            {formatMessage({ id: 'cancel', defaultMessage: 'Cancel' })}
           </Button>
         }
         endActions={
           <>
-            <Button variant="secondary">Add new stuff</Button>
-            <Button onClick={handleSubmit}>Finish</Button>
+            <Button variant="secondary">
+              {formatMessage({
+                id: getTrad('control-card.replace-media'),
+                defaultMessage: 'Replace media',
+              })}
+            </Button>
+            <Button onClick={handleSubmit}>
+              {formatMessage({ id: 'form.button.finish', defaultMessage: 'Finish' })}
+            </Button>
           </>
         }
       />
@@ -47,5 +103,12 @@ export const EditAssetDialog = ({ onClose }) => {
 };
 
 EditAssetDialog.propTypes = {
+  asset: PropTypes.shape({
+    height: PropTypes.number,
+    width: PropTypes.number,
+    size: PropTypes.number,
+    createdAt: PropTypes.string,
+    ext: PropTypes.string,
+  }).isRequired,
   onClose: PropTypes.func.isRequired,
 };
