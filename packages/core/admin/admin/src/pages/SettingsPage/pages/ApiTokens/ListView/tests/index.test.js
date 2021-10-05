275543a87b44a653da01a7dc73cd228173af7ed7
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-10-05 16:08:34 +0200

    update tests

--- packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/ListView/tests/index.test.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/ListView/tests/index.test.js
@@ -657,7 +657,7 @@ describe('ADMIN | Pages | API TOKENS | ListPage', () => {
 
     const { getByText } = render(app);
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(getByText('My super token')).toBeInTheDocument();
       expect(getByText('This describe my super token')).toBeInTheDocument();
     });
