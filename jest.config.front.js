c5520d40fa8b44c45f719f70a8a3dc2f96f00e50
fdel-car <fabien.delcarmen@gmail.com>
2021-10-05 16:56:01 +0200

    Update projects array in root jest configs

--- jest.config.front.js
+++ jest.config.front.js
@@ -2,8 +2,5 @@ const baseConfig = require('./jest.base-config.front');
 
 module.exports = {
   ...baseConfig,
-  projects: [
-    '<rootDir>/packages/core/*/jest.config.front.js',
-    '<rootDir>/packages/plugins/*/jest.config.front.js',
-  ],
+  projects: ['<rootDir>/packages/**/jest.config.front.js'],
 };
--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,5 @@ const baseConfig = require('./jest.base-config');
 
 module.exports = {
   ...baseConfig,
-  projects: [
-    '<rootDir>/packages/core/*/jest.config.js',
-    '<rootDir>/packages/plugins/*/jest.config.js',
-  ],
+  projects: ['<rootDir>/packages/**/jest.config.js'],
 };
