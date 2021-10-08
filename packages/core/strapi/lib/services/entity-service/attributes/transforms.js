599c9c809d160f0562a5e6ca69d27332585e5bc4
Convly <jean-sebastien.herbaux@epitech.eu>
2021-10-08 15:29:43 +0200

    Handle non-string & non-buffer scenarios for the password transform

--- packages/core/strapi/lib/services/entity-service/attributes/transforms.js
+++ packages/core/strapi/lib/services/entity-service/attributes/transforms.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const { getOr, toNumber } = require('lodash/fp');
+const { getOr, toNumber, isString, isBuffer } = require('lodash/fp');
 const bcrypt = require('bcrypt');
 
 const transforms = {
   password(value, context) {
     const { action, attribute } = context;
+
+    if (!isString(value) && !isBuffer(value)) {
+      return value;
+    }
 
     if (action !== 'create' && action !== 'update') {
       return value;
