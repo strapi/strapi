74f20d9af062f765db150aebb5a173925ccba0e9
Sacha STAFYNIAK <sacha@digisquad.io>
2021-10-12 12:40:17 +0200

    chore: clean d.ts example

--- examples/getstarted/global.d.ts
+++ examples/getstarted/global.d.ts
@@ -20,22 +20,4 @@ declare module '@strapi/strapi' {
   }
 }
 
-declare module '@strapi/strapi' {
-  /**
-   * augment strapi.entityService
-   */
-  interface StrapiContentTypes {
-    restaurantTATAT: Restaurant;
-  }
-
-  /**
-   * augment strapi.container.get
-   */
-  interface StrapiRegistries {
-    custom: {
-      bar(): 'test';
-    };
-  }
-}
-
 export {};
