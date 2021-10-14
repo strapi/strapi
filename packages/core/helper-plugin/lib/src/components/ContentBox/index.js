72aab41378c0c41a71efaeeef08aa4c6e16579c5
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-10-14 15:36:54 +0200

    copy token to clipboard

--- packages/core/helper-plugin/lib/src/components/ContentBox/index.js
+++ packages/core/helper-plugin/lib/src/components/ContentBox/index.js
@@ -28,7 +28,7 @@ const ContentBox = ({ title, subtitle, icon, iconBackground, endAction }) => {
       </IconWrapper>
       <Stack size={endAction ? 0 : 1}>
         <Flex>
-          <Text small bold style={{ 'word-break': 'break-all' }}>
+          <Text small bold style={{ wordBreak: 'break-all' }}>
             {title}
           </Text>
           {endAction}
