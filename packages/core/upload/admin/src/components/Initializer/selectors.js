0562592c378ea9ac92f0889af6c5d4c7198672e8
mfrachet <marvin.frachet@strapi.io>
2021-10-15 15:19:55 +0200

    Remove reducer and initializer

--- packages/core/upload/admin/src/components/Initializer/selectors.js
+++
@@ -1,5 +0,0 @@
-import pluginId from '../../pluginId';
-
-const selectFileModelTimestamps = state => state[`${pluginId}_fileModel`].fileModelTimestamps;
-
-export default selectFileModelTimestamps;
--- packages/core/upload/admin/src/pages/App/components/SortPicker.js
+++ packages/core/upload/admin/src/pages/App/components/SortPicker.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import { SimpleMenu, MenuItem } from '@strapi/parts/SimpleMenu';
 import { useQueryParams } from '@strapi/helper-plugin';
-import { useSelectTimestamps } from '../../../hooks';
 import { getTrad } from '../../../utils';
 
 export const SortPicker = () => {
-  const [createdAt, updatedAt] = useSelectTimestamps();
   const { formatMessage } = useIntl();
   const [, setQuery] = useQueryParams();
 
@@ -15,12 +13,12 @@ export const SortPicker = () => {
   };
 
   const filters = [
-    { key: 'sort.created_at_desc', value: `${createdAt}:DESC` },
-    { key: 'sort.created_at_asc', value: `${createdAt}:ASC` },
+    { key: 'sort.created_at_desc', value: `createdAt:DESC` },
+    { key: 'sort.created_at_asc', value: `createdAt:ASC` },
     { key: 'sort.name_asc', value: 'name:ASC' },
     { key: 'sort.name_desc', value: 'name:DESC' },
-    { key: 'sort.updated_at_desc', value: `${updatedAt}:DESC` },
-    { key: 'sort.updated_at_asc', value: `${updatedAt}:ASC` },
+    { key: 'sort.updated_at_desc', value: `updatedAt:DESC` },
+    { key: 'sort.updated_at_asc', value: `updatedAt:ASC` },
   ];
 
   return (
