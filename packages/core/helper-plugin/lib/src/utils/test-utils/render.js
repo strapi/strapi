196b3b13149d44bf04da93b92bcd6d44921e0ba8
mfrachet <marvin.frachet@strapi.io>
2021-10-18 09:22:11 +0200

    Init test utils

---
+++ packages/core/helper-plugin/lib/src/utils/test-utils/render.js
@@ -0,0 +1,40 @@
+import * as RTL from '@testing-library/react';
+import React from 'react';
+import { ThemeProvider, lightTheme } from '@strapi/parts';
+import { IntlProvider } from 'react-intl';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { NotificationsProvider } from '@strapi/helper-plugin';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const render = (
+  Component,
+  props,
+  {
+    theme = lightTheme,
+    toggleNotification = jest.fn(),
+    locale = 'en',
+    translations = {},
+    container = document.body,
+  }
+) =>
+  RTL.render(
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NotificationsProvider toggleNotification={toggleNotification}>
+          <IntlProvider locale={locale} messages={translations} defaultLocale={locale}>
+            <Component {...props} />
+          </IntlProvider>
+        </NotificationsProvider>
+      </ThemeProvider>
+    </QueryClientProvider>,
+    { container }
+  );
+
+export default { ...RTL, render };
