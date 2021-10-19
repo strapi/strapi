6c797a7d58d3be902fbc0fe8628f02168499eef6
Dieter Stinglhamber <dieter.stinglhamber@strapi.io>
2021-10-19 09:38:46 +0200

    use proper icon and fix semantic link

--- packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/ListView/DynamicTable/index.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/ListView/DynamicTable/index.js
@@ -5,7 +5,6 @@ import { Row } from '@strapi/parts/Row';
 import { RelativeTime, useQueryParams } from '@strapi/helper-plugin';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
 import DeleteButton from './DeleteButton';
 import UpdateButton from './UpdateButton';
 
@@ -13,11 +12,6 @@ const TableRows = ({ canDelete, canUpdate, onClickDelete, withBulkActions, rows 
   const { formatMessage } = useIntl();
   const [{ query }] = useQueryParams();
   const [, sortOrder] = query.sort.split(':');
-
-  const {
-    push,
-    location: { pathname },
-  } = useHistory();
 
   const apiTokens = rows.sort((a, b) => {
     const comparaison = a.name.localeCompare(b.name);
@@ -57,12 +51,7 @@ const TableRows = ({ canDelete, canUpdate, onClickDelete, withBulkActions, rows 
             {withBulkActions && (
               <Td>
                 <Row justifyContent="end">
-                  {canUpdate && (
-                    <UpdateButton
-                      tokenName={apiToken.name}
-                      onClickUpdate={() => push(`${pathname}/${apiToken.id}`)}
-                    />
-                  )}
+                  {canUpdate && <UpdateButton tokenName={apiToken.name} tokenId={apiToken.id} />}
                   {canDelete && (
                     <DeleteButton
                       tokenName={apiToken.name}
