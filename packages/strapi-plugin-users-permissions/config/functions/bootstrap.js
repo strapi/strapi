5b75a093c9cb51ab5807c0c941a0cef7a1a88826
Asaf Shakarzy <asaf@crossid.io>
2021-10-20 14:43:09 +0300

    feat(users-permissions): Add Crossid provider (#11305)
    Authenticate via crossid.io provider.

--- packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
+++ packages/strapi-plugin-users-permissions/config/functions/bootstrap.js
@@ -139,6 +139,16 @@ module.exports = async () => {
       scope: ['openid email'], // scopes should be space delimited
       subdomain: 'my.subdomain.com/cas',
     },
+    crossid: {
+      enabled: false,
+      icon: '',
+      key: '',
+      secret: '',
+      state: true,
+      callback: `${strapi.config.server.url}/auth/crossid/callback`,
+      subdomain: 'my-tenant',
+      scope: ['openid', 'email', 'profile'],
+    },
   };
   const prevGrantConfig = (await pluginStore.get({ key: 'grant' })) || {};
   // store grant auth config to db
