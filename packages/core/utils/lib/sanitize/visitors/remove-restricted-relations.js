700ee2ca01d471566b2a255710e5a45f0759a7c1
Convly <jean-sebastien.herbaux@epitech.eu>
2021-11-02 15:56:53 +0100

    Fix remove-restricted-relations visitor

--- packages/core/utils/lib/sanitize/visitors/remove-restricted-relations.js
+++ packages/core/utils/lib/sanitize/visitors/remove-restricted-relations.js
@@ -1,9 +1,9 @@
 'use strict';
 
-module.exports = (auth, mode) => async ({ key, schema, attribute }, { remove }) => {
+module.exports = (auth, mode) => async ({ key, attribute }, { remove }) => {
   const isRelation = attribute.type === 'relation';
 
-  if (!isRelation) {
+  if (!isRelation || attribute.relation.toLowerCase().startsWith('morph')) {
     return;
   }
 
@@ -13,11 +13,11 @@ module.exports = (auth, mode) => async ({ key, schema, attribute }, { remove }) 
   const outputActions = isTooMany ? ['find'] : ['findOne'];
 
   const actions = mode === 'input' ? inputActions : outputActions;
-  const scopes = actions.map(action => `${schema.uid}.${action}`);
+  const scopes = actions.map(action => `${attribute.target}.${action}`);
 
   for (const scope of scopes) {
     try {
-      await strapi.auth.verify(auth, scope);
+      await strapi.auth.verify(auth, { scope });
       // If at least one scope has been verified, then ignore the rest & return early
       return;
     } catch {
