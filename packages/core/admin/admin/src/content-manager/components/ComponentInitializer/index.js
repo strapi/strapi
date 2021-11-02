7b9ca4d9075190c714f0d81d604c4f57562f4601
ronronscelestes <julie.plantey@gmail.com>
2021-11-02 17:52:22 +0100

    feedback fixes

--- packages/core/admin/admin/src/content-manager/components/ComponentInitializer/index.js
+++ packages/core/admin/admin/src/content-manager/components/ComponentInitializer/index.js
@@ -1,30 +1,21 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useIntl } from 'react-intl';
 import PlusCircle from '@strapi/icons/PlusCircle';
 import { Box } from '@strapi/design-system/Box';
-import { BaseButton } from '@strapi/design-system/BaseButton';
 import { Stack } from '@strapi/design-system/Stack';
 import { Flex } from '@strapi/design-system/Flex';
 import { Text } from '@strapi/design-system/Text';
 import { pxToRem } from '@strapi/helper-plugin';
 import { getTrad } from '../../utils';
 
-const CursorBox = styled(Box)`
-  cursor: pointer;
-`;
-
-const IconButton = styled(BaseButton)`
-  border: none;
-  padding: 0;
-  background: transparent;
-  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+const IconWrapper = styled.span`
   > svg {
     width: ${pxToRem(24)};
     height: ${pxToRem(24)};
     > circle {
-      fill: ${({ theme }) => theme.colors.primary200}!important;
+      fill: ${({ theme }) => theme.colors.primary200};
     }
     > path {
       fill: ${({ theme }) => theme.colors.primary600};
@@ -34,28 +25,24 @@ const IconButton = styled(BaseButton)`
 
 const ComponentInitializer = ({ isReadOnly, onClick }) => {
   const { formatMessage } = useIntl();
-  const addEntryButtonRef = useRef();
-
-  const handleBoxClick = () => {
-    if (addEntryButtonRef.current) {
-      addEntryButtonRef.current.click();
-    }
-  };
 
   return (
-    <CursorBox
+    <Box
+      as="button"
       background="neutral100"
-      hasRadius
       borderColor="neutral200"
+      disabled={isReadOnly}
+      hasRadius
+      onClick={onClick}
       paddingTop={9}
       paddingBottom={9}
-      onClick={handleBoxClick}
+      type="button"
     >
       <Stack size={2}>
         <Flex justifyContent="center" style={{ cursor: isReadOnly ? 'not-allowed' : 'inherit' }}>
-          <IconButton ref={addEntryButtonRef} disabled={isReadOnly} onClick={onClick}>
+          <IconWrapper>
             <PlusCircle />
-          </IconButton>
+          </IconWrapper>
         </Flex>
         <Flex justifyContent="center">
           <Text textColor="primary600" small bold>
@@ -66,7 +53,7 @@ const ComponentInitializer = ({ isReadOnly, onClick }) => {
           </Text>
         </Flex>
       </Stack>
-    </CursorBox>
+    </Box>
   );
 };
 
