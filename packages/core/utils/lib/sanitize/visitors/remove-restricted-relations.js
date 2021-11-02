cee2d6485acbf1acb0130cfdd7edd2a2ca1884dd
Convly <jean-sebastien.herbaux@epitech.eu>
2021-11-02 17:42:04 +0100

    Handle polymorphic relation in the related visitor

--- packages/core/utils/lib/sanitize/visitors/remove-restricted-relations.js
+++ packages/core/utils/lib/sanitize/visitors/remove-restricted-relations.js
@@ -1,30 +1,66 @@
 'use strict';
 
-module.exports = (auth, mode) => async ({ key, attribute }, { remove }) => {
+module.exports = (auth, mode) => async ({ data, key, attribute }, { remove, set }) => {
   const isRelation = attribute.type === 'relation';
 
-  if (!isRelation || attribute.relation.toLowerCase().startsWith('morph')) {
+  if (!isRelation) {
     return;
   }
 
-  const isTooMany = attribute.relation.endsWith('Many');
+  const handleMorphRelation = async () => {
+    const newMorphValue = [];
 
-  const inputActions = ['find', 'findOne'];
-  const outputActions = isTooMany ? ['find'] : ['findOne'];
+    for (const element of data[key]) {
+      const scopes = ['find', 'findOne'].map(action => `${element.__type}.${action}`);
+      const isAllowed = await hasAccessToSomeScopes(scopes, auth);
 
-  const actions = mode === 'input' ? inputActions : outputActions;
-  const scopes = actions.map(action => `${attribute.target}.${action}`);
+      if (isAllowed) {
+        newMorphValue.push(element);
+      }
+    }
 
+    set(key, newMorphValue);
+  };
+
+  const handleRegularRelation = async () => {
+    const isTooMany = attribute.relation.endsWith('Many');
+
+    const inputActions = ['find', 'findOne'];
+    const outputActions = isTooMany ? ['find'] : ['findOne'];
+
+    const actions = mode === 'input' ? inputActions : outputActions;
+    const scopes = actions.map(action => `${attribute.target}.${action}`);
+
+    const isAllowed = await hasAccessToSomeScopes(scopes, auth);
+
+    // If the authenticated user don't have access to any of the scopes, then remove the field
+    if (!isAllowed) {
+      remove(key);
+    }
+  };
+
+  const isMorphRelation = attribute.relation.toLowerCase().startsWith('morph');
+
+  // Polymorphic relations
+  if (isMorphRelation) {
+    await handleMorphRelation();
+  }
+
+  // Regular relations
+  else {
+    await handleRegularRelation();
+  }
+};
+
+const hasAccessToSomeScopes = async (scopes, auth) => {
   for (const scope of scopes) {
     try {
       await strapi.auth.verify(auth, { scope });
-      // If at least one scope has been verified, then ignore the rest & return early
-      return;
+      return true;
     } catch {
       continue;
     }
   }
 
-  // If the authenticated user don't have access to any of the scopes, then remove the field
-  remove(key);
+  return false;
 };
