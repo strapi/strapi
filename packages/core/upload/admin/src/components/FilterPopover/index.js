d59d7d1c80eadf28b1b09c5d2726b2dec3332ace
ronronscelestes <julie.plantey@gmail.com>
2021-11-03 16:51:39 +0100

    added handleBlur for ML FilterPopover

--- packages/core/upload/admin/src/components/FilterPopover/index.js
+++ packages/core/upload/admin/src/components/FilterPopover/index.js
@@ -17,7 +17,7 @@ import Plus from '@strapi/icons/Plus';
 import FilterValueInput from './FilterValueInput';
 import getFilterList from './utils/getFilterList';
 
-const FilterPopover = ({ displayedFilters, filters, onSubmit, onToggle, source }) => {
+const FilterPopover = ({ displayedFilters, filters, onSubmit, onToggle, source, onBlur }) => {
   const { formatMessage } = useIntl();
 
   const [modifiedData, setModifiedData] = useState({
@@ -192,7 +192,7 @@ const FilterPopover = ({ displayedFilters, filters, onSubmit, onToggle, source }
 
   return (
     <>
-      <Popover source={source} padding={3} spacing={4}>
+      <Popover source={source} padding={3} spacing={4} onBlur={onBlur}>
         <FocusTrap onEscape={onToggle}>
           <form onSubmit={handleSubmit}>
             <Stack size={1} style={{ minWidth: 184 }}>
@@ -266,6 +266,7 @@ FilterPopover.propTypes = {
     })
   ).isRequired,
   filters: PropTypes.array.isRequired,
+  onBlur: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   onToggle: PropTypes.func.isRequired,
   source: PropTypes.shape({ current: PropTypes.instanceOf(Element) }).isRequired,
