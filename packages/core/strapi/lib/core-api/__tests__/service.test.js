882db487097c424321fbf4a8044a8fcac5447bef
vincentbpro <89356961+vincentbpro@users.noreply.github.com>
2021-11-09 11:09:27 +0100

    test: added tests for pagination service

--- packages/core/strapi/lib/core-api/__tests__/service.test.js
+++ packages/core/strapi/lib/core-api/__tests__/service.test.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 const { createService } = require('../service');
+const { getPaginationInfo } = require('../service/pagination');
 
 const maxLimit = 50;
 const defaultLimit = 20;
@@ -142,6 +143,122 @@ describe('Default Service', () => {
         });
 
         expect(strapi.entityService.delete).toHaveBeenCalledWith('testModel', 1);
+      });
+    });
+  });
+});
+
+describe('Pagination service', () => {
+  test('Uses default limit', () => {
+    const pagination = {};
+    const paginationInfo = getPaginationInfo({ pagination });
+
+    expect(paginationInfo).toEqual({
+      page: 1,
+      pageSize: defaultLimit,
+    });
+  });
+
+  describe('Paged pagination', () => {
+    test('Uses specified pageSize', () => {
+      const pagination = { pageSize: 5 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        page: 1,
+        pageSize: pagination.pageSize,
+      });
+    });
+
+    test('Uses maxLimit as pageSize', () => {
+      const pagination = { pageSize: 999 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        page: 1,
+        pageSize: maxLimit,
+      });
+    });
+
+    test('Uses 1 as pageSize', () => {
+      const pagination = { pageSize: 0 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        page: 1,
+        pageSize: 1,
+      });
+    });
+
+    test('Uses 1 as pageSize', () => {
+      const pagination = { pageSize: -1 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        page: 1,
+        pageSize: 1,
+      });
+    });
+
+    test('Uses 1 as pageSize', () => {
+      const pagination = { pageSize: -2 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        page: 1,
+        pageSize: 1,
+      });
+    });
+  });
+
+  describe('Offset pagination', () => {
+    test('Uses specified limit', () => {
+      const pagination = { limit: 5 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        start: 0,
+        limit: pagination.limit,
+      });
+    });
+
+    test('Uses maxLimit as limit', () => {
+      const pagination = { limit: 999 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        start: 0,
+        limit: maxLimit,
+      });
+    });
+
+    test('Uses 1 as limit', () => {
+      const pagination = { limit: 0 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        start: 0,
+        limit: 1,
+      });
+    });
+
+    test('Uses maxLimit as limit', () => {
+      const pagination = { limit: -1 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        start: 0,
+        limit: maxLimit,
+      });
+    });
+
+    test('Uses 1 as limit', () => {
+      const pagination = { limit: -2 };
+      const paginationInfo = getPaginationInfo({ pagination });
+
+      expect(paginationInfo).toEqual({
+        start: 0,
+        limit: 1,
       });
     });
   });
