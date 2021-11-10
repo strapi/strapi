b7057d41b100a691970ef7689dd8a610bf0465e4
mfrachet <marvin.frachet@strapi.io>
2021-11-10 09:21:02 +0100

    Making the blurry background + pdf file on doc asset

--- packages/core/upload/admin/src/components/AssetCard/DocAssetCard.js
+++ packages/core/upload/admin/src/components/AssetCard/DocAssetCard.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import {
   Card,
   CardAction,
-  CardAsset,
   CardBadge,
   CardBody,
   CardCheckbox,
@@ -13,9 +12,11 @@ import {
   CardTitle,
   CardSubtitle,
 } from '@strapi/design-system/Card';
+import { Flex } from '@strapi/design-system/Flex';
 import { IconButton } from '@strapi/design-system/IconButton';
 import Pencil from '@strapi/icons/Pencil';
-import Book from '@strapi/icons/Book';
+import FileIcon from '@strapi/icons/File';
+import FilePdfIcon from '@strapi/icons/FilePdf';
 import { useIntl } from 'react-intl';
 import { getTrad } from '../../utils';
 
@@ -27,6 +28,11 @@ const IconWrapper = styled.span`
   svg {
     font-size: 3rem;
   }
+`;
+
+const CardAsset = styled(Flex)`
+  border-radius: ${({ theme }) => theme.borderRadius} ${({ theme }) => theme.borderRadius} 0 0;
+  background: linear-gradient(180deg, #ffffff 0%, #f6f6f9 121.48%);
 `;
 
 export const DocAssetCard = ({ name, extension, selected, onSelect, onEdit, size }) => {
@@ -45,9 +51,17 @@ export const DocAssetCard = ({ name, extension, selected, onSelect, onEdit, size
             />
           </CardAction>
         )}
-        <CardAsset size={size}>
+        <CardAsset
+          width="100%"
+          height={size === 'S' ? `${88 / 16}rem` : `${164 / 16}rem`}
+          justifyContent="center"
+        >
           <IconWrapper>
-            <Book aria-label={name} />
+            {extension === 'pdf' ? (
+              <FilePdfIcon aria-label={name} />
+            ) : (
+              <FileIcon aria-label={name} />
+            )}
           </IconWrapper>
         </CardAsset>
       </CardHeader>
