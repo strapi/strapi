5acbd1e6f2f50066bbfe9a9b23f5b90d80363639
Alexandre Bodin <bodin.alex@gmail.com>
2021-11-12 18:23:00 +0100

    Add example in address routes

--- examples/getstarted/src/api/address/routes/address.js
+++ examples/getstarted/src/api/address/routes/address.js
@@ -2,4 +2,13 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::address.address');
+module.exports = createCoreRouter('api::address.address', {
+  prefix: '/my-prefix',
+  config: {
+    find: {
+      auth: false,
+    },
+  },
+  only: ['find', 'findOne'],
+  // except: [],
+});
