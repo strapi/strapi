6de3371e6106d465760594faef118debf96462fa
ronronscelestes <julie.plantey@gmail.com>
2021-11-17 17:46:38 +0100

    edit asset dialog + media library input

--- packages/core/upload/admin/src/components/MediaLibraryInput/Carousel/EmptyStateAsset.js
+++ packages/core/upload/admin/src/components/MediaLibraryInput/Carousel/EmptyStateAsset.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { useIntl } from 'react-intl';
 import { Icon } from '@strapi/design-system/Icon';
 import { Flex } from '@strapi/design-system/Flex';
-import { Text } from '@strapi/design-system/Text';
+import { Typography } from '@strapi/design-system/Typography';
 import PicturePlusIcon from '@strapi/icons/PicturePlus';
 import getTrad from '../../../utils/getTrad';
 import { rawFileToAsset } from '../../../utils/rawFileToAsset';
 import { AssetSource } from '../../../constants';
+
+const TextAlignTypography = styled(Typography)`
+  align-items: center;
+`;
 
 export const EmptyStateAsset = ({ disabled, onClick, onDropAsset }) => {
   const { formatMessage } = useIntl();
@@ -65,12 +70,18 @@ export const EmptyStateAsset = ({ disabled, onClick, onDropAsset }) => {
         color={disabled ? 'neutral400' : 'primary600'}
         marginBottom={3}
       />
-      <Text small bold textColor="neutral600" style={{ textAlign: 'center' }} as="span">
+      <TextAlignTypography
+        variant="pi"
+        fontWeight="bold"
+        textColor="neutral600"
+        style={{ textAlign: 'center' }}
+        as="span"
+      >
         {formatMessage({
           id: getTrad('mediaLibraryInput.placeholder'),
           defaultMessage: 'Click to select an asset or drag and drop one in this area',
         })}
-      </Text>
+      </TextAlignTypography>
     </Flex>
   );
 };
