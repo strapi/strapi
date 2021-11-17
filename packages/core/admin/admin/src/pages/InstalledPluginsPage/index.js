ed20b012060f72843c5ee2fed965dc70e5c6e5eb
soupette <cyril@strapi.io>
2021-11-17 15:08:43 +0100

    Add plugins page
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/admin/src/pages/InstalledPluginsPage/index.js
+++ packages/core/admin/admin/src/pages/InstalledPluginsPage/index.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import { CheckPagePermissions, NoContent } from '@strapi/helper-plugin';
-import { useIntl } from 'react-intl';
-import { Layout, HeaderLayout, ContentLayout } from '@strapi/design-system/Layout';
-import { Main } from '@strapi/design-system/Main';
+import { CheckPagePermissions } from '@strapi/helper-plugin';
+
 import adminPermissions from '../../permissions';
+import Plugins from './Plugins';
 
 const InstalledPluginsPage = () => {
-  const { formatMessage } = useIntl();
-
   return (
     <CheckPagePermissions permissions={adminPermissions.marketplace.main}>
-      <Layout>
-        <Main>
-          <HeaderLayout
-            title={formatMessage({
-              id: 'app.components.ListPluginsPage.helmet.title',
-              defaultMessage: 'List plugins',
-            })}
-          />
-          <ContentLayout>
-            <NoContent
-              content={{
-                id: 'coming.soon',
-                defaultMessage:
-                  'This content is currently under construction and will be back in a few weeks!',
-              }}
-            />
-          </ContentLayout>
-        </Main>
-      </Layout>
+      <Plugins />
     </CheckPagePermissions>
   );
 };
