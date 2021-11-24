a97c831cc020942715e5b1f8623ac151125a40fd
vincentbpro <89356961+vincentbpro@users.noreply.github.com>
2021-11-24 10:13:02 +0100

    tests: added tests for when maxLimit not configured(graphql)

--- packages/core/utils/lib/__tests__/pagination.test.js
+++ packages/core/utils/lib/__tests__/pagination.test.js
@@ -3,123 +3,242 @@
 const { withDefaultPagination } = require('../pagination');
 
 const defaultLimit = 20;
-const maxLimit = 50;
 const defaults = {
   offset: { limit: defaultLimit },
   page: { pageSize: defaultLimit },
 };
 
 describe('Pagination util', () => {
-  test('Uses default limit', () => {
-    const pagination = {};
-    const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+  describe('With maxLimit set', () => {
+    const maxLimit = 50;
 
-    expect(defaultPagination).toEqual({
-      start: 0,
-      limit: defaultLimit,
+    test('Uses default limit', () => {
+      const pagination = {};
+      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+      expect(defaultPagination).toEqual({
+        start: 0,
+        limit: defaultLimit,
+      });
+    });
+
+    describe('Paged pagination', () => {
+      test('Uses specified pageSize', () => {
+        const pagination = { pageSize: 5 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: pagination.pageSize,
+        });
+      });
+
+      test('Uses maxLimit as pageSize', () => {
+        const pagination = { pageSize: 999 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: maxLimit,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: 0 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: -1 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: -2 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+    });
+
+    describe('Offset pagination', () => {
+      test('Uses specified limit', () => {
+        const pagination = { limit: 5 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: pagination.limit,
+        });
+      });
+
+      test('Uses maxLimit as limit', () => {
+        const pagination = { limit: 999 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: maxLimit,
+        });
+      });
+
+      test('Uses 1 as limit', () => {
+        const pagination = { limit: 0 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+
+      test('Uses maxLimit as limit', () => {
+        const pagination = { limit: -1 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: maxLimit,
+        });
+      });
+
+      test('Uses 1 as limit', () => {
+        const pagination = { limit: -2 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
     });
   });
 
-  describe('Paged pagination', () => {
-    test('Uses specified pageSize', () => {
-      const pagination = { pageSize: 5 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+  describe('With maxLimit undefined', () => {
+    test('Uses default limit', () => {
+      const pagination = {};
+      const defaultPagination = withDefaultPagination(pagination, { defaults });
 
       expect(defaultPagination).toEqual({
         start: 0,
-        limit: pagination.pageSize,
+        limit: defaultLimit,
       });
     });
 
-    test('Uses maxLimit as pageSize', () => {
-      const pagination = { pageSize: 999 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+    describe('Paged pagination', () => {
+      test('Uses specified pageSize', () => {
+        const pagination = { pageSize: 5 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: maxLimit,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: pagination.pageSize,
+        });
+      });
+
+      test('Uses specified pageSize', () => {
+        const pagination = { pageSize: 999 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: pagination.pageSize,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: 0 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: -1 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
+      });
+
+      test('Uses 1 as pageSize', () => {
+        const pagination = { pageSize: -2 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
+
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
       });
     });
 
-    test('Uses 1 as pageSize', () => {
-      const pagination = { pageSize: 0 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+    describe('Offset pagination', () => {
+      test('Uses specified limit', () => {
+        const pagination = { limit: 5 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: 1,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: pagination.limit,
+        });
       });
-    });
 
-    test('Uses 1 as pageSize', () => {
-      const pagination = { pageSize: -1 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+      test('Uses apecified limit', () => {
+        const pagination = { limit: 999 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: 1,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 999,
+        });
       });
-    });
 
-    test('Uses 1 as pageSize', () => {
-      const pagination = { pageSize: -2 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+      test('Uses 1 as limit', () => {
+        const pagination = { limit: 0 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: 1,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
       });
-    });
-  });
 
-  describe('Offset pagination', () => {
-    test('Uses specified limit', () => {
-      const pagination = { limit: 5 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+      test('Uses -1 as limit', () => {
+        const pagination = { limit: -1 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: pagination.limit,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: -1,
+        });
       });
-    });
 
-    test('Uses maxLimit as limit', () => {
-      const pagination = { limit: 999 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
+      test('Uses 1 as limit', () => {
+        const pagination = { limit: -2 };
+        const defaultPagination = withDefaultPagination(pagination, { defaults });
 
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: maxLimit,
-      });
-    });
-
-    test('Uses 1 as limit', () => {
-      const pagination = { limit: 0 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
-
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: 1,
-      });
-    });
-
-    test('Uses maxLimit as limit', () => {
-      const pagination = { limit: -1 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
-
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: maxLimit,
-      });
-    });
-
-    test('Uses 1 as limit', () => {
-      const pagination = { limit: -2 };
-      const defaultPagination = withDefaultPagination(pagination, { defaults, maxLimit });
-
-      expect(defaultPagination).toEqual({
-        start: 0,
-        limit: 1,
+        expect(defaultPagination).toEqual({
+          start: 0,
+          limit: 1,
+        });
       });
     });
   });
