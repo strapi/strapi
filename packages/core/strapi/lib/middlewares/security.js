c807e88a89cbf55be1ee4effa16e78dc0f1868ae
mfrachet <marvin.frachet@strapi.io>
2021-11-25 15:34:47 +0100

    Fix CSP for safari

--- packages/core/strapi/lib/middlewares/security.js
+++ packages/core/strapi/lib/middlewares/security.js
@@ -14,6 +14,7 @@ const defaults = {
       'connect-src': ["'self'", 'https:'],
       'img-src': ["'self'", 'data:', 'blob:'],
       'media-src': ["'self'", 'data:', 'blob:'],
+      upgradeInsecureRequests: null,
     },
   },
   xssFilter: false,
