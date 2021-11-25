541ce9b891f059ad3e9b4926d7cd50a0e1ca29b7
ronronscelestes <julie.plantey@gmail.com>
2021-11-25 11:22:55 +0100

    pair prog errors refacto dz + fixed rep components

--- packages/core/admin/admin/src/content-manager/components/EditViewDataManagerProvider/utils/schema.js
+++ packages/core/admin/admin/src/content-manager/components/EditViewDataManagerProvider/utils/schema.js
@@ -112,6 +112,8 @@ const createYupSchema = (
               } else {
                 baseSchema = baseSchema.min(min, errorsTrads.min);
               }
+            } else if (required && !options.isDraft) {
+              baseSchema = baseSchema.min(1, errorsTrads.required);
             }
 
             if (max) {
@@ -153,20 +155,8 @@ const createYupSchema = (
 
         const { max, min } = attribute;
 
-        if (attribute.required && !options.isDraft) {
-          dynamicZoneSchema = dynamicZoneSchema.test('required', errorsTrads.required, value => {
-            if (options.isCreatingEntry) {
-              return value !== null || value !== undefined;
-            }
-
-            if (value === undefined) {
-              return true;
-            }
-
-            return value !== null;
-          });
-
-          if (min) {
+        if (min) {
+          if (attribute.required) {
             dynamicZoneSchema = dynamicZoneSchema
               .test('min', errorsTrads.min, value => {
                 if (options.isCreatingEntry) {
@@ -190,22 +180,21 @@ const createYupSchema = (
 
                 return value !== null;
               });
+          } else {
+            dynamicZoneSchema = dynamicZoneSchema.notEmptyMin(min);
           }
-        } else {
-          // eslint-disable-next-line no-lonely-if
-          if (min) {
-            dynamicZoneSchema = dynamicZoneSchema.test('min', errorsTrads.min, value => {
-              if (options.isCreatingEntry) {
-                return value && value.length >= min;
-              }
+        } else if (attribute.required && !options.isDraft) {
+          dynamicZoneSchema = dynamicZoneSchema.test('required', errorsTrads.required, value => {
+            if (options.isCreatingEntry) {
+              return value !== null || value !== undefined;
+            }
 
-              if (value === undefined) {
-                return true;
-              }
+            if (value === undefined) {
+              return true;
+            }
 
-              return value !== null && value.length >= min;
-            });
-          }
+            return value !== null;
+          });
         }
 
         if (max) {
