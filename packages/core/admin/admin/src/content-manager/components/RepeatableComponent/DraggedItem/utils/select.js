4a8f7cbf0a3bf0d29f56999147cb94274fb56537
Tim Brandin <tim.brandin@gmail.com>
2021-12-07 16:10:38 +0100

    Adding support for using a relationships entry title as a repeatable components entry title

--- packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/utils/select.js
+++ packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/utils/select.js
@@ -12,8 +12,13 @@ function useSelect({ schema, componentFieldName }) {
   } = useCMEditViewDataManager();
 
   const mainField = useMemo(() => get(schema, ['settings', 'mainField'], 'id'), [schema]);
+  const nestedObjectTitle = (((((schema.layouts.edit || [])[0] || [])[0] || {}).metadatas || {}).mainField || {}).name;
+  const nestedObjectField = (((schema.layouts.edit || [])[0] || [])[0] || {}).name;
+  const displayValuePath = (mainField === 'id' && !!nestedObjectTitle)
+    ? [...componentFieldName.split('.'), nestedObjectField, nestedObjectTitle]
+    : [...componentFieldName.split('.'), mainField];
   const displayedValue = toString(
-    get(modifiedData, [...componentFieldName.split('.'), mainField], '')
+    get(modifiedData, displayValuePath, '')
   );
 
   return {
