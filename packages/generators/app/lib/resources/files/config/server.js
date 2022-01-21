c9b0f048d71ee1968927847dd51ae2607e8f1267
Pierre Noël <petersg83@gmail.com>
2022-01-21 16:39:06 +0100

    remove url config

--- packages/generators/app/lib/resources/files/config/server.js
+++ packages/generators/app/lib/resources/files/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_SECRETS'),
   },
