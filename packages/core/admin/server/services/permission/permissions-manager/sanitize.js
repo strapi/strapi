4b8c87826af3041d8a9c6bb62bce0afdd1c574d7
denik1981 <demian.m1981@gmail.com>
2022-01-21 07:29:59 -0300

    Fix sanity checks for @casl/ability upgrade #12232

--- packages/core/admin/server/services/permission/permissions-manager/sanitize.js
+++ packages/core/admin/server/services/permission/permissions-manager/sanitize.js
@@ -76,30 +76,9 @@ module.exports = ({ action, ability, model }) => {
 
       const { subject, action: actionOverride } = getDefaultOptions(data, options);
 
-      // Since 5.1.0:
-      // CASL knows nothing about shapes of our entities, so the only way to tell him is to provide a fieldsFrom function.
-      // This function should return a list of fields from rule. Rule has no fields if it's allowed (or disallowed) to manage all of them.
-      // In this case, we return all fields, otherwise return what is inside our rule.
-      // References:
-      // https://github.com/stalniy/casl/blob/master/packages/casl-ability/CHANGELOG.md#breaking-changes
-      // https://casl.js.org/v5/en/guide/restricting-fields#field-patterns
-
-      // The empty array in (rule => rule.fields || []) is neccesary to keep the code stable
-      // meaning that the logic in place is already prepared to act upon
-      // getting an empty array when the action is referred to managed "all" the fields of an entity.
-
-      // @casl/ability@^5.1.0 won't allowed an undefined formFields when calling permittedFieldsOf.
-
       const permittedFields = permittedFieldsOf(ability, actionOverride, subject, {
         fieldsFrom: rule => rule.fields || [],
       });
-
-      // Since 5.1.0:
-      // "rulesFor" now expects a string with the subject type now. No more an object.
-      // References:
-      // https://github.com/stalniy/casl/blob/master/packages/casl-ability/CHANGELOG.md#breaking-changes
-      // https://casl.js.org/v5/en/guide/subject-type-detection#custom-subject-type-detection
-      // https://casl.js.org/v5/en/api/casl-ability
 
       const hasAtLeastOneRegistered = some(
         fields => !isNil(fields),
