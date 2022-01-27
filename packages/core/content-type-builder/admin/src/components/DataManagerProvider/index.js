0054e02363529b3054a0b2077e6ea5aef25c0ad8
Vincent <89356961+vincentbpro@users.noreply.github.com>
2022-01-27 14:33:09 +0100

    fix(guidedTour): trigger modal only at collection-type creation (#12318)
    Co-authored-by: ronronscelestes <julie.plantey@gmail.com>

--- packages/core/content-type-builder/admin/src/components/DataManagerProvider/index.js
+++ packages/core/content-type-builder/admin/src/components/DataManagerProvider/index.js
@@ -463,7 +463,7 @@ const DataManagerProvider = ({
 
       unlockAppWithAutoreload();
 
-      if (isCreating) {
+      if (isCreating && initialData.contentType?.schema.kind === 'collectionType') {
         setCurrentStep('contentTypeBuilder.success');
       }
 
