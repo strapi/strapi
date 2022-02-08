2f136aaf2ab1bcbb174e1046872af07f99ef7dc6
Alexandre Bodin <bodin.alex@gmail.com>
2022-02-08 22:23:02 +0100

    Add missing checks

--- .github/actions/check-pr-status/index.js
+++ .github/actions/check-pr-status/index.js
@@ -8,9 +8,29 @@ async function main() {
     const labels = context.payload.pull_request?.labels;
     const milestone = context.payload.pull_request?.milestone;
 
-    const hasBlockingLabel = labels.some(label => BLOCKING_LABELS.includes(label));
+    const hasBlockingLabel = labels.some(label => BLOCKING_LABELS.includes(label.name));
     if (hasBlockingLabel) {
-      core.setFailed(`This PR has been labelled with a blocking label (${BLOCKING_LABELS})`);
+      core.setFailed(
+        `The PR has been labelled with a blocking label (${BLOCKING_LABELS.join(', ')}).`
+      );
+
+      return;
+    }
+
+    const sourceLabelCount = labels.filter(label => label.name.startsWith('source: ')).length;
+    const issueLabelCount = labels.filter(label => label.name.startsWith('issue-type: ')).length;
+    const hasMilestone = !!milestone;
+
+    if (sourceLabelCount !== 1) {
+      core.setFailed(`The PR must have one and only one 'source:' label.`);
+    }
+
+    if (issueLabelCount !== 1) {
+      core.setFailed(`The PR must have one and only one 'issue-type:' label.`);
+    }
+
+    if (!hasMilestone) {
+      core.setFailed(`The PR must have a milestone.`);
     }
   } catch (error) {
     core.setFailed(error.message);
