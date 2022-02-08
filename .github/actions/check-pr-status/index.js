b1fc709509c7ac8058e80e1bd729bb177e8f25e0
Alexandre Bodin <bodin.alex@gmail.com>
2022-02-08 22:08:53 +0100

    Check for forbidden label

--- .github/actions/check-pr-status/index.js
+++ .github/actions/check-pr-status/index.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
 const { context } = require('@actions/github');
 
+const BLOCKING_LABELS = [`flag: 💥 Breaking change`, `flag: don't merge`];
+
 async function main() {
   try {
-    console.log(context.payload.pull_request.labels);
+    const labels = context.payload.pull_request?.labels;
+    const milestone = context.payload.pull_request?.milestone;
+
+    const hasBlockingLabel = labels.some(label => BLOCKING_LABELS.includes(label));
+    if (hasBlockingLabel) {
+      core.setFailed(`This PR has been labelled with a blocking label (${BLOCKING_LABELS})`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
