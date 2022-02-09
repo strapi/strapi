9e21f01a46bc9f937b898c4e5a86f278efde0821
Pierre Noël <petersg83@gmail.com>
2022-02-09 18:02:55 +0100

    update kitchensink config

--- examples/kitchensink/config/server.js
+++ examples/kitchensink/config/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6f75e424d1a0307077c294fcc3c7d78d'),
-    },
+  app: {
+    keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
   },
   app: {
     keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
