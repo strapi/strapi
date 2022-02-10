cebd7a892f5054d3f87026ee0860183f6739daef
harimkims <harimkims@gmail.com>
2022-02-10 17:08:59 +0900

    Fix mutation related to roles not working

--- packages/plugins/users-permissions/server/graphql/resolvers-configs.js
+++ packages/plugins/users-permissions/server/graphql/resolvers-configs.js
@@ -26,9 +26,9 @@ module.exports = ({ strapi }) => {
 
     // Scoped auth for replaced CRUD operations
     // Role
-    [`Mutation.${createRole}`]: { auth: { scope: [`${roleUID}.create`] } },
-    [`Mutation.${updateRole}`]: { auth: { scope: [`${roleUID}.update`] } },
-    [`Mutation.${deleteRole}`]: { auth: { scope: [`${roleUID}.delete`] } },
+    [`Mutation.${createRole}`]: { auth: { scope: [`${roleUID}.createRole`] } },
+    [`Mutation.${updateRole}`]: { auth: { scope: [`${roleUID}.updateRole`] } },
+    [`Mutation.${deleteRole}`]: { auth: { scope: [`${roleUID}.deleteRole`] } },
     // User
     [`Mutation.${createUser}`]: { auth: { scope: [`${userUID}.create`] } },
     [`Mutation.${updateUser}`]: { auth: { scope: [`${userUID}.update`] } },
