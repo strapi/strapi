65c03caede24a89e23d0075a29290ffec9d8e4c3
smoothdvd <madfxgao@gmail.com>
2022-02-11 18:51:01 +0800

    Update packages/plugins/users-permissions/server/bootstrap/index.js
    Co-authored-by: Alexandre BODIN <alexandrebodin@users.noreply.github.com>

--- packages/plugins/users-permissions/server/bootstrap/index.js
+++ packages/plugins/users-permissions/server/bootstrap/index.js
@@ -42,7 +42,7 @@ module.exports = async ({ strapi }) => {
 
 const initGrant = async pluginStore => {
   const apiPrefix = strapi.config.get('api.rest.prefix');
-  const baseURL = urljoin(`${strapi.config.server.url}/`, apiPrefix, '/auth');
+  const baseURL = urljoin(strapi.config.server.url, apiPrefix, 'auth');
 
   const grantConfig = {
     email: {
