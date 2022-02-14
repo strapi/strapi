37468e0824d24dafd5cd59c8bdd46d357527e56a
Gustav Hansen <gu@stav.dev>
2022-02-14 09:01:23 +0100

    DynamicTable: avoid wrapping single components in flex components

--- packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/SingleComponent/index.js
+++ packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/SingleComponent/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Flex } from '@strapi/design-system/Flex';
 import { Tooltip } from '@strapi/design-system/Tooltip';
 import { Typography } from '@strapi/design-system/Typography';
-import { stopPropagation } from '@strapi/helper-plugin';
 
 import CellValue from '../CellValue';
 
@@ -12,13 +10,11 @@ const SingleComponentCell = ({ value, metadatas }) => {
   const content = value[mainField.name];
 
   return (
-    <Flex {...stopPropagation}>
-      <Tooltip label={content}>
-        <Typography textColor="neutral800" ellipsis>
-          <CellValue type={mainField.type} value={content} />
-        </Typography>
-      </Tooltip>
-    </Flex>
+    <Tooltip label={content}>
+      <Typography textColor="neutral800" ellipsis>
+        <CellValue type={mainField.type} value={content} />
+      </Typography>
+    </Tooltip>
   );
 };
 
