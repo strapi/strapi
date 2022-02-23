529b6e41adc431fca1637e4bbf31a7c7a263395e
Mark Kaylor <mark.kaylor@strapi.io>
2022-02-23 17:15:14 +0100

    add remis last commit: Mock missing marketplace API calls

--- packages/core/admin/admin/src/pages/MarketplacePage/tests/index.test.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/tests/index.test.js
@@ -60,6 +60,10 @@ describe('Marketplace page', () => {
         padding-top: 8px;
       }
 
+      .c35 {
+        padding-left: 16px;
+      }
+
       .c32 {
         font-weight: 600;
         color: #32324d;
@@ -240,6 +244,17 @@ describe('Marketplace page', () => {
         align-items: normal;
       }
 
+      .c36 {
+        color: #328048;
+        margin-right: 8px;
+        width: 12;
+        height: 12;
+      }
+
+      .c37 path {
+        fill: #328048;
+      }
+
       .c20 {
         padding-top: 12px;
       }
@@ -276,6 +291,13 @@ describe('Marketplace page', () => {
 
       .c18 {
         color: #666687;
+        font-size: 0.875rem;
+        line-height: 1.43;
+      }
+
+      .c38 {
+        font-weight: 600;
+        color: #328048;
         font-size: 0.875rem;
         line-height: 1.43;
       }
@@ -982,38 +1004,28 @@ describe('Marketplace page', () => {
                             </svg>
                           </div>
                         </a>
-                        <button
-                          aria-disabled="false"
-                          class="c29 c30"
-                          type="button"
+                        <div
+                          class="c35"
                         >
+                          <svg
+                            class="c36 c37"
+                            fill="none"
+                            height="12"
+                            viewBox="0 0 24 24"
+                            width="12"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M20.727 2.97a.2.2 0 01.286 0l2.85 2.89a.2.2 0 010 .28L9.554 20.854a.2.2 0 01-.285 0l-9.13-9.243a.2.2 0 010-.281l2.85-2.892a.2.2 0 01.284 0l6.14 6.209L20.726 2.97z"
+                              fill="#212134"
+                            />
+                          </svg>
                           <span
-                            class="c31 c32"
+                            class="c38"
                           >
-                            Copy install command
+                            Installed
                           </span>
-                          <div
-                            aria-hidden="true"
-                            class="c33 c34"
-                          >
-                            <svg
-                              fill="none"
-                              height="1em"
-                              viewBox="0 0 24 24"
-                              width="1em"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M1.056 24h15.906c.583 0 1.056-.473 1.056-1.056V7.028c0-.583-.473-1.056-1.056-1.056H1.056C.473 5.972 0 6.445 0 7.028v15.916C0 23.527.473 24 1.056 24z"
-                                fill="#212134"
-                              />
-                              <path
-                                d="M8.094 2.111h13.795v13.795h-1.127v2.112h2.182A1.056 1.056 0 0024 16.962V1.056A1.056 1.056 0 0022.944 0H7.038a1.056 1.056 0 00-1.056 1.056v2.252h2.112V2.11z"
-                                fill="#212134"
-                              />
-                            </svg>
-                          </div>
-                        </button>
+                        </div>
                       </div>
                     </div>
                   </div>
