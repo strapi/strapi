52bd33fa6d1d2a62163f944382d0b3a99b73468a
B Hamilton <hamilton.b.develop@gmail.com>
2022-02-24 11:20:15 +0000

    repeatable relations - show title, not blank title

--- packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/utils/select.js
+++ packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/utils/select.js
@@ -12,9 +12,13 @@ function useSelect({ schema, componentFieldName }) {
   } = useCMEditViewDataManager();
 
   const mainField = useMemo(() => get(schema, ['settings', 'mainField'], 'id'), [schema]);
-  const displayedValue = toString(
-    get(modifiedData, [...componentFieldName.split('.'), mainField], '')
-  );
+  const nestedObjectTitle = schema.layouts.edit?.[0]?.[0]?.metadatas?.mainField?.name;
+  const nestedObjectField = schema.layouts.edit?.[0]?.[0]?.name;
+  const displayValuePath =
+    mainField === 'id' && !!nestedObjectTitle
+      ? [...componentFieldName.split('.'), nestedObjectField, nestedObjectTitle]
+      : [...componentFieldName.split('.'), mainField];
+  const displayedValue = toString(get(modifiedData, displayValuePath, ''));
 
   return {
     displayedValue,
