8741b0fc9fbea9e2bc854d9ab543d40e30c97ffb
Mark Kaylor <mark.kaylor@strapi.io>
2022-03-04 15:17:55 +0100

    remove stack

--- packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/EmptyPluginGrid.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/EmptyPluginGrid.js
@@ -4,7 +4,8 @@ import { Box } from '@strapi/design-system/Box';
 import { GridLayout } from '@strapi/design-system/Layout';
 
 const EmptyPluginCard = styled(Box)`
-  background: linear-gradient(180deg, rgba(234, 234, 239, 0) 0%, #eaeaef 100%);
+  background: ${({ theme }) =>
+    `linear-gradient(180deg, rgba(234, 234, 239, 0) 0%, ${theme.colors.neutral150} 100%)`};
   opacity: 0.33;
 `;
 
--- packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/index.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@strapi/design-system/Typography';
-import { Stack } from '@strapi/design-system/Stack';
 import { Box } from '@strapi/design-system/Box';
+import { Flex } from '@strapi/design-system/Flex';
 import { Icon } from '@strapi/design-system/Icon';
 import EmptyStateDocument from '@strapi/icons/EmptyDocuments';
 import { EmptyPluginGrid } from './EmptyPluginGrid';
@@ -12,14 +12,14 @@ export const EmptyPluginSearch = ({ content }) => {
     <Box position="relative">
       <EmptyPluginGrid />
       <Box position="absolute" top={11} width="100%">
-        <Stack style={{ alignItems: 'center' }} size={4} textAlign="center">
-          <Stack style={{ alignItems: 'center' }} size={6}>
-            <Icon as={EmptyStateDocument} color="" width="160px" height="88px" />
+        <Flex alignItems="center" justifyContent="center" direction="column">
+          <Icon as={EmptyStateDocument} color="" width="160px" height="88px" />
+          <Box marginTop={6}>
             <Typography variant="delta" as="p" textColor="neutral600">
               {content}
             </Typography>
-          </Stack>
-        </Stack>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
