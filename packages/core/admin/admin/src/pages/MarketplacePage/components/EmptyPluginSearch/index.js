3cb1464a2a4dcedcdb042bebfb7b713c5818ae8b
Mark Kaylor <mark.kaylor@strapi.io>
2022-03-04 14:32:56 +0100

    use inline styles vs styled-component on ds component

--- packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/index.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/components/EmptyPluginSearch/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { Typography } from '@strapi/design-system/Typography';
 import { Stack } from '@strapi/design-system/Stack';
 import { Box } from '@strapi/design-system/Box';
@@ -8,23 +7,19 @@ import { Icon } from '@strapi/design-system/Icon';
 import EmptyStateDocument from '@strapi/icons/EmptyDocuments';
 import { EmptyPluginGrid } from './EmptyPluginGrid';
 
-const EnhancedStack = styled(Stack)`
-  align-items: center;
-`;
-
 export const EmptyPluginSearch = ({ content }) => {
   return (
     <Box position="relative">
       <EmptyPluginGrid />
       <Box position="absolute" top={11} width="100%">
-        <EnhancedStack size={4} textAlign="center">
-          <EnhancedStack size={6}>
+        <Stack style={{ alignItems: 'center' }} size={4} textAlign="center">
+          <Stack style={{ alignItems: 'center' }} size={6}>
             <Icon as={EmptyStateDocument} color="" width="160px" height="88px" />
             <Typography variant="delta" as="p" textColor="neutral600">
               {content}
             </Typography>
-          </EnhancedStack>
-        </EnhancedStack>
+          </Stack>
+        </Stack>
       </Box>
     </Box>
   );
