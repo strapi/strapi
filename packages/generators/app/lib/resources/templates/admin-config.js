116505d892c389ffdbb96b5b1c5d87d1adec2c4c
Convly <jean-sebastien.herbaux@epitech.eu>
2022-03-09 16:27:49 +0100

    Handle ts projects with generators

--- packages/generators/app/lib/resources/templates/admin-config.js
+++ packages/generators/app/lib/resources/templates/admin-config.js
@@ -6,11 +6,15 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 
-module.exports = () => {
-  const tmpl = fs.readFileSync(path.join(__dirname, `admin-config.template`));
+module.exports = ({ useTypescript }) => {
+  // resolve ts or js version depending on project type
+  const language = useTypescript ? 'ts' : 'js';
+
+  const tmpl = fs.readFileSync(path.join(__dirname, language, `admin-config.template`));
   const compile = _.template(tmpl);
 
   return compile({
     adminJwtToken: crypto.randomBytes(16).toString('hex'),
+    useTypescript,
   });
 };
