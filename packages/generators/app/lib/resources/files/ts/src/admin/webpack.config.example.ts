764e4336ecc8b1543b0be8deeebe572a2438ab13
Convly <jean-sebastien.herbaux@epitech.eu>
2022-03-09 16:27:49 +0100

    Handle ts projects with generators

---
+++ packages/generators/app/lib/resources/files/ts/src/admin/webpack.config.example.ts
@@ -0,0 +1,6 @@
+export default (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  return config;
+};
