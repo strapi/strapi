34c4f349308129035c1e4115d07f287da55e824b
Convly <jean-sebastien.herbaux@epitech.eu>
2022-03-09 10:21:46 +0100

    Use primary process instead of master process

--- packages/core/strapi/lib/commands/develop.js
+++ packages/core/strapi/lib/commands/develop.js
@@ -25,7 +25,7 @@ module.exports = async function({ build, watchAdmin, polling, browser }) {
 
   try {
     if (cluster.isMaster || cluster.isPrimary) {
-      return masterProcess({ dir, build, browser });
+      return primaryProcess({ dir, build, browser });
     }
 
     if (cluster.isWorker) {
@@ -37,7 +37,7 @@ module.exports = async function({ build, watchAdmin, polling, browser }) {
   }
 };
 
-const masterProcess = async ({ dir, build, watchAdmin, browser }) => {
+const primaryProcess = async ({ dir, build, watchAdmin, browser }) => {
   const currentDirectory = process.cwd();
 
   if (tsUtils.isTypeScriptProject(currentDirectory)) {
