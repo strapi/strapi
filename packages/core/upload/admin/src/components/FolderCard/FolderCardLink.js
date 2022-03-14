88d1beb704b7556445cc87069c20400b6ea68dd9
Gustav Hansen <gustav.hansen@strapi.io>
2022-03-14 08:40:20 +0100

    ML: Add FolderCard component

---
+++ packages/core/upload/admin/src/components/FolderCard/FolderCardLink.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { BaseLink } from '@strapi/design-system/BaseLink';
+
+export const FolderCardLink = styled(BaseLink)`
+  text-decoration: none;
+`;
