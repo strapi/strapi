d85a387b24e40e63c86cf03ae4ad486704de57e1
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-03-15 15:20:50 +0700

    Make jest does not watch .json file in the --watch mode

--- jest.base-config.js
+++ jest.base-config.js
@@ -3,4 +3,5 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/unit.setup.js'],
   modulePathIgnorePatterns: ['.cache'],
   testMatch: ['/**/__tests__/**/*.[jt]s?(x)'],
+  watchPathIgnorePatterns: ['<rootDir>/.*\\.json'],
 };
