00abd419f27765fec6dc46a1d818d6c84e97e206
Convly <jean-sebastien.herbaux@epitech.eu>
2022-03-15 18:43:19 +0100

    Cleanup tsconfig & clean the dist dir before build

--- packages/core/strapi/lib/commands/builders/typescript.js
+++ packages/core/strapi/lib/commands/builders/typescript.js
@@ -1,13 +1,28 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs-extra');
 const tsUtils = require('@strapi/typescript-utils');
 
-module.exports = async ({ srcDir, watch = false }) => {
+const cleanupDistDirectory = async distDir => {
+  const dirContent = await fs.readdir(distDir);
+  const validFilenames = dirContent
+    // Ignore the admin build folder
+    .filter(filename => filename !== 'build');
+
+  for (const filename of validFilenames) {
+    await fs.remove(path.resolve(distDir, filename));
+  }
+};
+
+module.exports = async ({ srcDir, distDir, watch = false }) => {
   const isTSProject = await tsUtils.isTypeScriptProject(srcDir);
 
   if (!isTSProject) {
     throw new Error(`tsconfig file not found in ${srcDir}`);
   }
+
+  await cleanupDistDirectory(distDir);
 
   return tsUtils.compile(srcDir, { watch });
 };
