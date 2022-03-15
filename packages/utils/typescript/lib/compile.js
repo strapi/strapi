7340836b14aa2f885cd15c01df6562d8da451947
Convly <jean-sebastien.herbaux@epitech.eu>
2022-03-15 16:13:17 +0100

    Init new develop

--- packages/utils/typescript/lib/compile.js
+++ packages/utils/typescript/lib/compile.js
@@ -4,7 +4,7 @@ const compilers = require('./compilers');
 const getConfigPath = require('./utils/get-config-path');
 const copyResources = require('./utils/copy-resources');
 
-module.exports = async (srcDir, { watch = false }) => {
+module.exports = async (srcDir, { watch = false } = {}) => {
   // TODO: Use the Strapi debug logger instead or don't log at all
   console.log(`Starting the compilation for TypeScript files in ${srcDir}`);
 
