ca8407fdadeb0af63209410f69e368b5d1614378
vincentbpro <89356961+vincentbpro@users.noreply.github.com>
2022-03-15 11:34:55 +0100

    fix selected value text color

--- packages/core/admin/admin/src/content-manager/components/SelectWrapper/utils/getSelectStyles.js
+++ packages/core/admin/admin/src/content-manager/components/SelectWrapper/utils/getSelectStyles.js
@@ -78,7 +78,7 @@ const getSelectStyles = theme => {
       return { ...base, lineHeight: theme.spaces[5], backgroundColor, borderRadius: 4 };
     },
     placeholder: base => ({ ...base, marginLeft: 0 }),
-    singleValue: base => ({ ...base, marginLeft: 0 }),
+    singleValue: base => ({ ...base, marginLeft: 0, color: theme.colors.neutral800 }),
     valueContainer: base => ({
       ...base,
       padding: 0,
