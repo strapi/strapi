aea7e9fccf2d48bc536bf6d0bc275db8d7978ef4
Pierre Noël <petersg83@gmail.com>
2022-03-15 18:51:52 +0100

    refactor upload bootstrap

--- packages/core/upload/server/bootstrap.js
+++ packages/core/upload/server/bootstrap.js
@@ -49,33 +49,34 @@ const createProvider = config => {
 
   const providerInstance = provider.init(providerOptions);
 
-  return Object.assign(Object.create(baseProvider), {
-    ...providerInstance,
-    original: providerInstance,
-    uploadStream(file, options = actionOptions.upload) {
-      return providerInstance.uploadStream(file, options);
-    },
-    upload(file, options = actionOptions.upload) {
-      return providerInstance.upload(file, options);
-    },
-    delete(file, options = actionOptions.delete) {
-      return providerInstance.delete(file, options);
-    },
+  if (!providerInstance.delete) {
+    throw new Error(`The upload provider "${providerName}" didn't implement the delete method.`);
+  }
+
+  if (!providerInstance.upload && !providerInstance.uploadStream) {
+    throw new Error(
+      `The upload provider "${providerName}" didn't implement the uploadStream nor the upload method.`
+    );
+  }
+
+  if (!providerInstance.uploadStream) {
+    process.emitWarning(
+      `The upload provider "${providerName}" didn't implement the uploadStream function. Strapi will fallback on the upload method. Some performance issues may occur.`
+    );
+  }
+
+  const wrappedProvider = _.mapValues(providerInstance, (method, methodName) => {
+    return async function(file, options = actionOptions[methodName]) {
+      return providerInstance[methodName](file, options);
+    };
   });
+
+  return Object.assign(Object.create(baseProvider), wrappedProvider);
 };
 
 const baseProvider = {
   extend(obj) {
     Object.assign(this, obj);
-  },
-  uploadStream() {
-    throw new Error('Provider uploadStream method is not implemented');
-  },
-  upload() {
-    throw new Error('Provider upload method is not implemented');
-  },
-  delete() {
-    throw new Error('Provider delete method is not implemented');
   },
 };
 
