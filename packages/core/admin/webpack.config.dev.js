e0fb1fe0f33e24d3b3fd5521335f1f09ec5f13af
soupette <cyril@strapi.io>
2022-03-18 15:21:20 +0100

    Speed up webpack config by using esbuild-loader
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/webpack.config.dev.js
+++ packages/core/admin/webpack.config.dev.js
@@ -1,15 +1,40 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs-extra');
+
+// eslint-disable-next-line node/no-extraneous-require
+const glob = require('glob');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { DuplicateReporterPlugin } = require('duplicate-dependencies-webpack-plugin');
 
 const webpackConfig = require('./webpack.config');
 
+const getPluginsPath = () => {
+  const rootPath = path.join('..', '..', '..', 'packages');
+  const corePath = path.join(rootPath, 'core', '*');
+  const pluginsPath = path.join(rootPath, 'plugins', '*');
+  const corePackageDirs = glob.sync(corePath);
+  const pluginsPackageDirs = glob.sync(pluginsPath);
+  const packageDirs = [...corePackageDirs, ...pluginsPackageDirs].filter(dir => {
+    const isCoreAdmin = dir.includes('packages/core/admin/');
+    const pathToEntryPoint = path.join(dir, 'admin', 'src', 'index.js');
+    const doesAdminFolderExist = fs.pathExistsSync(pathToEntryPoint);
+
+    return !isCoreAdmin && doesAdminFolderExist;
+  });
+
+  return packageDirs.map(dir => path.resolve(__dirname, path.join(dir, 'admin', 'src')));
+};
+
 module.exports = () => {
   const analyzeBundle = process.env.ANALYZE_BUNDLE;
   const analyzeDuplicateDependencies = process.env.ANALYZE_DEPS;
-  const entry = path.join(__dirname, 'admin', 'src');
+  // Directly inject a polyfill in the webpack entry point before the entry point
+  // FIXME: I have noticed a bug regarding the helper-plugin and esbuild-loader
+  // The only I could fix it was to inject the babel polyfill
+  const babelPolyfill = '@babel/polyfill/dist/polyfill.min.js';
+  const entry = [babelPolyfill, path.join(__dirname, 'admin', 'src')];
   const dest = path.join(__dirname, 'build');
 
   // When running the analyze:bundle command, it needs a production build
@@ -23,7 +48,7 @@ module.exports = () => {
   const args = {
     entry,
     cacheDir: __dirname,
-    pluginsPath: [path.resolve(__dirname, '../../../packages')],
+    pluginsPath: getPluginsPath(),
     dest,
     env,
     options,
@@ -41,27 +66,6 @@ module.exports = () => {
 
   return {
     ...config,
-    snapshot: {
-      managedPaths: [
-        path.resolve(__dirname, '../content-type-builder'),
-        path.resolve(__dirname, '../upload'),
-        path.resolve(__dirname, '../helper-plugin'),
-      ],
-      buildDependencies: {
-        hash: true,
-        timestamp: true,
-      },
-      module: {
-        timestamp: true,
-      },
-      resolve: {
-        timestamp: true,
-      },
-      resolveBuildDependencies: {
-        hash: true,
-        timestamp: true,
-      },
-    },
 
     devServer: {
       port: 4000,
