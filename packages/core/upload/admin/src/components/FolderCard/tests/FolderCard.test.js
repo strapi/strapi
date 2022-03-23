23ca6c41baefea9f85968a9db811d775bb16f188
Gustav Hansen <gustav.hansen@strapi.io>
2022-03-23 14:40:53 +0100

    FolderCard: Update tests

--- packages/core/upload/admin/src/components/FolderCard/tests/FolderCard.test.js
+++ packages/core/upload/admin/src/components/FolderCard/tests/FolderCard.test.js
@@ -3,7 +3,7 @@ import { BaseLink } from '@strapi/design-system/BaseLink';
 import { Flex } from '@strapi/design-system/Flex';
 import { ThemeProvider, lightTheme } from '@strapi/design-system';
 import { Typography } from '@strapi/design-system/Typography';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 
 import { FolderCard } from '../FolderCard';
 import { FolderCardBody } from '../FolderCardBody';
@@ -12,7 +12,7 @@ import { FolderCardCheckbox } from '../FolderCardCheckbox';
 const ID_FIXTURE = 'folder-1';
 
 // eslint-disable-next-line react/prop-types
-const ComponentFixture = ({ children, ...props }) => {
+const ComponentFixture = props => {
   return (
     <ThemeProvider theme={lightTheme}>
       <FolderCard
@@ -23,30 +23,6 @@ const ComponentFixture = ({ children, ...props }) => {
         onDoubleClick={() => {}}
         {...props}
       >
-        {children || ''}
-      </FolderCard>
-    </ThemeProvider>
-  );
-};
-
-describe('FolderCard', () => {
-  it('renders and matches the snapshot', () => {
-    const { container } = render(<ComponentFixture />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it('properly calls the onDoubleClick callback', () => {
-    const callback = jest.fn();
-    const { container } = render(<ComponentFixture onDoubleClick={callback} />);
-
-    fireEvent(container.querySelector('a'), new MouseEvent('dblclick', { bubbles: true }));
-
-    expect(callback).toHaveBeenCalledTimes(1);
-  });
-
-  it('has all required ids set when rendering a start action', () => {
-    const { container } = render(
-      <ComponentFixture startAction={<FolderCardCheckbox value={false} />}>
         <FolderCardBody as="h2">
           <BaseLink href="https://strapi.io" textDecoration="none">
             <Flex direction="column" alignItems="flex-start">
@@ -56,10 +32,39 @@ describe('FolderCard', () => {
             </Flex>
           </BaseLink>
         </FolderCardBody>
-      </ComponentFixture>
-    );
+      </FolderCard>
+    </ThemeProvider>
+  );
+};
 
-    expect(container.querySelector(`[id="${ID_FIXTURE}-title"]`)).toBeInTheDocument();
-    expect(container.querySelector(`[aria-labelledby="${ID_FIXTURE}-title"]`)).toBeInTheDocument();
+const setup = props => render(<ComponentFixture {...props} />);
+
+describe('FolderCard', () => {
+  test('renders and matches the snapshot', () => {
+    const { container } = setup();
+    expect(container).toMatchSnapshot();
+  });
+
+  test('properly calls the onDoubleClick callback', () => {
+    const callback = jest.fn();
+    const { container } = setup({ onDoubleClick: callback });
+
+    act(() => {
+      fireEvent(container.querySelector('button'), new MouseEvent('dblclick', { bubbles: true }));
+    });
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  test('has all required ids set when rendering a start action', () => {
+    const { container } = setup({
+      startAction: <FolderCardCheckbox value={false} />,
+    });
+
+    expect(container).toMatchSnapshot();
+    expect(container.querySelector(`[id="${ID_FIXTURE}-3-title"]`)).toBeInTheDocument();
+    expect(
+      container.querySelector(`[aria-labelledby="${ID_FIXTURE}-3-title"]`)
+    ).toBeInTheDocument();
   });
 });
