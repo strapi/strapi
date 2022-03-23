5f66da98644f46e2d509b21811c692fe031d876a
soupette <cyril@strapi.io>
2022-03-23 10:39:31 +0100

    Add tests
    Signed-off-by: soupette <cyril@strapi.io>

--- packages/core/admin/utils/get-custom-app-config-file.js
+++ packages/core/admin/utils/get-custom-app-config-file.js
@@ -5,8 +5,8 @@ const fse = require('fs-extra');
 
 /**
  * Retrieve the custom admin entry file name
- * @param {String} dir
- * @param {Boolean} useTypeScript
+ * @param {String} dir - Directory of the admin panel
+ * @param {Boolean} useTypeScript - Does the custom admin entry file use TS
  * @returns String
  */
 const getCustomAppConfigFile = async (dir, useTypeScript) => {
