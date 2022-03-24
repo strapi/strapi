045399aeab95b81a08a4fb64a4b159105e3522d1
Gustav Hansen <gustav.hansen@strapi.io>
2022-03-24 17:31:44 +0100

    FolderCard: Restructure component files

--- packages/core/upload/admin/src/components/FolderCard/contexts/FolderCard.js
+++ packages/core/upload/admin/src/components/FolderCard/contexts/FolderCard.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react';
 
-export const FolderCardContext = createContext();
+export const FolderCardContext = createContext({});
 
 export function useFolderCard() {
   return useContext(FolderCardContext);
--- packages/core/upload/admin/src/components/FolderCard/hooks/useId.js
+++ packages/core/upload/admin/src/components/FolderCard/hooks/useId.js
@@ -4,8 +4,8 @@ let id = 0;
 
 const genId = () => ++id;
 
-const useId = (prefix, initialId) => {
-  const idRef = useRef(initialId || `${prefix}-${genId()}`);
+const useId = initialId => {
+  const idRef = useRef(`${initialId}-${genId()}`);
 
   return idRef.current;
 };
