9fb6bea5e21e92b1d209fa656c85ab7af58a2964
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:05:33 -0400

    spelling: permissions
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/admin/admin/src/contexts/index.js
+++ packages/core/admin/admin/src/contexts/index.js
@@ -1,4 +1,4 @@
 export { default as AdminContext } from './Admin';
 export { default as ConfigurationsContext } from './Configurations';
-export { default as PermissionsDataManagerContext } from './PermisssionsDataManagerContext';
+export { default as PermissionsDataManagerContext } from './PermissionsDataManagerContext';
 export { default as ThemeToggleContext } from './ThemeToggle';
