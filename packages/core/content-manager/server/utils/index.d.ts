422f19d90d6fce2f9826f4c2225c316989597c23
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:57:05 -0400

    spelling: metrics
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/content-manager/server/utils/index.d.ts
+++ packages/core/content-manager/server/utils/index.d.ts
@@ -3,7 +3,7 @@ import * as configuration from '../services/configuration';
 import * as contentTypes from '../services/content-types';
 import * as dataMapper from '../services/data-mapper';
 import * as entityManager from '../services/entity-manager';
-import * as metris from '../services/metris';
+import * as metrics from '../services/metrics';
 import * as permissionChecker from '../services/permission-checker';
 import * as permission from '../services/permission';
 import * as uid from '../services/uid';
@@ -15,7 +15,7 @@ type S = {
   ['permission-checker']: typeof permissionChecker;
   components: typeof components;
   configuration: typeof configuration;
-  metris: typeof metris;
+  metrics: typeof metrics;
   permission: typeof permission;
   uid: typeof uid;
 };
