b90d47e9e40020bfc260531a8330eaaac77c4321
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:06:08 -0400

    spelling: writing
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/strapi/lib/commands/configurationDump.js
+++ packages/core/strapi/lib/commands/configurationDump.js
@@ -43,7 +43,7 @@ module.exports = async function({ file: filePath, pretty }) {
   output.write('\n');
   output.end();
 
-  // log success only when writting to file
+  // log success only when writing to file
   if (filePath) {
     console.log(`Successfully exported ${exportData.length} configuration entries`);
   }
