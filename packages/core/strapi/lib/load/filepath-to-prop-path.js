914f5ea11a83ca585d5d013b36dc64e5f465a66c
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:06:08 -0400

    spelling: whether
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/strapi/lib/load/filepath-to-prop-path.js
+++ packages/core/strapi/lib/load/filepath-to-prop-path.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 /**
  * Returns a path (as an array) from a file path
  * @param {string} filePath - a file path
- * @param {boolean} useFileNameAsKey - wethear to skip the last path key
+ * @param {boolean} useFileNameAsKey - whether to skip the last path key
  */
 module.exports = (filePath, useFileNameAsKey = true) => {
   let cleanPath = filePath.startsWith('./') ? filePath.slice(2) : filePath;
