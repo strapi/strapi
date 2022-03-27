ae45fd187ca7470c6682d277321d9c0d9bd637c5
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:05:49 -0400

    spelling: retrieve
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/admin/admin/src/content-manager/components/InputJSON/jsonlint.js
+++ packages/core/admin/admin/src/content-manager/components/InputJSON/jsonlint.js
@@ -285,7 +285,7 @@ var jsonlint = (function() {
         newState,
         expected;
       while (true) {
-        // retreive state number from top of stack
+        // retrieve state number from top of stack
         state = stack[stack.length - 1];
 
         // use default actions if available
