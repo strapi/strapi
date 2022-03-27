d585dd8e2dbbf67534e58e42e36bd894c7d13900
Josh Soref <jsoref@users.noreply.github.com>
2022-03-26 23:05:36 -0400

    spelling: product
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core/content-type-builder/tests/front/integrationOld/createApi_specs.js
+++ packages/core/content-type-builder/tests/front/integrationOld/createApi_specs.js
@@ -212,13 +212,13 @@ describe('Test CTB', () => {
         .should('be.visible')
         .get('input[name="name"]')
         .type('{selectall}')
-        .type('produit')
+        .type('product')
         .submitForm()
         .wait('@updateProductModel')
         .wait(frontLoadingDelay);
 
       // Check that we can still go to the create page
-      cy.get('a[href="/admin/plugins/content-manager/produit?source=content-manager"')
+      cy.get('a[href="/admin/plugins/content-manager/product?source=content-manager"')
         .click()
         .wait(frontLoadingDelay)
         .get('button[label="content-manager.containers.List.addAnEntry"')
@@ -242,7 +242,7 @@ describe('Test CTB', () => {
 
   after(() => {
     cy.deleteApi('tag', jwt)
-      .deleteApi('produit', jwt)
+      .deleteApi('product', jwt)
       .deleteUser(userId, jwt);
   });
 });
