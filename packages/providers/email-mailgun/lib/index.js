e4fb25765d4cd5a83252648d21eafb22d7e9b536
Torgny Bjers <torgny@hey.com>
2022-03-28 14:57:41 -0400

    update options converter
    - pass through options that are not matched
    - merge defaults with passed options
    - add unit tests

--- packages/providers/email-mailgun/lib/index.js
+++ packages/providers/email-mailgun/lib/index.js
@@ -6,26 +6,30 @@ const { removeUndefined } = require('@strapi/utils');
 
 const optionsMap = {
   apiKey: { field: 'key', fn: value => value },
-  domain: { field: 'domain', fn: value => value },
   host: { field: 'url', fn: value => `https://${value || 'api.mailgun.net'}` },
 };
 
-const convertProviderOptions = (providerOptions = {}) => {
-  const newOptions = {};
-  if (typeof providerOptions === 'object') {
-    Object.keys(providerOptions).forEach(key => {
-      if (Object.keys(optionsMap).includes(key)) {
-        newOptions[optionsMap[key].field] = optionsMap[key].fn(providerOptions[key]);
-      }
-    });
-  }
-  return newOptions;
-};
-
 module.exports = {
+  convertProviderOptions(providerOptions = {}) {
+    const newOptions = {};
+    if (typeof providerOptions === 'object') {
+      Object.keys(providerOptions).forEach(key => {
+        if (Object.keys(optionsMap).includes(key)) {
+          newOptions[optionsMap[key].field] = optionsMap[key].fn(providerOptions[key]);
+        } else {
+          newOptions[key] = providerOptions[key];
+        }
+      });
+    }
+    return newOptions;
+  },
+
   init(providerOptions = {}, settings = {}) {
+    const defaults = {
+      username: 'api',
+    };
     const mailgun = new Mailgun(formData);
-    const mg = mailgun.client({ username: 'api', ...convertProviderOptions(providerOptions) });
+    const mg = mailgun.client({ ...defaults, ...this.convertProviderOptions(providerOptions) });
 
     return {
       send(options) {
