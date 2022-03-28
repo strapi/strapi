2ae631d90300e3fa78921d7380767612baa73f1a
Pierre Noël <petersg83@gmail.com>
2022-03-28 16:51:11 +0200

    forbid folder name starting or ending with a whitespace

--- packages/core/upload/server/controllers/validation/folder.js
+++ packages/core/upload/server/controllers/validation/folder.js
@@ -3,6 +3,7 @@
 const { yup, validateYupSchema } = require('@strapi/utils');
 
 const NO_SLASH_REGEX = /^[^/]+$/;
+const NO_SPACES_AROUND = /^(?! ).+(?<! )$/;
 
 const validateCreateFolderSchema = yup
   .object()
@@ -11,6 +12,7 @@ const validateCreateFolderSchema = yup
       .string()
       .min(1)
       .matches(NO_SLASH_REGEX, 'name cannot contain slashes')
+      .matches(NO_SPACES_AROUND, 'name cannot start or end with a whitespace')
       .required(),
     parent: yup.strapiID().nullable(),
   })
