146d9e4e9c56c8393c9c9245fa0dc2ea9cb35812
vincentbpro <89356961+vincentbpro@users.noreply.github.com>
2022-03-29 16:55:04 +0200

    test: update e2e permission snapshots

--- packages/core/admin/server/tests/admin-permission.test.e2e.js
+++ packages/core/admin/server/tests/admin-permission.test.e2e.js
@@ -336,6 +336,12 @@ describe('Role CRUD End to End', () => {
                 "subCategory": "marketplace",
               },
               Object {
+                "action": "admin::project-settings.update",
+                "category": "project",
+                "displayName": "Update the project level settings",
+                "subCategory": "general",
+              },
+              Object {
                 "action": "admin::roles.create",
                 "category": "users and roles",
                 "displayName": "Create",
@@ -1233,6 +1239,12 @@ describe('Role CRUD End to End', () => {
                   "category": "plugins and marketplace",
                   "displayName": "Access the marketplace",
                   "subCategory": "marketplace",
+                },
+                Object {
+                  "action": "admin::project-settings.update",
+                  "category": "project",
+                  "displayName": "Update the project level settings",
+                  "subCategory": "general",
                 },
                 Object {
                   "action": "admin::roles.create",
