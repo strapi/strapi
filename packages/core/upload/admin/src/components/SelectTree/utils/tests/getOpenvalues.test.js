be68fe2957ff01560904acd3ee480876965da887
Gustav Hansen <gustav.hansen@strapi.io>
2022-04-08 16:19:53 +0200

    Update test

--- packages/core/upload/admin/src/components/SelectTree/utils/tests/getOpenvalues.test.js
+++ packages/core/upload/admin/src/components/SelectTree/utils/tests/getOpenvalues.test.js
@@ -32,14 +32,14 @@ const FIXTURE = flattenTree([
 
 describe('getOpenValues', () => {
   test('returns 1 value for depth = 1', () => {
-    expect(getOpenValues(FIXTURE, 'f-1')).toStrictEqual(['f-1']);
+    expect(getOpenValues(FIXTURE, { value: 'f-1' })).toStrictEqual(['f-1']);
   });
 
   test('returns 2 values for depth = 2', () => {
-    expect(getOpenValues(FIXTURE, 'f-2-1')).toStrictEqual(['f-2-1', 'f-2']);
+    expect(getOpenValues(FIXTURE, { value: 'f-2-1' })).toStrictEqual(['f-2-1', 'f-2']);
   });
 
   test('returns 3 values for depth = 3', () => {
-    expect(getOpenValues(FIXTURE, 'f-2-2-1')).toStrictEqual(['f-2-2-1', 'f-2-2', 'f-2']);
+    expect(getOpenValues(FIXTURE, { value: 'f-2-2-1' })).toStrictEqual(['f-2-2-1', 'f-2-2', 'f-2']);
   });
 });
