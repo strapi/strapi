da5c6ff367b210f0e45cc8d81cb0b87727807910
Bassel Kanso <basselkanso82@gmail.com>
2022-05-09 02:07:30 +0300

    fix factories typings

--- packages/core/strapi/lib/factories.d.ts
+++ packages/core/strapi/lib/factories.d.ts
@@ -2,35 +2,36 @@ import { Service } from './core-api/service';
 import { Controller } from './core-api/controller';
 import { Middleware } from './middlewares';
 import { Policy } from './core/registries/policies';
+import { Strapi } from '@strapi/strapi'
 
 type ControllerConfig = Controller;
 
 type ServiceConfig = Service;
 
 type HandlerConfig = {
-  auth: false | { scope: string[] };
-  policies: Array<string | Policy>;
-  middlewares: Array<string | Middleware>;
+  auth?: false | { scope: string[] };
+  policies?: Array<string | Policy>;
+  middlewares?: Array<string | Middleware>;
 };
 
 type SingleTypeRouterConfig = {
-  find: HandlerConfig;
-  update: HandlerConfig;
-  delete: HandlerConfig;
+  find?: HandlerConfig;
+  update?: HandlerConfig;
+  delete?: HandlerConfig;
 };
 
 type CollectionTypeRouterConfig = {
-  find: HandlerConfig;
-  findOne: HandlerConfig;
-  create: HandlerConfig;
-  update: HandlerConfig;
-  delete: HandlerConfig;
+  find?: HandlerConfig;
+  findOne?: HandlerConfig;
+  create?: HandlerConfig;
+  update?: HandlerConfig;
+  delete?: HandlerConfig;
 };
 
 type RouterConfig = {
-  prefix: string;
+  prefix?: string;
   only: string[];
-  except: string[];
+  except?: string[];
   config: SingleTypeRouterConfig | CollectionTypeRouterConfig;
 };
 
@@ -43,6 +44,8 @@ interface Router {
   routes: Route[];
 }
 
+type Callback = (params:{strapi:Strapi}) => any;
+
 export function createCoreRouter(uid: string, cfg?: RouterConfig = {}): () => Router;
-export function createCoreController(uid: string, cfg?: ControllerConfig = {}): () => Controller;
-export function createCoreService(uid: string, cfg?: ServiceConfig = {}): () => Service;
+export function createCoreController(uid: string, cfg?: ControllerConfig | Callback = {}): () => Controller;
+export function createCoreService(uid: string, cfg?: ServiceConfig | Callback = {}): () => Service;
