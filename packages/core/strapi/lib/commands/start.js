0ba862f990b0f3492d5e654899a5c473d0b69790
Bassel Kanso <basselkanso82@gmail.com>
2022-05-20 15:53:42 +0300

    throw error incase outDir folder doesn't exist

--- packages/core/strapi/lib/commands/start.js
+++ packages/core/strapi/lib/commands/start.js
@@ -1,4 +1,5 @@
 'use strict';
+const fs = require('fs');
 const tsUtils = require('@strapi/typescript-utils');
 const strapi = require('../index');
 
@@ -9,11 +10,8 @@ module.exports = async specifiedDir => {
   const appDir = process.cwd();
   const isTSProject = await tsUtils.isUsingTypeScript(appDir);
   const outDir = await tsUtils.resolveOutDir(appDir);
-  if (isTSProject)
-    await tsUtils.compile(appDir, {
-      watch: false,
-      configOptions: { options: { incremental: true } },
-    });
+  const buildDirExists = fs.existsSync(outDir);
+  if (isTSProject && !buildDirExists) throw new Error('Please build first to run this command');
   const distDir = isTSProject && !specifiedDir ? outDir : specifiedDir;
 
   strapi({ distDir }).start();
