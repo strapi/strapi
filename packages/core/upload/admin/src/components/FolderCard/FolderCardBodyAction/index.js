2d1fd4b295b1b842e12d68b8a1eef8dd26800ca8
ronronscelestes <julie.plantey@gmail.com>
2022-05-24 13:23:35 +0200

    FolderCardBodyAction feedback fix

--- packages/core/upload/admin/src/components/FolderCard/FolderCardBodyAction/index.js
+++ packages/core/upload/admin/src/components/FolderCard/FolderCardBodyAction/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { Box } from '@strapi/design-system/Box';
 
-const FolderCardLink = styled(NavLink)`
-  max-width: 100%;
+const BoxTextDecoration = styled(Box)`
   text-decoration: none;
 `;
 
 export const FolderCardBodyAction = ({ to, ...props }) => {
   if (to) {
-    return <FolderCardLink to={to} {...props} />;
+    return <BoxTextDecoration as={NavLink} maxWidth="100%" to={to} {...props} />;
   }
 
-  return <button type="button" {...props} />;
+  return <Box as="button" type="button" maxWidth="100%" {...props} />;
 };
 
 FolderCardBodyAction.defaultProps = {
