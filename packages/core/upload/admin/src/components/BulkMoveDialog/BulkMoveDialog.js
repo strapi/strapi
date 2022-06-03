ec42375ee4d875a2b085f5f029776371534fe4c1
Gustav Hansen <gustav.hansen@strapi.io>
2022-06-03 09:33:04 +0200

    BulkMoveDialog: Simplify form submission

--- packages/core/upload/admin/src/components/BulkMoveDialog/BulkMoveDialog.js
+++ packages/core/upload/admin/src/components/BulkMoveDialog/BulkMoveDialog.js
@@ -1,5 +1,5 @@
 import { Formik } from 'formik';
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import { Button } from '@strapi/design-system/Button';
@@ -13,7 +13,6 @@ import {
 import { FieldLabel } from '@strapi/design-system/Field';
 import { Stack } from '@strapi/design-system/Stack';
 import { Typography } from '@strapi/design-system/Typography';
-import { VisuallyHidden } from '@strapi/design-system/VisuallyHidden';
 import { Form } from '@strapi/helper-plugin';
 
 import { getTrad } from '../../utils';
@@ -21,7 +20,6 @@ import SelectTree from '../SelectTree';
 import { useFolderStructure } from '../../hooks/useFolderStructure';
 
 export const BulkMoveDialog = ({ onClose, errors: initialErrors }) => {
-  const submitButtonRef = useRef(null);
   const { formatMessage } = useIntl();
   const { data: folderStructure, isLoading } = useFolderStructure();
   const initialFormData = {
@@ -38,24 +36,24 @@ export const BulkMoveDialog = ({ onClose, errors: initialErrors }) => {
 
   return (
     <ModalLayout onClose={handleClose} labelledBy="title">
-      <ModalHeader>
-        <Typography fontWeight="bold" textColor="neutral800" as="h2" id="title">
-          {formatMessage({
-            id: getTrad('modal.folder.move.title'),
-            defaultMessage: 'Move elements to',
-          })}
-        </Typography>
-      </ModalHeader>
+      <Formik
+        validateOnChange={false}
+        onSubmit={handleSubmit}
+        initialValues={initialFormData}
+        initialErrors={initialErrors}
+      >
+        {({ values, errors, setFieldValue }) => (
+          <Form noValidate>
+            <ModalHeader>
+              <Typography fontWeight="bold" textColor="neutral800" as="h2" id="title">
+                {formatMessage({
+                  id: getTrad('modal.folder.move.title'),
+                  defaultMessage: 'Move elements to',
+                })}
+              </Typography>
+            </ModalHeader>
 
-      <ModalBody>
-        <Formik
-          validateOnChange={false}
-          onSubmit={handleSubmit}
-          initialValues={initialFormData}
-          initialErrors={initialErrors}
-        >
-          {({ values, errors, setFieldValue }) => (
-            <Form noValidate>
+            <ModalBody>
               <Grid gap={4}>
                 <GridItem xs={12} col={12}>
                   <Stack spacing={1}>
@@ -96,29 +94,23 @@ export const BulkMoveDialog = ({ onClose, errors: initialErrors }) => {
                   </Stack>
                 </GridItem>
               </Grid>
+            </ModalBody>
 
-              <VisuallyHidden>
-                <button type="submit" tabIndex={-1} ref={submitButtonRef} name="hidden-submit">
-                  {formatMessage({ id: 'submit', defaultMessage: 'Submit' })}
-                </button>
-              </VisuallyHidden>
-            </Form>
-          )}
-        </Formik>
-      </ModalBody>
-
-      <ModalFooter
-        startActions={
-          <Button onClick={handleClose} variant="tertiary" name="cancel">
-            {formatMessage({ id: 'cancel', defaultMessage: 'Cancel' })}
-          </Button>
-        }
-        endActions={
-          <Button onClick={() => submitButtonRef.current.click()} name="submit" loading={isLoading}>
-            {formatMessage({ id: 'modal.folder.move.submit', defaultMessage: 'Move' })}
-          </Button>
-        }
-      />
+            <ModalFooter
+              startActions={
+                <Button onClick={handleClose} variant="tertiary" name="cancel">
+                  {formatMessage({ id: 'cancel', defaultMessage: 'Cancel' })}
+                </Button>
+              }
+              endActions={
+                <Button type="submit" loading={isLoading}>
+                  {formatMessage({ id: 'modal.folder.move.submit', defaultMessage: 'Move' })}
+                </Button>
+              }
+            />
+          </Form>
+        )}
+      </Formik>
     </ModalLayout>
   );
 };
