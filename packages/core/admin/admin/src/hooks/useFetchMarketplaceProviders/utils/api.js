e7a4f7f7fa14dd8d00a04e6813ed879d98e0b579
Mark Kaylor <mark.kaylor@strapi.io>
2022-06-03 16:57:47 +0200

    remove filter on providers response

--- packages/core/admin/admin/src/hooks/useFetchMarketplaceProviders/utils/api.js
+++ packages/core/admin/admin/src/hooks/useFetchMarketplaceProviders/utils/api.js
@@ -3,15 +3,9 @@ import axios from 'axios';
 const MARKETPLACE_API_URL = 'https://market-api.strapi.io';
 
 const fetchMarketplacePlugins = async () => {
-  const { data: response } = await axios.get(`${MARKETPLACE_API_URL}/providers`);
+  const { data } = await axios.get(`${MARKETPLACE_API_URL}/providers`);
 
-  // Only keep v4 plugins
-  const filteredResponse = {
-    ...response,
-    data: response.data.filter((provider) => provider.attributes.strapiCompatibility === 'v4'),
-  };
-
-  return filteredResponse;
+  return data;
 };
 
 export { fetchMarketplacePlugins };
