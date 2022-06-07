dc72d3c7e2c84ac07ae377a5fa37275353a02e87
Gustav Hansen <gustav.hansen@strapi.io>
2022-06-07 12:35:31 +0100

    chore: Update snapshot tests

--- packages/core/upload/admin/src/components/AssetCard/tests/ImageAssetCard.test.js
+++ packages/core/upload/admin/src/components/AssetCard/tests/ImageAssetCard.test.js
@@ -51,6 +51,7 @@ describe('ImageAssetCard', () => {
         background: #ffffff;
         border-radius: 4px;
         box-shadow: 0px 1px 4px rgba(33,33,52,0.1);
+        height: 100%;
       }
 
       .c3 {
@@ -397,6 +398,7 @@ describe('ImageAssetCard', () => {
         <article
           aria-labelledby="card-1-title"
           class="c0"
+          height="100%"
           tabindex="0"
         >
           <div
