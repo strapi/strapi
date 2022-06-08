3d5eb83d33906078ed465e02db3b71e36394217a
Gustav Hansen <gustav.hansen@strapi.io>
2022-06-08 14:00:08 +0100

    SelectTree: Start indention at 0 instead of 1

--- packages/core/upload/admin/src/components/SelectTree/utils/flattenTree.js
+++ packages/core/upload/admin/src/components/SelectTree/utils/flattenTree.js
@@ -1,4 +1,4 @@
-export default function flattenTree(tree, parent, depth = 1) {
+export default function flattenTree(tree, parent, depth = 0) {
   return tree.flatMap(item =>
     item.children
       ? [{ ...item, parent: parent?.value, depth }, ...flattenTree(item.children, item, depth + 1)]
