54c8cbf846f122f65912c8b440be16ce9944066c
Convly <jean-sebastien.herbaux@epitech.eu>
2022-07-01 14:19:07 +0200

    Fix i18n locale queries being forbidden even with the correct permission set

--- packages/plugins/i18n/server/graphql.js
+++ packages/plugins/i18n/server/graphql.js
@@ -66,6 +66,10 @@ module.exports = ({ strapi }) => ({
           ...createLocalizationResolversConfig,
           // locale arg transformation for localized createEntity mutations
           ...getLocalizedCreateMutationsResolversConfigs({ typeRegistry }),
+
+          // Modify the default scope associated to find and findOne locale queries to match the actual action name
+          'Query.i18NLocale': { auth: { scope: 'plugin::i18n.locales.listLocales' } },
+          'Query.i18NLocales': { auth: { scope: 'plugin::i18n.locales.listLocales' } },
         },
       };
     });
