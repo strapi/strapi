f44576d41ee4d700ac735624708e1b3f90c19222
Gustav Hansen <gustav.hansen@strapi.io>
2022-07-11 13:54:31 +0200

    chore: Update jest to 28.x

--- jest.base-config.front.js
+++ jest.base-config.front.js
@@ -51,6 +51,7 @@ module.exports = {
     '<rootDir>/packages/admin-test-utils/lib/setup/styled-components.js',
     '<rootDir>/packages/admin-test-utils/lib/setup/strapi.js',
   ],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.js$': ['@swc-node/jest', { jsx: true, dynamicImport: true }],
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
