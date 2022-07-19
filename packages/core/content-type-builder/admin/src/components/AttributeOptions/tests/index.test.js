e322d31ea130d4b5022c50c6a98a96521a15a9af
Mark Kaylor <mark.kaylor@strapi.io>
2022-07-19 12:00:40 +0200

    Simplify tests

--- packages/core/content-type-builder/admin/src/components/AttributeOptions/tests/index.test.js
+++ packages/core/content-type-builder/admin/src/components/AttributeOptions/tests/index.test.js
@@ -1,15 +1,8 @@
-import { render, screen, getByText, fireEvent } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { lightTheme, darkTheme } from '@strapi/design-system';
-import LanguageProvider from '../../../../../../admin/admin/src/components/LanguageProvider';
-import Theme from '../../../../../../admin/admin/src/components/Theme';
-import ThemeToggleProvider from '../../../../../../admin/admin/src/components/ThemeToggleProvider';
-import en from '../../../../../../admin/admin/src/translations/en.json';
+import { render, screen, getByText, fireEvent } from '@testing-library/react';
+import { lightTheme, ThemeProvider } from '@strapi/design-system';
+import { IntlProvider } from 'react-intl';
 import FormModalNavigationProvider from '../../FormModalNavigationProvider';
-import pluginEn from '../../../translations/en.json';
-import getTrad from '../../../utils/getTrad';
 import AttributeOptions from '../index';
 
 const mockAttributes = [
@@ -30,62 +23,38 @@ const mockAttributes = [
   ['component', 'dynamiczone'],
 ];
 
-const makeApp = () => {
-  const history = createMemoryHistory();
-  const messages = {
-    en: Object.keys(pluginEn).reduce(
-      (acc, current) => {
-        acc[getTrad(current)] = pluginEn[current];
+const App = (
+  <IntlProvider locale="en" messages={{}} textComponent="span">
+    <ThemeProvider theme={lightTheme}>
+      <FormModalNavigationProvider>
+        <AttributeOptions
+          attributes={mockAttributes}
+          forTarget="contentType"
+          kind="collectionType"
+        />
+      </FormModalNavigationProvider>
+    </ThemeProvider>
+  </IntlProvider>
+);
 
-        return acc;
-      },
-      { ...en }
-    ),
-  };
-
-  const localeNames = { en: 'English' };
-
-  return (
-    <LanguageProvider messages={messages} localeNames={localeNames}>
-      <ThemeToggleProvider themes={{ light: lightTheme, dark: darkTheme }}>
-        <Theme>
-          <Router history={history}>
-            <FormModalNavigationProvider>
-              <AttributeOptions
-                attributes={mockAttributes}
-                forTarget="contentType"
-                kind="collectionType"
-              />
-            </FormModalNavigationProvider>
-          </Router>
-        </Theme>
-      </ThemeToggleProvider>
-    </LanguageProvider>
-  );
-};
-
-describe('AttributeOptions', () => {
+describe('<AttributeOptions />', () => {
   it('renders and matches the snapshot', () => {
-    const App = makeApp();
     const { container } = render(App);
 
     expect(container).toMatchSnapshot();
   });
 
-  it('shows the simple tabs', async () => {
-    const App = makeApp();
+  it('shows the simple tabs', () => {
     render(App);
 
-    const tabs = screen.getByLabelText('Attribute type tabs');
-    const defaultTab = await getByText(tabs, 'Default');
-    const customTab = await getByText(tabs, 'Custom');
+    const defaultTab = screen.getByRole('tab', { selected: true });
+    const customTab = screen.getByRole('tab', { selected: false });
 
     expect(defaultTab).toBeVisible();
     expect(customTab).toBeVisible();
   });
 
-  it('defaults to the default tab', async () => {
-    const App = makeApp();
+  it('defaults to the default tab', () => {
     render(App);
 
     const comingSoonText = screen.queryByText('Coming soon');
@@ -93,17 +62,16 @@ describe('AttributeOptions', () => {
     expect(comingSoonText).toEqual(null);
   });
 
-  it('switches to the custom tab', async () => {
-    const App = makeApp();
+  it('switches to the custom tab', () => {
     render(App);
 
     const customTab = screen.getByRole('tab', { selected: false });
     fireEvent.click(customTab);
-    const button = screen.getByRole('tab', { selected: true });
-    const customTabActive = await getByText(button, 'Custom');
+    const customTabSelected = screen.getByRole('tab', { selected: true });
+    const customTabText = getByText(customTabSelected, 'Custom');
     const comingSoonText = screen.getByText('Coming soon');
 
-    expect(customTabActive).not.toBe(null);
+    expect(customTabText).not.toBe(null);
     expect(comingSoonText).toBeVisible();
   });
 });
