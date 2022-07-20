2e62447b551492c2940cff3b2959176056d34a96
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2022-07-20 14:45:33 +0200

    Use svg for attribute icon

--- examples/getstarted/src/plugins/mycustomfields/admin/src/components/ColorPicker/ColorPickerIcon/index.js
+++ examples/getstarted/src/plugins/mycustomfields/admin/src/components/ColorPicker/ColorPickerIcon/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
-import { Flex } from '@strapi/design-system/Flex';
-import { Icon } from '@strapi/design-system/Icon';
-import Puzzle from '@strapi/icons/Puzzle';
+import icon from './icon.svg';
 
-const ColorPickerIcon = () => {
-  return (
-    <Flex
-      justifyContent="center"
-      width={7}
-      height={6}
-      background="primary100"
-      borderColor="primary200"
-      hasRadius
-    >
-      <Icon as={Puzzle} width={3} color="primary600" />
-    </Flex>
-  );
-};
+const ColorPickerIcon = () => <img src={icon} />;
 
 export default ColorPickerIcon;
