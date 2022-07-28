1453bc0d67ca4aab777947a49c339550d5bbd401
Convly <jean-sebastien.herbaux@epitech.eu>
2022-07-28 11:57:40 +0200

    Fix light typings issues

--- packages/core/permissions/index.d.ts
+++ packages/core/permissions/index.d.ts
@@ -1,48 +1,42 @@
-import { Ability } from '@casl/ability';
 import { hooks, providerFactory } from '@strapi/utils';
-
-type Provider = ReturnType<typeof providerFactory>;
 
 interface Permission {
   action: string;
-  subject?: string | object;
+  subject?: string | object | null;
   properties?: object;
   conditions?: string[];
 }
 
-interface Action {
-  name: string;
-  section: string;
+type Provider = ReturnType<typeof providerFactory>;
+
+interface BaseAction {
+  actionId: string;
 }
 
-interface Condition {
+interface BaseCondition {
   name: string;
-
-  handler(): boolean | object;
-}
-
-type StrapiHook<
-  T extends keyof Pick<
-    typeof hooks,
-    'createAsyncParallelHook' | 'createAsyncSeriesHook' | 'createAsyncSeriesWaterfallHook'
-  >
-> = ReturnType<typeof hooks[T]>;
-
-interface EngineHooks {
-  willEvaluatePermission: StrapiHook<'createAsyncSeriesHook'>;
-  willRegisterPermission: StrapiHook<'createAsyncSeriesHook'>;
+  handler(...params: unknown[]): boolean | object;
 }
 
 interface ActionProvider<T extends Action = Action> extends Provider {}
 interface ConditionProvider<T extends Condition = Condition> extends Provider {}
 
 interface PermissionEngine {
-  hooks: EngineHooks;
+  hooks: object;
 
   generateAbility(permissions: Permission[], options?: object): Ability;
 }
 
+interface BaseAbility {
+  can: Function;
+}
+
+interface AbilityBuilder {
+  can(permission: Permission): void | Promise<void>;
+  build(): BaseAbility | Promise<BaseAbility>;
+}
+
 interface PermissionEngineParams {
   providers: { action: ActionProvider; condition: ConditionProvider };
-  abilityBuilderFactory(): { can: Function; build: Function };
+  abilityBuilderFactory(): AbilityBuilder;
 }
