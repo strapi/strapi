9c8cf0ebfc652968fbf7f0bc84514a0d2314deb4
Mark Kaylor <mark.kaylor@strapi.io>
2022-07-29 12:40:52 +0200

    Test custom field data manager provider actions

--- packages/core/content-type-builder/admin/src/components/DataManagerProvider/tests/reducer_add_custom_field_attribute_action.test.js
+++ packages/core/content-type-builder/admin/src/components/DataManagerProvider/tests/reducer_add_custom_field_attribute_action.test.js
@@ -15,13 +15,18 @@ describe('CTB | components | DataManagerProvider | reducer | ADD_CUSTOM_FIELD_AT
       },
     };
 
+    const newCustomFieldAttribute = {
+      // The underlying data type should be in the action and expected
+      // The type is converted to customField on the server,
+      type: 'string',
+      name: 'color',
+      options: { format: 'hex' },
+      customField: 'plugin::mycustomfields.color',
+    };
+
     const action = {
       type: ADD_CUSTOM_FIELD_ATTRIBUTE,
-      attributeToSet: {
-        type: 'string',
-        name: 'color',
-        options: { format: 'hex' },
-      },
+      attributeToSet: newCustomFieldAttribute,
       forTarget: 'contentType',
       targetUid: 'api::test.test',
       initialAttribute: {},
@@ -36,21 +41,149 @@ describe('CTB | components | DataManagerProvider | reducer | ADD_CUSTOM_FIELD_AT
     };
 
     const updatedContentType = cloneDeep(contentType);
-    updatedContentType.schema.attributes.push({
-      name: 'color',
-      // The type is converted to customField on the server,
-      // The underlying data type is expected here
-      type: 'string',
-      options: {
-        format: 'hex',
-      },
-    });
+    updatedContentType.schema.attributes.push(newCustomFieldAttribute);
 
     const expected = {
       ...initialState,
       modifiedData: {
         components: {},
         contentType: updatedContentType,
+      },
+    };
+
+    expect(reducer(state, action)).toEqual(expected);
+  });
+
+  it('adds a custom field to a component', () => {
+    const componentSchema = {
+      uid: 'basic.simple',
+      category: 'basic',
+      schema: {
+        icon: 'ambulance',
+        name: 'simple',
+        description: '',
+        connection: 'basic',
+        collectionName: 'components_basic_simples',
+        attributes: [
+          { type: 'string', required: true, name: 'name' },
+          { type: 'string', name: 'test' },
+        ],
+      },
+    };
+
+    const newCustomFieldAttribute = {
+      // The underlying data type should be in the action and expected
+      // The type is converted to customField on the server,
+      type: 'string',
+      name: 'color',
+      options: { format: 'hex' },
+      customField: 'plugin::mycustomfields.color',
+    };
+
+    const action = {
+      type: ADD_CUSTOM_FIELD_ATTRIBUTE,
+      attributeToSet: newCustomFieldAttribute,
+      forTarget: 'component',
+      targetUid: 'basic.simple',
+      initialAttribute: {},
+    };
+
+    const state = {
+      ...initialState,
+      modifiedData: {
+        components: {},
+        component: componentSchema,
+      },
+    };
+
+    const updatedComponent = cloneDeep(componentSchema);
+    updatedComponent.schema.attributes.push(newCustomFieldAttribute);
+
+    const expected = {
+      ...initialState,
+      modifiedData: {
+        components: {},
+        component: updatedComponent,
+      },
+    };
+
+    expect(reducer(state, action)).toEqual(expected);
+  });
+
+  it('adds a custom field to a component that is an attribute of a content type', () => {
+    const componentSchema = {
+      uid: 'basic.simple',
+      category: 'basic',
+      schema: {
+        icon: 'ambulance',
+        name: 'simple',
+        description: '',
+        connection: 'basic',
+        collectionName: 'components_basic_simples',
+        attributes: [
+          { type: 'string', required: true, name: 'name' },
+          { type: 'string', name: 'test' },
+        ],
+      },
+    };
+
+    const contentType = {
+      uid: 'api::test.test',
+      schema: {
+        name: 'test',
+        description: '',
+        connection: 'default',
+        collectionName: '',
+        attributes: [
+          { name: 'test', type: 'string' },
+          { name: 'testcompo', type: 'component', component: 'basic.simple' },
+        ],
+      },
+    };
+
+    const state = {
+      ...initialState,
+      components: {
+        'basic.simple': componentSchema,
+      },
+      modifiedData: {
+        components: {
+          'basic.simple': componentSchema,
+        },
+        contentType,
+      },
+    };
+
+    const newCustomFieldAttribute = {
+      // The underlying data type should be in the action and expected
+      // The type is converted to customField on the server,
+      type: 'string',
+      name: 'color',
+      options: { format: 'hex' },
+      customField: 'plugin::mycustomfields.color',
+    };
+
+    const action = {
+      type: ADD_CUSTOM_FIELD_ATTRIBUTE,
+      attributeToSet: newCustomFieldAttribute,
+      forTarget: 'components',
+      targetUid: 'basic.simple',
+      initialAttribute: {},
+    };
+
+    const updatedComponent = cloneDeep(componentSchema);
+    updatedComponent.schema.attributes.push(newCustomFieldAttribute);
+
+    const expected = {
+      ...initialState,
+      components: {
+        'basic.simple': componentSchema,
+      },
+      modifiedData: {
+        components: {
+          'basic.simple': updatedComponent,
+        },
+        contentType,
       },
     };
 
