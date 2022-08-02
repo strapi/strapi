4d3e0a4b15db7f0ed8172ff67c7d81d0e67d34c2
Mark Kaylor <mark.kaylor@strapi.io>
2022-08-02 09:15:26 +0200

    Add custom field validation registered by user

--- packages/core/content-type-builder/admin/src/utils/formAPI.js
+++ packages/core/content-type-builder/admin/src/utils/formAPI.js
@@ -68,11 +68,13 @@ const formsAPI = {
           },
         };
       }
+
       formType[field].validators.push(validator);
       formType[field].form.advanced.push(advanced);
       formType[field].form.base.push(base);
     });
   },
+
   getAdvancedForm(target, props = null) {
     const sectionsToAdd = get(this.types, [...target, 'form', 'advanced'], []).reduce(
       (acc, current) => {
@@ -84,6 +86,10 @@ const formsAPI = {
     );
 
     return sectionsToAdd;
+  },
+
+  makeCustomFieldValidator(initShape, validator, ...validatorArgs) {
+    return initShape.shape({ options: yup.object().shape(validator(validatorArgs)) });
   },
 
   makeValidator(target, initShape, ...args) {
