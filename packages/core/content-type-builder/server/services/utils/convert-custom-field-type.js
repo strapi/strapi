ebe194db77dbd44c60509ab0ef70ba76d64fd636
Mark Kaylor <mark.kaylor@strapi.io>
2022-08-02 16:16:35 +0200

    Handle errors when sending custom field from the admin to the server

--- packages/core/content-type-builder/server/services/utils/convert-custom-field-type.js
+++ packages/core/content-type-builder/server/services/utils/convert-custom-field-type.js
@@ -8,6 +8,21 @@
 const convertCustomFieldType = attributes => {
   Object.values(attributes).forEach(attribute => {
     if (attribute.customField) {
+      if (['component', 'relation', 'dynamiczone'].includes(attribute.type)) {
+        // When the registered custom fiel is using an invalid strapi type
+        throw new Error(`Type: ${attribute.type} is not a valid Custom Field type`);
+      }
+      // Use the custom field uid sent from the admin to get its equivalent on the server
+      // The getter will throw an error if the custom field is not found
+      const customField = strapi.container.get('custom-fields').get(attribute.customField);
+
+      if (customField.type !== attribute.type) {
+        // When there is a type mismatch between admin and server
+        throw new Error(
+          `Custom field: "${attribute.customField}" sent type: "${attribute.type}" from the admin but the server expected type: "${customField.type}"`
+        );
+      }
+
       attribute.type = 'customField';
     }
   });
