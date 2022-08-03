2b576869b673e576adb95796ac1f9b0ecdd60007
Bassel Kanso <basselkanso82@gmail.com>
2022-08-03 12:59:30 +0300

    only use id when state.count is *

--- packages/core/database/lib/query/query-builder.js
+++ packages/core/database/lib/query/query-builder.js
@@ -301,7 +301,11 @@ const createQueryBuilder = (uid, db) => {
           break;
         }
         case 'count': {
-          const dbColumnName = this.aliasColumn(helpers.toColumnName(meta, 'id'));
+          const dbColumnName =
+            state.count === '*'
+              ? this.aliasColumn(helpers.toColumnName(meta, 'id'))
+              : this.aliasColumn(helpers.toColumnName(meta, state.count));
+
           qb.countDistinct({ count: dbColumnName });
           break;
         }
