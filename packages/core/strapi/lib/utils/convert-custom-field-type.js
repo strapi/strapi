6895559c70ee380e701cb228c647cb76709ebc3c
Mark Kaylor <mark.kaylor@strapi.io>
2022-08-10 11:08:24 +0200

    Include components in custom field type conversion on start

--- packages/core/strapi/lib/utils/convert-custom-field-type.js
+++ packages/core/strapi/lib/utils/convert-custom-field-type.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const convertCustomFieldType = strapi => {
-  const allSchemasAttributes = Object.values(strapi.contentTypes).map(schema => schema.attributes);
+  const allContentTypeSchemaAttributes = Object.values(strapi.contentTypes).map(
+    schema => schema.attributes
+  );
+  const allComponentSchemaAttributes = Object.values(strapi.components).map(
+    schema => schema.attributes
+  );
+  const allSchemasAttributes = [...allContentTypeSchemaAttributes, ...allComponentSchemaAttributes];
+
   for (const schemaAttrbutes of allSchemasAttributes) {
     for (const attribute of Object.values(schemaAttrbutes)) {
       if (attribute.type === 'customField') {
