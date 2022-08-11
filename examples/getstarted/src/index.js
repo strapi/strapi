ede93cf1f284321bad49083e5f5a28d1a798f8b1
Ben Irvin <ben@innerdvations.com>
2022-08-11 10:49:05 +0200

    bootstrap test

--- examples/getstarted/src/index.js
+++ examples/getstarted/src/index.js
@@ -17,18 +17,23 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   async bootstrap({ strapi }) {
-    const payload = { type: 'custom', permissions: ['read', 'delete'], name: 'My custom token v2' };
+    const tokens = await strapi.admin.services['api-token'].list();
+    const promises = [];
+    tokens.forEach(({ id }) => {
+      promises.push(strapi.admin.services['api-token'].revoke(id));
+    });
+    await Promise.all(promises);
+    const tokens2 = await strapi.admin.services['api-token'].list();
+    console.log('tokens:', tokens2.length);
 
+    const payload = { type: 'custom', permissions: ['read', 'delete'], name: 'custom token v2' };
     const token = await strapi.admin.services['api-token'].create(payload);
-
-    console.log(JSON.stringify(token, null, 2));
-
+    console.log('custom-before', JSON.stringify(token, null, 2));
     const updatedToken = await strapi.admin.services['api-token'].update(token.id, {
       description: 'foobar',
-      permissions: ['read', 'update'],
+      permissions: ['admin::thing.read', 'admin::thing.update'],
     });
-
-    console.log(JSON.stringify(updatedToken, null, 2));
+    console.log('custom-after', JSON.stringify(updatedToken, null, 2));
   },
 
   /**
