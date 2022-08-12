d11457ad563b78d1399aee53850f03672312fb4b
Gustav Hansen <gustav.hansen@strapi.io>
2022-08-12 17:34:19 +0200

    RelationInput: Split up tests

---
+++ packages/core/helper-plugin/lib/src/components/RelationInput/components/tests/RelationList.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider, lightTheme } from '@strapi/design-system';
+
+import { RelationItem } from '../RelationItem';
+import { RelationList } from '../RelationList';
+
+const setup = ({ endAction }) =>
+  render(
+    <ThemeProvider theme={lightTheme}>
+      <RelationList>
+        <RelationItem endAction={endAction}>First relation</RelationItem>
+      </RelationList>
+    </ThemeProvider>
+  );
+
+describe('RelationList', () => {
+  it('should render and match snapshot', () => {
+    const { container } = setup({});
+
+    expect(container).toMatchSnapshot();
+  });
+});
