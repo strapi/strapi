63b76e8c9d898d73d7dfb2beb8c9b021df09a398
Ben Irvin <ben@innerdvations.com>
2022-08-17 09:49:03 +0200

    update bootstrap

--- examples/getstarted/src/index.js
+++ examples/getstarted/src/index.js
@@ -17,23 +17,26 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   async bootstrap({ strapi }) {
-    const tokens = await strapi.admin.services['api-token'].list();
-    const promises = [];
-    tokens.forEach(({ id }) => {
-      promises.push(strapi.admin.services['api-token'].revoke(id));
-    });
-    await Promise.all(promises);
-    const tokens2 = await strapi.admin.services['api-token'].list();
-    console.log('tokens:', tokens2.length);
-
-    const payload = { type: 'custom', permissions: ['read', 'delete'], name: 'custom token v2' };
-    const token = await strapi.admin.services['api-token'].create(payload);
-    console.log('custom-before', JSON.stringify(token, null, 2));
-    const updatedToken = await strapi.admin.services['api-token'].update(token.id, {
-      description: 'foobar',
-      permissions: ['admin::thing.read', 'admin::thing.update'],
-    });
-    console.log('custom-after', JSON.stringify(updatedToken, null, 2));
+    // Delete tokens
+    // const tokens = await strapi.admin.services['api-token'].list();
+    // const promises = [];
+    // tokens.forEach(({ id }) => {
+    //   promises.push(strapi.admin.services['api-token'].revoke(id));
+    // });
+    // await Promise.all(promises);
+    // Create a token
+    // const payload = {
+    //   type: 'custom',
+    //   permissions: ['api::restaurant.create'],
+    //   name: 'custom token v2',
+    // };
+    // const token = await strapi.admin.services['api-token'].create(payload);
+    // console.log('custom-before', JSON.stringify(token, null, 2));
+    // const updatedToken = await strapi.admin.services['api-token'].update(token.id, {
+    //   description: 'foobar',
+    //   permissions: ['api::restaurant.restaurant.find', 'api::restaurant.restaurant.findOne'],
+    // });
+    // console.log('custom-after', JSON.stringify(updatedToken, null, 2));
   },
 
   /**
