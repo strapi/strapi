e710cc432409e1ca1cdc2f8cd3e1210f3817c4ff
WalkingPizza <kevin.iatauro@gmail.com>
2022-08-17 10:08:59 +0200

    Move file size check

--- packages/core/upload/server/services/provider.js
+++ packages/core/upload/server/services/provider.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const { PayloadTooLargeError } = require('@strapi/utils/lib/errors');
 const { isFunction } = require('lodash/fp');
-const { streamToBuffer } = require('../utils/file');
+const { streamToBuffer, kbytesToBytes } = require('../utils/file');
 
 module.exports = ({ strapi }) => ({
   async upload(file) {
+    const config = strapi.config.get('plugin.upload');
+    const fileSize = kbytesToBytes(file.size);
+
+    if (config.sizeLimit && fileSize > config.sizeLimit) {
+      throw new PayloadTooLargeError();
+    }
+
     if (isFunction(strapi.plugin('upload').provider.uploadStream)) {
       file.stream = file.getStream();
       await strapi.plugin('upload').provider.uploadStream(file);
--- packages/core/upload/server/services/upload.js
+++ packages/core/upload/server/services/upload.js
@@ -22,7 +22,7 @@ const { NotFoundError } = require('@strapi/utils').errors;
 
 const { MEDIA_UPDATE, MEDIA_CREATE, MEDIA_DELETE } = webhookUtils.webhookEvents;
 
-const { ApplicationError, PayloadTooLargeError } = require('@strapi/utils/lib/errors');
+const { ApplicationError } = require('@strapi/utils/lib/errors');
 const { FILE_MODEL_UID } = require('../constants');
 const { getService } = require('../utils');
 const { bytesToKbytes } = require('../utils/file');
@@ -112,8 +112,6 @@ module.exports = ({ strapi }) => ({
   },
 
   async enhanceAndValidateFile(file, fileInfo = {}, metas = {}) {
-    const config = strapi.config.get('plugin.upload');
-
     const currentFile = await this.formatFileInfo(
       {
         filename: file.name,
@@ -131,10 +129,6 @@ module.exports = ({ strapi }) => ({
     const { optimize, isImage, isFaultyImage, isOptimizableImage } = strapi
       .plugin('upload')
       .service('image-manipulation');
-
-    if (config.sizeLimit && currentFile.size > config.sizeLimit) {
-      throw new PayloadTooLargeError();
-    }
 
     if (await isImage(currentFile)) {
       if (await isFaultyImage(currentFile)) {
