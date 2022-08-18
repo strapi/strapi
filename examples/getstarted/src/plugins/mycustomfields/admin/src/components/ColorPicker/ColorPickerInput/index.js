d81ecff2b40fea87db904023f0c5c1d5f1a6d41d
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2022-08-18 18:15:50 +0200

    Show options on custom field example

--- examples/getstarted/src/plugins/mycustomfields/admin/src/components/ColorPicker/ColorPickerInput/index.js
+++ examples/getstarted/src/plugins/mycustomfields/admin/src/components/ColorPicker/ColorPickerInput/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Stack } from '@strapi/design-system/Stack';
+import { Typography } from '@strapi/design-system/Typography';
 import { Field, FieldHint, FieldError, FieldLabel } from '@strapi/design-system/Field';
 import { useIntl } from 'react-intl';
 
@@ -18,19 +19,22 @@ const ColorPickerInput = ({
   const { formatMessage } = useIntl();
 
   return (
-    <Stack spacing={1}>
-      <Field
-        name={name}
-        id={name}
-        error={error && formatMessage(error)}
-        hint={description && formatMessage(description)}
-      >
+    <Field
+      name={name}
+      id={name}
+      error={error && formatMessage(error)}
+      hint={description && formatMessage(description)}
+    >
+      <Stack spacing={1}>
         <FieldLabel required={required}>{formatMessage(intlLabel)}</FieldLabel>
+        <Typography variant="pi" as="p">
+          Format: {attribute.options.format}
+        </Typography>
         <input type="color" id={name} name={name} value={value || ''} onChange={onChange} />
         <FieldHint />
         <FieldError />
-      </Field>
-    </Stack>
+      </Stack>
+    </Field>
   );
 };
 
