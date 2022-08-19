306d52dcac0ae860f29a23802f22517968325c5b
Alexandre Bodin <bodin.alex@gmail.com>
2022-08-19 17:49:34 +0200

    chore: docusaurus baseUrl config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Doc',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/strapi/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
