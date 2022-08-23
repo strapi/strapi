353efdf84c4e87a6acb9b9b53ac1b350910f960c
Ben Irvin <ben@innerdvations.com>
2022-08-23 18:28:34 +0200

    fix test

--- packages/core/admin/server/tests/admin-api-token.test.e2e.js
+++ packages/core/admin/server/tests/admin-api-token.test.e2e.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { omit, map, orderBy } = require('lodash');
+const { omit, map } = require('lodash');
 const { createStrapiInstance } = require('../../../../../test/helpers/strapi');
 const { createAuthRequest } = require('../../../../../test/helpers/request');
 
@@ -36,6 +36,7 @@ describe('Admin API Token v2 CRUD (e2e)', () => {
   const createValidToken = async (token = {}) => {
     const body = {
       type: 'read-only',
+      // eslint-disable-next-line no-plusplus
       name: `token_${String(currentTokens++)}`,
       description: 'generic description',
       ...token,
@@ -331,8 +332,14 @@ describe('Admin API Token v2 CRUD (e2e)', () => {
 
     expect(res.statusCode).toBe(200);
     expect(res.body.data.length).toBe(4);
-    expect(orderBy(res.body.data, ['id'])).toStrictEqual(
-      map(orderBy(tokens, ['id']), (t) => omit(t, ['accessKey']))
+    const mappedTokens = map(tokens, (t) => omit(t, ['accessKey']));
+    expect(res.body.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(mappedTokens[0]),
+        expect.objectContaining(mappedTokens[1]),
+        expect.objectContaining(mappedTokens[2]),
+        expect.objectContaining(mappedTokens[3]),
+      ])
     );
   });
 
