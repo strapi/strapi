a5d56169de6d81b0a607156d334fdfb1b1afe2e9
Mark Kaylor <mark.kaylor@strapi.io>
2022-08-25 09:16:07 +0200

    Remove all traces of map custom field

--- examples/getstarted/src/plugins/mycustomfields/admin/src/components/Map/MapInput/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ColorPickerInput = () => {
-  return <div>TODO: Map Input Component</div>;
-};
-
-export default ColorPickerInput;
--- examples/getstarted/src/plugins/mycustomfields/server/register.js
+++ examples/getstarted/src/plugins/mycustomfields/server/register.js
@@ -1,16 +1,9 @@
 'use strict';
 
 module.exports = ({ strapi }) => {
-  strapi.customFields.register([
-    {
-      name: 'color',
-      plugin: 'mycustomfields',
-      type: 'string',
-    },
-    {
-      name: 'map',
-      plugin: 'mycustomfields',
-      type: 'json',
-    },
-  ]);
+  strapi.customFields.register({
+    name: 'color',
+    plugin: 'mycustomfields',
+    type: 'string',
+  });
 };
