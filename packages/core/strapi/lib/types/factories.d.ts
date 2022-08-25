e64ba2de5efa04bf99160b2d15807c91f846a769
Sacha STAFYNIAK <sacha@digisquad.io>
2022-08-25 15:49:37 +0200

    fix(typescript): use declaration merging for factories utilities

--- packages/core/strapi/lib/types/factories.d.ts
+++ packages/core/strapi/lib/types/factories.d.ts
@@ -4,23 +4,54 @@ import { Middleware } from '../middlewares';
 import { Policy } from '../core/registries/policies';
 import { Strapi } from '@strapi/strapi';
 
-type ControllerConfig<T extends Controller = Controller> = T;
+/**
+ * Map of UID / services.
+ * It can be extended by the user application or plugins.
+ * 
+ * @example
+ * ```ts
+ * declare module '@strapi/strapi/lib/types/core/factories' {
+ *   export interface ServiceUidMap {
+ *     'xxx::xxx.uid': SingleTypeService;
+ *     'yyy::yyy.uid': CollectionTypeService;
+ *   }
+ * }
+ * ```
+ */
+export interface ServiceUidMap {
+  [key: string]: Service;
+}
 
-type ServiceConfig = Service;
+/**
+ * Map of UID / controllers.
+ * It can be extended by the user application or plugins.
+ * 
+ * @example
+ * ```ts
+ * declare module '@strapi/strapi/lib/types/core/factories' {
+ *   export interface ControllerUidMap {
+ *     'xxx::xxx.uid': Controller;
+ *   }
+ * }
+ * ```
+ */
+export interface ControllerUidMap {
+  [key: string]: Controller;
+}
 
-type HandlerConfig = {
+export interface HandlerConfig {
   auth?: false | { scope: string[] };
   policies?: Array<string | Policy | { name: string; config: object }>;
   middlewares?: Array<string | Middleware | { name: string; config: object }>;
 };
 
-type SingleTypeRouterConfig = {
+export interface SingleTypeRouterConfig {
   find?: HandlerConfig;
   update?: HandlerConfig;
   delete?: HandlerConfig;
 };
 
-type CollectionTypeRouterConfig = {
+export interface CollectionTypeRouterConfig {
   find?: HandlerConfig;
   findOne?: HandlerConfig;
   create?: HandlerConfig;
@@ -28,33 +59,43 @@ type CollectionTypeRouterConfig = {
   delete?: HandlerConfig;
 };
 
-type RouterConfig = {
+export type RouterConfig = {
   prefix?: string;
   only: string[];
   except?: string[];
   config: SingleTypeRouterConfig | CollectionTypeRouterConfig;
 };
 
-interface Route {
+export interface Route {
   method: string;
   path: string;
 }
-interface Router {
+export interface Router {
   prefix: string;
   routes: Route[];
 }
 
-type ControllerCallback<T extends GenericController = GenericController> = (params: {
-  strapi: Strapi;
-}) => T;
-type ServiceCallback<T extends GenericService = GenericService> = (params: { strapi: Strapi }) => T;
+export type CustomActions =
+  | undefined
+  | (({ strapi }: { strapi: Strapi }) => any)
+  | { [key: string]: any };
 
 export function createCoreRouter(uid: string, cfg?: RouterConfig = {}): () => Router;
-export function createCoreController<T extends GenericController = GenericController>(
-  uid: string,
-  cfg?: ControllerCallback<T> | T = {}
-): () => T & Controller;
-export function createCoreService<T extends GenericService = GenericService>(
-  uid: string,
-  cfg?: ServiceCallback<T> | T = {}
-): () => T;
+
+export function createCoreController<K extends keyof ControllerUidMap, T extends CustomActions>(
+  uid: K,
+  customActions?: T
+): T extends undefined
+  ? () => ControllerUidMap[K] // only base controller
+  : T extends (...args: any) => any // custom action is function ? ({ strapi }) => ({})
+  ? () => ReturnType<T> & ControllerUidMap[K] // merge custom action return type with base controller
+  : () => T & ControllerUidMap[K]; // merge custom action object with base controller
+
+export function createCoreService<K extends keyof ServiceUidMap, T extends CustomActions>(
+  uid: K,
+  customActions?: T
+): T extends undefined
+  ? () => ServiceUidMap[K] // only base service
+  : T extends (...args: any) => any // custom action is function ? ({ strapi }) => ({})
+  ? () => ReturnType<T> & ServiceUidMap[K] // merge custom action return type with base service
+  : () => T & ServiceUidMap[K]; // merge custom action object with base service
