b5bd86bab63a042f8640d6c6f1c18094ed94df57
Mark Kaylor <mark.kaylor@strapi.io>
2022-08-31 15:16:59 +0200

    Use Flex component with props

--- packages/plugins/custom-fields/admin/src/components/ColorPicker/ColorPickerIcon/index.js
+++ packages/plugins/custom-fields/admin/src/components/ColorPicker/ColorPickerIcon/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Icon } from '@strapi/design-system/Icon';
-import { Box } from '@strapi/design-system/Box';
+import { Flex } from '@strapi/design-system/Flex';
 import Paint from '@strapi/icons/Paint';
-
-const IconBox = styled(Box)`
-  background-color: ${({ theme }) => theme.colors.primary100};
-  border: 1px solid ${({ theme }) => theme.colors.primary200};
-  width: ${({ theme }) => theme.spaces[7]};
-  height: ${({ theme }) => theme.spaces[6]};
-  border-radius: 3px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 const ColorPickerIcon = () => {
   return (
-    <IconBox aria-hidden>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      background="primary100"
+      borderColor="primary200"
+      width={7}
+      height={6}
+      hasRadius
+      aria-hidden
+    >
       <Icon as={Paint} color="primary600" />
-    </IconBox>
+    </Flex>
   );
 };
 
