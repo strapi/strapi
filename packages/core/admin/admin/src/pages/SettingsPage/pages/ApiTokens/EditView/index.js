8e97e90c3d40368ae125cf1fd9c4aee3ccef8b15
Simone Taeggi <startae14@gmail.com>
2022-09-09 00:09:16 +0200

    add regenerate command even on read

--- packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/EditView/index.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/ApiTokens/EditView/index.js
@@ -354,9 +354,9 @@ const ApiTokenCreateView = () => {
                     })
                   }
                   primaryAction={
-                    canEditInputs && (
+                    canEditInputs ? (
                       <Stack horizontal spacing={2}>
-                        {canRegenerate && apiToken?.name && (
+                        {canRegenerate && apiToken?.id && (
                           <Regenerate
                             onRegenerate={handleRegenerate}
                             idToRegenerate={apiToken?.id}
@@ -375,6 +375,11 @@ const ApiTokenCreateView = () => {
                           })}
                         </Button>
                       </Stack>
+                    ) : (
+                      canRegenerate &&
+                      apiToken?.id && (
+                        <Regenerate onRegenerate={handleRegenerate} idToRegenerate={apiToken?.id} />
+                      )
                     )
                   }
                   navigationAction={
