bac89b8730a901a2e1a818bec3411c90087fce36
Julie Plantey <julie.plantey@gmail.com>
2022-09-15 14:28:09 +0200

    fix tests RelationInput

--- packages/core/admin/admin/src/content-manager/components/RelationInput/tests/RelationInput.test.js
+++ packages/core/admin/admin/src/content-manager/components/RelationInput/tests/RelationInput.test.js
@@ -95,6 +95,11 @@ describe('Content-Manager || RelationInput', () => {
     expect(screen.getByText('Some Relation')).toBeInTheDocument();
     expect(screen.getByText('Load more')).toBeInTheDocument();
     expect(screen.getByText('Select...')).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: /relation 1/i,
+      })
+    ).toBeInTheDocument();
   });
 
   describe('Callbacks', () => {
