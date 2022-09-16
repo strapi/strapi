ca73a1adc0b050e3650e6f7924dd7720461bd605
Marc-Roig <marc12info@gmail.com>
2022-09-16 15:26:29 +0200

    move verifySize logic inside check file size

--- packages/providers/upload-local/lib/index.js
+++ packages/providers/upload-local/lib/index.js
@@ -15,18 +15,13 @@ const { kbytesToBytes } = require('../../../core/upload/server/utils/file');
 const UPLOADS_FOLDER_NAME = 'uploads';
 
 module.exports = {
-  init({ sizeLimit } = {}) {
-    // TODO V5: remove sizeLimit
-    if (sizeLimit) {
+  init({ sizeLimit: providerOptionsSizeLimit } = {}) {
+    // TODO V5: remove providerOptions sizeLimit
+    if (providerOptionsSizeLimit) {
       process.emitWarning(
         `[deprecated] In future versions, "sizeLimit" argument will be ignored from upload.config.providerOptions. Move it to upload.config`
       );
     }
-    const verifySize = (file) => {
-      if (sizeLimit && kbytesToBytes(file.size) > sizeLimit) {
-        throw new PayloadTooLargeError();
-      }
-    };
 
     // Ensure uploads folder exists
     const uploadPath = path.resolve(strapi.dirs.static.public, UPLOADS_FOLDER_NAME);
@@ -37,8 +32,15 @@ module.exports = {
     }
 
     return {
+      checkFileSize(file, { sizeLimit } = {}) {
+        // TODO V5: remove providerOptions sizeLimit
+        if (providerOptionsSizeLimit) {
+          if (kbytesToBytes(file.size) > providerOptionsSizeLimit) throw new PayloadTooLargeError();
+        } else if (sizeLimit) {
+          if (kbytesToBytes(file.size) > sizeLimit) throw new PayloadTooLargeError();
+        }
+      },
       uploadStream(file) {
-        verifySize(file, { sizeLimit });
         return new Promise((resolve, reject) => {
           pipeline(
             file.stream,
@@ -56,7 +58,6 @@ module.exports = {
         });
       },
       upload(file) {
-        verifySize(file);
         return new Promise((resolve, reject) => {
           // write file in public/assets folder
           fs.writeFile(path.join(uploadPath, `${file.hash}${file.ext}`), file.buffer, (err) => {
