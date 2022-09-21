84247bed115cc51899a31b5101e8fa87a2139e6b
daesoo94 <tlaeotn123@gmail.com>
2022-09-21 11:22:03 +0900

    Update get-config-path.js

--- packages/utils/typescript/lib/utils/get-config-path.js
+++ packages/utils/typescript/lib/utils/get-config-path.js
@@ -17,7 +17,7 @@ const DEFAULT_TS_CONFIG_FILENAME = 'tsconfig.json';
  */
 module.exports = (dir, { filename = DEFAULT_TS_CONFIG_FILENAME, ancestorsLookup = false } = {}) => {
   const dirAbsolutePath = path.resolve(dir);
-  const configFilePath = ts.findConfigFile(dirAbsolutePath, ts.sys.fileExists, filename);
+  const configFilePath = path.resolve(ts.findConfigFile(dirAbsolutePath, ts.sys.fileExists, filename));
 
   if (!configFilePath || ancestorsLookup) {
     return configFilePath;
