26d6bb17ce1591258b8c84050adda3879789d281
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-10-04 09:51:02 +0100

    chore: remove rogue log

--- packages/plugins/color-picker/jest.config.front.js
+++ packages/plugins/color-picker/jest.config.front.js
@@ -3,8 +3,6 @@
 const baseConfig = require('../../../jest.base-config.front');
 const pkg = require('./package.json');
 
-console.log('BASE', baseConfig);
-
 module.exports = {
   ...baseConfig,
   displayName: (pkg.strapi && pkg.strapi.name) || pkg.name,
