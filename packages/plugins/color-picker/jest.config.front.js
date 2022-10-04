cbad2f0acfbd26e8ca258ac5d4b7826f27027c3b
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-10-04 08:45:36 +0100

    fix(test): manually increase timeout to avoid document being closed early

--- packages/plugins/color-picker/jest.config.front.js
+++ packages/plugins/color-picker/jest.config.front.js
@@ -3,6 +3,8 @@
 const baseConfig = require('../../../jest.base-config.front');
 const pkg = require('./package.json');
 
+console.log('BASE', baseConfig);
+
 module.exports = {
   ...baseConfig,
   displayName: (pkg.strapi && pkg.strapi.name) || pkg.name,
