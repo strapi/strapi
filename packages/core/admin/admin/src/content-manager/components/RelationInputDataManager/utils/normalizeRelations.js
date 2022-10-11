18ebde15cd7b301b25651524edc7d3fcb1608227
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-10-11 13:33:59 +0100

    fix: check for a mismatch in modified data and relations from query

--- packages/core/admin/admin/src/content-manager/components/RelationInputDataManager/utils/normalizeRelations.js
+++ packages/core/admin/admin/src/content-manager/components/RelationInputDataManager/utils/normalizeRelations.js
@@ -23,43 +23,68 @@ export const normalizeRelation = (relation, { shouldAddLink, mainFieldName, targ
 };
 
 /*
+
+ */
+
+/**
  * Applies some transformations to existing and new relations in order to display them correctly
- * relations: raw relations data coming from useRelations
- * shouldAddLink: comes from generateRelationQueryInfos, if true we display a link to the relation (TO FIX: explanation)
+ *
+ * @param {{ data?: { pages?: [{results: []}] } }} relations - raw relations data coming from useRelations
+ * @param {{ modifiedData?: { connect: [], disconnect: [] }, shouldAddLink?: boolean, mainFieldName: string, targetModel: string }} options
+ *
+ * shouldAddLink: comes from generateRelationQueryInfos, if true we display a link to the relation (TODO:: explanation)
  * mainFieldName: name of the main field inside the relation (e.g. text field), if no displayable main field exists (e.g. date field) we use the id of the entry
  * targetModel: the model on which the relation is based on, used to create an URL link
  */
-
 export const normalizeRelations = (
   relations,
   { modifiedData = {}, shouldAddLink = false, mainFieldName, targetModel }
 ) => {
+  const { data } = relations;
+  const { pages: relationsPages = [] } = data ?? {};
+
+  const { connect = [], disconnect = [], results: modifiedDataResults = [] } = modifiedData;
+
+  const [initialRelationsPage] = relationsPages;
+
+  /**
+   * When relations are mutated and updated in the document, the current "fetched" version is
+   * temporarily out of date (whilst being fetched), so we use the modifiedData to render the relations
+   * until the `relations` data is in sync with the browser data.
+   */
+  const mismatchInRelations =
+    modifiedDataResults.length > 0 &&
+    initialRelationsPage &&
+    Array.isArray(initialRelationsPage.results) &&
+    initialRelationsPage.results.length !== modifiedDataResults.length;
+
+  /**
+   * So, depending on the above boolean, we either use the modifiedData or the relations data
+   */
+  const pagesToMutate = mismatchInRelations
+    ? [{ results: modifiedDataResults }]
+    : [...relationsPages.reverse(), ...(connect.length > 0 ? [{ results: connect }] : [])];
+
   return {
     ...relations,
     data: {
-      pages:
-        [
-          ...(relations?.data?.pages.reverse() ?? []),
-          ...(modifiedData?.connect ? [{ results: modifiedData.connect }] : []),
-        ]
-          ?.map((page) =>
-            page?.results
-              .filter(
-                (relation) =>
-                  !modifiedData?.disconnect?.find(
-                    (disconnectRelation) => disconnectRelation.id === relation.id
-                  )
-              )
-              .map((relation) =>
-                normalizeRelation(relation, {
-                  shouldAddLink,
-                  mainFieldName,
-                  targetModel,
-                })
-              )
-              .filter(Boolean)
-          )
-          ?.filter((page) => page.length > 0) ?? [],
+      pages: pagesToMutate
+        .map((page) =>
+          page.results
+            .filter(
+              (relation) =>
+                !disconnect.find((disconnectRelation) => disconnectRelation.id === relation.id)
+            )
+            .map((relation) =>
+              normalizeRelation(relation, {
+                shouldAddLink,
+                mainFieldName,
+                targetModel,
+              })
+            )
+            .filter(Boolean)
+        )
+        .filter((page) => page.length > 0),
     },
   };
 };
