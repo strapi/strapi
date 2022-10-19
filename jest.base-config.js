b315b4f086c6b2a2007e21eec7e600ab6fea24ee
Bassel Kanso <basselkanso82@gmail.com>
2022-10-19 12:11:01 +0300

    Configure typescript tests

--- jest.base-config.js
+++ jest.base-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootDir: __dirname,
   setupFilesAfterEnv: ['<rootDir>/test/unit.setup.js'],
-  modulePathIgnorePatterns: ['.cache'],
+  modulePathIgnorePatterns: ['.cache', 'dist'],
   testMatch: ['/**/__tests__/**/*.[jt]s?(x)'],
   // Use `jest-watch-typeahead` version 0.6.5. Newest version 1.0.0 does not support jest@26
   // Reference: https://github.com/jest-community/jest-watch-typeahead/releases/tag/v1.0.0
