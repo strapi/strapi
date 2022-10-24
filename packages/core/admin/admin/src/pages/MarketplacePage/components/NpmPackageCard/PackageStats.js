9cf3b62dec33c22e00b2b09b943974a023cedf56
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2022-10-24 11:40:59 +0200

    updated review comments

--- packages/core/admin/admin/src/pages/MarketplacePage/components/NpmPackageCard/PackageStats.js
+++ packages/core/admin/admin/src/pages/MarketplacePage/components/NpmPackageCard/PackageStats.js
@@ -1,47 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Box } from '@strapi/design-system/Box';
 import { Typography } from '@strapi/design-system/Typography';
-import { Flex } from '@strapi/design-system/Flex';
 import { Icon } from '@strapi/design-system/Icon';
+import { Divider } from '@strapi/design-system/Divider';
+import { Stack } from '@strapi/design-system/Stack';
 import Github from '@strapi/icons/Github';
 import Download from '@strapi/icons/Download';
 import Star from '@strapi/icons/Star';
-import { Divider } from '@strapi/design-system/Divider';
 
 const VerticalDivider = styled(Divider)`
-  width: 12px;
+  width: 0.75rem;
   transform: rotate(90deg);
 `;
 
 const PackageStats = ({ githubStars, weeklyDownloads }) => {
   return (
-    <Flex>
+    <Stack horizontal spacing={1}>
       {!!githubStars && (
         <>
-          <Icon as={Github} height="12px" width="12px" />
-          <Icon as={Star} height="12px" width="12px" marginLeft={1} color="warning500" />
-          <Box marginLeft={1}>
+          <Icon as={Github} height="0.75rem" width="0.75rem" />
+          <Icon as={Star} height="0.75rem" width="0.75rem" color="warning500" />
+          <p aria-label={`This package was starred ${githubStars} on GitHub`}>
             <Typography variant="pi" textColor="neutral800" lineHeight={16}>
               {githubStars}
             </Typography>
-          </Box>
-          <VerticalDivider
-            unsetMargin={false}
-            background="neutral200"
-            marginLeft={1}
-            marginRight={1}
-          />
+          </p>
+          <VerticalDivider unsetMargin={false} background="neutral200" />
         </>
       )}
-      <Icon as={Download} height="12px" width="12px" />
-      <Box marginLeft={1}>
+      <Icon as={Download} height="0.75rem" width="0.75rem" />
+      <p aria-label={`This package was downloaded ${weeklyDownloads} times in last 7 days`}>
         <Typography variant="pi" textColor="neutral800" lineHeight={16}>
           {weeklyDownloads}
         </Typography>
-      </Box>
-    </Flex>
+      </p>
+    </Stack>
   );
 };
 
