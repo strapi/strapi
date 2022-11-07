fba0c0fcbb2dc7a463ac8a2cb6bbfbef5be582a3
Alexandre Bodin <bodin.alex@gmail.com>
2022-11-07 16:26:34 +0100

    Init playwright

--- test/create-test-app.js
+++ test/create-test-app.js
@@ -5,8 +5,6 @@ process.env.NODE_ENV = 'test';
 const yargs = require('yargs');
 const { cleanTestApp, generateTestApp } = require('./helpers/test-app-generator');
 
-const appName = 'testApp';
-
 const databases = {
   postgres: {
     client: 'postgres',
@@ -38,7 +36,7 @@ const databases = {
   },
 };
 
-const main = async (database) => {
+const main = async (database, appName) => {
   try {
     await cleanTestApp(appName);
     await generateTestApp({ appName, database });
@@ -51,30 +49,42 @@ const main = async (database) => {
 // eslint-disable-next-line no-unused-expressions
 yargs
   .command(
-    '$0 [databaseName]',
+    '$0 [appName]',
     'Create test app',
     (yarg) => {
-      yarg.positional('databaseName', {
+      yarg.option('database', {
+        alias: 'db',
+        describe: 'Database',
         choices: Object.keys(databases),
+      });
+
+      yargs.demandOption('database');
+
+      yarg.positional('appName', {
+        type: 'string',
+        default: 'testApp',
       });
     },
     (argv) => {
-      const { databaseName } = argv;
-      if (databaseName) {
-        return main(databases[databaseName]);
+      const { database, appName = 'testApp' } = argv;
+      if (database) {
+        return main(databases[database], appName);
       }
 
-      return main({
-        client: argv.dbclient,
-        connection: {
-          host: argv.dbhost,
-          port: argv.dbport,
-          database: argv.dbname,
-          username: argv.dbusername,
-          password: argv.dbpassword,
-          filename: argv.dbfile,
+      return main(
+        {
+          client: argv.dbclient,
+          connection: {
+            host: argv.dbhost,
+            port: argv.dbport,
+            database: argv.dbname,
+            username: argv.dbusername,
+            password: argv.dbpassword,
+            filename: argv.dbfile,
+          },
         },
-      });
+        appName
+      );
     }
   )
   .help().argv;
