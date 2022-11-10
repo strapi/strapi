abfa85672ac7e688dc4f06bb4a1b60e988dc96d9
Gustav Hansen <gustav.hansen@strapi.io>
2022-11-10 18:46:39 +0100

    Chore: Replace @swc-node/jest with @swc/jest

--- jest.config.front.js
+++ jest.config.front.js
@@ -67,7 +67,7 @@ module.exports = {
   ],
   testRegex: 'tests/.*\\.test\\.js$',
   transform: {
-    '^.+\\.js$': ['@swc-node/jest', { jsx: true, dynamicImport: true }],
+    '^.+\\.js$': ['@swc/jest', { jsc: { parser: { jsx: true, dynamicImport: true } } }],
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/fileTransformer.js',
   },
