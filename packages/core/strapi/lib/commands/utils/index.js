9699ce42bd8412dfbfd6a1bf0dd4772f2de8bf59
Ben Irvin <ben@innerdvations.com>
2022-11-11 16:13:14 +0100

    fix readableBytes

--- packages/core/strapi/lib/commands/utils/index.js
+++ packages/core/strapi/lib/commands/utils/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const readableBytes = (bytes, decimals = 1, padStart = undefined) => {
+const bytesPerKb = 1024;
+const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+
+const readableBytes = (bytes, decimals = 1, padStart = 0) => {
   if (!bytes) {
     return '0';
   }
-  const bytesPerKb = 1024;
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
   const i = Math.floor(Math.log(bytes) / Math.log(bytesPerKb));
-  const result = `${parseFloat((bytes / bytesPerKb ** i).toFixed(decimals))} ${sizes[i]}`;
-  if (padStart) {
-    return result.padStart(padStart);
-  }
-  return result;
+  const result = `${parseFloat((bytes / bytesPerKb ** i).toFixed(decimals))} ${sizes[i].padStart(
+    2
+  )}`;
+
+  return result.padStart(padStart);
 };
 
 module.exports = {
