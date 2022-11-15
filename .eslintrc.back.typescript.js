8c9d3a0ff60615149e629c9bd0aada6fabd049f8
Ben Irvin <ben@innerdvations.com>
2022-11-15 08:41:33 +0100

    add missing rules

--- .eslintrc.back.typescript.js
+++ .eslintrc.back.typescript.js
@@ -4,40 +4,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   /**
-   * TODO: this should extend @strapi/eslint-config but doing so requires configuring parserOption.project, which requires tsconfig.json configuration
+   * TODO: this should extend @strapi/eslint-config/typescript but doing so requires configuring parserOption.project, which requires tsconfig.json configuration
    */
   // extends: ['plugin:@typescript-eslint/recommended'],
   globals: {
     strapi: false,
   },
-  rules: {
-    // 'import/no-dynamic-require': 'off',
-    // 'global-require': 'off',
-    // 'import/no-extraneous-dependencies': [
-    //   'error',
-    //   {
-    //     devDependencies: [
-    //       'packages/admin-test-utils/**/*.js',
-    //       'packages/generators/admin/**/*.js',
-    //       'scripts/**/*.js',
-    //       '**/test/**/*.js',
-    //       '**/tests/**/*.js',
-    //       '**/__tests__/**/*.js',
-    //       '**/__mocks__/**/*.js',
-    //     ],
-    //   },
-    // ],
-    // 'prefer-destructuring': ['error', { AssignmentExpression: { array: false } }],
-    // 'no-underscore-dangle': 'off',
-    // 'no-use-before-define': 'off',
-    // 'no-continue': 'warn',
-    // 'no-process-exit': 'off',
-    // 'no-loop-func': 'off',
-    // 'no-param-reassign': [
-    //   'error',
-    //   {
-    //     props: false,
-    //   },
-    // ],
-  },
+  // Instead of extending (which includes values that interfere with this configuration), only take the rules field
+  rules: require('./.eslintrc.back.js').rules,
 };
