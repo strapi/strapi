aa4a1ada020d133478d2ca07f92ed34633a3a9f6
Gustav Hansen <gustav.hansen@strapi.io>
2022-11-16 16:31:01 +0100

    Chore: Fix eslint errors

--- packages/admin-test-utils/lib/setup/test-bundler.js
+++ packages/admin-test-utils/lib/setup/test-bundler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // See https://github.com/swc-project/swc/issues/6460
-// SWC is not able to include the core-js polyfill for 
+// SWC is not able to include the core-js polyfill for
 // array/at automatically at the moment of writing, which
 // makes some frontend tests fail on node@14.
 require('core-js/actual/array/at');
