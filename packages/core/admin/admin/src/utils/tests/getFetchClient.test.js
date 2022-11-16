36a35d865337dd21ed756dbc345d747d69018d83
Simone Taeggi <startae14@gmail.com>
2022-11-16 10:57:38 +0100

    add some test getFetchClient utility

---
+++ packages/core/admin/admin/src/utils/tests/getFetchClient.test.js
@@ -0,0 +1,17 @@
+import getFetchClient from '../getFetchClient';
+
+describe('ADMIN | utils | getFetchClient', () => {
+  it('should return the 4 HTTP methods to call GET, POST, PUT and DELETE apis', () => {
+    const response = getFetchClient();
+    expect(response).toHaveProperty('get');
+    expect(response).toHaveProperty('post');
+    expect(response).toHaveProperty('put');
+    expect(response).toHaveProperty('delete');
+  });
+  it('should contains the headers config values', async () => {
+    const response = getFetchClient();
+    const getData = await response.get('/admin/project-type');
+    expect(getData.config.headers.Authorization).toContain('Bearer');
+    expect(getData.config.headers.Accept).toBe('application/json');
+  });
+});
