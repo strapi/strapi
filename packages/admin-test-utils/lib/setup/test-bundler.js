9e47dd0356d99672daec93fe8654a05e4a91402d
Gustav Hansen <gustav.hansen@strapi.io>
2022-11-16 15:01:17 +0100

    Chore: Polyfill core-js/array/at for frontend tests

--- packages/admin-test-utils/lib/setup/test-bundler.js
+++ packages/admin-test-utils/lib/setup/test-bundler.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// See https://github.com/swc-project/swc/issues/6460
+// SWC is not able to include the core-js polyfill for 
+// array/at automatically at the moment of writing, which
+// makes some frontend tests fail on node@14.
+require('core-js/actual/array/at');
+
 const noop = () => {};
 // eslint-disable-next-line no-undef
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
