9cfac9b63adc5e7e1fc642e579b68599905215c4
Simone Taeggi <startae14@gmail.com>
2022-11-16 14:35:30 +0100

    add a test

--- packages/core/admin/admin/src/utils/tests/getFetchClient.test.js
+++ packages/core/admin/admin/src/utils/tests/getFetchClient.test.js
@@ -8,10 +8,11 @@ describe('ADMIN | utils | getFetchClient', () => {
     expect(response).toHaveProperty('put');
     expect(response).toHaveProperty('delete');
   });
-  it('should contains the headers config values', async () => {
+  it('should contains the headers config values and the data', async () => {
     const response = getFetchClient();
     const getData = await response.get('/admin/project-type');
     expect(getData.config.headers.Authorization).toContain('Bearer');
     expect(getData.config.headers.Accept).toBe('application/json');
+    expect(getData).toHaveProperty('data');
   });
 });
