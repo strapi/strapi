1593c7a9e7f3aededbd4c14fc33a735851beadf9
Alexandre Bodin <bodin.alex@gmail.com>
2022-11-16 14:19:17 +0100

    Use core-js to get necessary polyfill in tests

--- packages/admin-test-utils/lib/setup/test-bundler.js
+++ packages/admin-test-utils/lib/setup/test-bundler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('core-js/actual');
+
 const noop = () => {};
 // eslint-disable-next-line no-undef
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
