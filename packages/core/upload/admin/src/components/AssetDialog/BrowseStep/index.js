dd7ab6a447fcd4c35403fd850278ec31f0f4b848
Jamie Howard <jamie.howard@strapi.io>
2022-11-23 15:16:22 +0000

    chore(upload): rename folderlist to foldergridlist

--- packages/core/upload/admin/src/components/AssetDialog/BrowseStep/index.js
+++ packages/core/upload/admin/src/components/AssetDialog/BrowseStep/index.js
@@ -19,7 +19,7 @@ import getTrad from '../../../utils/getTrad';
 import { getBreadcrumbDataCM } from '../../../utils';
 import getAllowedFiles from '../../../utils/getAllowedFiles';
 import { AssetGridList } from '../../AssetGridList';
-import { FolderList } from '../../FolderList';
+import { FolderGridList } from '../../FolderGridList';
 import { EmptyAssets } from '../../EmptyAssets';
 import { Breadcrumbs } from '../../Breadcrumbs';
 import SortPicker from '../../SortPicker';
@@ -196,7 +196,7 @@ export const BrowseStep = ({
       )}
 
       {folderCount > 0 && (
-        <FolderList
+        <FolderGridList
           title={
             (((isSearchingOrFiltering && assetCount > 0) || !isSearchingOrFiltering) &&
               formatMessage(
@@ -256,7 +256,7 @@ export const BrowseStep = ({
               </GridItem>
             );
           })}
-        </FolderList>
+        </FolderGridList>
       )}
 
       {assetCount > 0 && folderCount > 0 && (
--- packages/core/upload/admin/src/pages/App/MediaLibrary.js
+++ packages/core/upload/admin/src/pages/App/MediaLibrary.js
@@ -25,9 +25,9 @@ import Pencil from '@strapi/icons/Pencil';
 import { UploadAssetDialog } from '../../components/UploadAssetDialog/UploadAssetDialog';
 import { EditFolderDialog } from '../../components/EditFolderDialog';
 import { EditAssetDialog } from '../../components/EditAssetDialog';
-import { AssetGridList } from '../../components/AssetGridList';
 import { TableList } from '../../components/TableList';
-import { FolderList } from '../../components/FolderList';
+import { AssetGridList } from '../../components/AssetGridList';
+import { FolderGridList } from '../../components/FolderGridList';
 import SortPicker from '../../components/SortPicker';
 import { useAssets } from '../../hooks/useAssets';
 import { useFolders } from '../../hooks/useFolders';
@@ -263,7 +263,7 @@ export const MediaLibrary = () => {
           {canRead && isGridView && (
             <>
               {folderCount > 0 && (
-                <FolderList
+                <FolderGridList
                   title={
                     // Folders title should only appear if:
                     // user is filtering and there are assets to display, to divide both type of elements
@@ -351,7 +351,7 @@ export const MediaLibrary = () => {
                       </GridItem>
                     );
                   })}
-                </FolderList>
+                </FolderGridList>
               )}
 
               {assetCount > 0 && folderCount > 0 && (
