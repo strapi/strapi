eadfa986ea5ae4eb7dc90df5c551d6d0f8a5e47d
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-11-25 15:25:37 +0000

    fix: border styles

--- packages/core/admin/admin/src/content-manager/components/RepeatableComponent/components/Accordion.js
+++ packages/core/admin/admin/src/content-manager/components/RepeatableComponent/components/Accordion.js
@@ -13,10 +13,19 @@ export const Footer = styled(Box)`
 `;
 
 export const Content = styled(Box)`
-  border: 1px solid ${({ theme }) => theme.colors.neutral200};
   border-bottom: none;
-  border-radius: ${({ theme }) => theme.borderRadius} ${({ theme }) => theme.borderRadius} 0 0;
   overflow: hidden;
+
+  /* add the borders and make sure the top is transparent to avoid jumping with the hover effect  */
+  & > div > div {
+    border: 1px solid ${({ theme }) => theme.colors.neutral200};
+    border-top-color: transparent;
+  }
+
+  /* the top accordion _does_ need a border though */
+  & > div:first-child > div {
+    border-top: 1px solid ${({ theme }) => theme.colors.neutral200};
+  }
 
   /* Reset all the border-radius' */
   & > div > div,
@@ -24,16 +33,10 @@ export const Content = styled(Box)`
     border-radius: unset;
   }
 
+  /* Give the border radius back to the first accordion */
   & > div:first-child > div,
   & > div:first-child > div > div {
-    /* use 1px less to avoid that weird thing where the borders don't align */
-    border-radius: ${({ theme }) => `calc(${theme.borderRadius} - 1px)`}
-      ${({ theme }) => `calc(${theme.borderRadius} - 1px)`} 0 0;
-  }
-
-  /* re-add the border bottom */
-  & > div > div {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.neutral200};
+    border-radius: ${({ theme }) => theme.borderRadius} ${({ theme }) => theme.borderRadius} 0 0;
   }
 
   & [data-strapi-expanded='true'] {
