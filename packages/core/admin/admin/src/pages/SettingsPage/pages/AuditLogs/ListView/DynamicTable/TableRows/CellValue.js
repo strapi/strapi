ca74f49068cd3859c6bcc9fe191eb5b008506ad8
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2022-11-25 18:51:20 +0100

    minor change

--- packages/core/admin/admin/src/pages/SettingsPage/pages/AuditLogs/ListView/DynamicTable/TableRows/CellValue.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/AuditLogs/ListView/DynamicTable/TableRows/CellValue.js
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types';
 import useFormatTimeStamp from '../../hooks/useFormatTimeStamp';
 
 const CellValue = ({ type, value }) => {
-  let formattedValue = value || '-';
   const formatTimeStamp = useFormatTimeStamp();
 
   if (type === 'date') {
     return formatTimeStamp(value);
   }
 
-  return formattedValue;
+  return value || '-';
 };
 
 CellValue.propTypes = {
