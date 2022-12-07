c8f4c5a5e7b99444c4385d837c2028beca190f66
Alexandre Bodin <bodin.alex@gmail.com>
2022-12-07 14:43:28 +0100

    Execute missing CE register

--- packages/core/admin/ee/server/register.js
+++ packages/core/admin/ee/server/register.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const executeCERegister = require('../../server/register');
 const createAuditLogsService = require('./services/audit-logs');
 
 module.exports = async ({ strapi }) => {
   strapi.auditLogs = createAuditLogsService(strapi);
+
+  await executeCERegister({ strapi });
 };
