c35a5636e4064b581c74f10e568e7968bfda0478
Simone Taeggi <startae14@gmail.com>
2022-12-12 09:40:49 +0100

    change log content

--- packages/core/helper-plugin/lib/src/components/DateTimePicker/index.js
+++ packages/core/helper-plugin/lib/src/components/DateTimePicker/index.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { DateTimePicker as MyDateTimePicker } from '@strapi/design-system';
 
+// TODO: remove DateTimePicker component from the helper-plugin in V5
 export default function DateTimePicker(props) {
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
       console.log(
         `
-        TODO V5: remove DateTimePicker component from the helper-plugin
         Deprecation warning: Usage of "DateTimePicker" component from the helper-plugin is deprecated and will be removed in the next major release. Instead, use the DateTimePicker from the Design System: import { DateTimePicker } from '@strapi/design-system';"
         `
       );
