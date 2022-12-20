9d36efa5cfffc51627a8bcfeaf04d616d13374be
Ben Irvin <ben@innerdvations.com>
2022-12-20 12:19:00 +0100

    clean up message

--- packages/core/admin/server/controllers/websocket-api.js
+++ packages/core/admin/server/controllers/websocket-api.js
@@ -15,20 +15,21 @@ const websocket = async (ctx) => {
   if (~upgradeHeader.indexOf('websocket')) {
     wss.handleUpgrade(ctx.req, ctx.request.socket, Buffer.alloc(0), (ws) => {
       wss.emit('connection', ws, ctx.req);
+      // expects message to be valid JSON
       ws.on('message', (args) => {
         try {
           const message = JSON.parse(args.toString());
           console.log(message);
+
+          ws.send(JSON.stringify('ok'));
         } catch (e) {
-          //
+          ws.send(JSON.stringify({ message: 'invalidInput' }));
         }
       });
     });
     ctx.respond = false;
     if (options.exposeServerOn) ctx[options.exposeServerOn] = wss;
   }
-
-  // TODO: add listeners
 };
 
 module.exports = {
