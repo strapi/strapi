3cce6488d609ba33a17872b1b5954350c4ebb5c2
Simone Taeggi <startae14@gmail.com>
2022-12-21 00:30:00 +0100

    replace axiosInstance in the users-permission EmailTemplates utils api fetchData and putEmailTemplates functions to replace the api call with the getFetchClient get and put methods

--- packages/plugins/users-permissions/admin/src/pages/EmailTemplates/utils/api.js
+++ packages/plugins/users-permissions/admin/src/pages/EmailTemplates/utils/api.js
@@ -1,13 +1,17 @@
-import { axiosInstance, getRequestURL } from '../../../utils';
+import { getFetchClient } from '@strapi/admin/admin/src/utils/getFetchClient';
+import { getRequestURL } from '../../../utils';
 
 const fetchData = async () => {
-  const { data } = await axiosInstance.get(getRequestURL('email-templates'));
+  const { get } = getFetchClient();
+  const { data } = await get(getRequestURL('email-templates'));
 
   return data;
 };
 
 const putEmailTemplate = (body) => {
-  return axiosInstance.put(getRequestURL('email-templates'), body);
+  const { put } = getFetchClient();
+
+  return put(getRequestURL('email-templates'), body);
 };
 
 export { fetchData, putEmailTemplate };
