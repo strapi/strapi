ce54a2697bf48453a75fce3fc6155b754b74d7f9
Gustav Hansen <gustav.hansen@strapi.io>
2022-12-22 15:45:16 +0100

    Fix: Handle more error cases in getAPIInnerErrors()

--- packages/core/helper-plugin/lib/src/utils/getAPIInnerErrors/index.js
+++ packages/core/helper-plugin/lib/src/utils/getAPIInnerErrors/index.js
@@ -1,16 +1,30 @@
+function getErrorPayload(error) {
+  const response = error.response.data;
+
+  if (['YupValidationError', 'ValidationError'].includes(response.error.name)) {
+    return response.error.details.errors;
+  }
+
+  return [response.error];
+}
+
 export default function getAPIInnerErrors(error, { getTrad }) {
-  const errorPayload = error.response.data.error.details.errors;
-  const validationErrors = errorPayload.reduce((acc, err) => {
-    acc[err.path.join('.')] = {
-      id: getTrad(`apiError.${err.message}`),
-      defaultMessage: err.message,
-      values: {
-        field: err.path[err.path.length - 1],
-      },
+  return getErrorPayload(error).reduce((acc, err, index) => {
+    const path = err?.path ? err?.path.join('.') : `${err.name}.${index}`;
+    const { message } = err;
+
+    acc[path] = {
+      id: getTrad(`apiError.${message}`),
+      defaultMessage: message,
+
+      // Only errors of type YupValidationError contain a path
+      values: err?.path
+        ? {
+            field: err.path[err.path.length - 1],
+          }
+        : undefined,
     };
 
     return acc;
   }, {});
-
-  return validationErrors;
 }
