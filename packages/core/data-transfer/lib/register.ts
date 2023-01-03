fd54488fadf28a8b4b8fdda45776647993a3caec
Ben Irvin <ben@innerdvations.com>
2023-01-03 15:31:21 +0100

    require admin::transfer.push permission

--- packages/core/data-transfer/lib/register.ts
+++ packages/core/data-transfer/lib/register.ts
@@ -5,7 +5,12 @@ const registerTransferRoute = (strapi: any) => {
     method: 'GET',
     path: '/transfer',
     handler: createTransferController(),
-    config: { auth: false },
+    config: {
+      policies: [
+        'admin::isAuthenticatedAdmin',
+        { name: 'admin::hasPermissions', config: { actions: ['admin::transfer.push'] } },
+      ],
+    },
   });
 };
 
