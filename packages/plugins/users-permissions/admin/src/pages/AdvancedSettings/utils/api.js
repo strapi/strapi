eff1d14ec2a90718ec7be5b6eb4f78e4956bfcaa
Simone Taeggi <startae14@gmail.com>
2023-01-05 17:08:01 +0100

    replace axiosInstance in the AdvancedSettings putAdvancedSettings utils to replace the api call with the getFetchClient put method

--- packages/plugins/users-permissions/admin/src/pages/AdvancedSettings/utils/api.js
+++ packages/plugins/users-permissions/admin/src/pages/AdvancedSettings/utils/api.js
@@ -1,5 +1,5 @@
 import { getFetchClient } from '@strapi/admin/admin/src/utils/getFetchClient';
-import { axiosInstance, getRequestURL } from '../../../utils';
+import { getRequestURL } from '../../../utils';
 
 const fetchData = async () => {
   const { get } = getFetchClient();
@@ -9,7 +9,9 @@ const fetchData = async () => {
 };
 
 const putAdvancedSettings = (body) => {
-  return axiosInstance.put(getRequestURL('advanced'), body);
+  const { put } = getFetchClient();
+
+  return put(getRequestURL('advanced'), body);
 };
 
 export { fetchData, putAdvancedSettings };
