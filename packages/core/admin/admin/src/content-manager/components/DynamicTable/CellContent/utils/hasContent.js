623ec80cae70635ac33b7f237c5e912703c8c650
Cameron Paczek <cpazek@gmail.com>
2023-01-19 19:21:31 -0800

    remove console.log

--- packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/utils/hasContent.js
+++ packages/core/admin/admin/src/content-manager/components/DynamicTable/CellContent/utils/hasContent.js
@@ -53,8 +53,6 @@ export default function hasContent(type, content, metadatas, fieldSchema) {
       doesn't deal with them.
   */
   if (isFieldTypeNumber(type) && type !== 'biginteger') {
-    console.log('checking if number', isNumber(content));
-
     if (metadatas.label === 'id' && ![undefined, null].includes(content)) {
       return true;
     }
