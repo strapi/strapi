e8bc90744d967041cf93207c7d4e82a13d335f0d
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-01-24 11:34:12 +0000

    chore: update DS to 1.6.0

--- packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/index.test.js
+++ packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/index.test.js
@@ -215,7 +215,6 @@ describe('Plugin | Users and Permissions | RoleListPage', () => {
         font-size: 0.75rem;
         line-height: 1.33;
         font-weight: 600;
-        line-height: 1.14;
         color: #32324d;
       }
 
