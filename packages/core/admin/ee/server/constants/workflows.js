73dbd2cfd9796d314aadc488fffb142928d1e596
Marc-Roig <marc12info@gmail.com>
2023-01-24 13:58:28 +0100

    workflows find and findOne services

---
+++ packages/core/admin/ee/server/constants/workflows.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// TODO concatenate admin + content type singular name
+module.exports = {
+  WORKFLOW_MODEL_UID: 'admin::workflow',
+};
--- packages/core/admin/ee/server/services/workflow.js
+++ packages/core/admin/ee/server/services/workflow.js
@@ -1,3 +1,15 @@
 'use strict';
 
-module.exports = {};
+const { WORKFLOW_MODEL_UID } = require('../constants/workflows');
+
+module.exports = ({ strapi }) => ({
+  find(opts) {
+    const params = { ...opts };
+    return strapi.entityService.findMany(WORKFLOW_MODEL_UID, params);
+  },
+
+  findOne(id) {
+    const params = {};
+    return strapi.entityService.findOne(WORKFLOW_MODEL_UID, id, params);
+  },
+});
