dcf70144c5116528f163c33be494be0ff07d08c4
Ben Irvin <ben@innerdvations.com>
2023-01-25 15:10:39 +0100

    test exiting on sigint

--- packages/core/strapi/bin/strapi.js
+++ packages/core/strapi/bin/strapi.js
@@ -27,6 +27,10 @@ const {
   validateExcludeOnly,
 } = require('../lib/commands/transfer/utils');
 
+process.on('SIGINT', function () {
+  process.exit();
+});
+
 const checkCwdIsStrapiApp = (name) => {
   const logErrorAndExit = () => {
     console.log(
