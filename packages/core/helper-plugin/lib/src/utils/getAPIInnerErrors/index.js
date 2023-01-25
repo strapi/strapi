e565118862ae7c2729e0f8443ab020b5d4fb5079
Gustav Hansen <gustav.hansen@strapi.io>
2023-01-25 15:08:58 +0100

    Fix getAPIInnerErrors() tests

--- packages/core/helper-plugin/lib/src/utils/getAPIInnerErrors/index.js
+++ packages/core/helper-plugin/lib/src/utils/getAPIInnerErrors/index.js
@@ -1,10 +1,27 @@
 import { normalizeAPIError } from '../../hooks/useAPIErrorHandler';
 
+/**
+ *
+ * Returns a normalized error message
+ *
+ * @deprecated
+ * @export
+ * @param {error} error - API error response object
+ * @param {object<{ getTrad }>} - Error message prefix callback
+ * @return {object}
+ */
 export default function getAPIInnerErrors(error, { getTrad }) {
   const normalizedError = normalizeAPIError(error, getTrad);
 
   if (normalizedError?.errors) {
-    return normalizedError.errors;
+    return normalizedError.errors.reduce((acc, error) => {
+      acc[error.values.path] = {
+        id: error.id,
+        defaultMessage: error.defaultMessage,
+      };
+
+      return acc;
+    }, {});
   }
 
   return normalizedError.defaultMessage;
