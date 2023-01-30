2aa3804787a42e9ce0e49c04642852cc1919a4f0
Gustav Hansen <gustav.hansen@strapi.io>
2023-01-30 16:27:39 +0100

    Chore: Add component tests for Stages and Stage

--- packages/core/admin/ee/admin/pages/SettingsPage/pages/ReviewWorkflows/constants.js
+++ packages/core/admin/ee/admin/pages/SettingsPage/pages/ReviewWorkflows/constants.js
@@ -6,6 +6,6 @@ export const ACTION_SET_LOADING_STATE = `Settings/Review_Workflows/SET_STATE`;
 export const ACTION_SET_STAGES = `Settings/Review_Workflows/SET_DATA`;
 
 export const StageType = PropTypes.shape({
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
 });
