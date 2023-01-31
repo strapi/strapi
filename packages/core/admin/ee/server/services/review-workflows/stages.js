545b5657823666505509e7ec325d85be5ff4bd9f
nathan-pichon <nathan.pichon@strapi.io>
2023-01-31 20:16:53 +0100

    fix(review-workflow): filters in stages requests

--- packages/core/admin/ee/server/services/review-workflows/stages.js
+++ packages/core/admin/ee/server/services/review-workflows/stages.js
@@ -5,7 +5,7 @@ const { STAGE_MODEL_UID } = require('../../constants/workflows');
 module.exports = ({ strapi }) => ({
   find({ workflowId, populate }) {
     const params = {
-      filter: { workflow: workflowId },
+      filters: { workflow: workflowId },
       populate,
     };
     return strapi.entityService.findMany(STAGE_MODEL_UID, params);
@@ -13,7 +13,7 @@ module.exports = ({ strapi }) => ({
 
   findById(id, { workflowId, populate }) {
     const params = {
-      filter: { workflow: workflowId },
+      filters: { workflow: workflowId },
       populate,
     };
     return strapi.entityService.findOne(STAGE_MODEL_UID, id, params);
