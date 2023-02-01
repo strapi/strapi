902edc013c22a037c9cb61260ff5bcc6502ac556
Cameron Paczek <cpazek@gmail.com>
2023-02-01 12:20:46 -0800

    fix bad json parsing

--- packages/core/database/lib/fields/json.js
+++ packages/core/database/lib/fields/json.js
@@ -4,11 +4,22 @@ const Field = require('./field');
 
 class JSONField extends Field {
   toDB(value) {
+    try {
+      JSON.parse(value);
+    } catch (e) {
+      throw new Error(`Invalid JSON value`);
+    }
     return JSON.stringify(value);
   }
 
   fromDB(value) {
-    if (typeof value === 'string') return JSON.parse(value);
+    if (typeof value === 'string') {
+      try {
+        return JSON.parse(value);
+      } catch (e) {
+        throw new Error(`Invalid JSON value`);
+      }
+    }
     return value;
   }
 }
