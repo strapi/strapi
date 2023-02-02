1b51476e9332efe07209ba6aad1a3b846fcff26a
Convly <jean-sebastien.herbaux@epitech.eu>
2023-02-02 16:52:48 +0100

    Add transfer.token.salt to the examples app admin
    Co-authored-by: Christian <christiancp100@gmail.com>
    Co-authored-by: Simone <simone.taeggi@strapi.io>
    Co-authored-by: Bassel Kanso <basselkanso82@gmail.com>
    Co-authored-by: Ben Irvin <innerdvations@users.noreply.github.com>

--- examples/kitchensink/config/admin.js
+++ examples/kitchensink/config/admin.js
@@ -6,4 +6,9 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT', 'example-salt'),
   },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'example-salt'),
+    },
+  },
 });
