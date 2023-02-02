e2d8282e81e17e19e3955b00578ce7a994a23d12
Jamie Howard <jamie.howard@strapi.io>
2023-02-02 15:41:35 +0000

    feat(helper-plugin): getYupInnerErrors use yup params in error messaging

--- packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
+++ packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
@@ -1,10 +1,11 @@
-import { get } from 'lodash';
-
 const getYupInnerErrors = (error) => {
-  return get(error, 'inner', []).reduce((acc, curr) => {
-    acc[curr.path.split('[').join('.').split(']').join('')] = {
-      id: curr.message,
-      defaultMessage: curr.message,
+  return (error?.inner || []).reduce((acc, currentError) => {
+    acc[currentError.path.split('[').join('.').split(']').join('')] = {
+      id: currentError.message,
+      defaultMessage: currentError.message,
+      values: Object.keys(currentError?.params || {})
+        .filter((key) => !['label', 'originalValue', 'path', 'value'].includes(key))
+        .reduce((current, key) => Object.assign(current, { [key]: currentError.params[key] }), {}),
     };
 
     return acc;
