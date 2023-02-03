2c0fa4897a6a447f1f022526645fc5200e2f95cf
Simone Taeggi <startae14@gmail.com>
2023-02-03 18:05:12 +0100

    refactor unit test

--- packages/core/admin/admin/src/pages/SettingsPage/pages/TransferTokens/EditView/utils/index.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/TransferTokens/EditView/utils/index.js
@@ -1,5 +1,4 @@
 import getDateOfExpiration from './getDateOfExpiration';
 import schema from './schema';
-import transformPermissionsData from './transformPermissionsData';
 
-export { getDateOfExpiration, schema, transformPermissionsData };
+export { getDateOfExpiration, schema };
