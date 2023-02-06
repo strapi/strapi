f2980f0618e786681c44a67a986e8831d9f2b62e
Jamie Howard <jamie.howard@strapi.io>
2023-02-06 10:48:37 +0000

    fix(CI): test app generation for api test

--- .github/actions/run-api-tests/script.sh
+++ .github/actions/run-api-tests/script.sh
@@ -5,11 +5,12 @@ else
   export STRAPI_DISABLE_LICENSE_PING=true
 fi
 
-export ENV_PATH="$(pwd)/test-apps/api/.env"
+appPath='test-apps/api'
+export ENV_PATH="$(pwd)/$appPath/.env"
 export JWT_SECRET="aSecret"
 
 opts=($DB_OPTIONS)
 
 yarn run -s build:ts
-yarn run -s test:generate-app "${opts[@]}"
+yarn run -s test:generate-app "${opts[@]}" --appPath "$appPath" --template ''
 yarn run -s test:api --no-generate-app
