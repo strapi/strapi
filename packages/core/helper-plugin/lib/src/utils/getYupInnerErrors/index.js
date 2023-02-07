d4e111a02775a0b1f790d376c37af22c860f3121
Jamie Howard <jamie.howard@strapi.io>
2023-02-07 17:14:52 +0000

    refactor(helper-plugin): function for extracting error values

--- packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
+++ packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
@@ -1,11 +1,20 @@
+/**
+ * Extract relevant values from the Yup error that can be used to enhance front
+ * end error messaging
+ * @param {Object} yupErrorParams
+ * @returns {Object}
+ */
+const extractValuesFromYupError = (yupErrorParams = {}) =>
+  Object.keys(yupErrorParams)
+    .filter((key) => !['label', 'originalValue', 'path', 'value'].includes(key))
+    .reduce((current, key) => Object.assign(current, { [key]: yupErrorParams[key] }), {});
+
 const getYupInnerErrors = (error) => {
   return (error?.inner || []).reduce((acc, currentError) => {
     acc[currentError.path.split('[').join('.').split(']').join('')] = {
       id: currentError.message,
       defaultMessage: currentError.message,
-      values: Object.keys(currentError?.params || {})
-        .filter((key) => !['label', 'originalValue', 'path', 'value'].includes(key))
-        .reduce((current, key) => Object.assign(current, { [key]: currentError.params[key] }), {}),
+      values: extractValuesFromYupError(currentError?.params || {}),
     };
 
     return acc;
