d3586dd13e724049a035574f87c8a776c25f8e0b
Boegie19 <Bramboegheim@gmail.com>
2023-02-07 12:03:35 +0100

    fix port

--- test/jest-api.global-setup.js
+++ test/jest-api.global-setup.js
@@ -19,7 +19,7 @@ const databases = {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
-      port: 3307,
+      port: 3306,
       database: 'strapi_test',
       user: 'root',
       password: 'strapi',
