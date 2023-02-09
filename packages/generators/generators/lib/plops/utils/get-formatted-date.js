ab9ba1a43d4031590a8b926742adb692cec21c47
dogusdeniz <32245914+dogusdeniz@users.noreply.github.com>
2023-02-09 17:25:15 +0300

    Update packages/generators/generators/lib/plops/utils/get-formatted-date.js
    Co-authored-by: Ben Irvin <ben@innerdvations.com>

--- packages/generators/generators/lib/plops/utils/get-formatted-date.js
+++ packages/generators/generators/lib/plops/utils/get-formatted-date.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = (date = new Date()) => {
-  return new Date(date.getTime() - date.getTimezoneOffset() * 60000).toJSON().replace(/[-:T]/g, "").replace(/\....Z/, "");
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+    .toJSON()
+    .replace(/[-:]/g, '.')
+    .replace(/\....Z/, '');
 };
