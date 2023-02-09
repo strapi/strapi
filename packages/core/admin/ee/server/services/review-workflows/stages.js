a542051b8a3431899ee788dfcbc12aaaed8686ae
nathan-pichon <nathan.pichon@strapi.io>
2023-02-09 15:01:52 +0100

    docs(review-workflows): add jsdoc to getDiffBetweenStages function

--- packages/core/admin/ee/server/services/review-workflows/stages.js
+++ packages/core/admin/ee/server/services/review-workflows/stages.js
@@ -67,6 +67,20 @@ module.exports = ({ strapi }) => {
   };
 };
 
+/**
+ * Compares two arrays of stages and returns an object indicating the differences.
+ * @typedef {{id: Number, name: String, workflow: Number}} Stage
+ * @typedef {{created: Stage[], updated: Stage[], deleted: Stage[]}} DiffStages
+ *
+ * The DiffStages object has three properties: `created`, `updated`, and `deleted`.
+ * `created` is an array of stages that are in `comparisonStages` but not in `sourceStages`.
+ * `updated` is an array of stages that have different names in `comparisonStages` and `sourceStages`.
+ * `deleted` is an array of stages that are in `sourceStages` but not in `comparisonStages`.
+ *
+ * @param {Stage[]} sourceStages
+ * @param {Stage[]} comparisonStages
+ * @returns { DiffStages }
+ */
 function getDiffBetweenStages(sourceStages, comparisonStages) {
   const result = comparisonStages.reduce(
     (acc, stageToCompare) => {
