639e1d10332336afacfa47c71b878ec345a3512a
Jamie Howard <jamie.howard@strapi.io>
2023-02-10 11:53:14 +0000

    refactor(helper-plugin)

--- packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
+++ packages/core/helper-plugin/lib/src/utils/getYupInnerErrors/index.js
@@ -1,21 +1,26 @@
 /**
- * Extract relevant values from the Yup error that can be used to enhance front
+ * Extract relevant value from the Yup error that can be used to enhance front
  * end error messaging
- * @param {String} errorType
- * @param {Object} errorParams
+ * @param {string|undefined} errorType
+ * @param {Object|undefined} errorParams
  * @returns {Object} values to pass to error translation string
  */
-const extractValuesFromYupError = (errorType, errorParams = {}) =>
-  Object.keys(errorParams)
-    .filter((key) => errorType === key)
-    .reduce((current, key) => Object.assign(current, { [key]: errorParams[key] }), {});
+const extractValuesFromYupError = (errorType, errorParams) => {
+  if (!errorType || !errorParams) {
+    return {};
+  }
+
+  return {
+    [errorType]: errorParams[errorType],
+  };
+};
 
 const getYupInnerErrors = (error) =>
   (error?.inner || []).reduce((acc, currentError) => {
     acc[currentError.path.split('[').join('.').split(']').join('')] = {
       id: currentError.message,
       defaultMessage: currentError.message,
-      values: extractValuesFromYupError(currentError.type, currentError?.params || {}),
+      values: extractValuesFromYupError(currentError?.type, currentError?.params),
     };
 
     return acc;
