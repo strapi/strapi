db4036bc4dfc741d8fe8e8c3f1259e2c83198b6e
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-02-16 13:51:54 +0100

    chore: remove axios-instance and fix types for StrapiApp

--- examples/getstarted/src/index.js
+++ examples/getstarted/src/index.js
@@ -7,7 +7,9 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register({ strapi }) {},
+  register(app) {
+    console.log(app);
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
