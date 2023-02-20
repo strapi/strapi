bd0c7a7e2559bdc295abaf5221d6365e8a928210
Marc-Roig <marc12info@gmail.com>
2023-02-20 14:28:22 +0100

    remove repeated test

--- packages/providers/upload-aws-s3/lib/__tests__/getBucketFromUrl.test.js
+++ packages/providers/upload-aws-s3/lib/__tests__/getBucketFromUrl.test.js
@@ -29,12 +29,6 @@ describe('Test for URLs', () => {
     });
   });
 
-  test('Virtual hosted style', async () => {
-    const url = 'https://s3.us-east-1.amazonaws.com/bucket';
-    const { bucket } = getBucketFromUrl(url);
-    expect(bucket).toEqual('bucket');
-  });
-
   test('S3 access point', async () => {
     const url = 'https://bucket.s3-accesspoint.us-east-1.amazonaws.com';
     const { bucket } = getBucketFromUrl(url);
