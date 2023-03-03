ecc36bc03079ae265c0388b604c5efd193e99182
nathan-pichon <nathan.pichon@strapi.io>
2023-03-03 12:09:39 +0100

    test(review-workflows): update mock to test method PUT

--- packages/core/admin/ee/server/middlewares/__tests__/review-workflows.test.js
+++ packages/core/admin/ee/server/middlewares/__tests__/review-workflows.test.js
@@ -13,6 +13,7 @@ describe('Review workflows middlewares', () => {
   describe('contentTypeMiddleware', () => {
     test('Should add middleware to content-type-builder route', () => {
       const ctxMock = {
+        method: 'PUT',
         request: {
           body: {
             contentType: {
@@ -29,7 +30,8 @@ describe('Review workflows middlewares', () => {
 
       expect(strapiMock.server.router.use).toBeCalled();
       expect(strapiMock.server.router.use).toBeCalledWith(
-        '/content-type-builder/content-types/:uid?'
+        '/content-type-builder/content-types/:uid?',
+        expect.any(Function)
       );
       expect(ctxMock.request.body.contentType.reviewWorkflows).toBeUndefined();
       expect(ctxMock.request.body.contentType.options?.reviewWorkflows).toBe(true);
