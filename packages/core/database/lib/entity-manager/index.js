1c1d8c077232508546074868817a38d9a0cea46e
Cameron Paczek <cpazek@gmail.com>
2023-03-05 15:19:37 -0800

    remove transaction for select query

--- packages/core/database/lib/entity-manager/index.js
+++ packages/core/database/lib/entity-manager/index.js
@@ -905,8 +905,7 @@ const createEntityManager = (db) => {
                   .whereIn(inverseJoinColumn.name, nonExistingRelsIds)
                   .where(joinTable.on || {})
                   .groupBy(inverseJoinColumn.name)
-                  .from(joinTable.name)
-                  .transacting(trx);
+                  .from(joinTable.name);
 
                 const maxMap = maxResults.reduce(
                   (acc, res) => Object.assign(acc, { [res[inverseJoinColumn.name]]: res.max }),
@@ -991,8 +990,7 @@ const createEntityManager = (db) => {
                   .whereIn(inverseJoinColumn.name, nonExistingRelsIds)
                   .where(joinTable.on || {})
                   .groupBy(inverseJoinColumn.name)
-                  .from(joinTable.name)
-                  .transacting(trx);
+                  .from(joinTable.name);
 
                 const maxMap = maxResults.reduce(
                   (acc, res) => Object.assign(acc, { [res[inverseJoinColumn.name]]: res.max }),
