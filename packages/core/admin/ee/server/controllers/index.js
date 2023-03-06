e0e2084422e306aa2eab4051383ad3aee2cdecce
Alexandre Bodin <bodin.alex@gmail.com>
2023-03-06 21:46:45 +0100

    Move RBAC into CE

--- packages/core/admin/ee/server/controllers/index.js
+++ packages/core/admin/ee/server/controllers/index.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   authentication: require('./authentication'),
-  permission: require('./permission'),
-  role: require('./role'),
-  user: require('./user'),
+  // permission: require('./permission'),
+  // role: require('./role'),
+  // user: require('./user'),
   auditLogs: require('./audit-logs'),
   admin: require('./admin'),
 };
