deb656d24792ef3c29d49f986ce382fd655cd104
Marc-Roig <marc12info@gmail.com>
2023-03-13 18:52:28 +0100

    simplify compose visitors

--- packages/core/utils/lib/sanitize/visitors/index.js
+++ packages/core/utils/lib/sanitize/visitors/index.js
@@ -1,22 +1,13 @@
 'use strict';
 
 const composeVisitors = (visitors) => {
-  /*
-    reduceRight is used to iterate through the array of visitors
-    in reverse order, starting with the last method in the array.
-    The initial value for the reduction is an empty function
-    that serves as the final "next" function in the sequence.
-  */
-  return visitors.reduceRight(
-    (next, visitor) => {
-      return async (options, utils) => {
-        await visitor(options, utils);
-        // If the visitor did not remove the key, call the next one.
-        if (options.key in options.data) return next(options, utils);
-      };
-    },
-    () => {}
-  );
+  return async (options, utils) => {
+    for (const visitor of visitors) {
+      await visitor(options, utils);
+      // If the visitor did not remove the key, continue with the next one.
+      if (!(options.key in options.data)) break;
+    }
+  };
 };
 
 module.exports = {
