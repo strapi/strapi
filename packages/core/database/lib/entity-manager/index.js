d94dfdc2db700213e011961f0acbf3bb4f654477
Marc-Roig <marc12info@gmail.com>
2023-03-14 09:01:01 +0100

    mention future support for join columns

--- packages/core/database/lib/entity-manager/index.js
+++ packages/core/database/lib/entity-manager/index.js
@@ -1199,11 +1199,14 @@ const createEntityManager = (db) => {
           );
         }
 
-        // TODO: add support for cloning polymorphic relations
+        if (attribute.joinColumn) {
+          // TODO: add support for cloning oneToMany relations on the owning side
+          return;
+        }
+
         if (!attribute.joinTable) {
-          throw new DatabaseError(
-            `Cloning relations of type ${attribute.relation} is not supported`
-          );
+          // TODO: add support for cloning polymorphic relations
+          return;
         }
 
         if (isOneToAny(attribute) && isBidirectional(attribute)) {
