c983ff54635e061bef9471563f04e077853c02d4
Alexandre Bodin <bodin.alex@gmail.com>
2023-03-22 10:22:18 +0100

    Apply pr review changes

--- packages/utils/logger/src/formats/exclude-colors.ts
+++ packages/utils/logger/src/formats/exclude-colors.ts
@@ -4,7 +4,7 @@ import { format, Logform } from 'winston';
  * This will remove the chalk color codes from the message provided.
  * It's used to log plain text in the log file
  */
-export default format.printf(({ message }: Logform.TransformableInfo) => {
+export default format.printf(({ message }) => {
   if (typeof message !== 'string') {
     return message;
   }
