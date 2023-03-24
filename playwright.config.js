4158c3e717e7bce1f92748c814d7b1e6e8a4ff21
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-03-24 17:42:48 +0000

    chore: add test-apps to workspace

--- playwright.config.js
+++ playwright.config.js
@@ -95,7 +95,7 @@ const config = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd test-apps/playwright && yarn start-experimental',
+    command: 'cd test-apps/playwright && yarn start',
     port: 1337,
     timeout: 30 * 1000,
     reuseExistingServer: !process.env.CI,
