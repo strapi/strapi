26d66c6a69da3db3be8cd30c2da7069ec3ff9ac2
nathan-pichon <nathan.pichon@strapi.io>
2023-03-27 11:43:52 +0200

    fix(review-workflows): add default value on destructuring

--- packages/core/admin/ee/admin/content-manager/pages/EditView/InformationBox/InformationBoxEE.js
+++ packages/core/admin/ee/admin/content-manager/pages/EditView/InformationBox/InformationBoxEE.js
@@ -25,10 +25,7 @@ export function InformationBoxEE() {
   const { formatMessage } = useIntl();
   const { formatAPIError } = useAPIErrorHandler();
 
-  const {
-    workflow: { data: workflows },
-  } = useReviewWorkflows(activeWorkflowStage?.workflow?.id);
-  const workflow = workflows?.at(0);
+  const { workflows: { data: [workflow] = [] } = {} } = useReviewWorkflows();
 
   const { error, isLoading, mutateAsync } = useMutation(async ({ entityId, stageId, uid }) => {
     const {
