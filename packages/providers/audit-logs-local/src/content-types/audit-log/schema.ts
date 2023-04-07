d4cf49f28fcdc7ba3f669c4d93a5140c372ab5bb
Alexandre Bodin <bodin.alex@gmail.com>
2023-04-07 11:22:22 +0200

    Migrate audit log provider

--- packages/providers/audit-logs-local/src/content-types/audit-log/schema.ts
+++ packages/providers/audit-logs-local/src/content-types/audit-log/schema.ts
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports = {
+export default {
   kind: 'collectionType',
   collectionName: 'strapi_audit_logs',
   info: {
--- packages/providers/audit-logs-local/src/index.ts
+++ packages/providers/audit-logs-local/src/index.ts
@@ -1,9 +1,19 @@
-'use strict';
+import type { Strapi } from '@strapi/strapi';
+import { schema as auditLogContentType } from './content-types/audit-log';
 
-const auditLogContentType = require('./content-types/audit-log');
+interface Event {
+  action: string;
+  date: Date;
+  userId: string | number;
+  payload: Record<string, unknown>;
+}
 
-const provider = {
-  async register({ strapi }) {
+interface Log extends Omit<Event, 'userId'> {
+  user: string | number;
+}
+
+export = {
+  async register({ strapi }: { strapi: Strapi }) {
     const contentTypes = strapi.container.get('content-types');
     if (!contentTypes.keys().includes('admin::audit-log')) {
       strapi.container.get('content-types').add('admin::', { 'audit-log': auditLogContentType });
@@ -11,10 +21,10 @@ const provider = {
 
     // Return the provider object
     return {
-      async saveEvent(event) {
-        // Rewrite userId key to user
-        const auditLog = { ...event, user: event.userId };
-        delete auditLog.userId;
+      async saveEvent(event: Event) {
+        const { userId, ...rest } = event;
+
+        const auditLog: Log = { ...rest, user: userId };
 
         // Save to database
         await strapi.entityService.create('admin::audit-log', { data: auditLog });
@@ -22,7 +32,7 @@ const provider = {
         return this;
       },
 
-      findMany(query) {
+      findMany(query: Record<string, unknown>) {
         return strapi.entityService.findPage('admin::audit-log', {
           populate: ['user'],
           fields: ['action', 'date', 'payload'],
@@ -30,14 +40,14 @@ const provider = {
         });
       },
 
-      findOne(id) {
+      findOne(id: string | number) {
         return strapi.entityService.findOne('admin::audit-log', id, {
           populate: ['user'],
           fields: ['action', 'date', 'payload'],
         });
       },
 
-      deleteExpiredEvents(expirationDate) {
+      deleteExpiredEvents(expirationDate: Date) {
         return strapi.entityService.deleteMany('admin::audit-log', {
           filters: {
             date: {
@@ -49,5 +59,3 @@ const provider = {
     };
   },
 };
-
-module.exports = provider;
