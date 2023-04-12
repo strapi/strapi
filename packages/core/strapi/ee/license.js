25c15f9ebfa392a812c96df401aee82ac586efe1
nathan-pichon <nathan.pichon@strapi.io>
2023-04-12 19:32:37 +0200

    feat(license): use strapi.fetch method instead of fetch

--- packages/core/strapi/ee/license.js
+++ packages/core/strapi/ee/license.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const { join } = require('path');
 const crypto = require('crypto');
-const fetch = require('node-fetch');
 
 const machineId = require('../lib/utils/machine-id');
 
@@ -69,12 +68,14 @@ const throwError = () => {
   throw new LicenseCheckError('Could not proceed to the online validation of your license.', true);
 };
 
-const fetchLicense = async (key, projectId) => {
-  const response = await fetch(`https://license.strapi.io/api/licenses/validate`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ key, projectId, deviceId: machineId() }),
-  }).catch(throwError);
+const fetchLicense = async ({ strapi }, key, projectId) => {
+  const response = await strapi
+    .fetch(`https://license.strapi.io/api/licenses/validate`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ key, projectId, deviceId: machineId() }),
+    })
+    .catch(throwError);
 
   const contentType = response.headers.get('Content-Type');
 
