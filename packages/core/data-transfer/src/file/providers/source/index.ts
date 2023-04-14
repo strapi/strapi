4ec704416fc0bc94d9c6a278f4a70259d8b77feb
Ben Irvin <ben@innerdvations.com>
2023-04-14 18:28:46 +0200

    filesystem agnostic loading of tar files

--- packages/core/data-transfer/src/file/providers/source/index.ts
+++ packages/core/data-transfer/src/file/providers/source/index.ts
@@ -45,6 +45,34 @@ export const createLocalFileSourceProvider = (options: ILocalFileSourceProviderO
   return new LocalFileSourceProvider(options);
 };
 
+/**
+ * Note: in versions of the transfer engine <=4.9.0, exports were generated with windows paths
+ * on Windows systems, and posix paths on posix systems.
+ *
+ * We now store all paths as posix, but need to leave a separator conversion for legacy purposes, and to
+ * support manually-created tar files coming from Windows systems (ie, if a user creates a
+ * backup file with a windows tar tool rather than using the `export` command)
+ *
+ * Because of this, export/import files may never contain files with a forward slash in the name, even escaped
+ * */
+
+// Check if the directory of a given filePath (which can be either posix or win32) resolves to the same as the given posix-format path posixDirName
+const isDirPathEquivalent = (posixDirName: string, filePath: string) => {
+  // if win32 convert to posix, then get dirname
+  const normalizedDir = path.posix.dirname(filePath.split(path.win32.sep).join(path.posix.sep));
+
+  return posixDirName === normalizedDir;
+};
+
+// Check if two file paths that can be either in posix or win32 format resolves to the same file
+const isFilePathEquivalent = (fileA: string, fileB: string) => {
+  // Check if paths appear to be win32 or posix, and if win32 convert to posix
+  const normalizedPathA = fileA.split(path.win32.sep).join(path.posix.sep);
+  const normalizedPathB = fileB.split(path.win32.sep).join(path.posix.sep);
+
+  return !path.posix.relative(normalizedPathB, normalizedPathA).length;
+};
+
 class LocalFileSourceProvider implements ISourceProvider {
   type: ProviderType = 'source';
 
@@ -133,15 +161,15 @@ class LocalFileSourceProvider implements ISourceProvider {
       [
         inStream,
         new tar.Parse({
+          // find only files in the assets/uploads folder
           filter(filePath, entry) {
             if (entry.type !== 'File') {
               return false;
             }
-
-            const parts = filePath.split('/');
-            return parts[0] === 'assets' && parts[1] === 'uploads';
+            return isDirPathEquivalent('./assets/uploads', filePath);
           },
           onentry(entry) {
+            // TODO: Check if we need to handle win32 paths here for the assets
             const { path: filePath, size = 0 } = entry;
             const file = path.basename(filePath);
             const asset: IAsset = {
@@ -196,14 +224,7 @@ class LocalFileSourceProvider implements ISourceProvider {
               return false;
             }
 
-            const parts = path.relative('.', filePath).split('/');
-
-            // TODO: this method is limiting us from having additional subdirectories and is requiring us to remove any "./" prefixes (the path.relative line above)
-            if (parts.length !== 2) {
-              return false;
-            }
-
-            return parts[0] === directory;
+            return isDirPathEquivalent(directory, filePath);
           },
 
           async onentry(entry) {
@@ -261,7 +282,11 @@ class LocalFileSourceProvider implements ISourceProvider {
              * Filter the parsed entries to only keep the one that matches the given filepath
              */
             filter(entryPath, entry) {
-              return !path.relative(filePath, entryPath).length && entry.type === 'File';
+              if (entry.type !== 'File') {
+                return false;
+              }
+
+              return isFilePathEquivalent(entryPath, filePath);
             },
 
             async onentry(entry) {
