9b7b25a3b978a7a8a793683edbaae0bb5991a74c
Ben Irvin <ben@innerdvations.com>
2023-04-14 15:42:03 +0200

    static imports

--- packages/core/strapi/lib/commands/index.js
+++ packages/core/strapi/lib/commands/index.js
@@ -2,37 +2,37 @@
 
 const { Command } = require('commander');
 
-const availableCommands = [
-  'admin/create-user',
-  'admin/reset-user-password',
-  'build',
-  'configuration/dump',
-  'configuration/restore',
-  'console',
-  'content-types/list',
-  'controllers/list',
-  'develop',
-  'export',
-  'generate',
-  'hooks/list',
-  'import',
-  'install',
-  'middlewares/list',
-  'new',
-  'policies/list',
-  'report',
-  'routes/list',
-  'services/list',
-  'start',
-  'telemetry/disable',
-  'telemetry/enable',
-  'templates/generate',
-  'transfer',
-  'ts/generate-types',
-  'uninstall',
-  'version',
-  'watch-admin',
-];
+const strapiCommands = {
+  'admin/create-user': require('./actions/admin/create-user/command'),
+  'admin/reset-user-password': require('./actions/admin/reset-user-password/command'),
+  build: require('./actions/build/command'),
+  'configuration/dump': require('./actions/configuration/dump/command'),
+  'configuration/restore': require('./actions/configuration/restore/command'),
+  console: require('./actions/console/command'),
+  'content-types/list': require('./actions/content-types/list/command'),
+  'controllers/list': require('./actions/controllers/list/command'),
+  develop: require('./actions/develop/command'),
+  export: require('./actions/export/command'),
+  generate: require('./actions/generate/command'),
+  'hooks/list': require('./actions/hooks/list/command'),
+  import: require('./actions/import/command'),
+  install: require('./actions/install/command'),
+  'middlewares/list': require('./actions/middlewares/list/command'),
+  new: require('./actions/new/command'),
+  'policies/list': require('./actions/policies/list/command'),
+  report: require('./actions/report/command'),
+  'routes/list': require('./actions/routes/list/command'),
+  'services/list': require('./actions/services/list/command'),
+  start: require('./actions/start/command'),
+  'telemetry/disable': require('./actions/telemetry/disable/command'),
+  'telemetry/enable': require('./actions/telemetry/enable/command'),
+  'templates/generate': require('./actions/templates/generate/command'),
+  transfer: require('./actions/transfer/command'),
+  'ts/generate-types': require('./actions/ts/generate-types/command'),
+  uninstall: require('./actions/uninstall/command'),
+  version: require('./actions/version/command'),
+  'watch-admin': require('./actions/watch-admin/command'),
+};
 
 const buildStrapiCommand = (argv, command = new Command()) => {
   // Initial program setup
@@ -43,11 +43,12 @@ const buildStrapiCommand = (argv, command = new Command()) => {
   command.addHelpCommand('help [command]', 'Display help for command');
 
   // Load all commands
-  availableCommands.forEach((cmdPath) => {
+  Object.keys(strapiCommands).forEach((name) => {
     try {
-      require(`./actions/${cmdPath}/command`)({ command, argv });
+      // Add this command to the Commander command object
+      strapiCommands[name]({ command, argv });
     } catch (e) {
-      console.error(`Failed to load command ${cmdPath}`, e);
+      console.error(`Failed to load command ${name}`, e);
     }
   });
 
@@ -61,5 +62,5 @@ const runStrapiCommand = async (argv = process.argv, command = new Command()) =>
 module.exports = {
   runStrapiCommand,
   buildStrapiCommand,
-  availableCommands,
+  strapiCommands,
 };
