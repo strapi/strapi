a69c8e00869b70b13185bbf653437e6679cb4348
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-04-17 13:00:45 +0100

    chore: pr fixes
    chore: tidy ups

--- packages/core/admin/admin/src/content-manager/pages/ListView/index.js
+++ packages/core/admin/admin/src/content-manager/pages/ListView/index.js
@@ -532,7 +532,7 @@ function ListView({
       )}
       <ContentLayout>
         {canRead ? (
-          <>
+          <Flex gap={4} direction="column" alignItems="stretch">
             <DynamicTable
               canCreate={canCreate}
               canDelete={canDelete}
@@ -565,11 +565,11 @@ function ListView({
                 withMainAction={canDelete && isBulkable}
               />
             </DynamicTable>
-            <Flex paddingTop={4} alignItems="flex-end" justifyContent="space-between">
+            <Flex alignItems="flex-end" justifyContent="space-between">
               <PageSizeURLQuery trackedEvent="willChangeNumberOfEntriesPerPage" />
               <PaginationURLQuery pagination={{ pageCount: pagination?.pageCount || 1 }} />
             </Flex>
-          </>
+          </Flex>
         ) : (
           <NoPermissions />
         )}
