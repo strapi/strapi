4d284ae7897e0d2c9bcb6e3f495b4629377e8c94
nathan-pichon <nathan.pichon@strapi.io>
2023-04-17 18:06:07 +0200

    feat(ee): add persist table by suffix

--- packages/core/admin/ee/server/utils/persisted-tables.js
+++ packages/core/admin/ee/server/utils/persisted-tables.js
@@ -1,34 +1,26 @@
 'use strict';
 
 /**
- * Finds all tables in the database that start with a prefix
- * @param {string} prefix
- * @returns {Array}
+ * Finds all tables in the database matching the regular expression
+ * @param {Object} ctx
+ * @param {Strapi} ctx.strapi
+ * @param {RegExp} regex
+ * @returns {Promise<string[]>}
  */
-const findTablesThatStartWithPrefix = async (prefix) => {
+async function findTables({ strapi }, regex) {
   const tables = await strapi.db.dialect.schemaInspector.getTables();
-  return tables.filter((tableName) => tableName.startsWith(prefix));
-};
+  return tables.filter((tableName) => regex.test(tableName));
+}
 
 /**
- * Get all reserved table names from the core store
- * @returns {Array}
+ * Add tables name to the reserved tables in core store
+ * @param {Object} ctx
+ * @param {Strapi} ctx.strapi
+ * @param  {string[]} tableNames
+ * @return {Promise<void>}
  */
-const getPersistedTables = async () =>
-  (await strapi.store.get({
-    type: 'core',
-    key: 'persisted_tables',
-  })) ?? [];
-
-/**
- * Add all table names that start with a prefix to the reserved tables in
- * core store
- * @param {string} tableNamePrefix
- */
-
-const persistTablesWithPrefix = async (tableNamePrefix) => {
-  const persistedTables = await getPersistedTables();
-  const tableNames = await findTablesThatStartWithPrefix(tableNamePrefix);
+async function addPersistTables({ strapi }, tableNames) {
+  const persistedTables = await getPersistedTables({ strapi });
   const notReservedTableNames = tableNames.filter((name) => !persistedTables.includes(name));
 
   if (!notReservedTableNames.length) {
@@ -41,9 +33,48 @@ const persistTablesWithPrefix = async (tableNamePrefix) => {
     key: 'persisted_tables',
     value: persistedTables,
   });
+}
+
+/**
+ * Get all reserved table names from the core store
+ * @param {Object} ctx
+ * @param {Strapi} ctx.strapi
+ * @returns {Promise<string[]>}
+ */
+const getPersistedTables = async ({ strapi }) =>
+  (await strapi.store.get({
+    type: 'core',
+    key: 'persisted_tables',
+  })) ?? [];
+
+/**
+ * Add all table names that start with a prefix to the reserved tables in
+ * core store
+ * @param {string} tableNamePrefix
+ * @return {Promise<void>}
+ */
+
+const persistTablesWithPrefix = async (tableNamePrefix) => {
+  const tableNameRegex = new RegExp(`^${tableNamePrefix}.*`);
+  const tableNames = await findTables({ strapi }, tableNameRegex);
+
+  await addPersistTables({ strapi }, tableNames);
+};
+
+/**
+ * Add all table names that end with a suffix to the reserved tables in core store
+ * @param {string} tableNameSuffix
+ * @return {Promise<void>}
+ */
+const persistTablesWithSuffix = async (tableNameSuffix) => {
+  const tableNameRegex = new RegExp(`.*${tableNameSuffix}$`);
+  const tableNames = await findTables({ strapi }, tableNameRegex);
+
+  await addPersistTables({ strapi }, tableNames);
 };
 
 module.exports = {
   persistTablesWithPrefix,
-  findTablesThatStartWithPrefix,
+  persistTablesWithSuffix,
+  findTables,
 };
