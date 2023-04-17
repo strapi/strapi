02278eb661a3abd2eb15419fb96f5ad4b1d380ad
nathan-pichon <nathan.pichon@strapi.io>
2023-04-17 18:14:40 +0200

    feat(review-workflow): add link tables to persist tables

--- packages/core/admin/ee/server/bootstrap.js
+++ packages/core/admin/ee/server/bootstrap.js
@@ -5,7 +5,7 @@ const { features } = require('@strapi/strapi/lib/utils/ee');
 const executeCEBootstrap = require('../../server/bootstrap');
 const { getService } = require('../../server/utils');
 const actions = require('./config/admin-actions');
-const { persistTablesWithPrefix } = require('./utils/persisted-tables');
+const { persistTablesWithPrefix, persistTablesWithSuffix } = require('./utils/persisted-tables');
 
 module.exports = async () => {
   const { actionProvider } = getService('permission');
@@ -22,6 +22,7 @@ module.exports = async () => {
 
   if (features.isEnabled('review-workflows')) {
     await persistTablesWithPrefix('strapi_workflows');
+    await persistTablesWithSuffix('_strapi_review_workflows_stage_links');
 
     const { bootstrap: rwBootstrap } = getService('review-workflows');
 
