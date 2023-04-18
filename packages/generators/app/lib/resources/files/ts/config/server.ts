bbd43d1ea5c6637844adf77d449308e6337d89ca
Berk Sümbül <berksmbl@gmail.com>
2023-04-19 00:00:24 +0300

    Update server.ts

--- packages/generators/app/lib/resources/files/ts/config/server.ts
+++ packages/generators/app/lib/resources/files/ts/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('url', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
