419eb1e53a2626955c237dd0760856ea17a78363
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-04-20 08:51:20 +0100

    chore: fix other build tools for HP

--- packages/core/helper-plugin/jest.config.front.js
+++ packages/core/helper-plugin/jest.config.front.js
@@ -1,4 +1,4 @@
 module.exports = {
   preset: '../../../jest-preset.front.js',
-  collectCoverageFrom: ['<rootDir>/packages/core/helper-plugin/lib/src/**/*.js'],
+  collectCoverageFrom: ['<rootDir>/packages/core/helper-plugin/src/**/*.js'],
 };
