720b3c70c8fa71e2e86c0b2a5f77c40e87b80c81
Marc-Roig <marc12info@gmail.com>
2023-04-21 10:54:52 +0200

    fix: audit log tests

--- packages/core/admin/ee/server/services/__tests__/audit-logs.test.js
+++ packages/core/admin/ee/server/services/__tests__/audit-logs.test.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const { features } = require('@strapi/strapi/lib/utils/ee');
+// const { features } = require('@strapi/strapi/lib/utils/ee');
 const { register } = require('@strapi/provider-audit-logs-local');
 const { scheduleJob } = require('node-schedule');
+const features = require('../../utils/features');
 const createAuditLogsService = require('../audit-logs');
 const createEventHub = require('../../../../../strapi/lib/services/event-hub');
 
 jest.mock('../../../../server/register');
 
-jest.mock('@strapi/strapi/lib/utils/ee', () => ({
-  features: {
-    isEnabled: jest.fn(),
-    get: jest.fn(),
-  },
+jest.mock('../../utils/features', () => ({
+  isEnabled: jest.fn(),
+  get: jest.fn(),
 }));
 
 jest.mock('@strapi/provider-audit-logs-local', () => ({
