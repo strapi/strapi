4cf5ab1a0cca1b49ab63d193c37007a170c6b5f3
Marc-Roig <marc12info@gmail.com>
2023-04-24 13:28:18 +0200

    feat: add stage color validation

--- packages/core/admin/ee/server/validation/review-workflows.js
+++ packages/core/admin/ee/server/validation/review-workflows.js
@@ -5,6 +5,10 @@ const { yup, validateYupSchema } = require('@strapi/utils');
 const stageObject = yup.object().shape({
   id: yup.number().integer().min(1),
   name: yup.string().max(255).required(),
+  color: yup
+    .string()
+    .matches(/^#(?:[0-9a-fA-F]{3}){1,2}$/i)
+    .nullable(), // hex color
 });
 
 const validateUpdateStagesSchema = yup.array().of(stageObject).required();
