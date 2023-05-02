9340d7966f28273d0d03f319752d49d7c81af59d
Marc-Roig <marc12info@gmail.com>
2023-05-02 18:58:29 +0200

    feat: remove sort attribute oneOf validation

--- packages/core/content-manager/server/controllers/validation/model-configuration.js
+++ packages/core/content-manager/server/controllers/validation/model-configuration.js
@@ -30,8 +30,7 @@ const createSettingsSchema = (schema) => {
       searchable: yup.boolean().required(),
       // should be reset when the type changes
       mainField: yup.string().oneOf(validAttributes.concat('id')).default('id'),
-      // should be reset when the type changes
-      defaultSortBy: yup.string().oneOf(validAttributes.concat('id')).default('id'),
+      defaultSortBy: yup.string().default('id'),
       defaultSortOrder: yup.string().oneOf(['ASC', 'DESC']).default('ASC'),
     })
     .noUnknown();
