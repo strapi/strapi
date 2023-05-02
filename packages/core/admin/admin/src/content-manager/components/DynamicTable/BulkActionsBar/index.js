bd4011e1a8ea17588ee5ccb5d56f49e01322a923
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-05-02 17:28:32 +0200

    Move confirm all dialogs to bulk actions bar file

--- packages/core/admin/admin/src/content-manager/components/DynamicTable/BulkActionsBar/index.js
+++ packages/core/admin/admin/src/content-manager/components/DynamicTable/BulkActionsBar/index.js
@@ -1,11 +1,141 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@strapi/design-system';
+import { Button, Flex, Typography } from '@strapi/design-system';
+import { Check, ExclamationMarkCircle, Trash } from '@strapi/icons';
 import { useIntl } from 'react-intl';
 import { useTracking } from '@strapi/helper-plugin';
-import ConfirmDialogDeleteAll from '../ConfirmDialogDeleteAll';
-import ConfirmDialogPublishAll from '../ConfirmDialogPublishAll';
-import ConfirmDialogUnpublishAll from '../ConfirmDialogUnpublishAll';
+import { getTrad } from '../../../utils';
+import InjectionZoneList from '../../InjectionZoneList';
+import ConfirmBulkActionDialog from '../ConfirmBulkActionDialog';
+
+const confirmDialogsPropTypes = {
+  isConfirmButtonLoading: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  onToggleDialog: PropTypes.func.isRequired,
+};
+
+const ConfirmDialogPublishAll = ({ isOpen, onToggleDialog, isConfirmButtonLoading, onConfirm }) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <ConfirmBulkActionDialog
+      isOpen={isOpen}
+      onToggleDialog={onToggleDialog}
+      icon={<ExclamationMarkCircle />}
+      dialogBody={
+        <Flex direction="column" alignItems="stretch" gap={2}>
+          <Typography id="confirm-description" textAlign="center">
+            {formatMessage({
+              id: getTrad('popUpWarning.bodyMessage.contentType.publish.all'),
+              defaultMessage: 'Are you sure you want to publish these entries?',
+            })}
+          </Typography>
+          <InjectionZoneList area="contentManager.listView.publishModalAdditionalInfos" />
+        </Flex>
+      }
+      endAction={
+        <Button
+          onClick={onConfirm}
+          variant="secondary"
+          startIcon={<Check />}
+          loading={isConfirmButtonLoading}
+        >
+          {formatMessage({
+            id: 'app.utils.publish',
+            defaultMessage: 'Publish',
+          })}
+        </Button>
+      }
+    />
+  );
+};
+
+ConfirmDialogPublishAll.propTypes = confirmDialogsPropTypes;
+
+const ConfirmDialogUnpublishAll = ({
+  isOpen,
+  onToggleDialog,
+  isConfirmButtonLoading,
+  onConfirm,
+}) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <ConfirmBulkActionDialog
+      isOpen={isOpen}
+      onToggleDialog={onToggleDialog}
+      icon={<ExclamationMarkCircle />}
+      dialogBody={
+        <Flex direction="column" alignItems="stretch" gap={2}>
+          <Typography id="confirm-description" textAlign="center">
+            {formatMessage({
+              id: getTrad('popUpWarning.bodyMessage.contentType.unpublish.all'),
+              defaultMessage: 'Are you sure you want to unpublish these entries?',
+            })}
+          </Typography>
+          <Flex>
+            <InjectionZoneList area="contentManager.listView.unpublishModalAdditionalInfos" />
+          </Flex>
+        </Flex>
+      }
+      endAction={
+        <Button
+          onClick={onConfirm}
+          variant="secondary"
+          startIcon={<Check />}
+          loading={isConfirmButtonLoading}
+        >
+          {formatMessage({
+            id: 'app.utils.unpublish',
+            defaultMessage: 'Unpublish',
+          })}
+        </Button>
+      }
+    />
+  );
+};
+
+ConfirmDialogUnpublishAll.propTypes = confirmDialogsPropTypes;
+
+const ConfirmDialogDeleteAll = ({ isOpen, onToggleDialog, isConfirmButtonLoading, onConfirm }) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <ConfirmBulkActionDialog
+      isOpen={isOpen}
+      onToggleDialog={onToggleDialog}
+      icon={<ExclamationMarkCircle />}
+      dialogBody={
+        <Flex direction="column" alignItems="stretch" gap={2}>
+          <Typography id="confirm-description" textAlign="center">
+            {formatMessage({
+              id: getTrad('popUpWarning.bodyMessage.contentType.delete.all'),
+              defaultMessage: 'Are you sure you want to delete these entries?',
+            })}
+          </Typography>
+          <InjectionZoneList area="contentManager.listView.deleteModalAdditionalInfos" />
+        </Flex>
+      }
+      endAction={
+        <Button
+          onClick={onConfirm}
+          variant="danger-light"
+          startIcon={<Trash />}
+          id="confirm-delete"
+          loading={isConfirmButtonLoading}
+        >
+          {formatMessage({
+            id: 'app.components.Button.confirm',
+            defaultMessage: 'Confirm',
+          })}
+        </Button>
+      }
+    />
+  );
+};
+
+ConfirmDialogDeleteAll.propTypes = confirmDialogsPropTypes;
 
 const BulkActionsBar = ({
   showPublish,
