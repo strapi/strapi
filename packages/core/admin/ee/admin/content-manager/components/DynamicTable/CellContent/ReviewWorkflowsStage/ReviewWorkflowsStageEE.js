392fa438b73081e4bb7816b3aca5a21d3da3d67e
Gustav Hansen <gustav.hansen@strapi.io>
2023-05-02 12:54:09 +0200

    CM: Add max length for stage names in the list-view

--- packages/core/admin/ee/admin/content-manager/components/DynamicTable/CellContent/ReviewWorkflowsStage/ReviewWorkflowsStageEE.js
+++ packages/core/admin/ee/admin/content-manager/components/DynamicTable/CellContent/ReviewWorkflowsStage/ReviewWorkflowsStageEE.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Flex, Typography } from '@strapi/design-system';
+import { pxToRem } from '@strapi/helper-plugin';
 
 export function ReviewWorkflowsStageEE({ color, name }) {
   return (
-    <Flex alignItems="center" gap={2}>
-      <Box height={2} background={color} hasRadius width={2} />
+    <Flex alignItems="center" gap={2} maxWidth={pxToRem(300)}>
+      <Box height={2} background={color} hasRadius shrink={0} width={2} />
 
-      <Typography fontWeight="regular" textColor="neutral700">
+      <Typography fontWeight="regular" textColor="neutral700" ellipsis>
         {name}
       </Typography>
     </Flex>
