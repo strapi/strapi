f0947c1547564214ac0fd22dc6ae8055a43c036e
nathan-pichon <nathan.pichon@strapi.io>
2023-05-04 17:15:23 +0200

    fix(review-workflows): sanitize content-api workflow controller

--- packages/core/admin/ee/server/controllers/content-api/workflows/index.js
+++ packages/core/admin/ee/server/controllers/content-api/workflows/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
+const { sanitize } = require('@strapi/utils');
 const { getService } = require('../../../utils');
+const { WORKFLOW_MODEL_UID } = require('../../../constants/workflows');
+
+function sanitizeOuput({ strapi }, data, ctx) {
+  const schema = strapi.getModel(WORKFLOW_MODEL_UID);
+  const { auth } = ctx.state;
+
+  return sanitize.contentAPI.output(data, schema, { auth });
+}
+function sanitizeQuery({ strapi }, data, ctx) {
+  const schema = strapi.getModel(WORKFLOW_MODEL_UID);
+  const { auth } = ctx.state;
+
+  return sanitize.contentAPI.query(data, schema, { auth });
+}
 
 module.exports = {
   /**
@@ -8,14 +23,14 @@ module.exports = {
    * @param {import('koa').BaseContext} ctx - koa context
    */
   async find(ctx) {
-    const { populate } = ctx.query;
+    const params = await sanitizeQuery({ strapi }, ctx.query, ctx);
     const workflowService = getService('workflows');
     const data = await workflowService.find({
-      populate,
+      populate: params.populate,
     });
 
     ctx.body = {
-      data,
+      data: await sanitizeOuput({ strapi }, data, ctx),
     };
   },
   /**
@@ -24,13 +39,13 @@ module.exports = {
    */
   async findById(ctx) {
     const { id } = ctx.params;
-    const { populate } = ctx.query;
+    const params = await sanitizeQuery({ strapi }, ctx.query, ctx);
 
     const workflowService = getService('workflows');
-    const data = await workflowService.findById(id, { populate });
+    const data = await workflowService.findById(id, { populate: params.populate });
 
     ctx.body = {
-      data,
+      data: await sanitizeOuput({ strapi }, data, ctx),
     };
   },
 };
