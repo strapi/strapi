43c6b9fd71665236b3a3cd7012965d812f139113
Simone Taeggi <startae14@gmail.com>
2023-05-08 11:18:54 +0200

    remove console log

--- packages/core/upload/admin/src/utils/downloadFile.js
+++ packages/core/upload/admin/src/utils/downloadFile.js
@@ -7,11 +7,9 @@ export const downloadFile = async (url, fileName) => {
     {responseType: 'blob'}
   );
   const urlDownload = window.URL.createObjectURL(new Blob([response.data]));
-  console.log('downloadFile url New', urlDownload);
   const link = document.createElement('a');
 
   link.href = urlDownload;
   link.setAttribute('download', fileName);
-  console.log('downloadFile link New', link);
   link.click();
 };
