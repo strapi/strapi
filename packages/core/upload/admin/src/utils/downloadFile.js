ab6033b544bed1aee933401224f4cbb6c672e492
Simone Taeggi <startae14@gmail.com>
2023-05-08 11:14:32 +0200

    replace the downloadFile axios call with getFetchClient

--- packages/core/upload/admin/src/utils/downloadFile.js
+++ packages/core/upload/admin/src/utils/downloadFile.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
+import { getFetchClient } from '@strapi/helper-plugin';
 
-export const downloadFile = (url, fileName) => {
-  axios({
+export const downloadFile = async (url, fileName) => {
+  const { get } = getFetchClient();
+  const response = await get(
     url,
-    method: 'GET',
-    responseType: 'blob',
-  }).then((response) => {
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
+    {responseType: 'blob'}
+  );
+  const urlDownload = window.URL.createObjectURL(new Blob([response.data]));
+  console.log('downloadFile url New', urlDownload);
+  const link = document.createElement('a');
 
-    link.href = url;
-    link.setAttribute('download', fileName);
-    link.click();
-  });
+  link.href = urlDownload;
+  link.setAttribute('download', fileName);
+  console.log('downloadFile link New', link);
+  link.click();
 };
