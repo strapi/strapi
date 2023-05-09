47f0a4986f61b677392e6de71f4ae9fe87c3cbc7
Simone Taeggi <startae14@gmail.com>
2023-05-09 16:00:12 +0200

    add mock getFetchClient in FromComputerForm test

--- packages/core/upload/admin/src/components/UploadAssetDialog/AddAssetStep/tests/FromComputerForm.test.js
+++ packages/core/upload/admin/src/components/UploadAssetDialog/AddAssetStep/tests/FromComputerForm.test.js
@@ -5,6 +5,13 @@ import { TrackingProvider } from '@strapi/helper-plugin';
 import { IntlProvider } from 'react-intl';
 import { FromComputerForm } from '../FromComputerForm';
 
+jest.mock('@strapi/helper-plugin', () => ({
+  ...jest.requireActual('@strapi/helper-plugin'),
+  getFetchClient: jest.fn().mockReturnValue({
+    get: jest.fn(),
+  }),
+}));
+
 describe('FromComputerForm', () => {
   it('snapshots the component', async () => {
     const { container } = renderTL(
