bb231a3ee2e6d14abadd3c28cb72145f15d6f5cf
Simone Taeggi <startae14@gmail.com>
2023-05-09 14:40:47 +0200

    move the fetchMarketplaceProviders util inside the useFetchMarketplaceProviders file

--- packages/core/admin/admin/src/hooks/useFetchMarketplaceProviders/index.js
+++ packages/core/admin/admin/src/hooks/useFetchMarketplaceProviders/index.js
@@ -1,6 +1,21 @@
 import { useQuery } from 'react-query';
-import { useNotification } from '@strapi/helper-plugin';
-import { fetchMarketplaceProviders } from './utils/api';
+import { getFetchClient, useNotification } from '@strapi/helper-plugin';
+import qs from 'qs';
+
+const MARKETPLACE_API_URL = 'https://market-api.strapi.io';
+const { get } = getFetchClient();
+
+const fetchMarketplaceProviders = async (params = {}) => {
+  const { data } = await get(`${MARKETPLACE_API_URL}/providers`, {
+    params,
+    paramsSerializer: {
+      encode: qs.parse,
+      serialize: qs.stringify,
+    },
+  });
+
+  return data;
+};
 
 const useFetchMarketplaceProviders = (notifyLoad, params) => {
   const toggleNotification = useNotification();
