7fcff782c97d37ecba237073cc55cf2bed0331d7
Marc-Roig <marc12info@gmail.com>
2023-05-11 13:54:08 +0200

    fix: workflow migration should only happen when name attribute is added

--- packages/core/admin/ee/server/migrations/review-workflows-workflow-name.js
+++ packages/core/admin/ee/server/migrations/review-workflows-workflow-name.js
@@ -8,7 +8,7 @@ async function migrateReviewWorkflowName({ oldContentTypes, contentTypes }) {
   const hasName = !!contentTypes?.[WORKFLOW_MODEL_UID]?.attributes?.name;
 
   // Add the default workflow name if name attribute was added
-  if (!hadName || hasName) {
+  if (!hadName && hasName) {
     await strapi.query(WORKFLOW_MODEL_UID).updateMany({
       data: {
         name: 'Default',
