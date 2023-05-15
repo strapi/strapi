d7fa412811ed96e00740e1b71bd564f6e495f366
Convly <jean-sebastien.herbaux@epitech.eu>
2023-05-15 17:22:21 +0200

    Scope types with modules

---
+++ packages/core/strapi/lib/types/core/namespace.d.ts
@@ -0,0 +1,94 @@
+import type * as Utils from '../utils';
+
+/**
+ * Namespace for admin resources
+ */
+export type Admin = 'admin';
+
+/**
+ * Namespace for strapi internal resources
+ */
+export type Strapi = 'strapi';
+
+/**
+ * Namespace for scoped APIs resources
+ */
+export type API<T extends string = string> = `api::${T}`;
+
+/**
+ * Namespace for scoped plugins resources
+ */
+export type Plugin<T extends string = string> = `plugin::${T}`;
+
+/**
+ * Namespace for global resources
+ */
+export type Global = 'global';
+
+/**
+ * Represents any namespace
+ */
+export type Any = API | Plugin | Admin | Strapi | Global;
+
+/**
+ * Return a {@link Separator} based on the given {@link Any} ({@link DotSeparator} for {@link Scoped} and {@link ColonsSeparator} for regular ones)
+ *
+ * @example
+ * type S = GetSeparator<Admin>
+ * // ^ '::'
+ *
+ * type S = GetSeparator<API>
+ * // ^ '.'
+ *
+ * type S = GetSeparator<Admin | API>
+ * // ^ '.' | '::'
+ */
+export type GetSeparator<T extends Any = Any> = T extends Scoped
+  ? // 'api::foo' | 'plugin::bar' => '.'
+    DotSeparator
+  : // 'admin' | 'strapi' | 'global' => '::'
+    ColonsSeparator;
+
+/**
+ * Adds the corresponding separator (using {@link GetSeparator}) at the end of a namespace
+ *
+ * Warning: Using WithSeparator with a union type might produce undesired results as it'll distribute every matching suffix to every union members
+ *
+ * @example
+ * type T = WithSeparator<Admin>
+ * // ^ 'admin::'
+ *
+ * type T = WithSeparator<API>
+ * // ^ 'api::{string}.'
+ *
+ * type T = WithSeparator<Admin | API>
+ * // ^ 'admin::' | 'admin.' | 'api::{string}.' | 'api::{string}::'
+ *
+ * type T = WithSeparator<Admin> | WithSeparator<API>
+ * // ^ 'admin::' | 'api::{string}.'
+ */
+export type WithSeparator<N extends Any> = Utils.Suffix<N, GetSeparator<N>>;
+
+/**
+ * Represents namespaces composed of an origin and a name, separated by colons
+ */
+export type Scoped<O extends string = string, S extends string = string> = Any &
+  `${O}${ColonsSeparator}${S}`;
+
+/**
+ * Extract the scope from the given scoped namespace
+ */
+export type ExtractScope<T> = T extends `${string}${ColonsSeparator}${infer S}` ? S : never;
+
+/**
+ * Extract the origin from the given scoped namespace
+ */
+export type ExtractOrigin<T> = T extends `${infer S}${ColonsSeparator}${string}` ? S : never;
+
+/**
+ * Separators used to join the different parts of a namespace (e.g. building a uid)
+ */
+export type Separator = ColonsSeparator | DotSeparator;
+
+type ColonsSeparator = '::';
+type DotSeparator = '.';
---
+++ packages/core/strapi/lib/types/core/uid.d.ts
@@ -0,0 +1,154 @@
+import type * as Namespace from './namespace';
+import type * as Utils from '../utils';
+
+type StringSuffix<T extends string> = Utils.Suffix<T, string>;
+
+/**
+ * Template for services' unique identifier
+ */
+export type Service = StringSuffix<
+  | Namespace.WithSeparator<Namespace.Admin>
+  | Namespace.WithSeparator<Namespace.API>
+  | Namespace.WithSeparator<Namespace.Plugin>
+>;
+
+/**
+ * Template for controllers' unique identifier
+ */
+export type Controller = StringSuffix<
+  | Namespace.WithSeparator<Namespace.Admin>
+  | Namespace.WithSeparator<Namespace.API>
+  | Namespace.WithSeparator<Namespace.Plugin>
+>;
+
+/**
+ * Template for policies' unique identifier
+ */
+export type Policy = StringSuffix<
+  | Namespace.WithSeparator<Namespace.Admin>
+  | Namespace.WithSeparator<Namespace.Strapi>
+  | Namespace.WithSeparator<Namespace.Global>
+  | Namespace.WithSeparator<Namespace.API>
+  | Namespace.WithSeparator<Namespace.Plugin>
+>;
+
+/**
+ * Template for middlewares' unique identifier
+ */
+export type Middleware = StringSuffix<
+  | Namespace.WithSeparator<Namespace.Admin>
+  | Namespace.WithSeparator<Namespace.Strapi>
+  | Namespace.WithSeparator<Namespace.Global>
+  | Namespace.WithSeparator<Namespace.API>
+  | Namespace.WithSeparator<Namespace.Plugin>
+>;
+
+/**
+ * Template for content-types' unique identifier
+ */
+export type ContentType = StringSuffix<
+  | Namespace.WithSeparator<Namespace.Admin>
+  | Namespace.WithSeparator<Namespace.Strapi>
+  | Namespace.WithSeparator<Namespace.API>
+  | Namespace.WithSeparator<Namespace.Plugin>
+>;
+
+/**
+ * Template for components' unique identifier
+ *
+ * Warning: Can cause overlap with other UID formats.
+ */
+export type Component = `${string}.${string}`;
+
+/**
+ * Represents any UID
+ */
+export type Any = Service | Controller | Policy | Middleware | ContentType | Component;
+
+/**
+ * Type representation of every UID component.
+ *
+ * The separator type is automatically inferred from the given namespace
+ */
+export interface Parsed<N extends Namespace.Any = Namespace.Any, E extends string = string> {
+  raw: `${N}${Namespace.GetSeparator<N>}${E}`;
+  namespace: N;
+  origin: N extends Namespace.Scoped ? Namespace.ExtractOrigin<N> : N;
+  scope: N extends Namespace.Scoped ? Namespace.ExtractScope<N> : never;
+  separator: Namespace.GetSeparator<N>;
+  name: E;
+}
+
+/**
+ * Parse an UID literal and returns a {@link Parsed} type.
+ *
+ * Warning: Using ParseUID with a union type might produce undesired results as it'll distribute every matching namespace parsing to every union members
+ *
+ * @example
+ * type T = Parse<'admin::foo'>
+ * // ^ { namespace: 'admin'; separator: '::'; name: 'foo'; }
+ *
+ * type T = Parse<'api::foo.bar'>
+ * // ^ { namespace: 'api::foo'; separator: '.'; name: 'bar'; }
+ *
+ * type T = Parse<'admin::foo' | 'api::foo.bar'>
+ * // ^ { namespace: 'admin' | 'api::foo' ; separator: '.' | '::'; name: 'foo' | 'bar' | 'foo.bar'; }
+ */
+export type Parse<U extends Any> = ExtractNamespace<U> extends infer B extends Namespace.Any
+  ? Namespace.GetSeparator<B> extends infer S extends Namespace.Separator
+    ? U extends `${infer N extends B}${S}${infer E extends string}`
+      ? Parsed<N, E>
+      : never
+    : never
+  : never;
+
+/**
+ * Determines if the UID's namespace matches the given one.
+ *
+ * It returns N (the {@link Namespace.Any} literal) if there is a match, never otherwise.
+ *
+ * @example
+ * type T = AssertNamespaceIs<'admin::foo', Namespace.Admin>
+ * // ^ Namespace.Admin
+ * @example
+ * type T = AssertNamespaceIs<'foo.bar', Namespace.API>
+ * // ^ never
+ * @example
+ * type T = AssertNamespaceIs<'api::foo.bar', Namespace.Plugin>
+ * // ^ never
+ */
+export type AssertNamespaceIs<U extends Any, N extends Namespace.Any> = U extends StringSuffix<
+  Namespace.WithSeparator<N>
+>
+  ? N
+  : never;
+
+/**
+ * Get parsed properties from a given raw UID
+ */
+export type Get<U extends Any, P extends keyof Parsed> = Parse<U>[P];
+
+/**
+ * Pick parsed properties from a given raw UID
+ */
+export type Select<U extends Any, P extends keyof Parse<U>> = Pick<Parse<U>, P>;
+
+/**
+ * Extract the namespace literal from a given UID.
+ *
+ * @example
+ * type T = ResolveUIDNamespace<'admin::foo'>
+ * // ^ AdminNamespace
+ * @example
+ * type T = ResolveUIDNamespace<'api::foo.bar'>
+ * // ^ ApiNamespace
+ * @example
+ * type T = ResolveUIDNamespace<'admin::foo' | 'api::foo.bar'>
+ * // ^ AdminNamespace | ApiNamespace
+ */
+export type ExtractNamespace<U extends Any> =
+  | AssertNamespaceIs<U, Namespace.Global>
+  | AssertNamespaceIs<U, Namespace.Admin>
+  | AssertNamespaceIs<U, Namespace.Strapi>
+  | AssertNamespaceIs<U, Namespace.API>
+  | AssertNamespaceIs<U, Namespace.Plugin>;
