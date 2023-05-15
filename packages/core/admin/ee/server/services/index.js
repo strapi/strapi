ea4321c47af60d6e5930a28d3cddbbce481b796b
Marc-Roig <marc12info@gmail.com>
2023-05-15 10:25:31 +0200

    feat: create assigned content type service

--- packages/core/admin/ee/server/services/index.js
+++ packages/core/admin/ee/server/services/index.js
@@ -7,6 +7,7 @@ module.exports = {
   'seat-enforcement': require('./seat-enforcement'),
   workflows: require('./review-workflows/workflows'),
   stages: require('./review-workflows/stages'),
+  'assigned-content-types': require('./review-workflows/assigned-content-types'),
   'review-workflows': require('./review-workflows/review-workflows'),
   'review-workflows-decorator': require('./review-workflows/entity-service-decorator'),
   'review-workflows-metrics': require('./review-workflows/metrics'),
