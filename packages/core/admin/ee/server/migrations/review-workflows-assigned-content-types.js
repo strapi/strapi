2232999a6181ebea9ff96937e3b3548c96a86e7f
Marc-Roig <marc12info@gmail.com>
2023-05-15 12:06:27 +0200

    feat: assign default workflow when migrating

--- packages/core/admin/ee/server/migrations/review-workflows-assigned-content-types.js
+++ packages/core/admin/ee/server/migrations/review-workflows-assigned-content-types.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { get, keys, pickBy, pipe } = require('lodash/fp');
 const { WORKFLOW_MODEL_UID } = require('../constants/workflows');
 
 async function migrateReviewWorkflowsAssignedContentTypes({ oldContentTypes, contentTypes }) {
@@ -10,10 +11,16 @@ async function migrateReviewWorkflowsAssignedContentTypes({ oldContentTypes, con
     !!contentTypes?.[WORKFLOW_MODEL_UID]?.attributes?.assignedContentTypes;
 
   if (!hadAssignedContentTypes && hasAssignedContentTypes) {
-    // TODO: If it was enabled, assign the default workflow.
+    // If RW was enabled on CT, assign the default workflow.
+    const reviewWorkflowContentTypes = pipe([pickBy(get('options.reviewWorkflows')), keys])(
+      oldContentTypes
+    );
+
+    // Before this release there was only one workflow,
+    // so it is safe to assign the default workflow to all content types.
     await strapi.query(WORKFLOW_MODEL_UID).updateMany({
       data: {
-        assignedContentTypes: [],
+        assignedContentTypes: reviewWorkflowContentTypes.length ? reviewWorkflowContentTypes : [],
       },
     });
   }
