cbab9313b1ce48bd47a448167cc4c7d98291272c
Convly <jean-sebastien.herbaux@epitech.eu>
2023-05-15 15:10:34 +0200

    Remove invalid import

--- packages/core/strapi/lib/types/index.d.ts
+++ packages/core/strapi/lib/types/index.d.ts
@@ -3,5 +3,4 @@ export * from './core';
 export * as utils from './utils';
 export * as factories from './factories';
 
-export * as registries from './registry';
 export * as shared from './shared';
