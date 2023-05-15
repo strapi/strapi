1f90e94d783ac0fb311083c76f49460f1c822039
Marc-Roig <marc12info@gmail.com>
2023-05-15 10:27:30 +0200

    feat: workflow create and update assigned Content Types

--- packages/core/admin/ee/server/services/review-workflows/workflows.js
+++ packages/core/admin/ee/server/services/review-workflows/workflows.js
@@ -15,34 +15,85 @@ module.exports = ({ strapi }) => ({
   },
 
   async create(opts) {
-    if (!opts.data.stages) {
+    if (!opts.data.stages || opts.data.stages.length === 0) {
       throw new ApplicationError('Can not create a workflow without stages');
     }
 
-    const stageIds = await getService('stages', { strapi })
-      .replaceStages([], opts.data.stages)
-      .then((stages) => stages.map((stage) => stage.id));
+    return strapi.db.transaction(async () => {
+      // Create stages
+      const stageIds = await getService('stages', { strapi })
+        .replaceStages([], opts.data.stages)
+        .then((stages) => stages.map((stage) => stage.id));
 
-    const createOpts = set('data.stages', stageIds, opts);
+      const createOpts = set('data.stages', stageIds, opts);
 
-    return strapi.entityService.create(WORKFLOW_MODEL_UID, createOpts);
+      // Update (un)assigned Content Types
+      if (opts.data.assignedContentTypes) {
+        await getService('assigned-content-types').create({
+          assigned: opts.data.assignedContentTypes,
+          defaultStage: { id: stageIds[0] },
+        });
+      }
+
+      // Create Workflow
+      return strapi.entityService.create(WORKFLOW_MODEL_UID, createOpts);
+    });
+  },
+
+  // TODO: Can not assign a CT twice
+  async update(workflow, opts) {
+    const stageService = getService('stages', { strapi });
+    let updateOpts = { ...opts, populate: { stages: true } };
+    let updatedStageIds = [];
+
+    return strapi.db.transaction(async () => {
+      // Update stages
+      if (opts.data.stages) {
+        opts.data.stages.forEach((stage) => this.assertStageBelongsToWorkflow(stage.id, workflow));
+
+        updatedStageIds = await stageService
+          .replaceStages(workflow.stages, opts.data.stages, workflow.assignedContentTypes)
+          .then((stages) => stages.map((stage) => stage.id));
+
+        updateOpts = set('data.stages', updatedStageIds, opts);
+      }
+
+      // Update (un)assigned Content Types
+      if (opts.data.assignedContentTypes) {
+        await getService('assigned-content-types').update({
+          srcAssigned: workflow.assignedContentTypes,
+          destAssigned: opts.data.assignedContentTypes,
+          defaultStage: updatedStageIds.length ? { id: updatedStageIds[0] } : workflow.stages[0],
+        });
+      }
+
+      // Update Workflow
+      return strapi.entityService.update(WORKFLOW_MODEL_UID, workflow.id, updateOpts);
+    });
   },
 
   count() {
     return strapi.entityService.count(WORKFLOW_MODEL_UID);
   },
 
-  async update(workflow, opts) {
-    let updateOpts = opts;
+  async assertContentTypeBelongsToWorkflow(uid) {
+    const workflow = await getService('assigned-content-types').getAssignedWorkflow(uid, {
+      populate: 'stages',
+    });
+    if (!workflow) {
+      throw new ApplicationError(`Review workflows is not activated on Content Type ${uid}.`);
+    }
+    return workflow;
+  },
 
-    if (opts.data.stages) {
-      const stageIds = await getService('stages', { strapi })
-        .replaceStages(workflow.stages, opts.data.stages)
-        .then((stages) => stages.map((stage) => stage.id));
-
-      updateOpts = set('data.stages', stageIds, opts);
+  assertStageBelongsToWorkflow(stageId, workflow) {
+    if (!stageId) {
+      return;
     }
 
-    return strapi.entityService.update(WORKFLOW_MODEL_UID, workflow.id, updateOpts);
+    const belongs = workflow.stages.some((stage) => stage.id === stageId);
+    if (!belongs) {
+      throw new ApplicationError(`Stage ${stageId} does not belong to workflow ${workflow.id}`);
+    }
   },
 });
