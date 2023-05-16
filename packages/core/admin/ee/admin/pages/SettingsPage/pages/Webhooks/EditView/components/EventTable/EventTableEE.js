e9a4167ffad878da60b107a8c720297a11015f35
Jamie Howard <jamie.howard@strapi.io>
2023-05-16 15:26:31 +0100

    feat(webhooks/EditView): Add CE and EE Edit View

---
+++ packages/core/admin/ee/admin/pages/SettingsPage/pages/Webhooks/EditView/components/EventTable/EventTableEE.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import EventTable from '../../../../../../../../../admin/src/pages/SettingsPage/pages/Webhooks/EditView/components/Events';
+
+export function EventTableEE() {
+  return (
+    <EventTable.Root>
+      <EventTable.Headers />
+      <EventTable.Body />
+    </EventTable.Root>
+  );
+}
