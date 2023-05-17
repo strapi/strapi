2a149028967f0768ce06d253d400c628e9a728cc
Convly <jean-sebastien.herbaux@epitech.eu>
2023-05-17 12:32:14 +0200

    Export dynamic UIDs union types based on the registry values

---
+++ packages/core/strapi/lib/types/core/common/uid.d.ts
@@ -0,0 +1,34 @@
+import type {
+  Shared,
+  Schema as SchemaNamespace,
+  Common,
+  Registry,
+  UID,
+  Utils,
+} from '@strapi/strapi';
+
+export type Service = Registry.Keys<Shared.Services, UID.Service>;
+
+export type Controller = Registry.Keys<Shared.Controllers, UID.Controller>;
+
+export type Policy = Registry.Keys<Shared.Policies, UID.Policy>;
+
+export type Middleware = Registry.Keys<Shared.Middlewares, UID.Middleware>;
+
+export type ContentType = Registry.Keys<Shared.ContentTypes, UID.ContentType>;
+export type CollectionType = Extract<
+  Utils.KeysBy<Shared.ContentTypes, SchemaNamespace.CollectionType>,
+  ContentType
+>;
+export type SingleType = Extract<
+  Utils.KeysBy<Shared.ContentTypes, SchemaNamespace.SingleType>,
+  ContentType
+>;
+
+export type Component = Registry.Keys<Shared.Components, UID.Component>;
+export type ComponentCategory = Component extends UID.Component<infer A> ? A : never;
+
+export type Schema = Registry.Keys<
+  Common.Schemas,
+  UID.ContentType | UID.Component<ComponentCategory>
+>;
