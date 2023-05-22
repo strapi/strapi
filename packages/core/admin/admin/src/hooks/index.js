95970ec91bc894a9f540fc377c0c6caec53e24dd
Gustav Hansen <gustav.hansen@strapi.io>
2023-05-22 12:23:52 +0200

    Chore: Use named export

--- packages/core/admin/admin/src/hooks/index.js
+++ packages/core/admin/admin/src/hooks/index.js
@@ -1,5 +1,5 @@
 export { default as useConfigurations } from './useConfigurations';
-export { default as useModels } from './useModels';
+export { useModels } from './useModels';
 export { default as useFetchPermissionsLayout } from './useFetchPermissionsLayout';
 export { default as useFetchRole } from './useFetchRole';
 export { default as useMenu } from './useMenu';
