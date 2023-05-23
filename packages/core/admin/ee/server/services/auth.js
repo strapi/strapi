72b5d2f4f0feb34cfe6d709a02ef69560c77aef5
Ben Irvin <ben@innerdvations.com>
2023-05-23 11:20:18 +0200

    move all code to ee

--- packages/core/admin/ee/server/services/auth.js
+++ packages/core/admin/ee/server/services/auth.js
@@ -1,54 +1,10 @@
 'use strict';
 
-const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const { getAbsoluteAdminUrl } = require('@strapi/utils');
 const { ApplicationError } = require('@strapi/utils').errors;
 const { getService } = require('../utils');
-const { isSsoLocked } = require('../../ee/server/utils/sso-lock');
-
-/**
- * hashes a password
- * @param {string} password - password to hash
- * @returns {string} hashed password
- */
-const hashPassword = (password) => bcrypt.hash(password, 10);
-
-/**
- * Validate a password
- * @param {string} password
- * @param {string} hash
- * @returns {Promise<boolean>} is the password valid
- */
-const validatePassword = (password, hash) => bcrypt.compare(password, hash);
-
-/**
- * Check login credentials
- * @param {Object} options
- * @param {string} options.email
- * @param {string} options.password
- */
-const checkCredentials = async ({ email, password }) => {
-  const user = await strapi.query('admin::user').findOne({
-    where: { email },
-  });
-
-  if (!user || !user.password) {
-    return [null, false, { message: 'Invalid credentials' }];
-  }
-
-  const isValid = await validatePassword(password, user.password);
-
-  if (!isValid) {
-    return [null, false, { message: 'Invalid credentials' }];
-  }
-
-  if (!(user.isActive === true)) {
-    return [null, false, { message: 'User not active' }];
-  }
-
-  return [null, user];
-};
+const { isSsoLocked } = require('../utils/sso-lock');
 
 /**
  * Send an email to the user if it exists and is not locked to SSO
@@ -60,7 +16,7 @@ const checkCredentials = async ({ email, password }) => {
 const forgotPassword = async ({ email } = {}) => {
   const user = await strapi.query('admin::user').findOne({ where: { email, isActive: true } });
 
-  if (!user) {
+  if (!user || (await isSsoLocked(user))) {
     return;
   }
 
@@ -103,7 +59,7 @@ const resetPassword = async ({ resetPasswordToken, password } = {}) => {
     .query('admin::user')
     .findOne({ where: { resetPasswordToken, isActive: true } });
 
-  if (!matchingUser) {
+  if (!matchingUser || isSsoLocked(matchingUser)) {
     throw new ApplicationError();
   }
 
@@ -114,9 +70,6 @@ const resetPassword = async ({ resetPasswordToken, password } = {}) => {
 };
 
 module.exports = {
-  checkCredentials,
-  validatePassword,
-  hashPassword,
   forgotPassword,
   resetPassword,
 };
