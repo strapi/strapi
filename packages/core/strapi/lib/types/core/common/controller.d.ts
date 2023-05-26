b30a3b426f9da4a11c8dcbb8fb474525e506d5b9
Convly <jean-sebastien.herbaux@epitech.eu>
2023-05-26 15:26:36 +0200

    Update ControllerHandler return type to accept void

--- packages/core/strapi/lib/types/core/common/controller.d.ts
+++ packages/core/strapi/lib/types/core/common/controller.d.ts
@@ -3,6 +3,6 @@ import type koa from 'koa';
 export type ControllerHandler = <T>(
   context: koa.ExtendableContext,
   next: koa.Next
-) => Promise<T> | T;
+) => Promise<T | void> | T | void;
 
 export type Controller = Record<string, ControllerHandler>;
