d527b7f3cf3cfeeb9f43042c52b61c200ccfc96b
Jamie Howard <jamie.howard@strapi.io>
2023-06-01 15:53:28 +0100

    chore(webhooks/EditView): rename constants

--- packages/core/admin/admin/src/pages/SettingsPage/pages/Webhooks/EditView/components/HeadersInput/Combobox.js
+++ packages/core/admin/admin/src/pages/SettingsPage/pages/Webhooks/EditView/components/HeadersInput/Combobox.js
@@ -2,17 +2,56 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useFormikContext } from 'formik';
 import { ComboboxOption, CreatableCombobox } from '@strapi/design-system';
-import { keys } from './constants';
+
+const HTTP_HEADERS = [
+  'A-IM',
+  'Accept',
+  'Accept-Charset',
+  'Accept-Encoding',
+  'Accept-Language',
+  'Accept-Datetime',
+  'Access-Control-Request-Method',
+  'Access-Control-Request-Headers',
+  'Authorization',
+  'Cache-Control',
+  'Connection',
+  'Content-Length',
+  'Content-Type',
+  'Cookie',
+  'Date',
+  'Expect',
+  'Forwarded',
+  'From',
+  'Host',
+  'If-Match',
+  'If-Modified-Since',
+  'If-None-Match',
+  'If-Range',
+  'If-Unmodified-Since',
+  'Max-Forwards',
+  'Origin',
+  'Pragma',
+  'Proxy-Authorization',
+  'Range',
+  'Referer',
+  'TE',
+  'User-Agent',
+  'Upgrade',
+  'Via',
+  'Warning',
+];
 
 const Combobox = ({ name, onChange, value, ...props }) => {
   const {
     values: { headers },
   } = useFormikContext();
-  const [options, setOptions] = useState(keys);
+  const [options, setOptions] = useState(HTTP_HEADERS);
 
   useEffect(() => {
     setOptions(
-      keys.filter((key) => !headers?.some((header) => header.key !== value && header.key === key))
+      HTTP_HEADERS.filter(
+        (key) => !headers?.some((header) => header.key !== value && header.key === key)
+      )
     );
   }, [headers, value]);
 
