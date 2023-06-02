a9a843250de215000129653582056b20cc8cfe41
Jamie Howard <jamie.howard@strapi.io>
2023-06-02 11:44:42 +0100

    chore: disable review workflow event options

--- packages/core/admin/ee/admin/pages/SettingsPage/pages/Webhooks/EditView/components/EventTable/EventTableEE.js
+++ packages/core/admin/ee/admin/pages/SettingsPage/pages/Webhooks/EditView/components/EventTable/EventTableEE.js
@@ -3,24 +3,27 @@ import React from 'react';
 import EventTable from '../../../../../../../../../admin/src/pages/SettingsPage/pages/Webhooks/EditView/components/Events';
 
 const events = {
-  'review-workflows': ['review-workflows.create'],
+  'review-workflows': ['workflows.updateEntryStage'],
 };
 
-const getHeaders = () => {
-  return [{ id: 'workflows.updateEntryStage', defaultMessage: 'Stage Change' }];
-};
+// TODO: extend this to support review workflow events once the BE logic is ready
+// const getHeaders = () => {
+//   return [{ id: 'review-workflows.updateEntryStage', defaultMessage: 'Stage Change' }];
+// };
 
 export function EventTableEE() {
   return (
     <EventTable.Root
-      renderChildren={({ isDraftAndPublish }) => (
-        <>
-          <EventTable.Headers isDraftAndPublish={isDraftAndPublish} />
-          <EventTable.Body isDraftAndPublish={isDraftAndPublish} extraEvents={events} />
-          <EventTable.Headers getHeaders={getHeaders} />
-          <EventTable.Body providedEvents={events} />
-        </>
-      )}
+      renderChildren={({ isDraftAndPublish }) => {
+        return (
+          <>
+            <EventTable.Headers isDraftAndPublish={isDraftAndPublish} />
+            <EventTable.Body isDraftAndPublish={isDraftAndPublish} extraEvents={events} />
+            {/* <EventTable.Headers getHeaders={getHeaders} />
+            <EventTable.Body providedEvents={events} /> */}
+          </>
+        );
+      }}
     />
   );
 }
