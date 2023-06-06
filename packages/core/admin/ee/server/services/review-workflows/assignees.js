33883aeb431873ad06f3c676782664aab734ae51
Marc-Roig <marc12info@gmail.com>
2023-06-06 17:56:49 +0200

    feat: add assignee updateEntity service

--- packages/core/admin/ee/server/services/review-workflows/assignees.js
+++ packages/core/admin/ee/server/services/review-workflows/assignees.js
@@ -1,13 +1,25 @@
 'use strict';
 
+const { ApplicationError } = require('@strapi/utils').errors;
+const { ENTITY_ASSIGNEE_ATTRIBUTE } = require('../../constants/workflows');
+const { getService } = require('../../utils');
+
 module.exports = () => {
   return {
     /**
      * Update the assignee of an entity
      */
     async updateEntity(entityInfo, assigneeId) {
-      // TODO find a user by the assignee ID and update the entity
-      // const user = await usersService.findOne(assigneeId);
+      const userExists = await getService('user').exists({ id: assigneeId });
+
+      if (!userExists) {
+        throw new ApplicationError(`Selected user does not exist`);
+      }
+
+      return strapi.entityService.update(entityInfo.modelUID, entityInfo.id, {
+        data: { [ENTITY_ASSIGNEE_ATTRIBUTE]: assigneeId },
+        populate: [ENTITY_ASSIGNEE_ATTRIBUTE],
+      });
     },
   };
 };
