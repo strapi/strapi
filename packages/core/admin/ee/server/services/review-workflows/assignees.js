82668517b2c636cf296a0f9a523ea7c7df508e80
Marc-Roig <marc12info@gmail.com>
2023-06-07 10:46:59 +0200

    feat: delete assignee if id is null

--- packages/core/admin/ee/server/services/review-workflows/assignees.js
+++ packages/core/admin/ee/server/services/review-workflows/assignees.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { ApplicationError } = require('@strapi/utils').errors;
+const { isNil } = require('lodash/fp');
 const { ENTITY_ASSIGNEE_ATTRIBUTE } = require('../../constants/workflows');
 const { getService } = require('../../utils');
 
@@ -9,16 +10,29 @@ module.exports = () => {
     /**
      * Update the assignee of an entity
      */
-    async updateEntity(entityInfo, assigneeId) {
+    async updateEntityAssignee(id, model, assigneeId) {
+      if (isNil(assigneeId)) {
+        return this.deleteEntityAssignee(id, model);
+      }
+
       const userExists = await getService('user').exists({ id: assigneeId });
 
       if (!userExists) {
         throw new ApplicationError(`Selected user does not exist`);
       }
 
-      return strapi.entityService.update(entityInfo.modelUID, entityInfo.id, {
+      return strapi.entityService.update(model, id, {
         data: { [ENTITY_ASSIGNEE_ATTRIBUTE]: assigneeId },
         populate: [ENTITY_ASSIGNEE_ATTRIBUTE],
+        fields: [],
+      });
+    },
+
+    async deleteEntityAssignee(id, model) {
+      return strapi.entityService.update(model, id, {
+        data: { [ENTITY_ASSIGNEE_ATTRIBUTE]: null },
+        populate: [ENTITY_ASSIGNEE_ATTRIBUTE],
+        fields: [],
       });
     },
   };
