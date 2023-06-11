980754ed3fbaeab87da7978b03a803a1733d3781
Marc-Roig <marc12info@gmail.com>
2023-06-11 10:34:13 +0200

    feat: throttle transformation

---
+++ packages/core/upload/server/utils/media-builder/transforms/throttle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { ThrottleGroup } = require('stream-throttle');
+
+const THROTTLE_MB_MULTIPLIER = 1024 * 1024;
+
+const tg = new ThrottleGroup({
+  // Rate in bytes per second  e.g 10240 = 10KB/s.
+  rate: 1 * THROTTLE_MB_MULTIPLIER, // 100 MB/s
+});
+
+const throttle = async (file) => {
+  return {
+    ...file,
+    getStream: () => file.getStream().pipe(tg.throttle()),
+  };
+};
+
+module.exports = throttle;
