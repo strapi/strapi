7ad9b94c6c443aafe314bdba918a6275d40bfe55
Marc-Roig <marc12info@gmail.com>
2023-06-12 09:13:54 +0200

    feat: limit sharp concurrency

--- packages/core/upload/server/utils/media-builder/transforms/sharp/index.js
+++ packages/core/upload/server/utils/media-builder/transforms/sharp/index.js
@@ -1,10 +1,17 @@
 'use strict';
 
+const sharp = require('sharp');
 const autoRotate = require('./auto-rotate');
 const breakpoints = require('./breakpoints');
 const metadata = require('./metadata');
 const optimize = require('./optimize');
 const thumbnail = require('./thumbnail');
+
+// set all necessary sharp options here to reduce ram usage
+sharp.concurrency(1);
+sharp.cache(false);
+// set VIPS_DISC_THRESHOLD to 50 megs
+// VIPS_DISC_THRESHOLD=50m
 
 module.exports = {
   autoRotate,
--- packages/core/upload/server/utils/media-builder/transforms/throttle.js
+++ packages/core/upload/server/utils/media-builder/transforms/throttle.js
@@ -6,7 +6,7 @@ const THROTTLE_MB_MULTIPLIER = 1024 * 1024;
 
 const tg = new ThrottleGroup({
   // Rate in bytes per second  e.g 10240 = 10KB/s.
-  rate: 1 * THROTTLE_MB_MULTIPLIER, // 100 MB/s
+  rate: 100 * THROTTLE_MB_MULTIPLIER, // 100 MB/s
 });
 
 const throttle = async (file) => {
