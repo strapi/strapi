af5b8256ecf6483c5baf17a5f58dd6d6c3e5c98b
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-06-12 11:13:44 +0100

    chore: remove history from U&P ListView

--- packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/server.js
+++ packages/plugins/users-permissions/admin/src/pages/Roles/ListPage/tests/server.js
@@ -4,7 +4,6 @@ import { setupServer } from 'msw/node';
 const handlers = [
   rest.get('*/roles', (req, res, ctx) => {
     return res(
-      ctx.delay(1000),
       ctx.status(200),
       ctx.json({
         roles: [
