c2ca98344156a6716a2771b90bce16cf8bfead54
nathan-pichon <nathan.pichon@strapi.io>
2023-06-21 14:36:33 +0200

    feat(reviewWorkflows): let people be able to deactivate rw stage attr
    - without touching the old boolean for retro-compat

--- packages/core/admin/ee/server/utils/review-workflows.js
+++ packages/core/admin/ee/server/utils/review-workflows.js
@@ -5,7 +5,11 @@ const { ENTITY_STAGE_ATTRIBUTE } = require('../constants/workflows');
 
 const getVisibleContentTypesUID = pipe([
   // Pick only content-types visible in the content-manager and option is not false
-  pickBy((value) => getOr(true, 'pluginOptions.content-manager.visible', value)),
+  pickBy(
+    (value) =>
+      getOr(true, 'pluginOptions.content-manager.visible', value) &&
+      !getOr(false, 'options.noStageAttribute', value)
+  ),
   // Get UIDs
   keys,
 ]);
