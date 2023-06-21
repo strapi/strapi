c2c33a3b329a2ca2a887cf8d32d4e6ba205a122a
Ben Irvin <ben@innerdvations.com>
2023-06-21 19:18:05 +0200

    add comments to providers

--- packages/core/data-transfer/types/providers.d.ts
+++ packages/core/data-transfer/types/providers.d.ts
@@ -1,28 +1,26 @@
-import type { PipelineSource, PipelineDestination, Readable, Writable } from 'stream';
-import type {
-  IDestinationProviderTransferResults,
-  IProviderTransferResults,
-  ISourceProviderTransferResults,
-  Stream,
-  MaybePromise,
-} from './utils';
+import type { Readable, Writable } from 'stream';
+import type { IProviderTransferResults, MaybePromise } from './utils';
 import type { IMetadata } from './common-entities';
 
 type ProviderType = 'source' | 'destination';
 
 interface IProvider {
   type: ProviderType;
-  name: string;
-  results?: IProviderTransferResults;
+  name: string; // a unique name for this provider
+  results?: IProviderTransferResults; // optional object for tracking any data needed from outside the engine
 
+  /**
+   * bootstrap() is called during transfer engine bootstrap
+   * It is used for initialization operations such as making a database connection, opening a file, checking authorization, etc
+   */
   bootstrap?(): MaybePromise<void>;
-  close?(): MaybePromise<void>;
 
-  getMetadata(): MaybePromise<IMetadata | null>;
-  getSchemas?(): MaybePromise<Strapi.Schemas>;
+  close?(): MaybePromise<void>; // called during transfer engine close
 
-  beforeTransfer?(): MaybePromise<void>;
-  validateOptions?(): MaybePromise<void>;
+  getMetadata(): MaybePromise<IMetadata | null>; // returns the transfer metadata to be used for version validation
+  getSchemas?(): MaybePromise<Strapi.Schemas>; // returns the schemas for the schema validation
+
+  beforeTransfer?(): MaybePromise<void>; // called immediately before transfer stages are run
 }
 
 export interface ISourceProvider extends IProvider {
@@ -40,6 +38,7 @@ export interface IDestinationProvider extends IProvider {
 
   /**
    * Optional rollback implementation
+   * Called when an error is thrown during a transfer to allow rollback operations to be performed
    */
   rollback?<T extends Error = Error>(e: T): MaybePromise<void>;
 
