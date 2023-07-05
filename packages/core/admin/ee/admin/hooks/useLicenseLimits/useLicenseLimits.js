f4720e22db771ff8f83625ac789efdeceb7a75f2
Gustav Hansen <gustav.hansen@strapi.io>
2023-07-05 12:31:02 +0200

    Enhancement: Expose getFeature() method for useLicenseLimits hooks

--- packages/core/admin/ee/admin/hooks/useLicenseLimits/useLicenseLimits.js
+++ packages/core/admin/ee/admin/hooks/useLicenseLimits/useLicenseLimits.js
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { useFetchClient, useRBAC } from '@strapi/helper-plugin';
 import { useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
@@ -27,5 +29,16 @@ export function useLicenseLimits() {
     }
   );
 
-  return { license: data ?? {}, isError, isLoading };
+  const license = data ?? {};
+
+  const getFeature = React.useCallback(
+    (name) => {
+      const feature = (license?.features ?? []).find((feature) => feature.name === name);
+
+      return feature?.options ?? {};
+    },
+    [license?.features]
+  );
+
+  return { license, getFeature, isError, isLoading };
 }
