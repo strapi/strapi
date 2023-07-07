c8b78ac8230fdd87246810eb788adf275bf56086
Gustav Hansen <gustav.hansen@strapi.io>
2023-07-07 14:57:59 +0200

    Fix: Fix FE tests after faulty merge

--- packages/core/admin/ee/admin/pages/SettingsPage/pages/ReviewWorkflows/hooks/useReviewWorkflows.js
+++ packages/core/admin/ee/admin/pages/SettingsPage/pages/ReviewWorkflows/hooks/useReviewWorkflows.js
@@ -2,28 +2,21 @@ import { useFetchClient } from '@strapi/helper-plugin';
 import { useQuery } from 'react-query';
 
 export function useReviewWorkflows(params = {}) {
+  const { get } = useFetchClient();
+
   const { id = '', ...queryParams } = params;
   const defaultQueryParams = {
     populate: 'stages',
   };
 
-  const { get } = useFetchClient();
-
   const { data, isLoading, status, refetch } = useQuery(
     ['review-workflows', 'workflows', id],
     async () => {
-      try {
-        const {
-          data: { data },
-        } = await get(`/admin/review-workflows/workflows/${id}`, {
-          params: { ...defaultQueryParams, ...queryParams },
-        });
+      const res = await get(`/admin/review-workflows/workflows/${id}`, {
+        params: { ...defaultQueryParams, ...queryParams },
+      });
 
-        return data;
-      } catch (err) {
-        // silence
-        return null;
-      }
+      return res.data;
     }
   );
 
