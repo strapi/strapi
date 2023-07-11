f3343b0c0b825df6413366ede326d32a24656ec6
Marc-Roig <marc12info@gmail.com>
2023-07-11 17:27:59 +0200

    chore: comment displayName firstname might not be mandatory

--- packages/core/helper-plugin/src/utils/getDisplayName/index.js
+++ packages/core/helper-plugin/src/utils/getDisplayName/index.js
@@ -9,24 +9,25 @@
  * @type {(user: AdminUserNamesAttributes, formatMessage: import('react-intl').formatMessage) => string}
  */
 const getDisplayName = ({ firstname, lastname, username, email }, formatMessage) => {
-    if (username) {
-      return username;
-    }
-  
-    if (firstname) {
-      return formatMessage(
-        {
-          id: 'global.fullname',
-          defaultMessage: '{firstname} {lastname}',
-        },
-        {
-          firstname,
-          lastname,
-        }
-      ).trim();
-    }
-  
-    return email;
-  };
-  
-  export { getDisplayName };
+  if (username) {
+    return username;
+  }
+
+  // firstname is not required if the user is created with a username
+  if (firstname) {
+    return formatMessage(
+      {
+        id: 'global.fullname',
+        defaultMessage: '{firstname} {lastname}',
+      },
+      {
+        firstname,
+        lastname,
+      }
+    ).trim();
+  }
+
+  return email;
+};
+
+export { getDisplayName };
