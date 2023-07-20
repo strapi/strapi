e1e177976af3c8b59f8e37ea8ff2293a6c09234d
Ben Irvin <ben@innerdvations.com>
2023-07-20 11:36:23 +0200

    allow defaultRole to be nullable

--- packages/core/admin/ee/server/validation/authentication.js
+++ packages/core/admin/ee/server/validation/authentication.js
@@ -6,8 +6,13 @@ const providerOptionsUpdateSchema = yup.object().shape({
   autoRegister: yup.boolean().required(),
   defaultRole: yup
     .strapiID()
-    .required()
+    .when('autoRegister', (value, initSchema) => {
+      return value ? initSchema.required() : initSchema.nullable();
+    })
     .test('is-valid-role', 'You must submit a valid default role', (roleId) => {
+      if (roleId === null) {
+        return true;
+      }
       return strapi.admin.services.role.exists({ id: roleId });
     }),
   ssoLockedRoles: yup
