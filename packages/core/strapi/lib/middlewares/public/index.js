d3c5101aacc8a70748d266b4d8ee451229a8ac68
Nitin <nitin.madelyn@gmail.com>
2023-07-21 13:15:08 +0530

    fix strapi::public middleware

--- packages/core/strapi/lib/middlewares/public/index.js
+++ packages/core/strapi/lib/middlewares/public/index.js
@@ -18,77 +18,84 @@ const defaults = {
  * @type {import('../').MiddlewareFactory}
  */
 module.exports = (config, { strapi }) => {
-  const { defaultIndex, maxAge } = defaultsDeep(defaults, config);
+  const { defaultIndex, index, maxAge } = defaultsDeep(defaults, config);
 
-  if (defaultIndex === true) {
-    const index = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-
-    const serveIndexPage = async (ctx, next) => {
-      // defer rendering of strapi index page
-      await next();
-
-      if (ctx.body != null || ctx.status !== 404) return;
-
-      ctx.url = 'index.html';
-      const isInitialized = await utils.isInitialized(strapi);
-      const data = {
-        serverTime: new Date().toUTCString(),
-        isInitialized,
-        ..._.pick(strapi, [
-          'config.info.version',
-          'config.info.name',
-          'config.admin.url',
-          'config.server.url',
-          'config.environment',
-          'config.serveAdminPanel',
-        ]),
-      };
-      const content = _.template(index)(data);
-      const body = stream.Readable({
-        read() {
-          this.push(Buffer.from(content));
-          this.push(null);
-        },
-      });
-      // Serve static.
-      ctx.type = 'html';
-      ctx.body = body;
-    };
-
-    strapi.server.routes([
-      {
-        method: 'GET',
-        path: '/',
-        handler: serveIndexPage,
-        config: { auth: false },
-      },
-      {
-        method: 'GET',
-        path: '/index.html',
-        handler: serveIndexPage,
-        config: { auth: false },
-      },
-      {
-        method: 'GET',
-        path: '/assets/images/(.*)',
-        handler: serveStatic(path.resolve(__dirname, 'assets/images'), {
-          maxage: maxAge,
-          defer: true,
-        }),
-        config: { auth: false },
-      },
-      // All other public GET-routes except /uploads/(.*) which is handled in upload middleware
-      {
-        method: 'GET',
-        path: '/((?!uploads/).+)',
-        handler: koaStatic(strapi.dirs.static.public, {
-          maxage: maxAge,
-          defer: true,
-        }),
-        config: { auth: false },
-      },
-    ]);
+  let filePath = 'index.html';
+  if (!defaultIndex && index && index !== 'index.html') {
+    filePath = `./public/${index}`;
   }
+
+  if (!defaultIndex && fs.existsSync('./public/index.html')) {
+    filePath = './public/index.html';
+  }
+  filePath = filePath === 'index.html' ? path.join(__dirname, filePath) : filePath;
+  const indexFile = fs.readFileSync(filePath, 'utf8');
+
+  const serveIndexPage = async (ctx, next) => {
+    // defer rendering of strapi index page
+    await next();
+
+    if (ctx.body != null || ctx.status !== 404) return;
+
+    ctx.url = index ? '/' : ctx.url;
+    const isInitialized = await utils.isInitialized(strapi);
+    const data = {
+      serverTime: new Date().toUTCString(),
+      isInitialized,
+      ..._.pick(strapi, [
+        'config.info.version',
+        'config.info.name',
+        'config.admin.url',
+        'config.server.url',
+        'config.environment',
+        'config.serveAdminPanel',
+      ]),
+    };
+    const content = _.template(indexFile)(data);
+    const body = stream.Readable({
+      read() {
+        this.push(Buffer.from(content));
+        this.push(null);
+      },
+    });
+    // Serve static.
+    ctx.type = 'html';
+    ctx.body = body;
+  };
+
+  strapi.server.routes([
+    {
+      method: 'GET',
+      path: '/',
+      handler: serveIndexPage,
+      config: { auth: false },
+    },
+    {
+      method: 'GET',
+      path: '/index.html',
+      handler: serveIndexPage,
+      config: { auth: false },
+    },
+    {
+      method: 'GET',
+      path: '/assets/images/(.*)',
+      handler: serveStatic(path.resolve(__dirname, 'assets/images'), {
+        maxage: maxAge,
+        defer: true,
+      }),
+      config: { auth: false },
+    },
+    // All other public GET-routes except /uploads/(.*) which is handled in upload middleware
+    {
+      method: 'GET',
+      path: '/((?!uploads/).+)',
+      handler: koaStatic(strapi.dirs.static.public, {
+        maxage: maxAge,
+        defer: true,
+      }),
+      config: { auth: false },
+    },
+  ]);
 
   return null;
 };
