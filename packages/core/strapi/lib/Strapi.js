38d0e1cfe2c980d3e5076a5be17484d4caaf0e17
Alexandre Bodin <bodin.alex@gmail.com>
2023-07-24 22:07:27 +0200

    Update getter order

--- packages/core/strapi/lib/Strapi.js
+++ packages/core/strapi/lib/Strapi.js
@@ -583,10 +583,6 @@ const initializer = (options) => {
 
 module.exports = new Proxy(initializer, {
   get(_, prop) {
-    if (!singleton) {
-      throw new Error('Strapi is not initialized');
-    }
-
     if (prop === 'Strapi') {
       return Strapi;
     }
@@ -599,9 +595,10 @@ module.exports = new Proxy(initializer, {
       return compile;
     }
 
+    if (!singleton) {
+      throw new Error('Strapi is not initialized');
+    }
+
     return Reflect.get(singleton, prop, singleton);
-  },
-  set() {
-    throw new Error('Strapi is a readonly object');
   },
 });
