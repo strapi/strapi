4b2f1bf6d3ba9649bd617dc693fcc0f5141ef99f
Simone Taeggi <startae14@gmail.com>
2023-07-24 12:02:44 +0200

    move the pencil icon to the right

--- packages/core/admin/admin/src/content-manager/pages/ListView/components/BulkActionButtons/SelectedEntriesModal/index.js
+++ packages/core/admin/admin/src/content-manager/pages/ListView/components/BulkActionButtons/SelectedEntriesModal/index.js
@@ -188,6 +188,7 @@ const SelectedEntriesTableContent = ({ isPublishing, rowsToDisplay, entriesToPub
                 )}
                 noBorder
                 target="_blank"
+                style={{ marginLeft: 'auto' }}
               >
                 <Pencil />
               </IconButton>
