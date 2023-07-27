2d93e68487839dafdc530e290c6ada785ce1b32c
Fernando Chavez <fernando.chavez@strapi.io>
2023-07-27 14:22:22 +0200

    fix lint error

--- packages/core/admin/admin/src/components/NpsSurvey/tests/index.test.js
+++ packages/core/admin/admin/src/components/NpsSurvey/tests/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { lightTheme, ThemeProvider } from '@strapi/design-system';
-import { render, screen, act, waitFor } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import NpsSurvey from '..';
