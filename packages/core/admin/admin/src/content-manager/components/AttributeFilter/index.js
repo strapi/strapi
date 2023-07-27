108be6636a505eb23fe139952d613eef0c828385
Fernando Chavez <fernando.chavez@strapi.io>
2023-07-27 12:00:54 +0200

    Add creator fields to filters and list view

--- packages/core/admin/admin/src/content-manager/components/AttributeFilter/index.js
+++ packages/core/admin/admin/src/content-manager/components/AttributeFilter/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 
+import { useQueryParams } from '@strapi/helper-plugin';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 
+import { useAdminUsers } from '../../../hooks/useAdminUsers';
+import { getDisplayName } from '../../utils';
+
+import { AdminUsersFilter } from './AdminUsersFilter';
 import Filters from './Filters';
 import useAllowedAttributes from './hooks/useAllowedAttributes';
 
+const CREATOR_ATTRIBUTES = ['createdBy', 'updatedBy'];
+
 const AttributeFilter = ({ contentType, slug, metadatas }) => {
   const { formatMessage } = useIntl();
+
+  const [{ query }] = useQueryParams();
+  // We get the users selected' ids
+  const selectedUsers =
+    query?.filters?.$and?.reduce((acc, filter) => {
+      const [key, value] = Object.entries(filter)[0];
+      const id = value.id?.$eq || value.id?.$ne;
+
+      if (CREATOR_ATTRIBUTES.includes(key) && !acc.includes(id)) {
+        acc.push(id);
+      }
+
+      return acc;
+    }, []) ?? [];
+  const { users, isLoading } = useAdminUsers(
+    { filter: { id: { in: selectedUsers } } },
+    {
+      enabled: selectedUsers.length > 0,
+    }
+  );
+
   const allowedAttributes = useAllowedAttributes(contentType, slug);
   const displayedFilters = allowedAttributes.map((name) => {
     const attribute = contentType.attributes[name];
@@ -20,13 +48,46 @@ const AttributeFilter = ({ contentType, slug, metadatas }) => {
 
     const { mainField, label } = metadatas[name].list;
 
-    return {
+    const filter = {
       name,
       metadatas: { label: formatMessage({ id: label, defaultMessage: label }) },
       fieldSchema: { type, options, mainField },
       trackedEvent,
     };
+
+    if (attribute.type === 'relation' && attribute.target === 'admin::user') {
+      filter.metadatas = {
+        ...filter.metadatas,
+        customOperators: [
+          {
+            intlLabel: { id: 'components.FilterOptions.FILTER_TYPES.$eq', defaultMessage: 'is' },
+            value: '$eq',
+          },
+          {
+            intlLabel: {
+              id: 'components.FilterOptions.FILTER_TYPES.$ne',
+              defaultMessage: 'is not',
+            },
+            value: '$ne',
+          },
+        ],
+        customInput: AdminUsersFilter,
+        options: users.map((user) => ({
+          label: getDisplayName(user, formatMessage),
+          customValue: user.id.toString(),
+        })),
+      };
+      filter.fieldSchema.mainField = {
+        name: 'id',
+      };
+    }
+
+    return filter;
   });
+
+  if (isLoading) {
+    return null;
+  }
 
   return <Filters displayedFilters={displayedFilters} />;
 };
