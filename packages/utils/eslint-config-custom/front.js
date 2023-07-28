1f2f103093dff5f6f1da53a4aa0a34def8844f76
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-07-28 08:02:17 +0100

    chore: bump strapi/eslint-config to 0.2.0

--- packages/utils/eslint-config-custom/front.js
+++ packages/utils/eslint-config-custom/front.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@babel/eslint-parser',
-  extends: ['@strapi/eslint-config/front'],
+  extends: ['@strapi/eslint-config/front/javascript'],
   env: {
     browser: true,
     commonjs: true,
