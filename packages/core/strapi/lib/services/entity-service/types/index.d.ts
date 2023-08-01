20c0340b8fb6b29fe67fc4973c9d11450b8da5ee
Bassel <basselkanso82@gmail.com>
2023-08-01 13:34:09 +0300

    typing findPage entity service

--- packages/core/strapi/lib/services/entity-service/types/index.d.ts
+++ packages/core/strapi/lib/services/entity-service/types/index.d.ts
@@ -1,6 +1,20 @@
+import type { Attribute, Common, EntityService } from '@strapi/strapi';
+
 // TODO: Move params to @strapi/utils (related to convert-query-params)
 export * as Params from './params';
 
 export * from './plugin';
 
-export interface EntityService {}
+type Pagination = {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+};
+
+export interface EntityService {
+  findPage<TContentTypeUID extends Common.UID.ContentType>(
+    uid: TContentTypeUID,
+    params?: EntityService.Params.Read<TContentTypeUID>
+  ): Promise<{ results: Attribute.GetValues<TContentTypeUID>[]; pagination: Pagination } | null>;
+}
