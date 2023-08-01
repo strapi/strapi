c41c3e715d9316fbf2d9179b8167cc845b7b028b
Christian Capeans <christiancp100@gmail.com>
2023-08-01 13:44:39 +0200

    Add findMany types for entity service

--- packages/core/strapi/lib/services/entity-service/types/index.d.ts
+++ packages/core/strapi/lib/services/entity-service/types/index.d.ts
@@ -1,6 +1,16 @@
+import type { Attribute, Common, EntityService } from '@strapi/strapi';
+
 // TODO: Move params to @strapi/utils (related to convert-query-params)
 export * as Params from './params';
 
 export * from './plugin';
 
-export interface EntityService {}
+export interface EntityService {
+  findMany<TContentTypeUID extends Common.UID.ContentType>(
+    uid: TContentTypeUID,
+    params?: Omit<
+      EntityService.Params.Read<TContentTypeUID>,
+      keyof EntityService.Params.Pagination.PageNotation
+    >
+  ): Promise<Attribute.GetValues<TContentTypeUID>[]>;
+}
