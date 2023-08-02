f55ad19141cf11609a679ea65b5740454fd67b9f
Ben Irvin <ben@innerdvations.com>
2023-08-02 10:01:49 +0200

    fix protocol error

--- packages/core/data-transfer/src/strapi/providers/local-source/assets.ts
+++ packages/core/data-transfer/src/strapi/providers/local-source/assets.ts
@@ -1,9 +1,14 @@
 import { join } from 'path';
 import https from 'https';
+import http from 'http';
 import { Duplex, PassThrough, Readable } from 'stream';
 import { stat, createReadStream, ReadStream } from 'fs-extra';
 
 import type { IAsset } from '../../../../types';
+
+const httpMethod = (filepath) => {
+  return filepath?.startsWith('https') ? https : http;
+};
 
 function getFileStream(filepath: string, isLocal = false): PassThrough | ReadStream {
   if (isLocal) {
@@ -12,7 +17,7 @@ function getFileStream(filepath: string, isLocal = false): PassThrough | ReadStr
 
   const readableStream = new PassThrough();
 
-  https
+  httpMethod(filepath)
     .get(filepath, (res) => {
       if (res.statusCode !== 200) {
         readableStream.emit(
@@ -36,7 +41,7 @@ function getFileStats(filepath: string, isLocal = false): Promise<{ size: number
     return stat(filepath);
   }
   return new Promise((resolve, reject) => {
-    https
+    httpMethod(filepath)
       .get(filepath, (res) => {
         if (res.statusCode !== 200) {
           reject(new Error(`Request failed with status code ${res.statusCode}`));
