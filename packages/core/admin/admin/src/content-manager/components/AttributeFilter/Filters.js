f5b4d05a7a4576983b19932f134007226c426412
Simone Taeggi <startae14@gmail.com>
2023-08-07 12:20:57 +0200

    remove useless Box

--- packages/core/admin/admin/src/content-manager/components/AttributeFilter/Filters.js
+++ packages/core/admin/admin/src/content-manager/components/AttributeFilter/Filters.js
@@ -21,25 +21,23 @@ const Filters = ({ displayedFilters }) => {
 
   return (
     <>
-      <Box>
-        <Button
-          variant="tertiary"
-          ref={buttonRef}
-          startIcon={<Filter />}
-          onClick={handleToggle}
-          size="S"
-        >
-          {formatMessage({ id: 'app.utils.filters', defaultMessage: 'Filters' })}
-        </Button>
-        {isVisible && (
-          <FilterPopoverURLQuery
-            displayedFilters={displayedFilters}
-            isVisible={isVisible}
-            onToggle={handleToggle}
-            source={buttonRef}
-          />
-        )}
-      </Box>
+      <Button
+        variant="tertiary"
+        ref={buttonRef}
+        startIcon={<Filter />}
+        onClick={handleToggle}
+        size="S"
+      >
+        {formatMessage({ id: 'app.utils.filters', defaultMessage: 'Filters' })}
+      </Button>
+      {isVisible && (
+        <FilterPopoverURLQuery
+          displayedFilters={displayedFilters}
+          isVisible={isVisible}
+          onToggle={handleToggle}
+          source={buttonRef}
+        />
+      )}
       <FilterListURLQuery filtersSchema={displayedFilters} />
     </>
   );
