590f23fc82d5b06cbc606d5bdbaad61a8eb0a608
Ben Irvin <ben@innerdvations.com>
2023-08-09 09:44:23 +0200

    add types to kitchensink ts

--- examples/kitchensink-ts/config/admin.ts
+++ examples/kitchensink-ts/config/admin.ts
@@ -1,13 +1,16 @@
-export default ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'example-token'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT', 'example-salt'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'example-salt'),
+import type { Config } from '@strapi/strapi';
+
+export default ({ env }) =>
+  ({
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'example-token'),
     },
-  },
-});
+    apiToken: {
+      salt: env('API_TOKEN_SALT', 'example-salt'),
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT', 'example-salt'),
+      },
+    },
+  } satisfies Config.Admin);
--- examples/kitchensink-ts/config/server.ts
+++ examples/kitchensink-ts/config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
-  },
-});
+import { Config } from '@strapi/strapi';
+
+export default ({ env }) =>
+  ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
+    },
+  } satisfies Config.Server);
