b3ab02608c61cf43be76cc8b0e0ae12ee7c7560e
Ben Irvin <ben@innerdvations.com>
2023-08-09 10:10:35 +0200

    alternate format

--- examples/kitchensink-ts/config/server.ts
+++ examples/kitchensink-ts/config/server.ts
@@ -1,9 +1,11 @@
 import { Config } from '@strapi/strapi';
 
-export default (({ env }) => ({
+const serverConfig: Config.Server = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
   },
-})) satisfies Config.Server as Config.Server;
+});
+
+export default serverConfig;
