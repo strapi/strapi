ff888e41e79b18740078ab5a11292db621097fde
VIKAS KUMAR PATEL <vikaspatelkomail@gmail.com>
2023-08-18 12:48:27 +0530

    Update index.js
    fixed error due to await this.db.pragma('journal_mode = WAL');

--- packages/core/database/lib/dialects/sqlite/index.js
+++ packages/core/database/lib/dialects/sqlite/index.js
@@ -32,7 +32,7 @@ class SqliteDialect extends Dialect {
 
   async initialize() {
     await this.db.connection.raw('pragma foreign_keys = on');
-    await this.db.pragma('journal_mode = WAL');
+    await this.db.connection.raw('pragma journal_mode = WAL');
     await this.db.connection.raw('pragma synchronous = normal');
   }
 
