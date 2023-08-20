f1a82fcf51f9b0f88554b4e019d0cc78691ce332
mkcy3 <milankornicer@gmail.com>
2023-08-20 09:43:48 -0400

    migrate contentbox test to ts

--- packages/core/helper-plugin/src/components/tests/ContentBox.test.tsx
+++ packages/core/helper-plugin/src/components/tests/ContentBox.test.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { lightTheme, ThemeProvider } from '@strapi/design-system';
 import { render, screen } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
