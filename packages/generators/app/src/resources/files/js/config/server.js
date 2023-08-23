0720d171e4ecae89f830241ef21e72a433301363
Abhishek Girnar <64825778+Abhi1234-tech@users.noreply.github.com>
2023-08-23 07:09:14 +0000

    [BUGFIX] - #17536

--- packages/generators/app/src/resources/files/js/config/server.js
+++ packages/generators/app/src/resources/files/js/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://localhost:1337/'), // Updated this line
   app: {
     keys: env.array('APP_KEYS'),
   },
