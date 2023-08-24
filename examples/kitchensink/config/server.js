3fbf56c6dd79a64292632368849dc0a3fb38edf7
/-Powersploit-/ -- <abhishekgirnar1000@gmail.com>
2023-08-24 11:59:05 +0530

    [BUGFIX]-#17536

--- examples/kitchensink/config/server.js
+++ examples/kitchensink/config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
-    keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
