a2c1c37f7ae2f2b60da93c838584867878ddfb60
Jamie Howard <jamie.howard@strapi.io>
2023-08-25 10:30:43 +0100

    fix(ee): use getDisplayName for CM list view assignee column

--- packages/core/admin/ee/admin/content-manager/pages/ListView/ReviewWorkflowsColumn/ReviewWorkflowsAssigneeEE.js
+++ packages/core/admin/ee/admin/content-manager/pages/ListView/ReviewWorkflowsColumn/ReviewWorkflowsAssigneeEE.js
@@ -4,48 +4,19 @@ import { Typography } from '@strapi/design-system';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 
-import getTrad from '../../../../../../admin/src/content-manager/utils/getTrad';
+import { getDisplayName } from '../../../../../../admin/src/content-manager/utils';
 
-export function ReviewWorkflowsAssigneeEE({ firstname, lastname, displayname }) {
+export function ReviewWorkflowsAssigneeEE({ user }) {
   const { formatMessage } = useIntl();
 
-  // TODO align with changes from this PR, using the getDisplayName util
-  // https://github.com/strapi/strapi/pull/17043/
-  if (displayname) {
-    return (
-      <Typography textColor="neutral800">
-        {formatMessage(
-          {
-            id: getTrad(`containers.ListPage.reviewWorkflows.assignee`),
-            defaultMessage: '{displayname}',
-          },
-          { displayname }
-        )}
-      </Typography>
-    );
-  }
-
-  return (
-    <Typography textColor="neutral800">
-      {formatMessage(
-        {
-          id: getTrad(`containers.ListPage.reviewWorkflows.assignee`),
-          defaultMessage: '{firstname} {lastname}',
-        },
-        { firstname, lastname }
-      )}
-    </Typography>
-  );
+  return <Typography textColor="neutral800">{getDisplayName(user, formatMessage)}</Typography>;
 }
 
-ReviewWorkflowsAssigneeEE.defaultProps = {
-  firstname: '',
-  lastname: '',
-  displayname: '',
-};
-
 ReviewWorkflowsAssigneeEE.propTypes = {
-  firstname: PropTypes.string,
-  lastname: PropTypes.string,
-  displayname: PropTypes.string,
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
+    username: PropTypes.string,
+  }).isRequired,
 };
