195d6c5a1d9e3c93b07660e6a7d59e0258414c3a
boris.shulyak <borysshulyak@gmail.com>
2023-08-25 14:25:17 +0300

    refactor(helper-plugin/prefixFileUrlWithBackendUrl): migrate on TS

--- packages/core/helper-plugin/src/utils/prefixFileUrlWithBackendUrl.ts
+++ packages/core/helper-plugin/src/utils/prefixFileUrlWithBackendUrl.ts
@@ -1,4 +1,4 @@
-const prefixFileUrlWithBackendUrl = (fileURL) => {
+const prefixFileUrlWithBackendUrl = (fileURL?: string): string | undefined => {
   return !!fileURL && fileURL.startsWith('/') ? `${window.strapi.backendURL}${fileURL}` : fileURL;
 };
 
