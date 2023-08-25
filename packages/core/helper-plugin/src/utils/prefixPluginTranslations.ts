1afc1542d4cbf6024f53a75503c665b860f54b6c
boris.shulyak <borysshulyak@gmail.com>
2023-08-25 13:54:33 +0300

    refactor(helper-plugin/prefixPluginTranslations): migrate on TS

--- packages/core/helper-plugin/src/utils/prefixPluginTranslations.ts
+++ packages/core/helper-plugin/src/utils/prefixPluginTranslations.ts
@@ -1,9 +1,11 @@
-const prefixPluginTranslations = (trad, pluginId) => {
+type TradOptions = Record<string, string>;
+
+const prefixPluginTranslations = (trad: TradOptions, pluginId: string): TradOptions => {
   return Object.keys(trad).reduce((acc, current) => {
     acc[`${pluginId}.${current}`] = trad[current];
 
     return acc;
-  }, {});
+  }, {} as TradOptions);
 };
 
 export { prefixPluginTranslations };
