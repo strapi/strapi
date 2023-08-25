9b97320018552009719f7d81745ab6827a3f2677
dallasclark <dallasjclark@gmail.com>
2023-08-25 12:42:47 +1000

    Added @strapi/design-system StatusVariant and ThemeColors Types and @deprecated message.

--- packages/core/helper-plugin/src/components/Status.tsx
+++ packages/core/helper-plugin/src/components/Status.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
+import { StatusVariant, ThemeColors as DesignSystemThemeColors } from '@strapi/design-system';
 import styled from 'styled-components';
 
 type BulletProps = {
-  backgroundColor: 'alternative600' | 'danger600' | 'neutral600' | 'primary600' | 'secondary600' | 'success600' | 'warning600';
-}
+  backgroundColor: keyof DesignSystemThemeColors;
+};
 
 const Bullet = styled.div<BulletProps>`
   margin-right: ${({ theme }) => theme.spaces[3]};
@@ -14,11 +16,14 @@ const Bullet = styled.div<BulletProps>`
 `;
 
 type StatusProps = {
-  variant: 'alternative' | 'danger' | 'neutral' | 'primary' | 'secondary' | 'success' | 'warning';
+  variant: StatusVariant;
 };
 
-const Status: React.FC<StatusProps> = ({ variant = 'primary' }) => {
-  const backgroundColor: BulletProps["backgroundColor"] = `${variant}600`;
+/**
+ * @deprecated use @strapi/design-system `Status` instead.
+ */
+const Status = ({ variant = 'primary' }: StatusProps) => {
+  const backgroundColor: BulletProps['backgroundColor'] = `${variant}600`;
 
   return <Bullet backgroundColor={backgroundColor} />;
 };
