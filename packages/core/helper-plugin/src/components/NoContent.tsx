02a2beeea579282de0cc4c66e8e93cb12fc2cdd2
nolliebigspin <alec96.w@gmail.com>
2023-08-28 13:51:46 +0200

    fix(NoContent): add missing React import

--- packages/core/helper-plugin/src/components/NoContent.tsx
+++ packages/core/helper-plugin/src/components/NoContent.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { EmptyStateLayout, EmptyStateLayoutProps } from '@strapi/design-system';
 import { EmptyDocuments } from '@strapi/icons';
 import { MessageDescriptor, PrimitiveType, useIntl } from 'react-intl';
