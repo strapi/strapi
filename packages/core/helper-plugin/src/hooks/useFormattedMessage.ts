ae76618eb708ff2235050342e18430a98b00cec3
yurimutti <muttiyuri@gmail.com>
2023-08-27 22:18:14 -0300

    refactor: remove values

--- packages/core/helper-plugin/src/hooks/useFormattedMessage.ts
+++ packages/core/helper-plugin/src/hooks/useFormattedMessage.ts
@@ -1,17 +1,8 @@
-import React from 'react';
-
 import isObject from 'lodash/isObject';
 import { useIntl, MessageDescriptor } from 'react-intl';
 
-import type { FormatXMLElementFn } from 'intl-messageformat';
-
-type MessageFormatPrimitiveValue = string | number | boolean | null | undefined;
 interface MessageProps {
   message: MessageDescriptor;
-  values?: Record<
-    string,
-    MessageFormatPrimitiveValue | React.ReactElement | FormatXMLElementFn<string, string>
-  >;
 }
 
 const useFormattedMessage = ({ message }: MessageProps) => {
