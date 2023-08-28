3b42db69a0814cd7fd9386893d58b712af66d93a
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-08-28 11:45:22 +0200

    Deprecate stopPropagation utils

--- packages/core/helper-plugin/src/utils/stopPropagation.ts
+++ packages/core/helper-plugin/src/utils/stopPropagation.ts
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+/**
+ * @deprecated Use the onClick prop on the element you want to stop propagation on instead
+ */
 export const stopPropagation = {
   onClick: (e: React.MouseEvent) => e.stopPropagation(),
   role: 'button',
@@ -20,7 +23,11 @@ export const onRowClick = ({ fn, condition = true }: OnRowClickProps) => {
   }
 };
 
-// Use createElement to avoid making this file a TSX since it's in the utils folder
+/**
+ *
+ * @deprecated Set the onClick prop on the element you want to stop propagation on instead
+ */
 export const StopPropagation = () => {
+  // Use createElement to avoid making this file a TSX since it's in the utils folder
   return React.createElement('div', stopPropagation);
 };
