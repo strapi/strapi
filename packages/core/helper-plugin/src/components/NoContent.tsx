b9acf9d572e7a7f2ac7531abf7e4ca9bc102d169
nolliebigspin <alec96.w@gmail.com>
2023-08-28 12:49:11 +0200

    fix(NoContent): migrate to typescript

---
+++ packages/core/helper-plugin/src/components/NoContent.tsx
@@ -0,0 +1,31 @@
+import { EmptyStateLayout, EmptyStateLayoutProps } from '@strapi/design-system';
+import { EmptyDocuments } from '@strapi/icons';
+import { MessageDescriptor, PrimitiveType, useIntl } from 'react-intl';
+
+export type NoContentProps = Omit<EmptyStateLayoutProps, 'content' | 'icon'> & {
+  content?: MessageDescriptor & { values?: Record<string, PrimitiveType> };
+};
+
+const NoContent = ({
+  content = {
+    id: 'app.components.EmptyStateLayout.content-document',
+    defaultMessage: 'No content found',
+    values: {},
+  },
+  ...rest
+}: NoContentProps) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <EmptyStateLayout
+      {...rest}
+      icon={<EmptyDocuments width="10rem" />}
+      content={formatMessage(
+        { id: content.id, defaultMessage: content.defaultMessage },
+        content.values
+      )}
+    />
+  );
+};
+
+export { NoContent };
