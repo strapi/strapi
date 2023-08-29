26f55f938708cd08fb8f5939afd3460064c273e6
Fernando Chavez <fernando.chavez@strapi.io>
2023-08-29 13:19:14 +0200

    remove not used fields on window strapi types

--- packages/core/helper-plugin/src/types/custom.d.ts
+++ packages/core/helper-plugin/src/types/custom.d.ts
@@ -4,13 +4,6 @@ declare global {
   interface Window {
     strapi: {
       backendURL: string;
-      isEE: boolean;
-      features: {
-        SSO: 'sso';
-        isEnabled: (featureName?: string) => boolean;
-      };
-      projectType: string;
-      telemetryDisabled: boolean;
     };
   }
 }
