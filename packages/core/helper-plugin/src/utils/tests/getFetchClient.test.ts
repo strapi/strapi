f71854221b4568beb897e9be53add4a3b7b0ea07
Fernando Chávez <fernando.chavez@strapi.io>
2023-08-29 14:49:52 +0200

    [Helper Plugin] migrate getFetchClient to TS (#17801)
    * migrate getFetchClient to typescript
    
    * apply feedback
    
    * remove not used fields on window strapi types

--- packages/core/helper-plugin/src/utils/tests/getFetchClient.test.ts
+++ packages/core/helper-plugin/src/utils/tests/getFetchClient.test.ts
@@ -1,3 +1,7 @@
+import { server } from '@tests/utils';
+import { AxiosError } from 'axios';
+import { rest } from 'msw';
+
 import { auth } from '../auth';
 import { getFetchClient } from '../getFetchClient';
 
@@ -13,34 +17,31 @@ describe('HELPER-PLUGIN | utils | getFetchClient', () => {
     expect(response).toHaveProperty('del');
   });
 
-  it('should contain the headers config values and the data when we try to reach an unknown API and the passed URL', async () => {
-    const response = getFetchClient();
-    try {
-      await response.get('/test');
-    } catch (err) {
-      const { headers, url } = err.config;
-      expect(headers.Authorization).toContain(`Bearer ${token}`);
-      expect(url).toBe('/test');
-    }
-  });
-
   it('should contain the normalized URL when we try to reach an unknown API with an URL without prepending slash', async () => {
     const response = getFetchClient();
     try {
-      await response.get('test');
+      await response.get('test-fetch-client');
     } catch (err) {
-      const { url } = err.config;
-      expect(url).toBe('/test');
+      const url = (err as AxiosError).config?.url;
+      expect(url).toBe('/test-fetch-client');
     }
   });
 
   it('should contain the URL passed when we try to reach an unknown API with a full URL', async () => {
+    server.use(
+      rest.get('https://notinternalurl.com', (req, res, ctx) => {
+        return res(ctx.status(200));
+      })
+    );
+
     const response = getFetchClient();
     try {
-      await response.get('https://example.com');
+      await response.get('https://notinternalurl.com');
     } catch (err) {
-      const { url } = err.config;
-      expect(url).toBe('https://example.com');
+      const url = (err as AxiosError).config?.url;
+      expect(url).toBe('https://notinternalurl.com');
     }
+
+    server.restoreHandlers();
   });
 });
