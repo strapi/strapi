ba5e97e557d2a998c218213aa11cd7a84dcd761c
Convly <jean-sebastien.herbaux@epitech.eu>
2023-08-29 21:36:33 +0200

    Add missing https scheme

--- .github/actions/security/lockfile/script.sh
+++ .github/actions/security/lockfile/script.sh
@@ -2,4 +2,4 @@ yarn dlx lockfile-lint \
     --type $LOCKFILE_TYPE \
     --path $LOCKFILE_PATH \
     --allowed-hosts $LOCKFILE_ALLOWED_HOSTS \
-    --allowed-schemes "npm:" "workspace:" "patch:"
+    --allowed-schemes "npm:" "workspace:" "patch:" "https:"
