0150d6ae50c74d77f86770e797f929896ca9a7e0
Fernando Chavez <fernando.chavez@strapi.io>
2023-09-01 10:27:23 +0200

    using renderHook on AutoReload tests

--- packages/core/helper-plugin/src/features/tests/AutoReloadOverlayBlocker.test.tsx
+++ packages/core/helper-plugin/src/features/tests/AutoReloadOverlayBlocker.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import { act } from '@testing-library/react';
-import { render } from '@tests/utils';
+import { act, screen } from '@testing-library/react';
+import { renderHook } from '@tests/utils';
 
 import {
   AutoReloadOverlayBlockerProvider,
@@ -9,21 +9,6 @@ import {
 } from '../AutoReloadOverlayBlocker';
 
 const MAX_ELAPSED_TIME = 30 * 1000;
-
-const Component = () => {
-  const { lockAppWithAutoreload, unlockAppWithAutoreload } = useAutoReloadOverlayBlocker();
-
-  return (
-    <>
-      <button type="button" onClick={() => lockAppWithAutoreload()}>
-        Lock
-      </button>
-      <button type="button" onClick={() => unlockAppWithAutoreload()}>
-        Unlock
-      </button>
-    </>
-  );
-};
 
 const wrapper = ({ children }: { children: React.ReactNode }) => (
   <AutoReloadOverlayBlockerProvider>{children}</AutoReloadOverlayBlockerProvider>
@@ -39,40 +24,45 @@ describe('AutoReloadOverlayBlocker', () => {
   });
 
   it('should lock the app', async () => {
-    const { getByRole, user, getByText } = render(<Component />, {
-      renderOptions: { wrapper },
-      userEventOptions: { advanceTimers: jest.advanceTimersByTime },
+    const { result } = renderHook(useAutoReloadOverlayBlocker, {
+      wrapper,
     });
 
-    await user.click(getByRole('button', { name: 'Lock' }));
+    act(() => {
+      result.current.lockAppWithAutoreload();
+    })
 
-    expect(getByText(/Waiting for restart/)).toBeInTheDocument();
+    expect(screen.getByText(/Waiting for restart/)).toBeInTheDocument();
   });
 
   it('should unlock the app', async () => {
-    const { getByRole, user, getByText, queryByText } = render(<Component />, {
-      renderOptions: { wrapper },
-      userEventOptions: { advanceTimers: jest.advanceTimersByTime },
+    const { result } = renderHook(useAutoReloadOverlayBlocker, {
+      wrapper,
     });
 
-    await user.click(getByRole('button', { name: 'Lock' }));
-    expect(getByText(/Waiting for restart/)).toBeInTheDocument();
-    await user.click(getByRole('button', { name: 'Unlock' }));
-    expect(queryByText(/Waiting for restart/)).not.toBeInTheDocument();
+    act(() => {
+      result.current.lockAppWithAutoreload();
+    })
+    expect(screen.getByText(/Waiting for restart/)).toBeInTheDocument();
+    act(() => {
+      result.current.unlockAppWithAutoreload();
+    })
+    expect(screen.queryByText(/Waiting for restart/)).not.toBeInTheDocument();
   });
 
   it('should show failed message after 30s', async () => {
-    const { getByRole, user, getByText } = render(<Component />, {
-      renderOptions: { wrapper },
-      userEventOptions: { advanceTimers: jest.advanceTimersByTime },
+    const { result } = renderHook(useAutoReloadOverlayBlocker, {
+      wrapper,
     });
 
-    await user.click(getByRole('button', { name: 'Lock' }));
+    act(() => {
+      result.current.lockAppWithAutoreload();
+    })
 
-    expect(getByText(/Waiting for restart/)).toBeInTheDocument();
+    expect(screen.getByText(/Waiting for restart/)).toBeInTheDocument();
 
     act(() => jest.advanceTimersByTime(MAX_ELAPSED_TIME));
 
-    expect(getByText(/The restart is taking longer than expected/)).toBeInTheDocument();
+    expect(screen.getByText(/The restart is taking longer than expected/)).toBeInTheDocument();
   });
 });
