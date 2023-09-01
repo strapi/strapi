5e93aa155519f680b20043768ce9531a74adf673
Mark Kaylor <mark.kaylor@strapi.io>
2023-09-01 09:40:07 +0200

    Migrate GuideTour context to TypeScript

--- packages/core/helper-plugin/src/features/GuidedTour.tsx
+++ packages/core/helper-plugin/src/features/GuidedTour.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import PropTypes from 'prop-types';
-
 /**
  * TODO: whats the value in having this in the `helper-plugin`? is it actually
  * used externally. I doubt it.
@@ -10,43 +8,76 @@ import PropTypes from 'prop-types';
 /* -------------------------------------------------------------------------------------------------
  * Context
  * ------------------x-----------------------------------------------------------------------------*/
+interface GuidedTourContextValue {
+  currentStep: string | null;
+  guidedTourState: {
+    contentTypeBuilder: {
+      create: boolean;
+      success: boolean;
+    };
+    contentManager: {
+      create: boolean;
+      success: boolean;
+    };
+    apiTokens: {
+      create: boolean;
+      success: boolean;
+    };
+  };
+  isGuidedTourVisible: boolean;
+  isSkipped: boolean;
+  setCurrentStep: (step: string) => void | null;
+  setGuidedTourVisibility: (isVisible: boolean) => void;
+  setSkipped: (isSkipped: boolean) => void;
+  setStepState: (step: string, state: { create: boolean; success: boolean }) => void;
+  startSection: (section: string) => string | null;
+}
 
-/**
- * @preserve
- * @typedef {Object} GuidedTourContextValue
- * @property {string} currentStep
- * @property {Object} guidedTourState
- * @property {boolean} isGuidedTourVisible
- * @property {boolean} isSkipped
- * @property {(step: string) => void} setCurrentStep
- * @property {(isVisible: boolean) => void} setGuidedTourVisibility
- * @property {(isSkipped: boolean) => void} setSkipped
- * @property {(step: string, state: { create: boolean; success: boolean }) => void} setStepState
- * @property {(section: string) => void} startSection
- */
-
-/**
- * @preserve
- * @type {React.Context<GuidedTourContextValue>}
- */
-const GuidedTourContext = React.createContext();
+const GuidedTourContext = React.createContext<GuidedTourContextValue>({
+  currentStep: null,
+  guidedTourState: {
+    contentTypeBuilder: {
+      create: false,
+      success: false,
+    },
+    contentManager: {
+      create: false,
+      success: false,
+    },
+    apiTokens: {
+      create: false,
+      success: false,
+    },
+  },
+  isGuidedTourVisible: false,
+  isSkipped: true,
+  setCurrentStep: () => null,
+  setGuidedTourVisibility: () => null,
+  setSkipped: () => null,
+  setStepState: () => null,
+  startSection: () => null,
+});
 
 /* -------------------------------------------------------------------------------------------------
  * Provider
  * -----------------------------------------------------------------------------------------------*/
 
+interface GuidedTourProviderProps extends GuidedTourContextValue {
+  children: React.ReactNode;
+}
+
 const GuidedTourProvider = ({
   children,
-  currentStep,
+  currentStep = null,
   guidedTourState,
-  isGuidedTourVisible,
+  isGuidedTourVisible = false,
   isSkipped,
   setCurrentStep,
   setGuidedTourVisibility,
   setSkipped,
   setStepState,
   startSection,
-}) => {
+}: GuidedTourProviderProps) => {
   const value = React.useMemo(
     () => ({
       currentStep,
@@ -75,37 +106,10 @@ const GuidedTourProvider = ({
   return <GuidedTourContext.Provider value={value}>{children}</GuidedTourContext.Provider>;
 };
 
-GuidedTourProvider.defaultProps = {
-  currentStep: null,
-  isGuidedTourVisible: false,
-};
-
-GuidedTourProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-  currentStep: PropTypes.string,
-  guidedTourState: PropTypes.objectOf(
-    PropTypes.shape({
-      create: PropTypes.bool,
-      success: PropTypes.bool,
-    })
-  ).isRequired,
-  isGuidedTourVisible: PropTypes.bool,
-  isSkipped: PropTypes.bool.isRequired,
-  setCurrentStep: PropTypes.func.isRequired,
-  setGuidedTourVisibility: PropTypes.func.isRequired,
-  setSkipped: PropTypes.func.isRequired,
-  setStepState: PropTypes.func.isRequired,
-  startSection: PropTypes.func.isRequired,
-};
-
 /* -------------------------------------------------------------------------------------------------
  * Hook
  * -----------------------------------------------------------------------------------------------*/
 
-/**
- * @preserve
- * @returns {GuidedTourContextValue}
- */
 const useGuidedTour = () => React.useContext(GuidedTourContext);
 
 export { GuidedTourContext, GuidedTourProvider, useGuidedTour };
