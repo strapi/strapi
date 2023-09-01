dd3c9bc9be67b5d84ad4547af27d8dd935317e11
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-09-01 09:56:57 +0100

    refactor: tracking to tsx (#17888)

--- packages/core/helper-plugin/custom.d.ts
+++ packages/core/helper-plugin/custom.d.ts
@@ -4,6 +4,8 @@ declare global {
   interface Window {
     strapi: {
       backendURL: string;
+      telemetryDisabled: boolean;
+      projectType: 'Enterprise' | 'Community';
     };
   }
 }
--- packages/core/helper-plugin/src/features/tests/Tracking.test.tsx
+++ packages/core/helper-plugin/src/features/tests/Tracking.test.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-
 import { renderHook } from '@testing-library/react';
 import axios from 'axios';
 
-import { useAppInfo } from '../AppInfo';
-import { TrackingProvider, useTracking } from '../Tracking';
-
-jest.mock('../AppInfo');
+import { AppInfoProvider } from '../AppInfo';
+import { TrackingProvider, TrackingProviderProps, useTracking } from '../Tracking';
 
 jest.mock('axios', () => ({
   ...jest.requireActual('axios'),
@@ -15,21 +11,29 @@ jest.mock('axios', () => ({
   }),
 }));
 
-const setup = (props) =>
+const setup = (props?: TrackingProviderProps['value']) =>
   renderHook(() => useTracking(), {
     wrapper: ({ children }) => (
-      <TrackingProvider
-        value={{
-          uuid: '1',
-          telemetryProperties: {
-            nestedProperty: true,
-          },
-          deviceId: 'someTestDeviceId',
-          ...props,
-        }}
+      <AppInfoProvider
+        currentEnvironment="testing"
+        userId="someTestUserId"
+        shouldUpdateStrapi={false}
+        setUserDisplayName={jest.fn()}
+        userDisplayName="someTestUserDisplayName"
       >
-        {children}
-      </TrackingProvider>
+        <TrackingProvider
+          value={{
+            uuid: '1',
+            telemetryProperties: {
+              useTypescriptOnServer: true,
+            },
+            deviceId: 'someTestDeviceId',
+            ...props,
+          }}
+        >
+          {children}
+        </TrackingProvider>
+      </AppInfoProvider>
     ),
   });
 
@@ -43,26 +47,19 @@ describe('useTracking', () => {
   });
 
   it('should call axios.post with all attributes by default when calling trackUsage()', async () => {
-    useAppInfo.mockReturnValue({
-      currentEnvironment: 'testing',
-      userId: 'someTestUserId',
-    });
-
     const { result } = setup();
 
-    const res = await result.current.trackUsage('event', { trackingProperty: true });
+    const res = await result.current.trackUsage('didAccessAuthenticatedAdministration');
 
     expect(axios.post).toBeCalledWith(
       'https://analytics.strapi.io/api/v2/track',
       {
         userId: 'someTestUserId',
         deviceId: 'someTestDeviceId',
-        event: 'event',
-        eventProperties: {
-          trackingProperty: true,
-        },
+        event: 'didAccessAuthenticatedAdministration',
+        eventProperties: {},
         groupProperties: {
-          nestedProperty: true,
+          useTypescriptOnServer: true,
           projectId: '1',
           projectType: 'Community',
         },
@@ -71,7 +68,7 @@ describe('useTracking', () => {
       {
         headers: {
           'Content-Type': 'application/json',
-          'X-Strapi-Event': 'event',
+          'X-Strapi-Event': 'didAccessAuthenticatedAdministration',
         },
       }
     );
@@ -88,7 +85,7 @@ describe('useTracking', () => {
 
     const { result } = setup();
 
-    await result.current.trackUsage();
+    await result.current.trackUsage('didAccessAuthenticatedAdministration');
 
     expect(axios.post).not.toBeCalled();
 
@@ -97,10 +94,10 @@ describe('useTracking', () => {
 
   it('should not track if there is no uuid set in the context', async () => {
     const { result } = setup({
-      uuid: null,
+      uuid: false,
     });
 
-    await result.current.trackUsage();
+    await result.current.trackUsage('didAccessAuthenticatedAdministration');
 
     expect(axios.post).not.toBeCalled();
   });
@@ -110,7 +107,7 @@ describe('useTracking', () => {
 
     const { result } = setup();
 
-    const res = await result.current.trackUsage('event', { trackingProperty: true });
+    const res = await result.current.trackUsage('didAccessAuthenticatedAdministration');
 
     expect(axios.post).toHaveBeenCalled();
     expect(res).toEqual(null);
