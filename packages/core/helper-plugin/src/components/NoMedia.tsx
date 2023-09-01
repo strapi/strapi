a06f285dfe4b14b31e3834c42043b9868f8a6798
dallasclark <dallasjclark@gmail.com>
2023-09-02 08:30:27 +1000

    Fixed issue with icon being overwritten by the component's props and removed icon from the NoMedia component's props.

--- packages/core/helper-plugin/src/components/NoMedia.tsx
+++ packages/core/helper-plugin/src/components/NoMedia.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { EmptyStateLayout, EmptyStateLayoutProps } from '@strapi/design-system';
 import { EmptyPictures } from '@strapi/icons';
 
+type NoMediaProps = Omit<EmptyStateLayoutProps, 'icon'>;
+
 /*
  * @deprecated use @strapi/design-system `EmptyStateLayout` instead.
  */
-const NoMedia = (props: EmptyStateLayoutProps) => {
-  return <EmptyStateLayout icon={<EmptyPictures width="10rem" />} {...props} />;
+const NoMedia = (props: NoMediaProps) => {
+  return <EmptyStateLayout {...props} icon={<EmptyPictures width="10rem" />} />;
 };
 
 export { NoMedia };
