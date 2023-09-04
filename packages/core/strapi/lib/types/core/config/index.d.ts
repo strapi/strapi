d1728623de1387138325e373211f99061ed12a59
Bassel <basselkanso82@gmail.com>
2023-09-04 15:30:15 +0300

    fix

--- packages/core/strapi/lib/types/core/config/index.d.ts
+++ packages/core/strapi/lib/types/core/config/index.d.ts
@@ -1,5 +1,5 @@
 export type { Server } from './server';
 export type { Admin } from './admin';
 export type { Api } from './api';
-export type { Plugins } from './plugin';
+export type { Plugin } from './plugin';
 export type { Database } from './database';
