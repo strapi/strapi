cab837b98a86b45776f40a8943acd5562116ebac
Fernando Chavez <fernando.chavez@strapi.io>
2023-09-05 09:29:58 +0200

    change deprecated DateTimePicker to use once

--- packages/core/helper-plugin/src/components/DateTimePicker.tsx
+++ packages/core/helper-plugin/src/components/DateTimePicker.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 
 import { DateTimePicker, DateTimePickerProps } from '@strapi/design-system';
 
+import { once } from '../utils/once';
+
 // TODO: remove DateTimePicker component from the helper-plugin in V5
 
 /**
  * @deprecated Use the DateTimePicker from the Design System instead.
  */
 const DateTimePickerLegacy = (props: DateTimePickerProps) => {
-  React.useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      console.warn(
-        `
-        Deprecation warning: Usage of "DateTimePicker" component from the helper-plugin is deprecated and will be removed in the next major release. Instead, use the DateTimePicker from the Design System: import { DateTimePicker } from '@strapi/design-system';"
-        `
-      );
-    }
-  }, []);
+  once(() => {
+    console.warn(
+      `
+      Deprecation warning: Usage of "DateTimePicker" component from the helper-plugin is deprecated and will be removed in the next major release. Instead, use the DateTimePicker from the Design System: import { DateTimePicker } from '@strapi/design-system';"
+      `
+    );
+  });
 
   return <DateTimePicker {...props} />;
 };
