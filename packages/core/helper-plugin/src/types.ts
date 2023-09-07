1f0590c25459832da07ca37a53f4b908d17f14a9
Fernando Chavez <fernando.chavez@strapi.io>
2023-09-07 10:34:57 +0200

    remove unnecessary permission type

--- packages/core/helper-plugin/src/types.ts
+++ packages/core/helper-plugin/src/types.ts
@@ -3,12 +3,3 @@ import type { MessageDescriptor, PrimitiveType } from 'react-intl';
 export interface TranslationMessage extends MessageDescriptor {
   values?: Record<string, PrimitiveType>;
 }
-
-// @TODO: When we find a way to share types between packages,
-// we should use that instead of duplicating the Permission interface
-export interface Permission {
-  action: string;
-  subject?: string | object | null;
-  properties?: object;
-  conditions?: string[];
-}
