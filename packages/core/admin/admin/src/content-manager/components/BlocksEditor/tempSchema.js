2273c096dbe47e0d8fd1de05c00a34afcb68124c
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2023-09-08 12:39:54 +0200

    test cases added

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/tempSchema.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/tempSchema.js
@@ -1,29 +1,18 @@
 export const blocksData = [
-  // A paragraph with the text split into different sections, each with its own modifier
   {
     type: 'paragraph',
     children: [
       { text: 'This is editable ' },
-      { text: 'rich', bold: true },
-      { text: ' text, ' },
-      { text: 'much', italic: true },
-      { text: ' better than a ' },
-      { text: '<textarea>', code: true },
-      { text: '!' },
-    ],
-  },
-  {
-    type: 'paragraph',
-    children: [
-      { type: 'text', text: 'Hello', bold: true },
-      { type: 'text', text: 'there.' },
+      { text: 'This is bold text', bold: true },
+      { text: 'This is deleted text', strikethrough: true },
+      { text: '<textarea>\n', code: true },
       { type: 'text', text: 'It is me', italic: true },
       { type: 'text', text: 'Joe', underline: true, strikethrough: true },
       { type: 'text', text: 'Kai Doe', underline: true },
       {
         type: 'link',
-        url: 'https://www.google.com/',
-        children: [{ type: 'text', text: 'looking for', italic: true }],
+        url: 'https://example.com',
+        children: [{ type: 'text', text: 'click me', italic: true }],
       },
     ],
   },
@@ -33,7 +22,7 @@ export const blocksData = [
     children: [
       {
         type: 'text',
-        text: 'This is bold text',
+        text: 'This is a text with multiple tags',
         bold: true,
         italic: true,
         underline: true,
@@ -201,53 +190,16 @@ export const blocksData = [
       },
     ],
   },
-  // {
-  //   type: 'paragraph',
-  //   children: [
-  //     {
-  //       type: 'image',
-  //       image: {
-  //         url: 'https://via.placeholder.com/300/09f/fff.png',
-  //         name: 'Dummy-image.png',
-  //         alternativeText: null,
-  //         caption: null,
-  //         width: 300,
-  //         height: 300,
-  //         formats: {
-  //           thumbnail: {
-  //             name: 'thumbnail_Screenshot 2023-08-22 at 10.33.50.png',
-  //             hash: 'thumbnail_Screenshot_2023_08_22_at_10_33_50_ac7d5fd5b1',
-  //             ext: '.png',
-  //             mime: 'image/png',
-  //             path: null,
-  //             width: 150,
-  //             height: 156,
-  //             size: 10.31,
-  //             url: '/uploads/thumbnail_Screenshot_2023_08_22_at_10_33_50_ac7d5fd5b1.png',
-  //           },
-  //           small: {
-  //             name: 'small_Screenshot 2023-08-22 at 10.33.50.png',
-  //             hash: 'small_Screenshot_2023_08_22_at_10_33_50_ac7d5fd5b1',
-  //             ext: '.png',
-  //             mime: 'image/png',
-  //             path: null,
-  //             width: 480,
-  //             height: 500,
-  //             size: 57.69,
-  //             url: '/uploads/small_Screenshot_2023_08_22_at_10_33_50_ac7d5fd5b1.png',
-  //           },
-  //         },
-  //         hash: 'Screenshot_2023_08_22_at_10_33_50_ac7d5fd5b1',
-  //         ext: '.png',
-  //         mime: 'image/png',
-  //         size: 17.95,
-  //         previewUrl: null,
-  //         provider: 'local',
-  //         provider_metadata: null,
-  //         createdAt: '2023-08-24T09:43:30.065Z',
-  //         updatedAt: '2023-08-24T09:43:30.065Z',
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    type: 'image',
+    children: [{ text: '' }], // for void elements this needs to be defined
+    image: {
+      url: 'https://via.placeholder.com/300/09f/fff.png',
+      name: 'Dummy-image.png',
+      alternativeText: null,
+      caption: null,
+      width: 300,
+      height: 300,
+    },
+  },
 ];
