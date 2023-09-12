44244d6df024878ac7671e7d88593c00ad94b8e9
Madhuri Sandbhor <madhurisandbhor@gmail.com>
2023-09-12 15:16:58 +0530

    [RTE]: Block renderers (#17980)
    * draft - blocks renderer
    
    * test cases added
    
    * removing unnecssary callbacks
    
    * added components to use typography and styled components, updated test case
    
    * minor css changes
    
    * named exports
    
    * draft - blocks renderer
    
    * test cases added
    
    * removing unnecssary callbacks
    
    * added components to use typography and styled components, updated test case
    
    * minor css changes
    
    * named exports
    
    * element components are moved inside main component

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/index.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/index.js
@@ -1,14 +1,107 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 
-import { Box, Flex, Typography, InputWrapper, Divider } from '@strapi/design-system';
-import { pxToRem } from '@strapi/helper-plugin';
+import { Box, Flex, Typography, InputWrapper, Divider, BaseLink } from '@strapi/design-system';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact, ReactEditor } from 'slate-react';
-import styled from 'styled-components';
+import styled, { useTheme, css } from 'styled-components';
 
+import { blocksData } from './tempSchema';
 import { BlocksToolbar } from './Toolbar';
+
+const H1 = styled(Typography).attrs({ as: 'h1' })`
+  font-size: ${42 / 16}rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const H2 = styled(Typography).attrs({ as: 'h2' })`
+  font-size: ${35 / 16}rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const H3 = styled(Typography).attrs({ as: 'h3' })`
+  font-size: ${29 / 16}rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const H4 = styled(Typography).attrs({ as: 'h4' })`
+  font-size: ${24 / 16}rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const H5 = styled(Typography).attrs({ as: 'h5' })`
+  font-size: ${20 / 16}rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const H6 = styled(Typography).attrs({ as: 'h6' })`
+  font-size: 1rem;
+  line-height: ${({ theme }) => theme.lineHeights[1]};
+`;
+
+const Italic = styled(Typography)`
+  font-style: italic;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+`;
+
+const InlineCode = styled.code`
+  background-color: ${({ theme }) => theme.colors.neutral150};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  padding: ${({ theme }) => `0 ${theme.spaces[2]}`};
+  font-family: 'SF Mono', SFMono-Regular, ui-monospace, 'DejaVu Sans Mono', Menlo, Consolas,
+    monospace;
+`;
+
+const CodeBlock = styled.pre`
+  border-radius: ${({ theme }) => theme.borderRadius};
+  background-color: #32324d; // since the color is same between the themes
+  max-width: 100%;
+  overflow: auto;
+  padding: ${({ theme }) => theme.spaces[2]};
+  & > code {
+    color: #839496; // TODO: to confirm with design and get theme color
+    overflow: auto;
+    max-width: 100%;
+    padding: ${({ theme }) => theme.spaces[2]};
+  }
+`;
+
+const Blockquote = styled.blockquote`
+  margin: ${({ theme }) => `${theme.spaces[6]} 0`};
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  border-left: ${({ theme }) => `${theme.spaces[1]} solid ${theme.colors.neutral150}`};
+  font-style: italic;
+  padding: ${({ theme }) => theme.spaces[2]} ${({ theme }) => theme.spaces[5]};
+`;
+
+const listStyle = css`
+  margin-block-start: ${({ theme }) => theme.spaces[4]};
+  margin-block-end: ${({ theme }) => theme.spaces[4]};
+  margin-inline-start: ${({ theme }) => theme.spaces[0]};
+  margin-inline-end: ${({ theme }) => theme.spaces[0]};
+  padding-inline-start: ${({ theme }) => theme.spaces[4]};
+
+  ol,
+  ul {
+    margin-block-start: ${({ theme }) => theme.spaces[0]};
+    margin-block-end: ${({ theme }) => theme.spaces[0]};
+  }
+`;
+
+const Orderedlist = styled.ol`
+  list-style-type: decimal;
+  ${listStyle}
+`;
+
+const Unorderedlist = styled.ul`
+  list-style-type: disc;
+  ${listStyle}
+`;
 
 const TypographyAsterisk = styled(Typography)`
   line-height: 0;
@@ -18,21 +111,130 @@ const EditorDivider = styled(Divider)`
   background: ${({ theme }) => theme.colors.neutral200};
 `;
 
-const style = {
-  fontSize: pxToRem(14),
+const Wrapper = styled(Box)`
+  width: 100%;
+  max-height: 512px;
+  overflow: auto;
+  padding: ${({ theme }) => `${theme.spaces[3]} ${theme.spaces[4]}`};
+  font-size: ${({ theme }) => theme.fontSizes[2]}
+  background-color: ${({ theme }) => theme.colors.neutral0};
+  color: ${({ theme }) => theme.colors.neutral800};
+  line-height: ${({ theme }) => theme.lineHeights[6]};
+  border-radius: ${({ theme }) => theme.borderRadius};
+`;
+
+const getEditorStyle = (theme) => ({
   // The outline style is set on the wrapper with :focus-within
   outline: 'none',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spaces[2],
+});
+
+const Image = ({ attributes, children, element }) => {
+  const { url, alternativeText, width, height } = element.image;
+
+  return (
+    <Box {...attributes}>
+      {children}
+      <Box contentEditable={false}>
+        <Img src={url} alt={alternativeText} width={width} height={height} />
+      </Box>
+    </Box>
+  );
 };
 
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
+const Heading = ({ attributes, children, element }) => {
+  switch (element.level) {
+    case 1:
+      return <H1 {...attributes}>{children}</H1>;
+    case 2:
+      return <H2 {...attributes}>{children}</H2>;
+    case 3:
+      return <H3 {...attributes}>{children}</H3>;
+    case 4:
+      return <H4 {...attributes}>{children}</H4>;
+    case 5:
+      return <H5 {...attributes}>{children}</H5>;
+    case 6:
+      return <H6 {...attributes}>{children}</H6>;
+    default: // do nothing
+      return null;
+  }
+};
+
+const List = ({ attributes, children, element }) => {
+  if (element.format === 'ordered') return <Orderedlist {...attributes}>{children}</Orderedlist>;
+
+  return <Unorderedlist {...attributes}>{children}</Unorderedlist>;
+};
+
+const renderElement = (props) => {
+  const { attributes, element, children } = props;
+
+  switch (element.type) {
+    case 'heading':
+      return <Heading {...props} />;
+    case 'link':
+      return (
+        <BaseLink href={element.url} {...attributes}>
+          {children}
+        </BaseLink>
+      );
+    case 'code':
+      return (
+        <CodeBlock {...attributes}>
+          <code>{children}</code>
+        </CodeBlock>
+      );
+    case 'quote':
+      return <Blockquote {...attributes}>{children}</Blockquote>;
+    case 'list':
+      return <List {...props} />;
+    case 'list-item':
+      return (
+        <Typography as="li" {...attributes}>
+          {children}
+        </Typography>
+      );
+    case 'image':
+      return <Image {...props} />;
+    default:
+      return (
+        <Typography as="p" variant="omega" {...attributes}>
+          {children}
+        </Typography>
+      );
+  }
+};
+
+const renderLeaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <Typography fontWeight="bold">{children}</Typography>;
+  }
+
+  if (leaf.italic) {
+    children = <Italic>{children}</Italic>;
+  }
+
+  if (leaf.underline) {
+    children = <Typography textDecoration="underline">{children}</Typography>;
+  }
+
+  if (leaf.strikethrough) {
+    children = <Typography textDecoration="line-through">{children}</Typography>;
+  }
+
+  if (leaf.code) {
+    children = <InlineCode>{children}</InlineCode>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
 
 const BlocksEditor = React.forwardRef(({ intlLabel, name, readOnly, required, error }, ref) => {
   const { formatMessage } = useIntl();
+  const theme = useTheme();
   const [editor] = React.useState(() => withReact(createEditor()));
 
   const label = intlLabel.id
@@ -66,11 +268,18 @@ const BlocksEditor = React.forwardRef(({ intlLabel, name, readOnly, required, er
           </Typography>
         </Flex>
 
-        <Slate editor={editor} initialValue={initialValue}>
+        <Slate editor={editor} initialValue={blocksData}>
           <InputWrapper direction="column" alignItems="flex-start">
             <BlocksToolbar />
             <EditorDivider width="100%" />
-            <Editable readOnly={readOnly} style={style} />
+            <Wrapper>
+              <Editable
+                readOnly={readOnly}
+                style={getEditorStyle(theme)}
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+              />
+            </Wrapper>
           </InputWrapper>
         </Slate>
       </Flex>
--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/tests/index.test.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/tests/index.test.js
@@ -29,12 +29,34 @@ describe('BlocksEditor', () => {
     setup();
 
     expect(screen.getByText('blocks type')).toBeInTheDocument();
-    expect(screen.getByText(/A line of text in a paragraph./));
   });
 
   it('should render blocks with error', () => {
     setup({ error: 'field is required' });
 
     expect(screen.getByText(/field is required/));
+  });
+
+  it('should render blocks with data', () => {
+    setup();
+
+    expect(screen.getByText('This is bold text').parentElement).toHaveStyle({
+      'font-weight': 600,
+    });
+
+    expect(screen.getByText('This is deleted text').parentElement).toHaveStyle({
+      'text-decoration': 'line-through',
+    });
+
+    expect(screen.getByText('click me').parentElement).toHaveStyle({
+      'font-style': 'italic',
+    });
+
+    const linkElement = screen.getByRole('link', { name: /click me/ });
+    expect(linkElement).toHaveAttribute('href', 'https://example.com');
+
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
   });
 });
