082cde58740bd1058fc5b2b333f6d29db0364f6a
Bassel <basselkanso82@gmail.com>
2023-09-13 15:41:16 +0300

    update controllers

--- packages/core/typings/src/types/core/plugins/config/strapi-server/controllers.ts
+++ packages/core/typings/src/types/core/plugins/config/strapi-server/controllers.ts
@@ -1,5 +1,12 @@
 import type { Context } from 'koa';
+import { Strapi } from '../../../../..';
+
+export type ControllerReturnType = {
+  [key: string]: (ctx: Context) => Promise<unknown> | unknown;
+};
+
+export type Controller = ({ strapi }: { strapi: Strapi }) => ControllerReturnType;
 
 export interface Controllers {
-  [key: string]: (ctx: Context) => Promise<unknown> | unknown;
+  [key: string]: Controller;
 }
