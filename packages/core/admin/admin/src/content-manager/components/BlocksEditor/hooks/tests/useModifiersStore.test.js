2f5c6541e8a96604cf270fabb255316f68de9cc8
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-09-17 23:21:22 +0200

    Add tests for modifier renderers

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/hooks/tests/useModifiersStore.test.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/hooks/tests/useModifiersStore.test.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
-import { renderHook } from '@testing-library/react';
+import { lightTheme, ThemeProvider } from '@strapi/design-system';
+import { render, renderHook, screen } from '@testing-library/react';
 import PropTypes from 'prop-types';
+import { IntlProvider } from 'react-intl';
 import { createEditor } from 'slate';
 import { Slate, withReact } from 'slate-react';
 
@@ -18,9 +20,13 @@ const Wrapper = ({ children }) => {
   const editor = React.useMemo(() => withReact(createEditor()), []);
 
   return (
-    <Slate initialValue={initialValue} editor={editor}>
-      {children}
-    </Slate>
+    <ThemeProvider theme={lightTheme}>
+      <IntlProvider messages={{}} locale="en">
+        <Slate initialValue={initialValue} editor={editor}>
+          {children}
+        </Slate>
+      </IntlProvider>
+    </ThemeProvider>
   );
 };
 
@@ -48,5 +54,47 @@ describe('useModifiersStore', () => {
       expect(modifier).toHaveProperty('handleToggle');
       expect(modifier).toHaveProperty('renderLeaf');
     });
+  });
+
+  it('should render a bold modifier properly', () => {
+    const { result } = renderHook(useModifiersStore, { wrapper: Wrapper });
+
+    render(result.current.bold.renderLeaf('This is bold text'), { wrapper: Wrapper });
+    const boldText = screen.getByText('This is bold text');
+    expect(boldText).toHaveStyle('font-weight: 600');
+  });
+
+  it('should render an italic modifier properly', () => {
+    const { result } = renderHook(useModifiersStore, { wrapper: Wrapper });
+
+    render(result.current.italic.renderLeaf('This is italic text'), { wrapper: Wrapper });
+    const italicText = screen.getByText('This is italic text');
+    expect(italicText).toHaveStyle('font-style: italic');
+  });
+
+  it('should render an underline modifier properly', () => {
+    const { result } = renderHook(useModifiersStore, { wrapper: Wrapper });
+
+    render(result.current.underline.renderLeaf('This is underlined text'), { wrapper: Wrapper });
+    const underlineText = screen.getByText('This is underlined text');
+    expect(underlineText).toHaveStyle('text-decoration: underline');
+  });
+
+  it('should render a strikethrough modifier properly', () => {
+    const { result } = renderHook(useModifiersStore, { wrapper: Wrapper });
+
+    render(result.current.strikethrough.renderLeaf('This is strikethrough text'), {
+      wrapper: Wrapper,
+    });
+    const strikethroughText = screen.getByText('This is strikethrough text');
+    expect(strikethroughText).toHaveStyle('text-decoration: line-through');
+  });
+
+  it('should render a code modifier properly', () => {
+    const { result } = renderHook(useModifiersStore, { wrapper: Wrapper });
+
+    render(result.current.code.renderLeaf('This is code text'), { wrapper: Wrapper });
+    const codeText = screen.getByText('This is code text');
+    expect(window.getComputedStyle(codeText).fontFamily).toMatch(/\bmonospace\b/i);
   });
 });
