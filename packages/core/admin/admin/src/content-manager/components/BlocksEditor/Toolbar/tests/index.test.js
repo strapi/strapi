1ba939d27fcb0e0711452cca40a55f76bab3c28e
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2023-09-18 15:43:59 +0200

    type:text added in block data

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/Toolbar/tests/index.test.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/Toolbar/tests/index.test.js
@@ -13,7 +13,12 @@ import { BlocksToolbar } from '..';
 const initialValue = [
   {
     type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
+    children: [
+      {
+        type: 'text',
+        text: 'A line of text in a paragraph.',
+      },
+    ],
   },
 ];
 
@@ -77,14 +82,17 @@ describe('BlocksEditor toolbar', () => {
         type: 'paragraph',
         children: [
           {
+            type: 'text',
             text: 'A ',
           },
           {
+            type: 'text',
             text: 'line of text',
             bold: true,
             italic: true,
           },
           {
+            type: 'text',
             text: ' in a paragraph.',
           },
         ],
@@ -134,6 +142,7 @@ describe('BlocksEditor toolbar', () => {
             type: 'list-item',
             children: [
               {
+                type: 'text',
                 text: 'A line of text in a paragraph.',
               },
             ],
@@ -161,6 +170,7 @@ describe('BlocksEditor toolbar', () => {
         level: 1,
         children: [
           {
+            type: 'text',
             text: 'A line of text in a paragraph.',
           },
         ],
@@ -176,6 +186,7 @@ describe('BlocksEditor toolbar', () => {
         type: 'paragraph',
         children: [
           {
+            type: 'text',
             text: 'A line of text in a paragraph.',
           },
         ],
@@ -201,6 +212,7 @@ describe('BlocksEditor toolbar', () => {
         type: 'quote',
         children: [
           {
+            type: 'text',
             text: 'A line of text in a paragraph.',
           },
         ],
@@ -216,6 +228,7 @@ describe('BlocksEditor toolbar', () => {
         type: 'paragraph',
         children: [
           {
+            type: 'text',
             text: 'A line of text in a paragraph.',
           },
         ],
