6c82851aa1d4bdbfc5156ee560bb945759b3c687
Jamie Howard <jamie.howard@strapi.io>
2023-09-21 16:13:49 +0100

    feat(email): define shared types for email package

---
+++ packages/core/email/shared/types.ts
@@ -0,0 +1,41 @@
+export interface ApiResponse<T> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: Record<string, string>;
+  config: {
+    transitional: {
+      silentJSONParsing: boolean;
+      forcedJSONParsing: boolean;
+      clarifyTimeoutError: boolean;
+    };
+    adapter: string;
+    transformRequest: any[];
+    transformResponse: any[];
+    timeout: number;
+    xsrfCookieName: string;
+    xsrfHeaderName: string;
+    maxContentLength: number;
+    maxBodyLength: number;
+    env: Record<string, any>;
+    headers: Record<string, string>;
+    paramsSerializer: Record<string, any>;
+    baseURL: string;
+    signal: any;
+    method: string;
+    url: string;
+  };
+  request: any;
+}
+
+export interface EmailSettings {
+  config: ConfigSettings;
+}
+
+export interface ConfigSettings {
+  provider: string;
+  settings: {
+    defaultFrom: string;
+    defaultReplyTo: string;
+  };
+}
