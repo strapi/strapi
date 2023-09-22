f11f5ec7dc57da50440f9d1eeff64142e0710e62
Ben Irvin <ben@innerdvations.com>
2023-09-22 17:21:11 +0200

    fix imports for lint

--- packages/core/types/src/types/core/index.ts
+++ packages/core/types/src/types/core/index.ts
@@ -1,9 +1,20 @@
+/**
+ * Note: linting will erroneously raise errors if a namespaced type has the same name as on exported by Strapi
+ * To work around it, use a pattern such as:
+ *
+ * import * as ConfigImport from './config'
+ * export type Config = typeof ConfigImport
+ *
+ * which causes linting to see the namespaces correctly.
+ */
+import * as ConfigImport from './config';
+
+export type Config = typeof ConfigImport;
+
 export * as Attribute from './attributes';
 export * as Schema from './schemas';
-export * as Config from './config';
 export * as Plugin from './plugins';
 export * from './strapi';
-
 export * as Common from './common';
 export * as Namespace from './namespace';
 export * as UID from './uid';
