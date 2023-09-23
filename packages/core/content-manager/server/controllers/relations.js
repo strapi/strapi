47df2ac24dcb9c203e4e51f2ff476e52ad83fa62
PatrickJira <udom726@gmail.com>
2023-09-23 16:24:32 +0700

    remove template literals

--- packages/core/content-manager/server/controllers/relations.js
+++ packages/core/content-manager/server/controllers/relations.js
@@ -140,8 +140,8 @@ module.exports = {
 
     // searching should be allowed only on mainField for permission reasons
     if (_q) {
-      const _filter = isOperatorOfType('where', `${query._filter}`) ? query._filter : '$containsi';
-      addFiltersClause(queryParams, { [mainField]: { [`${_filter}`]: _q } });
+      const _filter = isOperatorOfType('where', query._filter) ? query._filter : '$containsi';
+      addFiltersClause(queryParams, { [mainField]: { [_filter]: _q } });
     }
 
     if (entityId) {
