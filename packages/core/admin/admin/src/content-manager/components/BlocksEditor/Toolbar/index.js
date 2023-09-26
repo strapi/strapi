e9b702601de8a69728d21c02bae7d3d021962877
Simone Taeggi <startae14@gmail.com>
2023-09-26 09:05:08 +0200

    fix multiblocks selection

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/Toolbar/index.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/Toolbar/index.js
@@ -229,6 +229,16 @@ export const BlocksDropdown = () => {
     }
   };
 
+  React.useEffect(() => {
+    const selectedBlocks = Object.keys(blocks).filter((key) => {
+      return isBlockActive(editor, blocks[key].matchNode);
+    });
+
+    if (selectedBlocks.length === 1) {
+      setBlockSelected(selectedBlocks[0]);
+    }
+  }, [blocks, editor, editor.selection]);
+
   return (
     <>
       <Select
@@ -247,8 +257,6 @@ export const BlocksDropdown = () => {
             value={key}
             label={blocks[key].label}
             icon={blocks[key].icon}
-            matchNode={blocks[key].matchNode}
-            handleSelection={setBlockSelected}
             blockSelected={blockSelected}
           />
         ))}
@@ -258,18 +266,10 @@ export const BlocksDropdown = () => {
   );
 };
 
-const BlockOption = ({ value, icon, label, handleSelection, blockSelected, matchNode }) => {
+const BlockOption = ({ value, icon, label, blockSelected }) => {
   const { formatMessage } = useIntl();
-  const editor = useSlate();
 
-  const isActive = isBlockActive(editor, matchNode);
   const isSelected = value === blockSelected;
-
-  React.useEffect(() => {
-    if (isActive && !isSelected) {
-      handleSelection(value);
-    }
-  }, [handleSelection, isActive, isSelected, value]);
 
   return (
     <Option
@@ -288,8 +288,6 @@ BlockOption.propTypes = {
     id: PropTypes.string.isRequired,
     defaultMessage: PropTypes.string.isRequired,
   }).isRequired,
-  matchNode: PropTypes.func.isRequired,
-  handleSelection: PropTypes.func.isRequired,
   blockSelected: PropTypes.string.isRequired,
 };
 
