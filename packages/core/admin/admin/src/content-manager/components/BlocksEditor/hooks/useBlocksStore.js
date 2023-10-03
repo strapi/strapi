d77366d3ae870967fc15a2e51e9299f175167667
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2023-10-03 14:27:25 +0200

    fixed backspace key for empty list

--- packages/core/admin/admin/src/content-manager/components/BlocksEditor/hooks/useBlocksStore.js
+++ packages/core/admin/admin/src/content-manager/components/BlocksEditor/hooks/useBlocksStore.js
@@ -201,6 +201,39 @@ const handleEnterKeyOnList = (editor) => {
 };
 
 /**
+ * Common handler for the backspace event on ordered and unordered lists
+ * @param {import('slate').Editor} editor
+ * @param {Event} event
+ */
+const handleBackspaceKeyOnList = (editor, event) => {
+  const [currentListItem, currentListItemPath] = Editor.parent(editor, editor.selection.anchor);
+
+  const [currentList, currentListPath] = Editor.parent(editor, currentListItemPath);
+
+  const isListEmpty = currentList.children.length === 1 && currentListItem.children[0].text === '';
+
+  if (isListEmpty) {
+    event.preventDefault();
+    // Delete the empty list
+    Transforms.removeNodes(editor, { at: currentListPath });
+
+    if (currentListPath[0] === 0) {
+      // If the list was the only(or first) block element then insert empty paragraph as editor needs default value
+      Transforms.insertNodes(
+        editor,
+        {
+          type: 'paragraph',
+          children: [{ type: 'text', text: '' }],
+        },
+        { at: currentListPath }
+      );
+
+      Transforms.select(editor, currentListPath);
+    }
+  }
+};
+
+/**
  * Manages a store of all the available blocks.
  *
  * @returns {{
@@ -212,6 +245,7 @@ const handleEnterKeyOnList = (editor) => {
  *     matchNode: (node: Object) => boolean,
  *     isInBlocksSelector: true,
  *     handleEnterKey: (editor: import('slate').Editor) => void,
+ *     handleBackspaceKey?:(editor: import('slate').Editor, event: Event) => void,
  *   }
  * }} an object containing rendering functions and metadata for different blocks, indexed by name.
  */
@@ -434,6 +468,7 @@ export function useBlocksStore() {
       // TODO add icon and label and set isInBlocksEditor to true
       isInBlocksSelector: false,
       handleEnterKey: handleEnterKeyOnList,
+      handleBackspaceKey: handleBackspaceKeyOnList,
     },
     'list-unordered': {
       renderElement: (props) => <List {...props} />,
@@ -445,6 +480,7 @@ export function useBlocksStore() {
       // TODO add icon and label and set isInBlocksEditor to true
       isInBlocksSelector: false,
       handleEnterKey: handleEnterKeyOnList,
+      handleBackspaceKey: handleBackspaceKeyOnList,
     },
     'list-item': {
       renderElement: (props) => (
