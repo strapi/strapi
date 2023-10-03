6dc8e605f40172ed8177748d1a64aee6d3ca9233
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-10-03 09:55:42 +0100

    test(upload): fix useModalQueryParams

--- packages/core/upload/admin/tests/server.js
+++ packages/core/upload/admin/tests/server.js
@@ -3,28 +3,19 @@ import { setupServer } from 'msw/node';
 
 export const server = setupServer(
   ...[
-    rest.post('/admin/permissions/check', async (req, res, ctx) => {
-      return res(
-        ctx.json({
-          data: [true],
-        })
-      );
-    }),
-    rest.get('/use-fetch-client-test', (req, res, ctx) => {
+    rest.get('/upload/configuration', async (req, res, ctx) => {
       return res(
         ctx.json({
           data: {
-            results: [
-              { id: 2, name: 'newest', publishedAt: null },
-              { id: 1, name: 'oldest', publishedAt: null },
-            ],
-            pagination: { page: 1, pageCount: 10 },
+            /**
+             * we send the pageSize slightly different to defaults because
+             * in tests we can track that the async functions have finished.
+             */
+            pageSize: 20,
+            sort: 'updatedAt:DESC',
           },
         })
       );
-    }),
-    rest.get('/test-fetch-client', (req, res, ctx) => {
-      return res(ctx.status(200));
     }),
   ]
 );
