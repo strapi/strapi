9088270eada420ebb0626c17242e959ce1e59d87
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-10-03 14:21:16 +0100

    test(upload): fix UploadAssetDialog tests

--- packages/core/upload/admin/tests/server.js
+++ packages/core/upload/admin/tests/server.js
@@ -151,5 +151,17 @@ export const server = setupServer(
         })
       );
     }),
+
+    rest.get('*/an-image.png', (req, res, ctx) =>
+      res(ctx.set('Content-Type', 'image/png'), ctx.body())
+    ),
+    rest.get('*/a-pdf.pdf', (req, res, ctx) =>
+      res(ctx.set('Content-Type', 'application/pdf'), ctx.body())
+    ),
+    rest.get('*/a-video.mp4', (req, res, ctx) =>
+      res(ctx.set('Content-Type', 'video/mp4'), ctx.body())
+    ),
+    rest.get('*/not-working-like-cors.lutin', (req, res, ctx) => res(ctx.json({}))),
+    rest.get('*/some-where-not-existing.jpg', (req, res) => res.networkError('Failed to fetch')),
   ]
 );
