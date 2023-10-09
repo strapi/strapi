db265add76b50246dab5a754ee0e276b1437300a
Ben Irvin <ben@innerdvations.com>
2023-10-09 13:17:56 +0200

    add back accidentally deleted entity

--- packages/core/types/src/types/core/index.ts
+++ packages/core/types/src/types/core/index.ts
@@ -2,6 +2,7 @@ export type * as Config from './config';
 export type * as Attribute from './attributes';
 export type * as Schema from './schemas';
 export type * as Plugin from './plugins';
+export type * as Entity from './entity';
 export type * from './strapi';
 export type * as Common from './common';
 export type * as Namespace from './namespace';
