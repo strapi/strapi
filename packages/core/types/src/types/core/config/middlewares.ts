0ac69bc6ad9404cd90212d19b99a0b241c096704
Ben Irvin <ben@innerdvations.com>
2023-10-09 10:58:45 +0200

    add middleware handler

--- packages/core/types/src/types/core/config/middlewares.ts
+++ packages/core/types/src/types/core/config/middlewares.ts
@@ -1,3 +1,3 @@
-import type { MiddlewareConfig, MiddlewareName } from '../common';
+import type { MiddlewareConfig, MiddlewareHandler, MiddlewareName } from '../common';
 
-export type Middlewares = Array<MiddlewareName | MiddlewareConfig>;
+export type Middlewares = Array<MiddlewareName | MiddlewareConfig | MiddlewareHandler>;
