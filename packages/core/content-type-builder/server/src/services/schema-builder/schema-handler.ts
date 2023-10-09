a93977de6c25898a0f5bf4864aded6f9f93095d9
Ben Irvin <ben@innerdvations.com>
2023-10-09 09:44:43 +0200

    fix unmerged file

--- packages/core/content-type-builder/server/src/services/schema-builder/schema-handler.ts
+++ packages/core/content-type-builder/server/src/services/schema-builder/schema-handler.ts
@@ -1,9 +1,5 @@
 import path from 'path';
-<<<<<<< Updated upstream
 import type { UID, Schema } from '@strapi/types';
-=======
-import type { UID, Schema, Attribute } from '@strapi/types';
->>>>>>> Stashed changes
 import fse from 'fs-extra';
 import _ from 'lodash';
 
@@ -49,6 +45,7 @@ export default function createSchemaHandler(infos: Infos) {
     get plugin() {
       return initialState.plugin;
     },
+
     get category() {
       return initialState.category;
     },
@@ -90,24 +87,15 @@ export default function createSchemaHandler(infos: Infos) {
       return this;
     },
 
-<<<<<<< Updated upstream
     // get a particular path inside the schema
-=======
-    // get a particuar path inside the schema
->>>>>>> Stashed changes
     get(path: string[]) {
       return _.get(state.schema, path);
     },
 
-<<<<<<< Updated upstream
     // set a particular path inside the schema
     set(path: string[] | string, val: unknown) {
       if (!state.schema) return this;
 
-=======
-    // set a particuar path inside the schema
-    set(path: string[] | string, val: unknown) {
->>>>>>> Stashed changes
       modified = true;
 
       const value = _.defaultTo(val, _.get(state.schema, path));
@@ -116,11 +104,7 @@ export default function createSchemaHandler(infos: Infos) {
       return this;
     },
 
-<<<<<<< Updated upstream
     // delete a particular path inside the schema
-=======
-    // delete a particuar path inside the schema
->>>>>>> Stashed changes
     unset(path: string[]) {
       modified = true;
 
@@ -138,11 +122,7 @@ export default function createSchemaHandler(infos: Infos) {
       return this.get(['attributes', key]);
     },
 
-<<<<<<< Updated upstream
     setAttribute(key: string, attribute: any) {
-=======
-    setAttribute(key: string, attribute: Attribute.Any) {
->>>>>>> Stashed changes
       return this.set(['attributes', key], attribute);
     },
 
@@ -151,11 +131,7 @@ export default function createSchemaHandler(infos: Infos) {
     },
 
     setAttributes(newAttributes: Schema.Attributes) {
-<<<<<<< Updated upstream
       if (!this.schema) return this;
-
-=======
->>>>>>> Stashed changes
       // delete old configurable attributes
       for (const key in this.schema.attributes) {
         if (isConfigurable(this.schema.attributes[key])) {
@@ -172,13 +148,9 @@ export default function createSchemaHandler(infos: Infos) {
     },
 
     removeContentType(uid: UID.ContentType) {
-<<<<<<< Updated upstream
       if (!state.schema) return this;
 
       const attributes = state.schema.attributes as Record<string, any>;
-=======
-      const { attributes } = state.schema;
->>>>>>> Stashed changes
 
       Object.keys(attributes).forEach((key) => {
         const attribute = attributes[key];
@@ -193,13 +165,9 @@ export default function createSchemaHandler(infos: Infos) {
 
     // utils
     removeComponent(uid: UID.Component) {
-<<<<<<< Updated upstream
       if (!state.schema) return this;
 
       const attributes = state.schema.attributes as Record<string, any>;
-=======
-      const { attributes } = state.schema;
->>>>>>> Stashed changes
 
       Object.keys(attributes).forEach((key) => {
         const attr = attributes[key];
@@ -224,13 +192,9 @@ export default function createSchemaHandler(infos: Infos) {
     },
 
     updateComponent(uid: UID.Component, newUID: UID.Component) {
-<<<<<<< Updated upstream
       if (!state.schema) return this;
 
       const attributes = state.schema.attributes as Record<string, any>;
-=======
-      const { attributes } = state.schema;
->>>>>>> Stashed changes
 
       Object.keys(attributes).forEach((key) => {
         const attr = attributes[key];
@@ -348,8 +312,4 @@ export default function createSchemaHandler(infos: Infos) {
       return Promise.resolve();
     },
   };
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
