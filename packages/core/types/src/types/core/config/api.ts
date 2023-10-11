99df3468b7098bde195bfef2b6bb844d7dd3e03f
Christian Capeans <christiancp100@gmail.com>
2023-10-11 09:10:17 +0200

    Make rest props optional

--- packages/core/types/src/types/core/config/api.ts
+++ packages/core/types/src/types/core/config/api.ts
@@ -3,11 +3,11 @@ export interface ResponsesProp {
 }
 
 export interface RestProp {
-  prefix: string;
+  prefix?: string;
   port?: number;
-  defaultLimit: number;
-  maxLimit: number;
-  withCount: boolean;
+  defaultLimit?: number;
+  maxLimit?: number;
+  withCount?: boolean;
 }
 
 export interface Api {
