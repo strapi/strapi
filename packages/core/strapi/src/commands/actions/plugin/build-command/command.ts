846bad5941fe5326337908212924ea87eda94524
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-10-16 16:22:15 +0100

    refactor(strapi): plugin:build now uses pack-up underneath (#18453)
    * refactor(strapi): plugin:build now uses pack-up underneath
    
    * chore(email): use tsconfig in bundles for packup
    
    * chore: fix ts check

--- packages/core/strapi/src/commands/actions/plugin/build-command/command.ts
+++ packages/core/strapi/src/commands/actions/plugin/build-command/command.ts
@@ -11,6 +11,9 @@ const command: StrapiCommand = ({ command }) => {
     .description('Bundle your strapi plugin for publishing.')
     .addOption(forceOption)
     .option('-d, --debug', 'Enable debugging mode with verbose logs', false)
+    .option('--silent', "Don't log anything", false)
+    .option('--sourcemap', 'produce sourcemaps', false)
+    .option('--minify', 'minify the output', false)
     .action(getLocalScript('plugin/build-command'));
 };
 
