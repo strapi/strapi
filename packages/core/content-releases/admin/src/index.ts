42614f02b672679a32990d1500f794b262eb5bc6
markkaylor <mark.kaylor@strapi.io>
2023-10-17 12:12:08 +0200

    chore(releases): setup releases plugin (#18401)

---
+++ packages/core/content-releases/admin/src/index.ts
@@ -0,0 +1,27 @@
+import { prefixPluginTranslations } from '@strapi/helper-plugin';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  register() {},
+  async registerTrads({ locales }: { locales: string[] }) {
+    const importedTrads = await Promise.all(
+      locales.map((locale) => {
+        return import(`./translations/${locale}.json`)
+          .then(({ default: data }) => {
+            return {
+              data: prefixPluginTranslations(data, 'releases'),
+              locale,
+            };
+          })
+          .catch(() => {
+            return {
+              data: {},
+              locale,
+            };
+          });
+      })
+    );
+
+    return Promise.resolve(importedTrads);
+  },
+};
