ac49250424552716298d58b9e3661b32bbba5d50
DevDengChao <2325690622@qq.com>
2023-10-17 10:19:30 +0800

    refactor: migrate migrations.test.js to migrations.test.ts
    Signed-off-by: DevDengChao <2325690622@qq.com>

---
+++ packages/core/database/src/__tests__/migrations.test.ts
@@ -0,0 +1,63 @@
+import {findMigrationsDir} from '../migrations';
+import os from "os";
+import fs from "fs-extra";
+import path from "path";
+
+let playground = `${os.tmpdir()}/@strapi-playground/core/database/migrations/`;
+beforeAll(async () => {
+  fs.removeSync(playground);
+  fs.ensureDirSync(playground);
+})
+
+describe('js project', () => {
+  let projectDir = `${playground}/js-project`;
+  beforeEach(async () => {
+    fs.removeSync(projectDir);
+    fs.ensureDirSync(projectDir);
+    global.strapi = {
+      dirs: {
+        app: {
+          root: projectDir
+        }
+      }
+    }
+  })
+
+  it("should find migration dir under root", async () => {
+    let migrationDir = await findMigrationsDir(strapi.dirs.app.root);
+    expect(path.resolve(migrationDir)).toBe(path.resolve(`${projectDir}/database/migrations`));
+  })
+});
+
+describe('ts project', () => {
+  let projectDir = `${playground}/ts-project`;
+
+  beforeEach(async () => {
+    fs.removeSync(projectDir);
+    fs.ensureDirSync(projectDir);
+    fs.writeFileSync(`${projectDir}/index.ts`, "export const hello = 'world';")
+    fs.writeFileSync(`${projectDir}/tsconfig.json`,
+      // language=JSON
+      `{
+        "compilerOptions": {
+          "outDir": "dist",
+          "rootDir": "."
+        },
+        "include": [
+          "./"
+        ]
+      }`)
+    global.strapi = {
+      dirs: {
+        app: {
+          root: projectDir
+        }
+      }
+    }
+  })
+
+  it("should find migration dir under root/dist", async () => {
+    let migrationDir = await findMigrationsDir(strapi.dirs.app.root);
+    expect(path.resolve(migrationDir)).toBe(path.resolve(`${projectDir}/dist/database/migrations`));
+  })
+});
