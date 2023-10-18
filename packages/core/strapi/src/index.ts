f1b8037bd449748331ef2c7ea83c3b84514b54f2
Alexandre Bodin <bodin.alex@gmail.com>
2023-10-18 13:44:16 +0200

    Hardcode hacky export to all named and default exports to live together to maintain backward compatibility

--- packages/core/strapi/src/index.ts
+++ packages/core/strapi/src/index.ts
@@ -1,5 +1,13 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable node/exports-style */
+/* eslint-disable import/no-import-module-exports */
 import strapiFactory from './Strapi';
 
 export type * from '@strapi/types';
 
+export * as factories from './factories';
+
 export default strapiFactory;
+
+// TODO: drop in v5
+module.exports = exports = strapiFactory;
