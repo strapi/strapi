e7841d8f7db0c13c66d94c4bb0a74584c6e143ce
Ben Irvin <ben@innerdvations.com>
2023-10-19 12:35:30 +0200

    fix: add client replacement map

--- packages/core/database/src/connection.ts
+++ packages/core/database/src/connection.ts
@@ -1,8 +1,22 @@
 import knex from 'knex';
 import type { Knex } from 'knex';
 
+const clientMap = {
+  sqlite: 'better-sqlite3',
+  mysql: 'mysql',
+  postgres: 'pg',
+};
+
+function isClientValid(config: { client?: unknown }): config is { client: keyof typeof clientMap } {
+  return Object.keys(clientMap).includes(config.client as string);
+}
+
 export const createConnection = (config: Knex.Config) => {
-  const knexConfig = { ...config };
+  if (!isClientValid(config)) {
+    throw new Error(`Unsupported database client ${config.client}`);
+  }
+
+  const knexConfig = { ...config, client: (clientMap as any)[config.client] };
 
   return knex(knexConfig);
 };
