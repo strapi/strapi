bf1f4c4de8e6107b328eca85255c3fc46cbc632f
summayya2908 <summayya@tantiv4.com>
2023-10-21 22:37:42 +0530

    fix: add a undefined check for datetime picker

--- packages/core/helper-plugin/src/components/GenericInput.tsx
+++ packages/core/helper-plugin/src/components/GenericInput.tsx
@@ -312,7 +312,7 @@ const GenericInput = ({
           onClear={() => onChange({ target: { name, value: null, type } })}
           placeholder={formattedPlaceholder}
           required={required}
-          value={value}
+          value={value? value : undefined}
         />
       );
     }
