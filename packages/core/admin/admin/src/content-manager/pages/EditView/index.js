814d042b40dc1b4a3b183944b3b60077b14f6e58
Gustav Hansen <gustav.hansen@strapi.io>
2023-10-23 10:42:34 +0200

    chore(admin): convert InjectionZone to TS

--- packages/core/admin/admin/src/content-manager/pages/EditView/index.js
+++ packages/core/admin/admin/src/content-manager/pages/EditView/index.js
@@ -16,7 +16,7 @@ import { useLocation } from 'react-router-dom';
 
 import { useEnterprise } from '../../../hooks/useEnterprise';
 import { selectAdminPermissions } from '../../../pages/App/selectors';
-import { InjectionZone } from '../../../shared/components';
+import { InjectionZone } from '../../../shared/components/InjectionZone';
 import CollectionTypeFormWrapper from '../../components/CollectionTypeFormWrapper';
 import { DynamicZone } from '../../components/DynamicZone';
 import EditViewDataManagerProvider from '../../components/EditViewDataManagerProvider';
