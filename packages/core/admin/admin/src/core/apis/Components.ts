735b11891eb7d13b8287fc3a8cd6e558756417ba
Gustav Hansen <gustav.hansen@strapi.io>
2023-10-24 15:18:52 +0200

    chore(admin): convert core/apis/components to TS

--- packages/core/admin/admin/src/core/apis/Components.ts
+++ packages/core/admin/admin/src/core/apis/Components.ts
@@ -1,11 +1,23 @@
+/* eslint-disable import/no-default-export */
+/* eslint-disable check-file/filename-naming-convention */
+
+import * as React from 'react';
+
 import invariant from 'invariant';
 
+interface TComponent {
+  name: string;
+  Component: React.ComponentType;
+}
+
 class Components {
+  components: Record<string, TComponent['Component']>;
+
   constructor() {
     this.components = {};
   }
 
-  add(component) {
+  add(component: TComponent) {
     const { name, Component } = component;
 
     invariant(Component, 'A Component must be provided');
