244dfb381468f2b6929164b3af0e20f8235abf68
Jamie Howard <jamie.howard@strapi.io>
2023-10-25 10:48:10 +0100

    feature(admin): type transfer/runner controller

--- packages/core/admin/server/src/controllers/transfer/runner.ts
+++ packages/core/admin/server/src/controllers/transfer/runner.ts
@@ -1,3 +1,5 @@
+import { Context } from 'koa';
+
 // @ts-ignore - TS Build sometimes fails on this import, probably because of the circular dependency with DT
 import { strapi as dataTransferStrapi } from '@strapi/data-transfer';
 import { errors } from '@strapi/utils';
@@ -12,10 +14,10 @@ const {
 const { UnauthorizedError } = errors;
 
 /**
- * @param {import('koa').Context} ctx
- * @param {string} [scope]
+ * @param ctx the koa context
+ * @param scope the scope to verify
  */
-const verify = async (ctx: any, scope: any) => {
+const verify = async (ctx: Context, scope?: dataTransferStrapi.remote.constants.TransferMethod) => {
   const { auth } = ctx.state;
 
   if (!auth) {
