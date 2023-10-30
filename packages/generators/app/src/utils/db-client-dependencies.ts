8f471f6b2705f18a2d054d9b98ac1478fbee15a5
Ben Irvin <ben@innerdvations.com>
2023-10-30 12:29:45 +0100

    chore: update version to 3.6.2

--- packages/generators/app/src/utils/db-client-dependencies.ts
+++ packages/generators/app/src/utils/db-client-dependencies.ts
@@ -1,7 +1,7 @@
 import type { ClientName } from '../types';
 
 const sqlClientModule = {
-  mysql: { mysql2: '3.6.0' },
+  mysql: { mysql2: '3.6.2' },
   postgres: { pg: '8.8.0' },
   sqlite: { 'better-sqlite3': '9.0.0' },
 };
