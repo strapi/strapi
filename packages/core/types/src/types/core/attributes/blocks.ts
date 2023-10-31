c7273b0322e9f24f02b1cda18cd34d79a918ee43
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-10-31 12:33:57 +0100

    chore: add precise types to blocks attribute

--- packages/core/types/src/types/core/attributes/blocks.ts
+++ packages/core/types/src/types/core/attributes/blocks.ts
@@ -8,6 +8,75 @@ export type Blocks = Attribute.OfType<'blocks'> &
   Attribute.WritableOption &
   Attribute.VisibleOption;
 
-export type BlocksValue<T extends object = object> = T;
+interface TextInlineNode {
+  type: 'text';
+  text: string;
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  strikethrough?: boolean;
+  code?: boolean;
+}
+
+interface LinkInlineNode {
+  type: 'link';
+  url: string;
+  children: TextInlineNode[];
+}
+
+interface ListItemInlineNode {
+  type: 'list-item';
+  children: DefaultInlineNode[];
+}
+
+type DefaultInlineNode = TextInlineNode | LinkInlineNode;
+
+interface ParagraphBlockNode {
+  type: 'paragraph';
+  children: DefaultInlineNode[];
+}
+
+interface QuoteBlockNode {
+  type: 'quote';
+  children: DefaultInlineNode[];
+}
+
+interface CodeBlockNode {
+  type: 'code';
+  children: DefaultInlineNode[];
+}
+
+interface HeadingBlockNode {
+  type: 'heading';
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  children: DefaultInlineNode[];
+}
+
+interface ListBlockNode {
+  type: 'list';
+  format: 'ordered' | 'unordered';
+  children: (ListItemInlineNode | ListBlockNode)[];
+}
+
+interface ImageBlockNode {
+  type: 'image';
+  image: Attribute.GetValue<{
+    type: 'media';
+    allowedTypes: ['images'];
+    multiple: false;
+  }>;
+  children: [{ type: 'text'; text: '' }];
+}
+
+// Block node types
+type RootNode =
+  | ParagraphBlockNode
+  | QuoteBlockNode
+  | CodeBlockNode
+  | HeadingBlockNode
+  | ListBlockNode
+  | ImageBlockNode;
+
+export type BlocksValue = RootNode[];
 
 export type GetBlocksValue<T extends Attribute.Attribute> = T extends Blocks ? BlocksValue : never;
