95f7ba816d4e1f9a72cbbe741c8197acd9f0264b
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-10-31 08:15:45 +0000

    chore: pack-up all admin packages

--- packages/core/admin/ee/admin/src/hooks/tests/useLicenseLimitNotification.test.tsx
+++ packages/core/admin/ee/admin/src/hooks/tests/useLicenseLimitNotification.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
+/* eslint-disable check-file/filename-naming-convention */
 import { renderHook } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 
 import { useLicenseLimitNotification } from '../useLicenseLimitNotification';
+// @ts-expect-error - no types, yet.
 import { useLicenseLimits } from '../useLicenseLimits';
 
 const baseLicenseInfo = {
@@ -40,10 +40,10 @@ jest.mock('../../useLicenseLimits', () => ({
   })),
 }));
 
-const setup = (...args) =>
-  renderHook(() => useLicenseLimitNotification(...args), {
+const setup = () =>
+  renderHook(() => useLicenseLimitNotification(), {
     wrapper({ children }) {
-      return <IntlProvider>{children}</IntlProvider>;
+      return <IntlProvider locale="en">{children}</IntlProvider>;
     },
   });
 
