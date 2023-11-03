75a667a00f3437a44b908f86a24f315366a29d61
Ben Irvin <ben@innerdvations.com>
2023-11-03 15:59:57 +0100

    fix: use actual fetch typings

--- packages/core/strapi/src/utils/fetch.ts
+++ packages/core/strapi/src/utils/fetch.ts
@@ -1,29 +1,20 @@
 import { HttpsProxyAgent, HttpsProxyAgentOptions } from 'https-proxy-agent';
 
-import type { Strapi } from '@strapi/types';
-
-export interface Fetch {
-  (url: string, options: RequestInit): Promise<Response>;
-  dispatcher?: HttpsProxyAgent;
-}
+import type { Strapi, Fetch } from '@strapi/types';
 
 // Create a wrapper for Node's Fetch API that applies a global proxy
 export function createStrapiFetch(strapi: Strapi): Fetch {
-  function strapiFetch(url: string, options: RequestInit = {}) {
-    const fetchOptions = {
-      ...(strapiFetch.dispatcher ? { dispatcher: strapiFetch.dispatcher } : {}),
-      ...options,
-    };
-    strapi.log.debug(`Fetch request for ${url}`);
-    return fetch(url, fetchOptions);
-  }
-
   const { globalProxy } = strapi.config.get<{
     globalProxy: HttpsProxyAgentOptions;
   }>('server');
 
-  if (globalProxy) {
-    strapiFetch.dispatcher = new HttpsProxyAgent(globalProxy);
+  function strapiFetch(url: RequestInfo | URL, options: RequestInit = {}) {
+    const fetchOptions = {
+      ...(globalProxy ? { dispatcher: new HttpsProxyAgent(globalProxy) } : {}),
+      ...options,
+    };
+    strapi.log.debug(`Making request for ${url}`);
+    return fetch(url, fetchOptions);
   }
 
   return strapiFetch;
