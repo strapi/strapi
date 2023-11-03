bcc2b94f387834b023f0ca9e0c1adb2b38c7209f
Ben Irvin <ben@innerdvations.com>
2023-11-03 16:38:10 +0100

    fix: fetch type

--- packages/core/types/src/modules/fetch.ts
+++ packages/core/types/src/modules/fetch.ts
@@ -2,7 +2,6 @@
  * strapi.fetch interface is currently an identical wrapper for Node fetch()
  * See createStrapiFetch in strapi/utils
  * However, we want to retain the ability to extend it in the future.
- * We will also keep Fetch as an interface to prevent a breaking change.
  * */
 
 export interface Fetch {
