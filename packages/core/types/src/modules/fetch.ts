76b59a286fd6b81f5bd529bf9f8b2048832e63f4
Ben Irvin <ben@innerdvations.com>
2023-11-03 16:37:30 +0100

    fix: fetch type

--- packages/core/types/src/modules/fetch.ts
+++ packages/core/types/src/modules/fetch.ts
@@ -2,7 +2,8 @@
  * strapi.fetch interface is currently an identical wrapper for Node fetch()
  * See createStrapiFetch in strapi/utils
  * However, we want to retain the ability to extend it in the future.
- * We will also keep Fetch as an interface to prevent a breaking change.
  * */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Fetch extends ReturnType<typeof fetch> {}
+
+export interface Fetch {
+  (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>;
+}
