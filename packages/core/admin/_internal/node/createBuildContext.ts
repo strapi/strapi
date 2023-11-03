0733074737251b3268e96ad9dd61a67c1b4e4d0d
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-11-03 13:47:34 +0000

    fix: some packages might only have a server entry and that's okay

--- packages/core/admin/_internal/node/createBuildContext.ts
+++ packages/core/admin/_internal/node/createBuildContext.ts
@@ -218,7 +218,11 @@ const filterPluginsByAdminEntry = (plugin: Plugin) => {
 
     return isLocalPluginWithLegacyAdminFile;
   } catch (err) {
-    if (isError(err) && 'code' in err && err.code === 'MODULE_NOT_FOUND') {
+    if (
+      isError(err) &&
+      'code' in err &&
+      (err.code === 'MODULE_NOT_FOUND' || err.code === 'ERR_PACKAGE_PATH_NOT_EXPORTED')
+    ) {
       /**
        * the plugin does not contain FE code, so we
        * don't want to import it anyway
