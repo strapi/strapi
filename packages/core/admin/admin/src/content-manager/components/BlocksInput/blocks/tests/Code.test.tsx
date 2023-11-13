de8928b89329cae7aec8e457854285af7583be6e
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2023-11-13 17:05:08 +0100

    test individual block logic

--- packages/core/admin/admin/src/content-manager/components/BlocksInput/blocks/tests/Code.test.tsx
+++ packages/core/admin/admin/src/content-manager/components/BlocksInput/blocks/tests/Code.test.tsx
@@ -3,6 +3,9 @@ import { render, screen, within } from '@testing-library/react';
 import { codeBlocks } from '../Code';
 
 import { Wrapper } from './Wrapper';
+import { createEditor } from 'slate';
+import { Transforms } from 'slate';
+import { Editor } from 'slate';
 
 describe('Code', () => {
   it('renders a code block properly', () => {
@@ -28,5 +31,42 @@ describe('Code', () => {
 
     const code = within(pre).getByText('Some code');
     expect(code).toBeInTheDocument();
+  });
+
+  it('handles enter key on code block', () => {
+    const baseEditor = createEditor();
+    baseEditor.children = [
+      {
+        type: 'code',
+        children: [
+          {
+            type: 'text',
+            text: 'Line of code',
+          },
+        ],
+      },
+    ];
+
+    // Set the cursor at the end of the fast list item
+    Transforms.select(baseEditor, {
+      anchor: Editor.end(baseEditor, []),
+      focus: Editor.end(baseEditor, []),
+    });
+
+    // Simulate the enter key
+    codeBlocks.code.handleEnterKey?.(baseEditor);
+
+    // Should insert a newline within the code block (shoudn't exit the code block)
+    expect(baseEditor.children).toEqual([
+      {
+        type: 'code',
+        children: [
+          {
+            type: 'text',
+            text: 'Line of code\n',
+          },
+        ],
+      },
+    ]);
   });
 });
--- packages/core/admin/admin/src/content-manager/components/BlocksInput/blocks/tests/Quote.test.tsx
+++ packages/core/admin/admin/src/content-manager/components/BlocksInput/blocks/tests/Quote.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { quoteBlocks } from '../Quote';
 
 import { Wrapper } from './Wrapper';
+import { Editor, Transforms, createEditor } from 'slate';
 
 describe('Quote', () => {
   it('renders a quote block properly', () => {
@@ -26,5 +27,101 @@ describe('Quote', () => {
     const quote = screen.getByRole('blockquote');
     expect(quote).toBeInTheDocument();
     expect(quote).toHaveTextContent('Some quote');
+  });
+
+  it('handles enter key on a quote', () => {
+    const baseEditor = createEditor();
+
+    baseEditor.children = [
+      {
+        type: 'quote',
+        children: [
+          {
+            type: 'text',
+            text: 'Some quote',
+          },
+        ],
+      },
+    ];
+
+    // Simulate enter key press at the end of the quote
+    Transforms.select(baseEditor, {
+      anchor: Editor.end(baseEditor, []),
+      focus: Editor.end(baseEditor, []),
+    });
+    quoteBlocks.quote.handleEnterKey?.(baseEditor);
+
+    // Should enter a line break within the quote
+    expect(baseEditor.children).toEqual([
+      {
+        type: 'quote',
+        children: [
+          {
+            type: 'text',
+            text: 'Some quote\n',
+          },
+        ],
+      },
+    ]);
+
+    // Simulate enter key press at the end of the quote again
+    Transforms.select(baseEditor, {
+      anchor: Editor.end(baseEditor, []),
+      focus: Editor.end(baseEditor, []),
+    });
+    quoteBlocks.quote.handleEnterKey?.(baseEditor);
+
+    // Should delete the line break and create a paragraph after the quote
+    expect(baseEditor.children).toEqual([
+      {
+        type: 'quote',
+        children: [
+          {
+            type: 'text',
+            text: 'Some quote',
+          },
+        ],
+      },
+      {
+        type: 'paragraph',
+        children: [
+          {
+            type: 'text',
+            text: '',
+          },
+        ],
+      },
+    ]);
+  });
+
+  it('disables modifiers when creating a new node with enter key at the end of a quote', () => {
+    const baseEditor = createEditor();
+    baseEditor.children = [
+      {
+        type: 'quote',
+        children: [
+          {
+            type: 'text',
+            text: 'Some quote',
+            bold: true,
+            italic: true,
+          },
+        ],
+      },
+    ];
+
+    Transforms.select(baseEditor, {
+      anchor: Editor.end(baseEditor, []),
+      focus: Editor.end(baseEditor, []),
+    });
+
+    // Bold and italic should be enabled
+    expect(Editor.marks(baseEditor)).toEqual({ bold: true, italic: true, type: 'text' });
+
+    // Simulate the enter key then user typing
+    quoteBlocks.quote.handleEnterKey?.(baseEditor);
+
+    // Once on the new line, bold and italic should be disabled
+    expect(Editor.marks(baseEditor)).toEqual({ type: 'text' });
   });
 });
