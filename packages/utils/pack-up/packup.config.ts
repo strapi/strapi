e421d78a55566481174c0f8e7922afff0d74d0b4
Ben Irvin <ben@innerdvations.com>
2023-11-14 11:51:26 +0100

    chore: revert externals

--- packages/utils/pack-up/packup.config.ts
+++ packages/utils/pack-up/packup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       require: './dist/cli.js',
     },
   ],
-  externals: ['node:module', 'node:path', 'fs-extra'],
+  externals: ['node:module'],
   runtime: 'node',
   minify: false,
   sourcemap: true,
