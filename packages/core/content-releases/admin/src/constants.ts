db8a058a9161e6e2644a4d21e7d6fd8f0e6ef64e
Fernando Chavez <fernando.chavez@strapi.io>
2023-11-15 17:01:24 +0100

    chore(content-releases): remove unused const

--- packages/core/content-releases/admin/src/constants.ts
+++ packages/core/content-releases/admin/src/constants.ts
@@ -12,22 +12,3 @@ export const PERMISSIONS = {
     },
   ],
 };
-
-export const MOCK_ALL_RELEASES_PERMISSIONS = [
-  {
-    id: 301,
-    action: 'plugin::content-releases.read',
-    subject: null,
-    properties: {},
-    conditions: [],
-    actionParameters: {},
-  },
-  {
-    id: 302,
-    action: 'plugin::content-releases.create',
-    subject: null,
-    properties: {},
-    conditions: [],
-    actionParameters: {},
-  },
-];
