c2cbb34425f5d58d88f9cf6d300ae9f3ddacb602
Convly <jean-sebastien.herbaux@epitech.eu>
2023-11-15 10:39:37 +0100

    Update types

--- packages/utils/upgrade/src/types.ts
+++ packages/utils/upgrade/src/types.ts
@@ -1,3 +1,5 @@
+import { Logger } from './core/logger';
+
 export namespace Version {
   export type SemVer = `${number}.${number}.${number}`;
   export type Latest = 'latest';
@@ -5,10 +7,45 @@ export namespace Version {
   export type Any = SemVer | Latest;
 }
 
+export interface VersionRange {
+  from: Version.SemVer;
+  to: Version.Any;
+}
+
 export interface CLIOptions {
   // TODO: Add back the version option when we handle targeting specific versions
   // NOTE: For now we can only accept major upgrades & allow minors and patches in future releases
   // version?: Version.Latest | Version.Major;
-  silent?: boolean;
-  debug?: boolean;
+  dryRun: boolean;
+  silent: boolean;
+  debug: boolean;
+}
+
+export interface TaskOptions {
+  confirm?: (message: string) => Promise<boolean> | Promise<undefined> | boolean | undefined;
+  dryRun?: boolean;
+  logger: Logger;
+}
+
+export interface CodemodPath {
+  path: string;
+  formatted: string;
+  fullPath: string;
+  version: Version.SemVer;
+}
+
+export interface RunReport {
+  codemod: CodemodPath;
+  report: Report;
+}
+
+export type RunReports = RunReport[];
+
+export interface Report {
+  stats: Record<string, number>;
+  timeElapsed: string;
+  error: number;
+  ok: number;
+  nochange: number;
+  skip: number;
 }
