ee47e07b14dbc6eb5c150c45feff5aaeff91aecb
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-11-16 12:21:06 +0000

    test(releases): setup test harness for working with the admin app

--- packages/core/content-releases/admin/src/components/AddReleaseDialog.tsx
+++ packages/core/content-releases/admin/src/components/AddReleaseDialog.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 import {
   Button,
   ModalBody,
--- packages/core/content-releases/admin/src/pages/tests/ReleasesPage.test.tsx
+++ packages/core/content-releases/admin/src/pages/tests/ReleasesPage.test.tsx
@@ -1,49 +1,12 @@
-import { lightTheme, ThemeProvider } from '@strapi/design-system';
 import { within, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { IntlProvider } from 'react-intl';
-import { MemoryRouter } from 'react-router-dom';
+import { render } from '@tests/utils';
 
-import { renderWithProviders } from '../../../tests/utils';
-import { ReleasesPage } from '../ReleasesPage';
-
-const user = userEvent.setup();
-
-jest.mock('@strapi/helper-plugin', () => ({
-  ...jest.requireActual('@strapi/helper-plugin'),
-  // eslint-disable-next-line
-  CheckPermissions: ({ children }: { children: JSX.Element}) => <div>{children}</div>
-}));
-
-jest.mock('../../../store/hooks', () => ({
-  ...jest.requireActual('../../../store/hooks'),
-  useTypedSelector: jest.fn().mockReturnValue({
-    loading: false,
-    error: undefined,
-    releases: [],
-  }),
-}));
-
-const render = () =>
-  renderWithProviders(
-    <ThemeProvider theme={lightTheme}>
-      <IntlProvider locale="en" messages={{}} defaultLocale="en">
-        <MemoryRouter>
-          <ReleasesPage />
-        </MemoryRouter>
-      </IntlProvider>
-    </ThemeProvider>
-  );
+import { ReleasesPage } from '../ReleasePage';
 
 describe('Releases home page', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
   it('renders correctly the heading content', async () => {
-    render();
-
-    () => expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Releases');
+    const { user } = render(<ReleasesPage />);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Releases');
     // if there are 0 releases
     expect(screen.getByText('No releases')).toBeInTheDocument();
 
@@ -64,7 +27,7 @@ describe('Releases home page', () => {
   });
 
   it('hides the dialog', async () => {
-    render();
+    const { user } = render(<ReleasesPage />);
     const newReleaseButton = screen.getByRole('button', { name: 'New release' });
     await user.click(newReleaseButton);
 
@@ -78,7 +41,7 @@ describe('Releases home page', () => {
   });
 
   it('enables the submit button when there is content in the input', async () => {
-    render();
+    const { user } = render(<ReleasesPage />);
     const newReleaseButton = screen.getByRole('button', { name: 'New release' });
     await user.click(newReleaseButton);
 
