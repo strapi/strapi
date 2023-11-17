4e7b55fe62c8011f0e0dc1652f3b8eb3a5346584
Jamie Howard <jamie.howard@strapi.io>
2023-11-17 14:53:26 +0000

    feat(content-manager): types for policies

--- packages/core/content-manager/server/src/services/content-types.ts
+++ packages/core/content-manager/server/src/services/content-types.ts
@@ -39,7 +39,6 @@ const service = ({ strapi }: any) => ({
       return this.findAllContentTypes();
     }
 
-    // @ts-expect-error TODO when adding types
     return this.findAllContentTypes().filter(contentTypesUtils.isKind(kind));
   },
 
