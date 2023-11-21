30e8a6321d5a19ab8b01c0912be65f4782667acd
markkaylor <mark.kaylor@strapi.io>
2023-11-21 09:18:12 +0100

    feat(content-releases): add findOne endpoint (#18821)

--- packages/core/content-releases/server/src/services/release.ts
+++ packages/core/content-releases/server/src/services/release.ts
@@ -1,21 +1,21 @@
 import { setCreatorFields } from '@strapi/utils';
 import type { LoadedStrapi } from '@strapi/types';
 import { RELEASE_ACTION_MODEL_UID, RELEASE_MODEL_UID } from '../constants';
-import type { ReleaseCreateArgs, UserInfo, ReleaseActionCreateArgs } from '../../../shared/types';
+import type { GetReleases, CreateRelease, GetRelease } from '../../../shared/contracts/releases';
+import type { CreateReleaseAction } from '../../../shared/contracts/release-actions';
+import type { UserInfo } from '../../../shared/types';
 import { getService } from '../utils';
 
 const createReleaseService = ({ strapi }: { strapi: LoadedStrapi }) => ({
-  async create(releaseData: ReleaseCreateArgs, { user }: { user: UserInfo }) {
+  async create(releaseData: CreateRelease.Request['body'], { user }: { user: UserInfo }) {
     const releaseWithCreatorFields = await setCreatorFields({ user })(releaseData);
 
-    const release = await strapi.entityService.create(RELEASE_MODEL_UID, {
+    return strapi.entityService.create(RELEASE_MODEL_UID, {
       data: releaseWithCreatorFields,
     });
-
-    return release;
   },
-  async findMany(query: Record<string, unknown>) {
-    const { results, pagination } = await strapi.entityService.findPage(RELEASE_MODEL_UID, {
+  findMany(query?: GetReleases.Request['query']) {
+    return strapi.entityService.findPage(RELEASE_MODEL_UID, {
       ...query,
       populate: {
         actions: {
@@ -24,23 +24,28 @@ const createReleaseService = ({ strapi }: { strapi: LoadedStrapi }) => ({
         },
       },
     });
-
-    return {
-      data: results,
-      pagination,
-    };
+  },
+  findOne(id: GetRelease.Request['params']['id']) {
+    return strapi.entityService.findOne(RELEASE_MODEL_UID, id, {
+      populate: {
+        actions: {
+          // @ts-expect-error TS error on populate, is not considering count
+          count: true,
+        },
+      },
+    });
   },
   async createAction(
-    releaseId: ReleaseActionCreateArgs['releaseId'],
-    action: Pick<ReleaseActionCreateArgs, 'type' | 'entry'>
+    releaseId: CreateReleaseAction.Request['params']['releaseId'],
+    action: Pick<CreateReleaseAction.Request['body'], 'type' | 'entry'>
   ) {
     const { validateEntryContentType, validateUniqueEntry } = getService('release-validation', {
       strapi,
     });
 
     await Promise.all([
-      validateEntryContentType({ releaseId, ...action }),
-      validateUniqueEntry({ releaseId, ...action }),
+      validateEntryContentType(action.entry.contentType),
+      validateUniqueEntry(releaseId, action),
     ]);
 
     const { entry, type } = action;
