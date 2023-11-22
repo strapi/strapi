adb79006a66bb27753ae725222b53be335ba7b48
Convly <jean-sebastien.herbaux@epitech.eu>
2023-11-22 14:11:55 +0100

    Handle version ranges in project's dependencies

--- packages/utils/upgrade/src/core/project-loader.ts
+++ packages/utils/upgrade/src/core/project-loader.ts
@@ -1,12 +1,14 @@
 import { glob } from 'glob';
+import chalk from 'chalk';
 import path from 'node:path';
 import assert from 'node:assert';
 import fs from 'node:fs/promises';
 
+import { isSemVer } from './version';
 import * as f from './format';
 
 import type { Logger } from './logger';
-import type { SemVer } from '.';
+import type { SemVer } from './version';
 
 export interface ProjectLoaderOptions {
   cwd: string;
@@ -27,11 +29,19 @@ export interface ProjectComponents {
   strapiVersion: SemVer;
 }
 
+type MinimalPackageJSON = {
+  name: string;
+  version: string;
+  dependencies?: Record<string, string>;
+} & Record<string, unknown>;
+
 const PROJECT_PACKAGE_JSON = 'package.json';
 const PROJECT_DEFAULT_ALLOWED_ROOT_PATHS = ['src', 'config', 'public'];
 const PROJECT_DEFAULT_ALLOWED_EXTENSIONS = ['js', 'ts', 'json'];
 const PROJECT_DEFAULT_PATTERNS = ['package.json'];
 const STRAPI_DEPENDENCY_NAME = '@strapi/strapi';
+const F_STRAPI_DEPENDENCY_NAME = f.highlight(STRAPI_DEPENDENCY_NAME);
+const F_PROJECT_PACKAGE_JSON = f.highlight(PROJECT_PACKAGE_JSON);
 
 export const createProjectLoader = (options: ProjectLoaderOptions): ProjectLoader => {
   const { cwd, logger } = options;
@@ -52,17 +62,17 @@ export const createProjectLoader = (options: ProjectLoaderOptions): ProjectLoade
 // TODO: Remove any for the package.json structure, use yup validation on the package.json (validate dependencies)
 const loadPackageJSON = async (options: ProjectLoaderOptions): Promise<any> => {
   const { cwd, logger } = options;
+  const fPath = f.path(cwd);
 
   const packagePath = path.join(cwd, PROJECT_PACKAGE_JSON);
 
   try {
     await fs.access(packagePath);
   } catch {
-    throw new Error(`Could not find a ${f.highlight(PROJECT_PACKAGE_JSON)} file in ${f.path(cwd)}`);
+    throw new Error(`Could not find a ${F_PROJECT_PACKAGE_JSON} file in ${fPath}`);
   }
 
-  const buffer = await fs.readFile(packagePath);
-  const packageJSON = JSON.parse(buffer.toString());
+  const packageJSON = require(packagePath);
 
   logger.debug(
     `Loaded package.json for ${f.highlight(packageJSON.name)} (${f.version(packageJSON.version)})`
@@ -121,27 +131,106 @@ const formatGlobCollectionPattern = (collection: string[]): string => {
   return collection.length === 1 ? collection[0] : `{${collection}}`;
 };
 
-// TODO: Don't use the project version but look at the @strapi dependencies instead
-// ?: What strategy should we adopt if there are multiple @strapi dependencies with different versions?
-//     - Use latest?
-//     - Use @strapi/strapi one? <- Seems like the best choice for the moment
-const parseStrapiVersion = (packageJSON: any, options: ProjectLoaderOptions): SemVer => {
+const parseStrapiVersion = (
+  packageJSON: MinimalPackageJSON,
+  options: ProjectLoaderOptions
+): SemVer => {
   const { cwd, logger } = options;
 
-  const dependencies = packageJSON.dependencies ?? {};
-  const strapiVersion = dependencies[STRAPI_DEPENDENCY_NAME] as SemVer | undefined;
+  const strapiVersion =
+    // First try to get the strapi version from the package.json dependencies
+    findStrapiVersionFromProjectPackageJSON(packageJSON, options) ??
+    // If the version found is not a valid SemVer, get the Strapi version from the installed package
+    findLocallyInstalledStrapiVersion(packageJSON, options);
 
-  if (strapiVersion === undefined) {
-    throw new Error(
-      `No version of "${STRAPI_DEPENDENCY_NAME}" was found in the project's package.json. Are you in a valid Strapi project?`
-    );
-  }
+  // At this point, we are sure to have a semver-compliant Strapi version. (else it would've thrown an error)
 
-  const fDependencyName = f.highlight(STRAPI_DEPENDENCY_NAME);
   const fStrapiVersion = f.version(strapiVersion);
   const fPath = f.path(cwd);
 
-  logger.debug(`Found a "${fDependencyName}" dependency (${fStrapiVersion}) in ${fPath}`);
+  logger.debug(
+    `Resolved the "${F_STRAPI_DEPENDENCY_NAME}" dependency (${fStrapiVersion}) for ${fPath}`
+  );
+
+  return strapiVersion;
+};
+
+const findStrapiVersionFromProjectPackageJSON = (
+  packageJSON: MinimalPackageJSON,
+  options: ProjectLoaderOptions
+): SemVer | undefined => {
+  const { logger } = options;
+  const fProjectName = f.highlight(packageJSON.name);
+
+  const version = packageJSON.dependencies?.[STRAPI_DEPENDENCY_NAME];
+
+  if (version === undefined) {
+    throw new Error(
+      `No version of ${F_STRAPI_DEPENDENCY_NAME} was found in ${fProjectName}. Are you in a valid Strapi project?`
+    );
+  }
+
+  if (!isSemVer(version)) {
+    const fInvalidSemVer = chalk.italic.red(version);
+    logger.debug(
+      `Found a ${F_STRAPI_DEPENDENCY_NAME} dependency in ${fProjectName}, but it's not a valid semver: ${fInvalidSemVer}`
+    );
+
+    // We return undefined only if a strapi/strapi version is found, but it's not semver compliant
+    return undefined;
+  }
+
+  const fVersion = f.version(version);
+  logger.debug(
+    `Found a valid ${F_STRAPI_DEPENDENCY_NAME} version in the package.json dependencies: ${fVersion}`
+  );
+
+  return version;
+};
+
+const findLocallyInstalledStrapiVersion = (
+  packageJSON: MinimalPackageJSON,
+  options: ProjectLoaderOptions
+): SemVer => {
+  const { cwd, logger } = options;
+  const fProjectPath = f.path(cwd);
+  const fProjectName = f.highlight(packageJSON.name);
+
+  const packageSearchText = `${STRAPI_DEPENDENCY_NAME}/package.json`;
+
+  let strapiPackageJSONPath: string;
+  let strapiPackageJSON: MinimalPackageJSON;
+
+  try {
+    logger.debug(
+      `Trying to find a local installation of ${F_STRAPI_DEPENDENCY_NAME} for ${fProjectName} (${fProjectPath})`
+    );
+
+    strapiPackageJSONPath = require.resolve(packageSearchText, { paths: [cwd] });
+    strapiPackageJSON = require(strapiPackageJSONPath);
+
+    assert(typeof strapiPackageJSON === 'object');
+  } catch {
+    throw new Error(
+      `Cannot find a "${F_STRAPI_DEPENDENCY_NAME}" dependency with a valid "package.json" file installed for ${cwd}`
+    );
+  }
+
+  const fLocalInstallPath = f.path(strapiPackageJSONPath);
+
+  const strapiVersion = strapiPackageJSON.version;
+  const isValidSemVer = isSemVer(strapiVersion);
+
+  if (!isValidSemVer) {
+    const fInvalidVersion = chalk.red(strapiVersion);
+    throw new Error(
+      `Invalid ${F_STRAPI_DEPENDENCY_NAME} version found in ${fLocalInstallPath} (${fInvalidVersion})`
+    );
+  }
+
+  logger.debug(
+    `Found a local installation of ${F_STRAPI_DEPENDENCY_NAME} (${fLocalInstallPath}) for ${fProjectName}`
+  );
 
   return strapiVersion;
 };
