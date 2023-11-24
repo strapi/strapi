b3033159d8bc9ee5710df325f3bbf299ccd9716f
Ben Irvin <ben@innerdvations.com>
2023-11-24 13:01:01 +0100

    fix: default log level 'info'

--- packages/utils/logger/src/constants.ts
+++ packages/utils/logger/src/constants.ts
@@ -1,7 +1,7 @@
 import { config } from 'winston';
 
 const LEVELS = config.npm.levels;
-const LEVEL_LABEL = 'silly';
+const LEVEL_LABEL = 'info';
 const LEVEL = LEVELS[LEVEL_LABEL];
 
 export { LEVEL, LEVEL_LABEL, LEVELS };
