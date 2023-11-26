b26f11a15e659eadd33cfaefee4f16411406b6c3
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2023-11-26 07:32:12 +0000

    Add new files and configurations

---
+++ my-project/src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
