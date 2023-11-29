d5107956b7aaaed96d86cca3247921ce2aee7dba
Simone Taeggi <startae14@gmail.com>
2023-11-29 11:15:16 +0100

    reduce releases page unit test code

--- packages/core/content-releases/admin/src/pages/tests/ReleasesPage.test.tsx
+++ packages/core/content-releases/admin/src/pages/tests/ReleasesPage.test.tsx
@@ -13,16 +13,14 @@ jest.mock('@strapi/helper-plugin', () => ({
 }));
 
 describe('Releases home page', () => {
-  it('renders the page correctly when there are no releases', async () => {
+  it('renders the tab content correctly when there are no releases', async () => {
     server.use(
       rest.get('/content-releases', (req, res, ctx) =>
         res(ctx.json(mockReleasesPageData.emptyEntries))
       )
     );
 
-    const { user } = render(<ReleasesPage />);
-
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Releases');
+    render(<ReleasesPage />);
 
     const releaseSubtitle = await screen.findAllByText('No releases');
     expect(releaseSubtitle[0]).toBeInTheDocument();
@@ -30,49 +28,23 @@ describe('Releases home page', () => {
     const paginationCombobox = screen.queryByRole('combobox', { name: /entries per page/i });
     expect(paginationCombobox).not.toBeInTheDocument();
 
-    const newReleaseButton = screen.getByRole('button', { name: 'New release' });
-    expect(newReleaseButton).toBeInTheDocument();
-
-    await user.click(newReleaseButton);
-    const dialogContainer = screen.getByRole('dialog');
-    const dialogTitle = within(dialogContainer).getByText(/new release/i);
-    expect(dialogTitle).toBeInTheDocument();
-
     const pendingTabPanel = screen.getByRole('tabpanel', { name: /pending/i });
     const emptyPendingBodyContent = within(pendingTabPanel).getByText(/no releases/i);
 
     expect(emptyPendingBodyContent).toBeInTheDocument();
-
-    // change the tab to see the done empty body content
-    const doneTab = screen.getByRole('tab', { name: /done/i });
-    await user.click(doneTab);
-    const doneTabpanel = screen.getByRole('tabpanel', { name: /done/i });
-    const emptyDoneBodyContent = within(doneTabpanel).getByText(/no releases/i);
-    expect(emptyDoneBodyContent).toBeInTheDocument();
   });
 
-  it('renders the page correctly when at least one tab has content', async () => {
+  it('renders the tab content correctly when there are releases', async () => {
     server.use(
-      rest.get('/content-releases', (req, res, ctx) => {
-        if (
-          req.url.searchParams.get('page') === '1' &&
-          req.url.searchParams.get('pageSize') === '16' &&
-          req.url.searchParams.get('filters[$and][0][releasedAt][$notNull]') === 'true'
-        ) {
-          // Return a mocked response
-          return res(ctx.json(mockReleasesPageData.emptyEntries));
-        }
-        return res(ctx.json(mockReleasesPageData.pendingEntries));
-      })
+      rest.get('/content-releases', (req, res, ctx) =>
+        res(ctx.json(mockReleasesPageData.pendingEntries))
+      )
     );
 
     const { user } = render(<ReleasesPage />);
 
     const releaseSubtitle = await screen.findByText('17 releases');
     expect(releaseSubtitle).toBeInTheDocument();
-
-    const paginationCombobox = screen.queryByRole('combobox', { name: /entries per page/i });
-    expect(paginationCombobox).toBeInTheDocument();
 
     const firstEntry = screen.getByRole('heading', { level: 3, name: 'entry 1' });
     expect(firstEntry).toBeInTheDocument();
@@ -82,85 +54,5 @@ describe('Releases home page', () => {
 
     const lastEntry = screen.getByRole('heading', { level: 3, name: 'entry 17' });
     expect(lastEntry).toBeInTheDocument();
-
-    // change the tab to see the done empty body content
-    const doneTab = screen.getByRole('tab', { name: /done/i });
-    await user.click(doneTab);
-    const doneReleaseSubtitle = await screen.findAllByText('No releases');
-    expect(doneReleaseSubtitle[0]).toBeInTheDocument();
-
-    const paginationDoneCombobox = screen.queryByRole('combobox', { name: /entries per page/i });
-    expect(paginationDoneCombobox).not.toBeInTheDocument();
-
-    const doneTabpanel = screen.getByRole('tabpanel', { name: /done/i });
-
-    const emptyDoneBodyContent = within(doneTabpanel).getByText(/no releases/i);
-    expect(emptyDoneBodyContent).toBeInTheDocument();
-  });
-
-  it('renders the page correctly when both tabs have content', async () => {
-    server.use(
-      rest.get('/content-releases', (req, res, ctx) => {
-        if (
-          req.url.searchParams.get('page') === '1' &&
-          req.url.searchParams.get('pageSize') === '16' &&
-          req.url.searchParams.get('filters[$and][0][releasedAt][$notNull]') === 'true'
-        ) {
-          // Return a mocked response
-          return res(ctx.json(mockReleasesPageData.pendingDoneEntries.done));
-        }
-        return res(ctx.json(mockReleasesPageData.pendingDoneEntries.pending));
-      })
-    );
-
-    const { user } = render(<ReleasesPage />);
-
-    const releaseSubtitle = await screen.findByText('8 releases');
-    expect(releaseSubtitle).toBeInTheDocument();
-
-    // change the tab to see the done empty body content
-    const doneTab = screen.getByRole('tab', { name: /done/i });
-    await user.click(doneTab);
-    const doneReleaseSubtitle = await screen.findByText('10 releases');
-    expect(doneReleaseSubtitle).toBeInTheDocument();
-
-    const paginationDoneCombobox = screen.queryByRole('combobox', { name: /entries per page/i });
-    expect(paginationDoneCombobox).toBeInTheDocument();
-
-    const firstEntry = screen.getByRole('heading', { level: 3, name: 'entry 8' });
-    expect(firstEntry).toBeInTheDocument();
-  });
-
-  it('hides the dialog', async () => {
-    const { user } = render(<ReleasesPage />);
-
-    const newReleaseButton = screen.getByRole('button', { name: 'New release' });
-    await user.click(newReleaseButton);
-
-    const dialogContainer = screen.getByRole('dialog');
-    const dialogCancelButton = within(dialogContainer).getByRole('button', {
-      name: /cancel/i,
-    });
-    expect(dialogCancelButton).toBeInTheDocument();
-    await user.click(dialogCancelButton);
-    expect(dialogContainer).not.toBeInTheDocument();
-  });
-
-  it('enables the submit button when there is content in the input', async () => {
-    const { user } = render(<ReleasesPage />);
-
-    const newReleaseButton = screen.getByRole('button', { name: 'New release' });
-    await user.click(newReleaseButton);
-
-    const dialogContainer = screen.getByRole('dialog');
-    const dialogContinueButton = within(dialogContainer).getByRole('button', {
-      name: /continue/i,
-    });
-    expect(dialogContinueButton).toBeInTheDocument();
-    expect(dialogContinueButton).toBeDisabled();
-
-    const inputElement = within(dialogContainer).getByRole('textbox', { name: /name/i });
-    await user.type(inputElement, 'new release');
-    expect(dialogContinueButton).toBeEnabled();
   });
 });
