4524dae2077f6f14b69ab79a0c7223430d4277ac
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-12-01 08:49:51 +0000

    Update packages/core/strapi/src/services/features.ts
    Co-authored-by: Alexandre BODIN <alexandrebodin@users.noreply.github.com>

--- packages/core/strapi/src/services/features.ts
+++ packages/core/strapi/src/services/features.ts
@@ -6,21 +6,21 @@
 
 import type { Strapi, FeaturesService } from '@strapi/types';
 
+type FeatureName = keyof FeaturesService['config']['future'];
+
 const createFeaturesService = (strapi: Strapi): FeaturesService => {
-  return {
+  const service: FeaturesService = {
     get config() {
       return strapi.config.get<FeaturesService['config'] | undefined>('features');
     },
     future: {
       isEnabled(futureFlagName: string): boolean {
-        return (
-          strapi.config.get<FeaturesService['config'] | undefined>('features')?.future[
-            futureFlagName as keyof FeaturesService['config']['future']
-          ] === true
-        );
+        return service.config?.future[futureFlagName as FeatureName] === true;
       },
     },
   };
+  
+  return service;
 };
 
 export { createFeaturesService };
