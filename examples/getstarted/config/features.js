7c032c08805d47b5c6ae86ea141b7842d91f5bf6
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-12-05 14:21:45 +0000

    Update features.js
    Co-authored-by: Alexandre BODIN <alexandrebodin@users.noreply.github.com>

--- examples/getstarted/config/features.js
+++ examples/getstarted/config/features.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   future: {
-    contentReleases: env.bool('STRAPI_FEATURES_FUTURE_CONTENTRELEASES', false),
+    contentReleases: env.bool('STRAPI_FEATURES_FUTURE_CONTENT_RELEASES', false),
   },
 });
