bcdf25a438beb9a466b1c1ea88a7eb3b62ebd681
Convly <jean-sebastien.herbaux@epitech.eu>
2023-12-06 16:19:21 +0100

    Remove old upgrade core files

--- packages/utils/upgrade/src/core/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './version-parser';
-export * from './project-loader';
-export * from './transforms-loader';
-export * from './runner';
-export * from './version';
-export * from './logger';
-export * from './time';
-export * as f from './format';
