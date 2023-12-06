997b3498fce480d0dfeb7611325caa5d8fce291c
Convly <jean-sebastien.herbaux@epitech.eu>
2023-12-06 17:57:59 +0100

    chore: remove debug comments

--- packages/utils/upgrade/src/modules/runner/code/types.ts
+++ packages/utils/upgrade/src/modules/runner/code/types.ts
@@ -2,9 +2,9 @@ export interface CodeRunnerConfiguration {
   dry?: boolean;
   extensions?: string;
   runInBand?: boolean;
-  verbose?: number; // static 0
-  babel?: boolean; // static true
-  print?: boolean; // static false
-  silent?: boolean; // static true
-  parser?: 'js' | 'ts'; // static ts
+  verbose?: number;
+  babel?: boolean;
+  print?: boolean;
+  silent?: boolean;
+  parser?: 'js' | 'ts';
 }
