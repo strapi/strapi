af0176cf2e8b90e8550f1099a5ad7509a737dd1d
Convly <jean-sebastien.herbaux@epitech.eu>
2023-12-08 17:03:39 +0100

    feat: add positional arguments to xMany relations, split attributes files

--- packages/core/types/src/modules/entity-service/params/attributes/index.ts
+++ packages/core/types/src/modules/entity-service/params/attributes/index.ts
@@ -1,37 +1,11 @@
-import type { Attribute, Common, Utils, Entity } from '../../../types';
+import type { Attribute, Common, Utils } from '../../../../types';
+import type { OmitRelationsWithoutTarget, RelationInputValue } from './relation';
+import type { ID } from './id';
+import type * as Mocks from './mocks';
 
-export type NonFilterableKind = Extract<Attribute.Kind, 'password' | 'dynamiczone'>;
-export type FilterableKind = Exclude<Attribute.Kind, NonFilterableKind>;
-
-export type GetNonFilterableKeys<TSchemaUID extends Common.UID.Schema> = Utils.Object.KeysBy<
-  Attribute.GetAll<TSchemaUID>,
-  Attribute.OfType<NonFilterableKind>,
-  string
->;
-
-export type GetScalarKeys<TSchemaUID extends Common.UID.Schema> = Exclude<
-  Attribute.GetKeysByType<TSchemaUID, Attribute.NonPopulatableKind>,
-  GetNonFilterableKeys<TSchemaUID>
->;
-
-export type GetNestedKeys<TSchemaUID extends Common.UID.Schema> = Exclude<
-  Attribute.GetKeysWithTarget<TSchemaUID>,
-  GetNonFilterableKeys<TSchemaUID>
->;
-
-export type ID = Entity.ID;
-
-export type BooleanValue = boolean | 'true' | 'false' | 't' | 'f' | '1' | '0' | 1 | 0;
-
-export type NumberValue = string | number;
-
-export type DateValue = Attribute.DateValue | number;
-
-export type TimeValue = Attribute.TimeValue | number;
-
-export type DateTimeValue = Attribute.DateTimeValue | number;
-
-export type TimeStampValue = Attribute.TimestampValue;
+export * from './id';
+export * from './utils';
+export * from './mocks';
 
 /**
  * List of possible values for the scalar attributes
@@ -65,7 +39,7 @@ export type ScalarValues = GetValue<
  */
 export type GetValues<TSchemaUID extends Common.UID.Schema> = {
   id?: ID;
-} & OmitRelationWithoutTarget<
+} & OmitRelationsWithoutTarget<
   TSchemaUID,
   {
     [TKey in Attribute.GetOptionalKeys<TSchemaUID>]?: GetValue<Attribute.Get<TSchemaUID, TKey>>;
@@ -73,15 +47,6 @@ export type GetValues<TSchemaUID extends Common.UID.Schema> = {
     [TKey in Attribute.GetRequiredKeys<TSchemaUID>]-?: GetValue<Attribute.Get<TSchemaUID, TKey>>;
   }
 >;
-
-export type OmitRelationWithoutTarget<TSchemaUID extends Common.UID.Schema, TValue> = Omit<
-  TValue,
-  Exclude<Attribute.GetKeysByType<TSchemaUID, 'relation'>, Attribute.GetKeysWithTarget<TSchemaUID>>
->;
-
-type RelationScalarAssociation = ID;
-type RelationObjectAssociation = { id: ID };
-type RelationReorderingAssociation = { set: ID[] } | { connect: ID[] };
 
 /**
  * Attribute.GetValue override with extended values
@@ -99,15 +64,7 @@ export type GetValue<TAttribute extends Attribute.Attribute> = Utils.Expression.
         TAttribute extends Attribute.Relation<infer _TOrigin, infer TRelationKind, infer TTarget>
           ? Utils.Expression.If<
               Utils.Expression.IsNotNever<TTarget>,
-              Utils.Expression.If<
-                Attribute.IsManyRelation<TRelationKind>,
-                // Only add the relation reordering APIs if the relation is x(to)Many
-                | RelationReorderingAssociation
-                | RelationScalarAssociation[]
-                | RelationObjectAssociation[],
-                // Else stick with a simple ID
-                RelationScalarAssociation | RelationObjectAssociation
-              >
+              RelationInputValue<TRelationKind>
             >
           : never
       ],
@@ -137,21 +94,21 @@ export type GetValue<TAttribute extends Attribute.Attribute> = Utils.Expression.
           : never
       ],
       // Boolean
-      [Utils.Expression.Extends<TAttribute, Attribute.Boolean>, BooleanValue],
+      [Utils.Expression.Extends<TAttribute, Attribute.Boolean>, Mocks.BooleanValue],
       // Number
       [
         Utils.Expression.Extends<
           TAttribute,
           Attribute.Integer | Attribute.BigInteger | Attribute.Float | Attribute.Decimal
         >,
-        NumberValue
+        Mocks.NumberValue
       ],
       // Date / Time
-      [Utils.Expression.Extends<TAttribute, Attribute.Time>, TimeValue],
-      [Utils.Expression.Extends<TAttribute, Attribute.Date>, DateValue],
+      [Utils.Expression.Extends<TAttribute, Attribute.Time>, Mocks.TimeValue],
+      [Utils.Expression.Extends<TAttribute, Attribute.Date>, Mocks.DateValue],
       [
         Utils.Expression.Extends<TAttribute, Attribute.Timestamp | Attribute.DateTime>,
-        DateTimeValue
+        Mocks.DateTimeValue
       ],
       // Fallback
       // If none of the above attribute type, fallback to the original Attribute.GetValue (while making sure it's an attribute)
