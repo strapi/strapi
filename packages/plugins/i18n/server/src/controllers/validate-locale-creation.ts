64a06f3e9f16ab84911622dbcfbaf0dff813e526
Marc-Roig <marc12info@gmail.com>
2023-12-14 15:14:53 +0100

    feat: prevent empty string locale filtering

--- packages/plugins/i18n/server/src/controllers/validate-locale-creation.ts
+++ packages/plugins/i18n/server/src/controllers/validate-locale-creation.ts
@@ -23,7 +23,8 @@ const validateLocaleCreation: Common.MiddlewareHandler = async (ctx, next) => {
     return next();
   }
 
-  const locale = get('locale', query) || get('locale', body);
+  // Prevent empty string locale
+  const locale = get('locale', query) || get('locale', body) || undefined;
   const relatedEntityId = get('plugins.i18n.relatedEntityId', query);
   // cleanup to avoid creating duplicates in singletypes
   ctx.request.query = {};
