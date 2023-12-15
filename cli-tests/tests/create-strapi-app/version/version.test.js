59c93a379e5f92410e033b2cf77428bb32d466a0
Ben Irvin <ben@innerdvations.com>
2023-12-15 17:42:49 +0100

    feat: add jest test runner with concurrency

--- cli-tests/tests/create-strapi-app/version/version.test.js
+++ cli-tests/tests/create-strapi-app/version/version.test.js
@@ -1,18 +1,14 @@
 describe('--version', () => {
   const instances = [];
 
-  beforeAll(async ({ strapi }) => {
+  beforeAll(async () => {
     // TODO: get the test instances from helpers
-    
   });
-  beforeEach(async ({ strapi }) => {
+  beforeEach(async () => {
     // TODO: reset the test instances
   });
 
-  it('should output version', async ({
-    page,
-    context,
-  }) => {
+  it('should output version', async () => {
     // TODO: run the cli command `strapi --version` and check exit code and output
     // Should we use coffee (https://github.com/node-modules/coffee) or CLET (https://github.com/node-modules/clet) ?
   });
