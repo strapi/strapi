d5e752fe6130f7a39f6621ad435956550485ae8e
Ben Irvin <ben.irvin@strapi.io>
2024-01-08 11:23:26 +0100

    test: initialize CLI e2e testing framework
    * chore: lint test scripts
    * chore: clone playwright tests
    * chore: clean up test runner
    * feat: add jest test runner with concurrency
    * chore: documentation
    * chore: docs
    * chore: fix prettier
    * chore: lint
    * chore: remove log
    * chore: remove console.logs
    * fix: ensure enough test apps are available
    * chore: add log for yalc
    * chore: fix logging
    * chore: tests to todos

--- test/scripts/run-cli-tests.js
+++ test/scripts/run-cli-tests.js
@@ -7,34 +7,33 @@ const yargs = require('yargs');
 
 const chalk = require('chalk');
 const { cleanTestApp, generateTestApp } = require('../helpers/test-app');
-const { createConfig } = require('../../playwright.base.config');
 
 const cwd = path.resolve(__dirname, '../..');
-const testAppDirectory = path.join(cwd, 'test-apps', 'e2e');
-const testRoot = path.join(cwd, 'e2e');
+const testAppDirectory = path.join(cwd, 'test-apps', 'cli');
+const testRoot = path.join(cwd, 'cli-tests');
 const testsDir = path.join(testRoot, 'tests');
 const templateDir = path.join(testRoot, 'app-template');
 
 yargs
   .parserConfiguration({
     /**
-     * This lets us pass any other arguments to playwright
+     * This lets us pass any other arguments to the test runner
      * e.g. the name of a specific test or the project we want to run
      */
     'unknown-options-as-args': true,
   })
   .command({
     command: '*',
-    description: 'run the E2E test suite',
+    description: 'run the CLI test suite',
     async builder(yarg) {
+      // each directory in testDir is a domain
       const domains = await fs.readdir(testsDir);
 
       yarg.option('concurrency', {
         alias: 'c',
         type: 'number',
         default: domains.length,
-        describe:
-          'Number of concurrent test apps to run, a test app runs an entire test suite domain',
+        describe: 'Number of concurrent test domains to run',
       });
 
       yarg.option('domains', {
@@ -59,22 +58,50 @@ yargs
         /**
          * Publishing all packages to the yalc store
          */
+        console.log('Running yalc...');
         await execa('node', [path.join(__dirname, '../..', 'scripts', 'yalc-publish.js')], {
           stdio: 'inherit',
         });
+        console.log('Complete');
 
-        /**
-         * We don't need to spawn more apps than we have domains,
-         * but equally if someone sets the concurrency to 1
-         * then we should only spawn one and run every domain on there.
-         */
-        const testAppsToSpawn = Math.min(domains.length, concurrency);
+        const loadDomainConfigs = async (domain) => {
+          try {
+            const configPath = path.join(testsDir, domain, 'config.js');
+            await fs.access(configPath);
+            // Import config.js and call it as a function
+            const config = require(configPath);
+            if (typeof config === 'function') {
+              return await config(argv);
+            }
+            return config;
+          } catch (e) {
+            // use default config
+            return {
+              testApps: 1,
+            };
+          }
+        };
 
-        if (testAppsToSpawn === 0) {
+        // Load the domain configs into an object with keys of the name of the test domain
+        const domainConfigs = {};
+        await Promise.all(
+          domains.map(async (domain) => {
+            domainConfigs[domain] = await loadDomainConfigs(domain);
+          })
+        );
+
+        // Determine the number of simultaneous test apps we need by taking the concurrency number of highest testApps requested from config
+        const testAppsRequired = Object.entries(domainConfigs)
+          .map(([, value]) => value.testApps) // Extract testApps values from config
+          .sort((a, b) => b - a) // Sort in descending order
+          .slice(0, concurrency) // Take the top X values
+          .reduce((acc, value) => acc + value, 0); // Sum up the values
+
+        if (testAppsRequired === 0) {
           throw new Error('No test apps to spawn');
         }
 
-        const testAppPaths = Array.from({ length: testAppsToSpawn }, (_, i) =>
+        const testAppPaths = Array.from({ length: testAppsRequired }, (_, i) =>
           path.join(testAppDirectory, `test-app-${i}`)
         );
 
@@ -91,7 +118,7 @@ yargs
          * You can also force this setup if desired, e.g. you
          * update the app-template.
          */
-        if (setup || currentTestApps.length < testAppsToSpawn) {
+        if (setup || currentTestApps.length < testAppsRequired) {
           /**
            * this will effectively clean the entire directory before hand
            * as opposed to cleaning the ones we aim to spawn.
@@ -99,14 +126,17 @@ yargs
           await Promise.all(
             currentTestApps.map(async (testAppName) => {
               const appPath = path.join(testAppDirectory, testAppName);
-              console.log(`cleaning test app at path: ${chalk.bold(appPath)}`);
+              console.log(`Cleaning test app at path: ${chalk.bold(appPath)}`);
               await cleanTestApp(appPath);
             })
           );
 
+          /**
+           * Generate the test apps and modify the configuration as needed
+           */
           await Promise.all(
             testAppPaths.map(async (appPath) => {
-              console.log(`generating test apps at path: ${chalk.bold(appPath)}`);
+              console.log(`Generating test apps at path: ${chalk.bold(appPath)}`);
               await generateTestApp({
                 appPath,
                 database: {
@@ -119,6 +149,7 @@ yargs
                 template: templateDir,
                 link: true,
               });
+
               /**
                * Because we're running multiple test apps at the same time
                * and the env file is generated by the generator with no way
@@ -144,65 +175,68 @@ yargs
         }
 
         /**
-         * You can't change the webserver configuration of playwright directly so they'd
-         * all be looking at the same test app which we don't want, instead we'll generate
-         * a playwright config based off the base one
-         */
-        const chunkedDomains = domains.reduce((acc, _, i) => {
-          if (i % testAppsToSpawn === 0) acc.push(domains.slice(i, i + testAppsToSpawn));
-          return acc;
-        }, []);
+         * Run the tests in parallel based on concurrency value
+         * */
+        const availableTestApps = [...currentTestApps];
+
+        const batches = [];
+
+        for (let i = 0; i < domains.length; i += concurrency) {
+          batches.push(domains.slice(i, i + concurrency));
+        }
 
         // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < chunkedDomains.length; i++) {
-          const domains = chunkedDomains[i];
-
+        for (let i = 0; i < batches.length; i++) {
+          const batch = batches[i];
           await Promise.all(
-            domains.map(async (domain, j) => {
-              const testAppPath = testAppPaths[j];
-              const port = 8000 + j;
+            batch.map(async (domain) => {
+              const config = domainConfigs[domain];
 
-              const pathToPlaywrightConfig = path.join(testAppPath, 'playwright.config.js');
+              if (availableTestApps.length < config.testApps) {
+                console.error('Not enough test apps available; aborting');
+                process.exit();
+              }
 
-              console.log(
-                `Creating playwright config for domain: ${chalk.blue(
-                  domain
-                )}, at path: ${chalk.yellow(testAppPath)}`
-              );
+              // claim testApps for this domain to use
+              const testApps = availableTestApps.splice(-1 * config.testApps);
 
-              const config = createConfig({
-                testDir: path.join(testsDir, domain),
-                port,
-                appDir: testAppPath,
-              });
+              /**
+               * We do not start up the apps; the test runner is responsible for that if it's necessary,
+               * but most CLI commands don't need a started instance of strapi
+               * Instead, we just pass in the path of the test apps assigned for this test runner via env
+               *  */
+              try {
+                const env = {
+                  TEST_APPS: testApps.join(','),
+                };
+                const domainDir = path.join(testsDir, domain);
+                console.log('Running jest for domain', domain, 'with env', env, 'in', domainDir);
+                // run the command 'jest --rootDir <domainDir>'
+                const { stdout, stderr } = await execa('jest', ['--rootDir', domainDir], {
+                  cwd: domainDir, // run from the domain directory
+                  env, // pass it our custom env values
+                });
 
-              const configFileTemplate = `
-const config = ${JSON.stringify(config)}
-
-module.exports = config
-              `;
-
-              await fs.writeFile(pathToPlaywrightConfig, configFileTemplate);
-
-              console.log(`Running ${chalk.blue(domain)} e2e tests`);
-
-              await execa(
-                'yarn',
-                ['playwright', 'test', '--config', pathToPlaywrightConfig, ...argv._],
-                {
-                  stdio: 'inherit',
-                  cwd,
-                  env: {
-                    PORT: port,
-                    HOST: '127.0.0.1',
-                  },
+                // TODO: determine the best way to log this for the CI (stream it rather than wait for completion)
+                if (stdout) {
+                  console.log(stdout);
                 }
-              );
+                if (stderr) {
+                  console.log(stderr);
+                }
+              } catch (err) {
+                // If any tests fail
+                console.error('Test suite failed for', domain);
+                // TODO: determine how to integrate this with the CI, probably ending the process and returning an error exit code
+              }
+
+              // make them available again for the next batch
+              availableTestApps.push(...testApps);
             })
           );
         }
       } catch (err) {
-        console.error(chalk.red('Error running e2e tests:'));
+        console.error(chalk.red('Error running CLI tests:'));
         /**
          * This is a ExecaError, if we were in TS we could do `instanceof`
          */
@@ -231,7 +265,7 @@ module.exports = config
         await Promise.all(
           currentTestApps.map(async (testAppName) => {
             const appPath = path.join(testAppDirectory, testAppName);
-            console.log(`cleaning test app at path: ${chalk.bold(appPath)}`);
+            console.log(`Cleaning test app at path: ${chalk.bold(appPath)}`);
             await cleanTestApp(appPath);
           })
         );
