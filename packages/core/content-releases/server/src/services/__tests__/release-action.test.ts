fa02f6c7db1fdd4246f822ef54b4e7c56c1e377e
Mark Kaylor <mark.kaylor@strapi.io>
2024-01-09 17:59:58 +0100

    fix: use target_type to deleteMany

--- packages/core/content-releases/server/src/services/__tests__/release-action.test.ts
+++ packages/core/content-releases/server/src/services/__tests__/release-action.test.ts
@@ -1,12 +1,8 @@
-import { RELEASE_ACTION_MODEL_UID } from '../../constants';
 import createReleaseActionService from '../release-action';
 
 describe('Release Action service', () => {
   it('deletes all release actions given a content type uid', async () => {
     const strapiMock = {
-      entityService: {
-        findMany: jest.fn().mockReturnValue([{ id: 1, contentType: 'api::test.test' }]),
-      },
       db: {
         query: jest.fn().mockReturnValue({
           deleteMany: jest.fn(),
@@ -18,14 +14,9 @@ describe('Release Action service', () => {
     const releaseActionService = createReleaseActionService({ strapi: strapiMock });
     await releaseActionService.deleteManyForContentType('api::test.test');
 
-    expect(strapiMock.entityService.findMany).toHaveBeenCalledWith(RELEASE_ACTION_MODEL_UID, {
-      filters: {
-        contentType: 'api::test.test',
-      },
-    });
     expect(strapiMock.db.query().deleteMany).toHaveBeenCalledWith({
       where: {
-        id: { $in: [1] },
+        target_type: 'api::test.test',
       },
     });
   });
