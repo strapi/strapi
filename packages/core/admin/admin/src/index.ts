ac3e5bd043985661b4e0e2fb1d924ed721a5b1d0
Fernando Chavez <fernando.chavez@strapi.io>
2024-01-12 13:51:19 +0100

    fix error exporting Store twice

--- packages/core/admin/admin/src/index.ts
+++ packages/core/admin/admin/src/index.ts
@@ -3,4 +3,3 @@ export * from './render';
 
 export type { Store } from './core/store/configure';
 export type { SanitizedAdminUser } from '../../shared/contracts/shared';
-export type { Store } from './core/store/configure';
