0724d3faa052466502a78fb43a3cd2cede557d45
Ben Irvin <ben@innerdvations.com>
2024-01-15 14:11:44 +0100

    chore: cleanup koa typings

--- packages/core/content-type-builder/server/src/controllers/component-categories.ts
+++ packages/core/content-type-builder/server/src/controllers/component-categories.ts
@@ -4,7 +4,7 @@ import validateComponentCategory from './validation/component-category';
 
 export default {
   async editCategory(ctx: Context) {
-    const { body } = ctx.request;
+    const body = ctx.request.body as any;
 
     try {
       await validateComponentCategory(body);
@@ -18,7 +18,7 @@ export default {
 
     const componentCategoryService = getService('component-categories');
 
-    const newName = await componentCategoryService.editCategory(name, body as any);
+    const newName = await componentCategoryService.editCategory(name, body);
 
     setImmediate(() => strapi.reload());
 
