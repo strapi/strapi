d71c30a2ab9ac863c3d6aed02e398281804ebd8e
Ben Irvin <ben@innerdvations.com>
2024-01-15 18:38:45 +0100

    feat: init schemas in template

---
+++ cli-tests/app-template/template/src/api/single-type-localized/controllers/single-type-localized.ts
@@ -0,0 +1,7 @@
+/**
+ * single-type-localized controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::single-type-localized.single-type-localized');
---
+++ cli-tests/app-template/template/src/api/single-type-non-local/controllers/single-type-non-local.ts
@@ -0,0 +1,7 @@
+/**
+ * single-type-non-local controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::single-type-non-local.single-type-non-local');
