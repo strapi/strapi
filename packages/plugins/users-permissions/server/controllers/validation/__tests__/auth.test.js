8263926b4723e89c2a2b5f3c42fb6c3d7693531c
Ben Irvin <ben@innerdvations.com>
2024-01-16 18:14:32 +0100

    feat: register.allowedFields defaults to empty array

---
+++ packages/plugins/users-permissions/server/controllers/validation/__tests__/auth.test.js
@@ -0,0 +1,144 @@
+'use strict';
+
+const errors = require('@strapi/utils');
+const auth = require('../../auth');
+
+const mockStrapi = {
+  store: jest.fn(() => {
+    return {
+      get: jest.fn(() => {
+        return { allow_register: true };
+      }),
+    };
+  }),
+  config: {
+    get: jest.fn(() => {
+      return {
+        register: {
+          allowedFields: [],
+        },
+      };
+    }),
+  },
+  query: jest.fn(() => {
+    return {
+      findOne: jest.fn(() => {
+        return {
+          role: 1,
+        };
+      }),
+      count: jest.fn(() => {
+        return 0;
+      }),
+    };
+  }),
+  plugins: {
+    'users-permissions': {
+      controllers: {},
+      contentTypes: {},
+      policies: {},
+      services: {},
+    },
+  },
+  getModel: jest.fn(),
+};
+
+jest.mock('@strapi/utils', () => {
+  return {
+    ...jest.requireActual('@strapi/utils'),
+    sanitizeUser: jest.fn((input) => input),
+    sanitize: {
+      contentAPI: {
+        output: jest.fn((input) => input),
+      },
+    },
+  };
+});
+
+jest.mock('../../../utils', () => {
+  return {
+    getService: jest.fn(() => {
+      return {
+        add: jest.fn((user) => {
+          return user;
+        }),
+        issue: jest.fn(),
+      };
+    }),
+  };
+});
+
+describe('user-permissions auth', () => {
+  beforeAll(() => {
+    global.strapi = mockStrapi;
+  });
+
+  describe('register', () => {
+    test('accepts valid body', async () => {
+      const ctx = {
+        state: {
+          auth: {},
+        },
+        request: {
+          body: { username: 'testuser', email: 'test@example.com', password: 'Testpassword1!' },
+        },
+        send: jest.fn(),
+      };
+
+      await auth.register(ctx);
+      expect(ctx.send).toHaveBeenCalledTimes(1);
+    });
+
+    test("throws ValidationError when given fields that aren't allowed", async () => {
+      const ctx = {
+        state: {
+          auth: {},
+        },
+        request: {
+          body: {
+            confirmed: true,
+            username: 'testuser',
+            email: 'test@example.com',
+            password: 'Testpassword1!',
+          },
+        },
+        send: jest.fn(),
+      };
+      await expect(auth.register(ctx)).rejects.toThrow(errors.ValidationError);
+
+      expect(ctx.send).toHaveBeenCalledTimes(0);
+    });
+
+    test('allows exceptions from config register.allowedFields', async () => {
+      global.strapi = {
+        ...mockStrapi,
+        config: {
+          get: jest.fn(() => {
+            return {
+              register: {
+                allowedFields: ['confirmed'],
+              },
+            };
+          }),
+        },
+      };
+      const ctx = {
+        state: {
+          auth: {},
+        },
+        request: {
+          body: {
+            confirmed: true,
+            username: 'testuser',
+            email: 'test@example.com',
+            password: 'Testpassword1!',
+          },
+        },
+        send: jest.fn(),
+      };
+
+      await auth.register(ctx);
+      expect(ctx.send).toHaveBeenCalledTimes(1);
+    });
+  });
+});
