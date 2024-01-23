f615479eb1456f9e2f4179a8f4a2cf57fcd530cd
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-01-23 10:30:17 +0000

    chore: fix content-releases

--- packages/core/content-releases/server/src/controllers/release-action.ts
+++ packages/core/content-releases/server/src/controllers/release-action.ts
@@ -48,7 +48,7 @@ const releaseActionController = {
      * We need to sanitize the entry output according to that content type.
      * So, we group the sanitized output function by content type.
      */
-    const contentTypeOutputSanitizers = results.reduce((acc, action) => {
+    const contentTypeOutputSanitizers = results.reduce((acc: Record<string, any>, action: any) => {
       if (acc[action.contentType]) {
         return acc;
       }
@@ -68,7 +68,7 @@ const releaseActionController = {
      * sanitizeOutput doesn't work if you use it directly on the Release Action model, it doesn't sanitize the entries
      * So, we need to sanitize manually each entry inside a Release Action
      */
-    const sanitizedResults = await mapAsync(results, async (action) => ({
+    const sanitizedResults = await mapAsync(results, async (action: any) => ({
       ...action,
       entry: await contentTypeOutputSanitizers[action.contentType](action.entry),
     }));
