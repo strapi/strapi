09d94e2f2438e9efd6b675854cb279f7d9f452b3
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-01-31 11:31:24 +0100

    fix: types

--- packages/core/content-manager/server/src/history/controllers/index.ts
+++ packages/core/content-manager/server/src/history/controllers/index.ts
@@ -7,4 +7,4 @@ export const controllers = {
    * Casting is needed because the types aren't aware that Strapi supports
    * passing a controller factory as the value, instead of a controller object directly
    */
-} as Plugin.LoadedPlugin['controllers'];
+} as unknown as Plugin.LoadedPlugin['controllers'];
