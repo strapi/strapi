b5e5852817aa1522aad2f2243297588a4c3422b8
Simone Taeggi <startae14@gmail.com>
2024-02-02 11:40:17 +0100

    fix(content-releases): change action radio buttons behaviour and ui

--- packages/core/content-releases/admin/src/pages/ReleaseDetailsPage.tsx
+++ packages/core/content-releases/admin/src/pages/ReleaseDetailsPage.tsx
@@ -483,6 +483,9 @@ const ReleaseDetailsBody = () => {
     isError: isReleaseError,
     error: releaseError,
   } = useGetReleaseQuery({ id: releaseId });
+  const {
+    allowedActions: { canPublish, canUpdate },
+  } = useRBAC(PERMISSIONS);
 
   const release = releaseData?.data;
   const selectedGroupBy = query?.groupBy || 'contentType';
@@ -736,6 +739,7 @@ const ReleaseDetailsBody = () => {
                               selected={type}
                               handleChange={(e) => handleChangeType(e, id, [key, actionIndex])}
                               name={`release-action-${id}-type`}
+                              disabled={!canPublish || !canUpdate}
                             />
                           )}
                         </Td>
