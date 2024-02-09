d4ce3b655629a3f612ca9493ea1bdb1fa36a4984
Marc Roig <marc12info@gmail.com>
2024-02-09 08:55:45 +0100

    Update packages/core/core/src/services/document-service/transform/utils.ts
    Co-authored-by: Ben Irvin <ben.irvin@strapi.io>

--- packages/core/core/src/services/document-service/transform/utils.ts
+++ packages/core/core/src/services/document-service/transform/utils.ts
@@ -1,8 +1,7 @@
 const switchIdForDocumentId = (output: Record<string, any>) => {
   // Mutating for performance reasons
-  const documentId = output.documentId;
+  output.id = output.documentId;
   delete output.documentId;
-  output.id = documentId;
   return output;
 };
 
