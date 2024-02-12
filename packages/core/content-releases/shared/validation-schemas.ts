5680b671051d3e3f1355c9e194de4f3e24bf347d
Fernando Chavez <fernando.chavez@strapi.io>
2024-02-12 09:30:56 +0100

    fix ts error

--- packages/core/content-releases/shared/validation-schemas.ts
+++ packages/core/content-releases/shared/validation-schemas.ts
@@ -7,7 +7,7 @@ export const RELEASE_SCHEMA = yup
     // scheduledAt is a date, but we always receive strings from the client
     scheduledAt: yup.string().nullable(),
     timezone: yup.string().when('scheduledAt', {
-      is: (scheduledAt) => !!scheduledAt,
+      is: (scheduledAt: string) => !!scheduledAt,
       then: yup.string().required(),
       otherwise: yup.string().nullable(),
     }),
