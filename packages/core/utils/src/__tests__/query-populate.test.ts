1302e73fd0d08f612954712c216af933c5448f9a
Ben Irvin <ben@innerdvations.com>
2024-02-13 09:34:54 +0100

    test: ensure wildcard isn't expanded

--- packages/core/utils/src/__tests__/query-populate.test.ts
+++ packages/core/utils/src/__tests__/query-populate.test.ts
@@ -1,6 +1,55 @@
 import { traverseQueryPopulate } from '../traverse';
 
 describe('traverseQueryPopulate', () => {
+  test('should not modify wildcard', async () => {
+    const strapi = {
+      getModel: jest.fn((uid) => {
+        return {
+          uid,
+          attributes: {
+            street: {
+              type: 'string',
+            },
+          },
+        };
+      }),
+      db: {
+        metadata: {
+          get: jest.fn(() => ({
+            columnToAttribute: {
+              address: 'address',
+            },
+          })),
+        },
+      },
+    } as any;
+
+    global.strapi = strapi;
+
+    const query = await traverseQueryPopulate(jest.fn(), {
+      schema: {
+        kind: 'collectionType',
+        attributes: {
+          title: {
+            type: 'string',
+          },
+          address: {
+            type: 'relation',
+            relation: 'oneToOne',
+            target: 'api::address.address',
+          },
+          some: {
+            type: 'relation',
+            relation: 'ManyToMany',
+            target: 'api::some.some',
+          },
+        },
+      },
+    })('*');
+
+    expect(query).toEqual('*');
+  });
+
   test('should return only selected populatable field', async () => {
     const strapi = {
       getModel: jest.fn((uid) => {
