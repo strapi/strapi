e5d9e0f539303b30335858ca7aedc4508874f089
Marc-Roig <marc12info@gmail.com>
2024-02-14 18:03:33 +0100

    fix: available statuses

--- packages/core/content-manager/server/src/controllers/collection-types.ts
+++ packages/core/content-manager/server/src/controllers/collection-types.ts
@@ -145,9 +145,9 @@ export default {
 
     const setStatus = (document: any) => {
       // Available status of document
-      const availableStatus = documentsAvailableStatus.find((d: any) => d.id === document.id);
+      const availableStatuses = documentsAvailableStatus.filter((d: any) => d.id === document.id);
       // Compute document version status
-      document.status = documentMetadata.getStatus(document, [availableStatus]);
+      document.status = documentMetadata.getStatus(document, availableStatuses);
       return document;
     };
 
