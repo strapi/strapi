fd9909369a2a0450c77611e0e6b3985af799867e
Ben Irvin <ben@innerdvations.com>
2024-02-22 17:45:50 +0100

    fix: fix the shortening algorithm and add more tests

--- packages/core/database/src/utils/identifiers/__tests__/identifiers.test.ts
+++ packages/core/database/src/utils/identifiers/__tests__/identifiers.test.ts
@@ -1,6 +1,23 @@
-import { createHash, getNameFromTokens, tokenWithHash } from '..';
+import {
+  HASH_LENGTH,
+  HASH_SEPARATOR,
+  IDENTIFIER_SEPARATOR,
+  MIN_TOKEN_LENGTH,
+  createHash,
+  getNameFromTokens,
+  tokenWithHash,
+} from '..';
 
 describe('identifiers', () => {
+  describe('constants', () => {
+    test('HASH_LENGTH === 5', () => {
+      expect(HASH_LENGTH).toBe(5);
+    });
+    test('HASH_SEPARATOR === empty string', () => {
+      expect(HASH_SEPARATOR).toBe('');
+    });
+  });
+
   describe('createHash', () => {
     test('works with even number length', () => {
       const res = createHash('123456789', 2);
@@ -34,20 +51,28 @@ describe('identifiers', () => {
   });
 
   describe('tokenWithHash', () => {
-    test('only shortens when necessary', () => {
+    test('does not add hash when len == input length', () => {
       const res = tokenWithHash('1234567890', 10);
       expect(res).toEqual('1234567890');
     });
-    test('shortens to correct length', () => {
-      const res = tokenWithHash('1234567890', 7);
-      expect(res).toEqual('1234xcd');
+    test('returns original string when len > input length', () => {
+      const res = tokenWithHash('1234567890', 100);
+      expect(res).toEqual('1234567890');
     });
-    test('works with edge case - min length', () => {
-      const res = tokenWithHash('1234567890', 5);
-      expect(res).toEqual('12xcd');
+    test('throws when len < HASH_LENGTH + MIN_TOKEN_LENGTH', () => {
+      expect(() => tokenWithHash('1234567890', HASH_LENGTH + MIN_TOKEN_LENGTH - 1)).toThrow(
+        'length for part of identifier too short, minimum is hash length (5) plus min token length (3), received 7'
+      );
     });
-    test('throws when len < (HASH_LENGTH + 1 (_) + 2 (min identifier length))', () => {
-      expect(() => tokenWithHash('1234567890', 4)).toThrow('length too short');
+    test('adds hash when len < input length (with correct length)', () => {
+      const len = 9;
+      const res = tokenWithHash('1234567890', len);
+      expect(res).toEqual('1234cd65a');
+      expect(res.length).toBe(len);
+    });
+    test('adds hash when len == HASH_LENGTH + MIN_TOKEN_LENGTH', () => {
+      const res = tokenWithHash('1234567890', 9);
+      expect(res).toEqual('1234cd65a');
     });
     test('throws when len === 0', () => {
       expect(() => tokenWithHash('1234567890', 0)).toThrow('length must be a positive integer');
@@ -73,6 +98,7 @@ describe('identifiers', () => {
         )
       ).toThrow('snake_case');
     });
+
     test('does not shorten strings that fit in min length', () => {
       const name = getNameFromTokens(
         [
@@ -80,22 +106,115 @@ describe('identifiers', () => {
           { name: '12345', compressible: true },
           { name: 'links', compressible: false },
         ],
-        23
+        22
       );
       expect(name).toEqual('1234567890_12345_links');
     });
-    test('shortens strings that do not fit in min length (two compressible)', () => {
+
+    test('shortens string that does not fit in min length (one compressible)', () => {
+      const name = getNameFromTokens([{ name: '123456789012345', compressible: true }], 13);
+      expect(name).toEqual('1234567878db8');
+    });
+
+    test('returns original string when it fits (one compressible)', () => {
+      const name = getNameFromTokens([{ name: '12345', compressible: true }], 5);
+      expect(name).toEqual('12345');
+
+      const name2 = getNameFromTokens([{ name: '12345', compressible: true }], 10);
+      expect(name2).toEqual('12345');
+    });
+
+    test('shortens long string that do not fit in min length (two compressible one of which short, one suffix)', () => {
       const name = getNameFromTokens(
         [
           { name: '1234567890', compressible: true },
           { name: '12345', compressible: true },
           { name: 'links', compressible: false },
         ],
-        20
+        21
       );
-      expect(name).toEqual('12345xcd_12345_links');
+      expect(name).toEqual('1234cd65a_12345_links');
     });
-    test('shortens strings that do not fit in min length (three compressible)', () => {
+
+    test('throws when cannot compress without violating min length rules', () => {
+      expect(() =>
+        getNameFromTokens(
+          [
+            { name: '1234567890', compressible: true },
+            { name: '1234567890', compressible: true },
+            { name: 'links', compressible: false },
+          ],
+          21
+        )
+      ).toThrow(
+        'Maximum length is too small to accommodate all tokens'
+        // This means that it's trying to call createHash('1234567890', 7) because: 21 - 5 (links length) - 2 (separators length) = 14, 14/2 = 7
+        // 'length for part of identifier too short, minimum is hash length (5) plus min token length (3), received 7 for token 1234567890'
+      );
+    });
+
+    test('shortens two long strings when maxLength is the shortest possible', () => {
+      const len = (MIN_TOKEN_LENGTH + HASH_LENGTH) * 2 + 5 + IDENTIFIER_SEPARATOR.length * 2;
+      expect(len).toBe(23);
+
+      const name = getNameFromTokens(
+        [
+          { name: '1234567890', compressible: true },
+          { name: '1234567890', compressible: true },
+          { name: 'links', compressible: false },
+        ],
+        len
+      );
+      expect(name).toEqual('123cd65a_123cd65a_links');
+    });
+
+    test('works with max capacity', () => {
+      const res = getNameFromTokens(
+        [
+          { name: '12', compressible: true },
+          { name: '12', compressible: true },
+          { name: '12', compressible: true },
+          { name: '12', compressible: true },
+        ],
+        12
+      );
+      expect(res).toBe('12_12_12_12');
+    });
+
+    test('throws when compressible strings cannot fit', () => {
+      expect(() =>
+        getNameFromTokens(
+          [
+            { name: '12', compressible: true },
+            { name: '12', compressible: true },
+            { name: '12', compressible: true },
+            { name: '1', compressible: true },
+            { name: '12', compressible: true },
+          ],
+          12
+        )
+      ).toThrow('Maximum length is too small to accommodate all tokens');
+    });
+
+    test('throws when incompressible string cannot fit', () => {
+      expect(() => getNameFromTokens([{ name: '123456', compressible: false }], 5)).toThrow(
+        'incompressible string length greater than maxLength'
+      );
+    });
+
+    test('throws when incompressible strings cannot fit due to separators', () => {
+      expect(() =>
+        getNameFromTokens(
+          [
+            { name: '123456', compressible: false },
+            { name: '123456', compressible: false },
+          ],
+          12
+        )
+      ).toThrow('incompressible string length greater than maxLength');
+    });
+
+    test('shortens strings that result in exactly maxLength (three compressible, suffix)', () => {
       const name = getNameFromTokens(
         [
           { name: '1234567890', compressible: true },
@@ -103,9 +222,21 @@ describe('identifiers', () => {
           { name: '0987654321', compressible: true },
           { name: 'links', compressible: false },
         ],
-        25
+        30
       );
-      expect(name).toEqual('123xcd_12345_098x4a_links');
+      expect(name).toEqual('1234cd65a_12345_0984addb_links');
     });
+  });
+  test('shortens strings that do not fit in min length (three compressible, prefix)', () => {
+    const name = getNameFromTokens(
+      [
+        { name: 'inv_order', compressible: false },
+        { name: '1234567890', compressible: true },
+        { name: '12345', compressible: true },
+        { name: '0987654321', compressible: true },
+      ],
+      34
+    );
+    expect(name).toEqual('inv_order_1234cd65a_12345_0984addb');
   });
 });
--- packages/core/database/src/utils/identifiers/index.ts
+++ packages/core/database/src/utils/identifiers/index.ts
@@ -8,13 +8,19 @@ import crypto from 'node:crypto';
  * is different.
  *
  * If there are any test failures after updating this code, it means there is a breaking change that
- * will cause data loss, so beware
+ * will cause data loss, so beware; do not update the test to match your changes
  */
 
 // TODO: Names will not be shortened until this is set to a non-zero number
 export const MAX_DB_IDENTIFIER_LENGTH = 0;
-export const HASH_LENGTH = 4;
+
+// We will have 55 total length available
+// That means we can accept a number of compressible tokens up to:
+// tokens accepted = (MAX_LENGTH / (HASH_LENGTH + MIN_TOKEN_LENGTH) + (tokens * IDENTIFIER_SEPARATER.length))
+export const HASH_LENGTH = 5;
 export const HASH_SEPARATOR = ''; // no separator is needed, we will just attach hash directly to shortened name
+export const IDENTIFIER_SEPARATOR = '_';
+export const MIN_TOKEN_LENGTH = 3;
 
 export const ENTITY = 'entity';
 export const ID_COLUMN = 'id';
@@ -38,35 +44,41 @@ type NameOptions = {
 // returns a hash of length len
 export function createHash(data: string, len: number): string {
   if (!_.isInteger(len) || len <= 0) {
-    throw new Error('length must be a positive integer');
+    throw new Error(`createHash length must be a positive integer, received ${len}`);
   }
 
   // TODO: shake256 is based on SHA-3 and is slow, we don't care about cryptographically secure, only uniqueness and speed
-  //       investigate alternatives before releasing this
+  //       investigate alternatives before releasing this. But it is only run on startup, so it should be fine.
   const hash = crypto.createHash('shake256', { outputLength: Math.ceil(len / 2) }).update(data);
   return hash.digest('hex').substring(0, len);
 }
 
 export function tokenWithHash(name: string, len: number) {
   if (!_.isInteger(len) || len <= 0) {
-    throw new Error('length must be a positive integer');
+    throw new Error(`tokenWithHash length must be a positive integer, received ${len}`);
   }
   if (name.length <= len) {
     return name;
   }
-
-  const availableLength = len - HASH_LENGTH - 1;
-  if (availableLength < 2) {
-    throw new Error(`length too short, minimum is ${HASH_LENGTH + 3}, received ${len}`);
+  if (len < MIN_TOKEN_LENGTH + HASH_LENGTH) {
+    throw new Error(
+      `length for part of identifier too short, minimum is hash length (${HASH_LENGTH}) plus min token length (${MIN_TOKEN_LENGTH}), received ${len} for token ${name}`
+    );
   }
 
-  console.log('tokenWithHash', name, len, availableLength);
+  const availableLength = len - HASH_LENGTH - HASH_SEPARATOR.length;
+  if (availableLength < MIN_TOKEN_LENGTH) {
+    throw new Error(
+      `length for part of identifier minimum is less than min token length (${MIN_TOKEN_LENGTH}), received ${len} for token ${name}`
+    );
+  }
+
   return `${name.substring(0, availableLength)}${HASH_SEPARATOR}${createHash(name, HASH_LENGTH)}`;
 }
 
 export function getNameFromTokens(nameTokens: NameToken[], maxLength = MAX_DB_IDENTIFIER_LENGTH) {
   if (!_.isInteger(maxLength) || maxLength < 0) {
-    throw new Error('max length must be a positive integer or 0 to disable');
+    throw new Error('maxLength must be a positive integer or 0 to disable');
   }
 
   // Ensure all tokens are in snake_case
@@ -85,40 +97,76 @@ export function getNameFromTokens(nameTokens: NameToken[], maxLength = MAX_DB_ID
     { compressible: [], incompressible: [] }
   );
 
-  const incompressibleLength = incompressible.reduce((sum, token) => sum + token.name.length, 0);
-  const separatorsLength = HASH_SEPARATOR.length * (nameTokens.length - 1);
-  let available = maxLength - incompressibleLength - separatorsLength;
-
-  if (available <= 0) {
-    throw new Error('Not enough space available to shorten identifier');
+  const fullLengthName = nameTokens.map((token) => token.name).join(IDENTIFIER_SEPARATOR);
+  if (fullLengthName.length <= maxLength) {
+    return fullLengthName;
   }
 
-  // Calculate initial available length per compressible token
-  let availablePerToken = available / compressible.length;
+  const incompressibleLength = incompressible.reduce((sum, token) => sum + token.name.length, 0);
+  const separatorsLength = nameTokens.length * IDENTIFIER_SEPARATOR.length - 1;
+  if (incompressibleLength + separatorsLength > maxLength) {
+    throw new Error('incompressible string length greater than maxLength');
+  }
 
-  // Sort compressible tokens by length (shortest first) to redistribute length more effectively
-  compressible.sort((a, b) => a.name.length - b.name.length);
+  const available = maxLength - incompressibleLength - separatorsLength;
 
-  // Redistribute lengths
-  compressible.forEach((token, index) => {
+  // Calculate available length per compressible token
+  const availablePerToken = Math.floor(available / compressible.length);
+
+  // Calculate the remainder from the division and add it to the surplus
+  let surplus = available % compressible.length;
+
+  // Check that it's even possible to proceed
+  const totalLength = nameTokens.reduce((total, token) => {
+    if (token.compressible) {
+      if (token.name.length < availablePerToken) {
+        return total + token.name.length;
+      }
+      return total + HASH_LENGTH + MIN_TOKEN_LENGTH;
+    }
+    return total + token.name.length;
+  }, nameTokens.length * IDENTIFIER_SEPARATOR.length - 1);
+
+  // Check if the maximum length is less than the total length
+  if (maxLength < totalLength) {
+    throw new Error('Maximum length is too small to accommodate all tokens');
+  }
+
+  // Calculate total surplus length from shorter strings and total deficit length from longer strings
+  let deficits: NameToken[] = [];
+  compressible.forEach((token) => {
     const actualLength = token.name.length;
     if (actualLength < availablePerToken) {
-      // Redistribute surplus from this token to others
-      const surplus = availablePerToken - actualLength;
-      available -= actualLength; // Decrease available total by the actual length used
-      const remainingTokens = compressible.length - (index + 1);
-      if (remainingTokens > 0) {
-        availablePerToken += surplus / remainingTokens; // Redistribute surplus to remaining tokens
-      }
+      surplus += availablePerToken - actualLength;
+      token.allocatedLength = actualLength;
     } else {
-      token.allocatedLength = Math.floor(availablePerToken); // Allocate length to this token
-      available -= token.allocatedLength; // Update available length
-      if (index < compressible.length - 1) {
-        // Recalculate availablePerToken for remaining tokens if not the last token
-        availablePerToken = available / (compressible.length - (index + 1));
-      }
+      token.allocatedLength = availablePerToken;
+      deficits.push(token);
     }
   });
+
+  // Redistribute surplus length to longer strings, one character at a time
+  // This way we avoid issues with greed and trying to handle floating points by dividing available length
+  function filterAndIncreaseLength(token: NameToken) {
+    if (token.allocatedLength! < token.name.length && surplus > 0) {
+      token.allocatedLength! += 1;
+      surplus -= 1;
+      return true; // Keep this token in the deficits array for the next round
+    }
+    return false; // Remove this token from the deficits array
+  }
+
+  // Redistribute surplus length to longer strings, one character at a time
+  let previousSurplus = surplus + 1; // infinite loop protection
+  while (surplus > 0 && deficits.length > 0) {
+    deficits = deficits.filter((token) => filterAndIncreaseLength(token));
+
+    // infinite loop protection; if the surplus hasn't changed, there's nothing left to distribute it to
+    if (surplus === previousSurplus) {
+      break;
+    }
+    previousSurplus = surplus;
+  }
 
   // Build final string
   const shortenedName = nameTokens
@@ -132,7 +180,13 @@ export function getNameFromTokens(nameTokens: NameToken[], maxLength = MAX_DB_ID
       }
       return token.name;
     })
-    .join('_');
+    .join(IDENTIFIER_SEPARATOR);
+
+  if (shortenedName.length > maxLength) {
+    throw new Error(
+      `name shortening failed to generate a name of the correct maxLength; name ${shortenedName}`
+    );
+  }
 
   return shortenedName;
 }
