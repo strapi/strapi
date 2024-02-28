e9868d3b44324f9b97560f22146e619b2c0addab
Ben Irvin <ben@innerdvations.com>
2024-02-28 18:20:57 +0100

    test: clean up test utils

--- e2e/utils/shared.ts
+++ e2e/utils/shared.ts
@@ -7,17 +7,8 @@ export const describeOnCondition = (shouldDescribe: boolean) =>
   shouldDescribe ? test.describe : test.describe.skip;
 
 /**
- * Execute a test suite only if the condition is true
+ * Navigate to a page and confirm the header, awaiting each step
  */
-type Role = Parameters<Page['getByRole']>[0];
-type GetByRoleOptions = Parameters<Page['getByRole']>[1];
-
-export const waitVisible = async (page: Page, role: Role, name: GetByRoleOptions['name']) => {
-  // Use waitFor() on the locator to wait for the element to become visible
-  const headerLocator = page.getByRole(role, { name });
-  await headerLocator.waitFor({ state: 'visible' });
-};
-
 export const navToHeader = async (page: Page, navItems: string[], headerText: string) => {
   for (const navItem of navItems) {
     // This does not use getByRole because sometimes "Settings" is "Settings 1" if there's a badge notification
