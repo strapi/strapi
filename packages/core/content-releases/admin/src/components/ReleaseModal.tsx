086f1257d6aa2160a87db0dcbeaa8bbcbb353260
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2024-02-29 11:04:43 +0100

    fix: min date considering selected timezone added for scheduling

--- packages/core/content-releases/admin/src/components/ReleaseModal.tsx
+++ packages/core/content-releases/admin/src/components/ReleaseModal.tsx
@@ -17,7 +17,7 @@ import {
   ComboboxOption,
 } from '@strapi/design-system';
 import { formatISO, parse } from 'date-fns';
-import { zonedTimeToUtc } from 'date-fns-tz';
+import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz';
 import { Formik, Form, useFormikContext } from 'formik';
 import { useIntl } from 'react-intl';
 import { useLocation } from 'react-router-dom';
@@ -64,7 +64,7 @@ export const ReleaseModal = ({
     const { date, time, timezone } = values;
     if (!date || !time || !timezone) return null;
     const formattedDate = parse(time, 'HH:mm', new Date(date));
-    const timezoneWithoutOffset = timezone.split('_')[1];
+    const timezoneWithoutOffset = timezone.slice(timezone.indexOf('_') + 1);
     return zonedTimeToUtc(formattedDate, timezoneWithoutOffset);
   };
 
@@ -73,7 +73,7 @@ export const ReleaseModal = ({
    */
   const getTimezoneWithOffset = () => {
     const currentTimezone = timezoneList.find(
-      (timezone) => timezone.value.split('_')[1] === initialValues.timezone
+      (timezone) => timezone.value.slice(timezone.value.indexOf('_') + 1) === initialValues.timezone
     );
     return currentTimezone?.value || systemTimezone.value;
   };
@@ -96,7 +96,9 @@ export const ReleaseModal = ({
         onSubmit={(values) => {
           handleSubmit({
             ...values,
-            timezone: values.timezone ? values.timezone.split('_')[1] : null,
+            timezone: values.timezone
+              ? values.timezone.slice(values.timezone.indexOf('_') + 1)
+              : null,
             scheduledAt: values.isScheduled ? getScheduledTimestamp(values) : null,
           });
         }}
@@ -184,6 +186,10 @@ export const ReleaseModal = ({
                               }}
                               selectedDate={values.date || undefined}
                               required
+                              minDate={utcToZonedTime(
+                                new Date(),
+                                values.timezone.slice(values.timezone.indexOf('_') + 1)
+                              )}
                             />
                           </Box>
                           <Box width="100%">
@@ -260,7 +266,9 @@ const getTimezones = (selectedDate: Date) => {
   });
 
   const systemTimezone = timezoneList.find(
-    (timezone) => timezone.value.split('_')[1] === Intl.DateTimeFormat().resolvedOptions().timeZone
+    (timezone) =>
+      timezone.value.slice(timezone.value.indexOf('_') + 1) ===
+      Intl.DateTimeFormat().resolvedOptions().timeZone
   );
 
   return { timezoneList, systemTimezone };
@@ -279,7 +287,11 @@ const TimezoneComponent = ({ timezoneOptions }: { timezoneOptions: ITimezoneOpti
 
       const updatedTimezone =
         values.timezone &&
-        timezoneList.find((tz) => tz.value.split('_')[1] === values.timezone!.split('_')[1]);
+        timezoneList.find(
+          (tz) =>
+            tz.value.slice(tz.value.indexOf('_') + 1) ===
+            values.timezone!.slice(values.timezone!.indexOf('_') + 1)
+        );
       if (updatedTimezone) {
         setFieldValue('timezone', updatedTimezone!.value);
       }
