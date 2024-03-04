e5f5492d3640d1de4ed496206bb9f2455436e59d
Bassel Kanso <basselkanso82@gmail.com>
2024-03-04 16:50:50 +0200

    fix: change test name and add comments

--- e2e/tests/content-type-builder/ctb-edit-view.spec.ts
+++ e2e/tests/content-type-builder/ctb-edit-view.spec.ts
@@ -13,11 +13,14 @@ test.describe('Edit View CTB', () => {
   test('A user should be able to navigate to the Edit View of the content type builder and see the contentype fields', async ({
     page,
   }) => {
+    // Nabviagte to the content type builder
     await navToHeader(page, ['Content-Type Builder'], 'Article');
     const modal = page.getByRole('button', { name: 'Close' });
+    // Close the tour modal if it is visible
     if (modal.isVisible()) {
       modal.click();
     }
+    // Navigate to the Shop single type and verify some essential buttons are visible
     await page.getByRole('link', { name: 'Shop' }).click();
     await expect(
       page.getByRole('button', {
