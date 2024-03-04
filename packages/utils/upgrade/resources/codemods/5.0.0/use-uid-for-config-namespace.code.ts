3af836e7c017c7dd416a316957c9bbff94713e45
Ben Irvin <ben@innerdvations.com>
2024-03-04 15:28:30 +0100

    enhancement: update api paths as well when possible

--- packages/utils/upgrade/resources/codemods/5.0.0/use-uid-for-config-namespace.code.ts
+++ packages/utils/upgrade/resources/codemods/5.0.0/use-uid-for-config-namespace.code.ts
@@ -3,10 +3,13 @@ import { Transform } from 'jscodeshift';
 /**
  * Replaces string dot format for config get/set/has with uid format for 'plugin' namespace
  * For example, `strapi.config.get('plugin.anyString')` will become `strapi.config.get('plugin::anyString')`
+ * Also replaces 'api.' with 'api::' except when it's followed by 'rest' or 'responses' (the valid api config values in Strapi v4)
  */
 const transform: Transform = (file, api) => {
   const jscodeshift = api.jscodeshift;
   const root = jscodeshift(file.source);
+
+  const ignoreList = ['api.rest', 'api.responses'];
 
   ['get', 'has', 'set'].forEach((configMethod) => {
     root
@@ -19,7 +22,8 @@ const transform: Transform = (file, api) => {
           {
             type: 'Literal',
             value: (literalValue: unknown) =>
-              typeof literalValue === 'string' && literalValue.startsWith('plugin.'),
+              typeof literalValue === 'string' &&
+              (literalValue.startsWith('plugin.') || literalValue.startsWith('api.')),
           },
         ],
       })
@@ -30,6 +34,11 @@ const transform: Transform = (file, api) => {
           argumentNode.type === 'Literal' &&
           typeof argumentNode.value === 'string'
         ) {
+          if (
+            ignoreList.some((ignoreItem) => (argumentNode.value as string).startsWith(ignoreItem))
+          ) {
+            return;
+          }
           argumentNode.value = argumentNode.value.replace('.', '::');
         }
       });
