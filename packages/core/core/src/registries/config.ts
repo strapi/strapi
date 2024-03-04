b0beebb48b2997651a981e4693fae59ea41f87e4
Ben Irvin <ben@innerdvations.com>
2024-03-04 10:28:19 +0100

    fix: accept strapi for logging

--- packages/core/core/src/registries/config.ts
+++ packages/core/core/src/registries/config.ts
@@ -1,25 +1,28 @@
 import type { ConfigProvider } from '@strapi/types';
 import { get, set, has, isString, type PropertyName } from 'lodash';
+import type { LoadedStrapi } from '../Strapi';
 
 type Config = Record<string, unknown>;
 
-// Accessing model configs with dot (.) was deprecated between v4->v5, but to avoid a major breaking change
-// we will still support certain namespaces, currently only 'plugin.'
-const transformDeprecatedPaths = (path: PropertyName) => {
-  if (isString(path) && path.startsWith('plugin.')) {
-    console.warn(
-      `DEPRECATION: config namespaces with the uid (:: instead of .) -- config.get for requested ${path} found at ${path.replace(
-        '.',
-        '::'
-      )}; `
-    );
-    return path.replace('plugin.', 'plugin::');
-  }
-
-  return path;
-};
-export default (initialConfig = {}): ConfigProvider => {
+export default (initialConfig = {}, strapi?: Partial<LoadedStrapi>): ConfigProvider => {
   const _config: Config = { ...initialConfig }; // not deep clone because it would break some config
+
+  // Accessing model configs with dot (.) was deprecated between v4->v5, but to avoid a major breaking change
+  // we will still support certain namespaces, currently only 'plugin.'
+  const transformDeprecatedPaths = (path: PropertyName) => {
+    if (isString(path) && path.startsWith('plugin.')) {
+      // strapi may not be loaded yet, so fall back to console
+      (strapi?.log?.info || console.info)(
+        `DEPRECATION: Use plugin:: prefix insetad of  requested config ${path} found at ${path.replace(
+          '.',
+          '::'
+        )}; `
+      );
+      return path.replace('plugin.', 'plugin::');
+    }
+
+    return path;
+  };
 
   return {
     ..._config, // TODO: to remove
