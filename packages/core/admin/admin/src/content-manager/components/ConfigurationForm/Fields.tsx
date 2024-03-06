f67335d69ea4357828d0580c941e664abb66d1bb
Jamie Howard <jamie.howard@strapi.io>
2024-03-06 14:17:19 +0000

    fix(admin): allow empty list view configuration

--- packages/core/admin/admin/src/content-manager/components/ConfigurationForm/Fields.tsx
+++ packages/core/admin/admin/src/content-manager/components/ConfigurationForm/Fields.tsx
@@ -46,9 +46,9 @@ interface FieldsProps extends Pick<EditLayout, 'metadatas'>, Pick<FieldProps, 'c
 const Fields = ({ attributes, fieldSizes, components, metadatas = {} }: FieldsProps) => {
   const { formatMessage } = useIntl();
 
-  const layout = useForm(
+  const layout = useForm<ConfigurationFormData['layout']>(
     'Fields',
-    (state) => state.values.layout as ConfigurationFormData['layout']
+    (state) => state.values.layout ?? []
   );
   const onChange = useForm('Fields', (state) => state.onChange);
   const addFieldRow = useForm('Fields', (state) => state.addFieldRow);
