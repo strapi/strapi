14a91aaea5dca7d469bf36112c0f1708f56e206f
Convly <jean-sebastien.herbaux@epitech.eu>
2024-03-06 12:07:30 +0100

    chore: add more documentation for strapi/types.Utils

--- packages/core/types/src/utils/tuple.ts
+++ packages/core/types/src/utils/tuple.ts
@@ -1,7 +1,17 @@
 import type { Literal } from './string';
 
 /**
- * Aggregate the given tuple into a string, separated by the given `TSeparator` literal
+ * Transforms a tuple ({@link TCollection}) into a concatenated string, interlaced with a designated separator character ({@link TSeparator}).
+ *
+ * @template TCollection - Represents the array of elements to be joined.
+ * @template TSeparator - Represents the separator character used to join the elements of {@link TCollection}.
+ *
+ * @example
+ * ```typescript
+ * type R1 = Join<["John", "Doe", "Smith"], ",">; // type R1 = "John,Doe,Smith"
+ * type R2 = Join<[1, 2, 3, 4], "-">; // type R2 = "1-2-3-4"
+ * type R3 = Join<["Foo", "Bar", "Baz"], "," | "-">; // type R3 = "Foo,Bar,Baz" | "Foo-Bar-Baz"
+ * ```
  */
 export type Join<TCollection extends unknown[], TSeparator extends Literal> = TCollection extends [
   infer THead extends Literal,
