d7df66881f93a43cef7269ca53afb93838616f47
Marc-Roig <marc12info@gmail.com>
2024-03-15 11:12:58 +0100

    fix: date comparison

--- packages/core/content-manager/server/src/services/document-metadata.ts
+++ packages/core/content-manager/server/src/services/document-metadata.ts
@@ -31,19 +31,7 @@ const CONTENT_MANAGER_STATUS = {
   MODIFIED: 'modified',
 };
 
-/**
- * TODO: Remove this and make updatedAt dates be equal when publishing on the document-engine
- * Compares two dates and returns true if the absolute difference between them is less than or equal to the specified threshold.
- * @param date1 The first date to compare.
- * @param date2 The second date to compare.
- * @param threshold The threshold in milliseconds.
- * @returns True if the absolute difference between the dates is less than or equal to the threshold, false otherwise.
- */
-const areDatesEqual = (
-  date1: Date | string | null,
-  date2: Date | string | null,
-  threshold: number
-): boolean => {
+const areDatesEqual = (date1: Date | string | null, date2: Date | string | null): boolean => {
   if (!date1 || !date2) {
     return false;
   }
@@ -52,7 +40,7 @@ const areDatesEqual = (
   const time2 = new Date(date2).getTime();
   const difference = Math.abs(time1 - time2);
 
-  return difference <= threshold;
+  return difference === 0;
 };
 
 /**
@@ -157,7 +145,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
     }
 
     // The draft version is the same as the published version
-    if (areDatesEqual(version.updatedAt, otherDocumentStatuses.at(0)?.updatedAt, 500)) {
+    if (areDatesEqual(version.updatedAt, otherDocumentStatuses.at(0)?.updatedAt)) {
       return CONTENT_MANAGER_STATUS.PUBLISHED;
     }
 
