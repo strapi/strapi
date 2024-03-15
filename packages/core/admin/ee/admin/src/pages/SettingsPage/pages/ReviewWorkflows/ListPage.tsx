f2e89269f933bb182ea537a1d039072ad2a34c60
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-03-15 15:33:24 +0000

    chore!: move confirm dialog to admin package and refactor (#19782)

--- packages/core/admin/ee/admin/src/pages/SettingsPage/pages/ReviewWorkflows/ListPage.tsx
+++ packages/core/admin/ee/admin/src/pages/SettingsPage/pages/ReviewWorkflows/ListPage.tsx
@@ -16,7 +16,6 @@ import {
 } from '@strapi/design-system';
 import { Link, LinkButton } from '@strapi/design-system/v2';
 import {
-  ConfirmDialog,
   onRowClick,
   useAPIErrorHandler,
   useNotification,
@@ -28,6 +27,7 @@ import { useIntl } from 'react-intl';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { ConfirmDialog } from '../../../../../../../admin/src/components/ConfirmDialog';
 import { Page } from '../../../../../../../admin/src/components/PageHelpers';
 import { useTypedSelector } from '../../../../../../../admin/src/core/store/hooks';
 import { useContentTypes } from '../../../../../../../admin/src/hooks/useContentTypes';
@@ -365,16 +365,16 @@ export const ReviewWorkflowsListView = () => {
         )}
 
         <ConfirmDialog
-          bodyText={{
+          isOpen={!!workflowToDelete}
+          onClose={toggleConfirmDeleteDialog}
+          onConfirm={handleConfirmDeleteDialog}
+        >
+          {formatMessage({
             id: 'Settings.review-workflows.list.page.delete.confirm.body',
             defaultMessage:
               'If you remove this worfklow, all stage-related information will be removed for this content-type. Are you sure you want to remove it?',
-          }}
-          isConfirmButtonLoading={isDeleting}
-          isOpen={!!workflowToDelete}
-          onToggleDialog={toggleConfirmDeleteDialog}
-          onConfirm={handleConfirmDeleteDialog}
-        />
+          })}
+        </ConfirmDialog>
 
         <LimitsModal.Root isOpen={showLimitModal} onClose={() => setShowLimitModal(false)}>
           <LimitsModal.Title>
