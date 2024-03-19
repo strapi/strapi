b450051520789cdf2f997d51fa9957cb42f52618
Ben Irvin <ben@innerdvations.com>
2024-03-19 11:27:58 +0100

    fix: remove debugging

--- packages/core/database/src/migrations/internal-migrations/5.0.0-convert-identifiers-long-than-max-length.ts
+++ packages/core/database/src/migrations/internal-migrations/5.0.0-convert-identifiers-long-than-max-length.ts
@@ -1,6 +1,5 @@
 import type { Knex } from 'knex';
 import createDebug from 'debug';
-import fs from 'node:fs';
 import type { Migration } from '../common';
 import type { Metadata } from '../../metadata';
 import { type Database, type MetadataOptions } from '../..';
@@ -24,7 +23,6 @@ export const renameIdentifiersLongerThanMaxLength: Migration = {
   name: '5.0.0-rename-identifiers-longer-than-max-length',
   async up(knex, db) {
     const md = db.metadata;
-    console.log('metadata', fs.writeFileSync('md.json', JSON.stringify(Array.from(md), null, 2)));
 
     const maxLength = db.DEFAULT_MAX_IDENTIFIER_LENGTH;
 
