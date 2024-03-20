d3e0d63386ec4e5a156f078d22d2e36864a7e86a
Ben Irvin <ben@innerdvations.com>
2024-03-20 12:56:41 +0100

    revert: revert accidental push to v5/main

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -8,29 +8,19 @@ const config: UserConfig = {
       RuleConfigSeverity.Error,
       'always',
       [
-        'build',
         'chore',
         'ci',
-        'comments',
         'docs',
-        'enh',
         'enhancement',
         'feat',
         'fix',
         'release',
         'revert',
         'security',
-        'style',
         'test',
       ],
     ],
   },
-  ignores: [
-    (commitMessage) => {
-      // add an exception for github
-      return /^Merge branch '.*' into [a-zA-Z0-9\/\-_]+$/.test(commitMessage);
-    },
-  ],
 };
 
 export default config;
