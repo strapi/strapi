26b6bec411e17fd03c2c590e33ebfd9ef7f1bc82
Convly <jean-sebastien.herbaux@epitech.eu>
2024-03-21 14:39:02 +0100

    enhancement: only add the publication state methods when D&P is enabled on the CT

--- packages/core/types/src/modules/documents/service-instance.ts
+++ packages/core/types/src/modules/documents/service-instance.ts
@@ -4,73 +4,69 @@ import type { IsDraftAndPublishEnabled } from './draft-and-publish';
 import type * as Params from './params/document-engine';
 import type * as Result from './result/document-engine';
 
-export type ServiceInstance<TContentTypeUID extends UID.ContentType = UID.ContentType> = {
-  findMany: <TParams extends Params.FindMany<TContentTypeUID>>(
-    params?: TParams
-  ) => Result.FindMany<TContentTypeUID, TParams>;
+export type ServiceInstance<TContentTypeUID extends UID.ContentType = UID.ContentType> =
+  Utils.Intersect<
+    [
+      // Base methods (findMany, create, update, etc...)
+      BaseServiceInstance,
+      // Publication methods are only enabled if D&P is enabled for the content type
+      Utils.If<IsDraftAndPublishEnabled<TContentTypeUID>, DraftAndPublishMethods, unknown>
+    ]
+  >;
 
-  findFirst: <TParams extends Params.FindFirst<TContentTypeUID>>(
+type BaseServiceInstance<TContentTypeUID extends UID.ContentType = UID.ContentType> = {
+  findMany<TParams extends Params.FindMany<TContentTypeUID>>(
     params?: TParams
-  ) => Result.FindFirst<TContentTypeUID, TParams>;
+  ): Result.FindMany<TContentTypeUID, TParams>;
 
-  findOne: <TParams extends Params.FindOne<TContentTypeUID>>(
+  findFirst<TParams extends Params.FindFirst<TContentTypeUID>>(
+    params?: TParams
+  ): Result.FindFirst<TContentTypeUID, TParams>;
+
+  findOne<TParams extends Params.FindOne<TContentTypeUID>>(
     id: ID,
     params?: TParams
-  ) => Result.FindOne<TContentTypeUID, TParams>;
+  ): Result.FindOne<TContentTypeUID, TParams>;
 
-  delete: <TParams extends Params.Delete<TContentTypeUID>>(
+  delete<TParams extends Params.Delete<TContentTypeUID>>(
     documentId: ID,
     params?: TParams
-  ) => Result.Delete;
+  ): Result.Delete;
 
-  create: <TParams extends Params.Create<TContentTypeUID>>(
+  create<TParams extends Params.Create<TContentTypeUID>>(
     params: TParams
-  ) => Result.Create<TContentTypeUID, TParams>;
+  ): Result.Create<TContentTypeUID, TParams>;
 
   /**
    * @internal
    * Exposed for use within the Strapi Admin Panel
    */
-  clone: <TParams extends Params.Clone<TContentTypeUID>>(
+  clone<TParams extends Params.Clone<TContentTypeUID>>(
     documentId: ID,
     params: TParams
-  ) => Result.Clone<TContentTypeUID, TParams>;
+  ): Result.Clone<TContentTypeUID, TParams>;
 
-  update: <TParams extends Params.Update<TContentTypeUID>>(
+  update<TParams extends Params.Update<TContentTypeUID>>(
     documentId: ID,
     params: TParams
-  ) => Result.Update<TContentTypeUID, TParams>;
+  ): Result.Update<TContentTypeUID, TParams>;
 
-  count: <TParams extends Params.Count<TContentTypeUID>>(params?: TParams) => Result.Count;
+  count<TParams extends Params.Count<TContentTypeUID>>(params?: TParams): Result.Count;
+};
 
-  // Publication methods are only enabled if D&P is enabled for the content type
-  publish: Utils.If<
-    // If draft and publish is enabled for the content type
-    IsDraftAndPublishEnabled<TContentTypeUID>,
-    // Then, publish method is enabled
-    <TParams extends Params.Publish<TContentTypeUID>>(
-      documentId: ID,
-      params?: TParams
-    ) => Result.Publish<TContentTypeUID, TParams>,
-    // Otherwise, disable it
-    undefined
-  >;
+type DraftAndPublishMethods<TContentTypeUID extends UID.ContentType = UID.ContentType> = {
+  publish<TParams extends Params.Publish<TContentTypeUID>>(
+    documentId: ID,
+    params?: TParams
+  ): Result.Publish<TContentTypeUID, TParams>;
 
-  unpublish: Utils.If<
-    IsDraftAndPublishEnabled<TContentTypeUID>,
-    <TParams extends Params.Unpublish<TContentTypeUID>>(
-      documentId: ID,
-      params?: TParams
-    ) => Result.Unpublish<TContentTypeUID, TParams>,
-    undefined
-  >;
+  unpublish<TParams extends Params.Unpublish<TContentTypeUID>>(
+    documentId: ID,
+    params?: TParams
+  ): Result.Unpublish<TContentTypeUID, TParams>;
 
-  discardDraft: Utils.If<
-    IsDraftAndPublishEnabled<TContentTypeUID>,
-    <TParams extends Params.DiscardDraft<TContentTypeUID>>(
-      documentId: ID,
-      params?: TParams
-    ) => Result.DiscardDraft<TContentTypeUID, TParams>,
-    undefined
-  >;
+  discardDraft<TParams extends Params.DiscardDraft<TContentTypeUID>>(
+    documentId: ID,
+    params?: TParams
+  ): Result.DiscardDraft<TContentTypeUID, TParams>;
 };
