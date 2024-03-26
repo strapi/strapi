850bc902c04b52695c2e1e8bb5aeadca382fcbb9
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-03-26 10:52:07 +0000

    chore: init review-workflows-package

---
+++ packages/core/review-workflows/packup.config.ts
@@ -0,0 +1,30 @@
+import { Config, defineConfig } from '@strapi/pack-up';
+
+const config: Config = defineConfig({
+  bundles: [
+    {
+      source: './_internal/index.ts',
+      import: './dist/_internal.mjs',
+      require: './dist/_internal.js',
+      types: './dist/_internal/index.d.ts',
+      runtime: 'web',
+    },
+    // {
+    //   source: './server/src/index.ts',
+    //   import: './dist/server/index.mjs',
+    //   require: './dist/server/index.js',
+    //   types: './dist/server/src/index.d.ts',
+    //   tsconfig: './server/tsconfig.build.json',
+    //   runtime: 'node',
+    // },
+  ],
+  dist: './dist',
+  /**
+   * Because we're exporting a server & client package
+   * which have different runtimes we want to ignore
+   * what they look like in the package.json
+   */
+  exports: {},
+});
+
+export default config;
