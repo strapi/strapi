746ebe7325daedbf5916c4e540ef931a1458d42c
Ben Irvin <ben@innerdvations.com>
2024-03-28 17:34:20 +0100

    fix: use custom args

--- e2e/utils/shared.ts
+++ e2e/utils/shared.ts
@@ -25,20 +25,20 @@ export const navToHeader = async (page: Page, navItems: string[], headerText: st
 };
 
 /**
- * Look for an element containing text, and then click a sibling button containing closeText
+ * Look for an element containing text, and then click a sibling close button
  */
 export const findAndClose = async (
   page: Page,
   text: string,
-  toastRole: string = 'status',
-  closeText: string = 'Close'
+  role: string = 'status',
+  closeLabel: string = 'Close'
 ) => {
   // Verify the popup text is visible.
-  await expect(page.locator(`:has-text("${text}")[role="status"]`)).toBeVisible();
+  await expect(page.locator(`:has-text("${text}")[role="${role}"]`)).toBeVisible();
 
   // Find the 'Close' button that is a sibling of the element containing the specified text.
   const closeBtn = await page.locator(
-    `:has-text("${text}")[role="status"] ~ button[aria-label="Close"]`
+    `:has-text("${text}")[role="${role}"] ~ button[aria-label="${closeLabel}"]`
   );
 
   // Click the 'Close' button.
