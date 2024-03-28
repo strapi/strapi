bebdee5fadcb14077425ed8403256976ee59f677
Marc Roig <marc12info@gmail.com>
2024-03-28 08:51:53 +0100

    fix: clean document service relation transforms (#19859)

--- packages/core/core/src/services/document-service/transform/relations/extract/data-ids.ts
+++ packages/core/core/src/services/document-service/transform/relations/extract/data-ids.ts
@@ -1,126 +1,79 @@
-import { isObject } from 'lodash/fp';
-import { Modules, Schema, UID } from '@strapi/types';
-import { traverseEntity } from '@strapi/utils';
+import { curry } from 'lodash/fp';
+
+import { UID } from '@strapi/types';
+
 import { IdMap } from '../../id-map';
-import { ShortHand, LongHand, LongHandDocument } from '../utils/types';
-import { isShortHand, isLongHand } from '../utils/data';
 import { getRelationTargetLocale } from '../utils/i18n';
 import { getRelationTargetStatus } from '../utils/dp';
-/**
- *  Get relation ids from primitive representation (id, id[], {id}, {id}[])
- */
-const handlePrimitive = (
-  relation: ShortHand | LongHand | ShortHand[] | LongHand[] | null | undefined | any
-): LongHandDocument[] => {
-  if (!relation) {
-    return []; // null
-  }
+import { mapRelation, traverseEntityRelations } from '../utils/map-relation';
+import { LongHandDocument } from '../utils/types';
 
-  if (isShortHand(relation)) {
-    return [{ documentId: relation }]; // id
-  }
-
-  if (isLongHand(relation)) {
-    // { documentId, locale? }
-    if ('documentId' in relation) {
-      return [
-        { documentId: relation.documentId, locale: relation.locale, status: relation.status },
-      ];
-    }
-    // { id }
-    return [];
-  }
-
-  if (Array.isArray(relation)) {
-    return relation.map((item) => (isShortHand(item) ? { documentId: item } : item)); // id[]
-  }
-
-  return [];
-};
+interface Options {
+  uid: UID.Schema;
+  locale?: string | null;
+  status?: 'draft' | 'published';
+}
 
 /**
- * Get all relations document ids from a relation input value
+ * Load a relation documentId into the idMap.
  */
-const extractRelationIds = <T extends Schema.Attribute.RelationKind.Any>(
-  relation: Modules.EntityService.Params.Attribute.RelationInputValue<T>
-): LongHandDocument[] => {
-  const ids = handlePrimitive(relation);
-  if (!isObject(relation)) return ids;
+const addRelationDocId = curry(
+  (idMap: IdMap, targetUid: UID.Schema, source: Options, relation: LongHandDocument) => {
+    const targetLocale = getRelationTargetLocale(relation, {
+      targetUid,
+      sourceUid: source.uid,
+      sourceLocale: source.locale,
+    });
 
-  if ('set' in relation) ids.push(...handlePrimitive(relation.set)); // set: id[]
-  if ('disconnect' in relation) ids.push(...handlePrimitive(relation.disconnect)); // disconnect: id[]
-  if ('connect' in relation) {
-    // connect: id[] | { id } | ...
-    if (!relation.connect) return [];
-    ids.push(...handlePrimitive(relation.connect));
+    const targetStatus = getRelationTargetStatus(relation, {
+      targetUid,
+      sourceUid: source.uid,
+      sourceStatus: source.status,
+    });
 
-    // handle positional arguments
-    const connect = Array.isArray(relation.connect) ? relation.connect : [relation.connect];
-    connect.forEach((relation) => {
-      if (isShortHand(relation) || !('position' in relation)) return;
-
-      const { position } = relation;
-
-      // { connect: { id: id, position: { before: id } } }
-      if (position?.before) {
-        ids.push(...handlePrimitive({ ...position, documentId: position.before }));
-      }
-
-      // { connect: { id: id, position: { after: id } } }
-      if (position?.after) {
-        ids.push(...handlePrimitive({ ...position, documentId: position.after }));
-      }
+    targetStatus.forEach((status) => {
+      idMap.add({
+        uid: targetUid,
+        documentId: relation.documentId,
+        locale: targetLocale,
+        status,
+      });
     });
   }
-
-  return ids;
-};
+);
 
 /**
- * Iterate over all attributes of a Data object and extract all relational document ids.
+ * Iterate over all relations of a data object and extract all relational document ids.
  * Those will later be transformed to entity ids.
  */
-const extractDataIds = (
-  idMap: IdMap,
-  data: Record<string, any>,
-  opts: { uid: UID.Schema; locale?: string | null; status?: 'draft' | 'published' }
-) => {
-  return traverseEntity(
-    ({ value, attribute }) => {
-      // Find relational attributes, and return the document ids
-      if (attribute.type === 'relation') {
-        const extractedIds = extractRelationIds(value as any);
+const extractDataIds = (idMap: IdMap, data: Record<string, any>, source: Options) => {
+  return traverseEntityRelations(
+    async ({ attribute, value }) => {
+      const targetUid = attribute.target!;
+      const addDocId = addRelationDocId(idMap, targetUid, source);
 
-        // TODO: Handle morph relations (they have multiple targets)
-        const target = attribute.target;
-        if (!target) return;
+      return mapRelation((relation) => {
+        if (!relation || !relation.documentId) {
+          return relation;
+        }
 
-        // If not connecting to any version on disabled d&p, we should connect to both draft and published relations at the same time
-        extractedIds.forEach((relation) => {
-          const targetLocale = getRelationTargetLocale(relation, {
-            targetUid: target as UID.Schema,
-            sourceUid: opts.uid,
-            sourceLocale: opts.locale,
-          });
+        addDocId(relation);
 
-          const targetStatus = getRelationTargetStatus(relation, {
-            targetUid: target as UID.Schema,
-            sourceUid: opts.uid,
-            sourceStatus: opts.status,
-          });
+        // Handle positional arguments
+        const position = relation.position;
 
-          targetStatus.forEach((status) => {
-            idMap.add({
-              uid: target,
-              documentId: relation.documentId,
-              locale: targetLocale,
-              status,
-            });
-          });
-        });
-      }
+        if (position?.before) {
+          addDocId({ ...relation, ...position, documentId: position.before });
+        }
+
+        if (position?.after) {
+          addDocId({ ...relation, ...position, documentId: position.after });
+        }
+
+        return relation;
+      }, value as any);
     },
-    { schema: strapi.getModel(opts.uid), getModel: strapi.getModel.bind(strapi) },
+    { schema: strapi.getModel(source.uid), getModel: strapi.getModel.bind(strapi) },
     data
   );
 };
