dfc549626cb63b7a1e5a9d465763786879dd1b28
Ben Irvin <ben@innerdvations.com>
2024-03-29 15:21:16 +0100

    test: comment

--- e2e/tests/content-type-builder/collection-type/create-collection-type.spec.ts
+++ e2e/tests/content-type-builder/collection-type/create-collection-type.spec.ts
@@ -20,6 +20,7 @@ test.describe('Create collection type', () => {
     }
   });
 
+  // TODO: each test should have a beforeAll that does this, maybe combine all the setup into one util to simplify it
   // to keep other suites that don't modify files from needing to reset files, clean up after ourselves at the end
   test.afterAll(async () => {
     await resetFiles();
