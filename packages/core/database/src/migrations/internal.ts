58293768a1c460e905b2821f42ca7744da759ceb
Marc-Roig <marc12info@gmail.com>
2024-04-03 11:59:25 +0200

    chore: lazy load umzugProvider migrations

--- packages/core/database/src/migrations/internal.ts
+++ packages/core/database/src/migrations/internal.ts
@@ -10,44 +10,34 @@ import type { Database } from '..';
 export const createInternalMigrationProvider = (db: Database): InternalMigrationProvider => {
   const context = { db };
   const migrations: Migration[] = [...internalMigrations];
-  let umzug: Umzug;
 
-  const getUmzug = () => {
-    if (umzug) {
-      return umzug;
-    }
-
-    return new Umzug({
-      storage: createStorage({ db, tableName: 'strapi_migrations_internal' }),
-      logger: console,
-      context,
-      migrations: migrations.map((migration) => {
+  const umzugProvider = new Umzug({
+    storage: createStorage({ db, tableName: 'strapi_migrations_internal' }),
+    logger: console,
+    context,
+    migrations: () =>
+      migrations.map((migration) => {
         return {
           name: migration.name,
           up: wrapTransaction(context.db)(migration.up),
           down: wrapTransaction(context.db)(migration.down),
         };
       }),
-    });
-  };
+  });
 
   return {
     async register(migration: Migration) {
-      if (umzug !== undefined) {
-        throw new Error('Cannot register new migrations after the provider has been created');
-      }
-
       migrations.push(migration);
     },
     async shouldRun() {
-      const pendingMigrations = await getUmzug().pending();
+      const pendingMigrations = await umzugProvider.pending();
       return pendingMigrations.length > 0;
     },
     async up() {
-      await getUmzug().up();
+      await umzugProvider.up();
     },
     async down() {
-      await getUmzug().down();
+      await umzugProvider.down();
     },
   };
 };
