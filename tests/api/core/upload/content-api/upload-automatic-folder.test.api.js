e83d720a6a4bab0878bb8e8332c99052001f8756
Ben Irvin <ben@innerdvations.com>
2024-04-11 17:14:09 +0200

    test: expect 201 for create

--- tests/api/core/upload/content-api/upload-automatic-folder.test.api.js
+++ tests/api/core/upload/content-api/upload-automatic-folder.test.api.js
@@ -67,7 +67,7 @@ describe('Uploads folder', () => {
         },
       });
 
-      expect(res.statusCode).toBe(200);
+      expect(res.statusCode).toBe(201);
 
       const { body: file } = await rqAdmin({
         method: 'GET',
@@ -308,7 +308,7 @@ describe('Uploads folder', () => {
         },
       });
 
-      expect(res.statusCode).toBe(200);
+      expect(res.statusCode).toBe(201);
 
       const {
         body: { results: files },
@@ -370,7 +370,7 @@ describe('Uploads folder', () => {
         },
       });
 
-      expect(res.statusCode).toBe(200);
+      expect(res.statusCode).toBe(201);
 
       const {
         body: { results: files },
