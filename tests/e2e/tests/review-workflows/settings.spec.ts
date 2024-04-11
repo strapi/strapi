4fabb8aaa605dfd0510f19c25de9adab2413cdc1
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-04-11 10:39:19 +0100

    chore: make settings rw e2e run on EE only

--- tests/e2e/tests/review-workflows/settings.spec.ts
+++ tests/e2e/tests/review-workflows/settings.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../../utils/login';
 import { resetDatabaseAndImportDataFromPath } from '../../utils/dts-import';
-import { findAndClose } from '../../utils/shared';
+import { describeOnCondition, findAndClose } from '../../utils/shared';
 
-test.describe('settings', () => {
+const edition = process.env.STRAPI_DISABLE_EE === 'true' ? 'CE' : 'EE';
+
+describeOnCondition(edition === 'EE')('settings', () => {
   test.beforeEach(async ({ page }) => {
     await resetDatabaseAndImportDataFromPath('with-admin.tar');
     await page.goto('/admin');
