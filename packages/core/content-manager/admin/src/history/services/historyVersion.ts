b4f819aae7583987351af7199315edf1c3f091ee
markkaylor <mark.kaylor@strapi.io>
2024-04-16 09:18:33 +0200

    feat(history): restore version (#20102)

--- packages/core/content-manager/admin/src/history/services/historyVersion.ts
+++ packages/core/content-manager/admin/src/history/services/historyVersion.ts
@@ -1,4 +1,7 @@
-import { GetHistoryVersions } from '../../../../shared/contracts/history-versions';
+import {
+  GetHistoryVersions,
+  RestoreHistoryVersion,
+} from '../../../../shared/contracts/history-versions';
 import { contentManagerApi } from '../../services/api';
 
 const historyVersionsApi = contentManagerApi.injectEndpoints({
@@ -18,9 +21,21 @@ const historyVersionsApi = contentManagerApi.injectEndpoints({
       },
       providesTags: ['HistoryVersion'],
     }),
+    restoreVersion: builder.mutation<RestoreHistoryVersion.Response, RestoreHistoryVersion.Request>(
+      {
+        query({ params, body }) {
+          return {
+            url: `/content-manager/history-versions/${params.versionId}/restore`,
+            method: 'PUT',
+            data: body,
+          };
+        },
+        invalidatesTags: ['HistoryVersion'],
+      }
+    ),
   }),
 });
 
-const { useGetHistoryVersionsQuery } = historyVersionsApi;
+const { useGetHistoryVersionsQuery, useRestoreVersionMutation } = historyVersionsApi;
 
-export { useGetHistoryVersionsQuery };
+export { useGetHistoryVersionsQuery, useRestoreVersionMutation };
