863b960229f74ee2006d6a304efa2b527235ed1b
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2024-04-25 12:45:57 +0200

    fix: use generic translation errors

--- packages/core/content-releases/server/src/controllers/__tests__/release.test.ts
+++ packages/core/content-releases/server/src/controllers/__tests__/release.test.ts
@@ -107,7 +107,7 @@ describe('Release controller', () => {
       };
 
       // @ts-expect-error partial context
-      expect(() => releaseController.create(ctx)).rejects.toThrow('name is a required field');
+      expect(() => releaseController.create(ctx)).rejects.toThrow('This field is required');
     });
   });
 
@@ -129,7 +129,7 @@ describe('Release controller', () => {
       };
 
       // @ts-expect-error partial context
-      expect(() => releaseController.update(ctx)).rejects.toThrow('name is a required field');
+      expect(() => releaseController.update(ctx)).rejects.toThrow('This field is required');
     });
 
     it('throws an error given unknown request arguments', () => {
--- packages/core/content-releases/shared/validation-schemas.ts
+++ packages/core/content-releases/shared/validation-schemas.ts
@@ -1,24 +1,43 @@
 import * as yup from 'yup';
+import { translatedErrors } from '@strapi/admin/strapi-admin';
 
 export const RELEASE_SCHEMA = yup
   .object()
   .shape({
-    name: yup.string().trim().required(),
+    name: yup.string().trim().required({
+      id: translatedErrors.required.id,
+      defaultMessage: 'This field is required',
+    }),
     scheduledAt: yup.string().nullable(),
     isScheduled: yup.boolean().optional(),
     time: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().trim().required(),
+      then: yup.string().trim().required({
+        id: translatedErrors.required.id,
+        defaultMessage: 'This field is required',
+      }),
       otherwise: yup.string().nullable(),
     }),
     timezone: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().required().nullable(),
+      then: yup
+        .string()
+        .required({
+          id: translatedErrors.required.id,
+          defaultMessage: 'This field is required',
+        })
+        .nullable(),
       otherwise: yup.string().nullable(),
     }),
     date: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().required().nullable(),
+      then: yup
+        .string()
+        .required({
+          id: translatedErrors.required.id,
+          defaultMessage: 'This field is required',
+        })
+        .nullable(),
       otherwise: yup.string().nullable(),
     }),
   })
