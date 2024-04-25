1bbf6f1ca3ff163853c7b11aa7fb5aaf41959584
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2024-04-25 07:53:37 +0200

    fix: update error message to just "Required field"

--- packages/core/content-releases/shared/validation-schemas.ts
+++ packages/core/content-releases/shared/validation-schemas.ts
@@ -3,22 +3,22 @@ import * as yup from 'yup';
 export const RELEASE_SCHEMA = yup
   .object()
   .shape({
-    name: yup.string().trim().required(),
+    name: yup.string().trim().required('Required field'),
     scheduledAt: yup.string().nullable(),
     isScheduled: yup.boolean().optional(),
     time: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().trim().required(),
+      then: yup.string().trim().required('Required field'),
       otherwise: yup.string().nullable(),
     }),
     timezone: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().required().nullable(),
+      then: yup.string().required('Required field').nullable(),
       otherwise: yup.string().nullable(),
     }),
     date: yup.string().when('isScheduled', {
       is: true,
-      then: yup.string().required().nullable(),
+      then: yup.string().required('Required field').nullable(),
       otherwise: yup.string().nullable(),
     }),
   })
