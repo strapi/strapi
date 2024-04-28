5adf2184ec1bb4fec18736888e522aab6f369578
Ben Irvin <ben@innerdvations.com>
2024-04-28 16:44:37 +0200

    test(e2e): retry on login failure to load page

--- tests/e2e/utils/login.ts
+++ tests/e2e/utils/login.ts
@@ -3,18 +3,41 @@ import { ADMIN_EMAIL_ADDRESS, ADMIN_PASSWORD } from '../constants';
 
 /**
  * Log in to an e2e test app
+ * it is often flaky, resulting in an infinite spinning loader, so it will attempt several retries before failing
  */
+// TODO: solve why this is so flaky
 export const login = async ({ page, rememberMe = false }: { page: Page; rememberMe?: boolean }) => {
-  await page.getByLabel('Email').fill(ADMIN_EMAIL_ADDRESS);
-  await page
-    .getByLabel('Password*', {
-      exact: true,
-    })
-    .fill(ADMIN_PASSWORD);
+  const maxRetries = 5;
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      await page.getByLabel('Email').fill(ADMIN_EMAIL_ADDRESS);
+      await page
+        .getByLabel('Password*', {
+          exact: true,
+        })
+        .fill(ADMIN_PASSWORD);
 
-  if (rememberMe) {
-    await page.getByLabel('Remember me').click();
+      if (rememberMe) {
+        await page.getByLabel('Remember me').click();
+      }
+      await page.getByRole('button', { name: 'Login' }).click();
+      await page.waitForTimeout(2000);
+      if ((await page.title()) !== 'Homepage | Strapi') {
+        throw new Error('Login failed to load homepage');
+      }
+      break; // If the page loads successfully, break the loop
+    } catch (error) {
+      // If this was the last retry, give up
+      if (i === maxRetries - 1) {
+        throw error;
+      }
+
+      await page.reload();
+
+      // if it actually did log in, proceed instead of trying again
+      if ((await page.title()) === 'Homepage | Strapi') {
+        break;
+      }
+    }
   }
-
-  await page.getByRole('button', { name: 'Login' }).click();
 };
