b463790f8e3eda0806c26ba25f939994a8b9fc43
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2024-05-13 10:50:32 +0200

    fix: logout logs

--- packages/cli/cloud/src/logout/action.ts
+++ packages/cli/cloud/src/logout/action.ts
@@ -13,7 +13,7 @@ export default async (ctx: CLIContext) => {
   try {
     // we might want also to perform extra actions like logging out from the auth0 tenant
     await eraseToken();
-    logger.log('🔌 You have been logged out.');
+    logger.log('🔌 You have been logged out from the CLI. If you are on a shared computer, please make sure to log out from the Strapi Cloud Dashboard as well.');
   } catch (error) {
     logger.error('🥲 Oops! Something went wrong while logging you out. Please try again.');
     logger.debug(error);
