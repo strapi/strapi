3e0945bf8e2eacc81625c02023bd4d44ef117bcc
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-05-20 14:13:25 +0100

    chore: spelling error
    Co-authored-by: Bassel Kanso <basselkanso82@gmail.com>

--- packages/core/admin/admin/src/services/api.ts
+++ packages/core/admin/admin/src/services/api.ts
@@ -5,7 +5,7 @@ import { fetchBaseQuery } from '../utils/baseQuery';
 
 /**
  * @public
- * @description This is the reducl toolkit api for the admin panel, users
+ * @description This is the redux toolkit api for the admin panel, users
  * should use a combination of `enhanceEndpoints` to add their TagTypes
  * to utilise in their `injectEndpoints` construction for automatic cache
  * re-validation. We specifically do not store any tagTypes by default leaving
