400cabe90ccfd8aa0de7aff65d28d34f816bf76a
Ben Irvin <ben@innerdvations.com>
2024-05-22 11:08:15 +0200

    revert: fix accidental commit on port numbers

--- examples/getstarted/config/server.js
+++ examples/getstarted/config/server.js
@@ -4,8 +4,8 @@ const cronTasks = require('./src/cron-tasks');
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  url: 'http://localhost:1338',
+  port: env.int('PORT', 1337),
+  url: 'http://localhost:1337',
   cron: {
     enabled: true,
     tasks: cronTasks,
