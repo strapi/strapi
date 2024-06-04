67b9d302da8cd657a37a439622e9491b148dae0c
nathan-pichon <nathan.pichon@strapi.io>
2024-06-04 15:33:53 +0200

    chore(cloud-cli): update hardcoded production url for cli-api

--- packages/cli/cloud/src/config/api.ts
+++ packages/cli/cloud/src/config/api.ts
@@ -1,6 +1,6 @@
 import { env } from '@strapi/utils';
 
 export const apiConfig = {
-  apiBaseUrl: env('STRAPI_CLI_CLOUD_API', 'https://cli.cloud.strapi.io'),
+  apiBaseUrl: env('STRAPI_CLI_CLOUD_API', 'https://cloud-cli-api.strapi.io'),
   dashboardBaseUrl: env('STRAPI_CLI_CLOUD_DASHBOARD', 'https://cloud.strapi.io'),
 };
