93e05b5a3a83a27fc03326f8f9b1d5db518426cd
Ben Irvin <ben@innerdvations.com>
2024-06-10 21:19:20 +0200

    chore: fix test

--- tests/e2e/tests/content-manager/editview.spec.ts
+++ tests/e2e/tests/content-manager/editview.spec.ts
@@ -236,7 +236,6 @@ test.describe('Edit View', () => {
       await expect(page.getByRole('menuitem', { name: 'Discard changes' })).not.toBeDisabled();
 
       await page.getByRole('menuitem', { name: 'Discard changes' }).click();
-      await expect(page.getByRole('dialog', { name: 'Confirm' })).toBeVisible();
       await page.getByRole('button', { name: 'Confirm' }).click();
 
       await findAndClose(page, 'Changes discarded');
@@ -301,7 +300,6 @@ test.describe('Edit View', () => {
 
       await page.getByRole('button', { name: 'More actions' }).click();
       await page.getByRole('menuitem', { name: 'Delete document' }).click();
-      await expect(page.getByRole('dialog', { name: 'Confirm' })).toBeVisible();
       await page.getByRole('button', { name: 'Confirm' }).click();
 
       await findAndClose(page, 'Deleted Document');
