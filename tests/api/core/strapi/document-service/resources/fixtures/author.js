b551c2a080105c87b43b0e098e3b4c0b85359425
Convly <jean-sebastien.herbaux@epitech.eu>
2024-06-18 09:04:16 +0200

    test: replace json fixtures with js modules in document-service tests to avoid invalid sequences in pg

---
+++ tests/api/core/strapi/document-service/resources/fixtures/author.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    documentId: 'Author1',
+    name: 'Author1-Draft',
+    publishedAt: null,
+    locale: null,
+  },
+  {
+    documentId: 'Author2',
+    name: 'Author2-Draft',
+    publishedAt: null,
+    locale: null,
+  },
+];
