b566fe202e806e51f7ddeb1abc6cc8af8e2abbdb
gopi8324 <148228946+gopi8324@users.noreply.github.com>
2024-06-20 16:05:44 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-south-1"
+  default_tags {
+    tags = {
+      CreatedBy   = "gopi"
+    }
+  }
+}
