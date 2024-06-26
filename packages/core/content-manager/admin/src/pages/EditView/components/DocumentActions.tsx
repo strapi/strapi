8fd6a279bf0410582bfdeafa00c2086b82d953f0
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-06-26 14:23:22 +0200

    chore: use react router relative path links

--- packages/core/content-manager/admin/src/pages/EditView/components/DocumentActions.tsx
+++ packages/core/content-manager/admin/src/pages/EditView/components/DocumentActions.tsx
@@ -575,13 +575,13 @@ const PublishAction: DocumentActionComponent = ({
         );
 
         if ('data' in res && collectionType !== SINGLE_TYPES) {
-          /**
-           * TODO: refactor the router so we can just do `../${res.data.documentId}` instead of this.
-           */
-          navigate({
-            pathname: `../${collectionType}/${model}/${res.data.documentId}`,
-            search: rawQuery,
-          });
+          navigate(
+            {
+              pathname: `../${res.data.documentId}`,
+              search: rawQuery,
+            },
+            { relative: 'path' }
+          );
         } else if (
           'error' in res &&
           isBaseQueryError(res.error) &&
@@ -674,13 +674,13 @@ const UpdateAction: DocumentActionComponent = ({
           );
 
           if ('data' in res) {
-            /**
-             * TODO: refactor the router so we can just do `../${res.data.documentId}` instead of this.
-             */
-            navigate({
-              pathname: `../${collectionType}/${model}/${res.data.documentId}`,
-              search: rawQuery,
-            });
+            navigate(
+              {
+                pathname: `../${res.data.documentId}`,
+                search: rawQuery,
+              },
+              { relative: 'path' }
+            );
           } else if (
             'error' in res &&
             isBaseQueryError(res.error) &&
@@ -718,15 +718,12 @@ const UpdateAction: DocumentActionComponent = ({
           );
 
           if ('data' in res && collectionType !== SINGLE_TYPES) {
-            /**
-             * TODO: refactor the router so we can just do `../${res.data.documentId}` instead of this.
-             */
             navigate(
               {
-                pathname: `../${collectionType}/${model}/${res.data.documentId}`,
+                pathname: `../${res.data.documentId}`,
                 search: rawQuery,
               },
-              { replace: true }
+              { replace: true, relative: 'path' }
             );
           } else if (
             'error' in res &&
