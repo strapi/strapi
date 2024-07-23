124de87fdf21f8554c4cc4f06e86fd5b1162631c
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-07-23 10:19:41 +0200

    fix: json fields not parsed properly

--- packages/core/database/src/fields/json.ts
+++ packages/core/database/src/fields/json.ts
@@ -8,12 +8,19 @@ export default class JSONField extends Field {
   fromDB(value: unknown) {
     try {
       if (typeof value === 'string') {
-        return JSON.parse(value);
+        const parsedValue = JSON.parse(value);
+
+        if (typeof parsedValue === 'string') {
+          return JSON.parse(parsedValue);
+        }
+
+        return parsedValue;
       }
     } catch (error) {
       // Just return the value if it's not a valid JSON string
       return value;
     }
+
     return value;
   }
 }
