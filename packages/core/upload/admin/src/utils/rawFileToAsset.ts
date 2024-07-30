87806214127e68d5ca2560699056744d9c5b6bf7
Simone Taeggi <startae14@gmail.com>
2024-07-30 17:01:42 +0200

    chore: migrate rawToFileAsset

--- packages/core/upload/admin/src/utils/rawFileToAsset.ts
+++ packages/core/upload/admin/src/utils/rawFileToAsset.ts
@@ -1,6 +1,15 @@
 import { typeFromMime } from './typeFromMime';
+// TODO: import this file from the constants file when it will be migrated to TS
+import { AssetSource } from '../newConstants';
 
-export const rawFileToAsset = (rawFile, assetSource) => {
+interface RawFile extends Blob {
+  size: number;
+  lastModified: number;
+  name: string;
+  type: string;
+}
+
+export const rawFileToAsset = (rawFile: RawFile, assetSource: AssetSource) => {
   return {
     size: rawFile.size / 1000,
     createdAt: new Date(rawFile.lastModified).toISOString(),
