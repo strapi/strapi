d711123c27995c6bfcf14750573e84da7dd76d30
Ben Irvin <ben@innerdvations.com>
2024-08-06 14:36:59 +0200

    test(cli): add list command tests

---
+++ tests/cli/tests/strapi/strapi/hooks-list.test.cli.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const coffee = require('coffee');
+const semver = require('semver');
+const assert = require('assert');
+
+const utils = require('../../../utils');
+
+describe('hooks:list', () => {
+  let appPath;
+
+  beforeAll(async () => {
+    const testApps = utils.instances.getTestApps();
+
+    appPath = testApps.at(0);
+  });
+
+  it('should output list of hooks', async () => {
+    const { stdout } = await coffee
+      .spawn('npm', ['run', '-s', 'strapi', 'hooks:list'], { cwd: appPath })
+      .expect('code', 0)
+      .end();
+
+    const output = stdout.trim();
+
+    const expected = `
+    ┌──────────────────────────────────┐
+    │ Name                             │
+    ├──────────────────────────────────┤
+    │ strapi::content-types.beforeSync │
+    ├──────────────────────────────────┤
+    │ strapi::content-types.afterSync  │
+    └──────────────────────────────────┘
+    `;
+
+    utils.helpers.expectConsoleLinesToEqual(output, expected);
+  });
+});
