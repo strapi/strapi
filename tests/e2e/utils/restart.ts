3900782415e36ef7042fd4d445f98685fe31c3c1
Ben Irvin <ben@innerdvations.com>
2024-08-13 15:12:05 +0200

    test: simplify restarts and increase timeouts

--- tests/e2e/utils/restart.ts
+++ tests/e2e/utils/restart.ts
@@ -1,48 +1,46 @@
 import { type Page, test } from '@playwright/test';
 
-// Function to check modal visibility
-const isModalVisible = async (page: Page) => {
-  return await Promise.any([
-    page.waitForSelector('text="Waiting for restart..."', { state: 'visible' }),
-    page.waitForSelector('text="is taking longer"', { state: 'visible' }),
-  ])
-    .then(() => true)
-    .catch(() => false);
-};
-
 /**
  * Wait for a restart modal to appear and then disappear
  */
-export const waitForRestart = async (page, timeout = 60000) => {
-  test.setTimeout(100_000); // Increase timeout, sometimes server restarts are very slow
+export const waitForRestart = async (page: Page, timeout = 60_000) => {
+  test.setTimeout(120_000); // Increase timeout, sometimes server restarts are very slow
 
-  const initialWaitForModal = 3_000; // Time to wait for the modal to initially appear
-  let elapsedTime = 0;
-  const checkInterval = 2_000; // millseconds to poll for server coming back up
-  const waitForRestartTimeout = 30_000; // millseconds before trying to reload the page
+  const initialWaitForModal = 2_000; // Time to wait for the modal to initially appear
+  const waitForRestartTimeout = timeout; // milliseconds before trying to wait for modal to disappear
+  const checkInterval = 2_000; // milliseconds to poll for the modal being hidden
 
-  // Initially wait for the modal to become visible
+  // Initially wait for either of the modal messages to become visible
   try {
-    await page.waitForSelector('text="Waiting for restart..."', {
-      state: 'visible',
-      timeout: initialWaitForModal,
-    });
+    await Promise.any([
+      page.waitForSelector('text="Waiting for restart..."', {
+        state: 'visible',
+        timeout: initialWaitForModal,
+      }),
+      page.waitForSelector('text="is taking longer"', {
+        state: 'visible',
+        timeout: initialWaitForModal,
+      }),
+    ]);
   } catch (error) {
-    console.log('The modal did not become visible within the initial wait period.');
+    console.log('Neither of the modals became visible within the initial wait period.');
     throw error; // Or handle this scenario as appropriate
   }
 
-  // Now wait until the modal is not visible or until the reloadTimeout
-  let modalVisible = await isModalVisible(page);
-  while (modalVisible && elapsedTime < waitForRestartTimeout) {
+  // Now wait until both of the modal messages are not visible anymore
+  let elapsedTime = 0;
+  while (elapsedTime < waitForRestartTimeout) {
+    const isWaitingForRestartVisible = await page.isVisible('text="Waiting for restart..."');
+    const isTakingLongerVisible = await page.isVisible('text="is taking longer"');
+
+    if (!isWaitingForRestartVisible && !isTakingLongerVisible) {
+      console.log('Restart overlay has disappeared, proceeding with the test.');
+      return;
+    }
+
     await new Promise((r) => setTimeout(r, checkInterval));
     elapsedTime += checkInterval;
-    modalVisible = await isModalVisible(page);
   }
 
-  if (modalVisible) {
-    throw new Error("Restart modal didn't close");
-  }
-
-  console.log('Restart overlay has disappeared, proceeding with the test.');
+  throw new Error("Restart modal didn't close within the expected time.");
 };
