5eee2945c0ed452057e51e3f983fb8ed4c959473
Simone Taeggi <startae14@gmail.com>
2024-08-23 11:47:31 +0200

    chore: migrate to TS FilterValueInput

---
+++ packages/core/upload/admin/src/components/FilterPopover/FilterValueInput.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+
+import { DateTimePicker, SingleSelectOption, SingleSelect } from '@strapi/design-system';
+import { useIntl } from 'react-intl';
+
+interface FilterValueInputProps {
+  label?: string;
+  options?: { label: string; value: string }[];
+  onChange: (value: string) => void;
+  type: string;
+  value?: string;
+}
+
+const FilterValueInput = ({
+  label = '',
+  onChange,
+  options = [],
+  type,
+  value = '',
+}: FilterValueInputProps) => {
+  const { formatMessage } = useIntl();
+
+  if (type === 'date') {
+    return (
+      <DateTimePicker
+        clearLabel={formatMessage({ id: 'clearLabel', defaultMessage: 'Clear' })}
+        aria-label={label}
+        name="datetimepicker"
+        onChange={(date: Date | undefined) => {
+          const formattedDate = date ? new Date(date).toISOString() : '';
+
+          onChange(formattedDate);
+        }}
+        onClear={() => onChange('')}
+        value={value ? new Date(value) : undefined}
+      />
+    );
+  }
+
+  return (
+    <SingleSelect
+      aria-label={label}
+      onChange={(value: string | number) => onChange(value.toString())}
+      value={value}
+    >
+      {options?.map((option) => {
+        return (
+          <SingleSelectOption key={option.value} value={option.value}>
+            {option.label}
+          </SingleSelectOption>
+        );
+      })}
+    </SingleSelect>
+  );
+};
+
+export default FilterValueInput;
