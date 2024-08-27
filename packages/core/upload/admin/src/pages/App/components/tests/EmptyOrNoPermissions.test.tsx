2b3420df3f1dc5661c6681267f9fcf2195cec958
Simone Taeggi <startae14@gmail.com>
2024-08-27 10:23:48 +0200

    chore: migrate to TS : EmptyOrNoPermissions in the page App

--- packages/core/upload/admin/src/pages/App/components/tests/EmptyOrNoPermissions.test.tsx
+++ packages/core/upload/admin/src/pages/App/components/tests/EmptyOrNoPermissions.test.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 import { DesignSystemProvider } from '@strapi/design-system';
 import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 
-import { EmptyOrNoPermissions } from '../EmptyOrNoPermissions';
+import { EmptyOrNoPermissions, EmptyOrNoPermissionsProps } from '../EmptyOrNoPermissions';
 
-const setup = (props) =>
+const setup = (props: Partial<EmptyOrNoPermissionsProps>) =>
   render(
     <DesignSystemProvider>
       <IntlProvider locale="en" messages={{}}>
