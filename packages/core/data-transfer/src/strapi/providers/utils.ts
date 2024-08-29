e4864053d64c32494a0125688fd8697a77b7c4bd
Convly <jean-sebastien.herbaux@epitech.eu>
2024-08-29 10:55:52 +0200

    fix: refactor asset streaming with queue management and status handling

--- packages/core/data-transfer/src/strapi/providers/utils.ts
+++ packages/core/data-transfer/src/strapi/providers/utils.ts
@@ -215,3 +215,13 @@ export const connectToWebsocket = (address: Address, options?: Options): Promise
 export const trimTrailingSlash = (input: string): string => {
   return input.replace(/\/$/, '');
 };
+
+export const wait = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+
+export const waitUntil = async (test: () => boolean, interval: number): Promise<void> => {
+  while (!test()) {
+    await wait(interval);
+  }
+
+  return Promise.resolve();
+};
