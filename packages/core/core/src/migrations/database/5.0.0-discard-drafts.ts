5b5c5d0936554cbcf5d12786f43a8c2c3227e4c0
Marc-Roig <marc12info@gmail.com>
2024-08-30 15:25:52 +0200

    fix: draft relations

--- packages/core/core/src/migrations/database/5.0.0-discard-drafts.ts
+++ packages/core/core/src/migrations/database/5.0.0-discard-drafts.ts
@@ -1,3 +1,16 @@
+/**
+ * This migration is responsible for creating the draft counterpart for all the entries that were in a published state.
+ *
+ * In v4, entries could either be in a draft or published state, but not both at the same time.
+ * In v5, we introduced the concept of document, and an entry can be in a draft or published state.
+ *
+ * This means the migration needs to create the draft counterpart if an entry was published.
+ *
+ * This migration performs the following steps:
+ * 1. Creates draft entries for all published entries, without it's components, dynamic zones or relations.
+ * 2. Using the document service, discard those same drafts to copy its relations.
+ */
+
 /* eslint-disable no-continue */
 import type { UID } from '@strapi/types';
 import type { Database, Migration } from '@strapi/database';
@@ -5,6 +18,64 @@ import { async, contentTypes } from '@strapi/utils';
 
 type DocumentVersion = { documentId: string; locale: string };
 type Knex = Parameters<Migration['up']>[0];
+
+/**
+ * Copy all the published entries to draft entries, without it's components, dynamic zones or relations.
+ * This ensures all necessary draft's exist before copying it's relations.
+ */
+async function copyPublishedEntriesToDraft({
+  db,
+  trx,
+  uid,
+}: {
+  db: Database;
+  trx: Knex;
+  uid: string;
+}) {
+  // Extract all scalar attributes to use in the insert query
+  const meta = db.metadata.get(uid);
+
+  // Get scalar attributes that will be copied over the new draft
+  const scalarAttributes = Object.values(meta.attributes).reduce((acc, attribute: any) => {
+    if (['id'].includes(attribute.columnName)) {
+      return acc;
+    }
+
+    if (contentTypes.isScalarAttribute(attribute)) {
+      acc.push(attribute.columnName);
+    }
+
+    return acc;
+  }, [] as string[]);
+
+  /**
+   * Query to copy the published entries into draft entries.
+   *
+   * INSERT INTO tableName (columnName1, columnName2, columnName3, ...)
+   * SELECT columnName1, columnName2, columnName3, ...
+   * FROM tableName
+   */
+  await trx
+    // INSERT INTO tableName (columnName1, columnName2, columnName3, ...)
+    .into(trx.raw(`${meta.tableName} (${scalarAttributes.join(', ')})`))
+    .insert((subQb: typeof trx) => {
+      // SELECT columnName1, columnName2, columnName3, ...
+      subQb
+        .select(
+          ...scalarAttributes.map((att: string) => {
+            // Override 'publishedAt' and 'updatedAt' attributes
+            if (att === 'published_at') {
+              return trx.raw('NULL as published_at');
+            }
+
+            return att;
+          })
+        )
+        .from(meta.tableName)
+        // Only select entries that were published
+        .whereNotNull('published_at');
+    });
+}
 
 /**
  * Load a batch of versions to discard.
@@ -62,6 +133,11 @@ const migrateUp = async (trx: Knex, db: Database) => {
       continue;
     }
 
+    /**
+     * Create plain draft entries for all the entries that were published.
+     */
+    await copyPublishedEntriesToDraft({ db, trx, uid: meta.uid });
+
     const discardDraft = async (entry: DocumentVersion) =>
       strapi
         .documents(uid)
@@ -69,6 +145,8 @@ const migrateUp = async (trx: Knex, db: Database) => {
         .discardDraft({ documentId: entry.documentId, locale: entry.locale });
 
     /**
+     * Discard the drafts will copy the relations from the published entries to the newly created drafts.
+     *
      * Load a batch of entries (batched to prevent loading millions of rows at once ),
      * and discard them using the document service.
      */
