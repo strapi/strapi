d64dfc07d65394b6be646e3d28560b9248db7b96
Ziyi Yuan <daydreamnation@live.com>
2024-09-04 14:31:07 +0200

    fix: fix default date value bug in content type builder(#21098), refactor parseDateValue

--- packages/core/content-type-builder/admin/src/utils/parseDateValue.ts
+++ packages/core/content-type-builder/admin/src/utils/parseDateValue.ts
@@ -2,17 +2,17 @@ import { Schema } from '@strapi/types';
 
 export const parseDateValue = <TAttribute extends Schema.Attribute.AnyAttribute>(
   value: Schema.Attribute.Value<TAttribute>
-): Date | undefined => {
-  if (value instanceof Date) {
-    return isValidDate(value) ? value : undefined;
+) => {
+  if (value instanceof Date && isValidDate(value)) {
+    return value;
   }
 
   if (typeof value === 'string' || typeof value === 'number') {
     const date = new Date(value);
-    return isValidDate(date) ? date : undefined;
+    if (isValidDate(date)) {
+      return date;
+    }
   }
-
-  return undefined;
 };
 
 const isValidDate = (date: Date): boolean => !isNaN(date.getTime());
