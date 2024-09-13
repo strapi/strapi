80ca53536069ddae6c0bb7cf748160ad0c21550c
Jamie Howard <48524071+jhoward1994@users.noreply.github.com>
2024-09-13 10:41:04 +0100

    fix(content-type-builder): export auto loader provider for plugin use (#21250)
    * fix(content-type-builder): export auto loader provider for plugin use
    
    * chore: refer to reload watcher exports as private

--- packages/core/content-type-builder/admin/src/exports.ts
+++ packages/core/content-type-builder/admin/src/exports.ts
@@ -1,1 +1,16 @@
-export { useAutoReloadOverlayBlocker } from './components/AutoReloadOverlayBlocker';
+export {
+  /**
+   * @private
+   * @description This hook is private and should only be used internally.
+   * Pending a refactor to a more generic reload watcher outside of the content
+   * type builder.
+   */
+  useAutoReloadOverlayBlocker as private_useAutoReloadOverlayBlocker,
+  /**
+   * @private
+   * @description This provider is private and should only be used internally.
+   * Pending a refactor to a more generic reload watcher outside of the content
+   * type builder.
+   */
+  AutoReloadOverlayBlockerProvider as private_AutoReloadOverlayBlockerProvider,
+} from './components/AutoReloadOverlayBlocker';
