ff83356ce009037b9ee379bbf1ed5ede4f717937
Simone Taeggi <startae14@gmail.com>
2024-09-24 12:19:53 +0200

    chore: migrate to TS prefixPluginTranslations

---
+++ packages/core/upload/admin/src/utils/prefixPluginTranslations.ts
@@ -0,0 +1,17 @@
+type Translations = {
+  [key: string]: string;
+};
+
+const prefixPluginTranslations = (trad: Translations, pluginId?: string) => {
+  if (!pluginId) {
+    throw new TypeError("pluginId can't be empty");
+  }
+
+  return Object.keys(trad).reduce((acc: Translations, current: string) => {
+    acc[`${pluginId}.${current}`] = trad[current];
+
+    return acc;
+  }, {});
+};
+
+export { prefixPluginTranslations };
