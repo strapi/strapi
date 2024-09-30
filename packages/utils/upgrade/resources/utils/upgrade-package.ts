7736a2810c38676bfd5205a96ebc616349c1bb7f
Ben Irvin <ben@innerdvations.com>
2024-09-30 12:25:23 +0200

    fix: do not downgrade packages

--- packages/utils/upgrade/resources/utils/upgrade-package.ts
+++ packages/utils/upgrade/resources/utils/upgrade-package.ts
@@ -1,25 +1,35 @@
 import path from 'node:path';
+import semver from 'semver';
 import { type modules } from '../../dist';
 
 export const upgradeIfExists = (
   file: modules.runner.json.JSONSourceFile,
   params: modules.runner.json.JSONTransformParams,
   packagePath: string,
-  version: string
+  targetVersion: string
 ) => {
   const { cwd, json } = params;
 
-  const rootPackageJsonPath = path.join(cwd, 'package.json');
-
-  if (file.path !== rootPackageJsonPath) {
+  // Return early if the file path is not the root package.json
+  if (file.path !== path.join(cwd, 'package.json')) {
     return file.json;
   }
 
-  const j = json(file.json);
+  const packageJson = json(file.json);
 
-  if (j.has(packagePath)) {
-    j.set(packagePath, version);
+  // Check if the package exists
+  if (packageJson.has(packagePath)) {
+    const currentVersion = packageJson.get(packagePath);
+
+    // ensure we only upgrade, not downgrade
+    if (
+      typeof currentVersion === 'string' &&
+      semver.valid(currentVersion) &&
+      semver.lt(currentVersion, targetVersion)
+    ) {
+      packageJson.set(packagePath, targetVersion);
+    }
   }
 
-  return j.root();
+  return packageJson.root();
 };
