ae29ba94cec55f424e35d1c1a2c48d83ab4de0a7
Ben Irvin <ben@innerdvations.com>
2024-10-01 13:55:25 +0200

    fix: move upgradeIfExists util

--- packages/utils/upgrade/resources/codemods/5.1.0/dependency-better-sqlite3.json.ts
+++ packages/utils/upgrade/resources/codemods/5.1.0/dependency-better-sqlite3.json.ts
@@ -1,5 +1,6 @@
+import path from 'path';
+import semver from 'semver';
 import type { modules } from '../../../dist';
-import { upgradeIfExists } from '../../utils/upgrade-package';
 
 const DEP_NAME = 'better-sqlite3';
 const DEP_PATH = `dependencies.${DEP_NAME}`;
@@ -13,3 +14,35 @@ const transform: modules.runner.json.JSONTransform = (file, params) => {
 };
 
 export default transform;
+
+// TODO: move this to a utility once we solve the issue where codemods are not transpiled properly
+const upgradeIfExists = (
+  file: modules.runner.json.JSONSourceFile,
+  params: modules.runner.json.JSONTransformParams,
+  packagePath: string,
+  targetVersion: string
+) => {
+  const { cwd, json } = params;
+
+  // Return early if the file path is not the root package.json
+  if (file.path !== path.join(cwd, 'package.json')) {
+    return file.json;
+  }
+
+  const packageJson = json(file.json);
+
+  // Check if the package exists
+  if (packageJson.has(packagePath)) {
+    const currentVersion = packageJson.get(packagePath);
+    // ensure we only upgrade, not downgrade
+    if (
+      typeof currentVersion === 'string' &&
+      semver.valid(currentVersion) &&
+      semver.lt(currentVersion, targetVersion)
+    ) {
+      packageJson.set(packagePath, targetVersion);
+    }
+  }
+
+  return packageJson.root();
+};
