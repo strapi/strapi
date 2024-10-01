5b9ddce4400dd8f45f83919f2c3cf98912a50463
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2024-10-01 13:21:00 +0200

    chore: prettier

--- packages/cli/cloud/src/environment/list/action.ts
+++ packages/cli/cloud/src/environment/list/action.ts
@@ -28,15 +28,15 @@ export default async (ctx: CLIContext) => {
 
   const project = await getProject(ctx);
   if (!project) {
-    ctx.logger.debug(
-      `No valid local project configuration was found.`
-    );
+    ctx.logger.debug(`No valid local project configuration was found.`);
     return;
   }
 
   const cloudApiService = await cloudApiFactory(ctx, token);
   const spinner = logger.spinner('Fetching environments...').start();
-  await trackEvent(ctx, cloudApiService, 'willListEnvironment', { projectInternalName: project.name });
+  await trackEvent(ctx, cloudApiService, 'willListEnvironment', {
+    projectInternalName: project.name,
+  });
 
   try {
     const {
@@ -44,7 +44,9 @@ export default async (ctx: CLIContext) => {
     } = await cloudApiService.listEnvironments({ name: project.name });
     spinner.succeed();
     logger.log(environmentsList);
-    await trackEvent(ctx, cloudApiService, 'didListEnvironment', { projectInternalName: project.name });
+    await trackEvent(ctx, cloudApiService, 'didListEnvironment', {
+      projectInternalName: project.name,
+    });
   } catch (e: any) {
     if (e.response && e.response.status === 404) {
       spinner.succeed();
@@ -57,6 +59,8 @@ export default async (ctx: CLIContext) => {
       spinner.fail('An error occurred while fetching environments data from Strapi Cloud.');
       logger.debug('Failed to list environments', e);
     }
-    await trackEvent(ctx, cloudApiService, 'didNotListEnvironment', { projectInternalName: project.name });
+    await trackEvent(ctx, cloudApiService, 'didNotListEnvironment', {
+      projectInternalName: project.name,
+    });
   }
 };
