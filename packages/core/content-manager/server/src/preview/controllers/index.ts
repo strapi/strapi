84129093943d84571a39db718ff6b367e03c2821
Marc-Roig <marc12info@gmail.com>
2024-10-02 15:54:09 +0200

    chore: comment controllers type

--- packages/core/content-manager/server/src/preview/controllers/index.ts
+++ packages/core/content-manager/server/src/preview/controllers/index.ts
@@ -3,4 +3,8 @@ import { createPreviewController } from './preview';
 
 export const controllers = {
   preview: createPreviewController,
+  /**
+   * Casting is needed because the types aren't aware that Strapi supports
+   * passing a controller factory as the value, instead of a controller object directly
+   */
 } as unknown as Plugin.LoadedPlugin['controllers'];
