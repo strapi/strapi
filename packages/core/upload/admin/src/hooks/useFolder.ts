81845bec84873ddc24ba292c33829da6501a91a0
Simone Taeggi <startae14@gmail.com>
2024-10-03 18:37:47 +0200

    chore: migrate to TS useFolder

--- packages/core/upload/admin/src/hooks/useFolder.ts
+++ packages/core/upload/admin/src/hooks/useFolder.ts
@@ -3,14 +3,19 @@ import { useIntl } from 'react-intl';
 import { useQuery } from 'react-query';
 
 import pluginId from '../pluginId';
-import { getTrad } from '../utils';
+// TODO: replace this import with the import from the index file of the utils folder when it will be migrated to TS
+import getTrad from '../utils/getTrad';
+import { GetFolder } from '../../../shared/contracts/folders';
 
-export const useFolder = (id, { enabled = true } = {}) => {
+export const useFolder = (id: number, { enabled = true } = {}) => {
   const { toggleNotification } = useNotification();
   const { get } = useFetchClient();
   const { formatMessage } = useIntl();
 
-  const { data, error, isLoading } = useQuery(
+  const { data, error, isLoading } = useQuery<
+    GetFolder.Response['data'],
+    GetFolder.Response['error']
+  >(
     [pluginId, 'folder', id],
     async () => {
       const {
