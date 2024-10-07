1d67af3f6234d4a3eca12598da09631c8c3ea293
Fernando Chavez <fernando.chavez@strapi.io>
2024-10-07 16:37:42 +0200

    feat(content-releases): considered review stage when getting the action status

--- packages/core/content-releases/server/src/services/release-action.ts
+++ packages/core/content-releases/server/src/services/release-action.ts
@@ -1,6 +1,6 @@
 import { errors, async } from '@strapi/utils';
 
-import type { Core, Internal, Modules } from '@strapi/types';
+import type { Core, Internal, Modules, UID, Data } from '@strapi/types';
 
 import _ from 'lodash/fp';
 
@@ -361,6 +361,61 @@ const createReleaseActionService = ({ strapi }: { strapi: Core.Strapi }) => {
       getService('release', { strapi }).updateReleaseStatus(releaseId);
 
       return deletedAction;
+    },
+
+    async validateActionsByContentTypes(contentTypeUids: UID.ContentType[]) {
+      const actions = await strapi.db.query(RELEASE_ACTION_MODEL_UID).findMany({
+        where: {
+          contentType: {
+            $in: contentTypeUids,
+          },
+          // We only want to validate actions that are going to be published
+          type: 'publish',
+          release: {
+            releasedAt: {
+              $null: true,
+            },
+          },
+        },
+        populate: { release: true },
+      });
+
+      const releasesUpdated: Data.ID[] = [];
+
+      await async.map(actions, async (action: ReleaseAction) => {
+        const isValid = await getDraftEntryValidStatus(
+          {
+            contentType: action.contentType,
+            documentId: action.entryDocumentId,
+            locale: action.locale,
+          },
+          { strapi }
+        );
+
+        await strapi.db.query(RELEASE_ACTION_MODEL_UID).update({
+          where: {
+            id: action.id,
+          },
+          data: {
+            isEntryValid: isValid,
+          },
+        });
+
+        if (!releasesUpdated.includes(action.release.id)) {
+          releasesUpdated.push(action.release.id);
+        }
+
+        return {
+          id: action.id,
+          isEntryValid: isValid,
+        };
+      });
+
+      if (releasesUpdated.length > 0) {
+        await async.map(releasesUpdated, async (releaseId: number) => {
+          await getService('release', { strapi }).updateReleaseStatus(releaseId);
+        });
+      }
     },
   };
 };
