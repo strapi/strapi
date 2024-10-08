6f338277e2aec18d703865efbe1c60478b6a2c4e
Simone Taeggi <startae14@gmail.com>
2024-10-08 22:56:34 +0200

    chore: migrate to TS CarouselAssetActions component

--- packages/core/upload/admin/src/components/MediaLibraryInput/Carousel/CarouselAssetActions.tsx
+++ packages/core/upload/admin/src/components/MediaLibraryInput/Carousel/CarouselAssetActions.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-
 import { CarouselActions, IconButton } from '@strapi/design-system';
 import { Pencil, Plus, Trash } from '@strapi/icons';
-import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 
-import { AssetDefinition } from '../../../constants';
 import { getTrad, prefixFileUrlWithBackendUrl } from '../../../utils';
 import { CopyLinkButton } from '../../CopyLinkButton';
+import type { File } from '../../../../../shared/contracts/files';
 
-export const CarouselAssetActions = ({ asset, onDeleteAsset, onAddAsset, onEditAsset }) => {
+interface CarouselAssetActionsProps {
+  asset: File;
+  onDeleteAsset?: (asset: File) => void;
+  onAddAsset?: (asset: File) => void;
+  onEditAsset?: () => void;
+}
+
+export const CarouselAssetActions = ({
+  asset,
+  onDeleteAsset,
+  onAddAsset,
+  onEditAsset,
+}: CarouselAssetActionsProps) => {
   const { formatMessage } = useIntl();
 
   return (
@@ -26,7 +35,7 @@ export const CarouselAssetActions = ({ asset, onDeleteAsset, onAddAsset, onEditA
         </IconButton>
       )}
 
-      <CopyLinkButton url={prefixFileUrlWithBackendUrl(asset.url)} />
+      <CopyLinkButton url={prefixFileUrlWithBackendUrl(asset.url)!} />
 
       {onDeleteAsset && (
         <IconButton
@@ -53,17 +62,4 @@ export const CarouselAssetActions = ({ asset, onDeleteAsset, onAddAsset, onEditA
       )}
     </CarouselActions>
   );
-};
-
-CarouselAssetActions.defaultProps = {
-  onAddAsset: undefined,
-  onDeleteAsset: undefined,
-  onEditAsset: undefined,
-};
-
-CarouselAssetActions.propTypes = {
-  asset: AssetDefinition.isRequired,
-  onAddAsset: PropTypes.func,
-  onEditAsset: PropTypes.func,
-  onDeleteAsset: PropTypes.func,
 };
