9e39bd9d5d3032f2f4f9b50c50ba9b226041cff3
Simone Taeggi <startae14@gmail.com>
2024-10-08 22:49:42 +0200

    chore: migrate to TS CopyLinkButton component

--- packages/core/upload/admin/src/components/CopyLinkButton/index.tsx
+++ packages/core/upload/admin/src/components/CopyLinkButton/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-
 import { useNotification } from '@strapi/admin/strapi-admin';
 import { IconButton } from '@strapi/design-system';
 import { Link as LinkIcon } from '@strapi/icons';
-import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 
 import { useClipboard } from '../../hooks/useClipboard';
 import { getTrad } from '../../utils';
 
-export const CopyLinkButton = ({ url }) => {
+export const CopyLinkButton = ({ url }: { url: string }) => {
   const { toggleNotification } = useNotification();
   const { formatMessage } = useIntl();
   const { copy } = useClipboard();
@@ -39,8 +36,4 @@ export const CopyLinkButton = ({ url }) => {
       <LinkIcon />
     </IconButton>
   );
-};
-
-CopyLinkButton.propTypes = {
-  url: PropTypes.string.isRequired,
 };
