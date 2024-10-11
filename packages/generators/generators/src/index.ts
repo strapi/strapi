687325c1b8b6fd0bd596d4ea76a102635a41072b
Ben Irvin <ben@innerdvations.com>
2024-10-11 09:41:10 +0200

    chore: upgrade plop to 4.0.1

--- packages/generators/generators/src/index.ts
+++ packages/generators/generators/src/index.ts
@@ -1,28 +1,33 @@
 import { join } from 'node:path';
 import { Plop, run } from 'plop';
-import nodePlop from 'node-plop';
+import nodePlop, { NodePlopAPI } from 'node-plop';
 
 // Starts the Plop CLI programmatically
 export const runCLI = () => {
-  Plop.launch({ configPath: join(__dirname, 'plopfile.js') }, (env) => {
-    const options = { ...env, dest: join(process.cwd(), 'src') };
-    run(options, undefined, true);
-  });
+  Plop.prepare(
+    {
+      configPath: join(__dirname, 'plopfile.js'),
+    },
+    (env) => {
+      const argv = process.argv.slice(2); // Extract command-line arguments
+      Plop.execute(env, argv, (env, argv) => run(env, argv, true)); // Pass the third argument 'true' for passArgsBeforeDashes
+    }
+  );
 };
 
 // Runs a generator programmatically without prompts
-export const generate = async (
+export const generate = async <T extends Record<string, any>>(
   generatorName: string,
-  options: unknown,
+  options: T,
   { dir = process.cwd(), plopFile = 'plopfile.js' } = {}
 ) => {
-  const plop = nodePlop(join(__dirname, plopFile), {
+  const plop: NodePlopAPI = await nodePlop(join(__dirname, plopFile), {
     destBasePath: join(dir, 'src'),
     force: false,
   });
 
   const generator = plop.getGenerator(generatorName);
-  await generator.runActions(options, {
+  await generator.runActions(options satisfies T, {
     onSuccess() {},
     onFailure() {},
     onComment() {},
