922a70b0f4930137370736f8dae18b12023468ea
Ben Irvin <ben@innerdvations.com>
2024-10-14 16:34:11 +0200

    revert: jest presets

--- jest-preset.unit.js
+++ jest-preset.unit.js
@@ -10,16 +10,10 @@ module.exports = {
     '__tests__/resources',
     'tests/resources',
   ],
-  extensionsToTreatAsEsm: ['.ts'],
-  transformIgnorePatterns: [],
   prettierPath: require.resolve('prettier-2'),
   testMatch: ['**/__tests__/**/*.{js,ts,jsx,tsx}'],
   transform: {
-    '^.+\\.jsx?$': ['@swc/jest'],
-    '^.+\\.tsx?$': ['ts-jest', { useESM: false }],
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
   // Use `jest-watch-typeahead` version 0.6.5. Newest version 1.0.0 does not support jest@26
   // Reference: https://github.com/jest-community/jest-watch-typeahead/releases/tag/v1.0.0
