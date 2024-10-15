91da2610feb71c479197401de11c9dfbe4915cf9
marcoautiero <marcoautiero@gmail.com>
2024-10-15 15:04:19 +0200

    Adjusting the way STRAPI_HOSTING is checked

--- packages/core/admin/admin/src/components/NpsSurvey.tsx
+++ packages/core/admin/admin/src/components/NpsSurvey.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { env } from '@strapi/utils';
 
 import {
   Box,
@@ -189,7 +190,7 @@ const NpsSurvey = () => {
         environment: currentEnvironment,
         version: strapiVersion ?? undefined,
         license: window.strapi.projectType,
-        isHostedOnStrapiCloud: process.env.STRAPI_HOSTING === 'strapi.cloud',
+        isHostedOnStrapiCloud: env('STRAPI_HOSTING', null) === 'strapi.cloud',
       };
       const res = await fetch('https://analytics.strapi.io/submit-nps', {
         method: 'POST',
