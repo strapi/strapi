c2cbe4efbc228ba40a30b4711de9e9eb8ec74b1e
Shohzod Abdusamatov <78433129+Shohzod-Abdusamatov-7777777@users.noreply.github.com>
2024-10-18 17:41:17 +0500

    Update languageNativeNames.ts add O'zbekcha

--- packages/core/admin/admin/src/translations/languageNativeNames.ts
+++ packages/core/admin/admin/src/translations/languageNativeNames.ts
@@ -8,6 +8,7 @@ export const languageNativeNames = {
   'en-GB': 'English (United Kingdom)',
   es: 'Español',
   eu: 'Euskara',
+  uz: 'O'zbekcha',
   fr: 'Français',
   gu: 'Gujarati',
   he: 'עברית',
