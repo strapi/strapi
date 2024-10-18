ea86f3be1d4dc7879d5437083a9c406190fd9e7b
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2024-10-18 10:42:51 +0200

    fix: add ctx

--- packages/cli/cloud/src/environment/command.ts
+++ packages/cli/cloud/src/environment/command.ts
@@ -3,9 +3,9 @@ import { defineCloudNamespace } from '../cloud/command';
 
 let environmentCmd: Command | null = null;
 
-export const initializeEnvironmentCommand = (command: Command): Command => {
+export const initializeEnvironmentCommand = (command: Command, ctx: unknown): Command => {
   if (!environmentCmd) {
-    const cloud = defineCloudNamespace(command);
+    const cloud = defineCloudNamespace(command, ctx);
     environmentCmd = cloud.command('environment').description('Manage environments');
   }
   return environmentCmd;
