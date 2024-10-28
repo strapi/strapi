c222e1c4de12dd05c26938a605a1128fb3481d1d
Marc Roig <marc.roig.campos@strapi.io>
2024-10-28 09:28:04 +0100

    feat: fix filters attribute name (#21547)
    * feat: fix filters attribute name
    
    * chore: simplify test
    
    * chore: pr suggestions
    
    * fix: short hand populate
    
    * chore: comment
    
    * fix: lint
    
    * chore: suggestions
    
    * fix: assert

--- tests/api/core/strapi/api/populate/filtering/index.test.api.js
+++ tests/api/core/strapi/api/populate/filtering/index.test.api.js
@@ -19,6 +19,8 @@ const schemas = {
       attributes: {
         number: { type: 'integer' },
         field: { type: 'string' },
+        // NOTE: Test conflicting attribute names do not cause issues with populate object
+        filters: { type: 'json' },
       },
     },
     bar: {
@@ -26,6 +28,8 @@ const schemas = {
       attributes: {
         title: { type: 'string' },
         field: { type: 'password' },
+        // NOTE: Test conflicting attribute names do not cause issues with populate object
+        filters: { type: 'json' },
       },
     },
   },
@@ -40,6 +44,8 @@ const schemas = {
         pass: { type: 'password' },
         fooRef: { type: 'component', component: 'default.foo', repeatable: false },
         barRefs: { type: 'component', component: 'default.bar', repeatable: true },
+        // NOTE: Test conflicting attribute names do not cause issues with populate object
+        filters: { type: 'relation', relation: 'oneToOne', target: 'api::a.a' },
       },
     },
     b: {
@@ -219,7 +225,7 @@ describe('Populate filters', () => {
 
     test('No filters & deep populate', async () => {
       const qs = {
-        populate: ['second', 'third.fooRef'],
+        populate: ['second', 'third.fooRef', 'third.filters'],
       };
       const { status, body } = await rq.get(`/${schemas.contentTypes.c.pluralName}`, { qs });
 
