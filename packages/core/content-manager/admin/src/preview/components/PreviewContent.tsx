90dde44f9ca33ea3f3c55929adcc4ce0b8fda5c4
Marc-Roig <marc12info@gmail.com>
2024-10-29 11:46:29 +0100

    fix: pr suggestions

--- packages/core/content-manager/admin/src/preview/components/PreviewContent.tsx
+++ packages/core/content-manager/admin/src/preview/components/PreviewContent.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
 
+import { Box } from '@strapi/design-system';
+import { useIntl } from 'react-intl';
+
 import { usePreviewContext } from '../pages/Preview';
 
 const PreviewContent = () => {
   const previewUrl = usePreviewContext('PreviewContent', (state) => state.url);
 
+  const { formatMessage } = useIntl();
+
   return (
-    <iframe
+    <Box
       src={previewUrl}
-      title="Content Preview"
+      title={formatMessage({
+        id: 'content-manager.preview.panel.title',
+        defaultMessage: 'Preview',
+      })}
+      width="100%"
+      height="100%"
       style={{
-        width: '100%',
-        height: '100%',
         border: 'none',
       }}
+      tag="iframe"
     />
   );
 };
