b083e565640f596cdec567755dd36ac616803099
Marc-Roig <marc12info@gmail.com>
2024-11-05 10:01:15 +0100

    fix: use paddings

--- packages/core/content-manager/admin/src/preview/components/PreviewHeader.tsx
+++ packages/core/content-manager/admin/src/preview/components/PreviewHeader.tsx
@@ -199,7 +199,6 @@ const PreviewHeader = () => {
       gridCols={3}
       paddingLeft={2}
       paddingRight={2}
-      style={{ gridTemplateRows: '47px' }}
       background="neutral0"
       borderColor="neutral150"
       tag="header"
