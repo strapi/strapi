dd635feb280db92a56a321e76208ac194be86c1b
archanaagivale30 <archana.agivale@qed42.com>
2024-11-13 16:54:42 +0530

    fix: issue 20766 [Design] Roles missing space between labels and the dropdown field Ref : #20766

--- packages/core/admin/admin/src/pages/Settings/pages/Roles/components/ConditionsModal.tsx
+++ packages/core/admin/admin/src/pages/Settings/pages/Roles/components/ConditionsModal.tsx
@@ -240,7 +240,7 @@ const ActionRow = ({
 
   return (
     <Flex tag="li" background={isGrey ? 'neutral100' : 'neutral0'} paddingBottom={3} paddingTop={3}>
-      <Flex paddingLeft={6} style={{ width: 180 }}>
+      <Flex paddingLeft={6} paddingRight={1} style={{ width: 180 }}>
         <Typography variant="sigma" textColor="neutral600">
           {formatMessage({
             id: 'Settings.permissions.conditions.can',
