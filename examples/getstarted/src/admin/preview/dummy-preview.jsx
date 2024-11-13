a464d7ddee924c8a4ae9147963832a517ea1095a
Marc Roig <marc.roig.campos@strapi.io>
2024-11-13 09:51:28 +0100

    Update examples/getstarted/src/admin/preview/dummy-preview.jsx
    Co-authored-by: markkaylor <mark.kaylor@strapi.io>

--- examples/getstarted/src/admin/preview/dummy-preview.jsx
+++ examples/getstarted/src/admin/preview/dummy-preview.jsx
@@ -33,7 +33,7 @@ const PreviewComponent = () => {
             <Grid.Root gap={5} tag="dl">
               <Grid.Item col={6} s={12} direction="column" alignItems="start">
                 <Typography variant="sigma" textColor="neutral600" tag="dt">
-                  {'Content Type'}
+                  Content Type
                 </Typography>
                 <Flex gap={3} direction="column" alignItems="start" tag="dd">
                   <Typography>{uid}</Typography>
