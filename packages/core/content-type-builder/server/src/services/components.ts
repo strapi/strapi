02c055b1be274043988fdb2f18d353cbc18f5fa1
Ben Irvin <ben@innerdvations.com>
2024-11-18 15:42:26 +0100

    revert: undo delete queries fix

--- packages/core/content-type-builder/server/src/services/components.ts
+++ packages/core/content-type-builder/server/src/services/components.ts
@@ -86,52 +86,13 @@ export const editComponent = async (
   return updatedComponent;
 };
 
-export const deleteComponent = async (deleteUid: Internal.UID.Component) => {
-  const failedDeletes: any[] = [];
+export const deleteComponent = async (uid: Internal.UID.Component) => {
+  const builder = createBuilder();
 
-  const deletedComponent = await strapi.db.transaction(async ({ trx }) => {
-    const builder = createBuilder();
+  const deletedComponent = builder.deleteComponent(uid);
 
-    const models = [...builder.contentTypes.entries(), ...builder.components.entries()];
+  await builder.writeFiles();
 
-    // Find models that include an attribute with this component as a target and delete references to it from the db
-    for (const [modelUid] of models) {
-      const metadata = strapi.db.metadata.get(modelUid);
-
-      const matchingAttributes = Object.values(metadata.attributes).filter(
-        (attr: any) => attr.target === deleteUid && attr.joinTable?.name
-      );
-
-      // Delete entries in each join table associated with matching attributes
-      for (const attr of matchingAttributes) {
-        if (!('joinTable' in attr && attr.joinTable && attr.joinTable.name)) {
-          continue;
-        }
-
-        // Deleting component data is not critical
-        try {
-          await trx.delete().from(attr.joinTable.name).where('component_type', deleteUid);
-        } catch (error) {
-          failedDeletes.push({ table: attr.joinTable.name, error });
-        }
-      }
-    }
-
-    // Remove the component from schemas and write changes
-    const component = builder.deleteComponent(deleteUid);
-    await builder.writeFiles();
-
-    return component;
-  });
-
-  if (failedDeletes.length > 0) {
-    strapi.log.warn(
-      `Failed to delete component data for ${deleteUid} from ${failedDeletes.map((attr) => attr.table)}`
-    );
-  }
-
-  // Emit delete event after transaction completes
   strapi.eventHub.emit('component.delete', { component: deletedComponent });
-
   return deletedComponent;
 };
