6430dac3e247f078b4d4141cfbe6f3247ea36f40
Ben Irvin <ben@innerdvations.com>
2024-11-19 12:07:03 +0100

    fix: clean up component data after delete

--- packages/core/content-type-builder/server/src/services/components.ts
+++ packages/core/content-type-builder/server/src/services/components.ts
@@ -86,13 +86,60 @@ export const editComponent = async (
   return updatedComponent;
 };
 
-export const deleteComponent = async (uid: UID.Component) => {
-  const builder = createBuilder();
+export const deleteComponent = async (deleteUid: UID.Component) => {
+  const failedDeletes: any[] = [];
 
-  const deletedComponent = builder.deleteComponent(uid);
+  const deletedComponent = await strapi.db.transaction(async ({ trx }) => {
+    const builder = createBuilder();
 
-  await builder.writeFiles();
+    const models = [...builder.contentTypes.entries(), ...builder.components.entries()];
 
+    // Find models that include an attribute with this component as a target and delete references to it from the db
+    for (const [modelUid] of models) {
+      const metadata = strapi.db.metadata.get(modelUid);
+
+      const matchingAttributes = Object.values(metadata.attributes).filter(
+        (attr: any) => attr.target === deleteUid && attr.joinTable?.name
+      );
+
+      // Delete entries in each join table associated with matching attributes
+      for (const attr of matchingAttributes) {
+        if (!('joinTable' in attr && attr.joinTable && attr.joinTable.name)) {
+          continue;
+        }
+
+        // Deleting component data is not critical
+        try {
+          /**
+           * TODO: fix hardcoded component_type to use centralized naming method
+           * For now it's staying like this though because
+           * - the name isn't exported anywhere
+           * - it's unlikely to change
+           * - it's hardcoded elsewhere, so they should all be cleaned up together
+           **  */
+          await trx.delete().from(attr.joinTable.name).where('component_type', deleteUid);
+        } catch (error) {
+          failedDeletes.push({ table: attr.joinTable.name, error });
+        }
+      }
+    }
+
+    // Remove the component from schemas and write changes
+    const component = builder.deleteComponent(deleteUid);
+    await builder.writeFiles();
+
+    return component;
+  });
+
+  if (failedDeletes.length > 0) {
+    strapi.log.warn(`
+Component '${deleteUid}' was deleted, but references remain in the following database tables:
+${failedDeletes.map((attr) => attr.table).join('\r\n')}
+You may need to manually remove references to this component from those tables to avoid issues caused by orphaned data.
+`);
+  }
+
+  // Emit delete event after transaction completes
   strapi.eventHub.emit('component.delete', { component: deletedComponent });
 
   return deletedComponent;
