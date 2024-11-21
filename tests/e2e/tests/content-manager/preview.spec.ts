220b0f3351c6fd09163ad0577131476e45f2f3c5
Mark Kaylor <mark.kaylor@strapi.io>
2024-11-21 12:09:10 +0100

    fix: remove waitFor

--- tests/e2e/tests/content-manager/preview.spec.ts
+++ tests/e2e/tests/content-manager/preview.spec.ts
@@ -84,8 +84,6 @@ describeOnCondition(edition === 'EE')('Preview', () => {
 
     // Check if the iframe is present
     const iframe = await page.getByTestId('preview-iframe');
-    // Wait for the iframe to be attached to the DOM
-    await iframe.waitFor({ state: 'attached' });
     expect(iframe).not.toBeNull();
 
     // Check if the iframe is loading the correct URL
@@ -97,9 +95,6 @@ describeOnCondition(edition === 'EE')('Preview', () => {
     await clickAndWait(page, page.getByRole('tab', { name: /^Published$/ }));
 
     const updatedIframe = await page.getByTestId('preview-iframe');
-    // Wait for the iframe to be attached to the DOM
-    await updatedIframe.waitFor({ state: 'attached' });
-
     const srcPublished = await updatedIframe.getAttribute('src');
     expect(srcPublished).toContain('/preview/api::article.article/');
     expect(srcPublished).toContain('/en/published');
