5aea17fcfbdad7082c071b8579b01470b8e8f9e3
Ben Irvin <ben@innerdvations.com>
2024-11-28 12:01:02 +0100

    enhancement: use renamed value

--- packages/core/content-type-builder/server/src/controllers/content-types.ts
+++ packages/core/content-type-builder/server/src/controllers/content-types.ts
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import type { Context } from 'koa';
 import type {} from 'koa-body';
 import type { Internal } from '@strapi/types';
+import { isObject } from 'lodash/fp';
 import { MigrationBuilder } from '../services/migration-builder';
 import { getService } from '../utils';
 import {
@@ -114,15 +115,18 @@ export default {
 
       const contentTypeService = getService('content-types');
 
-      // TODO: pull this from ctx instead of only finding _renamed attributes
+      // Find attributes with a `renamed` field that contains the original name
       const renamedAttributes = Object.entries(body.contentType.attributes || {})
-        .filter(([key]) => key.endsWith('_renamed'))
-        .map(([key]) => {
-          const oldName = key.replace('_renamed', '');
-          const newName = key;
+        .filter(([, value]) => value && typeof value === 'object' && 'renamed' in value) // Check for `renamed` field
+        .map(([key, value]) => {
+          if (!isObject(value) || !('renamed' in value)) {
+            return undefined;
+          }
+          const oldName = value.renamed; // Original name is in the `renamed` field
+          const newName = key; // New name is the current key
           return { oldName, newName };
-        });
-
+        })
+        .filter((item) => item);
       if (
         renamedAttributes.length &&
         strapi.config.get('database.settings.automigrate.attributes', true)
