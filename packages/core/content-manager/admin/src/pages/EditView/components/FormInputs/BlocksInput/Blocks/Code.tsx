8857d7a89125c9a5007d3af429509a67cf8f4034
Simone Taeggi <startae14@gmail.com>
2024-11-28 18:00:31 +0100

    feat(content-manager): remove prismjs

--- packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/BlocksInput/Blocks/Code.tsx
+++ packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/BlocksInput/Blocks/Code.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 // Import the PrismJS theme to highlight the code
-import Prism from "prismjs";
-import 'prismjs/themes/prism.css';
+import 'highlight.js/styles/solarized-dark.css';
 
 import { Box, SingleSelect, SingleSelectOption } from '@strapi/design-system';
 import { CodeBlock as CodeBlockIcon } from '@strapi/icons';
