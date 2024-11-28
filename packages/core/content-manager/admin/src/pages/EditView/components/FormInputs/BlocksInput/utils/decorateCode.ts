15fcb1bcbf72e1eb3f8dabbb1a671d28c7c3e247
Simone Taeggi <startae14@gmail.com>
2024-11-28 16:51:47 +0100

    feat(content-manager): try to find the issue in gh action

--- packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/BlocksInput/utils/decorateCode.ts
+++ packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/BlocksInput/utils/decorateCode.ts
@@ -1,39 +1,39 @@
-// eslint-disable-next-line import/default
-import Prism from 'prismjs';
-import { BaseRange, Element, Node, NodeEntry } from 'slate';
-import './prismLanguages';
+// // eslint-disable-next-line import/default
+// import Prism from 'prismjs';
+// import { BaseRange, Element, Node, NodeEntry } from 'slate';
+// import './prismLanguages';
 
-import { codeLanguages } from './constants';
+// import { codeLanguages } from './constants';
 
-type BaseRangeCustom = BaseRange & { className: string };
+// type BaseRangeCustom = BaseRange & { className: string };
 
-export const decorateCode = ([node, path]: NodeEntry) => {
-  const ranges: BaseRangeCustom[] = [];
+// export const decorateCode = ([node, path]: NodeEntry) => {
+//   const ranges: BaseRangeCustom[] = [];
 
-  // make sure it is an Slate Element
-  if (!Element.isElement(node) || node.type !== 'code') return ranges;
-  // transform the Element into a string
-  const text = Node.string(node);
-  const decorateKey = codeLanguages.find((lang) => lang.value === node.language)?.decorate;
+//   // make sure it is an Slate Element
+//   if (!Element.isElement(node) || node.type !== 'code') return ranges;
+//   // transform the Element into a string
+//   const text = Node.string(node);
+//   const decorateKey = codeLanguages.find((lang) => lang.value === node.language)?.decorate;
 
-  const selectedLanguage = Prism.languages[decorateKey || 'plaintext'];
+//   const selectedLanguage = Prism.languages[decorateKey || 'plaintext'];
 
-  // create "tokens" with "prismjs" and put them in "ranges"
-  const tokens = Prism.tokenize(text, selectedLanguage);
-  let start = 0;
-  for (const token of tokens) {
-    const length = token.length;
-    const end = start + length;
-    if (typeof token !== 'string') {
-      ranges.push({
-        anchor: { path, offset: start },
-        focus: { path, offset: end },
-        className: `token ${token.type}`,
-      });
-    }
-    start = end;
-  }
+//   // create "tokens" with "prismjs" and put them in "ranges"
+//   const tokens = Prism.tokenize(text, selectedLanguage);
+//   let start = 0;
+//   for (const token of tokens) {
+//     const length = token.length;
+//     const end = start + length;
+//     if (typeof token !== 'string') {
+//       ranges.push({
+//         anchor: { path, offset: start },
+//         focus: { path, offset: end },
+//         className: `token ${token.type}`,
+//       });
+//     }
+//     start = end;
+//   }
 
-  // these will be found in "renderLeaf" in "leaf" and their "className" will be applied
-  return ranges;
-};
+//   // these will be found in "renderLeaf" in "leaf" and their "className" will be applied
+//   return ranges;
+// };
