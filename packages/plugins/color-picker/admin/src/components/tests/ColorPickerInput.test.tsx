f497b678d3489c8c4482b6b5b0f7521a9d77b60a
kaioduarte <duartedossantos@gmail.com>
2024-11-29 18:39:22 +0000

    chore: pair implementation with other form fields

--- packages/plugins/color-picker/admin/src/components/tests/ColorPickerInput.test.tsx
+++ packages/plugins/color-picker/admin/src/components/tests/ColorPickerInput.test.tsx
@@ -1,36 +1,26 @@
 import { DesignSystemProvider } from '@strapi/design-system';
 import { Form } from '@strapi/strapi/admin';
 import { render as renderRTL } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 import { IntlProvider } from 'react-intl';
 
 import { ColorPickerInput } from '../ColorPickerInput';
 
 const render = () => ({
-  ...renderRTL(
-    <ColorPickerInput
-      name="color"
-      label={'color-picker'}
-      type="string"
-      initialValue=""
-      value=""
-      onChange={() => {}}
-    />,
-    {
-      wrapper: ({ children }) => {
-        const locale = 'en';
-        return (
-          <IntlProvider locale={locale} messages={{}} textComponent="span">
-            <DesignSystemProvider locale={locale}>
-              <Form onSubmit={jest.fn()} method="POST">
-                {children}
-              </Form>
-            </DesignSystemProvider>
-          </IntlProvider>
-        );
-      },
-    }
-  ),
+  ...renderRTL(<ColorPickerInput name="color" label={'color-picker'} type="string" />, {
+    wrapper: ({ children }) => {
+      const locale = 'en';
+      return (
+        <IntlProvider locale={locale} messages={{}} textComponent="span">
+          <DesignSystemProvider locale={locale}>
+            <Form onSubmit={jest.fn()} method="POST">
+              {children}
+            </Form>
+          </DesignSystemProvider>
+        </IntlProvider>
+      );
+    },
+  }),
   user: userEvent.setup(),
 });
 
