6d558d8f52178e96e7d445ca4ccbc5e6f0a1059c
angrodher <15-11669@usb.ve>
2024-12-12 23:31:59 -0400

    Eliminacion de archivos innecesarios

--- issue_scrollbar_strapi/src/api/about/controllers/about.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  about controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::about.about');
--- issue_scrollbar_strapi/src/api/global/routes/global.ts
+++
@@ -1,7 +0,0 @@
-/**
- * global router.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::global.global');
