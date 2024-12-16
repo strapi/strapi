119596a668784f1d2414e2409a35098fc5b7103d
Bassel Kanso <basselkanso82@gmail.com>
2024-12-16 12:56:39 +0200

    fix: updating unit tests

--- packages/generators/generators/src/plops/utils/__tests__/get-file-path.test.ts
+++ packages/generators/generators/src/plops/utils/__tests__/get-file-path.test.ts
@@ -3,21 +3,21 @@ import getFilePath from '../get-file-path';
 describe('Get-File-Path util', () => {
   test('with destination set as api', () => {
     const filePath = getFilePath('api');
-    expect(filePath).toBe(`api/{{ api }}`);
+    expect(filePath).toBe(`src/api/{{ api }}`);
   });
 
   test('with destination set as plugin', () => {
     const filePath = getFilePath('plugin');
-    expect(filePath).toBe(`plugins/{{ plugin }}/server`);
+    expect(filePath).toBe(`src/plugins/{{ plugin }}/server`);
   });
 
   test('with destination set as root', () => {
     const filePath = getFilePath('root');
-    expect(filePath).toBe(`./`);
+    expect(filePath).toBe(`src`);
   });
 
   test('with empty destination string', () => {
     const filePath = getFilePath('');
-    expect(filePath).toBe(`api/{{ id }}`);
+    expect(filePath).toBe(`src/api/{{ id }}`);
   });
 });
