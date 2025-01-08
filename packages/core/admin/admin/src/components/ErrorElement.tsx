9cbe9754fe43cb839aee63b4a088dfab112b23f8
DMehaffy <derrickmehaffy@gmail.com>
2025-01-08 10:24:48 -0800

    fix: remove link to open a new issue directly

--- packages/core/admin/admin/src/components/ErrorElement.tsx
+++ packages/core/admin/admin/src/components/ErrorElement.tsx
@@ -68,7 +68,7 @@ ${error.stack}
                         isExternal
                         // hack to get rid of the current endIcon, which should be removable by using `null`.
                         endIcon
-                        href="https://github.com/strapi/strapi/issues/new?assignees=&labels=&projects=&template=BUG_REPORT.md"
+                        href="https://github.com/strapi/strapi/issues"
                       >{`Strapi's GitHub`}</Link>
                     ),
                   }
