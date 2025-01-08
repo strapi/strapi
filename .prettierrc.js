74c9f3f3de85261ea81677af8af1f1d49ca56b46
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2025-01-08 10:34:39 +0530

    Update .prettierrc.js
    The provided configuration appears to be a Prettier configuration, used to enforce consistent code formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
-module.exports = {
-  endOfLine: 'lf',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  printWidth: 100,
-  arrowParens: 'always',
+ module.exports = {
+  endOfLine: 'lf', // Ensure consistent line endings (LF)
+  semi: true, // Use semicolons at the end of statements
+  singleQuote: true, // Use single quotes for strings
+  tabWidth: 2, // Use 2 spaces per indentation level
+  trailingComma: 'es5', // Include trailing commas where valid in ES5
+  printWidth: 100, // Wrap lines at 100 characters for better readability
+  arrowParens: 'always', // Always include parentheses around arrow function arguments
 };
