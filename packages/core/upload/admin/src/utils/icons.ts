ceae704cdb935fbddc5a2eac282007d95b3b68b0
Perry Fardella <perry.fardella@live.com>
2025-01-13 19:47:57 +0700

    Fix typing errors that were causing errors during yarn setup

--- packages/core/upload/admin/src/utils/icons.ts
+++ packages/core/upload/admin/src/utils/icons.ts
@@ -1,17 +1,16 @@
 import { File, FileCsv, FilePdf, FileXls, FileZip } from '@strapi/icons';
 import { FileType } from '../constants';
 
-export const FILE_TYPE_ICON_COMPONENT_MAP = {
+export const FILE_TYPE_ICON_COMPONENT_MAP: Record<string, React.ComponentType<any>> = {
   [FileType.Pdf]: FilePdf,
   [FileType.Csv]: FileCsv,
   [FileType.Xls]: FileXls,
   [FileType.Zip]: FileZip,
 } as const;
 
-export const DEFAULT_FILE_ICON = File;
+export const DEFAULT_FILE_ICON: React.ComponentType<any> = File;
 
-export const getFileIconComponent = (fileType?: string) => {
-  // If we have an icon for the given file type, return it
+export const getFileIconComponent = (fileType?: string): React.ComponentType<any> => {
   if (Object.values(FileType).some((type) => fileType?.includes(type))) {
     return FILE_TYPE_ICON_COMPONENT_MAP[fileType as FileType] ?? DEFAULT_FILE_ICON;
   }
