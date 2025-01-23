74fa0d7baab303fba50d901ef24d217c2fe27a74
Ben Irvin <ben@innerdvations.com>
2025-01-23 15:38:57 +0100

    test(api): login with pass that previously validated

--- tests/api/core/admin/admin-auth.test.api.js
+++ tests/api/core/admin/admin-auth.test.api.js
@@ -13,7 +13,6 @@ describe('Admin Auth End to End', () => {
   let rq;
   let strapi;
   let utils;
-
   beforeAll(async () => {
     strapi = await createStrapiInstance();
     rq = await createAuthRequest({ strapi });
@@ -37,6 +36,38 @@ describe('Admin Auth End to End', () => {
         url: '/admin/login',
         method: 'POST',
         body: superAdmin.loginInfo,
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.data).toMatchObject({
+        token: expect.any(String),
+        user: {
+          firstname: expect.stringOrNull(),
+          lastname: expect.stringOrNull(),
+          username: expect.stringOrNull(),
+          email: expect.any(String),
+          isActive: expect.any(Boolean),
+        },
+      });
+    });
+
+    test('Can login with password that no longer passes validation', async () => {
+      // insert a user with a password that is over 72 bytes, which is not valid
+      const longPassword = `aA1${'b'.repeat(100)}`;
+      const someUser = await utils.createUser({
+        email: 'some-user@strapi.io',
+        firstname: 'some',
+        lastname: 'user',
+        password: longPassword,
+      });
+
+      const res = await rq({
+        url: '/admin/login',
+        method: 'POST',
+        body: {
+          email: someUser.email,
+          password: longPassword,
+        },
       });
 
       expect(res.statusCode).toBe(200);
