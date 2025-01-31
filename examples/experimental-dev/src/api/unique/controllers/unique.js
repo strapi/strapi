fe596546f76e54890dd4f2cce581206adfa19f1a
Convly <jean-sebastien.herbaux@epitech.eu>
2025-01-31 15:46:51 +0100

    chore: remove experimental-dev example folder and its configuration

--- examples/experimental-dev/src/api/unique/controllers/unique.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * unique controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::unique.unique');
