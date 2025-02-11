0b95be8182b503d90c00dc0e74f86ef87a6c9dc4
Perry Fardella <perry.fardella@live.com>
2025-02-11 16:23:04 +0800

    Clean up typing issues

--- packages/core/upload/admin/src/utils/icons.ts
+++ packages/core/upload/admin/src/utils/icons.ts
@@ -1,16 +1,29 @@
 import { File, FileCsv, FilePdf, FileXls, FileZip } from '@strapi/icons';
 import { DocType } from '../constants';
+import { DefaultTheme } from 'styled-components';
+import { SVGProps } from 'react';
 
-export const FILE_TYPE_ICON_COMPONENT_MAP: Record<string, React.ComponentType> = {
+// The IconProps interface is not exported from our design library package, so we need to redefine it here
+interface IconProps extends Omit<SVGProps<SVGSVGElement>, 'fill' | 'stroke'> {
+  /**
+   * @default "currentColor"
+   */
+  fill?: keyof DefaultTheme['colors'] | (string & {});
+  stroke?: keyof DefaultTheme['colors'] | (string & {});
+}
+
+type IconComponent = React.FC<IconProps>;
+
+export const FILE_TYPE_ICON_COMPONENT_MAP: Record<string, IconComponent> = {
   [DocType.Pdf]: FilePdf,
   [DocType.Csv]: FileCsv,
   [DocType.Xls]: FileXls,
   [DocType.Zip]: FileZip,
 } as const;
 
-export const DEFAULT_FILE_ICON: React.ComponentType = File;
+export const DEFAULT_FILE_ICON: IconComponent = File;
 
-export const getFileIconComponent = (docType?: string): React.ComponentType => {
+export const getFileIconComponent = (docType?: string) => {
   if (Object.values(DocType).some((type) => docType?.includes(type))) {
     return FILE_TYPE_ICON_COMPONENT_MAP[docType as DocType] ?? DEFAULT_FILE_ICON;
   }
