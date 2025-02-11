6e9ccaaeaf372189610d2025b345234fc2fbbb63
Perry Fardella <perry.fardella@live.com>
2025-02-11 15:02:59 +0800

    Remove explicit any typing, allowing typescript to interpret correct types.

--- packages/core/upload/admin/src/utils/icons.ts
+++ packages/core/upload/admin/src/utils/icons.ts
@@ -1,16 +1,16 @@
 import { File, FileCsv, FilePdf, FileXls, FileZip } from '@strapi/icons';
 import { DocType } from '../constants';
 
-export const FILE_TYPE_ICON_COMPONENT_MAP: Record<string, React.ComponentType<any>> = {
+export const FILE_TYPE_ICON_COMPONENT_MAP: Record<string, React.ComponentType> = {
   [DocType.Pdf]: FilePdf,
   [DocType.Csv]: FileCsv,
   [DocType.Xls]: FileXls,
   [DocType.Zip]: FileZip,
 } as const;
 
-export const DEFAULT_FILE_ICON: React.ComponentType<any> = File;
+export const DEFAULT_FILE_ICON: React.ComponentType = File;
 
-export const getFileIconComponent = (docType?: string): React.ComponentType<any> => {
+export const getFileIconComponent = (docType?: string): React.ComponentType => {
   if (Object.values(DocType).some((type) => docType?.includes(type))) {
     return FILE_TYPE_ICON_COMPONENT_MAP[docType as DocType] ?? DEFAULT_FILE_ICON;
   }
