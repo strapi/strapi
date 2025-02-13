2c6427fde615b37ad85fbf9d2ef11dc49882b659
Simone Taeggi <startae14@gmail.com>
2025-02-13 16:40:34 +0100

    feat(relations-on-th-fly): fix DZ and components problem in the Relation modal

--- packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/Relations/Relations.tsx
+++ packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/Relations/Relations.tsx
@@ -197,7 +197,7 @@ const UnstableRelationsField = React.forwardRef<HTMLDivElement, RelationsFieldPr
      * Same with `uid` and `documentModel`.
      */
     const id = componentId ? componentId.toString() : documentId;
-    const model = componentUID ?? documentModel;
+    const model = documentModel ?? componentUID;
 
     /**
      * The `name` prop is a complete path to the field, e.g. `field1.field2.field3`.
