42f988e41b31d1ed1c7ba1847939f8a398222729
Fahad Ali <141234330+md-fahad-ali@users.noreply.github.com>
2025-02-15 15:33:33 +0600

    Update NavUser.tsx and also fix the previous issue

--- packages/core/admin/admin/src/components/MainNav/NavUser.tsx
+++ packages/core/admin/admin/src/components/MainNav/NavUser.tsx
@@ -23,15 +23,10 @@ const MenuTrigger = styled(Menu.Trigger)`
 
 const MenuContent = styled(Menu.Content)`
   left: ${({ theme }) => theme.spaces[3]};
-  width: auto;
-  max-width: 300px;
-  height: auto;
+  padding: 0;
   max-height: 300px;
-  overflow-y: auto;
-  border-radius: ${({ theme }) => theme.spaces[2]};
-  box-shadow: ${({ theme }) => theme.shadows.filterShadow};
-  background: ${({ theme }) => theme.colors.neutral0};
-  color: ${({ theme }) => theme.colors.neutral800};
+  width: max-content;
+  max-width: 240px;
 `;
 
 const MenuItem = styled(Menu.Item)`
@@ -54,6 +49,21 @@ const MenuItemDanger = styled(MenuItem)`
   }
 `;
 
+const getDisplayUserName = (user: any) =>
+  user?.firstname && user?.lastname
+    ? `${user.firstname} ${user.lastname}`
+    : user?.username || 'Unknown User';
+
+const StyledMenuSeparator = styled(Menu.Separator)`
+  margin: 0;
+  height: 1px;
+  background: ${({ theme }) => theme.colors.neutral150};
+`;
+
+export interface NavUserProps extends ButtonProps {
+  initials: string;
+}
+
 export const NavUser = ({ children, initials, ...props }: NavUserProps) => {
   const { formatMessage } = useIntl();
   const navigate = useNavigate();
@@ -68,10 +78,12 @@ export const NavUser = ({ children, initials, ...props }: NavUserProps) => {
     navigate('/auth/login');
   };
 
+  const displayUserName = React.useMemo(() => getDisplayUserName(user), [user]);
+
   return (
     <Flex
       justifyContent="center"
-      padding={3}
+      padding={1}
       borderStyle="solid"
       borderWidth="1px 0 0 0"
       borderColor="neutral150"
@@ -80,30 +92,43 @@ export const NavUser = ({ children, initials, ...props }: NavUserProps) => {
       <Menu.Root>
         <MenuTrigger endIcon={null} fullWidth justifyContent="center">
           <Avatar.Item delayMs={0} fallback={initials} />
-          <VisuallyHidden tag="span">{children}</VisuallyHidden>
+          <VisuallyHidden tag="span">{displayUserName}</VisuallyHidden>
         </MenuTrigger>
-        <MenuContent popoverPlacement="top-center" zIndex={3}>
+        <MenuContent popoverPlacement="top-start" zIndex={3}>
           {user && (
-            <Flex direction="column" padding={0} gap={0} alignItems="flex-start">
-              <Flex direction="column" padding={4} gap={0} alignItems="flex-start">
-                <Typography variant="beta" fontWeight="bold" style={{ marginBottom: '2px' }}>
-                  {user.firstname ? `${user.firstname} ${user.lastname}` : user.username}
-                </Typography>
-                <Typography variant="pi" textColor="neutral600">
-                  {user.email}
-                </Typography>
+            <Flex
+              direction="column"
+              gap={0}
+              alignItems="flex-start"
+              padding={4}
+            >
+              <Typography
+                variant="omega"
+                fontWeight="bold"
+                textTransform="none"
                 
-                <Flex wrap="wrap" gap={1} style={{ paddingBlockStart: '12px'}}>
-                  {user.roles.map((role) => (
-                    <Badge key={role.id}>
-                      {role.name}
-                    </Badge>
-                  ))}
-                </Flex>
+              >
+                {displayUserName}
+              </Typography>
+
+              <Typography
+                variant="pi"
+                textColor="neutral600"
+                marginBottom={3}
+              >
+                {user?.email}
+              </Typography>
+
+              <Flex wrap="wrap" gap={2} width="100%">
+                {user?.roles?.map((role) => (
+                  <Badge key={role.id}>{role.name}</Badge>
+                ))}
               </Flex>
             </Flex>
           )}
-          <hr style={{ width: '100%', borderColor: '#4a4a6ac7', color: '#32324d'}} />
+
+          <StyledMenuSeparator />
+
           <MenuItem onSelect={handleProfile}>
             {formatMessage({
               id: 'global.profileSettings',
