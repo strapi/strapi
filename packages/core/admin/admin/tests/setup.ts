e3ea6a28f489c824c26d30ec8e79db4327a326b3
Ben Irvin <ben@innerdvations.com>
2025-02-17 15:25:27 +0100

    revert: remove accidentally added code

--- packages/core/admin/admin/tests/setup.ts
+++ packages/core/admin/admin/tests/setup.ts
@@ -2,10 +2,6 @@ import { TextEncoder } from 'util';
 
 import { server } from './server';
 
-// Note: We set this here because setting it in the config is broken for projects: https://github.com/jestjs/jest/issues/9696
-// Also, there are issues with async tests unless it is set at global scope: https://github.com/jestjs/jest/issues/11543
-jest.setTimeout(60 * 1000);
-
 // Jest doesn't have access to TextEncoder
 // See https://github.com/inrupt/solid-client-authn-js/issues/1676#issuecomment-917016646
 global.TextEncoder = TextEncoder;
