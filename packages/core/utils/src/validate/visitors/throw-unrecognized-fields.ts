1bca6bd937d7cb128c6979182e5bc67f98ffacb0
Ben Irvin <ben.irvin@strapi.io>
2025-02-18 10:40:40 +0100

    Update packages/core/utils/src/validate/visitors/throw-unrecognized-fields.ts

--- packages/core/utils/src/validate/visitors/throw-unrecognized-fields.ts
+++ packages/core/utils/src/validate/visitors/throw-unrecognized-fields.ts
@@ -12,7 +12,7 @@ import type { Visitor } from '../../traverse-entity';
 import { throwInvalidKey } from '../utils';
 
 // TODO these should all be centralized somewhere instead of maintaining a list
-const ID_FIELDS = [constants.DOC_ID_ATTRIBUTE];
+const ID_FIELDS = [constants.ID_ATTRIBUTE, constants.DOC_ID_ATTRIBUTE];
 const ALLOWED_ROOT_LEVEL_FIELDS = [constants.DOC_ID_ATTRIBUTE];
 const MORPH_TO_ALLOWED_FIELDS = ['__type'];
 const DYNAMIC_ZONE_ALLOWED_FIELDS = ['__component'];
