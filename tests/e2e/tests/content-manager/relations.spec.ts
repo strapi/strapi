0adce7726550a976beab3caa058ce80abaddcf27
Simone Taeggi <startae14@gmail.com>
2025-02-24 15:40:34 +0100

    feat(relations-on-the-fly): fix e2e test

---
+++ tests/e2e/tests/content-manager/relations.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../../utils/login';
+import { resetDatabaseAndImportDataFromPath } from '../../utils/dts-import';
+import { describeOnCondition } from '../../utils/shared';
+
+describeOnCondition(process.env.STRAPI_FEATURES_UNSTABLE_RELATIONS_ON_THE_FLY === 'true')(
+  'Relations on the fly tests',
+  () => {
+    test.beforeEach(async ({ page }) => {
+      await resetDatabaseAndImportDataFromPath('with-admin.tar');
+      await page.goto('/admin');
+      await login({ page });
+    });
+    test.describe('Edit Modal', () => {
+      test('as a user I want to open a relation modal inside a collection', async ({ page }) => {
+        await page.getByRole('link', { name: 'Content Manager' }).click();
+        await page.getByRole('link', { name: 'Author' }).click();
+        await page.getByRole('gridcell', { name: 'Ted Lasso' }).click();
+
+        await expect(page.getByRole('heading', { name: 'Ted Lasso' })).toBeVisible();
+
+        await page.getByRole('button', { name: 'Pourquoi je préfère le' }).click();
+        // it opens the edit relations modal
+        await expect(page.getByText('Edit a relation')).toBeVisible();
+      });
+    });
+  }
+);
