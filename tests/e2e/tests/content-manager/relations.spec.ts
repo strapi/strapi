e8ef476228f815c971c69fa3676fa4c08ac1e221
Simone Taeggi <startae14@gmail.com>
2025-02-26 15:24:09 +0100

    feat(relations-on-the-fly): add e2e test for the fullpage

--- tests/e2e/tests/content-manager/relations.spec.ts
+++ tests/e2e/tests/content-manager/relations.spec.ts
@@ -3,6 +3,9 @@ import { login } from '../../utils/login';
 import { resetDatabaseAndImportDataFromPath } from '../../utils/dts-import';
 import { clickAndWait, describeOnCondition } from '../../utils/shared';
 
+const AUTHOR_EDIT_URL =
+  /\/admin\/content-manager\/collection-types\/api::author.author\/(?!create)[^/]/;
+
 describeOnCondition(process.env.STRAPI_FEATURES_UNSTABLE_RELATIONS_ON_THE_FLY === 'true')(
   'Unstable Relations on the fly',
   () => {
@@ -12,7 +15,9 @@ describeOnCondition(process.env.STRAPI_FEATURES_UNSTABLE_RELATIONS_ON_THE_FLY ==
       await login({ page });
     });
 
-    test('as a user I want to open a relation modal inside a collection', async ({ page }) => {
+    test('as a user I want to open a relation modal inside a collection and then open it full page', async ({
+      page,
+    }) => {
       await clickAndWait(page, page.getByRole('link', { name: 'Content Manager' }));
       await clickAndWait(page, page.getByRole('link', { name: 'Article' }));
       await clickAndWait(
@@ -24,9 +29,18 @@ describeOnCondition(process.env.STRAPI_FEATURES_UNSTABLE_RELATIONS_ON_THE_FLY ==
         page.getByRole('heading', { name: 'West Ham post match analysis' })
       ).toBeVisible();
 
-      await clickAndWait(page, page.getByRole('button', { name: 'West Ham post match analysis' }));
+      // Add a new relation to the entry
+      await clickAndWait(page, page.getByRole('combobox', { name: 'authors' }));
+      await clickAndWait(page, page.getByLabel('Coach BeardDraft'));
+      await clickAndWait(page, page.getByRole('button', { name: 'Coach Beard' }));
       // it opens the edit relations modal
       await expect(page.getByText('Edit a relation')).toBeVisible();
+
+      // click on the full page icon
+      await clickAndWait(page, page.getByRole('link', { name: 'Go to entry' }));
+      await clickAndWait(page, page.getByRole('button', { name: 'Confirm' }));
+      await page.waitForURL(AUTHOR_EDIT_URL);
+      await expect(page.getByRole('heading', { name: 'Coach Beard' })).toBeVisible();
     });
   }
 );
