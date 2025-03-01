af9f49129346fd74fdcde33f2369f8e2a944c173
med8bra <med8bra@gmail.com>
2025-03-01 19:52:46 +0000

    chore(email-amazon-ses): migrate email provider from node-ses to @aws-sdk/client-ses

--- packages/providers/email-amazon-ses/src/index.ts
+++ packages/providers/email-amazon-ses/src/index.ts
@@ -1,4 +1,4 @@
-import nodeSES from 'node-ses';
+import { SESClient, SESClientConfig, SendEmailCommand } from '@aws-sdk/client-ses';
 
 interface Settings {
   defaultFrom: string;
@@ -7,9 +7,9 @@ interface Settings {
 
 interface SendOptions {
   from?: string;
-  to: string;
-  cc: string;
-  bcc: string;
+  to: string | string[];
+  cc?: string | string[];
+  bcc?: string | string[];
   replyTo?: string;
   subject: string;
   text: string;
@@ -17,45 +17,77 @@ interface SendOptions {
   [key: string]: unknown;
 }
 
-interface ProviderOptions {
+/**
+ * For backward compatibility with node-ses provider options
+ */
+interface LegacyProviderOptions {
   key: string;
   secret: string;
   amazon?: string;
 }
 
+type ProviderOptions = LegacyProviderOptions | SESClientConfig;
+
+const asArray = (value: string | string[]) => (Array.isArray(value) ? value : [value]);
+const getConfig = (config: ProviderOptions): SESClientConfig => {
+  const isLegacyConfig = (config: ProviderOptions): config is LegacyProviderOptions =>
+    'secret' in config;
+
+  if (!isLegacyConfig(config)) {
+    return config;
+  }
+  return {
+    credentials: {
+      accessKeyId: config.key,
+      secretAccessKey: config.secret,
+    },
+    // extract region from https://email.<region>.amazonaws.com
+    region: config.amazon?.split('.')[1] ?? 'us-east-1',
+  };
+};
+
 export default {
   init(providerOptions: ProviderOptions, settings: Settings) {
-    const client = nodeSES.createClient(providerOptions);
+    const client = new SESClient(getConfig(providerOptions));
 
     return {
-      send(options: SendOptions): Promise<void> {
-        return new Promise((resolve, reject) => {
-          const { from, to, cc, bcc, replyTo, subject, text, html, ...rest } = options;
-
-          const msg: nodeSES.sendEmailOptions = {
-            from: from || settings.defaultFrom,
-            subject,
-            message: html,
+      async send(options: SendOptions): Promise<void> {
+        try {
+          const {
+            from = settings.defaultFrom,
             to,
             cc,
             bcc,
-            replyTo: replyTo || settings.defaultReplyTo,
-            altText: text,
-            ...rest,
-          };
+            replyTo = settings.defaultReplyTo,
+            subject,
+            text,
+            html,
+          } = options;
 
-          client.sendEmail(msg, (err) => {
-            if (err) {
-              if (err.Message) {
-                // eslint-disable-next-line prefer-promise-reject-errors
-                reject(`${err.Message} ${err.Detail ? err.Detail : ''}`);
-              }
-              reject(err);
-            } else {
-              resolve();
-            }
+          const emailCommand = new SendEmailCommand({
+            Source: from,
+            Destination: {
+              ToAddresses: asArray(to),
+              CcAddresses: cc ? asArray(cc) : undefined,
+              BccAddresses: bcc ? asArray(bcc) : undefined,
+            },
+            Message: {
+              Subject: { Data: subject },
+              Body: {
+                Text: { Data: text },
+                Html: { Data: html },
+              },
+            },
+            ReplyToAddresses: replyTo ? [replyTo || from] : undefined,
           });
-        });
+
+          await client.send(emailCommand);
+        } catch (error) {
+          if (error instanceof Error) {
+            throw new Error(`AWS SES Error: ${error.message}`);
+          }
+          throw error;
+        }
       },
     };
   },
