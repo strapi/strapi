92d6f75d733d2c2164e675bc5fd0b91492d968d4
Convly <jean-sebastien.herbaux@epitech.eu>
2025-04-14 11:51:08 +0200

    feat: add context classes & factories

---
+++ packages/core/openapi/src/context/types.ts
@@ -0,0 +1,37 @@
+import type { Core } from '@strapi/types';
+import type { ComponentRegistry } from '../registries';
+import type { Timer } from '../utils';
+
+export interface ContextRegistries {
+  components: ComponentRegistry;
+}
+
+export interface TimeStats {
+  startTime: number;
+  endTime: number;
+  elapsedTime: number;
+}
+
+export interface Stats {
+  time: TimeStats;
+}
+
+export interface ContextOutput<T> {
+  data: T;
+  stats: Stats;
+}
+
+export interface Context<T = unknown> {
+  routes: Core.Route[];
+  strapi: Core.Strapi;
+  timer: Timer;
+  registries: ContextRegistries;
+  output: ContextOutput<T>;
+}
+
+export type PartialContext<T> = Partial<Pick<Context<T>, 'timer' | 'registries'>> &
+  Required<Pick<Context<T>, 'strapi' | 'routes'>>;
+
+export interface ContextFactory<T> {
+  create(context: PartialContext<T>, defaultValue: T): Context<T>;
+}
