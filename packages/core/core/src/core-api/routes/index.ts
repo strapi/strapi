103096eb27c9fe2d306af9a9628b186a546d133b
Convly <jean-sebastien.herbaux@epitech.eu>
2025-05-05 11:07:11 +0200

    feat: add route validation and response schemas using CoreContentTypeRouteValidator

--- packages/core/core/src/core-api/routes/index.ts
+++ packages/core/core/src/core-api/routes/index.ts
@@ -1,68 +1,137 @@
+import type { Core, Schema } from '@strapi/types';
 import { contentTypes as contentTypeUtils } from '@strapi/utils';
-import type { Schema } from '@strapi/types';
+import { z } from 'zod';
+import { CoreContentTypeRouteValidator } from './validation';
 
-export const createRoutes = ({ contentType }: { contentType: Schema.ContentType }) => {
+export const createRoutes = ({
+  strapi,
+  contentType,
+}: {
+  strapi: Core.Strapi;
+  contentType: Schema.ContentType;
+}) => {
   if (contentTypeUtils.isSingleType(contentType)) {
-    return getSingleTypeRoutes(contentType);
+    return getSingleTypeRoutes(contentType, strapi);
   }
 
-  return getCollectionTypeRoutes(contentType);
+  return getCollectionTypeRoutes(contentType, strapi);
 };
 
-const getSingleTypeRoutes = ({ uid, info }: Schema.ContentType) => {
+const getSingleTypeRoutes = (
+  { uid, info }: Schema.ContentType,
+  strapi: Core.Strapi
+): Record<string, Partial<Core.Route>> => {
+  const validator = new CoreContentTypeRouteValidator(strapi, uid);
+
   return {
     find: {
       method: 'GET',
       path: `/${info.singularName}`,
       handler: `${uid}.find`,
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
       config: {},
     },
     update: {
       method: 'PUT',
       path: `/${info.singularName}`,
       handler: `${uid}.update`,
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
       config: {},
     },
     delete: {
       method: 'DELETE',
       path: `/${info.singularName}`,
       handler: `${uid}.delete`,
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
       config: {},
     },
   };
 };
 
-const getCollectionTypeRoutes = ({ uid, info }: Schema.ContentType) => {
+const getCollectionTypeRoutes = (
+  schema: Schema.ContentType,
+  strapi: Core.Strapi
+): Record<string, Partial<Core.Route>> => {
+  const { uid, info } = schema;
+
+  const validator = new CoreContentTypeRouteValidator(strapi, uid);
+
   return {
     find: {
       method: 'GET',
       path: `/${info.pluralName}`,
       handler: `${uid}.find`,
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        200: z.object({ data: validator.documents }),
+      },
       config: {},
     },
     findOne: {
       method: 'GET',
       path: `/${info.pluralName}/:id`,
       handler: `${uid}.findOne`,
-      config: {},
+      request: {
+        params: { id: validator.documentID },
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
     },
     create: {
       method: 'POST',
       path: `/${info.pluralName}`,
       handler: `${uid}.create`,
+      request: {
+        params: { id: validator.documentID },
+        query: validator.query(['fields', 'sort', 'populate']),
+      },
+      responses: {
+        201: z.object({ data: validator.document }),
+      },
       config: {},
     },
     update: {
       method: 'PUT',
       path: `/${info.pluralName}/:id`,
       handler: `${uid}.update`,
-      config: {},
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+        params: { id: validator.documentID },
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
     },
     delete: {
       method: 'DELETE',
       path: `/${info.pluralName}/:id`,
       handler: `${uid}.delete`,
-      config: {},
+      request: {
+        query: validator.query(['fields', 'sort', 'populate']),
+        params: { id: validator.documentID },
+      },
+      responses: {
+        200: z.object({ data: validator.document }),
+      },
     },
   };
 };
