25379bb83212618ecca1ad52dfa1cef62c1252b3
Convly <jean-sebastien.herbaux@epitech.eu>
2025-05-12 16:00:33 +0200

    fix: prettier

--- packages/core/core/src/core-api/routes/validation/index.ts
+++ packages/core/core/src/core-api/routes/validation/index.ts
@@ -1,4 +1,4 @@
 export { AbstractCoreRouteValidator } from './common';
 export { CoreContentTypeRouteValidator } from './content-type';
 export { CoreComponentRouteValidator } from './component';
-export { mapAttributeToSchema, mapAttributeToInputSchema } from './attributes'
+export { mapAttributeToSchema, mapAttributeToInputSchema } from './attributes';
