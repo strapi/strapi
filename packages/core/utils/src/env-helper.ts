52034d460a20de619cc47d297fdd8205237586c5
Eric Ferreira <eric@ericmakesapps.com>
2025-05-27 16:30:31 -0400

    Fix types of `env` helper
    The env helper should not return a nullable value when a default is passed. This commit fixes all functions in the env helper to conform to the correct types.

--- packages/core/utils/src/env-helper.ts
+++ packages/core/utils/src/env-helper.ts
@@ -2,7 +2,9 @@ import _ from 'lodash';
 
 export type Env = typeof envFn & typeof utils;
 
-function envFn<T>(key: string, defaultValue?: T): string | T | undefined {
+function envFn(key: string): string | undefined;
+function envFn(key: string, defaultValue: string): string;
+function envFn(key: string, defaultValue?: string): string | undefined {
   return _.has(process.env, key) ? process.env[key] : defaultValue;
 }
 
@@ -10,90 +12,104 @@ function getKey(key: string) {
   return process.env[key] ?? '';
 }
 
+function int(key: string): number | undefined;
+function int(key: string, defaultValue: number): number;
+function int(key: string, defaultValue?: number): number | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return parseInt(getKey(key), 10);
+}
+
+function float(key: string): number | undefined;
+function float(key: string, defaultValue: number): number;
+function float(key: string, defaultValue?: number): number | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return parseFloat(getKey(key));
+}
+
+function bool(key: string): boolean | undefined;
+function bool(key: string, defaultValue: boolean): boolean;
+function bool(key: string, defaultValue?: boolean): boolean | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return getKey(key) === 'true';
+}
+
+function json(key: string, defaultValue?: object): any {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  try {
+    return JSON.parse(getKey(key));
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Invalid json environment variable ${key}: ${error.message}`);
+    }
+
+    throw error;
+  }
+}
+
+function array(key: string): string[] | undefined;
+function array(key: string, defaultValue: string[]): string[];
+function array(key: string, defaultValue?: string[]): string[] | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  let value = getKey(key);
+
+  if (value.startsWith('[') && value.endsWith(']')) {
+    value = value.substring(1, value.length - 1);
+  }
+
+  return value.split(',').map((v) => {
+    return _.trim(_.trim(v, ' '), '"');
+  });
+}
+
+function date(key: string): Date | undefined;
+function date(key: string, defaultValue: Date): Date;
+function date(key: string, defaultValue?: Date): Date | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return new Date(getKey(key));
+}
+
+/** Gets a value from env that matches oneOf provided values */
+function oneOf<T extends string>(key: string, expectedValues: T[]): T | undefined;
+function oneOf<T extends string>(key: string, expectedValues: T[], defaultValue: T): T;
+function oneOf(key: string, expectedValues: string[], defaultValue?: string) {
+  if (!expectedValues) {
+    throw new Error(`env.oneOf requires expectedValues`);
+  }
+
+  if (defaultValue && !expectedValues.includes(defaultValue)) {
+    throw new Error(`env.oneOf requires defaultValue to be included in expectedValues`);
+  }
+
+  const rawValue = env(key, defaultValue);
+  return expectedValues.includes(rawValue) ? rawValue : defaultValue;
+}
+
 const utils = {
-  int(key: string, defaultValue?: number): number | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return parseInt(getKey(key), 10);
-  },
-
-  float(key: string, defaultValue?: number): number | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return parseFloat(getKey(key));
-  },
-
-  bool(key: string, defaultValue?: boolean): boolean | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return getKey(key) === 'true';
-  },
-
-  json(key: string, defaultValue?: object) {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    try {
-      return JSON.parse(getKey(key));
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Invalid json environment variable ${key}: ${error.message}`);
-      }
-
-      throw error;
-    }
-  },
-
-  array(key: string, defaultValue?: string[]): string[] | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    let value = getKey(key);
-
-    if (value.startsWith('[') && value.endsWith(']')) {
-      value = value.substring(1, value.length - 1);
-    }
-
-    return value.split(',').map((v) => {
-      return _.trim(_.trim(v, ' '), '"');
-    });
-  },
-
-  date(key: string, defaultValue?: Date): Date | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return new Date(getKey(key));
-  },
-
-  /**
-   * Gets a value from env that matches oneOf provided values
-   * @param {string} key
-   * @param {string[]} expectedValues
-   * @param {string|undefined} defaultValue
-   * @returns {string|undefined}
-   */
-  oneOf(key: string, expectedValues?: unknown[], defaultValue?: unknown) {
-    if (!expectedValues) {
-      throw new Error(`env.oneOf requires expectedValues`);
-    }
-
-    if (defaultValue && !expectedValues.includes(defaultValue)) {
-      throw new Error(`env.oneOf requires defaultValue to be included in expectedValues`);
-    }
-
-    const rawValue = env(key, defaultValue);
-    return expectedValues.includes(rawValue) ? rawValue : defaultValue;
-  },
+  int,
+  float,
+  bool,
+  json,
+  array,
+  date,
+  oneOf,
 };
 
 const env: Env = Object.assign(envFn, utils);
