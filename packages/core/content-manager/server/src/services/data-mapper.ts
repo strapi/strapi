c79dedbc354da5501851a6de31940b6fc359b465
Adrien Lepoutre <thewebsdoor@gmail.com>
2025-05-27 14:52:22 +0200

    fix(content-manager): documentId missing from fields for the view and filters

--- packages/core/content-manager/server/src/services/data-mapper.ts
+++ packages/core/content-manager/server/src/services/data-mapper.ts
@@ -26,6 +26,9 @@ export default () => ({
         id: {
           type: 'integer',
         },
+        documentId: {
+          type: 'string',
+        },
         ...formatAttributes(contentType),
       },
     };
