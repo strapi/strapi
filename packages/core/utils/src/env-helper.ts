556b0063df8248a68d91fa7a80b6308c9176dba2
Eric Ferreira <eric@ericmakesapps.com>
2025-05-29 17:42:54 -0400

    Also allow passing nullable default value (to pass the docs build)

--- packages/core/utils/src/env-helper.ts
+++ packages/core/utils/src/env-helper.ts
@@ -3,9 +3,9 @@ import _ from 'lodash';
 export type Env = typeof envFn & typeof utils;
 
 function envFn(key: string, defaultValue: string): string;
-function envFn(key: string, defaultValue?: string): string | undefined;
-function envFn(key: string, defaultValue?: string): string | undefined {
-  return _.has(process.env, key) ? process.env[key] : defaultValue;
+function envFn(key: string, defaultValue?: string | null | undefined): string | undefined;
+function envFn(key: string, defaultValue?: string | null | undefined): string | undefined {
+  return _.has(process.env, key) ? process.env[key] : (defaultValue ?? undefined);
 }
 
 function getKey(key: string) {
@@ -13,38 +13,38 @@ function getKey(key: string) {
 }
 
 function int(key: string, defaultValue: number): number;
-function int(key: string, defaultValue?: number): number | undefined;
-function int(key: string, defaultValue?: number): number | undefined {
+function int(key: string, defaultValue?: number | null | undefined): number | undefined;
+function int(key: string, defaultValue?: number | null | undefined): number | undefined {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   return parseInt(getKey(key), 10);
 }
 
 function float(key: string, defaultValue: number): number;
-function float(key: string, defaultValue?: number): number | undefined;
-function float(key: string, defaultValue?: number): number | undefined {
+function float(key: string, defaultValue?: number | null | undefined): number | undefined;
+function float(key: string, defaultValue?: number | null | undefined): number | undefined {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   return parseFloat(getKey(key));
 }
 
 function bool(key: string, defaultValue: boolean): boolean;
-function bool(key: string, defaultValue?: boolean): boolean | undefined;
-function bool(key: string, defaultValue?: boolean): boolean | undefined {
+function bool(key: string, defaultValue?: boolean | null | undefined): boolean | undefined;
+function bool(key: string, defaultValue?: boolean | null | undefined): boolean | undefined {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   return getKey(key) === 'true';
 }
 
-function json(key: string, defaultValue?: object): any {
+function json(key: string, defaultValue?: object | null | undefined): any {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   try {
@@ -59,10 +59,10 @@ function json(key: string, defaultValue?: object): any {
 }
 
 function array(key: string, defaultValue: string[]): string[];
-function array(key: string, defaultValue?: string[]): string[] | undefined;
-function array(key: string, defaultValue?: string[]): string[] | undefined {
+function array(key: string, defaultValue?: string[] | null | undefined): string[] | undefined;
+function array(key: string, defaultValue?: string[] | null | undefined): string[] | undefined {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   let value = getKey(key);
@@ -77,10 +77,10 @@ function array(key: string, defaultValue?: string[]): string[] | undefined {
 }
 
 function date(key: string, defaultValue: Date): Date;
-function date(key: string, defaultValue?: Date): Date | undefined;
-function date(key: string, defaultValue?: Date): Date | undefined {
+function date(key: string, defaultValue?: Date | null | undefined): Date | undefined;
+function date(key: string, defaultValue?: Date | null | undefined): Date | undefined {
   if (!_.has(process.env, key)) {
-    return defaultValue;
+    return defaultValue ?? undefined;
   }
 
   return new Date(getKey(key));
@@ -88,8 +88,16 @@ function date(key: string, defaultValue?: Date): Date | undefined {
 
 /** Gets a value from env that matches oneOf provided values */
 function oneOf<T extends string>(key: string, expectedValues: T[], defaultValue: T): T;
-function oneOf<T extends string>(key: string, expectedValues: T[], defaultValue?: T): T | undefined;
-function oneOf(key: string, expectedValues: string[], defaultValue?: string) {
+function oneOf<T extends string>(
+  key: string,
+  expectedValues: T[],
+  defaultValue?: T | null | undefined
+): T | undefined;
+function oneOf(
+  key: string,
+  expectedValues: string[],
+  defaultValue?: string | null | undefined
+): string | undefined {
   if (!expectedValues) {
     throw new Error(`env.oneOf requires expectedValues`);
   }
@@ -100,7 +108,9 @@ function oneOf(key: string, expectedValues: string[], defaultValue?: string) {
 
   const rawValue = env(key, defaultValue);
 
-  return rawValue != null && expectedValues.includes(rawValue) ? rawValue : defaultValue;
+  return rawValue != null && expectedValues.includes(rawValue)
+    ? rawValue
+    : (defaultValue ?? undefined);
 }
 
 const utils = {
