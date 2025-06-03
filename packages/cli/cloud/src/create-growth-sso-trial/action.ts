7871ec8735035a746b5367866e9411b344815808
Arthur Moreau <kibtpwd@gmail.com>
2025-06-03 19:58:02 +0200

    feat(cli): Introduce the Growth Trial prompt (#23498)

---
+++ packages/cli/cloud/src/create-growth-sso-trial/action.ts
@@ -0,0 +1,50 @@
+import { cloudApiFactory, tokenServiceFactory } from '../services';
+import { createLogger } from '../services/logger';
+import { trackEvent } from '../utils/analytics';
+
+interface CreateGrowthSsoTrialInput {
+  strapiVersion: string | undefined;
+}
+
+interface CreateGrowthSsoTrialResponse {
+  license: string;
+}
+
+export default async ({
+  strapiVersion,
+}: CreateGrowthSsoTrialInput): Promise<CreateGrowthSsoTrialResponse | undefined> => {
+  const logger = createLogger();
+  const { retrieveToken } = await tokenServiceFactory({ logger });
+
+  const token = await retrieveToken();
+  if (!token) {
+    return;
+  }
+
+  const cloudApiService = await cloudApiFactory({ logger }, token);
+
+  try {
+    const { data: config } = await cloudApiService.config();
+    if (!config?.featureFlags?.growthSsoTrialEnabled) {
+      return;
+    }
+  } catch (e: unknown) {
+    logger.debug('Failed to get cli config', e);
+    return;
+  }
+
+  try {
+    const response = await cloudApiService.createTrial({ strapiVersion: strapiVersion || '' });
+
+    await trackEvent({ logger, cwd: process.cwd() }, cloudApiService, 'didCreateGrowthSsoTrial', {
+      strapiVersion: strapiVersion || '',
+    });
+
+    return { license: response.data?.licenseKey };
+  } catch (e: Error | unknown) {
+    logger.debug(e);
+    logger.error(
+      'We encountered an issue while creating your trial. Please try again in a moment. If the problem persists, contact support for assistance.'
+    );
+  }
+};
