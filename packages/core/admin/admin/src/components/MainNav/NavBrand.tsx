9b112d50394b191258b2abb8535ff07ba43c1eff
Adrien Lepoutre <thewebsdoor@gmail.com>
2025-06-27 11:19:28 +0200

    fix: use back configuration context for navbrand

--- packages/core/admin/admin/src/components/MainNav/NavBrand.tsx
+++ packages/core/admin/admin/src/components/MainNav/NavBrand.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, type FlexComponent, VisuallyHidden } from '@strapi/design-sy
 import { useIntl } from 'react-intl';
 import { styled } from 'styled-components';
 
-import { useInitQuery } from '../../services/admin';
+import { useConfiguration } from '../../features/Configuration';
 
 const BrandIconWrapper = styled<FlexComponent>(Flex)`
   svg,
@@ -16,21 +16,21 @@ const BrandIconWrapper = styled<FlexComponent>(Flex)`
 
 export const NavBrand = () => {
   const { formatMessage } = useIntl();
-  const { data: { menuLogo } = {} } = useInitQuery();
+  const {
+    logos: { menu },
+  } = useConfiguration('LeftMenu');
   return (
     <Box padding={3}>
       <BrandIconWrapper direction="column" justifyContent="center" width="3.2rem" height="3.2rem">
-        {menuLogo && (
-          <img
-            src={menuLogo}
-            alt={formatMessage({
-              id: 'app.components.LeftMenu.logo.alt',
-              defaultMessage: 'Application logo',
-            })}
-            width="100%"
-            height="100%"
-          />
-        )}
+        <img
+          src={menu.custom?.url || menu.default}
+          alt={formatMessage({
+            id: 'app.components.LeftMenu.logo.alt',
+            defaultMessage: 'Application logo',
+          })}
+          width="100%"
+          height="100%"
+        />
         <VisuallyHidden>
           <span>
             {formatMessage({
