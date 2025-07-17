0f0379365da590fdfbdaa88361b2224ddbf06390
Adrien Lepoutre <thewebsdoor@gmail.com>
2025-07-17 10:00:44 +0200

    fix: migrate widgets object to an array

--- packages/core/content-manager/admin/src/index.ts
+++ packages/core/content-manager/admin/src/index.ts
@@ -8,7 +8,7 @@ import { previewAdmin } from './preview';
 import { routes } from './router';
 import { prefixPluginTranslations } from './utils/translations';
 
-import type { WidgetType } from '@strapi/admin/strapi-admin';
+import type { WidgetArgs, WidgetType } from '@strapi/admin/strapi-admin';
 
 // NOTE: we have to preload it to ensure chunks will have it available as global
 import 'prismjs';
@@ -48,63 +48,40 @@ export default {
     app.registerPlugin(cm.config);
 
     // Register homepage widgets
-    app.widgets.register([
-      {
-        icon: PuzzlePiece,
-        title: {
-          id: `${PLUGIN_ID}.widget.chart-entries.title`,
-          defaultMessage: 'Entries',
-        },
-        component: async () => {
-          const { ChartEntriesWidget } = await import('./components/Widgets');
-          return ChartEntriesWidget;
-        },
-        pluginId: PLUGIN_ID,
-        id: 'chart-entries',
-        permissions: [{ action: 'plugin::content-manager.explorer.read' }],
-      },
-      {
-        icon: Pencil,
-        title: {
-          id: `${PLUGIN_ID}.widget.last-edited.title`,
-          defaultMessage: 'Last edited entries',
-        },
-        component: async () => {
-          const { LastEditedWidget } = await import('./components/Widgets');
-          return LastEditedWidget;
-        },
-        pluginId: PLUGIN_ID,
-        id: 'last-edited-entries',
-        permissions: [{ action: 'plugin::content-manager.explorer.read' }],
-      },
-      {
-        icon: CheckCircle,
-        title: {
-          id: `${PLUGIN_ID}.widget.last-published.title`,
-          defaultMessage: 'Last published entries',
-        },
-        component: async () => {
-          const { LastPublishedWidget } = await import('./components/Widgets');
-          return LastPublishedWidget;
-        },
-        pluginId: PLUGIN_ID,
-        id: 'last-published-entries',
-        permissions: [{ action: 'plugin::content-manager.explorer.read' }],
-      },
-    ]);
-
     // Always put the last-edited-entries and last-published-entries widgets first in the list of widgets
-    app.widgets.register((widgets: Record<string, WidgetType>) => {
-      const desiredOrder = [
-        'plugin::content-manager.last-edited-entries',
-        'plugin::content-manager.last-published-entries',
-      ];
-      const ordered = [
-        ...desiredOrder.map((uid) => widgets[uid]).filter(Boolean),
-        ...Object.values(widgets).filter((w) => !desiredOrder.includes(w.uid)),
-      ];
-      return Object.fromEntries(ordered.map((w) => [w.uid, w]));
-    });
+    app.widgets.register((widgets: WidgetArgs[] | WidgetType[]) => [
+      ...[
+        {
+          icon: Pencil,
+          title: {
+            id: `${PLUGIN_ID}.widget.last-edited.title`,
+            defaultMessage: 'Last edited entries',
+          },
+          component: async () => {
+            const { LastEditedWidget } = await import('./components/Widgets');
+            return LastEditedWidget;
+          },
+          pluginId: PLUGIN_ID,
+          id: 'last-edited-entries',
+          permissions: [{ action: 'plugin::content-manager.explorer.read' }],
+        },
+        {
+          icon: CheckCircle,
+          title: {
+            id: `${PLUGIN_ID}.widget.last-published.title`,
+            defaultMessage: 'Last published entries',
+          },
+          component: async () => {
+            const { LastPublishedWidget } = await import('./components/Widgets');
+            return LastPublishedWidget;
+          },
+          pluginId: PLUGIN_ID,
+          id: 'last-published-entries',
+          permissions: [{ action: 'plugin::content-manager.explorer.read' }],
+        },
+      ],
+      ...widgets,
+    ]);
   },
   bootstrap(app: any) {
     if (typeof historyAdmin.bootstrap === 'function') {
