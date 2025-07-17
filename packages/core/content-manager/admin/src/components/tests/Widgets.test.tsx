8e95d619c5758705ec20a6f5816e4f2ec94875ee
Adrien Lepoutre <thewebsdoor@gmail.com>
2025-07-17 15:01:37 +0200

    fix: adding redux to upload plugin tests fix: all failing tests

--- packages/core/content-manager/admin/src/components/tests/Widgets.test.tsx
+++ packages/core/content-manager/admin/src/components/tests/Widgets.test.tsx
@@ -3,8 +3,30 @@ import { render, screen } from '@tests/utils';
 
 import { ChartEntriesWidget } from '../Widgets';
 
-jest.mock('../../services/homepage', () => ({
+jest.mock('@strapi/admin/strapi-admin', () => ({
   useGetCountDocumentsQuery: jest.fn(),
+  adminApi: {
+    enhanceEndpoints: jest.fn(() => ({
+      injectEndpoints: jest.fn(() => ({
+        useGetRecentDocumentsQuery: jest.fn(),
+      })),
+    })),
+  },
+  Widget: {
+    Loading: () => <div>Loading widget content...</div>,
+    Error: () => <div>Couldn&apos;t load widget content</div>,
+    NoData: ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
+  },
+}));
+
+jest.mock('../../services/api', () => ({
+  contentManagerApi: {
+    enhanceEndpoints: jest.fn(() => ({
+      injectEndpoints: jest.fn(() => ({
+        useGetRecentDocumentsQuery: jest.fn(),
+      })),
+    })),
+  },
 }));
 
 describe('ChartEntriesWidget', () => {
