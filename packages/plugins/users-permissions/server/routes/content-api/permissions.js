b37f7fb05f2feb6e837e6bb0129579e7ee249a48
Jamie Howard <jhoward1994@gmail.com>
2025-07-21 11:48:25 +0100

    feat(users-permissions): zod schemas for content-api routes

--- packages/plugins/users-permissions/server/routes/content-api/permissions.js
+++ packages/plugins/users-permissions/server/routes/content-api/permissions.js
@@ -1,9 +1,16 @@
 'use strict';
 
-module.exports = [
-  {
-    method: 'GET',
-    path: '/permissions',
-    handler: 'permissions.getPermissions',
-  },
-];
+const { UsersPermissionsRouteValidator } = require('./validation');
+
+module.exports = (strapi) => {
+  const validator = new UsersPermissionsRouteValidator(strapi);
+
+  return [
+    {
+      method: 'GET',
+      path: '/permissions',
+      handler: 'permissions.getPermissions',
+      response: validator.permissionsResponseSchema,
+    },
+  ];
+};
