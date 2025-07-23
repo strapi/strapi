a2e3862f0be67d5abd6e50d0e069fb7a41a4496e
Adrien L <thewebsdoor@gmail.com>
2025-07-23 11:39:31 +0200

    fix: add reducer to be able to mutate widgets in register function (#23908)

--- packages/core/admin/admin/src/core/apis/Widgets.ts
+++ packages/core/admin/admin/src/core/apis/Widgets.ts
@@ -27,38 +27,56 @@ type WidgetArgs = {
   permissions?: Array<Pick<Permission, 'action'> & Partial<Omit<Permission, 'action'>>>;
 };
 
-type Widget = Omit<WidgetArgs, 'id' | 'pluginId'> & { uid: WidgetUID };
+type WidgetWithUID = Omit<WidgetArgs, 'id' | 'pluginId'> & { uid: WidgetUID };
+
+type DescriptionReducer = (prev: WidgetArgs[]) => WidgetArgs[];
 
 class Widgets {
-  widgets: Record<string, Widget>;
+  widgets: WidgetArgs[];
 
   constructor() {
-    this.widgets = {};
+    this.widgets = [];
   }
 
-  register = (widget: WidgetArgs | WidgetArgs[]) => {
-    if (Array.isArray(widget)) {
-      widget.forEach((newWidget) => {
-        this.register(newWidget);
-      });
-    } else {
+  private generateUid = (widget: WidgetArgs): WidgetUID => {
+    return widget.pluginId ? `plugin::${widget.pluginId}.${widget.id}` : `global::${widget.id}`;
+  };
+
+  private checkWidgets = (widgets: WidgetArgs[]): void => {
+    widgets.forEach((widget) => {
       invariant(widget.id, 'An id must be provided');
       invariant(widget.component, 'A component must be provided');
       invariant(widget.title, 'A title must be provided');
       invariant(widget.icon, 'An icon must be provided');
-
-      // Replace id and pluginId with computed uid
-      const { id, pluginId, ...widgetToStore } = widget;
-      const uid: WidgetUID = pluginId ? `plugin::${pluginId}.${id}` : `global::${id}`;
-
-      this.widgets[uid] = { ...widgetToStore, uid };
-    }
+    });
   };
 
-  getAll = () => {
-    return Object.values(this.widgets);
+  register(widgets: WidgetArgs | WidgetArgs[] | DescriptionReducer) {
+    if (Array.isArray(widgets)) {
+      this.checkWidgets(widgets);
+      this.widgets = [...this.widgets, ...widgets];
+    } else if (typeof widgets === 'function') {
+      const newWidgets = widgets(this.widgets);
+      this.checkWidgets(newWidgets);
+      this.widgets = newWidgets;
+    } else if (typeof widgets === 'object') {
+      this.checkWidgets([widgets]);
+      this.widgets.push(widgets);
+    } else {
+      throw new Error('Expected widgets to be an array or a reducer function');
+    }
+  }
+
+  getAll = (): WidgetWithUID[] => {
+    return this.widgets.map((widget) => {
+      const { id, pluginId, ...widgetBase } = widget;
+      return {
+        ...widgetBase,
+        uid: this.generateUid(widget),
+      };
+    });
   };
 }
 
 export { Widgets };
-export type { WidgetArgs, Widget };
+export type { WidgetArgs, WidgetWithUID };
