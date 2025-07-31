b057f306b601feae5133d8a98af04458f2abc99c
Mark Kaylor <mark.kaylor@strapi.io>
2025-07-31 10:14:48 +0200

    fix: incorrect config won't run app

--- examples/empty/config/admin.ts
+++ examples/empty/config/admin.ts
@@ -1,20 +1,19 @@
-export default ({ env }) => ({
+const adminConfig = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'example-token'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'example-salt'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'example-salt'),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
+
+export default adminConfig;
--- examples/empty/config/server.ts
+++ examples/empty/config/server.ts
@@ -1,7 +1,9 @@
-export default ({ env }) => ({
+const serverConfig = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['toBeModified1', 'toBeModified2']),
   },
 });
+
+export default serverConfig;
