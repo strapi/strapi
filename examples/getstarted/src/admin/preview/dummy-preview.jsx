6d80f47ec5062e887f95e903d2e34a20abadc8f1
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2025-08-01 15:25:45 +0200

    chore: revamp dummy preview page

--- examples/getstarted/src/admin/preview/dummy-preview.jsx
+++ examples/getstarted/src/admin/preview/dummy-preview.jsx
@@ -3,21 +3,76 @@ import { useParams } from 'react-router-dom';
 
 // @ts-ignore
 import { Page, Layouts } from '@strapi/admin/strapi-admin';
-import { unstable_useDocument as useDocument } from '@strapi/content-manager/strapi-admin';
 import { Grid, Flex, Typography, JSONInput } from '@strapi/design-system';
 
+const modelsRouteNames = {
+  'api::address.address': 'addresses',
+  'api::category.category': 'categories',
+  'api::condition.condition': 'conditions',
+  'api::country.country': 'countries',
+  'api::homepage.homepage': 'homepages',
+  'api::kitchensink.kitchensink': 'kitchensink',
+  'api::like.like': 'likes',
+  'api::menu.menu': 'menus',
+  'api::menusection.menusection': 'menusections',
+  'api::relation-locale.relation-locale': 'relation-locales',
+  'api::restaurant.restaurant': 'restaurants',
+  'api::review.review': 'reviews',
+  'api::tag.tag': 'tags',
+  'api::temp.temp': 'temps',
+};
+
 const PreviewComponent = () => {
-  const { uid: model, documentId, locale, status, collectionType } = useParams();
-  const { document, refetch } = useDocument({
-    model,
-    documentId,
-    params: {
-      locale,
-      status,
-      populate: '*',
-    },
-    collectionType,
-  });
+  const { uid: model, documentId, locale, status: documentStatus, collectionType } = useParams();
+
+  const [data, setData] = React.useState(null);
+  const [status, setStatus] = React.useState('idle');
+
+  const fetchData = async () => {
+    if (!documentId) return;
+
+    setStatus('loading');
+
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+
+      if (process.env.STRAPI_ADMIN_API_TOKEN) {
+        headers.Authorization = `Bearer ${process.env.STRAPI_ADMIN_API_TOKEN}`;
+      }
+
+      const searchParams = new URLSearchParams({
+        locale,
+        status: documentStatus,
+        encodeSourceMaps: 'true',
+      });
+      const response = await fetch(
+        `http://localhost:1337/api/${modelsRouteNames[model]}/${documentId}?${searchParams.toString()}`,
+        {
+          headers,
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result.data);
+      setStatus('success');
+      window.parent?.postMessage({ type: 'strapiReady' }, '*');
+    } catch (error) {
+      setStatus('error');
+    }
+  };
+
+  const filterAttributes = (item) => {
+    const excludedKeys = ['documentId', 'id', 'createdAt', 'updatedAt', 'publishedAt'];
+    return Object.entries(item).filter(([key]) => !excludedKeys.includes(key));
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, [documentId]);
 
   React.useEffect(() => {
     const handleMessage = (event) => {
@@ -28,7 +83,8 @@ const PreviewComponent = () => {
       }
 
       if (data?.type === 'strapiUpdate') {
-        refetch();
+        // The data is stale, force a refetch
+        fetchData();
       } else if (data?.type === 'strapiScript') {
         const script = window.document.createElement('script');
         script.textContent = event.data.script;
@@ -68,25 +124,9 @@ const PreviewComponent = () => {
             paddingLeft={7}
           >
             <Typography variant="delta" tag="h3">
-              Details
+              URL metadata
             </Typography>
-
             <Grid.Root gap={5} tag="dl">
-              {document?.name && (
-                <Grid.Item col={6} s={12} direction="column" alignItems="start">
-                  <Typography variant="sigma" textColor="neutral600" tag="dt">
-                    Name
-                  </Typography>
-                  <Flex gap={3} direction="column" alignItems="start" tag="dd">
-                    <Typography
-                      // TODO: don't hardcode this
-                      data-strapisrc="name"
-                    >
-                      {document.name}
-                    </Typography>
-                  </Flex>
-                </Grid.Item>
-              )}
               <Grid.Item col={6} s={12} direction="column" alignItems="start">
                 <Typography variant="sigma" textColor="neutral600" tag="dt">
                   Content Type
@@ -108,7 +148,7 @@ const PreviewComponent = () => {
                   Status
                 </Typography>
                 <Flex gap={3} direction="column" alignItems="start" tag="dd">
-                  <Typography>{status}</Typography>
+                  <Typography>{documentStatus}</Typography>
                 </Flex>
               </Grid.Item>
               <Grid.Item col={6} s={12} direction="column" alignItems="start">
@@ -118,7 +158,40 @@ const PreviewComponent = () => {
                 <Typography tag="dd">{locale}</Typography>
               </Grid.Item>
             </Grid.Root>
-            {document && <JSONInput value={JSON.stringify(document, null, 2)} disabled />}
+            <Typography variant="delta" tag="h3">
+              Rest API data
+            </Typography>
+
+            {status === 'loading' && <Typography>Loading data...</Typography>}
+
+            {status === 'error' && (
+              <Typography textColor="danger600">Error loading data</Typography>
+            )}
+
+            {status === 'success' && !data && (
+              <Typography textColor="neutral600">No data found</Typography>
+            )}
+            {status === 'success' && data && (
+              <>
+                <Grid.Root gap={5} tag="dl">
+                  {filterAttributes(data).map(([key, value]) => (
+                    <Grid.Item key={key} col={6} s={12} direction="column" alignItems="start">
+                      <Typography variant="sigma" textColor="neutral600" tag="dt">
+                        {key}
+                      </Typography>
+                      <Flex gap={3} direction="column" alignItems="start" tag="dd">
+                        <Typography>
+                          {typeof value === 'object' && value !== null
+                            ? JSON.stringify(value, null, 2)
+                            : String(value)}
+                        </Typography>
+                      </Flex>
+                    </Grid.Item>
+                  ))}
+                </Grid.Root>
+                <JSONInput value={JSON.stringify(data, null, 2)} disabled />
+              </>
+            )}
           </Flex>
         </Layouts.Content>
       </Page.Main>
