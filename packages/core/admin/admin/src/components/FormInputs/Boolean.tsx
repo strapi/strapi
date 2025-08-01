bf535126c52a2689a3eb3248fdd393471cbd8be2
akash-dabhi-qed <akash.dabhi@qed42.com>
2025-08-01 18:08:13 +0530

    Revert "made boolean toggle nullable, added variations to the clear button"
    This reverts commit 5d6ff26cf1c680e53bbb25ff940101f02884520d.

--- packages/core/admin/admin/src/components/FormInputs/Boolean.tsx
+++ packages/core/admin/admin/src/components/FormInputs/Boolean.tsx
@@ -1,7 +1,6 @@
 import { forwardRef, memo } from 'react';
 
-import { Toggle, useComposedRefs, Field, Flex } from '@strapi/design-system';
-// import { Toggle, useComposedRefs, Field, Button } from '@strapi/design-system';
+import { Toggle, useComposedRefs, Field } from '@strapi/design-system';
 import { useIntl } from 'react-intl';
 
 import { useFocusInputField } from '../../hooks/useFocusInputField';
@@ -17,70 +16,12 @@ const BooleanInput = forwardRef<HTMLInputElement, InputProps>(
 
     const composedRefs = useComposedRefs(ref, fieldRef);
 
-    const handleToggleChange = () => {
-      let newValue;
-      if (field.value === null || field.value === undefined) {
-        newValue = true;
-      } else {
-        newValue = !field.value;
-      }
-      field.onChange(name, newValue);
-    };
-
-    const handleSetFalseClick = () => {
-      field.onChange(name, false);
-    };
-
-    const handleSetTrueClick = () => {
-      field.onChange(name, true);
-    };
-
-    // The key change is here: We check if the cleared value matches the initial value.
-    const handleClear = () => {
-      // The `useField` hook provides an `initialValue` property.
-      // We check if the field's initial value was `undefined` or `null`.
-      // If it was, we set the value back to that specific initial value.
-      const initialValue = field.initialValue;
-      if (initialValue === null || initialValue === undefined) {
-        field.onChange(name, initialValue);
-      } else {
-        // If the initial value was a boolean, we set it back to null to "clear" it.
-        field.onChange(name, null);
-      }
-    };
-
     return (
       <Field.Root error={field.error} name={name} hint={hint} required={required} maxWidth="320px">
-        <Flex justifyContent={'space-between'}>
-          <Field.Label action={labelAction}>{label}</Field.Label>
-          {(field.value === true || field.value === false) && (
-            <div
-              onClick={handleClear}
-              role="button"
-              tabIndex={0}
-              style={{ cursor: 'pointer', fontSize: '1.2rem' }}
-            >
-              <a href="#" style={{ textDecoration: 'none', color: '#7b79ff' }}>
-                {formatMessage({
-                  id: 'app.components.Boolean.clear',
-                  defaultMessage: 'Clear',
-                })}
-              </a>
-            </div>
-          )}
-        </Flex>
-        {/* <Field.Label action={labelAction}>{label}</Field.Label>
-        {(field.value === true || field.value === false) && (
-          <Button variant="tertiary" size="S" onClick={() => field.onChange(name, null)}>
-            {formatMessage({
-              id: 'app.components.Boolean.clear',
-              defaultMessage: 'Clear',
-            })}
-          </Button>
-        )} */}
+        <Field.Label action={labelAction}>{label}</Field.Label>
         <Toggle
           ref={composedRefs}
-          checked={field.value === false ? false : field.value === true ? true : null}
+          checked={field.value === null ? null : field.value || false}
           offLabel={formatMessage({
             id: 'app.components.ToggleCheckbox.off-label',
             defaultMessage: 'False',
@@ -89,37 +30,9 @@ const BooleanInput = forwardRef<HTMLInputElement, InputProps>(
             id: 'app.components.ToggleCheckbox.on-label',
             defaultMessage: 'True',
           })}
-          onChange={handleToggleChange}
+          onChange={field.onChange}
           {...props}
         />
-
-        {(field.value === null || field.value === undefined) && (
-          <div style={{ position: 'relative', top: '-36px', width: '100%', height: '36px' }}>
-            <div
-              style={{
-                position: 'absolute',
-                left: '0',
-                top: '0',
-                width: '50%',
-                height: '100%',
-                cursor: 'pointer',
-              }}
-              onClick={handleSetFalseClick}
-            />
-            <div
-              style={{
-                position: 'absolute',
-                right: '0',
-                top: '0',
-                width: '50%',
-                height: '100%',
-                cursor: 'pointer',
-              }}
-              onClick={handleSetTrueClick}
-            />
-          </div>
-        )}
-
         <Field.Hint />
         <Field.Error />
       </Field.Root>
