7fe987c68005ef9d04ff2051e257a2f8a5db239a
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2025-09-04 13:55:49 +0200

    chore: make email clickable in dummy preview

--- examples/getstarted/src/admin/preview/dummy-preview.jsx
+++ examples/getstarted/src/admin/preview/dummy-preview.jsx
@@ -3,7 +3,17 @@ import { useParams, useLoaderData, useRevalidator } from 'react-router-dom';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 
 import { Page, Layouts } from '@strapi/admin/strapi-admin';
-import { Grid, Flex, Typography, JSONInput, Box, IconButton } from '@strapi/design-system';
+import {
+  Grid,
+  Flex,
+  Typography,
+  JSONInput,
+  Box,
+  IconButton,
+  Link,
+  LinkButton,
+  Button,
+} from '@strapi/design-system';
 import { ChevronDown, ChevronRight } from '@strapi/icons';
 
 const filterAttributes = (item) => {
@@ -45,17 +55,20 @@ const ToggleableContainer = ({ headerText, children, isEmpty = false }) => {
   );
 };
 
-const NestedValue = ({ value, level = 0, arrayIndex = undefined }) => {
-  const isBlocksField =
-    Array.isArray(value) &&
-    value.length > 0 &&
-    value.every((item) => 'type' in item && 'children' in item);
-
-  if (isBlocksField) {
+const NestedValue = ({ value, level = 0, arrayIndex = undefined, fieldName = undefined }) => {
+  if (fieldName === 'blocks') {
     return (
       <Flex direction="column" alignItems="flex-start" fontSize="1.4rem" gap={2}>
         <BlocksRenderer content={value} />
       </Flex>
+    );
+  }
+
+  if (fieldName === 'email') {
+    return (
+      <Button variant="tertiary" onClick={() => window.alert('Clicked, the button was')}>
+        {value}
+      </Button>
     );
   }
 
@@ -69,13 +82,23 @@ const NestedValue = ({ value, level = 0, arrayIndex = undefined }) => {
           {value.map((item, index) => (
             <Box key={index}>
               {typeof item === 'object' && item !== null ? (
-                <NestedValue value={item} level={level + 1} arrayIndex={index} />
+                <NestedValue
+                  value={item}
+                  level={level + 1}
+                  arrayIndex={index}
+                  fieldName={fieldName}
+                />
               ) : (
                 <Flex direction="column" gap={1}>
                   <Typography variant="pi" textColor="neutral500">
                     [{index}]
                   </Typography>
-                  <NestedValue value={item} level={level + 1} arrayIndex={index} />
+                  <NestedValue
+                    value={item}
+                    level={level + 1}
+                    arrayIndex={index}
+                    fieldName={fieldName}
+                  />
                 </Flex>
               )}
             </Box>
@@ -99,7 +122,7 @@ const NestedValue = ({ value, level = 0, arrayIndex = undefined }) => {
               <Typography variant="sigma" textColor="neutral600">
                 {key}
               </Typography>
-              <NestedValue value={val} level={level + 1} />
+              <NestedValue value={val} level={level + 1} fieldName={key} />
             </Flex>
           ))}
         </Flex>
@@ -234,7 +257,7 @@ const PreviewComponent = () => {
                           {key}
                         </Typography>
                         <Flex gap={3} direction="column" alignItems="start" tag="dd">
-                          <NestedValue value={value} />
+                          <NestedValue value={value} fieldName={key} />
                         </Flex>
                       </Grid.Item>
                     ))}
