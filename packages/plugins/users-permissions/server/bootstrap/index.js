12c7a4518365adbfc2c7af288e6debad466f4c69
Ben Irvin <ben.irvin@strapi.io>
2025-09-15 09:20:21 +0200

    Update packages/plugins/users-permissions/server/bootstrap/index.js

--- packages/plugins/users-permissions/server/bootstrap/index.js
+++ packages/plugins/users-permissions/server/bootstrap/index.js
@@ -118,7 +118,7 @@ module.exports = async ({ strapi }) => {
   strapi.sessionManager.defineOrigin('users-permissions', {
     jwtSecret: upConfig.jwtSecret || strapi.config.get('admin.auth.secret'),
     accessTokenLifespan: upConfig.sessions?.accessTokenLifespan || 7 * 24 * 60 * 60,
-    maxRefreshTokenLifespan: upConfig.sessions?.maxRefreshTokenLifespan || 60 * 24 * 60 * 60,
+    maxRefreshTokenLifespan: upConfig.sessions?.maxRefreshTokenLifespan || 30 * 24 * 60 * 60,
     idleRefreshTokenLifespan: upConfig.sessions?.idleRefreshTokenLifespan || 7 * 24 * 60 * 60,
     maxSessionLifespan: upConfig.sessions?.maxRefreshTokenLifespan || 60 * 24 * 60 * 60,
     idleSessionLifespan: upConfig.sessions?.idleRefreshTokenLifespan || 7 * 24 * 60 * 60,
