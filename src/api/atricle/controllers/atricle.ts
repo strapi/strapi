528cb9d7b1931438e9f558232675f518ca6be7af
Jose <josefernandez.developer@gmail.com>
2025-09-23 21:33:13 -0500

    feat: se añaden controladores y rutas para el modelo de artículo en Strapi

---
+++ src/api/atricle/controllers/atricle.ts
@@ -0,0 +1,7 @@
+/**
+ * atricle controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::atricle.atricle');
---
+++ src/api/atricle/routes/atricle.ts
@@ -0,0 +1,7 @@
+/**
+ * atricle router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::atricle.atricle');
