84333d2ac3627fe242ef111d6beaf45c862435af
Ayushd785 <ayushd785@gmail.com>
2025-10-16 03:45:32 +0530

    fix: format code with prettier

--- packages/core/upload/admin/src/pages/SettingsPage/SettingsPage.tsx
+++ packages/core/upload/admin/src/pages/SettingsPage/SettingsPage.tsx
@@ -59,7 +59,7 @@ export const SettingsPage = () => {
           message: formatMessage({ id: 'notification.form.success.fields' }),
         });
       },
-      onError(err:any) {
+      onError(err: any) {
         toggleNotification({
           type: 'danger',
           message: err.message || formatMessage({ id: 'notification.error' }),
