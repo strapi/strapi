5f1051155c1f02cb3d46e97320c503d8d443eaed
abhinav <abhinavabhi7575@gmail.com>
2025-10-17 16:10:31 +0530

    useFolder hook to get the current folder path

--- packages/core/upload/admin/src/components/AssetDialog/AssetDialog.tsx
+++ packages/core/upload/admin/src/components/AssetDialog/AssetDialog.tsx
@@ -33,6 +33,7 @@ import { SelectedStep } from './SelectedStep/SelectedStep';
 import type { File as Asset, FilterCondition, Query } from '../../../../shared/contracts/files';
 import type { Folder, FolderDefinition } from '../../../../shared/contracts/folders';
 import type { AllowedTypes } from '../AssetCard/AssetCard';
+import { useFolder } from '../../hooks/useFolder';
 
 const LoadingBody = styled(Flex)`
   /* 80px are coming from the Tabs component that is not included in the ModalBody */
@@ -107,6 +108,17 @@ export const AssetContent = ({
   } = useAssets({ skipWhen: !canRead, query: queryObject });
 
   const {
+    data: currentFolder,
+    isLoading: isCurrentFolderLoading,
+    error: currentFolderError,
+  } = useFolder(folderId as number | null | undefined, {
+    enabled: canRead && !!folderId,
+  });
+  if (currentFolder) {
+    queryObject!.folderPath = currentFolder.path;
+  }
+
+  const {
     data: folders,
     isLoading: isLoadingFolders,
     error: errorFolders,
@@ -143,8 +155,8 @@ export const AssetContent = ({
     return multiple ? selectOne(asset as Asset) : selectOnly(asset as Asset);
   };
 
-  const isLoading = isLoadingPermissions || isLoadingAssets || isLoadingFolders;
-  const hasError = errorAssets || errorFolders;
+  const isLoading = isLoadingPermissions || isLoadingAssets || isLoadingFolders || isCurrentFolderLoading;
+  const hasError = errorAssets || errorFolders || currentFolderError;
 
   const [activeTab, setActiveTab] = React.useState(
     selectedAssets.length > 0 ? 'selected' : 'browse'
