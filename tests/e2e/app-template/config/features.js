ac082884478035e5298ea3de55b8b926280f4e25
Mark Kaylor <mark.kaylor@strapi.io>
2025-10-17 13:02:40 +0200

    chore: always run e2e test for unstable feature

--- tests/e2e/app-template/config/features.js
+++ tests/e2e/app-template/config/features.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   future: {
-    unstableAILocalizations: env.bool('STRAPI_FEATURES_UNSTABLE_AI_LOCALIZATIONS', false),
+    unstableAILocalizations: true,
   },
 });
