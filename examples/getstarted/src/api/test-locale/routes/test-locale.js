4f053eb18d3f09c1641fe82b77a7c80b9020b755
Araksya Gevorgyan <araks.gevorkyan@gmail.com>
2025-10-17 11:11:25 +0200

    feature(i18n): adding single type related schema for testing

---
+++ examples/getstarted/src/api/test-locale/routes/test-locale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-locale router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::test-locale.test-locale');
