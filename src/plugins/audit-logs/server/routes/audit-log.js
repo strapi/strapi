e5967107190eabc843cd4cb274156b17ebdd5ee1
namankau <naman.kaushik06@gmail.com>
2025-10-24 22:55:28 +0530

    Add GET route for fetching audit logs

--- src/plugins/audit-logs/server/routes/audit-log.js
+++ src/plugins/audit-logs/server/routes/audit-log.js
@@ -1,1 +1,16 @@
+'use strict';
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/audit-logs',
+    handler: 'audit-log.find',
+    config: {
+      auth: {
+        scope: ['plugin::users-permissions.read_audit_logs'],
+      },
+      policies: ['plugin::audit-logs.read-audit-logs'],
+    },
+  },
+];
 
