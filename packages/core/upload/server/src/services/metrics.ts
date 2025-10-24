e2f651aaac4c7888f29da54fca56a434db443b13
Mark Kaylor <mark.kaylor@strapi.io>
2025-10-24 14:00:25 +0200

    chore: update test and remove log

--- packages/core/upload/server/src/services/metrics.ts
+++ packages/core/upload/server/src/services/metrics.ts
@@ -7,7 +7,7 @@ const isProviderPrivate = async () => strapi.plugin('upload').provider.isPrivate
 export default ({ strapi }: { strapi: Core.Strapi }) => ({
   async trackUsage(event: string, properties?: Record<string, any>) {
     const settings: Settings = await strapi.plugin('upload').service('upload').getSettings();
-    console.log('did do something I want', settings.aiMetadata);
+
     return strapi.telemetry.send(event, {
       ...properties,
       eventProperties: {
