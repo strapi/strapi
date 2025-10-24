6e8923d82b30dd59f1c53fa1916aaa06c88b66d5
namankau <naman.kaushik06@gmail.com>
2025-10-24 22:54:37 +0530

    Implement find method in audit-log controller

--- src/plugins/audit-logs/server/controllers/audit-log.js
+++ src/plugins/audit-logs/server/controllers/audit-log.js
@@ -1,1 +1,12 @@
+'use strict';
+
+module.exports = {
+  async find(ctx) {
+    const { query } = ctx;
+    const auditService = strapi.plugin('audit-logs').service('auditLog');
+
+    const result = await auditService.find(query);
+    ctx.body = result;
+  },
+};
 
