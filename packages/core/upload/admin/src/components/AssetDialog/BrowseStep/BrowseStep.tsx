9d09605599a7f33cd2867ca58e2880badd212ad2
dannyweb <62189099+dagadevelop@users.noreply.github.com>
2025-10-28 09:08:21 -0400

    fix: use native component property
    Change inline style to props

--- packages/core/upload/admin/src/components/AssetDialog/BrowseStep/BrowseStep.tsx
+++ packages/core/upload/admin/src/components/AssetDialog/BrowseStep/BrowseStep.tsx
@@ -459,7 +459,7 @@ export const BrowseStep = ({
           paddingTop={4}
           position="relative"
           zIndex={1}
-          style={{ overflow: 'hidden' }}
+          overflow="hidden"
         >
           <PageSize
             pageSize={queryObject.pageSize! as number}
