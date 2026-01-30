0b435307825403a812aef3d621eaa7cc442acfb5
pksr <pksr@users.noreply.github.com>
2026-01-30 06:54:55 +0100

    Changed validation if not using draft and published

--- packages/core/content-manager/admin/src/pages/EditView/components/DocumentActions.tsx
+++ packages/core/content-manager/admin/src/pages/EditView/components/DocumentActions.tsx
@@ -1090,6 +1090,9 @@ const UpdateAction: DocumentActionComponent = ({
   );
   const { schema } = useDoc();
 
+  const suitableSchema = fromRelationModal ? relationalModalSchema : schema
+  const hasDraftAndPublished = suitableSchema.options?.draftAndPublish ?? false;
+
   const handleUpdate = async () => {
     setSubmitting(true);
 
@@ -1099,7 +1102,8 @@ const UpdateAction: DocumentActionComponent = ({
       }
 
       const { errors } = await validate(true, {
-        status: 'draft',
+        // enforce "published" validation if not using "draft and published"
+        status: !hasDraftAndPublished ? 'published' : 'draft',
       });
 
       if (errors) {
@@ -1141,7 +1145,7 @@ const UpdateAction: DocumentActionComponent = ({
         }
       } else if (documentId || collectionType === SINGLE_TYPES) {
         const { data } = handleInvisibleAttributes(transformData(document), {
-          schema: fromRelationModal ? relationalModalSchema : schema,
+          schema: suitableSchema,
           initialValues,
           components,
         });
