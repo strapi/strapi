01c6ddf65fe0d51de208e04bb3dd7eb0e39a1515
kpcode11 <prajapatikeshav497@gmail.com>
2026-02-02 15:40:44 +0530

    feat: add sample content type and documentation 3

--- my-strapi-task1/config/database.ts
+++ my-strapi-task1/config/database.ts
@@ -1,5 +1,22 @@
 import path from 'node:path';
 
+function sslOptions(env) {
+  if (!env.bool('DATABASE_SSL', false)) return undefined;
+
+  return {
+    key: env('DATABASE_SSL_KEY', undefined),
+    cert: env('DATABASE_SSL_CERT', undefined),
+    ca: env('DATABASE_SSL_CA', undefined),
+    capath: env('DATABASE_SSL_CAPATH', undefined),
+    cipher: env('DATABASE_SSL_CIPHER', undefined),
+    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+  };
+}
+
+function poolConfig(env) {
+  return { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) };
+}
+
 export default function database({ env }) {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
@@ -11,16 +28,9 @@ export default function database({ env }) {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        ssl: sslOptions(env),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: poolConfig(env),
     },
     postgres: {
       connection: {
@@ -30,17 +40,10 @@ export default function database({ env }) {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        ssl: sslOptions(env),
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: poolConfig(env),
     },
     sqlite: {
       connection: {
