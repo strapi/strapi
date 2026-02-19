41f5ecd9cddd76d1a009886ef7991158123ff78c
Owen Rossi-Keen <owenrossikeen@gmail.com>
2026-02-19 17:18:52 -0500

    feat(content-manager): translate status query filter into document service params
    Transform a "status" filter in URL query to a top level document-service param

--- packages/core/content-manager/admin/src/utils/api.ts
+++ packages/core/content-manager/admin/src/utils/api.ts
@@ -17,6 +17,51 @@ type TransformedQuery<TQuery extends Query> = Omit<TQuery, 'plugins'> & {
 
 /**
  * @description
+ * Extracts status filter from filters.$and and transforms it to a top-level
+ * "status" or "hasPublishedVersion" query param
+ */
+const extractStatusFilter = <T extends Record<string, any>>(params: T): T => {
+  const filters = params?.filters;
+  if (!filters?.$and || !Array.isArray(filters.$and)) return params;
+
+  const remainingFilters = [];
+  const statusFilters = [];
+
+  for (const filter of filters.$and) {
+    if (!filter || typeof filter !== 'object' || !('status' in filter) || !filter.status?.$eq) {
+      remainingFilters.push(filter);
+    } else {
+      statusFilters.push(filter);
+    }
+  }
+
+  if (statusFilters.length === 0) return params;
+
+  const modifiedQuery: Record<string, any> = {
+    ...params,
+    filters: remainingFilters.length > 0 ? { ...filters, $and: remainingFilters } : undefined,
+  };
+
+  // Currently the UI has no logic to prevent multiple filters being applied to an enum field.
+  // Technically, multiple filters on an enum field should produce no result, which is why
+  // this has the ability to apply mutually exclusive filters.
+  for (const statusFilter of statusFilters) {
+    const value = statusFilter.status.$eq;
+
+    if (value === 'published') {
+      modifiedQuery.status = 'published';
+    }
+
+    if (value === 'draft') {
+      modifiedQuery.hasPublishedVersion = 'false';
+    }
+  }
+
+  return modifiedQuery as T;
+};
+
+/**
+ * @description
  * Creates a valid query params object for get requests
  * ie. plugins[i18n][locale]=en becomes locale=en
  */
@@ -32,7 +77,10 @@ const buildValidParams = <TQuery extends Query>(query: TQuery): TransformedQuery
     ),
   };
 
-  return validQueryParams;
+  // Hoist status filter to the top level of the query params
+  const validQueryParamsWithStatus = extractStatusFilter(validQueryParams);
+
+  return validQueryParamsWithStatus;
 };
 
 type BaseQueryError = ApiError | UnknownApiError;
