58dd700465dbc433183aa17b0975ae078dcecbd4
Andrea Cappuccio <hood@null.net>
2026-03-04 17:31:14 +0100

    chore: fix test

--- packages/core/content-manager/admin/src/pages/ListView/components/BulkActions/tests/formatErrorMessages.test.ts
+++ packages/core/content-manager/admin/src/pages/ListView/components/BulkActions/tests/formatErrorMessages.test.ts
@@ -3,19 +3,13 @@ import { formatErrorMessages } from '../PublishAction';
 describe('formatErrorMessages', () => {
   it('should format error messages correctly', () => {
     const errors = {
-      errors: {
-        Content: [
-          {
-            cards: {
-              id: 'components.Input.error.validation.required',
-              defaultMessage: 'This value is required.',
-            },
-          },
-        ],
+      'Content.0.cards': {
+        id: 'components.Input.error.validation.required',
+        defaultMessage: 'This value is required.',
       },
     };
 
-    const formattedMessages = formatErrorMessages(errors, '', () => (msg) => msg);
+    const formattedMessages = formatErrorMessages(errors, '', (msg) => msg.defaultMessage);
 
     expect(formattedMessages).toEqual(['This value is required.']);
   });
