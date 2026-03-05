aa88fcdcf337c5cefaa7703987356d9c3cfdffbc
Ben Irvin <ben@innerdvations.com>
2026-03-05 09:23:10 +0100

    fix: separate coverage

--- packages/core/openapi/jest.config.js
+++ packages/core/openapi/jest.config.js
@@ -7,6 +7,17 @@ module.exports = {
   },
   testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
   displayName: 'Strapi - OpenAPI',
+  // Override root collectCoverageFrom so coverage is collected from this package when run via root jest (CI).
+  // Root config uses <rootDir>/packages/** which resolves to packages/core/openapi/packages/** otherwise.
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,ts}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/*.config.{js,ts,mjs}',
+    '!**/jest*.{js,ts}',
+    '!**/rollup*.{js,ts,mjs}',
+  ],
   coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
   coverageThreshold: {
     global: {
