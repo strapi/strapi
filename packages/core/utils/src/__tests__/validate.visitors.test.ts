c45d9ccf5a990e6f780dfea04fa3b5593d1dd984
Ben Irvin <ben@innerdvations.com>
2026-03-30 14:04:37 +0200

    test(utils): cover invalid populate on morphTo relations without on

--- packages/core/utils/src/__tests__/validate.visitors.test.ts
+++ packages/core/utils/src/__tests__/validate.visitors.test.ts
@@ -295,6 +295,57 @@ describe('Validate visitors util', () => {
         ValidationError
       );
     });
+
+    test('throws ValidationError for invalid populate in polymorphic structures (morphTo relation without on)', async () => {
+      const morphTargetModel: any = {
+        uid: 'api::target.target',
+        modelType: 'contentType',
+        kind: 'collectionType',
+        info: { singularName: 'target', pluralName: 'targets', displayName: 'Target' },
+        options: {},
+        attributes: {
+          id: { type: 'integer' },
+        },
+      };
+
+      const morphHostModel: any = {
+        uid: 'api::host.host',
+        modelType: 'contentType',
+        kind: 'collectionType',
+        info: { singularName: 'host', pluralName: 'hosts', displayName: 'Host' },
+        options: {},
+        attributes: {
+          id: { type: 'integer' },
+          morphLink: {
+            type: 'relation',
+            relation: 'morphToOne',
+            target: 'api::target.target',
+          },
+        },
+      };
+
+      const models = {
+        'api::host.host': morphHostModel,
+        'api::target.target': morphTargetModel,
+      };
+
+      const getModelMock = (uid: string) => models[uid];
+
+      const ctxMorph: any = {
+        schema: morphHostModel,
+        getModel: getModelMock,
+      };
+
+      const invalidPopulate = {
+        morphLink: {
+          populate: 'deep',
+        },
+      };
+
+      await expect(validators.defaultValidatePopulate(ctxMorph, invalidPopulate)).rejects.toThrow(
+        ValidationError
+      );
+    });
   });
 
   describe('throwRestrictedRelations', () => {
