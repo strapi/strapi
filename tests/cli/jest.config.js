8d0dd9dfa3830a44127276015ce0333a96d20c8e
Ben Irvin <ben@innerdvations.com>
2026-04-15 10:56:10 +0200

    fix: find test files if parent path includes cache

--- tests/cli/jest.config.js
+++ tests/cli/jest.config.js
@@ -10,7 +10,7 @@ const config = {
   transform: {
     '^.+\\.ts$': ['@swc/jest'],
   },
-  modulePathIgnorePatterns: ['.cache'],
+  modulePathIgnorePatterns: ['(?:^|[\\\\/])\\.cache(?:[\\\\/]|$)'],
 };
 
 module.exports = config;
