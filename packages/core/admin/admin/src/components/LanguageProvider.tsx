c962040aeeb6eb92abafa2297e994b03ec123228
vibhu gupta <vibhugupta022@gmail.com>
2026-04-24 02:01:54 +0530

    fix(admin): suppress MISSING_TRANSLATION errors to fix sluggish editing

--- packages/core/admin/admin/src/components/LanguageProvider.tsx
+++ packages/core/admin/admin/src/components/LanguageProvider.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import defaultsDeep from 'lodash/defaultsDeep';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, ReactIntlErrorCode } from 'react-intl';
 
 import { useTypedSelector } from '../core/store/hooks';
 
@@ -18,8 +18,24 @@ const LanguageProvider = ({ children, messages }: LanguageProviderProps) => {
   const locale = useTypedSelector((state) => state.admin_app.language.locale);
   const appMessages = defaultsDeep(messages[locale], messages.en);
 
+  const handleIntlError = React.useCallback(
+    (err: Parameters<NonNullable<React.ComponentProps<typeof IntlProvider>['onError']>>[0]) => {
+      if (err.code === ReactIntlErrorCode.MISSING_TRANSLATION) {
+        return;
+      }
+      console.error(err);
+    },
+    []
+  );
+
   return (
-    <IntlProvider locale={locale} defaultLocale="en" messages={appMessages} textComponent="span">
+    <IntlProvider
+      locale={locale}
+      defaultLocale="en"
+      messages={appMessages}
+      textComponent="span"
+      onError={handleIntlError}
+    >
       {children}
     </IntlProvider>
   );
