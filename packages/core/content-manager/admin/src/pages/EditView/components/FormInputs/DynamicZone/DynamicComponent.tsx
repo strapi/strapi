4694aeb9346acf51d17aa23a75d85cf7531ef795
Maksim Zhukau <maks_zhukov_97@mail.ru>
2026-06-26 16:16:00 +0200

    fix(content-manager): capitalize component category names in dynamic zone (closes #24426) (#26337)

--- packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/DynamicZone/DynamicComponent.tsx
+++ packages/core/content-manager/admin/src/pages/EditView/components/FormInputs/DynamicZone/DynamicComponent.tsx
@@ -12,6 +12,7 @@ import {
   BoxComponent,
 } from '@strapi/design-system';
 import { Drag, More, Trash, ArrowUp, ArrowDown } from '@strapi/icons';
+import upperFirst from 'lodash/upperFirst';
 import { getEmptyImage } from 'react-dnd-html5-backend';
 import { useIntl } from 'react-intl';
 import { styled } from 'styled-components';
@@ -281,7 +282,7 @@ const DynamicComponent = ({
               {Object.entries(dynamicComponentsByCategory).map(([category, components]) => (
                 <React.Fragment key={category}>
                   <Menu.Label>
-                    {formatMessage({ id: category, defaultMessage: category })}
+                    {formatMessage({ id: category, defaultMessage: upperFirst(category) })}
                   </Menu.Label>
                   {components.map(({ displayName, uid }) => (
                     <Menu.Item key={uid} onSelect={() => onAddComponent(uid, index)}>
@@ -303,7 +304,7 @@ const DynamicComponent = ({
               {Object.entries(dynamicComponentsByCategory).map(([category, components]) => (
                 <React.Fragment key={category}>
                   <Menu.Label>
-                    {formatMessage({ id: category, defaultMessage: category })}
+                    {formatMessage({ id: category, defaultMessage: upperFirst(category) })}
                   </Menu.Label>
                   {components.map(({ displayName, uid }) => (
                     <Menu.Item key={uid} onSelect={() => onAddComponent(uid, index + 1)}>
