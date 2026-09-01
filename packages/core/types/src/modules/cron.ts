7636d528240eec03c07b50266dc5adbab0132ef3
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2026-09-01 16:10:57 +0100

    fix(core): allow synchronous cron task handlers

--- packages/core/types/src/modules/cron.ts
+++ packages/core/types/src/modules/cron.ts
@@ -8,7 +8,7 @@ interface JobSpec {
   name: string | null;
 }
 
-type TaskFn = ({ strapi }: { strapi: Strapi }, ...args: unknown[]) => Promise<unknown>;
+type TaskFn = ({ strapi }: { strapi: Strapi }, ...args: unknown[]) => Promise<unknown> | void;
 
 type Task =
   | TaskFn
