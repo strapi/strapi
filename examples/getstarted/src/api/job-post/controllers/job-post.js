156bda8e98be74e25343906867292744ebe1b4d6
ayushnegiexe <ayushnegiexe@gmail.com>
2026-09-02 11:07:26 +0530

    feat(getstarted): add resilient Hacker News ingestion example

---
+++ examples/getstarted/src/api/job-post/controllers/job-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * job-post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::job-post.job-post');
