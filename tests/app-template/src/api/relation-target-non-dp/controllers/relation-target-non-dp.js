cf8be3b7d415c2c373dd042873473fa1fdce6aed
Bassel Kanso <basselkanso82@gmail.com>
2026-09-10 12:31:59 +0300

    fix(content-manager): allow re-adding draft relations retained in published entries

---
+++ tests/app-template/src/api/relation-target-non-dp/controllers/relation-target-non-dp.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::relation-target-non-dp.relation-target-non-dp');
