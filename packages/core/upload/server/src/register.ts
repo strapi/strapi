a954d6c2ff71979bc2296e9c861d4b0b1ac09024
Luca Saba <luca.saba@facile.it>
2026-09-16 11:31:44 +0200

    fix: add replace and replaceStream wrapped provider options

--- packages/core/upload/server/src/register.ts
+++ packages/core/upload/server/src/register.ts
@@ -109,7 +109,23 @@ const createProvider = (config: Config) => {
     );
   }
 
+  if (!providerInstance.replace && !providerInstance.replaceStream) {
+    throw new Error(
+      `The upload provider "${providerName}" doesn't implement the replaceStream nor the replace method.`
+    );
+  }
+
+  if (!providerInstance.replaceStream) {
+    process.emitWarning(
+      `The upload provider "${providerName}" doesn't implement the replaceStream function. Strapi will fallback on the replace method. Some performance issues may occur.`
+    );
+  }
+
   const wrappedProvider = _.mapValues(providerInstance, (method, methodName) => {
+    if (methodName === 'replace' || methodName === 'replaceStream') {
+      return async (newFile: File, oldFile: File, options = actionOptions[methodName]) =>
+        providerInstance[methodName](newFile, oldFile, options);
+    }
     return async (file: File, options = actionOptions[methodName]) =>
       providerInstance[methodName](file, options);
   });
