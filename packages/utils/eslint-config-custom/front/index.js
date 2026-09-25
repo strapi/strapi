c79165c98e372b67351175819841f86265a2df38
nclsndr <nicolas.andre@strapi.io>
2026-09-25 15:57:43 +0200

    chore(lint): prevent lodash/fp imports
    Reject FP root and subpath imports, re-exports and CommonJS requires. Retain the existing frontend restriction on broad lodash imports.

--- packages/utils/eslint-config-custom/front/index.js
+++ packages/utils/eslint-config-custom/front/index.js
@@ -53,6 +53,12 @@ const config = {
     'no-restricted-imports': [
       'error',
       {
+        patterns: [
+          {
+            group: ['lodash/fp', 'lodash/fp/**'],
+            message: 'Use regular lodash methods or a focused helper instead.',
+          },
+        ],
         paths: [
           {
             name: 'lodash',
@@ -61,6 +67,7 @@ const config = {
         ],
       },
     ],
+    'no-restricted-modules': ['error', { patterns: ['lodash/fp', 'lodash/fp/**'] }],
     'no-restricted-globals': [
       'error',
       {
--- packages/utils/eslint-config-custom/front/typescript.js
+++ packages/utils/eslint-config-custom/front/typescript.js
@@ -36,6 +36,12 @@ const config = {
     'no-restricted-imports': [
       'error',
       {
+        patterns: [
+          {
+            group: ['lodash/fp', 'lodash/fp/**'],
+            message: 'Use regular lodash methods or a focused helper instead.',
+          },
+        ],
         paths: [
           {
             name: 'lodash',
@@ -44,6 +50,7 @@ const config = {
         ],
       },
     ],
+    'no-restricted-modules': ['error', { patterns: ['lodash/fp', 'lodash/fp/**'] }],
     'no-restricted-globals': [
       'error',
       {
